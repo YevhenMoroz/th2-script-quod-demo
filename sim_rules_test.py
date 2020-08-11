def773d480ee8ae55e32225243f2343744419a94
andrey.bakhirev <andrey.bakhirev@exactprosystems.com>
2020-08-11 06:15:33 -0700

    added support sim request

--- sim_rules_test.py
+++ sim_rules_test.py
@@ -1,7 +1,8 @@
 from grpc_modules import quod_simulator_pb2
-from grpc_modules import quod_simulator_pb2_grpc
+from grpc_modules import quod_simulator_pb2_grpc, infra_pb2
 from grpc_modules import simulator_pb2
 import grpc
 
-simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(grpc.insecure_channel('10.44.74.110:63342'))
-print(simulator.createQuodNOSRule(request=simulator_pb2.RuleID(id=1)))
+simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(grpc.insecure_channel('localhost:8081'))
+print(simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child'))))
+# print(simulator.createRule_FIX(request=quod_simulator_pb2.TemplateFixCreate(fields = {"a":infra_pb2.Value(simple_value="123")}, connection_id=infra_pb2.ConnectionID(session_alias="123"))))
