1127054e327f630cec8c276d84020ce6931dd118
andrey.bakhirev <andrey.bakhirev@exactprosystems.com>
2020-08-12 08:33:11 -0700

    added support sim request and sim stop

--- sim_rules_test.py
+++ sim_rules_test.py
@@ -1,8 +1,13 @@
 from grpc_modules import quod_simulator_pb2
-from grpc_modules import quod_simulator_pb2_grpc, infra_pb2
+from grpc_modules import quod_simulator_pb2_grpc, infra_pb2, simulator_pb2_grpc
 from grpc_modules import simulator_pb2
 import grpc
 
-simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(grpc.insecure_channel('localhost:8081'))
-print(simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child'))))
-# print(simulator.createRule_FIX(request=quod_simulator_pb2.TemplateFixCreate(fields = {"a":infra_pb2.Value(simple_value="123")}, connection_id=infra_pb2.ConnectionID(session_alias="123"))))
+#start rule
+channel = grpc.insecure_channel('localhost:8081')
+simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
+QuodRuleID = simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
+print(QuodRuleID)
+# stop rule
+# core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
+# core.removeRule(QuodRuleID)
