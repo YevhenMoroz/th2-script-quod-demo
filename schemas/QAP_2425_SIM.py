797e9a7131b9107714a9063f40b3ec81c7ea7a05
andrey.bakhirev <andrey.bakhirev@exactprosystems.com>
2020-08-25 08:19:57 -0700

    added grpc calls for demo

--- schemas/QAP_2425_SIM.py
+++ schemas/QAP_2425_SIM.py
@@ -11,9 +11,9 @@ from grpc_modules import simulator_pb2
 import grpc
 
 channel = grpc.insecure_channel('localhost:8081')
-simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
-NOS = simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
-OCR = simulator.createQuodOCRRule(request=quod_simulator_pb2.TemplateQuodOCRRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
+# simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
+# NOS = simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
+# OCR = simulator.createQuodOCRRule(request=quod_simulator_pb2.TemplateQuodOCRRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -402,7 +402,7 @@ def execute(case_name, report_id, case_params):
     logger.info("Case {} was executed in {} sec.".format(
         case_name, str(round(datetime.now().timestamp() - seconds))))
 
-    # stop rule
-    core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
-    core.removeRule(NOS)
-    core.removeRule(OCR)
+    # # stop rule
+    # core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
+    # core.removeRule(NOS)
+    # core.removeRule(OCR)
--- sim_rules_test.py
+++ sim_rules_test.py
@@ -3,12 +3,14 @@ from grpc_modules import quod_simulator_pb2_grpc, infra_pb2, simulator_pb2_grpc
 from grpc_modules import simulator_pb2
 import grpc
 
-#start rule
+# start rule
 channel = grpc.insecure_channel('localhost:8081')
 simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
-NOS = simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
-OCR = simulator.createQuodOCRRule(request=quod_simulator_pb2.TemplateQuodOCRRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
-# OCRR = simulator.createQuodOCRRRule(request=quod_simulator_pb2_grpc_simulator_pb2.TemplateQuodOCRRRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
+DemoRule = simulator.createTemplateQuodDemoRule(
+    request=quod_simulator_pb2.TemplateQuodDemoRule(
+        connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child'),
+        demo_field1=123,
+        demo_field2='KCH_QA_RET_CHILD'))
 # stop rule
-# core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
-# core.removeRule(NOS)
+core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
+core.removeRule(DemoRule)
