0c3b6ef223d9df3c25860b984cfeb3163b2c994f
andrey.bakhirev <andrey.bakhirev@exactprosystems.com>
2020-08-26 05:48:41 -0700

    added grpc calls for demo. UPD

--- schemas/QAP_2425_SIM.py
+++ schemas/QAP_2425_SIM.py
@@ -10,10 +10,16 @@ from grpc_modules import quod_simulator_pb2_grpc, infra_pb2
 from grpc_modules import simulator_pb2
 import grpc
 
+#start rule
 channel = grpc.insecure_channel('localhost:8081')
-# simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
-# NOS = simulator.createQuodNOSRule(request=quod_simulator_pb2.TemplateQuodNOSRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
-# OCR = simulator.createQuodOCRRule(request=quod_simulator_pb2.TemplateQuodOCRRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
+simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
+DemoRule = simulator.createTemplateQuodDemoRule(
+    request=quod_simulator_pb2.TemplateQuodDemoRule(
+        connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child'),
+        demo_field1=123,
+        demo_field2='KCH_QA_RET_CHILD'))
+
+OCR = simulator.createQuodOCRRule(request=quod_simulator_pb2.TemplateQuodOCRRule(connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child')))
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -404,5 +410,7 @@ def execute(case_name, report_id, case_params):
 
     # # stop rule
     # core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
-    # core.removeRule(NOS)
-    # core.removeRule(OCR)
+    # stop rule
+    core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
+    core.removeRule(DemoRule)
+    core.removeRule(OCR)
