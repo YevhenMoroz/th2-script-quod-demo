c15ec22daf7377c73cfed9e6bb871df9b8f97ddf
dmitry.zhuravlev <T0wer_Enc0unter>
2020-12-21 17:48:11 +0300

    QAP-2769 was added

---
+++ test_cases/QAP_2769.py
@@ -0,0 +1,213 @@
+import logging
+from copy import deepcopy
+import time
+from datetime import datetime
+from custom import basic_custom_actions as bca
+from grpc_modules import infra_pb2
+from grpc_modules.infra_pb2 import Direction, ConnectionID
+from grpc_modules.quod_simulator_pb2 import RequestMDRefID
+from stubs import Stubs
+from grpc_modules.quod_simulator_pb2 import TemplateQuodSingleExecRule, TemplateNoPartyIDs
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+timeouts = False
+
+
+def execute(report_id):
+    act = Stubs.fix_act
+    verifier = Stubs.verifier
+    simulator = Stubs.simulator
+    sim = Stubs.sim
+
+    seconds, nanos = bca.timestamps()  # Store case start time
+    case_name = "QAP-2769"
+
+    # Create sub-report for case
+    case_id = bca.create_event(case_name, report_id)
+
+    case_params = {
+        'TraderConnectivity': 'gtwquod3',
+        'TraderConnectivity2': 'fix-bs-eq-paris',
+        'TraderConnectivity3': 'fix-bs-eq-trqx',
+        'SenderCompID': 'QUODFX_UAT',
+        'TargetCompID': 'QUOD3',
+        'SenderCompID2': 'KCH_QA_RET_CHILD',
+        'TargetCompID2': 'QUOD_QA_RET_CHILD',
+        'Account': 'KEPLER',
+        'HandlInst': '2',
+        'Side': '2',
+        'OrderQty': 1000,
+        'OrdType': '2',
+        'Price': 25,
+        'TimeInForce': '0',
+        'TargetStrategy': 1011,
+        'Instrument': {
+            'Symbol': 'FR0000125460_EUR',
+            'SecurityID': 'FR0000125460',
+            'SecurityIDSource': '4',
+            'SecurityExchange': 'XPAR'
+        }
+    }
+    reusable_order_params = {  # This parameters can be used for ExecutionReport message
+        'Account': case_params['Account'],
+        'HandlInst': case_params['HandlInst'],
+        'Side': case_params['Side'],
+        'TimeInForce': case_params['TimeInForce'],
+        'OrdType': case_params['OrdType'],
+        'OrderCapacity': 'A',
+        'Currency': 'EUR',
+        'TargetStrategy': case_params['TargetStrategy']
+    }
+    symbol_1 = "596"
+    symbol_2 = "3390"
+    trade_rule_1 = simulator.createQuodSingleExecRule(request=TemplateQuodSingleExecRule(
+        connection_id=ConnectionID(session_alias="fix-bs-eq-paris"),
+        no_party_ids=[
+            TemplateNoPartyIDs(party_id="KEPLER", party_id_source="D", party_role="1"),
+            TemplateNoPartyIDs(party_id="1", party_id_source="D", party_role="2"),
+            TemplateNoPartyIDs(party_id="2", party_id_source="D", party_role="3")
+        ],
+        cum_qty=int(case_params['OrderQty'] / 2),
+        mask_as_connectivity="fix-fh-eq-paris",
+        md_entry_size={500: 0},
+        md_entry_px={30: 25},
+        symbol=symbol_1
+    ))
+    trade_rule_2 = simulator.createQuodSingleExecRule(request=TemplateQuodSingleExecRule(
+        connection_id=ConnectionID(session_alias="fix-bs-eq-trqx"),
+        no_party_ids=[
+            TemplateNoPartyIDs(party_id="KEPLER", party_id_source="D", party_role="1"),
+            TemplateNoPartyIDs(party_id="1", party_id_source="D", party_role="2"),
+            TemplateNoPartyIDs(party_id="2", party_id_source="D", party_role="3")
+        ],
+        cum_qty=int(case_params['OrderQty'] / 2),
+        mask_as_connectivity="fix-fh-eq-trqx",
+        md_entry_size={500: 0},
+        md_entry_px={30: 25},
+        symbol=symbol_2
+    ))
+
+    # Send MarketDataSnapshotFullRefresh message
+
+    MDRefID_1 = simulator.getMDRefIDForConnection(request=RequestMDRefID(
+        symbol=symbol_1,
+        connection_id=ConnectionID(session_alias="fix-fh-eq-paris")
+    )).MDRefID
+    MDRefID_2 = simulator.getMDRefIDForConnection(request=RequestMDRefID(
+        symbol=symbol_2,
+        connection_id=ConnectionID(session_alias="fix-fh-eq-trqx")
+    )).MDRefID
+    mdfr_params_1 = {
+        'MDReportID': "1",
+        'MDReqID': MDRefID_1,
+        'Instrument': {
+            'Symbol': symbol_1
+        },
+        'NoMDEntries': [
+            {
+                'MDEntryType': '0',
+                'MDEntryPx': '25',
+                'MDEntrySize': '500',
+                'MDEntryPositionNo': '1'
+            },
+            {
+                'MDEntryType': '1',
+                'MDEntryPx': '30',
+                'MDEntrySize': '500',
+                'MDEntryPositionNo': '1'
+            }
+        ]
+    }
+    mdfr_params_2 = deepcopy(mdfr_params_1)
+    mdfr_params_2['MDReqID'] = MDRefID_2
+    mdfr_params_2['Instrument'] = {
+            'Symbol': symbol_2
+    }
+    act.sendMessage(request=bca.convert_to_request(
+        'Send MarketDataSnapshotFullRefresh', "fix-fh-eq-paris", case_id,
+        bca.message_to_grpc('MarketDataSnapshotFullRefresh', mdfr_params_1)
+    ))
+    act.sendMessage(request=bca.convert_to_request(
+        'Send MarketDataSnapshotFullRefresh', "fix-fh-eq-trqx", case_id,
+        bca.message_to_grpc('MarketDataSnapshotFullRefresh', mdfr_params_2)
+    ))
+
+    # Send sorping order
+
+    sor_order_params = {
+        'Account': case_params['Account'],
+        'HandlInst': case_params['HandlInst'],
+        'Side': case_params['Side'],
+        'OrderQty': case_params['OrderQty'],
+        'TimeInForce': case_params['TimeInForce'],
+        'Price': case_params['Price'],
+        'OrdType': case_params['OrdType'],
+        'ClOrdID': bca.client_orderid(9),
+        'TransactTime': datetime.utcnow().isoformat(),
+        'Instrument': case_params['Instrument'],
+        'OrderCapacity': 'A',
+        # 'OrdSubStatus': 'SORPING',
+        'Currency': 'EUR',
+        'ComplianceID': 'FX5',
+        'ClientAlgoPolicyID': 'QA_SORPING',
+        'TargetStrategy': case_params['TargetStrategy'],
+        'Text': 'QAP-2769'
+    }
+    # print(bca.message_to_grpc('NewOrderSingle', sor_order_params))
+    new_sor_order = act.placeOrderFIX(
+        bca.convert_to_request(
+            'Send NewSingleOrder',
+            case_params['TraderConnectivity'],
+            case_id,
+            bca.message_to_grpc('NewOrderSingle', sor_order_params)
+        ))
+    checkpoint_1 = new_sor_order.checkpoint_id
+    pending_er_params = {
+        **reusable_order_params,
+        'ClOrdID': sor_order_params['ClOrdID'],
+        'OrderID': new_sor_order.response_messages_list[0].fields['OrderID'].simple_value,
+        'TransactTime': '*',
+        'CumQty': '0',
+        'LastPx': '0',
+        'LastQty': '0',
+        'QtyType': '0',
+        'AvgPx': '0',
+        'OrdStatus': 'A',
+        'ExecType': 'A',
+        # 'TradingParty': sor_order_params['TradingParty'],
+        'NoParty': [{
+            'PartyID': 'gtwquod3',
+            'PartyIDSource': 'D',
+            'PartyRole': '36'
+        }],
+        'LeavesQty': sor_order_params['OrderQty'],
+        'Instrument': case_params['Instrument']
+    }
+    verifier.submitCheckRule(
+        bca.create_check_rule(
+            "ER Pending NewOrderSingle Received",
+            bca.filter_to_grpc("ExecutionReport", pending_er_params, ['ClOrdID', 'OrdStatus']),
+            checkpoint_1, case_params['TraderConnectivity'], case_id
+        )
+    )
+
+    new_er_params = deepcopy(pending_er_params)
+    new_er_params['OrdStatus'] = new_er_params['ExecType'] = '0'
+    new_er_params['Instrument'] = {
+        'Symbol': case_params['Instrument']['Symbol'],
+        'SecurityExchange': case_params['Instrument']['SecurityExchange']
+    }
+    verifier.submitCheckRule(
+        bca.create_check_rule(
+            "ER New NewOrderSingle Received",
+            bca.filter_to_grpc("ExecutionReport", new_er_params, ['ClOrdID', 'OrdStatus']),
+            checkpoint_1, case_params['TraderConnectivity'], case_id
+        )
+    )
+    sim.removeRule(trade_rule_1)
+    sim.removeRule(trade_rule_2)
+
+    logger.info("Case {} was executed in {} sec.".format(
+        case_name, str(round(datetime.now().timestamp() - seconds))))
