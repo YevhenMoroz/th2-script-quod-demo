fb2bb8ab4abb5bc9ad06250216c94a5a0b51fbf6
dmitry.zhuravlev <T0wer_Enc0unter>
2021-01-12 12:29:53 +0300

    sim_rules_test.py was updated

--- sim_rules_test.py
+++ sim_rules_test.py
@@ -1,73 +1,75 @@
-import google.protobuf.empty_pb2
-
-from grpc_modules import quod_simulator_pb2
-from grpc_modules import quod_simulator_pb2_grpc, infra_pb2, simulator_pb2_grpc
-from grpc_modules import simulator_pb2
-import grpc
+from google.protobuf.empty_pb2 import Empty
+from stubs import Stubs
+from th2_grpc_sim_quod.sim_pb2 import TemplateQuodOCRRule, TemplateQuodRFQRule, TemplateQuodRFQTRADERule
+from th2_grpc_sim_quod.sim_pb2 import TemplateQuodMDRRule, TemplateQuodSingleExecRule, TemplateNoPartyIDs
+from th2_grpc_sim_quod.sim_pb2 import RequestMDRefID
+from th2_grpc_common.common_pb2 import ConnectionID
+from th2_grpc_sim.sim_pb2 import RuleID
 
 # start rule
-channel = grpc.insecure_channel('10.0.22.22:31977')
-# channel = grpc.insecure_channel('localhost:8081')
-simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
+simulator = Stubs.simulator
 
 # DemoRule = simulator.createTemplateQuodDemoRule(
-#     request=quod_simulator_pb2.TemplateQuodDemoRule(
+#     request=TemplateQuodDemoRule(
 #         connection_id=infra_pb2.ConnectionID(session_alias='kch-qa-ret-child'),
 #         demo_field1=123,
 #         demo_field2='KCH_QA_RET_CHILD'))
 
-# OCR = simulator.createQuodOCRRule(request=quod_simulator_pb2.TemplateQuodOCRRule(connection_id=infra_pb2.ConnectionID(session_alias='fix-bs-eq-paris')))
+OCR = simulator.createQuodOCRRule(
+    request=TemplateQuodOCRRule(connection_id=ConnectionID(session_alias='fix-bs-eq-paris')))
 
-# RFQ = simulator.createQuodRFQRule(request=quod_simulator_pb2.TemplateQuodRFQRule(connection_id=infra_pb2.ConnectionID(session_alias='fix-fh-fx-rfq')))
-#
-# TRFQ = simulator.createQuodRFQTRADERule(request=quod_simulator_pb2.TemplateQuodRFQTRADERule(connection_id=infra_pb2.ConnectionID(session_alias='')))
-#
-# MDESP = simulator.createQuodMDRRule(request=quod_simulator_pb2.TemplateQuodMDRRule(
-#     connection_id=infra_pb2.ConnectionID(session_alias="fix-fh-fx-esp"),
-#     sender="QUOD_UTP",
-#     md_entry_size={1000: 1000},
-#     md_entry_px={40: 30}))
+RFQ = simulator.createQuodRFQRule(
+    request=TemplateQuodRFQRule(connection_id=ConnectionID(session_alias='fix-fh-fx-rfq')))
 
-# SingleExecParis = simulator.createQuodSingleExecRule(request=quod_simulator_pb2.TemplateQuodSingleExecRule(
-#     connection_id=infra_pb2.ConnectionID(session_alias="fix-bs-eq-paris"),
-#     no_party_ids=[
-#             quod_simulator_pb2.TemplateNoPartyIDs(party_id="KEPLER", party_id_source="D", party_role="1"),
-#             quod_simulator_pb2.TemplateNoPartyIDs(party_id="1", party_id_source="D", party_role="2"),
-#             quod_simulator_pb2.TemplateNoPartyIDs(party_id="2", party_id_source="D", party_role="3")
-#         ],
-#     cum_qty=1000,
-#     mask_as_connectivity="fix-fh-eq-paris",
-#     md_entry_size={0: 1000},
-#     md_entry_px={0: 30},
-#     symbol="1062"
-# ))
-#
-# SingleExecTrqx = simulator.createQuodSingleExecRule(request=quod_simulator_pb2.TemplateQuodSingleExecRule(
-#     connection_id=infra_pb2.ConnectionID(session_alias="fix-bs-eq-trqx"),
-#     no_party_ids=[
-#             quod_simulator_pb2.TemplateNoPartyIDs(party_id="KEPLER", party_id_source="D", party_role="1"),
-#             quod_simulator_pb2.TemplateNoPartyIDs(party_id="1", party_id_source="D", party_role="2"),
-#             quod_simulator_pb2.TemplateNoPartyIDs(party_id="2", party_id_source="D", party_role="3")
-#         ],
-#     cum_qty=100,
-#     mask_as_connectivity="fix-fh-eq-trqx",
-#     md_entry_size={900: 1000},
-#     md_entry_px={40: 30},
-#     symbol="3503"
-# ))
-#
-# MDR_paris = simulator.createQuodMDRRule(request=quod_simulator_pb2.TemplateQuodMDRRule(
-#     connection_id=infra_pb2.ConnectionID(session_alias="fix-fh-eq-paris"),
-#     sender="QUOD_UTP",
-#     md_entry_size={1000: 1000},
-#     md_entry_px={40: 30}))
-#
-#
-# MDR_trqx = simulator.createQuodMDRRule(request=quod_simulator_pb2.TemplateQuodMDRRule(
-#     connection_id=infra_pb2.ConnectionID(session_alias="fix-fh-eq-trqx"),
-#     sender="QUOD_UTP",
-#     md_entry_size={1000: 1000},
-#     md_entry_px={40: 30}))
+# TRFQ = simulator.createQuodRFQTRADERule(
+#     request=TemplateQuodRFQTRADERule(connection_id=ConnectionID(session_alias='')))
+
+MDESP = simulator.createQuodMDRRule(request=TemplateQuodMDRRule(
+    connection_id=ConnectionID(session_alias="fix-fh-fx-esp"),
+    sender="QUOD_UTP",
+    md_entry_size={1000: 1000},
+    md_entry_px={40: 30}))
+
+SingleExecParis = simulator.createQuodSingleExecRule(request=TemplateQuodSingleExecRule(
+    connection_id=ConnectionID(session_alias="fix-bs-eq-paris"),
+    no_party_ids=[
+            TemplateNoPartyIDs(party_id="KEPLER", party_id_source="D", party_role="1"),
+            TemplateNoPartyIDs(party_id="1", party_id_source="D", party_role="2"),
+            TemplateNoPartyIDs(party_id="2", party_id_source="D", party_role="3")
+        ],
+    cum_qty=1000,
+    mask_as_connectivity="fix-fh-eq-paris",
+    md_entry_size={0: 1000},
+    md_entry_px={0: 30},
+    symbol="1062"
+))
+
+SingleExecTrqx = simulator.createQuodSingleExecRule(request=TemplateQuodSingleExecRule(
+    connection_id=ConnectionID(session_alias="fix-bs-eq-trqx"),
+    no_party_ids=[
+            TemplateNoPartyIDs(party_id="KEPLER", party_id_source="D", party_role="1"),
+            TemplateNoPartyIDs(party_id="1", party_id_source="D", party_role="2"),
+            TemplateNoPartyIDs(party_id="2", party_id_source="D", party_role="3")
+        ],
+    cum_qty=100,
+    mask_as_connectivity="fix-fh-eq-trqx",
+    md_entry_size={900: 1000},
+    md_entry_px={40: 30},
+    symbol="3503"
+))
+
+MDR_paris = simulator.createQuodMDRRule(request=TemplateQuodMDRRule(
+    connection_id=ConnectionID(session_alias="fix-fh-eq-paris"),
+    sender="QUOD_UTP",
+    md_entry_size={1000: 1000},
+    md_entry_px={40: 30}))
+
+
+MDR_trqx = simulator.createQuodMDRRule(request=TemplateQuodMDRRule(
+    connection_id=ConnectionID(session_alias="fix-fh-eq-trqx"),
+    sender="QUOD_UTP",
+    md_entry_size={1000: 1000},
+    md_entry_px={40: 30}))
 
 
 ## Use Def Rules for debug only
@@ -80,25 +82,29 @@ simulator = quod_simulator_pb2_grpc.TemplateSimulatorServiceStub(channel)
 # ))
 
 # stop rule
-core = simulator_pb2_grpc.ServiceSimulatorStub(channel)
-# core.removeRule(DemoRule)
-# # core.removeRule(OCR)
-# channel.close()
+core = Stubs.core
+
+core.removeRule(OCR)
+core.removeRule(RFQ)
+core.removeRule(MDESP)
+core.removeRule(SingleExecParis)
+core.removeRule(SingleExecTrqx)
+core.removeRule(MDR_paris)
+core.removeRule(MDR_trqx)
 
 # get rules
-running_rules = core.getRulesInfo(request=google.protobuf.empty_pb2.Empty()).info
+running_rules = core.getRulesInfo(request=Empty()).info
 print(running_rules, "Rules running:", len(running_rules))
 # remove rule
-for r in running_rules:
-    core.removeRule(simulator_pb2.RuleID(id=r.id.id))
+# for r in running_rules:
+#     core.removeRule(RuleID(id=r.id.id))
 
 
-
-MDRefID = simulator.getMDRefIDForConnection(request=quod_simulator_pb2.RequestMDRefID(
-    symbol="1062",
-    connection_id=infra_pb2.ConnectionID(session_alias="fix-fh-eq-paris")
-)).MDRefID
+# MDRefID = simulator.getMDRefIDForConnection(request=RequestMDRefID(
+#     symbol="1062",
+#     connection_id=ConnectionID(session_alias="fix-fh-eq-paris")
+# )).MDRefID
 
 # for i in range(479,483):
-#     core.removeRule(simulator_pb2.RuleID(id=i))
+#     core.removeRule(RuleID(id=i))
 
