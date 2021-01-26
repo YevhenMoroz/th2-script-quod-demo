13120078078edc14281987caed018053342998b8
andrey.grunichev <andrey.grunichev@exactprosystems.com>
2021-01-26 15:20:18 +0300

    RFQ_example(not final version, need to add verify part) + settlement date class

---
+++ custom/tenor_settlement_date.py
@@ -0,0 +1,13 @@
+from pandas import Timestamp as tm
+from pandas.tseries.offsets import BusinessDay as bd
+from datetime import datetime
+
+
+def spo():
+    return (tm(datetime.utcnow().isoformat()) + bd(n=2)).date().strftime('%Y%m%d')
+
+
+def wk1():
+    return (tm(datetime.utcnow().isoformat()) + bd(n=5)).date().strftime('%Y%m%d')
+
+# TODO need to add another tenors
