36178fef5dc3da00981f0e887e91d5c262f52149
denis.shepelev <denis.shepelev@exactprosystems.com>
2021-02-11 15:11:46 +0300

    updated QAP-585

--- test_cases/QAP_585.py
+++ test_cases/QAP_585.py
@@ -1,5 +1,4 @@
 import logging
-from datetime import datetime
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 
@@ -15,24 +14,25 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def send_rfq(br, service):
-    details = RFQTileDetails(base=br)
+def send_rfq(base_request, service):
+    details = RFQTileDetails(base=base_request)
     call(service.createRFQ, details.request())
 
 
-def send_order(ex_id):
+def send_order(base_request, service, venue):
     details = RFQTileOrderDetails(base=base_request)
     details.set_venue(venue)
     details.set_action(RFQTileOrderSide.BUY)
-    call(rfq_service.sendRFQOrder, details.request())
+    call(service.sendRFQOrder, details.request())
 
 
-def cancel_rfq(ex_id):
-    pass
+def cancel_rfq(base_request, service):
+    details = RFQTilePanelDetails(base=base_request)
+    call(service.cancelRFQ, details.request())
 
 
-def check_qrb(ex_id, br, service, act):
-    qrb = QuoteDetailsRequest(base=br)
+def check_qrb(ex_id, base_request, service, act):
+    qrb = QuoteDetailsRequest(base=base_request)
     qrb.set_extraction_id(ex_id)
     # qrb.set_filter(["Venue", "HSBCR"])
     qrb_venue = ExtractionDetail("quoteRequestBook.venue", "Venue")
@@ -46,34 +46,60 @@ def check_qrb(ex_id, br, service, act):
                                            verify_ent("QRB QuoteStatus", qrb_quote_status.name, "Accepted")]))
 
 
-def check_qb(ex_id, br, service, act, owner):
-    qb = QuoteDetailsRequest(base=br)
+def check_qb(ex_id, base_request, service, act, owner):
+    qb = QuoteDetailsRequest(base=base_request)
     qb.set_extraction_id(ex_id)
     qb_owner = ExtractionDetail("quoteBook.owner", "Owner")
     qb_quote_status = ExtractionDetail("quoteBook.quotestatus", "QuoteStatus")
-    qb.add_extraction_details([qb_owner, qb_quote_status])
-    call(service.getQuoteBookDetails, qb.request())
+    qb_id = ExtractionDetail("quoteBook.id", "Id")
+    qb.add_extraction_details([qb_owner, qb_quote_status, qb_id])
+    data = call(service.getQuoteBookDetails, qb.request())
     call(act.verifyEntities, verification(ex_id, "checking QB",
                                           [verify_ent("QB Owner", qb_owner.name, owner),
                                            verify_ent("QB QuoteStatus", qb_quote_status.name, "Accepted")]))
+    return data[qb_id.name]
+
+
+def check_ob(ex_id, base_request, instr_type, act, act_ob, qb_id):
+    ob = OrdersDetails()
+    ob.set_default_params(base_request)
+    ob.set_extraction_id(ex_id)
+    ob_insrt_type = ExtractionDetail("orderBook.instrtype", "InstrType")
+    ob_venue = ExtractionDetail("orderBook.venue", "Venue")
+    ob_exec_sts = ExtractionDetail("orderBook.execsts", "ExecSts")
+    ob_id = ExtractionDetail("orderBook.quoteid", "QuoteID")
+    ob.add_single_order_info(
+        OrderInfo.create(
+            action=ExtractionAction.create_extraction_action(extraction_details=[ob_insrt_type,
+                                                                                 ob_venue,
+                                                                                 ob_exec_sts,
+                                                                                 ob_id])))
+    data = call(act_ob.getOrdersDetails, ob.request())
+    call(act.verifyEntities, verification(ex_id, "checking OB",
+                                                 [verify_ent("OB InstrType", ob_insrt_type.name, instr_type),
+                                                  verify_ent("OB Venue", ob_venue.name, "HSBCR"),
+                                                  verify_ent("OB ExecSts", ob_exec_sts.name, "Filled"),
+                                                  verify_ent("OB ID vs QB ID", ob_id.name, qb_id)]))
 
 
 def execute(report_id):
     common_act = Stubs.win_act
 
     # Rules
-    # rule_manager = rm.RuleManager()
-    # RFQ = rule_manager.add_RFQ('fix-fh-fx-rfq')
-    # TRFQ = rule_manager.add_TRFQ('fix-fh-fx-rfq')
+    rule_manager = rm.RuleManager()
+    RFQ = rule_manager.add_RFQ('fix-fh-fx-rfq')
+    TRFQ = rule_manager.add_TRFQ('fix-fh-fx-rfq')
 
-    # Store case start time
-    seconds, nanos = bca.timestamps()
     case_name = "QAP-585"
+    quote_owner = "dshepelev"
+    case_instr_type = "Spot"
+    case_venue = "HSB"
+
     # Create sub-report for case
     case_id = bca.create_event(case_name, report_id)
     session_id = set_session_id()
     set_base(session_id, case_id)
-    base_request = get_base_request(session_id, case_id)
+    case_base_request = get_base_request(session_id, case_id)
     rfq_service = Stubs.win_act_rfq_service
     ob_act = Stubs.win_act_order_book
 
@@ -81,184 +107,40 @@ def execute(report_id):
         prepare_fe_2(case_id, session_id)
 
     try:
-        quote_owner = "dshepelev"
-        instr_type = "Spot"
-        venue = "HSB"
-
         # Steps 1-2
-        send_rfq(base_request, rfq_service)
-        check_qrb("QRB_0", base_request, rfq_service, common_act)
-        check_qb("QB_0", base_request, rfq_service, common_act, quote_owner)
+        send_rfq(case_base_request, rfq_service)
+        check_qrb("QRB_0", case_base_request, rfq_service, common_act)
+        qb_quote_id = check_qb("QB_0", case_base_request, rfq_service, common_act, quote_owner)
 
-        # not refactored section
         # Step 3
-        details = RFQTileOrderDetails(base=base_request)
-        details.set_venue(venue)
-        details.set_action(RFQTileOrderSide.BUY)
-        call(rfq_service.sendRFQOrder, details.request())
-
-        details = RFQTilePanelDetails(base=base_request)
-        call(rfq_service.cancelRFQ, details.request())
-
-        # Check OB
-        extraction_id = 'OB_0'
-        ob = OrdersDetails()
-        ob.set_default_params(base_request)
-        ob.set_extraction_id(extraction_id)
-        ob_insrt_type = ExtractionDetail("orderBook.instrtype", "InstrType")
-        ob_venue = ExtractionDetail("orderBook.venue", "Venue")
-        ob_exec_sts = ExtractionDetail("orderBook.execsts", "ExecSts")
-        ob.add_single_order_info(
-            OrderInfo.create(
-                action=ExtractionAction.create_extraction_action(extraction_details=[ob_insrt_type,
-                                                                                     ob_venue,
-                                                                                     ob_exec_sts])))
-
-        call(ob_act.getOrdersDetails, ob.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking OB",
-                                                     [verify_ent("OB InstrType", ob_insrt_type.name, instr_type),
-                                                      verify_ent("OB Venue", ob_venue.name, "HSBCR"),
-                                                      verify_ent("OB ExecSts", ob_exec_sts.name, "Filled")]))
+        send_order(case_base_request, rfq_service, case_venue)
+        cancel_rfq(case_base_request, rfq_service)
+        check_ob("OB_0", case_base_request, case_instr_type, common_act, ob_act, qb_quote_id)
 
         # Step 4
-        details = RFQTileDetails(base=base_request)
-        call(rfq_service.createRFQ, details.request())
-
-        # Check QRB
-        extraction_id = 'QRB_1'
-        qrb = QuoteDetailsRequest(base=base_request)
-        qrb.set_extraction_id(extraction_id)
-
-        # qrb.set_filter(["Venue", "HSBCR"])
-        qrb_venue = ExtractionDetail("quoteRequestBook.venue", "Venue")
-        qrb_status = ExtractionDetail("quoteRequestBook.status", "Status")
-        qrb_quote_status = ExtractionDetail("quoteRequestBook.qoutestatus", "QuoteStatus")
-        qrb.add_extraction_details([qrb_venue, qrb_status, qrb_quote_status])
-
-        call(rfq_service.getQuoteRequestBookDetails, qrb.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking QRB",
-                                                     [verify_ent("QRB Venue", qrb_venue.name, "HSBCR"),
-                                                      verify_ent("QRB Status", qrb_status.name, "New"),
-                                                      verify_ent("QRB QuoteStatus", qrb_quote_status.name,
-                                                                 "Accepted")]))
-
-        # Check QB
-        extraction_id = 'QB_1'
-        qb = QuoteDetailsRequest(base=base_request)
-        qb.set_extraction_id(extraction_id)
-        qb_owner = ExtractionDetail("quoteBook.owner", "Owner")
-        qb_quote_status = ExtractionDetail("quoteBook.quotestatus", "QuoteStatus")
-        qb.add_extraction_details([qb_owner, qb_quote_status])
-
-        call(rfq_service.getQuoteBookDetails, qb.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking QB",
-                                                     [verify_ent("QB Owner", qb_owner.name, quote_owner),
-                                                      verify_ent("QB QuoteStatus", qb_quote_status.name,
-                                                                 "Accepted")]))
+        send_rfq(case_base_request, rfq_service)
+        check_qrb("QRB_1", case_base_request, rfq_service, common_act)
+        qb_quote_id = check_qb("QB_1", case_base_request, rfq_service, common_act, quote_owner)
 
         # Step 5
-        details = RFQTileOrderDetails(base=base_request)
-        details.set_venue(venue)
-        details.set_action(RFQTileOrderSide.BUY)
-        call(rfq_service.sendRFQOrder, details.request())
+        send_order(case_base_request, rfq_service, case_venue)
+        cancel_rfq(case_base_request, rfq_service)
+        check_ob("OB_1", case_base_request, case_instr_type, common_act, ob_act, qb_quote_id)
 
-        details = RFQTilePanelDetails(base=base_request)
-        call(rfq_service.cancelRFQ, details.request())
-
-        # Check OB
-        extraction_id = 'OB_0'
-        ob = OrdersDetails()
-        ob.set_default_params(base_request)
-        ob.set_extraction_id(extraction_id)
-        ob_insrt_type = ExtractionDetail("orderBook.instrtype", "InstrType")
-        ob_venue = ExtractionDetail("orderBook.venue", "Venue")
-        ob_exec_sts = ExtractionDetail("orderBook.execsts", "ExecSts")
-        ob.add_single_order_info(
-            OrderInfo.create(
-                action=ExtractionAction.create_extraction_action(extraction_details=[ob_insrt_type,
-                                                                                     ob_venue,
-                                                                                     ob_exec_sts])))
-
-        call(ob_act.getOrdersDetails, ob.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking OB",
-                                                     [verify_ent("OB InstrType", ob_insrt_type.name, instr_type),
-                                                      verify_ent("OB Venue", ob_venue.name, "HSBCR"),
-                                                      verify_ent("OB ExecSts", ob_exec_sts.name, "Filled")]))
-
-        # Steps 7
-        details = RFQTileDetails(base=base_request)
-        call(rfq_service.createRFQ, details.request())
-
-        # Check QRB
-        extraction_id = 'QRB_1'
-        qrb = QuoteDetailsRequest(base=base_request)
-        qrb.set_extraction_id(extraction_id)
-
-        # qrb.set_filter(["Venue", "HSBCR"])
-        qrb_venue = ExtractionDetail("quoteRequestBook.venue", "Venue")
-        qrb_status = ExtractionDetail("quoteRequestBook.status", "Status")
-        qrb_quote_status = ExtractionDetail("quoteRequestBook.qoutestatus", "QuoteStatus")
-        qrb.add_extraction_details([qrb_venue, qrb_status, qrb_quote_status])
-
-        call(rfq_service.getQuoteRequestBookDetails, qrb.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking QRB",
-                                                     [verify_ent("QRB Venue", qrb_venue.name, "HSBCR"),
-                                                      verify_ent("QRB Status", qrb_status.name, "New"),
-                                                      verify_ent("QRB QuoteStatus", qrb_quote_status.name,
-                                                                 "Accepted")]))
-
-        # Check QB
-        extraction_id = 'QB_1'
-        qb = QuoteDetailsRequest(base=base_request)
-        qb.set_extraction_id(extraction_id)
-        qb_owner = ExtractionDetail("quoteBook.owner", "Owner")
-        qb_quote_status = ExtractionDetail("quoteBook.quotestatus", "QuoteStatus")
-        qb.add_extraction_details([qb_owner, qb_quote_status])
-
-        call(rfq_service.getQuoteBookDetails, qb.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking QB",
-                                                     [verify_ent("QB Owner", qb_owner.name, quote_owner),
-                                                      verify_ent("QB QuoteStatus", qb_quote_status.name,
-                                                                 "Accepted")]))
+        # Step 7
+        send_rfq(case_base_request, rfq_service)
+        check_qrb("QRB_2", case_base_request, rfq_service, common_act)
+        qb_quote_id = check_qb("QB_2", case_base_request, rfq_service, common_act, quote_owner)
 
         # Step 8
-        details = RFQTileOrderDetails(base=base_request)
-        details.set_venue(venue)
-        details.set_action(RFQTileOrderSide.BUY)
-        call(rfq_service.sendRFQOrder, details.request())
+        send_order(case_base_request, rfq_service, case_venue)
+        cancel_rfq(case_base_request, rfq_service)
+        check_ob("OB_2", case_base_request, case_instr_type, common_act, ob_act, qb_quote_id)
 
-        details = RFQTilePanelDetails(base=base_request)
-        call(rfq_service.cancelRFQ, details.request())
-
-        # Check OB
-        extraction_id = 'OB_0'
-        ob = OrdersDetails()
-        ob.set_default_params(base_request)
-        ob.set_extraction_id(extraction_id)
-        ob_insrt_type = ExtractionDetail("orderBook.instrtype", "InstrType")
-        ob_venue = ExtractionDetail("orderBook.venue", "Venue")
-        ob_exec_sts = ExtractionDetail("orderBook.execsts", "ExecSts")
-        ob.add_single_order_info(
-            OrderInfo.create(
-                action=ExtractionAction.create_extraction_action(extraction_details=[ob_insrt_type,
-                                                                                     ob_venue,
-                                                                                     ob_exec_sts])))
-
-        call(ob_act.getOrdersDetails, ob.request())
-
-        call(common_act.verifyEntities, verification(extraction_id, "checking OB",
-                                                     [verify_ent("OB InstrType", ob_insrt_type.name, instr_type),
-                                                      verify_ent("OB Venue", ob_venue.name, "HSBCR"),
-                                                      verify_ent("OB ExecSts", ob_exec_sts.name, "Filled")]))
+        close_fe_2(case_id, session_id)
 
     except Exception as e:
         logging.error("Error execution", exc_info=True)
 
-    # for rule in [RFQ, TRFQ]:
-    #     rule_manager.remove_rule(r
+    for rule in [RFQ, TRFQ]:
+        rule_manager.remove_rule(rule)
