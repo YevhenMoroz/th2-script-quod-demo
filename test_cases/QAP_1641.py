55f9ffb77febc9a65c348a96905ebed5ba8de1f5
dmitry.zhuravlev <T0wer_Enc0unter>
2021-02-11 17:31:50 +0300

    QAP-1641 was updated

--- test_cases/QAP_1641.py
+++ test_cases/QAP_1641.py
@@ -6,8 +6,11 @@ import time
 from datetime import datetime
 from stubs import Stubs
 from logging import getLogger, INFO
-from custom.basic_custom_actions import timestamps, create_event
-from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe_2, call, close_fe_2
+from custom.basic_custom_actions import timestamps, create_event, message_to_grpc, convert_to_request
+from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe, call, close_fe
+from th2_grpc_sim_quod.sim_pb2 import RequestMDRefID
+from th2_grpc_common.common_pb2 import ConnectionID
+from rule_management import RuleManager
 
 
 logger = getLogger(__name__)
@@ -21,15 +24,20 @@ def execute(report_id):
     # Create sub-report for case
     case_id = create_event(case_name, report_id)
 
+    rule_manager = RuleManager()
+    nos_rule = rule_manager.add_NOS("fix-bs-eq-paris", "XPAR_CLIENT1")
+
     session_id = set_session_id()
     set_base(session_id, case_id)
     base_request = get_base_request(session_id, case_id)
+    work_dir = Stubs.custom_config['qf_trading_fe_folder_305']
+    username = Stubs.custom_config['qf_trading_fe_user_305']
+    password = Stubs.custom_config['qf_trading_fe_password_305']
     if not Stubs.frontend_is_open:
-        prepare_fe_2(case_id, session_id)
+        prepare_fe(case_id, session_id, work_dir, username, password)
     qty = "8000"
     limit = "1.2"
-    lookup = "NPK"
-    # lookup = "PROL"
+    lookup = "PROL"
 
     try:
         order_ticket = OrderTicketDetails()
@@ -37,7 +45,7 @@ def execute(report_id):
         order_ticket.set_limit(limit)
         order_ticket.set_client("CLIENT1")
         order_ticket.set_order_type("Limit")
-        order_ticket.set_care_order(Stubs.custom_config['qf_trading_fe_user'], True)
+        order_ticket.set_care_order(Stubs.custom_config['qf_trading_fe_user_305'], True)
 
         new_order_details = NewOrderDetails()
         new_order_details.set_lookup_instr(lookup)
@@ -70,7 +78,10 @@ def execute(report_id):
 
         order_info_extraction = "getOrderInfo"
 
-        call(common_act.getOrderFields, fields_request(order_info_extraction, ["order.status", "Sts"]))
+        data = call(common_act.getOrderFields, fields_request(order_info_extraction,
+                                                       ["order.status", "Sts", "order.order_id", "Order ID"]))
+        care_order_id = data["order.order_id"]
+        # care_order_id = "CO1210211115759412001"
         call(common_act.verifyEntities, verification(order_info_extraction, "checking order",
                                                      [verify_ent("Order Status", "order.status", "Open")]))
 
@@ -82,100 +93,99 @@ def execute(report_id):
 
         # step 4 make trade 10000
 
-        order_ticket_dma1 = OrderTicketDetails()
-        order_ticket_dma1.set_quantity('10000')
-        order_ticket_dma1.set_limit('1.2')
-        order_ticket_dma1.buy()
+        time.sleep(5)
+        symbol_1 = "1042"
 
-        dma_order_details = NewOrderDetails()
-        dma_order_details.set_lookup_instr(lookup)
-        dma_order_details.set_order_details(order_ticket_dma1)
-        dma_order_details.set_default_params(base_request)
-
-        call(order_ticket_service.placeOrder, dma_order_details.build())
-
-        order_ticket_dma2 = OrderTicketDetails()
-        order_ticket_dma2.set_quantity('10000')
-        order_ticket_dma2.set_limit('1.2')
-        order_ticket_dma2.sell()
-
-        dma_order_details = NewOrderDetails()
-        dma_order_details.set_lookup_instr(lookup)
-        dma_order_details.set_order_details(order_ticket_dma2)
-        dma_order_details.set_default_params(base_request)
-
-        call(order_ticket_service.placeOrder, dma_order_details.build())
+        MDRefID_1 = Stubs.simulator.getMDRefIDForConnection(request=RequestMDRefID(
+            symbol=symbol_1,
+            connection_id=ConnectionID(session_alias="fix-fh-eq-paris")
+        )).MDRefID
+        mdir_params_trade = {
+            'MDReqID': MDRefID_1,
+            'NoMDEntriesIR': [
+                {
+                    'MDUpdateAction': '0',
+                    'MDEntryType': '2',
+                    'MDEntryPx': '1.2',
+                    'MDEntrySize': '10000',
+                    'MDEntryDate': datetime.utcnow().date().strftime("%Y%m%d"),
+                    'MDEntryTime': datetime.utcnow().time().strftime("%H:%M:%S")
+                }
+            ]
+        }
+        Stubs.fix_act.sendMessage(request=convert_to_request(
+            'Send MarketDataIncrementalRefresh', "fix-fh-eq-paris", case_id,
+            message_to_grpc('MarketDataIncrementalRefresh', mdir_params_trade, "fix-fh-eq-paris")
+        ))
 
         time.sleep(120)
 
         #  make trade 5000
 
-        order_ticket_dma1 = OrderTicketDetails()
-        order_ticket_dma1.set_quantity('5000')
-        order_ticket_dma1.set_limit('1.2')
-        order_ticket_dma1.buy()
+        mdir_params_trade_2 = {
+            'MDReqID': MDRefID_1,
+            'NoMDEntriesIR': [
+                {
+                    'MDUpdateAction': '0',
+                    'MDEntryType': '2',
+                    'MDEntryPx': '1.2',
+                    'MDEntrySize': '5000',
+                    'MDEntryDate': datetime.utcnow().date().strftime("%Y%m%d"),
+                    'MDEntryTime': datetime.utcnow().time().strftime("%H:%M:%S")
+                }
+            ]
+        }
+        Stubs.fix_act.sendMessage(request=convert_to_request(
+            'Send MarketDataIncrementalRefresh', "fix-fh-eq-paris", case_id,
+            message_to_grpc('MarketDataIncrementalRefresh', mdir_params_trade_2, "fix-fh-eq-paris")
+        ))
 
-        dma_order_details = NewOrderDetails()
-        dma_order_details.set_lookup_instr(lookup)
-        dma_order_details.set_order_details(order_ticket_dma1)
-        dma_order_details.set_default_params(base_request)
-
-        call(order_ticket_service.placeOrder, dma_order_details.build())
-
-        order_ticket_dma2 = OrderTicketDetails()
-        order_ticket_dma2.set_quantity('5000')
-        order_ticket_dma2.set_limit('1.2')
-        order_ticket_dma2.sell()
-
-        dma_order_details = NewOrderDetails()
-        dma_order_details.set_lookup_instr(lookup)
-        dma_order_details.set_order_details(order_ticket_dma2)
-        dma_order_details.set_default_params(base_request)
-
-        call(order_ticket_service.placeOrder, dma_order_details.build())
-
-        time.sleep(30)
+        time.sleep(120)
 
         # check child orders
 
-        extraction_id = "order.care"
+        extraction_id = "child_orders"
 
-        sub_lv2_1_qty = ExtractionDetail("subOrder_1_lv2.qty", "Qty")
-        sub_lv2_2_qty = ExtractionDetail("subOrder_2_lv2.qty", "Qty")
-        sub_lv2_3_qty = ExtractionDetail("subOrder_3_lv2.qty", "Qty")
+        child_order_lvl1_details = OrdersDetails()
+        child_order_lvl1_details.set_default_params(base_request)
+        child_order_lvl1_details.set_extraction_id(extraction_id)
+        child_order_lvl1_details.set_filter(["ParentOrdID", care_order_id])
 
-        extraction_action_sub_lv2 = ExtractionAction.create_extraction_action(
-            extraction_details=[sub_lv2_1_qty, sub_lv2_2_qty, sub_lv2_3_qty])
+        child_order_lvl2_1_qty = ExtractionDetail("child_order_lvl2_1.qty", "Qty")
+        child_order_lvl2_1_info = OrderInfo.create(
+            action=ExtractionAction.create_extraction_action(extraction_detail=child_order_lvl2_1_qty))
+        child_order_lvl2_2_qty = ExtractionDetail("child_order_lvl2_2.qty", "Qty")
+        child_order_lvl2_2_info = OrderInfo.create(
+            action=ExtractionAction.create_extraction_action(extraction_detail=child_order_lvl2_2_qty))
+        child_order_lvl2_3_qty = ExtractionDetail("child_order_lvl2_3.qty", "Qty")
+        child_order_lvl2_3_info = OrderInfo.create(
+            action=ExtractionAction.create_extraction_action(extraction_detail=child_order_lvl2_3_qty))
 
-        # sub_lv2_details = OrdersDetails.create(info=OrderInfo.create(action=extraction_action_sub_lv2))
-        # length_name = "subOrders_lv2.length"
-        # sub_lv2_details.extract_length(length_name)
+        child_order_lvl2_details = OrdersDetails.create(
+            order_info_list=[child_order_lvl2_1_info, child_order_lvl2_2_info, child_order_lvl2_3_info]
+        )
+        length_name = "child_orders_lvl2.length"
+        child_order_lvl2_details.extract_length(length_name)
 
-        sub_lv1_qty = ExtractionDetail("subOrder_lv1.qty", "Qty")
-        extraction_action_sub_lv1 = ExtractionAction.create_extraction_action(extraction_detail=sub_lv1_qty)
-        sub_lv1_info = OrderInfo.create(action=extraction_action_sub_lv1)
-        # sub_lv1_info.set_sub_orders_details(sub_lv2_details)
+        child_order_lvl1_qty = ExtractionDetail("child_order_lvl1.qty", "Qty")
+        child_order_lvl1_ext_act = ExtractionAction.create_extraction_action(extraction_detail=child_order_lvl1_qty)
+        child_order_lvl1_info = OrderInfo.create(
+            action=child_order_lvl1_ext_act, sub_order_details=child_order_lvl2_details)
 
-        main_order_info = OrderInfo.create(sub_order_details=OrdersDetails.create(info=sub_lv1_info))
+        child_order_lvl1_details.add_single_order_info(order_info=child_order_lvl1_info)
 
-        main_order_details = OrdersDetails()
-        main_order_details.set_default_params(base_request)
-        main_order_details.set_extraction_id(extraction_id)
-        main_order_details.set_filter(["Lookup", lookup, "ExecPcy", "Care"])
-        main_order_details.add_single_order_info(main_order_info)
-
-        call(order_book_service.getOrdersDetails, main_order_details.request())
-
+        call(order_book_service.getChildOrdersDetails, child_order_lvl1_details.request())
         call(common_act.verifyEntities, verification(
             extraction_id, "Checking child orders", [
-                verify_ent("Sub Lvl 1 Qty", sub_lv1_qty.name, "8,000")
-                # verify_ent("Sub 1 Lvl 2 Qty", sub_lv2_1_qty.name, "1,666"),
-                # verify_ent("Sub 2 Lvl 2 Qty", sub_lv2_2_qty.name, "1,333"),
-                # verify_ent("Sub 3 Lvl 2 Qty", sub_lv2_3_qty.name, "2,000"),
-                # verify_ent("Sub order Lvl 2 count", length_name, "3")
+                verify_ent("Child order lvl 1 Qty", child_order_lvl1_qty.name, "8,000"),
+                verify_ent("Child order lvl 2 1 Qty", child_order_lvl2_1_qty.name, "1,666"),
+                verify_ent("Child order lvl 2 2 Qty", child_order_lvl2_2_qty.name, "1,333"),
+                verify_ent("Child order lvl 2 3 Qty", child_order_lvl2_3_qty.name, "2,000"),
+                verify_ent("Child orders lvl 2 count", length_name, "3")
             ]
         ))
     except Exception:
         logger.error("Error execution", exc_info=True)
-    close_fe_2(case_id, session_id)
+    rule_manager.remove_rule(nos_rule)
+    close_fe(case_id, session_id)
     logger.info(f"Case {case_name} was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
