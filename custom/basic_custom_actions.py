1eb13466483cb2c01ae3204743ddbcafd0254920
dmitry.zhuravlev <T0wer_Enc0unter>
2021-02-16 14:49:50 +0300

    basic_custom_actions.py: functions descriptions were added

--- custom/basic_custom_actions.py
+++ custom/basic_custom_actions.py
@@ -1,3 +1,5 @@
+""" This module contains functions which are used in test cases """
+
 from copy import deepcopy
 from uuid import uuid1
 from datetime import datetime, timedelta, date
@@ -8,17 +10,11 @@ from th2_grpc_check1.check1_pb2 import CheckRuleRequest
 from th2_grpc_check1.check1_pb2 import CheckSequenceRuleRequest
 from th2_grpc_check1.check1_pb2 import CheckpointRequest
 
-from th2_grpc_sim_quod.sim_pb2 import TemplateQuodNOSRule, TemplateQuodOCRRule, TemplateQuodMDRRule
-
-
 from th2_grpc_act_quod.act_fix_pb2 import PlaceMessageRequest
 from stubs import Stubs
 
 from th2_grpc_common.common_pb2 import ValueFilter, FilterOperation, MessageMetadata, MessageFilter, ConnectionID, \
     EventID, ListValue, Value, Message, ListValueFilter, MessageID, Event, EventBatch, Direction
-
-# Debug output
-PrintMessages = False
 
 
 def __find_closest_workday(from_date: date, is_weekend_holiday: bool) -> date:
@@ -48,10 +44,24 @@ def timestamps():
 
 
 def client_orderid(length: int) -> str:
+    """ Generates unique id in the string format of specified length
+        Parameters:
+            length (int): the length of generated id.
+        Returns:
+            client_orderid (str)
+    """
     return str(uuid1().time)[-length:]
 
 
 def message_to_grpc(message_type: str, content: dict, session_alias: str) -> Message:
+    """ Creates grpc wrapper for message
+        Parameters:
+            message_type (str): Type of message (NewOrderSingle, ExecutionReport, etc.)
+            content (dict): Fields and values, represented in Python dictionary format ({'Price': 10, 'OrderQty': 100}).
+            session_alias (str): Name of connectivity box (fix-bs-eq-trqx, fix-fh-eq-paris, gtwquod3).
+        Returns:
+            message_to_grpc (Message): grpc wrapper for message
+    """
     content = deepcopy(content)
     for tag in content:
         if isinstance(content[tag], (str, int, float)):
@@ -86,6 +96,16 @@ def message_to_grpc(message_type: str, content: dict, session_alias: str) -> Mes
 
 
 def filter_to_grpc(message_type: str, content: dict, keys=None) -> MessageFilter:
+    """ Creates grpc wrapper for filter
+        Parameters:
+            message_type (str): Type of message (NewOrderSingle, ExecutionReport, etc.)
+            content (dict): Fields and values, represented in Python dictionary format ({'Price': 10,
+                'OrderQty': 100}).
+            keys (list): Optional parameter. A list of fields, that must be used as key fields during the message
+                verification. Default value is None.
+        Returns:
+            filter_to_grpc (MessageFilter): grpc wrapper for filter
+    """
     if keys is None:
         keys = []
     content = deepcopy(content)
@@ -129,8 +149,19 @@ def filter_to_grpc(message_type: str, content: dict, keys=None) -> MessageFilter
     return MessageFilter(messageType=message_type, fields=content)
 
 
-def convert_to_request(description: str, connectivity: str, event_id: EventID, message: dict,
+def convert_to_request(description: str, connectivity: str, event_id: EventID, message: Message,
                        key_fields=None) -> PlaceMessageRequest:
+    """ Creates grpc request for sending message to the system.
+        Parameters:
+            description (str): Text for displaying in report.
+            connectivity (str): Name of connectivity box (fix-bs-eq-trqx, fix-fh-eq-paris, gtwquod3).
+            event_id (str): ID of the parent event.
+            message (Message): Message, which must be sent to the system.
+            key_fields (list): Optional parameter. A list of fields, that must be used as key fields during the message
+                receiving by act box. Default value is None.
+        Returns:
+            convert_to_request (PlaceMessageRequest): request to act box
+    """
     if key_fields is None:
         key_fields = []
     connectivity = ConnectionID(session_alias=connectivity)
@@ -145,6 +176,21 @@ def convert_to_request(description: str, connectivity: str, event_id: EventID, m
 
 def create_check_rule(description: str, message_filter: MessageFilter, checkpoint, connectivity: str, event_id: EventID,
                       direction=Direction.Value("FIRST"), timeout=3000) -> CheckRuleRequest:
+    """ Creates grpc request for verification only one message
+        Parameters:
+            description (str): Text for displaying in report.
+            message_filter (MessageFilter): Expected values in grpc wrapper.
+            checkpoint (str): Checkpoint id, which is used as start point during message verification.
+            connectivity (str): Name of connectivity box (fix-bs-eq-trqx, fix-fh-eq-paris, gtwquod3).
+            event_id: ID of the parent event.
+            direction (Direction): Optional parameter. Direction of messages stream.
+                Values: FIRST - from system to connectivity; SECOND - from connectivity to system
+                Default value is FIRST.
+            timeout (int): Optional parameter. Time in milliseconds, during which check1 must find message.
+                Default value is 3000 ms.
+        Returns:
+            create_check_rule (CheckRuleRequest): request to check1 box
+    """
     return CheckRuleRequest(
         connectivity_id=ConnectionID(session_alias=connectivity),
         filter=message_filter,
@@ -158,6 +204,24 @@ def create_check_rule(description: str, message_filter: MessageFilter, checkpoin
 
 def create_check_sequence_rule(description: str, prefilter: PreFilter, msg_filters: list, checkpoint, connectivity: str,
                                event_id: EventID, check_order=True, timeout=3000, direction=Direction.Value("FIRST")):
+    """ Creates grpc request for verification several messages and an order of their receiving
+        Parameters:
+            description (str): Text for displaying in report.
+            prefilter (PreFilter):
+            msg_filters (list): A list of filters (a sets of expected values) in grpc wrappers.
+            checkpoint (str): Checkpoint id, which is used as start point during message verification.
+            connectivity (str): Name of connectivity box (fix-bs-eq-trqx, fix-fh-eq-paris, gtwquod3).
+            event_id: ID of the parent event.
+            check_order (bool): Optional parameter. Defines if the order of messages is considered
+                in the verification or not. Default value is True.
+            direction (Direction): Optional parameter. Direction of messages stream.
+                Values: FIRST - from system to connectivity; SECOND - from connectivity to system.
+                Default value is FIRST.
+            timeout (int): Optional parameter. Time in milliseconds, during which check1 must find message.
+                Default value is 3000 ms.
+        Returns:
+            create_check_sequence_rule (CheckSequenceRuleRequest): request to check1 box
+    """
     return CheckSequenceRuleRequest(
         connectivity_id=ConnectionID(session_alias=connectivity),
         pre_filter=prefilter,
@@ -172,10 +236,21 @@ def create_check_sequence_rule(description: str, prefilter: PreFilter, msg_filte
 
 
 def create_event_id() -> EventID:
+    """ Creates unique id for event.
+        Returns:
+            create_event_id (EventID): unique id.
+    """
     return EventID(id=str(uuid1()))
 
 
 def create_event(event_name: str, parent_id: EventID = None) -> EventID:
+    """ Creates a new event.
+        Parameters:
+            event_name (str): Text that will be displayed in the report.
+            parent_id (EventID): Optional parameter. ID of the parent event. Default value is None.
+        Returns:
+            create_event (EventID): id of the created event.
+    """
     event_store = Stubs.event_store
     seconds, nanos = timestamps()
     event_id = create_event_id()
@@ -194,6 +269,14 @@ def create_event(event_name: str, parent_id: EventID = None) -> EventID:
 
 
 def prefilter_to_grpc(content: dict, _nesting_level=0) -> PreFilter:
+    """ Creates a grpc wrapper for specified fields and their values to preliminary filtration of messages
+        in the stream.
+        Parameters:
+            content (dict): Fields and their values, which are used to filter.
+            _nesting_level (int): For internal purpose.
+        Returns:
+            prefilter_to_grpc (PreFilter): fields and their values in the grpc wrapper.
+    """
     content = deepcopy(content)
     for tag in content:
         if isinstance(content[tag], (str, int, float)):
@@ -211,4 +294,10 @@ def prefilter_to_grpc(content: dict, _nesting_level=0) -> PreFilter:
 
 
 def create_checkpoint_request(event_id: EventID) -> CheckpointRequest:
+    """ Creates grpc request for sending checkpoint.
+        Parameters:
+            event_id (EventID): ID of the parent (for checkpoint) event.
+        Returns:
+            create_checkpoint_request (CheckpointRequest): grpc request with checkpoint
+    """
     return CheckpointRequest(description="TestCheckpoint", parent_event_id=event_id)
