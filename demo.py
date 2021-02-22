e8f8c9de62047328c43591dc280aa9940232bb38
BritConstantine <brit111001@gmail.com>
2021-02-22 09:28:09 +0200

    cleaned demo.py and QAP_1544.py

--- demo.py
+++ demo.py
@@ -2,25 +2,7 @@ import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
 from stubs import Stubs
-from th2_grpc_sim_quod.sim_pb2 import TemplateQuodNOSRule, TemplateQuodOCRRule
-from th2_grpc_common.common_pb2 import ConnectionID
-from test_cases import QAP_1641
-from test_cases import QAP_1987
-from test_cases import QAP_2407
-from test_cases import QAP_2409
-from test_cases import QAP_2425_SIM
-from test_cases import QAP_2462_SIM
-from test_cases import QAP_2540
-from test_cases import QAP_2561
-from test_cases import QAP_2620
-from test_cases import QAP_2684
-from test_cases import QAP_2702
-from test_cases import QAP_2740
-from test_cases import QAP_2769
-from th2_grpc_sim.sim_pb2 import RuleID
-from win_gui_modules.utils import prepare_fe, close_fe
-from google.protobuf.empty_pb2 import Empty
-
+from test_cases import QAP_585
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -34,157 +16,10 @@ def test_run():
     # Generation id and time for test run
     report_id = bca.create_event('QUOD demo ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    # Reference data for test cases
 
-    instrument = {
-        'Symbol': 'FR0000121329',
-        'SecurityID': 'FR0000121329',
-        'SecurityIDSource': '4',
-        'SecurityExchange': 'XPAR'
 
-    }
-
-    instrument_2 = {
-        'Symbol': 'SE0000818569_SEK',
-        'SecurityID': 'SE0000818569',
-        'SecurityIDSource': '4',
-        'SecurityExchange': 'XSTO'
-    }
-    instrument_3 = {
-        'Symbol': 'FR0010263202_EUR',
-        'SecurityID': 'FR0010263202',
-        'SecurityIDSource': 4,
-        'SecurityExchange': 'XPAR'
-    }
-
-    instrument_4 = {
-        'Symbol': 'FR0000125007',
-        'SecurityID': 'FR0000125007',
-        'SecurityIDSource': '4',
-        'SecurityExchange': 'XPAR'
-    }
-    instrument_5 = {
-        'Symbol': 'FR0010542647_EUR',
-        'SecurityID': 'FR0010542647',
-        'SecurityIDSource': '4',
-        'SecurityExchange': 'XPAR'
-    }
-
-    instrument_6 = {
-        'Symbol': 'FR0000125460_EUR',
-        'SecurityID': 'FR0000125460',
-        'SecurityIDSource': '4',
-        'SecurityExchange': 'XPAR'
-    }
-
-    # Specific data for test case test
     test_cases = {
-        'QAP-2425': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'Account': 'KEPLER',
-            'HandlInst': '2',
-            'Side': '1',
-            'SenderCompID': 'QUODFX_UAT',
-            'TargetCompID': 'QUOD3',
-            'OrderQty': '500',
-            'OrdType': '2',
-            'Price': '20',
-            'TimeInForce': '0',
-            'Instrument': instrument_3
-        },
-        'QAP-2422': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'SenderCompID': 'QUODFX_UAT',
-            'TargetCompID': 'QUOD3',
-            'Account': 'KEPLER',
-            'HandlInst': '2',
-            'Side': '1',
-            'OrderQty': '400',
-            'OrdType': '2',
-            'Price': '20',
-            'TimeInForce': '0',
-            'Instrument': instrument_2
-        },
-        'QAP-AMEND': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'SenderCompID': 'QUODFX_UAT',
-            'TargetCompID': 'QUOD3',
-            'Account': 'KEPLER',
-            'HandlInst': '2',
-            'Side': '1',
-            'OrderQty': '400',
-            'newOrderQty': '500',
-            'OrdType': '2',
-            'Price': '20',
-            'newPrice': '19',
-            'TimeInForce': '0',
-            'Instrument': instrument_3
-        },
-        'QUOD-TRADE': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'SenderCompID': 'QUODFX_UAT',
-            'Account': 'KEPLER',
-            'HandlInst': '2',
-            'OrderQty': '500',
-            'OrdType': '2',
-            'Price': '20',
-            'TimeInForce': '0',
-            'TargetCompID': 'QUOD3',
-            'Instrument': instrument_2
-        },
-        'QUOD-TRADE2': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'SenderCompID': 'QUODFX_UAT',
-            'Account': 'KEPLER',
-            'HandlInst': '1',
-            'OrderQty_ord1': '200',
-            'OrderQty_ord2': '100',
-            'OrdType': '2',
-            'Price': '20',
-            'TimeInForce': '0',
-            'TargetCompID': 'QUOD3',
-            'Instrument': instrument_2
-        },
-        'QUOD-AMEND-TRADE': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'SenderCompID': 'QUODFX_UAT',
-            'Account': 'KEPLER',
-            'HandlInst': '2',
-            'OrderQty': '500',
-            'OrdType': '2',
-            'Price1': '20',
-            'Price2': '23',
-            'TimeInForce': '0',
-            'TargetCompID': 'QUOD3',
-            'Instrument': instrument_2
-        },
-        'QUOD_PART_TRADE': {
-            **channels,
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'gtwquod3',
-            'SenderCompID': 'QUODFX_UAT',
-            'Account': 'KEPLER',
-            'HandlInst': '2',
-            'OrderQty1': '500',
-            'OrderQty2': '800',
-            'OrdType': '2',
-            'Price': '20',
-            'TimeInForce': '0',
-            'TargetCompID': 'QUOD3',
-            'Instrument': instrument_2
-        },
+
         'RFQ_example': {
             **channels,
             'case_id': bca.create_event_id(),
@@ -195,49 +30,10 @@ def test_run():
         }
     }
 
-    # start rule
-    # NOS_1 = Stubs.simulator.createQuodNOSRule(request=TemplateQuodNOSRule(
-    #     connection_id=ConnectionID(session_alias='fix-bs-eq-paris')
-    # ))
-    # OCR_1 = Stubs.simulator.createQuodOCRRule(request=TemplateQuodOCRRule(
-    #     connection_id=ConnectionID(session_alias='fix-bs-eq-paris')))
-    # NOS_2 = Stubs.simulator.createQuodNOSRule(request=TemplateQuodNOSRule(
-    #     connection_id=ConnectionID(session_alias='fix-bs-eq-trqx')
-    # ))
-    # OCR_2 = Stubs.simulator.createQuodOCRRule(request=TemplateQuodOCRRule(
-    #     connection_id=ConnectionID(session_alias='fix-bs-eq-trqx')))
-    # logger.info(f"Start rules with id's: \n {NOS_1}, {OCR_1}, {NOS_2}, {OCR_2}")
-
-    try:
-        # amend_and_trade.execute('QUOD-AMEND-TRADE', report_id, test_cases['QUOD-AMEND-TRADE'])
-        # part_trade.execute('QUOD_PART_TRADE', report_id, test_cases['QUOD_PART_TRADE'])
-        # send_and_amend.execute('QAP-AMEND', report_id, test_cases['QAP-AMEND'])
-        # simple_trade2.execute('QUOD-TRADE2', report_id, test_cases['QUOD-TRADE2'])
-        # simple_trade.execute('QUOD-TRADE', report_id, test_cases['QUOD-TRADE'])
-        # RFQ_example.execute('RFQ_example', report_id, test_cases['RFQ_example'])
-
-        # QAP_1641.execute(report_id)
-        QAP_1987.execute(report_id)
-        # QAP_2407.execute(report_id)
-        # QAP_2409.execute(report_id)
-        # QAP_2425_SIM.execute(report_id)
-        # QAP_2462_SIM.execute(report_id)
-        # QAP_2540.execute(report_id)
-        # QAP_2561.execute(report_id)
-        # QAP_2620.execute(report_id)
-        # QAP_2684.execute(report_id)
-        # QAP_2702.execute(report_id)
-        # QAP_2740.execute(report_id)
-        # QAP_2769.execute(report_id)
-
-    except Exception as e:
+     try:
+         QAP_585.execute('RFQ_example', report_id, test_cases['RFQ_example'])
+    except Exception:
         logging.error("Error execution", exc_info=True)
-
-    # stop rule
-    # Stubs.core.removeRule(NOS_1)
-    # Stubs.core.removeRule(OCR_1)
-    # Stubs.core.removeRule(NOS_2)
-    # Stubs.core.removeRule(OCR_2)
 
 
 if __name__ == '__main__':
--- quod_qa/fx/QAP_1544.py
+++ quod_qa/fx/QAP_1544.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from custom import basic_custom_actions as bca, tenor_settlement_date as tsd
 from stubs import Stubs
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
@@ -11,15 +10,7 @@ timeouts = True
 
 def execute(case_name, report_id, case_params):
     act = Stubs.fix_act
-    event_store = Stubs.event_store
     verifier = Stubs.verifier
-    # simulator = Stubs.simulator
-    quote_request_qty = '500000'
-    order_qty = '1000000'
-    instrument = 'EUR/USD'
-    tenor = 'FXSPOT'
-    settl_date = tsd.spo()
-    settl_type = '0'
 
     seconds, nanos = bca.timestamps()  # Store case start time
     case_id = bca.create_event(case_name, report_id)
@@ -28,55 +19,57 @@ def execute(case_name, report_id, case_params):
         'Account': case_params['Account'],
         'Side': 1,
         'Instrument': {
-            'Symbol': instrument,
-            'SecurityType': tenor
-        },
-        'SettlDate': settl_date,
-        'SettlType': settl_type
-    }
+            'Symbol': 'EUR/USD',
+            'SecurityType': 'FXSPOT'
+            },
+        'SettlDate': tsd.spo(),
+        'SettlType': '0',
+        'RfqQty': '500000',
+        'OrderQty': '1000000'
+        }
 
     rfq_params = {
         'QuoteReqID': bca.client_orderid(9),
         'NoRelatedSymbols': [{
-                                **reusable_params,
-                                'Currency': instrument[0:3],
-                                'QuoteType': '1',
-                                'OrderQty': quote_request_qty,
-                                'OrdType': 'D',
-                                'ExpireTime': reusable_params['SettlDate'] + '-23:59:00.000',
-                                'TransactTime': (datetime.utcnow().isoformat())}]
-    }
+            **reusable_params,
+            'Currency': reusable_params['Instrument']['Symbol'][0:3],
+            'QuoteType': '1',
+            'OrderQty': reusable_params['RfqQty'],
+            'OrdType': 'D',
+            'ExpireTime': reusable_params['SettlDate'] + '-23:59:00.000',
+            'TransactTime': (datetime.utcnow().isoformat())}]
+        }
     logger.debug("Send new order with ClOrdID = {}".format(rfq_params['QuoteReqID']))
 
     send_rfq = act.placeQuoteFIX(
-        bca.convert_to_request(
-            'Send QuoteRequest',
-            case_params['TraderConnectivity'],
-            case_id,
-            bca.message_to_grpc('QuoteRequest', rfq_params, case_params['TraderConnectivity'])
-        ))
+            bca.convert_to_request(
+                    'Send QuoteRequest',
+                    case_params['TraderConnectivity'],
+                    case_id,
+                    bca.message_to_grpc('QuoteRequest', rfq_params, case_params['TraderConnectivity'])
+                    ))
 
     quote_params = {
         **reusable_params,
         'QuoteReqID': rfq_params['QuoteReqID'],
         'Product': 4,
         'OfferPx': '35.001',
-        'OfferSize': quote_request_qty,
+        'OfferSize': reusable_params['RfqQty'],
         'QuoteID': '*',
         'OfferSpotRate': '35.001',
         'ValidUntilTime': '*',
         'Currency': 'EUR'
-    }
+        }
 
     verifier.submitCheckRule(
-        bca.create_check_rule(
-            'Receive Quote message',
-            bca.filter_to_grpc('Quote', quote_params, ['QuoteReqID']),
-            send_rfq.checkpoint_id,
-            case_params['TraderConnectivity'],
-            case_id
-        )
-    )
+            bca.create_check_rule(
+                    'Receive Quote message',
+                    bca.filter_to_grpc('Quote', quote_params, ['QuoteReqID']),
+                    send_rfq.checkpoint_id,
+                    case_params['TraderConnectivity'],
+                    case_id
+                    )
+            )
 
     order_params = {
         **reusable_params,
@@ -84,54 +77,46 @@ def execute(case_name, report_id, case_params):
         'ClOrdID': bca.client_orderid(9),
         'OrdType': 'D',
         'TransactTime': (datetime.utcnow().isoformat()),
-        'OrderQty': order_qty,
+        'OrderQty': reusable_params['OrderQty'],
         'Price': send_rfq.response_messages_list[0].fields['OfferPx'].simple_value,
         'Product': 4,
         'TimeInForce': 4
-    }
+        }
 
     send_order = act.placeOrderFIX(
-        bca.convert_to_request(
-            'Send NewOrderSingle',
-            case_params['TraderConnectivity'],
-            case_id,
-            bca.message_to_grpc('NewOrderSingle', order_params, case_params['TraderConnectivity'])
-        ))
+            bca.convert_to_request(
+                    'Send NewOrderSingle',
+                    case_params['TraderConnectivity'],
+                    case_id,
+                    bca.message_to_grpc('NewOrderSingle', order_params, case_params['TraderConnectivity'])
+                    ))
 
     er_pending_params = {
         'Side': reusable_params['Side'],
         'Account': reusable_params['Account'],
         'ClOrdID': order_params['ClOrdID'],
         'OrderQty': order_params['OrderQty'],
-        # 'LeavesQty': order_params['OrderQty'],
         'TimeInForce': order_params['TimeInForce'],
-        # 'OrdType': order_params['OrdType'],
-        'OrdRejReason': '99',#OTHER
-        # # 'Price': send_rfq.response_messages_list[0].fields['OfferPx'].simple_value,
-        # # 'OrderID': send_order.response_messages_list[0].fields['OrderID'].simple_value,
-        # 'NoParty': [
-        #     {'PartyRole': 36, 'PartyID': 'gtwquod5', 'PartyIDSource': 'D'}
-        # ],
+        'OrdRejReason': '99',
         'Instrument': {
             'Symbol': 'EUR/USD',
             'SecurityIDSource': 8,
             'SecurityID': 'EUR/USD',
             'SecurityExchange': 'XQFX'
-        },
-        # 'SettlCurrency': 'USD',
+            },
         'Text': "11605 'OrdQty' (1000000) doesn't match the quote's 'OfferSize' (500000)",
 
-    }
+        }
 
     verifier.submitCheckRule(
-        bca.create_check_rule(
-            'Receive ExecutionReport (pending)',
-            bca.filter_to_grpc('ExecutionReport', er_pending_params, ['ClOrdID', 'OrdStatus', 'ExecType']),
-            send_order.checkpoint_id,
-            case_params['TraderConnectivity'],
-            case_id
-        )
-    )
+            bca.create_check_rule(
+                    'Receive ExecutionReport (pending)',
+                    bca.filter_to_grpc('ExecutionReport', er_pending_params, ['ClOrdID', 'OrdStatus', 'ExecType']),
+                    send_order.checkpoint_id,
+                    case_params['TraderConnectivity'],
+                    case_id
+                    )
+            )
 
     logger.info("Case {} was executed in {} sec.".format(
-        case_name, str(round(datetime.now().timestamp() - seconds))))
+            case_name, str(round(datetime.now().timestamp() - seconds))))
