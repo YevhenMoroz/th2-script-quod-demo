b5bc1d23f9189730824322e612e0ab38752b1bfa
BritConstantine <brit111001@gmail.com>
2021-02-22 16:16:44 +0200

    Added case QAP_1537.py and default_params_fx.py

---
+++ quod_qa/fx/default_params_fx.py
@@ -0,0 +1,19 @@
+from custom import tenor_settlement_date as tsd
+defauot_quote_params = {
+    'Side': 1,
+    'Instrument': {
+        'Symbol': 'EUR/USD',
+        'SecurityType': 'FXSPOT'
+        },
+    'SettlDate': tsd.spo(),
+    'SettlType': '0',
+    'OrderQty': 1000000
+    }
+
+text_messages = {
+    'erPending': 'Receive ExecutionReport (pending)',
+    'sendQR': 'Send QuoteRequest',
+    'recQ': 'Receive Quote message',
+    'sendNOS': 'Send NewOrderSingle',
+    'recQC': 'Receive QuoteCancel message'
+    }
