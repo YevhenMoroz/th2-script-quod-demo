c44d517c8fd32addd1d35179e2dbcdd0e8f26943
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-03-16 18:54:07 +0200

    update QAP_1952.py

--- quod_qa/eq/Algo_Multilisted/QAP_1952.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1952.py
@@ -88,9 +88,9 @@ def execute(report_id):
     fix_cancel = FixMessage(cancel_parms)
     responce_cancel = fix_manager_qtwquod5.Send_OrderCancelRequest_FixMessage(fix_cancel)
     cancel_er_params = {
-        "OrigClOrdID": fix_message_multilisting.get_ClOrdID(),
+        "ClOrdID": fix_message_multilisting.get_ClOrdID(),
         "OrdStatus": "4"
     }
-    fix_verifier_ss.CheckExecutionReport(cancel_er_params, responce_cancel )
+    fix_verifier_ss.CheckExecutionReport(cancel_er_params, responce_cancel)
     rule_manager.remove_rule(nos_rule)
     rule_manager.remove_rule(ocr_rule)
