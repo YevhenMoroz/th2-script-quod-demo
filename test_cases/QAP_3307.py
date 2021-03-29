baa7c27382a19bb5e69d37e956ef575282628285
lado.mindadze <lado.mindadze@exactprosystems.com>
2021-03-29 18:32:27 +0300

    QAP_3307 updated (added book order)

--- test_cases/QAP_3307.py
+++ test_cases/QAP_3307.py
@@ -10,6 +10,7 @@ from win_gui_modules.order_ticket_wrappers import NewOrderDetails
 from win_gui_modules.utils import set_session_id, prepare_fe, close_fe, get_base_request, call
 from win_gui_modules.order_book_wrappers import ManualExecutingDetails
 from win_gui_modules.order_book_wrappers import CompleteOrdersDetails
+from win_gui_modules.middle_office_wrappers import ModifyTicketDetails
 from win_gui_modules.wrappers import *
 from rule_management import RuleManager
 
@@ -95,6 +96,45 @@ def execute(report_id):
         # complete_orders_details.set_selected_row_count(2)
 
         call(service.completeOrders, complete_orders_details.build())
+
+
+        # book order
+        middle_office_service = Stubs.win_act_middle_office_service
+
+        modify_request = ModifyTicketDetails(base=base_request)
+        modify_request.set_filter(["Owner", username, "Order ID", care_order_id])
+        #modify_request.set_selected_row_count(4)
+
+        ticket_details = modify_request.add_ticket_details()
+        ticket_details.set_client("MOClient")
+        ticket_details.set_trade_date("2/21/2021")
+        ticket_details.set_net_gross_ind("Gross")
+        # ticket_details.set_give_up_broker("GiveUpBroker")
+        ticket_details.set_agreed_price("5")
+
+        settlement_details = modify_request.add_settlement_details()
+        settlement_details.set_settlement_type("Regular")
+        settlement_details.set_settlement_currency("EUR")
+        settlement_details.set_exchange_rate("1")
+        settlement_details.set_exchange_rate_calc("Multiply")
+        # settlement_details.toggle_settlement_date()
+        settlement_details.set_settlement_date("2/21/2021")
+        settlement_details.toggle_recompute()
+
+        commissions_details = modify_request.add_commissions_details()
+        #commissions_details.toggle_manual()
+        commissions_details.add_commission(basis="Absolute", rate="5")
+
+        extraction_details = modify_request.add_extraction_details()
+        extraction_details.set_extraction_id("BookExtractionId")
+        extraction_details.extract_net_price("book.netPrice")
+        extraction_details.extract_net_amount("book.netAmount")
+        extraction_details.extract_total_comm("book.totalComm")
+        extraction_details.extract_gross_amount("book.grossAmount")
+        extraction_details.extract_total_fees("book.totalFees")
+        extraction_details.extract_agreed_price("book.agreedPrice")
+
+        response = call(middle_office_service.bookOrder, modify_request.build())
 
     except Exception as e:
         logging.error("Error execution", exc_info=True)
