610e129edd84d8f541c5491c5408a389beb1e3eb
BritConstantine <brit111001@gmail.com>
2021-04-06 12:58:06 +0300

    aply local changes for my user connection and act

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from schemas import rfq_tile_example
 from stubs import Stubs
 from quod_qa.fx.fx_taker_rfq import QAP_574, QAP_585, QAP_569,  QAP_578, QAP_579, QAP_580
 from quod_qa.fx.fx_taker_rfq import QAP_574, QAP_585, QAP_569, QAP_578, QAP_579, QAP_580
-
+from quod_qa.fx import ui_tests
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -17,7 +17,7 @@ channels = dict()
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event(' tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('kbrit tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
 
     test_cases = {
@@ -34,7 +34,7 @@ def test_run():
     try:
 
         # QAP_1746.execute(report_id, test_cases['RFQ_example'])
-
+        ui_tests.execute(report_id)
         rm = RuleManager()
 
     except Exception:
