876aa5bb5378dbe15c11884b54768bc91ebfac62
BritConstantine <brit111001@gmail.com>
2021-04-06 09:36:52 +0300

    small cleaning of demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,11 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
-# from quod_qa.fx import  QAP_1746
-# from quod_qa.fx import ui_tests, QAP_1746
 from rule_management import RuleManager
+from schemas import rfq_tile_example
 from stubs import Stubs
-from quod_qa.fx import ui_tests
-
-# from test_cases import QAP_1552, QAP_585, QAP_2143, QAP_1560_class
-# from test_cases import QAP_1552
+from quod_qa.fx.fx_taker_rfq import QAP_574, QAP_585, QAP_569,  QAP_578, QAP_579, QAP_580
+from quod_qa.fx.fx_taker_rfq import QAP_574, QAP_585, QAP_569, QAP_578, QAP_579, QAP_580
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -20,7 +17,7 @@ channels = dict()
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('kbrit tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event(' tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
 
     test_cases = {
@@ -35,18 +32,10 @@ def test_run():
         }
 
     try:
-        rm = RuleManager()
-        # rm.add_RFQ('fix-fh-fx-rfq')
-        # rm.add_TRFQ('fix-fh-fx-rfq')
-        # rm.remove_rules_by_id_range( 7,15)
-        # rm.print_active_rules()
 
-
-        ui_tests.execute(report_id)
-
-        # QAP_568.execute(report_id)
         # QAP_1746.execute(report_id, test_cases['RFQ_example'])
-        # QAP_1552.execute(report_id, test_cases['RFQ_example'])
+
+        rm = RuleManager()
 
     except Exception:
         logging.error("Error execution", exc_info=True)
@@ -56,3 +45,4 @@ if __name__ == '__main__':
     logging.basicConfig()
     test_run()
     Stubs.factory.close()
+
