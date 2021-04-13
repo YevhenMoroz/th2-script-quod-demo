e14941d68b0f621094e118c292a5d8d374653346
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-04-13 19:18:37 +0300

    modify QAP_1035.py

--- quod_qa/eq/Care/QAP_1035.py
+++ quod_qa/eq/Care/QAP_1035.py
@@ -48,7 +48,7 @@ def execute(report_id):
     eq_wrappers.switch_user(session_id, case_id)
     # endregion
     # region create CO
-    eq_wrappers.create_care_order(base_request, qty, client, lookup, order_type, recipient, desk,price)
+    eq_wrappers.create_care_order(base_request, qty, client, lookup, order_type, recipient, desk, price)
     # endregion
     # region Check values in OrderBook
     before_order_details_id = "before_order_details"
@@ -57,13 +57,22 @@ def execute(report_id):
     order_details.set_extraction_id(before_order_details_id)
 
     order_status = ExtractionDetail("order_status", "Sts")
+    order_qty = ExtractionDetail("order_qty", "Qty")
+    order_price = ExtractionDetail("order_price", "LmtPrice")
+    order_pts = ExtractionDetail("order_pts", "PostTradeStatus")
+    order_dfd = ExtractionDetail("order_dfd", "DoneForDay")
+    order_es = ExtractionDetail("order_es", "ExecSts")
     order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
-
+                                                                                            order_qty,
+                                                                                            order_price,
+                                                                                            order_pts,
+                                                                                            order_dfd,
+                                                                                            order_es
                                                                                             ])
     order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
     call(act.getOrdersDetails, order_details.request())
     call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
-                                                 [verify_ent("Order Status", order_status.name, "Sent"),
+                                                 [verify_ent("Order Status", order_status.name, "Sent")
                                                   ]))
 
     # endregion
@@ -81,4 +90,15 @@ def execute(report_id):
     # endregion
     # region complete
     eq_wrappers.complete_order(base_request)
+    # endregion
+    # region Check values after complete
+    call(act.getOrdersDetails, order_details.request())
+    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
+                                                 [verify_ent("Order Status", order_status.name, "Open"),
+                                                  verify_ent("Order Qty", order_qty.name, qty),
+                                                  verify_ent("Order Price", order_price.name, price),
+                                                  verify_ent("PostTradeStatus", order_pts.name, "ReadyToBook"),
+                                                  verify_ent("DoneForDay", order_dfd.name, "Yes"),
+                                                  verify_ent("ExecSts", order_status.name, "Filled")
+                                                  ]))
     # endregion
