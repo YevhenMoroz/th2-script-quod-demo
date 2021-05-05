269af2af00fe11d3b7f8a71d57204d2385abe2d3
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2021-05-05 20:02:07 +0300

    add QAP-1037(update)

--- quod_qa/eq/Care/QAP_1072.py
+++ quod_qa/eq/Care/QAP_1072.py
@@ -7,8 +7,8 @@ from quod_qa.wrapper.fix_verifier import FixVerifier
 from win_gui_modules.order_book_wrappers import OrdersDetails, CancelOrderDetails
 from custom.basic_custom_actions import create_event, timestamps
 import time
-from quod_qa.wrapper.fix_manager import FixManager
 from quod_qa.wrapper.fix_message import FixMessage
+from quod_qa.wrapper.fix_manager import FixManager
 from rule_management import RuleManager
 from quod_qa.wrapper import eq_wrappers
 from stubs import Stubs
@@ -21,7 +21,7 @@ logger.setLevel(logging.INFO)
 timeouts = True
 
 def execute(report_id):
-    case_name = "QAP-1071"
+    case_name = "QAP-1072"
 
     seconds, nanos = timestamps()  # Store case start time
 
@@ -36,45 +36,32 @@ def execute(report_id):
     lookup = "PROL"
     client = "CLIENTSKYLPTOR"
     # endregion
+    list_param = {'qty': qty, 'newPrice': newPrice}
     # region Open FE
 
     case_id = create_event(case_name, report_id)
     session_id = set_session_id()
+    fix_verifier_ss = FixVerifier('gtwquod5', case_id)
     set_base(session_id, case_id)
     base_request = get_base_request(session_id, case_id)
     work_dir = Stubs.custom_config['qf_trading_fe_folder2']
     username = Stubs.custom_config['qf_trading_fe_user']
     password = Stubs.custom_config['qf_trading_fe_password']
-    eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
+    eq_wrappers.open_fe(session_id,report_id,case_id, work_dir,username,password)
     # endregionA
+
     # region Create CO
     fix_message = eq_wrappers.create_order_via_fix(case_id, 3, 2, client, 2, qty, 0, price)
-    param_list = {'Price': newPrice}
-    #Amend fix order
+    param_list = {'Qty': newPrice}
+    # region ManualExecute
+    eq_wrappers.manual_execution(base_request, str(int((int(qty)/2))), price)
+    responce = fix_message.pop('responce')
+    # Amend fix order
     eq_wrappers.amend_order_via_fix(fix_message, case_id, param_list)
-    #region
-
-    # region Reject amend
-    eq_wrappers.reject_order(lookup, qty, price)
     # endregion
+    # region accept amend
+    eq_wrappers.accept_order(lookup, qty, price)
+    # endregion
+    eq_wrappers.check_value_via_fix(list_param, case_id, responce)
 
-    #region CheckOrder
-    before_order_details_id = "before_order_details"
-    order_details = OrdersDetails()
-    order_details.set_default_params(base_request)
-    order_details.set_extraction_id(before_order_details_id)
-    order_status = ExtractionDetail("order_status", "Sts")
-    order_id = ExtractionDetail("order_id", "Order ID")
-    order_qty = ExtractionDetail("order_qty", "Qty")
-    order_price = ExtractionDetail("order_price", "LmtPrice")
-    order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
-                                                                                            order_id,
-                                                                                            order_qty,
-                                                                                            order_price
-                                                                                            ])
-    order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
-    request = call(act.getOrdersDetails, order_details.request())
-    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
-                                                 [verify_ent("Order Price", order_price.name, price)
-                                                  ]))
-    #endregion
+
