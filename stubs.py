2adec4fa16ec61e2f226c048214e7d182408686f
dmitry.zhuravlev <T0wer_Enc0unter>
2021-05-05 12:07:00 +0300

    Added absolute path for configs directory in stubs.py

--- stubs.py
+++ stubs.py
@@ -1,3 +1,4 @@
+from os.path import abspath, dirname, join
 from th2_common.schema.factory.common_factory import CommonFactory
 from th2_grpc_act_gui_quod.act_ui_win_service import ActUIWinService
 from th2_grpc_act_gui_quod.ar_operations_service import AggregatedRatesOperationsService
@@ -12,13 +13,13 @@ from th2_grpc_act_gui_quod.cp_operations_service import ClientPricingOperationsS
 from th2_grpc_sim_http.sim_template_service import SimTemplateService
 
 
-
 class Stubs:
+    configs_dir = join(dirname(abspath(__file__)), 'configs')
     factory = CommonFactory(
-        grpc_router_config_filepath="./configs/grpc.json",
-        rabbit_mq_config_filepath="./configs/rabbit.json",
-        mq_router_config_filepath="./configs/mq.json",
-        custom_config_filepath="./configs/script-params.json"
+        grpc_router_config_filepath=join(configs_dir, "grpc.json"),
+        rabbit_mq_config_filepath=join(configs_dir, "rabbit.json"),
+        mq_router_config_filepath=join(configs_dir, "mq.json"),
+        custom_config_filepath=join(configs_dir, "script-params.json")
     )
     fix_act = factory.grpc_router.get_service(ActFixService)
     event_store = factory.event_batch_router
@@ -35,4 +36,4 @@ class Stubs:
 
     custom_config = factory.create_custom_configuration()
     session_id = None
-    frontend_is_open = True
+    frontend_is_open = False
