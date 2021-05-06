b3b7ec5044ee69be2e0b17db2cbe48078b9ee891
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-05-06 19:02:25 +0300

    add QAP_2408.py

--- quod_qa/eq/Sorping/QAP_2408.py
+++ quod_qa/eq/Sorping/QAP_2408.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 from copy import deepcopy
 from datetime import datetime
 
@@ -20,15 +21,15 @@ timeouts = True
 
 qty = 2000
 display_qty = 100
-limit = 20
-side = 1
+price = 33
+side = 2
 ex_destination_1 = "QL1"
 client = "KEPLER"
 order_type = "Limit"
 case_name = os.path.basename(__file__)
-connectivity_fh = "fix-fh-310-columbia"
-connectivity_bs = "fix-bs-310-columbia"
-connectivity_ss = "fix-ss-310-columbia-standart"
+connectivity_feed_handler = "fix-fh-310-columbia"
+connectivity_buy_side = "fix-bs-310-columbia"
+connectivity_sell_side = "fix-ss-310-columbia-standart"
 listing_id1 = "9400000038"
 
 instrument = {
@@ -40,10 +41,11 @@ instrument = {
 
 def rule_creation():
     rule_manager = RuleManager()
-    ioc_rule = rule_manager.add_NewOrdSingle_IOC(connectivity_bs, "KEPLER", "QDL1", False, 2000, 33)
-    nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(connectivity_bs, ex_destination_1 + "_" + client, ex_destination_1, limit)
-    ocr_rule = rule_manager.add_OrderCancelRequest('fix-bs-eq-' + ex_destination_1.lower(), ex_destination_1 + '_' + client, ex_destination_1, True)
-    return [nos_rule, ocr_rule, ioc_rule]
+    ioc_rule_1 = rule_manager.add_NewOrdSingle_IOC(connectivity_buy_side, "KEPLER", "QDD1", False, qty, price)
+    ioc_rule_2 = rule_manager.add_NewOrdSingle_IOC(connectivity_buy_side, "KEPLER", "QDD2", False, qty, price)
+    nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(connectivity_buy_side, "KEPLER", "QDL1", price)
+    ocr_rule = rule_manager.add_OrderCancelRequest(connectivity_buy_side, "KEPLER", "QDL1", True)
+    return [nos_rule, ioc_rule_1, ioc_rule_2, ocr_rule]
 
 def rule_destroyer(list_rules):
     if list_rules != None:
@@ -54,7 +56,7 @@ def rule_destroyer(list_rules):
 def send_MD(symbol: str, case_id :str):
     MDRefID = Stubs.simulator.getMDRefIDForConnection(request=RequestMDRefID(
         symbol=symbol,
-        connection_id=ConnectionID(session_alias=connectivity_fh)
+        connection_id=ConnectionID(session_alias=connectivity_feed_handler)
     )).MDRefID
     mdir_params_bid = {
         'MDReqID': MDRefID,
@@ -76,66 +78,251 @@ def send_MD(symbol: str, case_id :str):
 
     Stubs.fix_act.sendMessage(request=convert_to_request(
         'Send MarketDataSnapshotFullRefresh',
-        connectivity_fh,
+        connectivity_feed_handler,
         case_id,
-        message_to_grpc('MarketDataSnapshotFullRefresh', mdir_params_bid, connectivity_fh)
+        message_to_grpc('MarketDataSnapshotFullRefresh', mdir_params_bid, connectivity_feed_handler)
     ))
 
 def execute(report_id):
     case_id = bca.create_event(os.path.basename(__file__), report_id)
     rule_list = rule_creation()
     # send_MD(listing_id1, case_id)
-    fix_manager_qtwquod5 = FixManager(connectivity_ss, case_id)
-    fix_verifier_ss = FixVerifier(connectivity_ss, case_id)
-    fix_verifier_bs = FixVerifier(connectivity_bs, case_id)
+    fix_manager_310 = FixManager(connectivity_sell_side, case_id)
+    verifier_310_sell_side = FixVerifier(connectivity_sell_side, case_id)
+    verifier_310_buy_side = FixVerifier(connectivity_buy_side, case_id)
 
 
     # Send NewOrderSingle
     new_order_single_params = {
         'Account': client,
-        'HandlInst': "2",
+        'HandlInst': 2,
         'Side': side,
         'OrderQty': qty,
-        'TimeInForce': "0",
-        'Price': "20",
-        'OrdType': "2",
+        'TimeInForce': 0,
+        'Price': price,
+        'OrdType': 2,
         'TransactTime': datetime.utcnow().isoformat(),
         'Instrument': instrument,
         'OrderCapacity': 'A',
         'Currency': 'EUR',
-        'TargetStrategy': "1011",
+        'TargetStrategy': 1011,
         'ClientAlgoPolicyID': 'QA_SORPING'
     }
     fix_message_new_order_single = FixMessage(new_order_single_params)
     fix_message_new_order_single.add_random_ClOrdID()
-    responce_new_order_single = fix_manager_qtwquod5.Send_NewOrderSingle_FixMessage(fix_message_new_order_single)
+    responce_new_order_single = fix_manager_310.Send_NewOrderSingle_FixMessage(fix_message_new_order_single)
 
-    #Check on ss
-    er_params_pending ={
-        'ExecType': "A",
+
+    #Check that FIXQUODSELL5 receive 35=D
+    nos_1 = dict(
+        fix_message_new_order_single.get_parameters(),
+        TransactTime='*',
+        ClOrdID=fix_message_new_order_single.get_parameter('ClOrdID'))
+
+    verifier_310_sell_side.CheckNewOrderSingle(nos_1, responce_new_order_single, direction='SECOND')
+
+    #Check that FIXQUODSELL5 sent 35=8 pending new
+    er_1 = dict(
+        ExecID='*',
+        OrderQty=qty,
+        LastQty=0,
+        TransactTime='*',
+        Side=side,
+        AvgPx=0,
+        Currency='EUR',
+        TimeInForce=0,
+        HandlInst =2,
+        LeavesQty=qty,
+        CumQty=0,
+        LastPx=0,
+        OrdType=2,
+        ClOrdID=fix_message_new_order_single.get_parameter('ClOrdID'),
+        OrderCapacity='A',
+        QtyType=0,
+        Price=price,
+        TargetStrategy=1011,
+        ExecType="A",
+        OrdStatus='A',
+        OrderID=responce_new_order_single.response_messages_list[0].fields['OrderID'].simple_value,
+        Instrument='*',
+        NoParty='*'
+    )
+
+    verifier_310_sell_side.CheckExecutionReport(er_1, responce_new_order_single)
+
+    #Check that FIXQUODSELL5 sent 35=8 new
+    er_2 = dict(
+        er_1,
+        ExecType="0",
+        OrdStatus='0',
+        SettlDate='*',
+        ExecRestatementReason='*',
+        SettlType='*',
+    )
+    verifier_310_sell_side.CheckExecutionReport(er_2, responce_new_order_single)
+
+
+    # Check that algo ping QDD1
+
+    nos_2 = {
+        'Side': side,
+        'Price': price,
+        'ExDestination': 'QDD1',
+        'Account': client,
+        'OrderQty': qty,
+        'OrdType': 2,
+        'ClOrdID': '*',
+        'OrderCapacity': 'A',
+        'TransactTime': '*',
+        'ChildOrderID': '*',
+        'SettlDate': '*',
+        'Currency': 'EUR',
+        'TimeInForce': 3,
+        'Instrument': '*',
+        'HandlInst': 1
+    }
+    verifier_310_buy_side.CheckNewOrderSingle(nos_2, responce_new_order_single,key_parameters = ['ExDestination', 'Side', 'Price'])
+
+    # Check that algo ping QDD2
+    nos_3 = dict(
+        nos_2,
+        ExDestination ='QDD2'
+    )
+    verifier_310_buy_side.CheckNewOrderSingle(nos_3, responce_new_order_single,key_parameters = ['ExDestination', 'Side', 'Price'])
+
+    # Check that sim cancel IOC order on QDD1
+    er_3 = {
+        'Side': side,
+        # 'Price': price,
+        # 'ExDestination': 'QDD1',
+        # 'Account': client,
+        'ExecID': '*',
+        'OrderQty': qty,
+        'AvgPx': 0,
+        'OrdStatus': 4,
+        'OrdType': 2,
+        'ClOrdID': '*',
+        # 'OrderCapacity': 'A',
+        'TransactTime': '*',
+        # 'ChildOrderID': '*',
+        'TimeInForce': 3,
+        'ExecType': 4,
+        'LeavesQty': 0,
+        'CumQty': 0,
+        'Text': '*',
+        'OrderID': '*'
+    }
+    verifier_310_buy_side.CheckExecutionReport(er_3, responce_new_order_single,key_parameters = ['Side', 'ExecType'], direction='SECOND')
+
+#TODO Add difference between cancel on QDD1 and QDD2
+
+    # Check that sim cancel IOC order on QDD2
+    er_3 = {
+        'Side': side,
+        # 'Price': price,
+        # 'ExDestination': 'QDD1',
+        # 'Account': client,
+        'ExecID': '*',
+        'OrderQty': qty,
+        'AvgPx': 0,
+        'OrdStatus': 4,
+        'OrdType': 2,
+        'ClOrdID': '*',
+        # 'OrderCapacity': 'A',
+        'TransactTime': '*',
+        # 'ChildOrderID': '*',
+        'TimeInForce': 3,
+        'ExecType': 4,
+        'LeavesQty': 0,
+        'CumQty': 0,
+        'Text': '*',
+        'OrderID': '*'
+    }
+    verifier_310_buy_side.CheckExecutionReport(er_3, responce_new_order_single, key_parameters=['Side', 'ExecType'],
+                                               direction='SECOND')
+
+
+    # Check that algo send dday order on QUODLIT1
+
+    nos_4 = dict(
+        nos_2,
+        ExDestination ='QDL1',
+        TimeInForce = 0
+    )
+    verifier_310_buy_side.CheckNewOrderSingle(nos_4, responce_new_order_single,key_parameters = ['ExDestination', 'Side', 'Price'])
+
+    er_4 = {
+        'ExDestination': 'QDL1',
+        'ExecType': 'A',
         'OrdStatus': 'A',
-        'OrderID': responce_new_order_single.response_messages_list[0].fields['OrderID'].simple_value,
+        'Account': client,
+        'CumQty': 0,
+        'ExecID': '*',
+        'OrderQty': qty,
+        'OrdType': 2,
+        'ClOrdID': '*',
+        'Text': '*',
+        'OrderID': '*',
+        'TransactTime': '*',
+        'Side': side,
+        'AvgPx': 0,
+        'Price': price,
+        'TimeInForce': 0,
+        'LeavesQty': 0
     }
-    fix_verifier_ss.CheckExecutionReport(er_params_pending, responce_new_order_single)
+    verifier_310_buy_side.CheckExecutionReport(er_4, responce_new_order_single,key_parameters = ['ExDestination', 'ExecType', 'OrdStatus'],direction='SECOND')
 
-    #Check on ss
-    er_params_new ={
-        'ExecType': "0",
-        'OrdStatus': '0',
-        'OrderID': responce_new_order_single.response_messages_list[0].fields['OrderID'].simple_value,
+    er_5 = dict(
+        er_4,
+        ExecType='A',
+        OrdStatus='A',
+    )
+    verifier_310_buy_side.CheckExecutionReport(er_5, responce_new_order_single,key_parameters = ['ExDestination', 'ExecType', 'OrdStatus'],direction='SECOND')
+
+    # Cancel order
+    cancel_parms = {
+        "ClOrdID": fix_message_new_order_single.get_ClOrdID(),
+        "Account": fix_message_new_order_single.get_parameter('Account'),
+        "Side": fix_message_new_order_single.get_parameter('Side'),
+        "TransactTime": datetime.utcnow().isoformat(),
+        "OrigClOrdID": fix_message_new_order_single.get_ClOrdID()
     }
-    fix_verifier_ss.CheckExecutionReport(er_params_new, responce_new_order_single)
-
-    #Check on bs
-    new_order_single_bs = {
-        'Side': new_order_single_params['Side'],
-        'Price': new_order_single_params['Price']
+    fix_cancel = FixMessage(cancel_parms)
+    responce_cancel = fix_manager_310.Send_OrderCancelRequest_FixMessage(fix_cancel)
+    cancel_er_params = {
+        'ClOrdID': fix_message_new_order_single.get_ClOrdID(),
+        'OrdStatus': '4',
+        'ExecID': '*',
+        'OrderQty': qty,
+        'LastQty': 0,
+        'OrderID':responce_new_order_single.response_messages_list[0].fields['OrderID'].simple_value,
+        'TransactTime': '*',
+        'Side': side,
+        'AvgPx': 0,
+        'SettlDate': '*',
+        'Currency': 'EUR',
+        'TimeInForce': 0,
+        'ExecType': 4,
+        'HandlInst': 2,
+        'LeavesQty': 0,
+        'NoParty': '*',
+        'CumQty': 0,
+        'LastPx': 0,
+        'OrdType': 2,
+        'OrderCapacity': 'A',
+        'QtyType': 0,
+        'ExecRestatementReason': 4,
+        'SettlType': 0,
+        'Price': price,
+        'TargetStrategy': 1011,
+        'Instrument': '*',
+        'OrigClOrdID': '*'
     }
-    fix_verifier_bs.CheckNewOrderSingle(new_order_single_bs, responce_new_order_single)
+    time.sleep(1)
+    verifier_310_sell_side.CheckExecutionReport(cancel_er_params, responce_cancel)
 
 
-
-
+    time.sleep(1)
 
     rule_destroyer(rule_list)
 
