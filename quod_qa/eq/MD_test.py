ad42775e8c8aa9657b8a4de534bd87fd0d38857e
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-05-11 17:48:31 +0300

    fix

--- quod_qa/eq/MD_test.py
+++ quod_qa/eq/MD_test.py
@@ -15,104 +15,58 @@ def execute(report_id):
     verifier = Stubs.verifier
     simulator = Stubs.simulator
 
-    case_name = "QAP-2409"
-
-    symbol = "3631"
-    # Create sub-report for case
-    case_id = bca.create_event(case_name, report_id)
-    MDRefID_1 = simulator.getMDRefIDForConnection(request=RequestMDRefID(
-        symbol=symbol,
-        connection_id=ConnectionID(session_alias="fix-fh-eq-trqx")
+    MDRefID = Stubs.simulator.getMDRefIDForConnection(request=RequestMDRefID(
+        symbol="734",
+        connection_id=ConnectionID(session_alias="fix-fh-310-columbia")
     )).MDRefID
-
-    # mdir_params_trade = {
-    #     'MDReqID': MDRefID_1,
-    #     'NoMDEntries': [
-    #
-    #         {
-    #             'MDUpdateAction': '0',
-    #             'MDEntryType': '2',
-    #             'MDEntryPx': '1.2',
-    #             'MDEntrySize': '10000',
-    #             'MDEntryDate': datetime.utcnow().date().strftime("%Y%m%d"),
-    #             'MDEntryTime': datetime.utcnow().time().strftime("%H:%M:%S")
-    #         }
-    #     ]
-    # }
-
-    mdir_params_trade_2 = {
-        'MDReqID': MDRefID_1,
-        'NoMDEntriesIR': {
-            "NoMDEntriesIR_NoMDEntriesIRIDs ":
-                [
-                    {
-                        'MDUpdateAction': '0',
-                        'MDEntryType': '2',
-                        'MDEntryPx': '1.2',
-                        'MDEntrySize': '10000',
-                        'MDEntryDate': datetime.utcnow().date().strftime("%Y%m%d"),
-                        'MDEntryTime': datetime.utcnow().time().strftime("%H:%M:%S")
-                    }
-                ]
-        }
-    }
-    mdfr_params_1 = {
-        'MDReportID': "1",
-        'MDReqID': MDRefID_1,
-        'Instrument': {
-            'Symbol': symbol
-        },
-        # 'LastUpdateTime': "",
+    mdir_params_bid = {
+        'MDReqID': MDRefID,
         'NoMDEntries': [
             {
                 'MDEntryType': '0',
                 'MDEntryPx': '30',
-                'MDEntrySize': '650'
+                'MDEntrySize': '1000',
+                'MDEntryPositionNo': '1'
             },
             {
                 'MDEntryType': '1',
                 'MDEntryPx': '40',
-                'MDEntrySize': '600'
+                'MDEntrySize': '1000',
+                'MDEntryPositionNo': '1'
             }
         ]
     }
-    #
-    # act.sendMessage(
-    #     request=bca.convert_to_request(
-    #         'Send MarketDataSnapshotFullRefresh',
-    #         "fix-fh-eq-paris",
-    #         case_id,
-    #         bca.message_to_grpc('MarketDataSnapshotFullRefresh', mdfr_params_1, "fix-fh-eq-paris")
-    #     )
-    # )
-    #
-    act.sendMessage(
-        request=bca.convert_to_request(
-            'Send MarketDataIncrementalRefresh',
-            "fix-fh-eq-trqx",
-            case_id,
-            bca.message_to_grpc('MarketDataIncrementalRefresh', mdir_params_trade_2, "fix-fh-eq-trqx")
-        )
-    )
-
-
-    mdir_params_trade = Message()
-    mdir_params_trade.metadata.message_type = "MarketDataSnapshotFullRefresh"
-    mdir_params_trade.metadata.id.connection_id.session_alias = "fix-fh-eq-trqx"
-    mdir_params_trade.fields['MDReqID'].simple_value = MDRefID
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDEntryType'].simple_value = "0"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDEntryPx'].simple_value = "30"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDEntrySize'].simple_value = "5000"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDUpdateAction'].simple_value = "0"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDEntryType'].simple_value = "1"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDEntryPx'].simple_value = "40"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDEntrySize'].simple_value = "5000"
-    mdir_params_trade.fields['NoMDEntries'].message_value.fields['NoMDEntries'].list_value.values.add().message_value.fields['MDUpdateAction'].simple_value = "0"
-
+    Stubs.fix_act.sendMessage(request=convert_to_request(
+        'Send MarketDataSnapshotFullRefresh',
+        "fix-fh-310-columbia",
+        report_id,
+        message_to_grpc('MarketDataSnapshotFullRefresh', mdir_params_bid, "fix-fh-310-columbia")
+    ))
+    MDRefID = Stubs.simulator.getMDRefIDForConnection(request=RequestMDRefID(
+        symbol="3416",
+        connection_id=ConnectionID(session_alias="fix-fh-310-columbia")
+    )).MDRefID
+    mdir_params_bid = {
+        'MDReqID': MDRefID,
+        'NoMDEntries': [
+            {
+                'MDEntryType': '0',
+                'MDEntryPx': '30',
+                'MDEntrySize': '1000',
+                'MDEntryPositionNo': '1'
+            },
+            {
+                'MDEntryType': '1',
+                'MDEntryPx': '40',
+                'MDEntrySize': '1000',
+                'MDEntryPositionNo': '1'
+            }
+        ]
+    }
 
     Stubs.fix_act.sendMessage(request=convert_to_request(
         'Send MarketDataSnapshotFullRefresh',
-        connectivity_fh,
-        case_id,
-        message_to_grpc('MarketDataSnapshotFullRefresh', mdir_params_bid, connectivity_fh)
+        "fix-fh-310-columbia",
+        report_id,
+        message_to_grpc('MarketDataSnapshotFullRefresh', mdir_params_bid, "fix-fh-310-columbia")
     ))
