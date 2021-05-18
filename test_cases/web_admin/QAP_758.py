51d87e862981e396f42f1f5464e602f3d233178f
denis.shepelev <denis.shepelev@exactprosystems.com>
2021-05-18 14:16:22 +0300

    updated QAP-758

--- test_cases/web_admin/QAP_758.py
+++ test_cases/web_admin/QAP_758.py
@@ -18,16 +18,18 @@ class TestCase:
     def get_precondition(self):
         data = 'TEST'
 
+        # Navigate to SubVenues tab
         reference_data_tab = self.driver.find_element_by_xpath('//*[text()="Reference Data"]')
         reference_data_tab.click()
-
         sub_venues_tab = reference_data_tab.find_element_by_xpath('//*[text()="SubVenues"]')
         sub_venues_tab.click()
 
+        # Press New button
         new_button = self.wait.until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="actions-header"]//*[text()="New"]')))
         new_button.click()
 
+        # Fill required fields
         name_input = self.wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Name"]')))
         name_input.click()
         name_input.send_keys(data)
@@ -35,26 +37,30 @@ class TestCase:
         venue_input.click()
         venue_input.send_keys(data, Keys.ARROW_DOWN, Keys.ENTER)
 
+        # Go to Summary tab
         summary_tab = self.driver.find_element_by_xpath('//*[@id="header-wizard-tab-3"]')
         summary_tab.click()
 
+        # Submit
         submit_button = self.driver.find_element_by_xpath('//*[text()="Submit"]')
         submit_button.click()
-
         self.wait.until(EC.presence_of_element_located((By.XPATH,
                                                         '//*[text()="Sub Venue created with success"]')))
 
     def delete_sub_venue(self):
+        # Find TEST row
         row = self.driver.find_element_by_xpath('//*[text()="TEST"]')
         row.click()
 
+        # Delete tow
         delete_btn = self.driver.find_element_by_xpath('//datatable-body-row[contains(@class, "active")]' +
                                                        '//*[@mattooltip="Delete"]')
         delete_btn.click()
+
+        # Confirm delete
         confirm_btn = self.wait.until(EC.presence_of_element_located((By.XPATH,
                                                                       '//*[text()="OK"]')))
         confirm_btn.click()
-
         self.wait.until(EC.presence_of_element_located((By.XPATH,
                                                         '//*[text()="SubVenue has been deleted with success"]')))
 
