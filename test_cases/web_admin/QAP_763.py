62eeea72d841fbfe18dcd9bee85bc2e73579e4b3
denis.shepelev <denis.shepelev@exactprosystems.com>
2021-05-18 14:30:15 +0300

    updated QAP-763

--- test_cases/web_admin/QAP_763.py
+++ test_cases/web_admin/QAP_763.py
@@ -5,8 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from custom.verifier import Verifier
-from web_admin_modules.web_wrapper import call, login, logout, check_exists_by_xpath, check_is_clickable
+from web_admin_modules.web_wrapper import call, login, logout
 
 
 class TestCase:
