34752638c17f2a49e008ddbcc2b26a44d221f254
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-05-21 15:17:42 +0300

    add_OrderCancelRequest_ExecutionReport

--- rule_management.py
+++ rule_management.py
@@ -1,7 +1,8 @@
 from th2_grpc_sim_quod.sim_pb2 import TemplateQuodNOSRule, TemplateQuodOCRRRule, TemplateQuodOCRRule, \
     TemplateQuodRFQRule, TemplateQuodRFQTRADERule, TemplateQuodSingleExecRule, \
     TemplateNoPartyIDs, TemplateNewOrdSingleExecutionReportTrade, TemplateNewOrdSingleExecutionReportPendingAndNew, \
-    TemplateNewOrdSingleIOC, TemplateNewOrdSingleFOK, TemplateOrderCancelRequest, TemplateNewOrdSingleMarket
+    TemplateNewOrdSingleIOC, TemplateNewOrdSingleFOK, TemplateOrderCancelRequest, TemplateNewOrdSingleMarket, \
+    TemplateOrderCancelReplaceExecutionReport
 from th2_grpc_sim.sim_pb2 import RuleID
 from th2_grpc_common.common_pb2 import ConnectionID
 
@@ -179,6 +180,13 @@ class RuleManager:
                                                                      trade=trade,
                                                                      tradedQty=tradedQty,
                                                                      avgPrice=avgPrice
+                                            ))
+
+    @staticmethod
+    def add_OrderCancelRequest_ExecutionReport(session: str, trade: bool):
+        return Stubs.simulator.createOrderCancelReplaceExecutionReport(
+            request=TemplateOrderCancelReplaceExecutionReport(connection_id=ConnectionID(session_alias=session),
+                                                                     trade=trade
                                             ))
 
     # ------------------------
