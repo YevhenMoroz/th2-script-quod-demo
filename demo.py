2ac88c7b4766670814f0ae81a2ea9e71345a2e34
ikobyliatskyi <ilia.kobyliatskyi.contractor@quodfinancial.com>
2021-05-21 18:51:09 +0300

    Adding automated test cases: RIN_1447, RIN_1444, RIN_1441

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
-from quod_qa.fx import fix_demo, ui_tests
-from quod_qa.fx.fx_taker_rfq import QAP_636
-from rule_management import RuleManager
+# from quod_qa.fx import fix_demo, ui_tests
+# from rule_management import RuleManager
 from stubs import Stubs
-from test_cases import QAP_638
-from quod_qa.eq.DMA import QAP_2547
+from quod_qa.eq.DMA import RIN_1147
+# from quod_qa.eq.DMA import RIN_1144
+# from quod_qa.eq.DMA import RIN_1141
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -16,39 +16,16 @@ timeouts = False
 
 channels = dict()
 
+
 def test_run():
     # Generation id and time for test run
     report_id = bca.create_event(' i.kobyliatskyi  tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
 
     try:
-        QAP_2547.execute(report_id)
-        test_cases =  {
-                'case_id': bca.create_event_id(),
-                'TraderConnectivity': 'gtwquod5-fx',
-                'Account': 'MMCLIENT1',
-                'SenderCompID': 'QUODFX_UAT',
-                'TargetCompID': 'QUOD5',
-                }
-
-        # rm = RuleManager()
-        # # rm.remove_rules_by_id_range(5,150)
-        # # rm.add_RFQ('fix-fh-fx-rfq')
-        # # rm.add_TRFQ('fix-fh-fx-rfq')
-        # # rm.print_active_rules()
-        # # ui_tests.execute(report_i)
-        # start = datetime.now()
-        # print(f'start time = {start}')
-        #
-        # # fix_demo.execute(report_id,test_cases)
-        # ui_tests.execute(report_id)
-        # # QAP_1520.TestCase(report_id).execute()
-        # # QAP_636.execute(report_id)
-        # print("1 - done")
-        # print('duration time = ' + str(datetime.now() - start), str(report_id))inec
-
-
-
+        RIN_1147.execute(report_id)
+        # RIN_1144.execute(report_id)
+        # RIN_1141.execute(report_id)
     except Exception:
         logging.error("Error execution", exc_info=True)
 
@@ -57,4 +34,3 @@ if __name__ == '__main__':
     logging.basicConfig()
     test_run()
     Stubs.factory.close()
-
---
+++ quod_qa/eq/DMA/RIN_1147.py
@@ -0,0 +1,224 @@
+import logging
+
+from datetime import datetime
+
+from win_gui_modules.order_book_wrappers import OrdersDetails
+
+
+from custom.basic_custom_actions import create_event, timestamps
+from win_gui_modules.order_ticket_wrappers import NewOrderDetails
+
+from stubs import Stubs
+from win_gui_modules.order_book_wrappers import ExtractionDetail, ExtractionAction, OrderInfo
+from win_gui_modules.order_ticket import OrderTicketDetails
+from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe, call, get_opened_fe
+from win_gui_modules.wrappers import set_base, verification, verify_ent
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+timeouts = True
+
+
+def execute(report_id):
+    case_name = "RIN_1147"
+    seconds, nanos = timestamps()  # Store case start time
+    # region Declarations
+    act = Stubs.win_act_order_book
+    qty = ["400", "abc"]  # Setting values for all orders
+    price = ["0", "-10", "10", "abc"]
+    stop_price = ["20", "abc"]
+    client = "HAKKIM"
+    lookup = "ACC"
+    # endregion
+
+    # region Open FE
+    case_id = create_event(case_name, report_id)
+    session_id = set_session_id()
+    set_base(session_id, case_id)
+    base_request = get_base_request(session_id, case_id)
+    work_dir = Stubs.custom_config['qf_trading_fe_folder']
+    username = Stubs.custom_config['qf_trading_fe_user']
+    password = Stubs.custom_config['qf_trading_fe_password']
+
+    if not Stubs.frontend_is_open:
+        prepare_fe(case_id, session_id, work_dir, username, password)
+    else:
+        get_opened_fe(case_id, session_id)
+    # endregion
+
+    # region Create 1st order via FE
+    order_ticket = OrderTicketDetails()
+    order_ticket.set_order_type("Limit")
+    order_ticket.set_quantity(qty[0])
+    order_ticket.set_limit(price[0])
+    order_ticket.set_client(client)
+    order_ticket.buy()
+
+    new_order_details = NewOrderDetails()
+    new_order_details.set_lookup_instr(lookup)
+    new_order_details.set_order_details(order_ticket)
+    new_order_details.set_default_params(base_request)
+    order_ticket_service = Stubs.win_act_order_ticket
+    common_act = Stubs.win_act
+
+    call(order_ticket_service.placeOrder, new_order_details.build())
+    extraction_id = "order.dma"
+    main_order_details = OrdersDetails()
+    main_order_details.set_default_params(base_request)
+    main_order_details.set_extraction_id(extraction_id)
+    main_order_details.set_filter(["Lookup", lookup])
+    # endregion
+
+    # region Check values in OrderBook
+    before_order_details_id = "before_order_details"
+
+    order_details = OrdersDetails()
+    order_details.set_default_params(base_request)
+    order_details.set_extraction_id(before_order_details_id)
+    order_status = ExtractionDetail("order_status", "Sts")
+    order_client = ExtractionDetail("order_client", "Client")
+    order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
+                                                                                            order_client,
+                                                                                            ])
+    order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
+
+    call(act.getOrdersDetails, order_details.request())
+    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
+                                                 [verify_ent("Status", order_status.name, "Rejected"),
+                                                  verify_ent("Client", order_client.name, client),
+                                                  ]))
+    # endregion
+
+    # region Create 2nd order via FE
+    order_ticket = OrderTicketDetails()
+    order_ticket.set_order_type("Limit")
+    order_ticket.set_quantity(qty[0])
+    order_ticket.set_limit(price[1])
+    order_ticket.set_client(client)
+    order_ticket.buy()
+
+    new_order_details = NewOrderDetails()
+    new_order_details.set_lookup_instr(lookup)
+    new_order_details.set_order_details(order_ticket)
+    new_order_details.set_default_params(base_request)
+    order_ticket_service = Stubs.win_act_order_ticket
+    common_act = Stubs.win_act
+
+    call(order_ticket_service.placeOrder, new_order_details.build())
+    extraction_id = "order.dma"
+    main_order_details = OrdersDetails()
+    main_order_details.set_default_params(base_request)
+    main_order_details.set_extraction_id(extraction_id)
+    main_order_details.set_filter(["Lookup", lookup])
+    # endregion
+
+    # region Check values in OrderBook
+    before_order_details_id = "before_order_details"
+
+    order_details = OrdersDetails()
+    order_details.set_default_params(base_request)
+    order_details.set_extraction_id(before_order_details_id)
+    order_status = ExtractionDetail("order_status", "Sts")
+    order_client = ExtractionDetail("order_client", "Client")
+    order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
+                                                                                            order_client,
+                                                                                            ])
+    order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
+
+    call(act.getOrdersDetails, order_details.request())
+    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
+                                                 [verify_ent("Status", order_status.name, "Rejected"),
+                                                  verify_ent("Client", order_client.name, client),
+                                                  ]))
+    # endregion
+
+    # region Create 3rd order via FE
+    order_ticket = OrderTicketDetails()
+    order_ticket.set_order_type("Limit")
+    order_ticket.set_quantity(qty[0])
+    order_ticket.set_limit(price[2])
+    order_ticket.set_stop_price(stop_price[0])
+    order_ticket.set_client(client)
+    order_ticket.buy()
+
+    new_order_details = NewOrderDetails()
+    new_order_details.set_lookup_instr(lookup)
+    new_order_details.set_order_details(order_ticket)
+    new_order_details.set_default_params(base_request)
+    order_ticket_service = Stubs.win_act_order_ticket
+    common_act = Stubs.win_act
+
+    call(order_ticket_service.placeOrder, new_order_details.build())
+    extraction_id = "order.dma"
+    main_order_details = OrdersDetails()
+    main_order_details.set_default_params(base_request)
+    main_order_details.set_extraction_id(extraction_id)
+    main_order_details.set_filter(["Lookup", lookup])
+    # endregion
+
+    # region Check values in OrderBook
+    before_order_details_id = "before_order_details"
+
+    order_details = OrdersDetails()
+    order_details.set_default_params(base_request)
+    order_details.set_extraction_id(before_order_details_id)
+    order_status = ExtractionDetail("order_status", "Sts")
+    order_client = ExtractionDetail("order_client", "Client")
+    order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
+                                                                                            order_client,
+                                                                                            ])
+    order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
+
+    call(act.getOrdersDetails, order_details.request())
+    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
+                                                 [verify_ent("Status", order_status.name, "Rejected"),
+                                                  verify_ent("Client", order_client.name, client),
+                                                  ]))
+    # endregion
+
+    # region Create 4th order via FE
+    order_ticket = OrderTicketDetails()
+    order_ticket.set_order_type("Limit")
+    order_ticket.set_quantity(qty[1])
+    order_ticket.set_limit(price[3])
+    order_ticket.set_stop_price(stop_price[1])
+    order_ticket.set_client(client)
+    order_ticket.buy()
+
+    new_order_details = NewOrderDetails()
+    new_order_details.set_lookup_instr(lookup)
+    new_order_details.set_order_details(order_ticket)
+    new_order_details.set_default_params(base_request)
+    order_ticket_service = Stubs.win_act_order_ticket
+    common_act = Stubs.win_act
+
+    call(order_ticket_service.placeOrder, new_order_details.build())
+    extraction_id = "order.dma"
+    main_order_details = OrdersDetails()
+    main_order_details.set_default_params(base_request)
+    main_order_details.set_extraction_id(extraction_id)
+    main_order_details.set_filter(["Lookup", lookup])
+    # endregion
+
+    # region Check values in OrderBook
+    before_order_details_id = "before_order_details"
+
+    order_details = OrdersDetails()
+    order_details.set_default_params(base_request)
+    order_details.set_extraction_id(before_order_details_id)
+    order_status = ExtractionDetail("order_status", "Sts")
+    order_client = ExtractionDetail("order_client", "Client")
+    order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
+                                                                                            order_client,
+                                                                                            ])
+    order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
+
+    call(act.getOrdersDetails, order_details.request())
+    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
+                                                 [verify_ent("Status", order_status.name, "Rejected"),
+                                                  verify_ent("Client", order_client.name, client),
+                                                  ]))
+    # endregion
+
+    logger.info(f"Case {case_name} was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
