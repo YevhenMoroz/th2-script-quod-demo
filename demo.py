07744f59217a96384f4106332019664565e3e74a
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-05-24 13:28:49 +0300

    QAP_1951

--- demo.py
+++ demo.py
@@ -2,8 +2,7 @@ import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
 from quod_qa.eq.Algo_Multilisted import  QAP_2982, QAP_1986, QAP_1988, QAP_1965, QAP_1985, QAP_1979, QAP_1977, QAP_1998, QAP_1974, QAP_1968, QAP_1969, QAP_1976, QAP_1975, QAP_1961, QAP_1960, QAP_1980, QAP_1959, QAP_1810, QAP_1952, QAP_1997, QAP_1996, QAP_1995, QAP_1992, QAP_2857, QAP_3019, QAP_3021, QAP_3022, QAP_3025, QAP_3027,QAP_1951, QAP_1990, QAP_3028
-from quod_qa.eq.Algo_TWAP import QAP_3124, QAP_2706, QAP_3122
-from quod_qa.eq.Algo_TWAP import QAP_3123, QAP_2706, QAP_3122, QAP_3124
+from quod_qa.eq.Algo_TWAP import QAP_3032, QAP_2955, QAP_3123, QAP_2706, QAP_3122, QAP_3124
 from rule_management import RuleManager
 from stubs import Stubs
 
@@ -20,7 +19,7 @@ def test_run():
     report_id = bca.create_event('FiLL tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     try:
-        QAP_3123.execute(report_id)
+        QAP_1951.execute(report_id)
     except Exception:
         logging.error("Error execution",exc_info=True)
 
