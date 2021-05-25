c23a28281b4438371a38ca5cf91a5f4887af9ce4
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-05-25 13:42:15 +0300

    add checkExecutionReport Sequence

--- quod_qa/wrapper/fix_verifier.py
+++ quod_qa/wrapper/fix_verifier.py
@@ -1,6 +1,9 @@
+from th2_grpc_check1.check1_pb2 import PreFilter
+
 from custom import basic_custom_actions as bca
 from stubs import Stubs
-from th2_grpc_common.common_pb2 import Direction
+from th2_grpc_common.common_pb2 import Direction, ValueFilter, MessageFilter, FilterOperation
+
 
 class FixVerifier:
 
@@ -17,6 +20,38 @@ class FixVerifier:
             bca.create_check_rule(
                 message_name,
                 bca.filter_to_grpc("ExecutionReport", parameters, key_parameters),
+                response.checkpoint_id,
+                self.TraderConnectivity,
+                case,
+                Direction.Value(direction)
+            )
+        )
+
+    def CheckExecutionReportSequence(self, parameters, response, key_parameters = ['ClOrdID', 'OrdStatus'], message_name='Check ExecutionReport', direction='FIRST', case = None):
+        if case == None:
+            case = self.case_id
+
+        pre_filter = PreFilter(
+            fields={
+                'header': ValueFilter(
+                    message_filter=MessageFilter(
+                        fields={
+                            'MsgType': ValueFilter(simple_filter='0', operation=FilterOperation.NOT_EQUAL)
+                        }
+                    )
+                )
+            }
+        )
+
+        message_sequence = list()
+        for param in parameters:
+            message_sequence.append(bca.filter_to_grpc("ExecutionReport", param, key_parameters))
+
+        self.verifier.submitCheckSequenceRule(
+            bca.create_check_sequence_rule(
+                message_name,
+                pre_filter,
+                message_sequence,
                 response.checkpoint_id,
                 self.TraderConnectivity,
                 case,
