dc30c63f49c9cb1caedf6bc0a0d9c86f3343a786
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2021-05-25 16:23:22 +0300

    Change QAP-3589

--- quod_qa/eq/DMA/QAP_2550.py
+++ quod_qa/eq/DMA/QAP_2550.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from quod_qa.wrapper import eq_wrappers
 from quod_qa.wrapper.fix_verifier import FixVerifier
 from custom.basic_custom_actions import create_event, timestamps
+from stubs import Stubs
 from win_gui_modules.utils import set_session_id, get_base_request
 from win_gui_modules.wrappers import set_base
 
@@ -23,7 +24,11 @@ def execute(report_id):
     base_request = get_base_request(session_id, case_id)
     # endregion
     # region create order with account
-    eq_wrappers.create_order(base_request, qty, client, 'XPAR', 'Limit', 'Day', True, 'CLIENTYMOROZ', '50', False,
+    work_dir = Stubs.custom_config['qf_trading_fe_folder']
+    username = Stubs.custom_config['qf_trading_fe_user']
+    password = Stubs.custom_config['qf_trading_fe_password']
+    eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
+    eq_wrappers.create_order(base_request, qty, client, 'XPAR', 'Limit', 'Day', False, 'CLIENTYMOROZ', '50', False,
                              True, False)
     # endregion
 
@@ -31,7 +36,7 @@ def execute(report_id):
     eq_wrappers.verify_value(base_request, case_id, 'Account ID', 'TestAccount')
     # endregion
     # create order with washbook
-    eq_wrappers.create_order(base_request, qty, client, 'XPAR', 'Limit', 'Day', True, 'CLIENTYMOROZ', '50', True,
+    eq_wrappers.create_order(base_request, qty, client, 'XPAR', 'Limit', 'Day', False, 'CLIENTYMOROZ', '50', True,
                              False, False)
     # region verify value
     eq_wrappers.verify_value(base_request, case_id, 'Wash Book', 'CareWB')
