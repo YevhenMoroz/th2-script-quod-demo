d002801300b89321867c67bcb16d862ccdcfa278
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-05-26 16:29:36 +0300

    update demo.py

--- demo.py
+++ demo.py
@@ -6,9 +6,6 @@ from quod_qa.eq.Algo_TWAP import QAP_3124, QAP_2706, QAP_3122
 from quod_qa.eq.Algo_TWAP import QAP_3123, QAP_2706, QAP_3122, QAP_3124
 from rule_management import RuleManager
 from stubs import Stubs
-from MD_SOR import md
-from test_cases import QAP_638
-from quod_qa.eq.Care import QAP_4015
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -23,30 +20,10 @@ def test_run():
     report_id = bca.create_event('FiLL tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     try:
-        QAP_4015.execute(report_id)
-        test_cases =  {
-                'case_id': bca.create_event_id(),
-                'TraderConnectivity': 'gtwquod5-fx',
-                'Account': 'MMCLIENT1',
-                'SenderCompID': 'QUODFX_UAT',
-                'TargetCompID': 'QUOD5',
-                }
 
-        # rm = RuleManager()
-        # # rm.remove_rules_by_id_range(5,150)
-        # # rm.add_RFQ('fix-fh-fx-rfq')
-        # # rm.add_TRFQ('fix-fh-fx-rfq')
-        # # rm.print_active_rules()
-        # # ui_tests.execute(report_i)
-        # start = datetime.now()
-        # print(f'start time = {start}')
-        #
-        # # fix_demo.execute(report_id,test_cases)
-        # ui_tests.execute(report_id)
-        # # QAP_1520.TestCase(report_id).execute()
-        # # QAP_636.execute(report_id)
-        # print("1 - done")
-        # print('duration time = ' + str(datetime.now() - start), str(report_id))inec
+
+        QAP_3122.execute(report_id)
+
 
     except Exception:
         logging.error("Error execution",exc_info=True)
