182bd50058c4b5b01af36548ed099e9b1277cb2c
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-05-27 10:05:50 +0300

    QAP_1988

--- quod_qa/eq/Algo_Multilisted/QAP_1988.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1988.py
@@ -333,6 +333,7 @@ def execute(report_id):
 
 
         time.sleep(1)
-        rule_destroyer(rule_list)
     except:
         logging.error("Error execution",exc_info=True)
+    finally:
+        rule_destroyer(rule_list)
