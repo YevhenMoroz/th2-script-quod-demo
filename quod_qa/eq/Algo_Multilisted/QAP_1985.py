f557b5cac9bc422ebb67d72e9efee6105e8889c8
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-05-31 14:56:45 +0300

    QAP_1985

--- quod_qa/eq/Algo_Multilisted/QAP_1985.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1985.py
@@ -205,7 +205,6 @@ def execute(report_id):
             OrdStatus='0',
             SettlDate='*',
             ExecRestatementReason='*',
-            SettlType='*'
         )
         fix_verifier_ss.CheckExecutionReport(er_2, responce_new_order_single, case=case_id_1, message_name='FIXQUODSELL5 sent 35=8 New', key_parameters=['ClOrdID', 'OrdStatus', 'ExecType'])
         
@@ -296,7 +295,7 @@ def execute(report_id):
             'TimeInForce': new_order_single_params['TimeInForce'],
             'ExecType': "A",
             'ExDestination': ex_destination_1,
-            'LeavesQty': '0'
+            'LeavesQty': qty
         }
 
         fix_verifier_bs.CheckExecutionReport(er_3, responce_new_order_single, direction='SECOND', case=case_id_3, message_name='FIXQUODSELL5 sent 35=8 Pending New', key_parameters=['ExecType', 'OrdStatus'])
@@ -380,7 +379,6 @@ def execute(report_id):
             'OrderCapacity': new_order_single_params['OrderCapacity'],
             'QtyType': '0',
             'ExecRestatementReason': '*',
-            'SettlType': '*',
             'StopPx': stop_price,
             'Price': price,
             'TargetStrategy': new_order_single_params['TargetStrategy'],
