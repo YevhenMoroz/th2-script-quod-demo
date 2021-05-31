f76a77718a2cd0b9c1949b80c4a86024b8de1d88
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-05-31 15:51:08 +0300

    QAP_1995

--- quod_qa/eq/Algo_Multilisted/QAP_1995.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1995.py
@@ -199,7 +199,6 @@ def execute(report_id):
             ExecType="0",
             OrdStatus='0',
             ExecRestatementReason='*',
-            SettlType='*'
         )
         fix_verifier_ss.CheckExecutionReport(er_2, responce_new_order_single, case=case_id_1, message_name='FIXQUODSELL5 sent 35=8 New', key_parameters=['ClOrdID', 'OrdStatus', 'ExecType'])
         #endregion
@@ -311,7 +310,6 @@ def execute(report_id):
             'OrderCapacity': new_order_single_params['OrderCapacity'],
             'QtyType': '0',
             'ExecRestatementReason': '*',
-            'SettlType': '*',
             'TargetStrategy': new_order_single_params['TargetStrategy'],
             'Instrument': instrument,
         }
