1796f93dc7966b98db24ebb964aa31abb179d5ed
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-06-01 09:41:34 +0300

    QAP_1961

--- quod_qa/eq/Algo_Multilisted/QAP_1961.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1961.py
@@ -201,7 +201,6 @@ def execute(report_id):
             OrdStatus='0',
             SettlDate='*',
             ExecRestatementReason='*',
-            SettlType='*',
             ExpireDate= expire_date,
         )
         fix_verifier_ss.CheckExecutionReport(er_2, responce_new_order_single, case=case_id_1, message_name='FIXQUODSELL5 sent 35=8 New', key_parameters=['ClOrdID', 'OrdStatus', 'ExecType'])
@@ -237,6 +236,7 @@ def execute(report_id):
             'CumQty': '0',
             'ExecID': '*',
             'OrderQty': qty,
+            'ExpireDate': expire_date,
             'Text': text_pn,
             'OrdType': '2',
             'ClOrdID': '*',
@@ -249,7 +249,7 @@ def execute(report_id):
             'TimeInForce': new_order_single_params['TimeInForce'],
             'ExecType': "A",
             'ExDestination': ex_destination_1,
-            'LeavesQty': '0'
+            'LeavesQty': qty
         }
 
         fix_verifier_bs.CheckExecutionReport(er_3, responce_new_order_single, direction='SECOND', case=case_id_2, message_name='FIXQUODSELL5 sent 35=8 Pending New', key_parameters=['ExecType', 'OrdStatus'])
@@ -342,7 +342,6 @@ def execute(report_id):
             'OrderCapacity': new_order_single_params['OrderCapacity'],
             'QtyType': '0',
             'ExecRestatementReason': '*',
-            'SettlType': '*',
             'Price': price,
             'TargetStrategy': new_order_single_params['TargetStrategy'],
             'Instrument': instrument,
@@ -422,7 +421,6 @@ def execute(report_id):
             'OrderCapacity': new_order_single_params['OrderCapacity'],
             'QtyType': '0',
             'ExecRestatementReason': '*',
-            'SettlType': '*',
             'Price': price,
             'TargetStrategy': new_order_single_params['TargetStrategy'],
             'Instrument': instrument,
