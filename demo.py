addd50e51df5b570db3b9ea256a4f4c00b72a7ae
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-06-02 14:37:32 +0300

    demo

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
-from quod_qa.eq.Algo_Multilisted import  QAP_1953, QAP_3021, QAP_2982, QAP_1986, QAP_1988, QAP_1965, QAP_1985, QAP_1979, QAP_1977, QAP_1998, QAP_1974, QAP_1968, QAP_1969, QAP_1976, QAP_1975, QAP_1961, QAP_1960, QAP_1980, QAP_1959, QAP_1810, QAP_1952, QAP_1997, QAP_1996, QAP_1995, QAP_1992, QAP_2857, QAP_3019, QAP_3021, QAP_3022, QAP_3025, QAP_3027,QAP_1951, QAP_1990, QAP_3028
-from quod_qa.eq.Algo_TWAP import QAP_1318, QAP_1319, QAP_3032, QAP_2955, QAP_3123, QAP_2706, QAP_3122, QAP_3124
+from quod_qa.eq.Algo_Multilisted import  QAP_1967, QAP_1966, QAP_1963, QAP_1962, QAP_1958, QAP_1957, QAP_1954, QAP_1983, QAP_1984, QAP_2068, QAP_1953, QAP_3021, QAP_2982, QAP_1986, QAP_1988, QAP_1965, QAP_1985, QAP_1979, QAP_1977, QAP_1998, QAP_1974, QAP_1968, QAP_1969, QAP_1976, QAP_1975, QAP_1961, QAP_1960, QAP_1980, QAP_1959, QAP_1810, QAP_1952, QAP_1997, QAP_1996, QAP_1995, QAP_1992, QAP_2857, QAP_3019, QAP_3021, QAP_3022, QAP_3025, QAP_3027,QAP_1951, QAP_1990, QAP_3028
+from quod_qa.eq.Algo_TWAP import QAP_2478, QAP_3532, QAP_2977, QAP_1318, QAP_1319, QAP_3032, QAP_2955, QAP_3123, QAP_2706, QAP_3122, QAP_3124
+from quod_qa.eq.Algo_PercentageVolume import QAP_2583, QAP_3062, QAP_3530
 from quod_qa.eq.Care import QAP_1013
 from rule_management import RuleManager
 from stubs import Stubs
@@ -20,7 +21,7 @@ def test_run():
     report_id = bca.create_event('FiLL tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     try:
-        QAP_1013.execute(report_id)
+        QAP_2478.execute(report_id)
     except Exception:
         logging.error("Error execution",exc_info=True)
 
