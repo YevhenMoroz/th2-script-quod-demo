0b31be8ebd410a451d4f52b9606c13a13c23a127
nikita.rogozhin <nikita.rogozhin@exactprosystems.com>
2021-06-08 18:44:02 +0300

    QAP_2505.py updated

--- test_cases/QAP_2505.py
+++ test_cases/QAP_2505.py
@@ -48,6 +48,7 @@ class TestCase:
         self.venue = 'HSB'
         self.user = Stubs.custom_config['qf_trading_fe_user_303']
         self.quote_id = None
+        self.price = None
 
         # Case rules
         self.rule_manager = RuleManager()
@@ -71,6 +72,8 @@ class TestCase:
             'SettlType': '0'
         }
 
+        self.cond_params = {}
+
     # Add case rules method
     def add_rules(self):
         self.RFQ = self.rule_manager.add_RFQ('fix-fh-fx-rfq')
@@ -89,35 +92,13 @@ class TestCase:
         self.rule_manager.remove_rule(self.TRFQ)
         self.rule_manager.print_active_rules()
 
-    # Set near date method
-    def set_near_date(self, date):
-        modify_request = ModifyRFQTileRequest(details=self.base_details)
-        modify_request.set_settlement_date(bca.get_t_plus_date(date))
-        call(self.ar_service.modifyRFQTile, modify_request.build())
-
-    # Set near date method
-    def modify_tile(self, details, qty, near, cur_from, cur_to):
-        modify_request = ModifyRFQTileRequest(details=details)
-        modify_request.set_quantity(qty)
-        modify_request.set_from_currency(cur_from)
-        modify_request.set_to_currency(cur_to)
-        modify_request.set_near_tenor(near)
-        call(self.ar_service.modifyRFQTile, modify_request.build())
-
-    # Send RFQ method
-    def send_rfq(self):
-        call(self.ar_service.sendRFQOrder, self.base_details.build())
-
-    # Cancel RFQ method
-    def cancel_rfq(self):
-        call(self.ar_service.cancelRFQ, self.base_details.build())
-
-    def send_rfq_via_FIX(self, qty):
+    def send_rfq_via_FIX(self, qty, side, name):
 
         rfq_params_terminated = {
             'QuoteReqID': bca.client_orderid(9),
             'NoRelatedSymbols': [{
                 **self.reusable_params,
+                'Side': side,
                 'Currency': 'EUR',
                 'QuoteType': '1',
                 'OrderQty': qty,
@@ -128,19 +109,33 @@ class TestCase:
         }
         rfq_term = self.act.placeQuoteFIX(
             bca.convert_to_request(
-                'Send RFQ_Terminated',
+                'Send RFQ for: ' + name,
                 self.case_params['TraderConnectivity'],
                 self.case_id,
                 bca.message_to_grpc('QuoteRequest', rfq_params_terminated, self.case_params['TraderConnectivity'])
             ))
 
+        if side == 1:
+            self.price = rfq_term.response_messages_list[0].fields['OfferPx'].simple_value
+            self.cond_params = {
+                'OfferSpotRate': '*',
+                'OfferPx': '*',
+                'OfferSize': rfq_params_terminated['NoRelatedSymbols'][0]['OrderQty']
+            }
+        elif side == 2:
+            self.price = rfq_term.response_messages_list[0].fields['BidPx'].simple_value
+            self.cond_params = {
+                'BidSpotRate': '*',
+                'BidPx': '*',
+                'BidSize': rfq_params_terminated['NoRelatedSymbols'][0]['OrderQty']
+            }
+
         quote_params = {
             **self.reusable_params,
+            **self.cond_params,
+            'Side': rfq_params_terminated['NoRelatedSymbols'][0]['Side'],
             'QuoteReqID': rfq_params_terminated['QuoteReqID'],
-            'OfferPx': 35.001,
-            'OfferSize': rfq_params_terminated['NoRelatedSymbols'][0]['OrderQty'],
             'QuoteID': '*',
-            'OfferSpotRate': '35.001',
             'ValidUntilTime': '*',
             'Currency': rfq_params_terminated['NoRelatedSymbols'][0]['Currency'],
             'QuoteType': 1
@@ -148,43 +143,45 @@ class TestCase:
 
         self.verifier.submitCheckRule(
             bca.create_check_rule(
-                'Quote Received',
+                'Quote Received for: ' + name,
                 bca.filter_to_grpc('Quote', quote_params, ['QuoteReqID']),
                 rfq_term.checkpoint_id,
                 self.case_params['TraderConnectivity'],
                 self.case_id
             )
         )
+
         # NewOrderSingle params for Quote Filled
         order_params = {
             **self.reusable_params,
+            'Side': side,
             'QuoteID': rfq_term.response_messages_list[0].fields['QuoteID'],
             'ClOrdID': bca.client_orderid(9),
             'OrdType': 'D',
             'TransactTime': (datetime.utcnow().isoformat()),
             'OrderQty': rfq_params_terminated['NoRelatedSymbols'][0]['OrderQty'],
-            'Price': rfq_term.response_messages_list[0].fields['OfferPx'].simple_value,
+            'Price': self.price,
             'TimeInForce': 4
         }
 
         # Send NewOrderSingle for Quote Filled
         send_order = self.act.placeOrderFIX(
             bca.convert_to_request(
-                "Send NewOrderSingle",
+                'Send NewOrderSingle for: ' + name,
                 self.case_params['TraderConnectivity'],
                 self.case_id,
                 bca.message_to_grpc('NewOrderSingle', order_params, self.case_params['TraderConnectivity'])
             ))
 
         er_pending_params = {
-            'Side': self.reusable_params['Side'],
+            'Side': order_params['Side'],
             'Account': self.reusable_params['Account'],
             'ClOrdID': order_params['ClOrdID'],
             'OrderQty': order_params['OrderQty'],
             'LeavesQty': order_params['OrderQty'],
             'TimeInForce': order_params['TimeInForce'],
             'OrdType': order_params['OrdType'],
-            'Price': rfq_term.response_messages_list[0].fields['OfferPx'].simple_value,
+            'Price': self.price,
             'OrderID': send_order.response_messages_list[0].fields['OrderID'].simple_value,
             'NoParty': [
                 {'PartyRole': 36, 'PartyID': 'gtwquod5', 'PartyIDSource': 'D'}
@@ -222,8 +219,8 @@ class TestCase:
         )
 
         er_filled_params = {
-            'Side': self.reusable_params['Side'],
-            'Account': self.reusable_params['Account'],
+            'Side': order_params['Side'],
+            'Account': 'MMCLIENT1_1',
             'SettlDate': self.reusable_params['SettlDate'],
             'TradeDate': datetime.utcnow().strftime('%Y%m%d'),
             'SettlType': self.reusable_params['SettlType'],
@@ -232,7 +229,7 @@ class TestCase:
             'LeavesQty': 0,
             'TimeInForce': order_params['TimeInForce'],
             'OrdType': order_params['OrdType'],
-            'Price': rfq_term.response_messages_list[0].fields['OfferPx'].simple_value,
+            'Price': self.price,
             'OrderID': send_order.response_messages_list[0].fields['OrderID'].simple_value,
             'NoParty': [
                 {'PartyRole': 36, 'PartyID': 'gtwquod5', 'PartyIDSource': 'D'}
@@ -248,16 +245,16 @@ class TestCase:
             'SettlCurrency': 'USD',
             'Currency': 'EUR',
             'HandlInst': 1,
-            'AvgPx': rfq_term.response_messages_list[0].fields['OfferPx'].simple_value,
+            'AvgPx': self.price,
             'QtyType': 0,
             'LastQty': order_params['OrderQty'],
             'CumQty': order_params['OrderQty'],
-            'LastPx': rfq_term.response_messages_list[0].fields['OfferPx'].simple_value,
+            'LastPx': self.price,
             'OrdStatus': 2,
             'ExecType': 'F',
             'ExecID': '*',
             'TransactTime': '*',
-            'LastSpotRate': 35.001,
+            'LastSpotRate': '*',
             'OrderCapacity': 'A',
             'ExDestination': 'XQFX',
             'GrossTradeAmt': '*'
@@ -274,6 +271,10 @@ class TestCase:
                 self.case_id
             )
         )
+
+    def con_qty(self, qty):
+        con_qty = f"{qty.replace(',', '')}"
+        return float(con_qty)
 
     # Check OrderBook method
     def check_ob(self):
@@ -294,7 +295,7 @@ class TestCase:
             OrderInfo.create(
                 action=ExtractionAction.create_extraction_action(), sub_order_details=lvl1_details))
         request = call(self.ob_act.getOrdersDetails, ob.request())
-        return request[sub_order_price.name]
+        return self.con_qty(request[sub_order_price.name])
         # print("ExecPrice: " + request[sub_order_price.name])
 
     def get_dealing_position(self):
@@ -303,7 +304,7 @@ class TestCase:
         quote_pos_details.set_default_params(self.base_request)
         quote_pos_details.set_extraction_id(extraction_id)
 
-        quote_pos = ExtractionPositionsFieldsDetails("qt_pos.position", "Quote Position")
+        quote_pos = ExtractionPositionsFieldsDetails("qt_pos.position", "Quote Position (EUR)")
 
         quote_pos_details.add_single_positions_info(
             PositionsInfo.create(
@@ -311,38 +312,59 @@ class TestCase:
 
         response = call(self.pos_service.getFxDealingPositionsDetails, quote_pos_details.request())
         # print(response[quote_pos.value])
-        con_qty = f"{response[quote_pos.value].replace(',','')}"
-        return float(con_qty)
+        return self.con_qty(response[quote_pos.value])
 
-    def compare_position(self, pre_position, position, post_positon):
+    def compare_position(self, pre_position, position, post_positon, name):
         cust_verifier = Verifier(self.case_id)
+        cust_verifier.set_event_name('Checking Quote Position for ' + name)
         post_pos = pre_position + position
-        cust_verifier.compare_values('Quote Position', post_pos, post_positon)
+        cust_verifier.compare_values('Quote Position', str(post_pos), str(post_positon))
         cust_verifier.verify()
 
     # Main method. Must call in demo.py by "QAP_682.TestCase(report_id).execute()" command
     def execute(self):
         try:
             qty_1 = 6000000
-            # Step 1
+            qty_2 = 2000000
+            qty_3 = 8000000
+            qty_4 = 3000000
             self.prepare_frontend()
-            # self.send_rfq_via_FIX(qty_1)
-            # self.check_ob()
+
+            # Step 1: RFQ + Order Buy Side Qty: 6M
+            name = '6M Buy Side'
             pre_position = self.get_dealing_position()
-            self.send_rfq_via_FIX(qty_1)
-            post_position = self.get_dealing_position()
-            print(pre_position, post_position)
-            # position = self.check_ob() * -abs(qty_1)
-            # self.compare_position(pre_position, position, post_position)
-            # self.send_rfq_via_FIX(qty_1)
-            # self.check_dealing_position(position)
-            # self.check_ob()
-            # self.modify_tile(self.base_request, 6000000, "Spot", "EUR", "USD")
-            # self.send_rfq()
-            # self.cancel_rfq()
+            self.send_rfq_via_FIX(qty_1, 1, name)
+            position_6M = self.get_dealing_position()
+            exec_price = self.check_ob()
+            position = exec_price * -abs(qty_1)
+            self.compare_position(pre_position, position, position_6M, name)
+
+            # Step 2: RFQ + Order Buy Side Qty: 2M
+            name = '2M Buy Side'
+            self.send_rfq_via_FIX(qty_2, 1, name)
+            position_2M = self.get_dealing_position()
+            exec_price = self.check_ob()
+            position = exec_price * -abs(qty_2)
+            self.compare_position(position_6M, position, position_2M, name)
+
+            # Step 3: RFQ + Order Sell Side Qty: 8M
+            name = '8M Sell Side'
+            self.send_rfq_via_FIX(qty_3, 2, name)
+            position_8M = self.get_dealing_position()
+            exec_price = self.check_ob()
+            position = exec_price * abs(qty_3)
+            self.compare_position(position_2M, position, position_8M, name)
+
+            # Step 4: RFQ + Order Sell Side Qty: 3M
+            name = '3M Sell Side'
+            self.send_rfq_via_FIX(qty_4, 2, name)
+            position_3M = self.get_dealing_position()
+            exec_price = self.check_ob()
+            position = exec_price * abs(qty_4)
+            self.compare_position(position_8M, position, position_3M, name)
+
         except Exception as e:
             logging.error('Error execution', exc_info=True)
-            # close_fe(self.case_id, self.session_id)
 
         close_fe(self.case_id, self.session_id)
 
