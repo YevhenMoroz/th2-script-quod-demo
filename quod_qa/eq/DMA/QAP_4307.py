5e0a653daca4f95235ff0ea9fedae080bdb29b00
ikobyliatskyi <ilia.kobyliatskyi.contractor@quodfinancial.com>
2021-06-16 17:14:22 +0300

    QAP_4307 added.

---
+++ quod_qa/eq/DMA/QAP_4307.py
@@ -0,0 +1,59 @@
+import logging
+
+from datetime import datetime
+
+from custom.basic_custom_actions import create_event, timestamps
+
+from stubs import Stubs
+
+from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe, get_opened_fe
+from win_gui_modules.wrappers import set_base
+
+from quod_qa.wrapper import eq_wrappers
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+timeouts = True
+
+
+def execute(report_id):
+    case_name = "QAP_4307"
+
+    seconds, nanos = timestamps()  # Store case start time
+
+    # region Declarations
+    lookup = "RELIANCE"  # Setting values for all orders
+    order_type = "Limit"
+    price = "10"
+    qty = "200"
+    tif = "Day"
+    client = "HAKKIM"
+    # endregion
+
+    # region Open FE
+    case_id = create_event(case_name, report_id)
+    session_id = set_session_id()
+    set_base(session_id, case_id)
+    base_request = get_base_request(session_id, case_id)
+    work_dir = Stubs.custom_config['qf_trading_fe_folder']
+    username = Stubs.custom_config['qf_trading_fe_user']
+    password = Stubs.custom_config['qf_trading_fe_password']
+
+    if not Stubs.frontend_is_open:
+        prepare_fe(case_id, session_id, work_dir, username, password)
+    else:
+        get_opened_fe(case_id, session_id)
+    # endregion
+
+    # region Create order via FE
+    eq_wrappers.create_order(base_request, qty, client, lookup, order_type, tif,
+                             False, None, price, False, False, None)
+    # endregion
+
+    # region Check values in OrderBook
+    eq_wrappers.verify_value(base_request, case_id, "Sts", "Held")
+    eq_wrappers.verify_value(base_request, case_id, "GatingRuleName", "QAP-4307(Gr_for_DMA)")
+    eq_wrappers.verify_value(base_request, case_id, "GatingRuleCondName", "DMATinyQty")
+    # endregion
+
+    logger.info(f"Case {case_name} was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
