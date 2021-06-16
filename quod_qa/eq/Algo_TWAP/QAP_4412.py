9cae18c8089ee294c9b4b60055ae3325361e5aa6
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-06-16 14:17:27 +0300

    add QAP_4412.py

---
+++ quod_qa/eq/Algo_TWAP/QAP_4412.py
@@ -0,0 +1,122 @@
+import os
+import logging
+import time
+from datetime import datetime, timedelta
+from copy import deepcopy
+from custom import basic_custom_actions as bca
+from th2_grpc_sim_quod.sim_pb2 import RequestMDRefID, TemplateQuodOCRRule, TemplateQuodOCRRRule, TemplateQuodNOSRule
+from th2_grpc_common.common_pb2 import ConnectionID, Direction
+from quod_qa.wrapper.fix_manager import FixManager
+from quod_qa.wrapper.fix_message import FixMessage
+from quod_qa.wrapper.fix_verifier import FixVerifier
+from rule_management import RuleManager
+from stubs import Stubs
+from custom.basic_custom_actions import message_to_grpc, convert_to_request
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+timeouts = True
+
+qty = 500
+text_pn = 'Pending New status'
+text_n = 'New status'
+text_ocrr = 'OCRRRule'
+text_c = 'order canceled'
+text_f = 'Fill'
+text_ret = 'reached end time'
+text_s = 'sim work'
+text_r = 'order replaced'
+side = 1
+price = 20
+tif_day = 0
+tif_ioc = 3
+ex_destination_1 = "XPAR"
+client = "CLIENT2"
+order_type = 2
+account = 'XPAR_CLIENT2'
+currency = 'EUR'
+s_par = '1015'
+
+now = datetime.today() - timedelta(hours=3)
+
+case_name = os.path.basename(__file__)
+connectivity_buy_side = "fix-bs-310-columbia"
+connectivity_sell_side = "fix-sell-side-316-ganymede"
+connectivity_fh = 'fix-fh-310-columbia'
+
+instrument = {
+    'Symbol': 'AT000000STR1_EUR',
+    'SecurityID': 'AT000000STR1',
+    'SecurityIDSource': '4',
+    'SecurityExchange': 'WBAH'
+}
+
+
+
+def execute(report_id):
+    try:
+        case_id = bca.create_event(os.path.basename(__file__), report_id)
+        # Send_MarkerData
+        fix_manager_310 = FixManager(connectivity_sell_side, case_id)
+        fix_verifier_ss = FixVerifier(connectivity_sell_side, case_id)
+        fix_verifier_bs = FixVerifier(connectivity_buy_side, case_id)
+
+        case_id_0 = bca.create_event("Send Market Data", case_id)
+
+
+        #region Send NewOrderSingle (35=D)
+        case_id_1 = bca.create_event("Create Algo Order", case_id)
+        new_order_single_params = {
+            'Account': client,
+            'HandlInst': 2,
+            'Side': side,
+            'OrderQty': qty,
+            'TimeInForce': tif_day,
+            'OrdType': order_type,
+            'TransactTime': datetime.utcnow().isoformat(),
+            'Instrument': instrument,
+            'OrderCapacity': 'A',
+            'Price': price,
+            'Currency': currency,
+            'TargetStrategy': 1005,
+            'ExDestination': 'CHIX',
+                    'NoStrategyParameters': [
+                {
+                    'StrategyParameterName': 'StartDate',
+                    'StrategyParameterType': '19',
+                    'StrategyParameterValue': now.strftime("%Y%m%d-%H:%M:%S")
+                },
+                {
+                    'StrategyParameterName': 'EndDate',
+                    'StrategyParameterType': '19',
+                    'StrategyParameterValue': (now + timedelta(minutes=3)).strftime("%Y%m%d-%H:%M:%S")
+                },
+                {
+                    'StrategyParameterName': 'MaxWouldShares',
+                    'StrategyParameterType': '6',
+                    'StrategyParameterValue': '350'
+                },
+                {
+                    'StrategyParameterName': 'MaxWouldPercentage',
+                    'StrategyParameterType': '6',
+                    'StrategyParameterValue': '20'
+                },
+                {
+                    'StrategyParameterName': 'WouldPriceReference',
+                    'StrategyParameterType': '14',
+                    'StrategyParameterValue': 'LTP'
+                },
+                {
+                    'StrategyParameterName': 'WouldPriceOffset',
+                    'StrategyParameterType': '1',
+                    'StrategyParameterValue': '-1'
+                }
+            ]
+        }
+        fix_message_new_order_single = FixMessage(new_order_single_params)
+        fix_message_new_order_single.add_random_ClOrdID()
+        responce_new_order_single = fix_manager_310.Send_NewOrderSingle_FixMessage(fix_message_new_order_single, case=case_id_1)
+
+    except:
+        logging.error("Error execution", exc_info=True)
+
