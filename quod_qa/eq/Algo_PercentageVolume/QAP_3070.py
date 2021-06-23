c2348a4f3e8b3e250965d4bb8ff4cb8a5899aea2
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-06-23 14:14:10 +0300

    QAP_3070

--- quod_qa/eq/Algo_PercentageVolume/QAP_3070.py
+++ quod_qa/eq/Algo_PercentageVolume/QAP_3070.py
@@ -379,7 +379,7 @@ def execute(report_id):
         time.sleep(3)
 
         #region Cancel Algo Order
-        case_id_7 = bca.create_event("Cansel Algo Order", case_id)
+        case_id_7 = bca.create_event("Cancel Algo Order", case_id)
 
         cancel_parms = {
         "ClOrdID": fix_message_new_order_single.get_ClOrdID(),
