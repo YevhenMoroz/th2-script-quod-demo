3431d69a8b0114b013677075a1cebf689478d200
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-06-23 14:10:02 +0300

    QAP_2955

--- quod_qa/eq/Algo_TWAP/QAP_2955.py
+++ quod_qa/eq/Algo_TWAP/QAP_2955.py
@@ -276,13 +276,12 @@ def execute(report_id):
             ExecType="0",
             OrdStatus='0',
             SettlDate='*',
-            ExecRestatementReason='*',
-            SettlType='*'
+            ExecRestatementReason='*'
         )
         fix_verifier_ss.CheckExecutionReport(er_2, responce_new_order_single, case=case_id_1, message_name='FIXQUODSELL5 sent 35=8 New', key_parameters=['ClOrdID', 'OrdStatus', 'ExecType'])
 
         #region Cancel Algo Order
-        case_id_2 = bca.create_event("Cansel Algo Order", case_id)
+        case_id_2 = bca.create_event("Cancel Algo Order", case_id)
 
         cancel_parms = {
         "ClOrdID": fix_message_new_order_single.get_ClOrdID(),
@@ -330,7 +329,6 @@ def execute(report_id):
         'OrderCapacity': new_order_single_params['OrderCapacity'],
         'QtyType': '0',
         'ExecRestatementReason': '*',
-        'SettlType': '*',
         'Price': price,
         'TargetStrategy': new_order_single_params['TargetStrategy'],
         'Instrument': instrument,
