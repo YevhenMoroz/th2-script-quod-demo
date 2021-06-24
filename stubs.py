bdabde58fc209f91609af46812b2d9095fe06cd7
ostronov <oleksandr.stronov>
2021-06-24 16:45:42 +0300

    Add order_book_fx service

--- stubs.py
+++ stubs.py
@@ -6,6 +6,7 @@ from th2_grpc_act_gui_quod.fx_dealing_positions_service import FxDealingPosition
 from th2_grpc_act_gui_quod.layout_panel_service import LayoutPanelServiceService
 from th2_grpc_act_gui_quod.middle_office_service import MiddleOfficeOperationsService
 from th2_grpc_act_gui_quod.order_book_service import OrderBookServiceService
+from th2_grpc_act_gui_quod.order_book_fx_service import OrderBookFXServiceService
 from th2_grpc_act_gui_quod.order_ticket_service import OrderTicketServiceService
 from th2_grpc_act_quod.act_fix_service import ActFixService
 from th2_grpc_check1.check1_service import Check1Service
@@ -36,6 +37,7 @@ class Stubs:
     core = factory.grpc_router.get_service(SimService)
     win_act = factory.grpc_router.get_service(ActUIWinService)
     win_act_order_book = factory.grpc_router.get_service(OrderBookServiceService)
+    win_act_order_book_fx = factory.grpc_router.get_service(OrderBookFXServiceService)
     win_act_order_ticket = factory.grpc_router.get_service(OrderTicketServiceService)
     win_act_order_ticket_fx = factory.grpc_router.get_service(OrderTicketFxServiceService)
     win_act_aggregated_rates_service = factory.grpc_router.get_service(AggregatedRatesOperationsService)
