8a3e12e5e996f0b25f6c1378d23f60bd8f91f0ba
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-06-29 10:36:58 +0300

    twap_regression

--- regression_cycle/algo_regression_cycle/twap_regression.py
+++ regression_cycle/algo_regression_cycle/twap_regression.py
@@ -15,22 +15,22 @@ channels = dict()
 def test_run(parent_id= None):
     report_id = bca.create_event('TWAP ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'), parent_id)
     try:
-        #FIX/FE
-        QAP_2864.execute(report_id)
-        QAP_2865.execute(report_id)
-        #end FIX/FE
-        QAP_2706.execute(report_id)
-        QAP_2478.execute(report_id)
-        QAP_2955.execute(report_id) 
-        QAP_2977.execute(report_id)
-        QAP_3032.execute(report_id)
-        QAP_3117.execute(report_id)
-        QAP_3119.execute(report_id)
-        QAP_3120.execute(report_id)
-        QAP_3121.execute(report_id)
-        QAP_3122.execute(report_id)
-        QAP_3123.execute(report_id)
-        QAP_3124.execute(report_id)
+        # #FIX/FE
+        # QAP_2864.execute(report_id)
+        # QAP_2865.execute(report_id)
+        # #end FIX/FE
+        # QAP_2706.execute(report_id)
+        # QAP_2478.execute(report_id)
+        # QAP_2955.execute(report_id) 
+        # QAP_2977.execute(report_id)
+        # QAP_3032.execute(report_id)
+        # QAP_3117.execute(report_id)
+        # QAP_3119.execute(report_id)
+        # QAP_3120.execute(report_id)
+        # QAP_3121.execute(report_id)
+        # QAP_3122.execute(report_id)
+        # QAP_3123.execute(report_id)
+        # QAP_3124.execute(report_id)
         QAP_3532.execute(report_id)
     except Exception:
         logging.error("Error execution", exc_info=True)
