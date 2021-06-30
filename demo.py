775f565e76ddc6e6479a87e2bd83a1715aea55a8
ostronov <oleksandr.stronov>
2021-06-30 15:42:43 +0300

    update demo

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
+from quod_qa.fx import ui_tests
 from quod_qa.fx.fx_mm_esp import QAP_1560, QAP_2825, QAP_2555, QAP_2038, QAP_1599, QAP_1518, QAP_2034, QAP_2075, \
     QAP_1601, QAP_2039, QAP_2035, QAP_1418, QAP_2556, QAP_2117, QAP_2855, QAP_3563, QAP_2069, QAP_2646, QAP_2587, \
     QAP_2796, QAP_1536, QAP_2037, QAP_2072, QAP_2523, QAP_3045
-from quod_qa.fx.fx_mm_rfq import QAP_1746
-from quod_qa.fx.fx_taker_esp import QAP_833, QAP_1115, QAP_110
+from quod_qa.fx.fx_mm_positions import QAP_2505, tests, QAP_2378, QAP_2491
+from quod_qa.fx.fx_taker_esp import QAP_833, QAP_1115, QAP_110, QAP_231, QAP_1591, QAP_492
 from quod_qa.fx.fx_taker_rfq import QAP_6, QAP_564, QAP_565, QAP_566, QAP_567, QAP_568, QAP_569, QAP_570, QAP_571, \
     QAP_573, QAP_574, QAP_576, QAP_577, QAP_578, QAP_579, QAP_580, QAP_581, QAP_582, QAP_584, QAP_585, QAP_587, QAP_589, \
     QAP_590, QAP_591, QAP_593, QAP_594, QAP_595, QAP_597, QAP_598, QAP_599, QAP_600, QAP_601, QAP_602, QAP_604, QAP_605, \
@@ -166,13 +167,24 @@ def test_run():
         # mm_esp_plus_fe(report_id, session_id)
         # endregion
 
-        QAP_1536.execute(report_id, session_id)
-
+        # QAP_1536.execute(report_id, session_id)
+        # QAP_579.execute(report_id, session_id)
+        # QAP_231.execute(report_id, session_id)
+        # QAP_492.execute(report_id, session_id)
+        # QAP_2505.execute(report_id, session_id)
+        # QAP_718.execute(report_id, session_id)
+        # QAP_2523.execute(report_id, session_id)
+        # tests.execute(report_id, session_id)
+        # QAP_1591.execute(report_id, session_id)
+        QAP_2378.execute(report_id, session_id)
+        # QAP_2491.execute(report_id, session_id)
+        # ui_tests.execute(report_id, session_id)
         print('duration time = ' + str(datetime.now() - start))
-        Stubs.win_act.unregister(session_id)
+
     except Exception:
         logging.error("Error execution", exc_info=True)
-
+    finally:
+        Stubs.win_act.unregister(session_id)
 
 if __name__ == '__main__':
     logging.basicConfig()
