03108e6a0479e6e47737673b989b9af363ba71e4
ostronov <oleksandr.stronov>
2021-06-30 15:34:25 +0300

    Update test for quod314@luna

--- quod_qa/fx/fx_mm_positions/QAP_2491.py
+++ quod_qa/fx/fx_mm_positions/QAP_2491.py
@@ -1,13 +1,19 @@
 import logging
 from pathlib import Path
+
+from th2_grpc_act_gui_quod.common_pb2 import BaseTileData
+
 from custom import basic_custom_actions as bca
 from custom.verifier import Verifier
 from stubs import Stubs
-from win_gui_modules.client_pricing_wrappers import ModifyRatesTileRequest, PlaceRatesTileOrderRequest
+from win_gui_modules.client_pricing_wrappers import ModifyRatesTileRequest, PlaceRatesTileOrderRequest, \
+    PlaceRateTileTableOrderRequest, RatesTileTableOrdSide
 
 from win_gui_modules.common_wrappers import BaseTileDetails
 from win_gui_modules.dealing_positions_wrappers import GetOrdersDetailsRequest, ExtractionPositionsFieldsDetails, \
     PositionsInfo, ExtractionPositionsAction
+from win_gui_modules.order_ticket import FXOrderDetails
+from win_gui_modules.order_ticket_wrappers import NewFxOrderDetails
 from win_gui_modules.utils import prepare_fe_2, get_base_request, call, get_opened_fe
 from win_gui_modules.wrappers import set_base
 
@@ -32,14 +38,19 @@ def place_order_buy(base_request, service, qty, slippage, client):
     call(service.placeRatesTileOrder, place_request.build())
 
 
-def place_order_sell(base_request, service, qty, slippage, client):
-    place_request = PlaceRatesTileOrderRequest(details=base_request)
-    place_request.set_quantity(qty)
-    place_request.set_slippage(slippage)
-    place_request.set_client(client)
-    place_request.set_order_price_pips("115")
-    place_request.sell()
-    call(service.placeRatesTileOrder, place_request.build())
+def open_order_ticket_sell(btd, service, row):
+    request = PlaceRateTileTableOrderRequest(btd, row, RatesTileTableOrdSide.SELL)
+    call(service.placeRateTileTableOrder, request.build())
+
+
+def place_order(base_request, service, qty, slippage, client):
+    order_ticket = FXOrderDetails()
+    order_ticket.set_qty(qty)
+    order_ticket.set_client(client)
+    order_ticket.set_slippage(slippage)
+    order_ticket.set_place()
+    new_order_details = NewFxOrderDetails(base_request, order_ticket)
+    call(service.placeFxOrder, new_order_details.build())
 
 
 def get_dealing_positions_details(del_act, base_request, symbol, account):
@@ -92,6 +103,7 @@ def check_avg_price_empty(case_id, avg_price):
 def execute(report_id, session_id):
     cp_service = Stubs.win_act_cp_service
     pos_service = Stubs.act_fx_dealing_positions
+    order_ticket_service = Stubs.win_act_order_ticket_fx
 
     case_name = Path(__file__).name[:-3]
     client_tier = "Argentum"
@@ -110,7 +122,7 @@ def execute(report_id, session_id):
     set_base(session_id, case_id)
     case_base_request = get_base_request(session_id, case_id)
     base_details = BaseTileDetails(base=case_base_request)
-
+    base_tile_data = BaseTileData(base=case_base_request)
     try:
         if not Stubs.frontend_is_open:
             prepare_fe_2(case_id, session_id)
@@ -132,12 +144,14 @@ def execute(report_id, session_id):
                               position_info["dealingpositions.position"],
                               position_info["dealingpositions.dailyAvgPx"])
         # Step 4
-        place_order_sell(base_details, cp_service, qty_8m, slippage, client)
+        open_order_ticket_sell(base_tile_data, cp_service, 3)
+        place_order(case_base_request, order_ticket_service, qty_8m, slippage, client)
         position_info_after_8m = get_dealing_positions_details(pos_service, case_base_request, symbol, client)
         # Step 5
         check_avg_price_empty(case_id, position_info_after_8m["dealingpositions.avgPx"])
         # Step 6
-        place_order_sell(base_details, cp_service, qty_3m, slippage, client)
+        open_order_ticket_sell(base_tile_data, cp_service, 2)
+        place_order(case_base_request, order_ticket_service, qty_3m, slippage, client)
         position_info_after_3m = get_dealing_positions_details(pos_service, case_base_request, symbol, client)
         check_daily_avg_price(case_id, position_info_after_3m["dealingpositions.dailyMTMQuotePos"],
                               position_info_after_3m["dealingpositions.position"],
