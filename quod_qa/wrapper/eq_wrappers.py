d525531e0735d72f0a13f4345d63927c7941bf9f
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-07-01 18:35:19 +0300

    add QAP_3310.py

--- quod_qa/wrapper/eq_wrappers.py
+++ quod_qa/wrapper/eq_wrappers.py
@@ -110,7 +110,7 @@ def create_order(base_request, qty, client, lookup, order_type, tif="Day", is_ca
         rule_manager.remove_rule(nos_rule)
 
 
-def create_order_via_fix(case_id, handl_inst, side, client, ord_type, qty, tif, price=None,alloc_account=None):
+def create_order_via_fix(case_id, handl_inst, side, client, ord_type, qty, tif, price=None, alloc_account=None):
     try:
         fix_manager = FixManager(sell_connectivity, case_id)
         fix_params = {
@@ -514,9 +514,7 @@ def book_order(request, client, agreed_price, net_gross_ind="Gross", give_up_bro
     if exchange_rate_calc is not None:
         settlement_details.set_exchange_rate_calc(exchange_rate_calc)
     if settlement_date is not None:
-        response = check_booking_toggle_manual(request)
-        if response['book.manualCheckboxState'] != 'checked':
-            settlement_details.toggle_settlement_date()
+        settlement_details.toggle_settlement_date()
         settlement_details.set_settlement_date(settlement_date)
     if pset is not None:
         settlement_details.set_pset(pset)
@@ -525,7 +523,9 @@ def book_order(request, client, agreed_price, net_gross_ind="Gross", give_up_bro
 
     commissions_details = modify_request.add_commissions_details()
     if comm_basis is not None:
-        commissions_details.toggle_manual()
+        response = check_booking_toggle_manual(request)
+        if response['book.manualCheckboxState'] != 'checked':
+            commissions_details.toggle_manual()
         commissions_details.add_commission(comm_basis, comm_rate)
     if remove_commission:
         commissions_details.remove_commissions()
@@ -593,7 +593,7 @@ def amend_block(request, agreed_price=None, net_gross_ind=None, give_up_broker=N
     if exchange_rate_calc is not None:
         settlement_details.set_exchange_rate_calc(exchange_rate_calc)
     if settlement_date is not None:
-        # settlement_details.toggle_settlement_date()
+        settlement_details.toggle_settlement_date()
         settlement_details.set_settlement_date(settlement_date)
     if pset is not None:
         settlement_details.set_pset(pset)
@@ -606,7 +606,9 @@ def amend_block(request, agreed_price=None, net_gross_ind=None, give_up_broker=N
         fees_details.remove_fees()
     if comm_basis and comm_rate is not None:
         commissions_details = modify_request.add_commissions_details()
-        commissions_details.toggle_manual()
+        response = check_booking_toggle_manual(request)
+        if response['book.manualCheckboxState'] != 'checked':
+            commissions_details.toggle_manual()
         commissions_details.add_commission(comm_basis, comm_rate)
     if fees_basis and fees_rate is not None:
         fees_details = modify_request.add_fees_details()
