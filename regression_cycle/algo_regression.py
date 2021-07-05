c3e92c79a15ef6f6872bd5675097798e38bb3b6c
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-07-05 13:41:37 +0300

    algo regression

--- regression_cycle/algo_regression.py
+++ regression_cycle/algo_regression.py
@@ -8,7 +8,7 @@ from datetime import datetime
 def test_run(parent_id= None):
     report_id = bca.create_event('Algo regression_cycle', parent_id)
     try:
-        #multilisted_regression.test_run(report_id)
+        multilisted_regression.test_run(report_id)
         twap_regression.test_run(report_id)
     except Exception:
         logging.error("Error execution", exc_info=True)
