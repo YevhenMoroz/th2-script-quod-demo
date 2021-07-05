277b0d59ce72e35662e513f7c50d9e9b648b9b53
ostronov <oleksandr.stronov>
2021-07-05 16:15:09 +0300

    Update demo

--- demo.py
+++ demo.py
@@ -5,8 +5,8 @@ from quod_qa.fx import ui_tests
 from quod_qa.fx.fx_mm_esp import QAP_1560, QAP_2825, QAP_2555, QAP_2038, QAP_1599, QAP_1518, QAP_2034, QAP_2075, \
     QAP_1601, QAP_2039, QAP_2035, QAP_1418, QAP_2556, QAP_2117, QAP_2855, QAP_3563, QAP_2069, QAP_2646, QAP_2587, \
     QAP_2796, QAP_1536, QAP_2037, QAP_2072, QAP_2523, QAP_3045, QAP_1589
-from quod_qa.fx.fx_mm_positions import QAP_2505, tests, QAP_2378, QAP_2491, QAP_2492, QAP_2494, QAP_2496, QAP_2497, \
-    QAP_1897, QAP_1898, QAP_2506, QAP_2508, QAP_2500, QAP_2779, QAP_2501_WIP
+from quod_qa.fx.fx_mm_positions import QAP_2505, QAP_2378, QAP_2491, QAP_2492, QAP_2494, QAP_2496, QAP_2497, \
+    QAP_1897, QAP_1898, QAP_2506, QAP_2508, QAP_2500, QAP_2779, QAP_2501_WIP, QAP_3484_WIP
 from quod_qa.fx.fx_taker_esp import QAP_833, QAP_1115, QAP_110, QAP_231, QAP_1591, QAP_492, QAP_105, QAP_851
 from quod_qa.fx.fx_taker_rfq import QAP_6, QAP_564, QAP_565, QAP_566, QAP_567, QAP_568, QAP_569, QAP_570, QAP_571, \
     QAP_573, QAP_574, QAP_576, QAP_577, QAP_578, QAP_579, QAP_580, QAP_581, QAP_582, QAP_584, QAP_585, QAP_587, QAP_589, \
@@ -177,8 +177,8 @@ def test_run():
         # QAP_2505.execute(report_id, session_id)
         # QAP_2506.execute(report_id, session_id)
         # QAP_2508.execute(report_id, session_id)
-        QAP_2500.execute(report_id, session_id)
-        # QAP_2779.execute(report_id, session_id)
+        # QAP_2500.execute(report_id, session_id)
+        QAP_2779.execute(report_id, session_id)
         # QAP_105.execute(report_id, session_id)
         # QAP_1589.execute(report_id, session_id)
         # QAP_231.execute(report_id, session_id)
@@ -187,6 +187,8 @@ def test_run():
         # QAP_851.execute(report_id, session_id)
         # QAP_2523.execute(report_id, session_id)
         # ui_tests.execute(report_id, session_id)
+        # QAP_2556.execute(report_id, session_id)
+        # QAP_3484.execute(report_id, session_id)
         print('duration time = ' + str(datetime.now() - start))
 
     except Exception:
