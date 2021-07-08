0346b2c99ee04fac2d5f19a28bdac1dab4752ced
Yaroslav <yaroslav.furmuzal17@gmail.com>
2021-07-08 10:05:35 +0300

    QAP-4370: WA - describe WashBook page + related test cases

---
+++ quod_qa/web_admin/web_admin_core/pages/client_accounts/washbook/washbook_routes_sub_wizard.py
@@ -0,0 +1,54 @@
+from quod_qa.web_admin.web_admin_core.pages.client_accounts.washbook.washbook_constants import WashBookConstants
+from quod_qa.web_admin.web_admin_core.pages.common_page import CommonPage
+from quod_qa.web_admin.web_admin_core.utils.web_driver_container import WebDriverContainer
+
+
+class WashBookRoutesSubWizard(CommonPage):
+    def __init__(self, web_driver_container: WebDriverContainer):
+        super().__init__(web_driver_container)
+
+    # region ~~ROUTES~~
+    def click_on_plus_button(self):
+        self.find_by_xpath(WashBookConstants.PLUS_AT_ROUTES_TAB).click()
+
+    def click_on_checkmark(self):
+        self.find_by_xpath(WashBookConstants.CHECKMARK_AT_ROUTES_TAB).click()
+
+    def click_on_cancel(self):
+        self.find_by_xpath(WashBookConstants.CANCEL_AT_ROUTES_TAB).click()
+
+    def click_on_edit(self):
+        self.find_by_xpath(WashBookConstants.EDIT_AT_ROUTES_TAB).click()
+
+    def click_on_delete(self):
+        self.find_by_xpath(WashBookConstants.DELETE_AT_ROUTES_TAB).click()
+
+    # set and get
+
+    def set_default_route(self, value):
+        self.set_combobox_value(WashBookConstants.DEFAULT_ROUTE_AT_ROUTES_TAB, value)
+
+    def get_default_route(self):
+        return self.get_text_by_xpath(WashBookConstants.DEFAULT_ROUTE_AT_ROUTES_TAB)
+
+    def set_route_account_name(self, value):
+        self.set_text_by_xpath(WashBookConstants.ROUTE_ACCOUNT_NAME_AT_ROUTES_TAB, value)
+
+    def get_route_account_name(self):
+        return self.get_text_by_xpath(WashBookConstants.ROUTE_ACCOUNT_NAME_AT_ROUTES_TAB)
+
+    def set_route(self, value):
+        self.set_combobox_value(WashBookConstants.ROUTE_AT_ROUTES_TAB, value)
+
+    def get_route(self):
+        return self.get_text_by_xpath(WashBookConstants.ROUTE_AT_ROUTES_TAB)
+
+    # filters
+
+    def set_route_account_name_filter(self, value):
+        self.set_text_by_xpath(WashBookConstants.ROUTE_ACCOUNT_NAME_FILTER_AT_ROUTES_TAB, value)
+
+    def set_route_filter(self, value):
+        self.set_text_by_xpath(WashBookConstants.ROUTE_FILTER_AT_ROUTES_TAB, value)
+
+    # endregion
---
+++ quod_qa/web_admin/web_admin_test_cases/client_accounts/QAP_2165.py
@@ -0,0 +1,62 @@
+import time
+from datetime import datetime
+
+from quod_qa.web_admin.web_admin_core.pages.client_accounts.washbook.washbook_page import WashBookPage
+from quod_qa.web_admin.web_admin_core.pages.client_accounts.washbook.washbook_wizard import WashBookWizard
+from quod_qa.web_admin.web_admin_core.pages.login.login_page import LoginPage
+from quod_qa.web_admin.web_admin_core.pages.root.side_menu import SideMenu
+from quod_qa.web_admin.web_admin_core.utils.web_driver_container import WebDriverContainer
+from quod_qa.web_admin.web_admin_test_cases.common_test_case import CommonTestCase
+
+
+class QAP_2165(CommonTestCase):
+    def __init__(self, web_driver_container: WebDriverContainer, second_lvl_id):
+        super().__init__(web_driver_container, self.__class__.__name__, second_lvl_id)
+        now = datetime.now()
+        '''
+        iteration_value this variable was created for concatenation id and ext_id_client values,
+        because this values must be unique all time
+        '''
+        iteration_value = now.strftime("%d/%m/%Y %H:%M:%S")
+        self.id = "test 2165: " + str(iteration_value)
+        self.ext_id_client = "65: " + str(iteration_value)
+        self.client = "BROKER"
+        self.client_id_source = "BIC"
+
+    def precondition(self):
+        login_page = LoginPage(self.web_driver_container)
+        login_page.set_login("adm07")
+        login_page.set_password("adm07")
+        login_page.click_login_button()
+        login_page.check_is_login_successful()
+        side_menu = SideMenu(self.web_driver_container)
+        side_menu.open_washbook_page()
+        wash_book_main_menu = WashBookPage(self.web_driver_container)
+        wash_book_main_menu.click_on_new_button()
+        wash_book_wizard = WashBookWizard(self.web_driver_container)
+        time.sleep(1)
+        wash_book_wizard.set_id_at_values_tab(self.id)
+        time.sleep(1)
+        wash_book_wizard.set_ext_id_client_at_values_tab(self.ext_id_client)
+        time.sleep(1)
+        wash_book_wizard.set_client_at_values_tab(self.client)
+        time.sleep(1)
+        wash_book_wizard.set_client_id_source_at_values_tab(self.client_id_source)
+        time.sleep(1)
+        wash_book_wizard.click_on_save_changes()
+        time.sleep(1)
+        wash_book_main_menu.set_id_filter(self.id)
+        wash_book_main_menu.set_enabled_filter("true")
+        time.sleep(1)
+        wash_book_main_menu.click_on_more_actions()
+
+    def test_context(self):
+        self.precondition()
+        wash_book_main_menu = WashBookPage(self.web_driver_container)
+        expected_pdf_content = ["ID: {}".format(wash_book_main_menu.get_id_at_main_page()),
+                                "Ext ID Client: {}".format(wash_book_main_menu.get_ext_id_client_at_main_page()),
+                                "Client ID Source: BIC",
+                                "Clearing Account Type: Firm"]
+
+        self.verify(f"Is PDF contains {expected_pdf_content}", True,
+                    wash_book_main_menu.click_download_pdf_entity_button_and_check_pdf(expected_pdf_content))
