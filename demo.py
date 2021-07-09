fab149da94461b0b10d5a013ae2d40dde1960b98
ostronov <oleksandr.stronov>
2021-07-09 17:13:26 +0300

    Update demo

--- demo.py
+++ demo.py
@@ -4,9 +4,10 @@ from custom import basic_custom_actions as bca
 from quod_qa.fx import ui_tests
 from quod_qa.fx.fx_mm_esp import QAP_1560, QAP_2825, QAP_2555, QAP_2038, QAP_1599, QAP_1518, QAP_2034, QAP_2075, \
     QAP_1601, QAP_2039, QAP_2035, QAP_1418, QAP_2556, QAP_2117, QAP_2855, QAP_3563, QAP_2069, QAP_2646, QAP_2587, \
-    QAP_2796, QAP_1536, QAP_2037, QAP_2072, QAP_2523, QAP_3045, QAP_1589
+    QAP_2796, QAP_1536, QAP_2037, QAP_2072, QAP_2523, QAP_3045, QAP_1589, QAP_1643_wip
 from quod_qa.fx.fx_mm_positions import QAP_2505, QAP_2378, QAP_2491, QAP_2492, QAP_2494, QAP_2496, QAP_2497, \
     QAP_1897, QAP_1898, QAP_2506, QAP_2508, QAP_2500, QAP_2779, QAP_2501_WIP, QAP_3484
+from quod_qa.fx.fx_mm_rfq import QAP_1746
 from quod_qa.fx.fx_taker_esp import QAP_833, QAP_1115, QAP_110, QAP_231, QAP_1591, QAP_492, QAP_105, QAP_851
 from quod_qa.fx.fx_taker_rfq import QAP_6, QAP_564, QAP_565, QAP_566, QAP_567, QAP_568, QAP_569, QAP_570, QAP_571, \
     QAP_573, QAP_574, QAP_576, QAP_577, QAP_578, QAP_579, QAP_580, QAP_581, QAP_582, QAP_584, QAP_585, QAP_587, QAP_589, \
@@ -18,6 +19,7 @@ from quod_qa.fx.fx_taker_rfq import QAP_6, QAP_564, QAP_565, QAP_566, QAP_567, Q
 from rule_management import RuleManager
 
 from stubs import Stubs
+from test_cases import QAP_1552
 from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
@@ -46,12 +48,20 @@ def test_run():
     # Generation id and time for test run
     report_id = bca.create_event('ostronov tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
+    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
 
     session_id = set_session_id()
     # rules = rule_creation()
     try:
         start = datetime.now()
         print(f'start time = {start}')
+        case_params = {
+            'case_id': bca.create_event_id(),
+            'TraderConnectivity': 'fix-ss-rfq-314-luna-standard',
+            'Account': 'Iridium1',
+            'SenderCompID': 'QUODFX_UAT',
+            'TargetCompID': 'QUOD9',
+        }
 
         if not Stubs.frontend_is_open:
             prepare_fe_2(report_id, session_id)
@@ -59,28 +69,17 @@ def test_run():
             get_opened_fe(report_id, session_id)
         #
 
-        #
-        # # Create rules for RFQ Taker
-        #
         # # Add scripts
-        QAP_568.execute(report_id, session_id)
-        # QAP_569.execute(report_id, session_id)
-        # QAP_570.execute(report_id, session_id)
-        # QAP_574.execute(report_id, session_id)
-        # QAP_571.execute(report_id, session_id)
+        QAP_564.execute(report_id ,session_id)
+        # QAP_710.execute(report_id, session_id)
 
         rule_manager = RuleManager()
-        rule_manager.remove_rules_by_id_list([442, 443])
         rule_manager.print_active_rules()
-
-        # QAP_3484.execute(report_id, session_id)
         print('duration time = ' + str(datetime.now() - start))
 
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
-        # Remove rules
-        # rule_destroyer(rules)
         Stubs.win_act.unregister(session_id)
 
 
