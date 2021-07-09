f1a7817f18f7f13e4999ddc1e60659e7ffd3ecc4
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2021-07-09 14:53:47 +0300

    Add QAP-4458

--- quod_qa/eq/Care/QAP_3933.py
+++ quod_qa/eq/Care/QAP_3933.py
@@ -17,7 +17,6 @@ from stubs import Stubs
 from win_gui_modules.order_book_wrappers import ExtractionDetail, ExtractionAction, OrderInfo
 from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe, call, get_opened_fe
 from win_gui_modules.wrappers import set_base, verification, verify_ent, accept_order_request
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
