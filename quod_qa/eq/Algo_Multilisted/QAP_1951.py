6bbe8d04fe7bb1316e0ea8fc9cb0fd41c2868e23
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-07-15 11:45:41 +0300

    QAP_1951

--- quod_qa/eq/Algo_Multilisted/QAP_1951.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1951.py
@@ -207,7 +207,6 @@ def execute(report_id):
             SettlDate='*',
             ExecRestatementReason='*',
         )
-        er_2.pop('Account')
         fix_verifier_ss.CheckExecutionReport(er_2, responce_new_order_single, case=case_id_1, message_name='FIXQUODSELL5 sent 35=8 New', key_parameters=['ClOrdID', 'OrdStatus', 'ExecType'])
         #endregion
 
