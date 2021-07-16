06df8cbadf6125f7c5d756f0b4019e09466e1b32
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-07-16 10:57:10 +0300

    add add_NewOrderSingle_ExecutionReport_Reject

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from custom import basic_custom_actions as bca
 from custom.basic_custom_actions import convert_to_request, message_to_grpc
 from quod_qa.eq import MD_test_3, MD_test
+from rule_management import RuleManager
 
 from stubs import Stubs
 from win_gui_modules.utils import prepare_fe_2, get_opened_fe, set_session_id
@@ -38,32 +39,34 @@ def test_run():
         # MD_test_3.execute(report_id)
         # MD_test.execute(report_id)
 
-        params = {
-            "OrderID": "test_1",
-            "ExecID": "test_1",
-            "CumQty": "0",
-            "ExecType": "A",
-            "OrdStatus": "A",
-            "AvgPx": "0",
-            "Text": "Pending New status",
-            "LeavesQty": 10,
-            "Price": 20,
-            "Account": "XPAR_CLIENT1",
-            "ExDestination": "XPAR",
-            "TransactTime": datetime.utcnow().isoformat(),
-            "Side": "1",
-            "ClOrdID": "123456",
-            "OrdType": "2",
-            "OrderQty": "100",
-            "TimeInForce": "0",
-            "OrigClOrdID": "123456"
-        }
-        Stubs.fix_act.sendMessage(request=convert_to_request(
-            'Send ExecutionReport',
-            "fix-buy-side-316-ganymede",
-            report_id,
-            message_to_grpc('ExecutionReport', params, "fix-buy-side-316-ganymede")
-        ))
+        # params = {
+        #     "OrderID": "test_1",
+        #     "ExecID": "test_1",
+        #     "CumQty": "0",
+        #     "ExecType": "A",
+        #     "OrdStatus": "A",
+        #     "AvgPx": "0",
+        #     "Text": "Pending New status",
+        #     "LeavesQty": 10,
+        #     "Price": 20,
+        #     "Account": "XPAR_CLIENT1",
+        #     "ExDestination": "XPAR",
+        #     "TransactTime": datetime.utcnow().isoformat(),
+        #     "Side": "1",
+        #     "ClOrdID": "123456",
+        #     "OrdType": "2",
+        #     "OrderQty": "100",
+        #     "TimeInForce": "0",
+        #     "OrigClOrdID": "123456"
+        # }
+        # Stubs.fix_act.sendMessage(request=convert_to_request(
+        #     'Send ExecutionReport',
+        #     "fix-buy-side-316-ganymede",
+        #     report_id,
+        #     message_to_grpc('ExecutionReport', params, "fix-buy-side-316-ganymede")
+        # ))
+        rm = RuleManager()
+        rm.add_NewOrderSingle_ExecutionReport_Reject("fix-buy-side-316-ganymede", "XPAR_CLIENT1", "XPAR", 20)
     except Exception:
         logging.error("Error execution", exc_info=True)
 
