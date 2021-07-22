6f6a9def3d8358b3925891bc233e8b7e25706ff7
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-07-22 10:53:52 +0300

    modify QAP3910.py

--- quod_qa/eq/Care/QAP_3910.py
+++ quod_qa/eq/Care/QAP_3910.py
@@ -1,12 +1,12 @@
 import logging
 import time
 
-from custom.basic_custom_actions import create_event, timestamps
+from custom.basic_custom_actions import create_event
+from custom.verifier import Verifier
 from quod_qa.wrapper import eq_wrappers
-from quod_qa.wrapper.fix_verifier import FixVerifier
 from rule_management import RuleManager
 from stubs import Stubs
-from win_gui_modules.utils import set_session_id, get_base_request
+from win_gui_modules.utils import get_base_request
 from win_gui_modules.wrappers import set_base
 
 logger = logging.getLogger(__name__)
@@ -22,6 +22,7 @@ def execute(report_id, session_id):
     client = "CLIENT_FIX_CARE"
     lookup = "VETO"
     # endregion
+
     # region Open FE
     case_id = create_event(case_name, report_id)
     set_base(session_id, case_id)
@@ -49,15 +50,27 @@ def execute(report_id, session_id):
         rule_manager.remove_rule(nos_rule2)
     exec_id = eq_wrappers.get_2nd_lvl_detail(base_request, "ExecID")
     # endregion
+
     # region create CO
-    eq_wrappers.create_order_via_fix(case_id, 3, 1, client, 1, int(qty), 0)
+    eq_wrappers.create_order_via_fix(case_id, 3, 1, client, 1, qty, 0)
     eq_wrappers.accept_order(lookup, qty, price)
     co_order = eq_wrappers.get_order_id(base_request)
     # endregion
     # region Manual Match
-    eq_wrappers.manual_match(base_request, qty, ["OrderId", co_order], ["ExecID", exec_id])
+    eq_wrappers.manual_match(base_request, str(int(qty)/2), ["OrderId", co_order], ["ExecID", exec_id])
+    # endregion
+    # region Verify
+    eq_wrappers.verify_order_value(base_request, case_id, "UnmatchedQty", str(int(int(qty)/2)))
+    eq_wrappers.verify_order_value(base_request, case_id, "ExecSts", "PartiallyFilled")
+    # endregion
+    # region Manual Match
+    eq_wrappers.manual_match(base_request, str(int(qty) / 2), ["OrderId", co_order], ["ExecID", exec_id])
     # endregion
     # region Verify
     eq_wrappers.verify_order_value(base_request, case_id, "UnmatchedQty", "0")
     eq_wrappers.verify_order_value(base_request, case_id, "ExecSts", "Filled")
+    verifier = Verifier(case_id)
+    verifier.set_event_name("Checking Fees")
+    verifier.compare_values("ExecType", "Trade", eq_wrappers.get_2nd_lvl_detail(base_request, "ExecType"))
+    verifier.verify()
     # endregion
