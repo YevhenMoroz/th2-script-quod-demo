658bfbfcc2cfbf31b43285977b61844f49a9f6f8
nikita.rogozhin <nikita.rogozhin@exactprosystems.com>
2021-07-27 15:07:29 +0300

    QAP_2715 updated

--- test_cases/QAP_2715.py
+++ test_cases/QAP_2715.py
@@ -3,6 +3,7 @@ import logging
 from th2_grpc_act_gui_quod.act_ui_win_pb2 import VenueStatusesRequest
 from th2_grpc_act_gui_quod.ar_operations_pb2 import ExtractOrderTicketValuesRequest, ExtractDirectVenueExecutionRequest
 
+from custom.basic_custom_actions import wrap_message
 from custom.verifier import Verifier, VerificationMethod
 from stubs import Stubs
 from custom import basic_custom_actions as bca
@@ -206,7 +207,7 @@ class TestCase:
             ]
         }
         self.api.sendMessage(
-            request=SubmitMessageRequest(message=bca.message_to_grpc('ModifyVenue', modify_params, 'rest_wa303'),
+            request=SubmitMessageRequest(message=bca.wrap_message(modify_params, 'ModifyVenue', 'rest_wa303'),
                                          parent_event_id=self.case_id))
 
         modify_venue_params = {
@@ -223,10 +224,10 @@ class TestCase:
         }
         nos_response = self.api.sendMessage(
             request=SubmitMessageRequest(
-                message=bca.message_to_grpc('ModifyVenueStatus', modify_venue_params, 'rest_wa303'),
+                message=wrap_message(modify_venue_params, 'ModifyVenueStatus', 'rest_wa303'),
                 parent_event_id=self.case_id)
         )
-        # print(bca.message_to_grpc('ModifyVenueStatus', modify_venue_params,'rest_wa303'))
+        # print(wrap_message(modify_venue_params, 'ModifyVenueStatus', 'rest_wa303'))
 
     # FE open method
     def prepare_frontend(self):
@@ -340,4 +341,4 @@ class TestCase:
 
         except Exception as e:
             logging.error('Error execution', exc_info=True)
-        close_fe(self.case_id, self.session_id)
+        # close_fe(self.case_id, self.session_id)
