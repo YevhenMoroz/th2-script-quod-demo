06abeb6cd18ab818511fa32cbcb4dec66b846fb0
ostronov <oleksandr.stronov>
2021-07-30 12:15:16 +0300

    Complete test

--- quod_qa/fx/fx_mm_rfq/QAP_1552.py
+++ quod_qa/fx/fx_mm_rfq/QAP_1552.py
@@ -8,34 +8,32 @@ from quod_qa.fx.fx_wrapper.FixClientSellRfq import FixClientSellRfq
 
 def execute(report_id):
     case_name = Path(__file__).name[:-3]
-    case_id = bca.create_event(case_name, report_id)
 
     client_tier = "Iridium1"
     account = "Iridium1_1"
-    qty_1 = 1000000
+    settle_type = "0"
     symbol = "GBP/USD"
-    security_type_spo = "FXSPOT"
-    settle_date_spo = spo()
     currency = "GBP"
-    settle_type = '0'
+    security_type = "FXSPOT"
     side = "1"
-    leg1_side = "1"
-    leg2_side = "2"
+    order_qty = "1000000"
+    settle_date = spo()
+
+    # Create sub-report for case
+    case_id = bca.create_event(case_name, report_id)
 
     try:
-        # Step 1
-        params_spot = CaseParamsSellRfq(client_tier, case_id, orderqty=qty_1, symbol=symbol,
-                                        settldate=settle_date_spo,
-                                        settltype=settle_type, securitytype=security_type_spo,
-                                        currency=currency,
-                                        account=client_tier)
-
-        rfq = FixClientSellRfq(params_spot)
-        rfq.send_request_for_quote().verify_quote_pending()
-        offer_px = rfq.extract_filed('OfferPx')
-        rfq.send_new_order_single(offer_px)
-        rfq.verify_order_pending()
-        rfq.verify_order_filled()
+        rfq = FixClientSellRfq(
+            CaseParamsSellRfq(client_tier, case_id, side=side, orderqty=order_qty, symbol=symbol,
+                              securitytype=security_type, securityid=symbol,
+                              settldate=settle_date,
+                              settltype=settle_type, currency=currency, account=account)). \
+            send_request_for_quote(). \
+            verify_quote_pending()
+        price = rfq.extract_filed("OfferPx")
+        rfq.send_new_order_single(price). \
+            verify_order_pending(). \
+            verify_order_filled()
         # Step 2
 
     except Exception:
