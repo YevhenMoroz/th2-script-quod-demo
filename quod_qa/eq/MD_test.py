54208c5bb8c1a164ed14189f406ff182ffd68dd4
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-08-02 10:58:01 +0300

    update example with TradingSession

--- quod_qa/eq/MD_test.py
+++ quod_qa/eq/MD_test.py
@@ -16,7 +16,7 @@ def execute(report_id):
     simulator = Stubs.simulator
 
     MDRefID = Stubs.simulator.getMDRefIDForConnection(request=RequestMDRefID(
-        symbol="1085",
+        symbol="722",
         connection_id=ConnectionID(session_alias="fix-feed-handler-316-ganymede")
     )).MDRefID
     mdir_params_bid = {
@@ -27,29 +27,18 @@ def execute(report_id):
                 'MDEntryPx': '19',
                 'MDEntrySize': '1000',
                 'MDEntryPositionNo': '1',
-                # 'TradingSessionSubID': '6',
+                'TradingSessionSubID': '2',
+                'SecurityTradingStatus': '3'
             },
             {
                 'MDEntryType': '1',
                 'MDEntryPx': '19.99',
                 'MDEntrySize': '1000',
                 'MDEntryPositionNo': '1',
-                # 'TradingSessionSubID': '6'
+                'TradingSessionSubID': '2',
+                'SecurityTradingStatus': '3'
+
             },
-            {
-                'MDEntryType': '4',
-                'MDEntryPx': '19.99',
-                'MDEntrySize': '1000',
-                'MDEntryPositionNo': '1',
-                # 'TradingSessionSubID': '6'
-            },
-            {
-                'MDEntryType': '5',
-                'MDEntryPx': '19.99',
-                'MDEntrySize': '1000',
-                'MDEntryPositionNo': '1',
-                # 'TradingSessionSubID': '6'
-            }
         ]
     }
 
