4639f60407d20775b87bc2f772cd10c671518bc2
ostronov <oleksandr.stronov>
2021-08-02 09:02:13 +0300

    Commit demo before merge

--- demo.py
+++ demo.py
@@ -1,19 +1,13 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
-from quod_qa.fx import send_md
-from quod_qa.fx.fx_mm_esp import QAP_1418
-from quod_qa.fx.fx_mm_positions import QAP_1897
-from quod_qa.fx.fx_mm_rfq import QAP_2483, QAP_2490, QAP_2488, QAP_2484, QAP_2486, QAP_2489, QAP_2877, QAP_2878
-from quod_qa.fx.fx_taker_esp import QAP_2, QAP_19, QAP_492, QAP_228, QAP_458, QAP_530, QAP_3066, QAP_3068, QAP_3069, \
-    QAP_3157, QAP_3644
-
-from quod_qa.fx.fx_taker_rfq import QAP_612
+from quod_qa.fx.fx_mm_autohedging import QAP_2228
+from quod_qa.fx.fx_mm_rfq import QAP_1552, QAP_1539, QAP_2091, QAP_2101, QAP_2104
+from quod_qa.fx.my_methods import send_rfq
 
 from rule_management import RuleManager
 
 from stubs import Stubs
-from test_cases import QAP_1552, QAP_2143
 from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
@@ -57,26 +51,29 @@ def test_run():
             'TargetCompID': 'QUOD9',
         }
 
-        if not Stubs.frontend_is_open:
-            prepare_fe_2(report_id, session_id)
-        else:
-            get_opened_fe(report_id, session_id)
+        # if not Stubs.frontend_is_open:
+        #     prepare_fe_2(report_id, session_id)
+        # else:
+        #     get_opened_fe(report_id, session_id)
 
 
-        # # Add scripts
-        # QAP_2483.execute(report_id, session_id)
-        # QAP_2484.execute(report_id, session_id)
-        # QAP_2486.execute(report_id, session_id)
-        QAP_2488.execute(report_id, session_id)
-        # QAP_2489.execute(report_id, session_id)
-        # QAP_2490.execute(report_id, session_id)
-        # QAP_2877.execute(report_id, session_id)
-        # QAP_2878.execute(report_id, session_id)
+         # Add scripts
+        # QAP_2867.execute(report_id, session_id)
+        # QAP_2868.execute(report_id, session_id)
+        # QAP_2866.execute(report_id, session_id)
+        # QAP_2228.execute(report_id, session_id)
+        # QAP_2091.execute(report_id)
+        # QAP_2101.execute(report_id, session_id)
+        QAP_2104.execute(report_id, session_id)
+        # QAP_1539.execute(report_id, session_id)
+        # rm = RuleManager()
+        # ui_tests.execute(report_id, session_id)
+        # rm.print_active_rules()
+        # rm.print_active_rules_sim_test()
 
-        # rule_manager = RuleManager()
-        # rule_manager.remove_rules_by_id_list([5, 7])
-        # rule_manager.add_RFQ('fix-bs-rfq-314-luna-standard')
-        # rule_manager.print_active_rules()
+        # rm.remove_rules_by_id_list([5, 7])
+        # rm.add_RFQ('fix-bs-rfq-314-luna-standard')
+
         print('duration time = ' + str(datetime.now() - start))
 
     except Exception:
