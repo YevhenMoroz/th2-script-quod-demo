d450fbce8bad5e58cd63c03062ba4a1112f58c76
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-08-03 19:20:15 +0300

    modify commission_regression.py_regression.py

--- quod_qa/eq/Commissions/QAP_2998.py
+++ quod_qa/eq/Commissions/QAP_2998.py
@@ -1,14 +1,13 @@
+import logging
 import time
 
+from custom.basic_custom_actions import create_event
 from custom.verifier import Verifier
 from quod_qa.wrapper import eq_wrappers
-from quod_qa.wrapper.fix_verifier import FixVerifier
 from rule_management import RuleManager
 from stubs import Stubs
-from custom.basic_custom_actions import create_event
 from win_gui_modules.order_book_wrappers import ExtractionDetail, ExtractionAction, OrderInfo, OrdersDetails
-from win_gui_modules.utils import set_session_id, get_base_request, call
-import logging
+from win_gui_modules.utils import get_base_request, call
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -34,7 +33,7 @@ def execute(report_id, session_id):
             }
     # endregion
     # region Open FE
-    #eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
+    eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
     # endregion
     # region Create Order
     try:
--- quod_qa/eq/Commissions/QAP_3351.py
+++ quod_qa/eq/Commissions/QAP_3351.py
@@ -1,14 +1,12 @@
+import logging
 import time
 
-from custom.verifier import Verifier, VerificationMethod
+from custom.basic_custom_actions import create_event
 from quod_qa.wrapper import eq_wrappers
 from quod_qa.wrapper.fix_verifier import FixVerifier
 from rule_management import RuleManager
 from stubs import Stubs
-from custom.basic_custom_actions import create_event
-from win_gui_modules.order_book_wrappers import ExtractionDetail, ExtractionAction, OrderInfo, OrdersDetails
-from win_gui_modules.utils import set_session_id, get_base_request, call
-import logging
+from win_gui_modules.utils import get_base_request
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,8 +18,7 @@ def execute(report_id, session_id):
     # region Declarations
     qty = "900"
     price = "10"
-    lookup = "VETO"
-    client = "CLIENT_YMOROZ"#"MOClient"
+    client = "MOClient"
     account = "MOClient_SA1"
     work_dir = Stubs.custom_config['qf_trading_fe_folder']
     username = Stubs.custom_config['qf_trading_fe_user']
@@ -29,7 +26,7 @@ def execute(report_id, session_id):
     base_request = get_base_request(session_id, case_id)
     # endregion
     # region Open FE
-    #eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
+    eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
     # endregion
 
     # region Create Order
