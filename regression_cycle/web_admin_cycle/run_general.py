501f56f1af8c1606c00f660e411f814900ddce19
Yaroslav <yaroslav.furmuzal17@gmail.com>
2021-08-03 17:25:11 +0300

    QAP-4531:Add tests to WebAdmin regression cycle.

---
+++ regression_cycle/web_admin_cycle/run_general.py
@@ -0,0 +1,26 @@
+import time
+import traceback
+from datetime import timedelta
+
+from quod_qa.web_admin.web_admin_core.utils.web_driver_container import WebDriverContainer
+from quod_qa.web_admin.web_admin_test_cases.general.QAP_796 import QAP_796
+
+from custom import basic_custom_actions as bca
+class RunGeneral:
+    def __init__(self, web_driver_container: WebDriverContainer,root_report_id):
+        self.folder_name = 'web admin'
+        self.first_lvl_id = bca.create_event(self.folder_name, root_report_id)
+        self.second_lvl_id = bca.create_event(self.__class__.__name__, self.first_lvl_id)
+        self.web_driver_container = web_driver_container
+
+    def execute(self):
+        try:
+            start_time = time.monotonic()
+
+            QAP_796(self.web_driver_container, self.second_lvl_id).run()
+
+            end_time = time.monotonic()
+            print("Run General ~execution time~ = " + str(timedelta(seconds=end_time - start_time)))
+
+        except Exception:
+            print(traceback.format_exc() + " Execute ERROR !->  " + self.__class__.__name__)
