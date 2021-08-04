15f4ba3626401e1ea179c3acfddd4353f5bbb3ff
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-08-04 14:01:18 +0300

    QAP_4646

--- quod_qa/eq/Algo_PercentageVolume/QAP_4646.py
+++ quod_qa/eq/Algo_PercentageVolume/QAP_4646.py
@@ -62,7 +62,6 @@ instrument = {
 def rule_creation():
     rule_manager = RuleManager()
     nos_ioc_md_rule = rule_manager.add_NewOrdSingle_IOC_MarketData(connectivity_buy_side, account, ex_destination_1, price, child_ioc_qty, True, connectivity_fh, s_par, [NoMDEntries(MDEntryType="0", MDEntryPx="19.99", MDEntrySize="500", MDEntryPositionNo="1"), NoMDEntries(MDEntryType="1", MDEntryPx="19.995", MDEntrySize="430", MDEntryPositionNo="1")])
-    #nos_ioc_rule = rule_manager.add_NewOrdSingle_IOC(connectivity_buy_side, account, ex_destination_1, True, child_ioc_qty, price)
     nos_rule1 = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(connectivity_buy_side, account, ex_destination_1, price_1)
     nos_rule2 = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(connectivity_buy_side, account, ex_destination_1, price_2)
     ocr_rule = rule_manager.add_OrderCancelRequest(connectivity_buy_side, account,ex_destination_1, True)
