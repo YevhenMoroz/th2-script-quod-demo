d3c39521b3f48904db1f43099fc6debcdbe1dd05
Yaroslav <yaroslav.furmuzal17@gmail.com>
2021-08-12 17:17:29 +0300

    QAP-5026 : WA- Describe Quoting sessions page+test-cases.

--- regression_cycle/web_admin_cycle/run_fx_market_making.py
+++ regression_cycle/web_admin_cycle/run_fx_market_making.py
@@ -6,6 +6,9 @@ from custom import basic_custom_actions as bca
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_1647 import QAP_1647
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_1688 import QAP_1688
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_1695 import QAP_1695
+from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_1756 import QAP_1756
+from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_1757 import QAP_1757
+from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_1758 import QAP_1758
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2004 import QAP_2004
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2011 import QAP_2011
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2022 import QAP_2022
@@ -18,6 +21,7 @@ from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2247 import QAP
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2289 import QAP_2289
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2379 import QAP_2379
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2442 import QAP_2442
+from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2626 import QAP_2626
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2649 import QAP_2649
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_2772 import QAP_2772
 from quod_qa.web_admin.web_admin_test_cases.fx_market_making.QAP_3008 import QAP_3008
@@ -36,27 +40,31 @@ class RunFxMarketMaking:
     def execute(self):
         try:
             start_time = time.monotonic()
-            QAP_1647(self.web_driver_container, self.second_lvl_id).run()
-            QAP_1688(self.web_driver_container, self.second_lvl_id).run()
-            QAP_1695(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2247(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2289(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2379(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2442(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2649(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2004(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2011(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2022(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2030(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2040(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2056(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2772(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2158(self.web_driver_container, self.second_lvl_id).run()
-            QAP_3008(self.web_driver_container, self.second_lvl_id).run()
-            QAP_3009(self.web_driver_container, self.second_lvl_id).run()
-            QAP_3010(self.web_driver_container, self.second_lvl_id).run()
-            QAP_2045(self.web_driver_container, self.second_lvl_id).run()
-            QAP_3275(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_1647(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_1688(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_1695(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_1756(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_1757(self.web_driver_container, self.second_lvl_id).run()
+            #QAP_1758(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2247(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2289(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2379(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2442(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2649(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2004(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2011(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2022(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2030(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2040(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2056(self.web_driver_container, self.second_lvl_id).run()
+            QAP_2626(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2772(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2158(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_3008(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_3009(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_3010(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_2045(self.web_driver_container, self.second_lvl_id).run()
+            # QAP_3275(self.web_driver_container, self.second_lvl_id).run()
 
             end_time = time.monotonic()
             print("Run FXMM ~execution time~ = " + str(timedelta(seconds=end_time - start_time)))
