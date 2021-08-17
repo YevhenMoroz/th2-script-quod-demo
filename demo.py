a4940e1cd81279b5c3a4529a91266bac36b0f1a9
ostronov <oleksandr.stronov>
2021-08-17 09:04:31 +0300

    Update demo

--- demo.py
+++ demo.py
@@ -4,9 +4,11 @@ from custom import basic_custom_actions as bca
 from quod_qa.fx.fx_mm_autohedging import QAP_2228, QAP_2290
 from quod_qa.fx.fx_mm_positions import QAP_1898
 from quod_qa.fx.fx_mm_rfq import QAP_1552, QAP_1539, QAP_2091, QAP_2101, QAP_2104, QAP_2105, QAP_2295, QAP_2296, \
-    QAP_2297, QAP_2958, QAP_1746
+    QAP_2297, QAP_2958, QAP_1746, QAP_1540, QAP_1562, QAP_1563, QAP_1970, QAP_2103, QAP_2177, QAP_3565, QAP_2877, \
+    QAP_4228, QAP_4085
 from quod_qa.fx.fx_taker_esp import QAP_2949
-from quod_qa.fx.my_methods import send_rfq
+from quod_qa.fx.fx_taker_rfq import QAP_2826
+from quod_qa.fx.my_methods import send_rfq, send_md
 
 from rule_management import RuleManager
 
@@ -39,7 +41,7 @@ def test_run():
     # Generation id and time for test run
     report_id = bca.create_event('ostronov tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
+    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial"
 
     session_id = set_session_id()
     # rules = rule_creation()
@@ -59,17 +61,22 @@ def test_run():
         # else:
         #     get_opened_fe(report_id, session_id)
 
-
-         # Add scripts
-        # QAP_1746.execute(report_id)
-        # QAP_1552.execute(report_id)
-        # QAP_1898.execute(report_id, session_id)
         # rm = RuleManager()
+        # # rm.remove_rule_by_id_test_sim(2)
         # rm.print_active_rules()
         # rm.print_active_rules_sim_test()
-        send_rfq.execute(report_id)
-        # rm.remove_rules_by_id_list([5, 7])
-        # rm.add_RFQ('fix-bs-rfq-314-luna-standard')
+         # Add scripts
+        # QAP_2101.execute(report_id, session_id)
+        # QAP_2103.execute(report_id)
+        # QAP_2177.execute(report_id)
+        # QAP_3565.execute(report_id)
+
+        # send_rfq.execute(report_id)
+
+        # QAP_2877.execute(report_id, session_id)
+        # QAP_4228.execute(report_id)
+        # QAP_4085.execute(report_id)
+        send_md.execute(report_id)
 
         print('duration time = ' + str(datetime.now() - start))
 
