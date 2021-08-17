c05aae8bb85069cfe25a0d687f7832124d388258
ostronov <oleksandr.stronov>
2021-08-17 09:34:27 +0300

    Restore files after merge schema_quod into ostronov

--- demo.py
+++ demo.py
@@ -1,90 +1,71 @@
 import logging
 from datetime import datetime
+
+from th2_grpc_sim_quod.sim_pb2 import NoMDEntries
+
 from custom import basic_custom_actions as bca
-from quod_qa.fx.fx_mm_autohedging import QAP_2228, QAP_2290
-from quod_qa.fx.fx_mm_positions import QAP_1898
-from quod_qa.fx.fx_mm_rfq import QAP_1552, QAP_1539, QAP_2091, QAP_2101, QAP_2104, QAP_2105, QAP_2295, QAP_2296, \
-    QAP_2297, QAP_2958, QAP_1746, QAP_1540, QAP_1562, QAP_1563, QAP_1970, QAP_2103, QAP_2177, QAP_3565, QAP_2877, \
-    QAP_4228, QAP_4085
-from quod_qa.fx.fx_taker_esp import QAP_2949
-from quod_qa.fx.fx_taker_rfq import QAP_2826
-from quod_qa.fx.my_methods import send_rfq, send_md
-
+from examples import example_java_api
+from quod_qa.eq.Algo_PercentageVolume import QAP_1324
+from quod_qa.eq.Test import read_log_example
 from rule_management import RuleManager
-
 from stubs import Stubs
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
+from stubs import Stubs
+from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe, call, close_fe, get_opened_fe, \
+    prepare_fe_2
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 timeouts = False
+work_dir = Stubs.custom_config['qf_trading_fe_folder']
+username = Stubs.custom_config['qf_trading_fe_user']
+password = Stubs.custom_config['qf_trading_fe_password']
 
 channels = dict()
 
 
-def rule_creation():
-    rule_manager = RuleManager()
-    rfq_quote = rule_manager.add_RFQ('fix-bs-rfq-314-luna-standard')
-    rfq_trade = rule_manager.add_TRFQ('fix-bs-rfq-314-luna-standard')
-    # return [rfq_quote, rfq_trade]
-
-
-def rule_destroyer(list_rules):
-    if list_rules != None:
-        rule_manager = RuleManager()
-        for rule in list_rules:
-            rule_manager.remove_rule(rule)
-
-
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('ostronov tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event(' tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial"
-
-    session_id = set_session_id()
-    # rules = rule_creation()
+    # session_id = set_session_id()
     try:
-        start = datetime.now()
-        print(f'start time = {start}')
-        case_params = {
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'fix-ss-rfq-314-luna-standard',
-            'Account': 'Iridium1',
-            'SenderCompID': 'QUODFX_UAT',
-            'TargetCompID': 'QUOD9',
-        }
+        # if not Stubs.frontend_is_open:
+        #     prepare_fe(report_id, session_id, work_dir, username, password)
+        # else:
+        #     get_opened_fe(report_id, session_id, work_dir)
 
-        if not Stubs.frontend_is_open:
-            prepare_fe_2(report_id, session_id)
-        else:
-            get_opened_fe(report_id, session_id)
+        # example_java_api.TestCase(report_id).execute()
+        # QAP_1324.execute(report_id, session_id)
+
+
+        read_log_example.execute(report_id)
+
 
         # rm = RuleManager()
-        # # rm.remove_rule_by_id_test_sim(2)
+        # nos_ioc_md_rule = rm.add_NewOrdSingle_IOC_MarketData(
+        #     "fix-buy-side-316-ganymede",
+        #     "XPAR_CLIENT1",
+        #     "XPAR",
+        #     20,
+        #     100,
+        #     True,
+        #     "fix-feed-handler-316-ganymede",
+        #     "756",
+        #     20,
+        #     350,
+        #         [NoMDEntries(MDEntryType="0", MDEntryPx="0", MDEntrySize="0", MDEntryPositionNo="1"),NoMDEntries(MDEntryType="1", MDEntryPx="20", MDEntrySize="500", MDEntryPositionNo="1")],
+        #         [NoMDEntries(MDUpdateAction='0', MDEntryType='2', MDEntryPx='40', MDEntrySize='1000', MDEntryDate=datetime.utcnow().date().strftime("%Y%m%d"),
+        #                  MDEntryTime=datetime.utcnow().time().strftime("%H:%M:%S"))])
         # rm.print_active_rules()
-        # rm.print_active_rules_sim_test()
-         # Add scripts
-        # QAP_2101.execute(report_id, session_id)
-        # QAP_2103.execute(report_id)
-        # QAP_2177.execute(report_id)
-        # QAP_3565.execute(report_id)
-
-        # send_rfq.execute(report_id)
-
-        QAP_2877.execute(report_id, session_id)
-        # QAP_4228.execute(report_id)
-        # QAP_4085.execute(report_id)
-        # send_md.execute(report_id)
-
-        print('duration time = ' + str(datetime.now() - start))
+        # rm.remove_rule_by_id(10)
 
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
-        Stubs.win_act.unregister(session_id)
-
+        pass
+        # Stubs.win_act.unregister(session_id)
 
 if __name__ == '__main__':
     logging.basicConfig()
