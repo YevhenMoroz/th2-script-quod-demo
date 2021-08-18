b02ed2309af4c51d66c03708088da0a3eb5a758b
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-08-18 11:24:57 +0300

    QAP_1960

--- quod_qa/eq/Algo_Multilisted/QAP_1960.py
+++ quod_qa/eq/Algo_Multilisted/QAP_1960.py
@@ -17,20 +17,24 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
-
-qty = 1300
+#text
 text_pn='Pending New status'
 text_n='New status'
 text_ocrr='OCRRRule'
 text_c='order canceled'
+
+#order param
+qty = 1300
 tif_gtc = 1
 price = 20
 side = 1
+order_type = 2
+currency = 'EUR'
+
+#venue param
 ex_destination_1 = "XPAR"
 client = "CLIENT2"
-order_type = 2
 account = 'XPAR_CLIENT2'
-currency = 'EUR'
 s_par = '734'
 s_trqx = '3416'
 
