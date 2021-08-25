554c02bd3479d01d77eeaf8da70e5785def34f5e
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-08-25 12:19:55 +0300

    modify QAP_478.py  QAP_480.py  QAP_481.py

--- quod_qa/eq/Care/QAP_478.py
+++ quod_qa/eq/Care/QAP_478.py
@@ -1,17 +1,12 @@
 import logging
 import time
 from datetime import datetime
-from th2_grpc_hand import rhbatch_pb2
-
 import quod_qa.wrapper.eq_fix_wrappers
-from quod_qa.wrapper import eq_wrappers
 from custom.basic_custom_actions import create_event, timestamps
+from quod_qa.wrapper import eq_wrappers, eq_fix_wrappers
 from rule_management import RuleManager
 from stubs import Stubs
-from win_gui_modules.order_book_wrappers import OrdersDetails, ExtractionDetail, ExtractionAction, OrderInfo
-from win_gui_modules.utils import set_session_id, get_base_request, close_fe, call
-from win_gui_modules.wrappers import set_base, verification, verify_ent
-
+from win_gui_modules.utils import get_base_request
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
@@ -22,47 +17,40 @@ def execute(report_id, session_id):
     seconds, nanos = timestamps()
     case_id = create_event(case_name, report_id)
     # region Declarations
-    act = Stubs.win_act_order_book
-    common_act = Stubs.win_act
     qty = "900"
-    price = "20"
+    price = "10"
     client = "CLIENT_FIX_CARE"
     lookup = "VETO"
-    order_type = "Limit"
     work_dir = Stubs.custom_config['qf_trading_fe_folder']
     username = Stubs.custom_config['qf_trading_fe_user']
     password = Stubs.custom_config['qf_trading_fe_password']
-    desk = Stubs.custom_config['qf_trading_fe_user_desk']
     base_request = get_base_request(session_id, case_id)
     # endregion
     # region Open FE
     eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
     # endregion
     # region create CO
-    fix_message = quod_qa.wrapper.eq_fix_wrappers.create_order_via_fix(case_id, 3, 1, client, 2, qty, 0, price)
+    quod_qa.wrapper.eq_fix_wrappers.create_order_via_fix(case_id, 3, 1, client, 1, qty, 0)
     # endregions
     # region AcceptOrder
-    eq_wrappers.accept_order(lookup, qty, price)
+    eq_wrappers.accept_order(lookup, qty, "")
     # endregion
     # region Check values in OrderBook
     eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Open")
     # endregion
-
     # region DirectMOC split
     try:
         rule_manager = RuleManager()
-        nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(
-            quod_qa.wrapper.eq_fix_wrappers.get_buy_connectivity(),
-            client + '_PARIS', "XPAR", float(price))
-        eq_wrappers.direct_loc_order('50', 'Route via FIXBUYTH2 - component used by TH2 simulator and autotests')
+        nos_rule = rule_manager.add_NewOrdSingle_Market(eq_fix_wrappers.get_buy_connectivity(),
+            client + '_PARIS', "XPAR", False, 0, 0)
+        eq_wrappers.direct_moc_order('50', 'Route via FIXBUYTH2 - component used by TH2 simulator and autotests')
     except Exception:
         logger.error("Error execution", exc_info=True)
     finally:
-        time.sleep(1)
+        time.sleep(2)
         rule_manager.remove_rule(nos_rule)
     # endregion
-
-    # check sub Order status
+    # region Check sub Order status
     eq_wrappers.verify_order_value(base_request, case_id, 'Qty', str(int(int(qty) / 2)), True)
     eq_wrappers.verify_order_value(base_request, case_id, 'ExecPcy', 'DMA', True)
     # endregion
