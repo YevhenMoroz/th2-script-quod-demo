2c57c312b69f652ae0e17041c87e7aa002d64a0c
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-09-03 12:24:51 +0300

    QAP-4061 added

--- demo.py
+++ demo.py
@@ -2,9 +2,9 @@ import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
 from quod_qa.fx.fx_mm_rfq import QAP_4748, QAP_4223, QAP_2103, QAP_2382
-from quod_qa.fx.fx_mm_esp import QAP_3661
+from quod_qa.fx.fx_mm_esp import QAP_3661, QAP_4061
 from quod_qa.fx.fx_mm_rfq.interpolation import QAP_3734, QAP_3739, QAP_3689
-from MyFiles import MyTest, SendMD, Test
+from MyFiles import MyTest, SendMD, Test, StringThing
 from rule_management import RuleManager
 from stubs import Stubs
 from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe_2, get_opened_fe
@@ -78,9 +78,14 @@ def test_run():
         # QAP_2103.execute(report_id)
         # QAP_2382.execute(report_id)
         # QAP_3661.execute(report_id, session_id)
+        QAP_4061.execute(report_id, session_id)
+
+        # region my test files
         # SendMD.execute(report_id)
-        Test.execute(report_id, session_id)
+        # Test.execute(report_id, session_id)
         # ui_tests.execute(report_id, session_id)
+        # StringThing.execute()
+        # endregion
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
