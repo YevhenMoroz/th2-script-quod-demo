7de5dd4d90d2ef47a58acb2272726bc132c765d8
ostronov <oleksandr.stronov>
2021-09-03 09:22:18 +0300

    Update demo

--- demo.py
+++ demo.py
@@ -7,9 +7,9 @@ from quod_qa.fx.fx_mm_positions import QAP_1898
 from quod_qa.fx.fx_mm_rfq import QAP_1552, QAP_1539, QAP_2091, QAP_2101, QAP_2104, QAP_2105, QAP_2295, QAP_2296, \
     QAP_2297, QAP_2958, QAP_1746, QAP_1540, QAP_1562, QAP_1563, QAP_1970, QAP_2103, QAP_2177, QAP_3565, QAP_2877, \
     QAP_4228, QAP_4085, QAP_3106, QAP_3107, QAP_3108, QAP_3109, QAP_2382, QAP_3110, QAP_3111, QAP_3112, QAP_3113, \
-    QAP_3234, QAP_3250, QAP_1978
+    QAP_3234, QAP_3250, QAP_1978, QAP_3409, QAP_3494, QAP_2353, QAP_3704, QAP_3003, QAP_4509
 from quod_qa.fx.fx_taker_esp import QAP_2949
-from quod_qa.fx.fx_taker_rfq import QAP_2826
+from quod_qa.fx.fx_taker_rfq import QAP_2826, QAP_3048
 from quod_qa.fx.my_methods import send_rfq, send_md, sequence_test
 
 from rule_management import RuleManager
@@ -58,44 +58,27 @@ def test_run():
             'TargetCompID': 'QUOD9',
         }
 
-        if not Stubs.frontend_is_open:
-            prepare_fe_2(report_id, session_id)
-        else:
-            get_opened_fe(report_id, session_id)
+        # if not Stubs.frontend_is_open:
+        #     prepare_fe_2(report_id, session_id)
+        # else:
+        #     get_opened_fe(report_id, session_id)
 
         # rm = RuleManager()
-        # rm.remove_all_rules()
-        # rm.add_TRFQ_test_sim("fix-bs-rfq-314-luna-standard")
-        # rm.remove_rule_by_id_test_sim(6)
         # rm.print_active_rules()
         # rm.print_active_rules_sim_test()
          # Add scripts
-        # QAP_2101.execute(report_id, session_id)
-        # QAP_2103.execute(report_id)
-        # QAP_2177.execute(report_id)
-        # QAP_3565.execute(report_id)
+        # QAP_3409.execute(report_id, session_id)
+        # QAP_3048.execute(report_id, session_id)
+        # QAP_3003.execute(report_id)
+        QAP_4509.execute(report_id)
+        # QAP_3494.execute(report_id)
 
-        # send_rfq.execute(report_id)
+        # QAP_2296.execute(report_id, session_id)
 
-        # QAP_1978.execute(report_id)
-        # QAP_2877.execute(report_id, session_id)
-        # QAP_2382.execute(report_id)
-        # QAP_3106.execute(report_id, session_id)
-        # QAP_3107.execute(report_id, session_id)
-        # QAP_3108.execute(report_id, session_id)
-        # QAP_3109.execute(report_id, session_id)
-
-        # QAP_3110.execute(report_id, session_id)
-        # QAP_3111.execute(report_id, session_id)
-        # QAP_3112.execute(report_id, session_id)
-        # QAP_3113.execute(report_id, session_id)
-        # QAP_3234.execute(report_id)
-
+        # QAP_2353.execute(report_id)
+        # QAP_3704.execute(report_id, session_id)
         # sequence_test.execute(report_id)
-        QAP_3250.execute(report_id, session_id)
-        # send_md.execute(report_id)
-
-        # QAP_4094.execute(report_id)
+        # send_rfq.execute(report_id)
 
         print('duration time = ' + str(datetime.now() - start))
 
