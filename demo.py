a8b436738205009e805cb01f63e7eecd25b545c7
ostronov <oleksandr.stronov>
2021-09-13 09:55:14 +0300

    Restore params after merge

--- demo.py
+++ demo.py
@@ -1,112 +1,41 @@
 import logging
 from datetime import datetime
+
 from custom import basic_custom_actions as bca
-from quod_qa.fx.fx_mm_autohedging import QAP_2228, QAP_2290, QAP_2250
-from quod_qa.fx.fx_mm_esp import QAP_1418, QAP_4094, QAP_2082, QAP_2078, QAP_2797, QAP_1518, QAP_2825, QAP_1558, \
-    QAP_1559
-from quod_qa.fx.fx_mm_positions import QAP_1898
-from quod_qa.fx.fx_mm_rfq import QAP_1552, QAP_1539, QAP_2091, QAP_2101, QAP_2104, QAP_2105, QAP_2295, QAP_2296, \
-    QAP_2297, QAP_2958, QAP_1746, QAP_1540, QAP_1562, QAP_1563, QAP_1970, QAP_2103, QAP_2177, QAP_3565, QAP_2877, \
-    QAP_4228, QAP_4085, QAP_3106, QAP_3107, QAP_3108, QAP_3109, QAP_2382, QAP_3110, QAP_3111, QAP_3112, QAP_3113, \
-    QAP_3234, QAP_3250, QAP_1978, QAP_3409, QAP_3494, QAP_2353, QAP_3704, QAP_3003, QAP_4509, QAP_4510
-from quod_qa.fx.fx_mm_rfq.interpolation import QAP_3766, QAP_3805
-from quod_qa.fx.fx_mm_rfq.rejection import QAP_3735
-from quod_qa.fx.fx_taker_esp import QAP_2949, QAP_3157
-from quod_qa.fx.fx_taker_rfq import QAP_2826, QAP_3048
-from quod_qa.fx.my_methods import send_rfq, send_md, sequence_test
-
-from rule_management import RuleManager
-
+from quod_qa.eq.DMA import QAP_2000
 from stubs import Stubs
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
+from win_gui_modules.utils import set_session_id
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 timeouts = False
+work_dir = Stubs.custom_config['qf_trading_fe_folder']
+username = Stubs.custom_config['qf_trading_fe_user']
+password = Stubs.custom_config['qf_trading_fe_password']
 
 channels = dict()
 
-
-def rule_creation():
-    rule_manager = RuleManager()
-    rfq_quote = rule_manager.add_RFQ('fix-bs-rfq-314-luna-standard')
-    rfq_trade = rule_manager.add_TRFQ('fix-bs-rfq-314-luna-standard')
-    # return [rfq_quote, rfq_trade]
-
-
-def rule_destroyer(list_rules):
-    if list_rules != None:
-        rule_manager = RuleManager()
-        for rule in list_rules:
-            rule_manager.remove_rule(rule)
-
-
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('ostronov tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('Yehor tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
-
     session_id = set_session_id()
-    # rules = rule_creation()
+
     try:
-        start = datetime.now()
-        print(f'start time = {start}')
-        case_params = {
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'fix-ss-rfq-314-luna-standard',
-            'Account': 'Iridium1',
-            'SenderCompID': 'QUODFX_UAT',
-            'TargetCompID': 'QUOD9',
-        }
+        # example_java_api.TestCase(report_id).execute()
 
-        # if not Stubs.frontend_is_open:
-        #     prepare_fe_2(report_id, session_id)
-        # else:
-        #     get_opened_fe(report_id, session_id)
-
+        QAP_2000.execute(report_id, session_id)
         # rm = RuleManager()
-        # # rm.remove_rule_by_id(2)
         # rm.print_active_rules()
-        # rm.add_fx_md_to_test_sim("fix-fh-314-luna")
-        # rm.print_active_rules_sim_test()
-         # Add scripts
-        # QAP_3409.execute(report_id, session_id)
-        # QAP_3048.execute(report_id, session_id)
-        # QAP_3003.execute(report_id)
-        # QAP_4509.execute(report_id)
-        # QAP_4510.execute(report_id)
-        # QAP_3494.execute(report_id)
-
-        # QAP_2296.execute(report_id, session_id)
-        # QAP_3735.execute(report_id, session_id)
-
-        # QAP_2353.execute(report_id)
-        # QAP_3704.execute(report_id, session_id)
-        # sequence_test.execute(report_id)
-        # send_rfq.execute(report_id)
-        # QAP_1746.execute(report_id)
-        # QAP_2082.execute(report_id)
-        # QAP_2797.execute(report_id)
-        # QAP_1518.execute(report_id)
-        # QAP_2825.execute(report_id, session_id)
-        # QAP_1558.execute(report_id)
-        # QAP_1559.execute(report_id)
-        # QAP_3157.execute(report_id, session_id)
-
-        # send_md.execute(report_id)
-
-        QAP_2250.execute(report_id, session_id)
-
-
-        print('duration time = ' + str(datetime.now() - start))
+        # rm.remove_rules_by_id_list([2078, 2079, 2729, 2733])
+        # rm.print_active_rules()
 
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
         Stubs.win_act.unregister(session_id)
-
 
 if __name__ == '__main__':
     logging.basicConfig()
