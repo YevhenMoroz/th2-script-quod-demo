df1eb6942e55c6ef0a06b58460633328f35ef892
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-09-13 18:45:59 +0300

    update Redburn MOC demo tests

--- quod_qa/eq/Algo_Redburn/Algo_MOC/CLO_WW_01.py
+++ quod_qa/eq/Algo_Redburn/Algo_MOC/CLO_WW_01.py
@@ -6,7 +6,7 @@ from stubs import Stubs
 timeouts = True
 
 
-def execute(report_id):
+def     execute(report_id):
     instrument = {
     'Symbol': 'GB00BH4HKS39-XLON',
     'SecurityID': 'GB00BH4HKS39',
@@ -16,12 +16,12 @@ def execute(report_id):
 
     new_order_single_params = {
         'Account': "REDBURN",
-        'ClOrdID': bca.client_orderid(9),
+        'ClOrdID': 'CLO_WW_01 '+ bca.client_orderid(9),
         'HandlInst': 2,
         'Side': 1,
-        'OrderQty': 100,
+        'OrderQty': 10000000,
         'TimeInForce': 0,
-        'Price': 20,
+        'Price': 120,
         'OrdType': 2,
         'TransactTime': datetime.utcnow().isoformat(),
         'Instrument': instrument,
@@ -33,7 +33,7 @@ def execute(report_id):
         'QuodFlatParameters': {
             'MaxParticipation': '10',
             'WouldInAuction': '1',
-            'TriggerPriceRed': '18',
+            'TriggerPriceRed': '117.9',
             'ExcludePricePoint2': '1'
         }
     }
