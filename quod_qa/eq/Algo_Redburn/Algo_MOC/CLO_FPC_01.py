6a094046bd0caaf181de0dc3f72140868afa9d0f
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-09-14 15:38:05 +0300

    update Redburn CLO demo tests

--- quod_qa/eq/Algo_Redburn/Algo_MOC/CLO_FPC_01.py
+++ quod_qa/eq/Algo_Redburn/Algo_MOC/CLO_FPC_01.py
@@ -32,9 +32,9 @@ def execute(report_id):
         'Text': 'CLO_FPC_01',
         'QuodFlatParameters': {
             'MaxParticipation': '10',
-            'PricePoint1Price': '200',
+            'PricePoint1Price': '118',
             'PricePoint1Participation': '12',
-            'PricePoint2Price': '202',
+            'PricePoint2Price': '117',
             'PricePoint2Participation': '14',
         }
     }
