ccfdbab9764abd047a06d979b768ac043bdeca26
Yaroslav <yaroslav.furmuzal17@gmail.com>
2021-09-22 09:38:15 +0300

    QAP-4845: Support changes in framework and tests. In general folder created common folder with files for describe the main page.

---
+++ quod_qa/web_admin/web_admin_core/pages/general/common/common_constants.py
@@ -0,0 +1,6 @@
+class CommonConstants:
+    DOWNLOAD_CSV_XPATH = '//*[@class="right size-small"]//a//*[@data-name="download"]'
+    REFRESH_PAGE_XPATH = '//*[@class="right size-small"]//a//*[@data-name="refresh"]'
+    SEND_FEEDBACK_BUTTON_XPATH = '//*[@nbtooltip="Send Feedback to Quod Financial"]'
+    SEND_FEEDBACK_TEXT_AREA_XPATH = '//*[@placeholder="Please share your feedback..."]'
+    SEND_FEEDBACK_SEND_BUTTON_XPATH = '//*[text()="Send"]'
