748fd7d711885bfcd6bf2a77c8c521f7c85b44cd
kkondratiev <kkon870@gmail.com>
2021-09-22 17:41:45 +0300

    2255

--- regression_cycle/fx_regression_cycle/fx_mm_AH_regression.py
+++ regression_cycle/fx_regression_cycle/fx_mm_AH_regression.py
@@ -1,4 +1,4 @@
-from quod_qa.fx.fx_mm_autohedging import QAP_2252, QAP_2113, QAP_2228, QAP_2250, QAP_2251, QAP_2290, QAP_2159
+from quod_qa.fx.fx_mm_autohedging import QAP_2252, QAP_2113, QAP_2228, QAP_2250, QAP_2251, QAP_2290, QAP_2159, QAP_2255
 from stubs import Stubs
 import logging
 from custom import basic_custom_actions as bca
@@ -29,6 +29,7 @@ def test_run(parent_id=None):
         QAP_2252.execute(report_id, session_id)
         QAP_2290.execute(report_id, session_id)
         QAP_2159.execute(report_id, session_id)
+        QAP_2255.execute(report_id, session_id)
     except Exception:
         logging.error("Error execution", exc_info=True)
 
