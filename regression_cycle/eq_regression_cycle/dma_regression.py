e86d9e9c86f15be4a9eab09e251d7fb324657e1f
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-09-29 17:36:15 +0300

    modify tests

--- regression_cycle/eq_regression_cycle/dma_regression.py
+++ regression_cycle/eq_regression_cycle/dma_regression.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from custom import basic_custom_actions as bca
 from quod_qa.eq.DMA import QAP_2001, QAP_2002, QAP_2003, QAP_2005, QAP_2551, QAP_4393, QAP_4375, QAP_2006, QAP_2007, \
-    QAP_2008, QAP_2522, QAP_3723
+    QAP_2008, QAP_2522, QAP_3723, QAP_2000
 from stubs import Stubs
 from win_gui_modules.utils import set_session_id
 
@@ -15,10 +15,10 @@ channels = dict()
 
 
 def test_run(parent_id=None):
-    report_id = bca.create_event('dma ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'), parent_id)
+    report_id = bca.create_event('DMA ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'), parent_id)
     session_id = set_session_id()
     try:
-        # QAP_2000.execute(report_id, session_id)
+        QAP_2000.execute(report_id, session_id)
         # QAP_2001.execute(report_id, session_id)
         # QAP_2002.execute(report_id, session_id)
         # QAP_2003.execute(report_id, session_id)
@@ -30,7 +30,7 @@ def test_run(parent_id=None):
         # QAP_2007.execute(report_id, session_id)
         ##QAP_2008.execute(report_id, session_id)
         ##QAP_2522.execute(report_id, session_id)
-        QAP_3723.execute(report_id, session_id)
+        #QAP_3723.execute(report_id, session_id)
 
 
 
