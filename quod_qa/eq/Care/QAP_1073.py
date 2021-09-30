07923ce5431cdbcaacfa1e90d0d60e83e9841741
ipalamar <ivan.palamarchuk.contractor@quodfinancial.com>
2021-09-30 09:46:04 +0300

    Edit QAP_1073.py

--- quod_qa/eq/Care/QAP_1073.py
+++ quod_qa/eq/Care/QAP_1073.py
@@ -1,13 +1,11 @@
 import logging
 
 import quod_qa.wrapper.eq_fix_wrappers
-from quod_qa.wrapper.fix_message import FixMessage
-from quod_qa.wrapper.fix_verifier import FixVerifier
-from custom.basic_custom_actions import create_event, timestamps
-import time
+from custom.basic_custom_actions import create_event
 from quod_qa.wrapper import eq_wrappers
+from quod_qa.wrapper.fix_verifier import FixVerifier
 from stubs import Stubs
-from win_gui_modules.utils import set_session_id, get_base_request
+from win_gui_modules.utils import get_base_request
 from win_gui_modules.wrappers import set_base
 
 logger = logging.getLogger(__name__)
@@ -21,9 +19,9 @@ def execute(report_id, session_id):
     # region Declarations
     qty = "900"
     price = "40"
-    newPrice = "1"
+    new_price = "1"
     lookup = "VETO"
-    client = "CLIENT_FIX_CARE"
+    client = "MOClient"
     # endregion
     # region Open FE
     case_id = create_event(case_name, report_id)
@@ -37,14 +35,12 @@ def execute(report_id, session_id):
     # region Create CO
     fix_message = quod_qa.wrapper.eq_fix_wrappers.create_order_via_fix(case_id, 3, 1, client, 2, qty, 0, price)
     eq_wrappers.accept_order(lookup, qty, price)
-    param_list = {'Price': newPrice}
+    param_list = {'Price': new_price}
     # region ManualExecute
     eq_wrappers.manual_execution(base_request, str(int((int(qty) / 2))), price)
     response = fix_message.pop('response')
-    # print(fix_message['Price'])
     # Amend fix order
-    fix_message1 = FixMessage(fix_message)
-    quod_qa.wrapper.eq_fix_wrappers.amend_order_via_fix(case_id, fix_message1, param_list, "PARIS_" + client)
+    quod_qa.wrapper.eq_fix_wrappers.amend_order_via_fix(case_id, fix_message, param_list)
     # endregion
     # region reject amend
     eq_wrappers.reject_order(lookup, qty, price)
@@ -75,22 +71,15 @@ def execute(report_id, session_id):
         'LastMkt': '*',
         'OrderCapacity': '*',
         'QtyType': '*',
-        'SettlDate': '*',
         'SettlType': '*',
         'NoParty': '*',
         'Instrument': '*',
         'header': '*',
         'LastCapacity': '*',
         'ExDestination': '*',
-        'GrossTradeAmt': '*'
+        'GrossTradeAmt': '*',
+        'ExpireDate': "*",
+        "VenueType": "*"
     }
     fix_verifier_ss = FixVerifier(quod_qa.wrapper.eq_fix_wrappers.get_sell_connectivity(), case_id)
     fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'ExecType', 'OrdStatus'])
-    fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'ExecType'])
-    fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'OrdStatus'])
-    fix_verifier_ss = FixVerifier(quod_qa.wrapper.eq_fix_wrappers.get_buy_connectivity(), case_id)
-    fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'ExecType'])
-    fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'OrdStatus'])
-    fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'ExecType'], direction="SECOND")
-    fix_verifier_ss.CheckExecutionReport(params, response, key_parameters=['ClOrdID', 'OrdStatus'], direction="SECOND")
-
