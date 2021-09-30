e317a579146d737d7e9dcebbafb6bbccd65093d7
ipalamar <ivan.palamarchuk.contractor@quodfinancial.com>
2021-09-30 08:35:14 +0300

    edit demo.py

--- demo.py
+++ demo.py
@@ -13,20 +13,13 @@ logger.setLevel(logging.INFO)
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('Yehor tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('ipalamar ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-
     session_id = set_session_id()
 
     try:
         # example_java_api.TestCase(report_id).execute()
-
-        QAP_3361.execute(report_id, session_id)
-        # rm = RuleManager()
-        # rm.print_active_rules()
-        # rm.remove_rules_by_id_list([2078, 2079, 2729, 2733])
-        # rm.print_active_rules()
-
+        QAP_1045.execute(report_id, session_id)
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
