8098129ceb890756f2f1d352ea1869a9a5797596
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-10-05 12:10:53 +0300

    Test cases updated

--- demo.py
+++ demo.py
@@ -1,12 +1,16 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
-from quod_qa.fx.fx_mm_autohedging import QAP_2292, QAP_2291, QAP_2265, QAP_3902, QAP_2470
-from quod_qa.fx.fx_mm_rfq import QAP_4748, QAP_4223, QAP_2103, QAP_2382, QAP_2296, QAP_2101, QAP_2091, QAP_5345
-from quod_qa.fx.fx_mm_esp import QAP_3661, QAP_4016, QAP_2750, QAP_4094, QAP_2844, QAP_3394
+from quod_qa.fx.fx_mm_autohedging import QAP_2292, QAP_2291, QAP_2265, QAP_3902, QAP_2470, QAP_2113, QAP_2250, QAP_2252, \
+    QAP_2251
+from quod_qa.fx.fx_mm_rfq import QAP_4748, QAP_4223, QAP_2103, QAP_2382, QAP_2296, QAP_2101, QAP_2091, QAP_5345, \
+    QAP_2055, QAP_2958, QAP_4777
+from quod_qa.fx.fx_mm_esp import QAP_3661, QAP_4016, QAP_2750, QAP_4094, QAP_2844, QAP_3394, QAP_1511, QAP_1589, \
+    QAP_2035, QAP_2556, QAP_4794
 from quod_qa.fx.fx_mm_rfq.interpolation import QAP_3734, QAP_3739, QAP_3689
 from MyFiles import MyTest, SendMD, Test, StringThing, send_rfq
-from quod_qa.fx.fx_taker_esp import QAP_2373, QAP_2761, QAP_2812, QAP_4768
+from quod_qa.fx.fx_taker_esp import QAP_2373, QAP_2761, QAP_2812, QAP_4768, QAP_1591, QAP_105, QAP_3742, QAP_4673, \
+    QAP_4677
 from quod_qa.fx.fx_taker_rfq import QAP_2836
 from rule_management import RuleManager
 from stubs import Stubs
@@ -51,55 +55,54 @@ def test_run():
     # rule_check()
     try:
         pass
-        # if not Stubs.frontend_is_open:
-        #     prepare_fe_2(report_id, session_id)
-        # else:
-        #     get_opened_fe(report_id, session_id)
-        # QAP_1591.execute(report_id, session_id)
-        # QAP_105.execute(report_id, session_id)
-        # QAP_1511.execute(report_id, session_id)
-        # QAP_1589.execute(report_id, session_id)
-        # QAP_2055.execute(report_id, session_id)
-        # QAP_3742.execute(report_id, session_id)
-        # QAP_4673.execute(report_id, session_id)
-        # QAP_4677.execute(report_id, session_id)
-        # QAP_2113.execute(report_id, session_id)
-        # QAP_2250.execute(report_id, session_id)
-        # QAP_2252.execute(report_id, session_id)
-        # QAP_2035.execute(report_id, session_id)
-        # QAP_2556.execute(report_id, session_id)
-        # QAP_4768.execute(report_id, session_id)
-        # QAP_4794.execute(report_id, session_id)
-        # QAP_4748.execute(report_id, session_id)
-        # QAP_2958.execute(report_id, session_id)
-        # QAP_2251.execute(report_id, session_id)
-        # QAP_4777.execute(report_id, session_id)
-        # QAP_4223.execute(report_id, session_id)
-        # QAP_3739.execute(report_id)
-        # QAP_3734.execute(report_id, session_id)
-        # QAP_3689.execute(report_id)
-        # QAP_2103.execute(report_id)
-        # QAP_2382.execute(report_id)
-        # QAP_3661.execute(report_id, session_id)
-        # QAP_4016.execute(report_id, session_id)
-        # QAP_2296.execute(report_id, session_id)
-        # QAP_2101.execute(report_id, session_id)
-        # QAP_2373.execute(report_id, session_id)
-        # QAP_2750.execute(report_id)
-        # QAP_4094.execute(report_id)
-        # QAP_2091.execute(report_id)
-        # QAP_5345.execute(report_id)
-        # QAP_2761.execute(report_id, session_id)
-        # QAP_2292.execute(report_id, session_id)
-        # QAP_2291.execute(report_id, session_id)
-        # QAP_2265.execute(report_id, session_id)
-        # QAP_2812.execute(report_id, session_id)
-        # QAP_2844.execute(report_id, session_id)
-        # QAP_3394.execute(report_id, session_id)
-        # QAP_2836.execute(report_id, session_id)
+        if not Stubs.frontend_is_open:
+            prepare_fe_2(report_id, session_id)
+        else:
+            get_opened_fe(report_id, session_id)
+        QAP_1591.execute(report_id, session_id)
+        QAP_105.execute(report_id, session_id)
+        QAP_1511.execute(report_id, session_id)
+        QAP_1589.execute(report_id, session_id)
+        QAP_2055.execute(report_id, session_id)
+        QAP_3742.execute(report_id, session_id)
+        QAP_4673.execute(report_id, session_id)
+        QAP_4677.execute(report_id, session_id)
+        QAP_2113.execute(report_id, session_id)
+        QAP_2250.execute(report_id, session_id)
+        QAP_2252.execute(report_id, session_id)
+        QAP_2035.execute(report_id, session_id)
+        QAP_2556.execute(report_id, session_id)
+        QAP_4768.execute(report_id, session_id)
+        QAP_4794.execute(report_id, session_id)
+        QAP_4748.execute(report_id, session_id)
+        QAP_2958.execute(report_id, session_id)
+        QAP_2251.execute(report_id, session_id)
+        QAP_4777.execute(report_id, session_id)
+        QAP_4223.execute(report_id, session_id)
+        QAP_3739.execute(report_id)
+        QAP_3734.execute(report_id, session_id)
+        QAP_3689.execute(report_id)
+        QAP_2103.execute(report_id)
+        QAP_2382.execute(report_id)
+        QAP_3661.execute(report_id, session_id)
+        QAP_4016.execute(report_id, session_id)
+        QAP_2296.execute(report_id, session_id)
+        QAP_2101.execute(report_id, session_id)
+        QAP_2373.execute(report_id, session_id)
+        QAP_2750.execute(report_id)
+        QAP_4094.execute(report_id)
+        QAP_2091.execute(report_id)
+        QAP_5345.execute(report_id)
+        QAP_2761.execute(report_id, session_id)
+        QAP_2292.execute(report_id, session_id)
+        QAP_2291.execute(report_id, session_id)
+        QAP_2265.execute(report_id, session_id)
+        QAP_2812.execute(report_id, session_id)
+        QAP_2844.execute(report_id, session_id)
+        QAP_3394.execute(report_id, session_id)
+        QAP_2836.execute(report_id, session_id)
         QAP_3902.execute(report_id, session_id)
         QAP_2470.execute(report_id, session_id)
-
 
         # region my test files
         # SendMD.execute(report_id)
@@ -109,6 +112,7 @@ def test_run():
         # StringThing.execute()
         # MyTest.execute(report_id, session_id)
         # endregion
+
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
