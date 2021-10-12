093175a58362bd17e0405c4f8aab63e0094b8819
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-10-12 18:43:17 +0300

    Cycle update

--- demo.py
+++ demo.py
@@ -1,13 +1,20 @@
 import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
-
-from quod_qa.fx.fx_taker_esp import QAP_5537_not_ready, QAP_5635_not_ready
-from quod_qa.fx.qs_fx_routine import SendMD
-from quod_qa.fx.ui_wrappers import wrapper_test
+from quod_qa.fx.fx_mm_autohedging import QAP_2292, QAP_2291, QAP_2265, QAP_3902, QAP_2470, QAP_2113, QAP_2250, QAP_2252, \
+    QAP_2251, QAP_3819, AH_Precondition, QAP_3147
+from quod_qa.fx.fx_mm_rfq import QAP_4748, QAP_4223, QAP_2103, QAP_2382, QAP_2296, QAP_2101, QAP_2091, QAP_5345, \
+    QAP_2055, QAP_2958, QAP_4777
+from quod_qa.fx.fx_mm_esp import QAP_3661, QAP_4016, QAP_2750, QAP_4094, QAP_2844, QAP_3394, QAP_1511, QAP_1589, \
+    QAP_2035, QAP_2556, QAP_4794
+from quod_qa.fx.fx_mm_rfq.interpolation import QAP_3734, QAP_3739, QAP_3689
+from MyFiles import MyTest, SendMD, Test, StringThing, send_rfq
+from quod_qa.fx.fx_taker_esp import QAP_2373, QAP_2761, QAP_2812, QAP_4768, QAP_1591, QAP_105, QAP_3742, QAP_4673, \
+    QAP_4677
+from quod_qa.fx.fx_taker_rfq import QAP_2836
 from rule_management import RuleManager
 from stubs import Stubs
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
+from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -16,111 +23,106 @@ timeouts = False
 
 channels = dict()
 
+
+def rule_creation():
+    rule_manager = RuleManager()
+    rfq_quote = rule_manager.add_RFQ('fix-bs-rfq-314-luna-standard')
+    rfq_trade = rule_manager.add_TRFQ('fix-bs-rfq-314-luna-standard')
+    # return [rfq_quote, rfq_trade]
+
+
+def rule_destroyer(list_rules):
+    if list_rules != None:
+        rule_manager = RuleManager()
+        for rule in list_rules:
+            rule_manager.remove_rule(rule)
+
+
+def rule_check():
+    rm = RuleManager()
+    rm.print_active_rules()
+    rm.print_active_rules_sim_test()
+
+
 def test_run():
     # Generation id and time for test run
-
-
-    report_id = bca.create_event('KKL    ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
-
+    report_id = bca.create_event('aleksey tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
-
-
-    test_cases = {
-        'case_id': bca.create_event_id(),
-        'TraderConnectivity': 'fix-ss-rfq-314-luna-standard',
-        'Account': 'Iridium1',
-        'SenderCompID': 'QUODFX_UAT',
-        'TargetCompID': 'QUOD5',
-    }
-
-    session_id=set_session_id()
+    session_id = set_session_id()
+    # rules = rule_creation()
+    start = datetime.now()
+    print(f'start time = {start}')
+    # rule_check()
     try:
-        # if not Stubs.frontend_is_open:
-        #     prepare_fe_2(report_id, session_id)
-        # else:
-        #     get_opened_fe(report_id, session_id)
-
-        # QAP_2290.execute(report_id, session_id)
-        #
-        # QAP_5635_not_ready.execute(report_id)
-        # wrapper_test.execute(report_id,session_id)
-        # QAP_5537_not_ready.execute(report_id)
-        # for_Daria.execute(report_id,session_id)
-        SendMD.execute(report_id)
-
-
-
-        # QAP_2092WIP.execute(report_id,session_id)
-        # QAP_1558.execute(report_id)
-        # QAP_2290.execute(report_id,session_id)
-        # QAP_2322.execute(report_id, session_id)
-        # java_api_MDReq.TestCase().execute(report_id)
-        # rfq_spot.execute(report_id)
-        # esp_1W.execute(report_id)
-        # java_api.TestCase(report_id).execute()
-        # java_api_Subscribe.TestCase().execute(report_id)
-        # example_java_api.TestCase(report_id).execute()
-
-
+        if not Stubs.frontend_is_open:
+            prepare_fe_2(report_id, session_id)
+        else:
+            get_opened_fe(report_id, session_id)
+        # QAP_1591.execute(report_id, session_id)
+        # QAP_105.execute(report_id, session_id)
+        # QAP_1511.execute(report_id, session_id)
+        # QAP_1589.execute(report_id, session_id)
+        # QAP_2055.execute(report_id, session_id)
+        # QAP_3742.execute(report_id, session_id)
+        # QAP_4673.execute(report_id, session_id)
+        # QAP_4677.execute(report_id, session_id)
+        # QAP_2113.execute(report_id, session_id)
+        # QAP_2250.execute(report_id, session_id)
+        # QAP_2252.execute(report_id, session_id)
+        # QAP_2035.execute(report_id, session_id)
+        # QAP_2556.execute(report_id, session_id)
+        # QAP_4768.execute(report_id, session_id)
+        # QAP_4794.execute(report_id, session_id)
+        # QAP_4748.execute(report_id, session_id)
+        # QAP_2958.execute(report_id, session_id)
+        # QAP_2251.execute(report_id, session_id)
+        # QAP_4777.execute(report_id, session_id)
+        # QAP_4223.execute(report_id, session_id)
+        # QAP_3739.execute(report_id)
+        # QAP_3734.execute(report_id, session_id)
         # QAP_3689.execute(report_id)
-        # QAP_2353.execute(report_id)
-        # QAP_3806.execute(report_id)
+        # QAP_2103.execute(report_id)
+        # QAP_2382.execute(report_id)
+        # QAP_3661.execute(report_id, session_id)
+        # QAP_4016.execute(report_id, session_id)
+        # QAP_2296.execute(report_id, session_id)
+        # QAP_2101.execute(report_id, session_id)
+        # QAP_2373.execute(report_id, session_id)
+        # QAP_2750.execute(report_id)
+        # QAP_4094.execute(report_id)
+        # QAP_2091.execute(report_id)
+        # QAP_5345.execute(report_id)
+        # QAP_2761.execute(report_id, session_id)
+        # QAP_2292.execute(report_id, session_id)
+        # QAP_2291.execute(report_id, session_id)
+        # QAP_2265.execute(report_id, session_id)
+        # QAP_2812.execute(report_id, session_id)
+        # QAP_2844.execute(report_id, session_id)
+        # QAP_3394.execute(report_id, session_id)
+        # QAP_2836.execute(report_id, session_id)
+        # QAP_3902.execute(report_id, session_id)
+        QAP_2470.execute(report_id, session_id)
+        # QAP_3819.execute(report_id, session_id)
+        # QAP_3147.execute(report_id, session_id)
 
-        # QAP_3747.execute(report_id)
-
-
-        # QAP_3805.execute(report_id)
-        # QAP_3689.execute(report_id)
-
-
-        rm = RuleManager()
-
-        # rm.add_TRFQ('fix-bs-rfq-314-luna-standard')
-        # rm.print_active_rules()
-        # rm.print_active_rules_sim_test()
-
-
-
-        # rm.remove_rule_by_id(4)
-        # rm.remove_rule_by_id(5)
-
-        # rm.remove_rule_by_id(10)
-        # rm.remove_rule_by_id_test_sim(4)
-        # rm.remove_rule_by_id_test_sim(3)
-        # # # rm.add_RFQ('fix-bs-rfq-314-luna-standard')
-        # rm.add_fx_md_to('fix-fh-q-314-luna')
-        # rm.add_fx_md_to_test_sim('fix-fh-314-luna')
-        # rm.add_fx_md_to_test_sim('fix-fh-q-314-luna')
-        # rm.add_TRFQ_test_sim('fix-bs-rfq-314-luna-standard')
-        # rm.add_TRFQ('fix-bs-rfq-314-luna-standard')
-        # # rm.add_RFQ('fix-bs-rfq-314-luna-standard')
-        # # rm.remove_rule_by_id(574)
-        # rm.print_active_rules()
-
+        # region my test files
+        # SendMD.execute(report_id)
+        # send_rfq.execute(report_id)
+        # Test.execute(report_id, session_id)
+        # ui_tests.execute(report_id, session_id)
+        # StringThing.execute()
+        # MyTest.execute(report_id, session_id)
+        # AH_Precondition.execute(report_id)
+        # endregion
 
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
         Stubs.win_act.unregister(session_id)
+        print('duration time = ' + str(datetime.now() - start))
 
-#
-   #     rm.print_active_rules()
-#
-    #   ui_tests.execute(report_id)
-
-    #except Exception:
-        #logging.error("Error execution", exc_info=True)
 
 if __name__ == '__main__':
     logging.basicConfig()
     test_run()
     Stubs.factory.close()
-
-
-
-
-
-
-
-
