0aec5718a23294b54aa8b0ce32eebd78bd987d06
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-10-18 13:06:15 +0300

    modify eq wrappers

--- quod_qa/win_gui_wrappers/equity/eq_order_book.py
+++ quod_qa/win_gui_wrappers/equity/eq_order_book.py
@@ -6,14 +6,13 @@ from win_gui_modules.order_book_wrappers import OrdersDetails, OrderInfo, Cancel
 class EQOrderBook(BaseOrderBook):
     def __init__(self, case_id, base_request):
         super().__init__(case_id, base_request)
-        self.order_details = OrdersDetails()
         self.order_info = OrderInfo()
-        self.set_order_details()
-        self.grpc_call_cancel_order = None
+        self.order_details = OrdersDetails()
+        self.cancel_order_details = CancelOrderDetails()
+        self.get_orders_details_call = Stubs.win_act_order_book.getOrdersDetails
+        self.cancel_order_call = Stubs.win_act_order_book.cancelOrder
 
     def cancel_order(self, cancel_children: bool):
-        self.grpc_call = Stubs.win_act_order_book.cancelOrder
-        self.request_details = CancelOrderDetails()
         super().cancel_order(cancel_children)
 
 
