b326d1f81e7795b15670192a742b71e41e247864
ostronov <oleksandr.stronov>
2021-10-20 14:47:14 +0300

    Get files from mzuban

---
+++ quod_qa/wrapper_test/FixMessageOrderCancelRequest.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from custom import basic_custom_actions
+from quod_qa.wrapper_test.FixMessageNewOrderSingle import FixMessage, FixMessageNewOrderSingle
+
+
+class FixMessageOrderCancelRequest(FixMessage):
+
+    def __init__(self, new_order_single: FixMessageNewOrderSingle = None, parameters: dict = None):
+        super().__init__(message_type="F")
+        if new_order_single is not None:
+            self.update_fix_message(new_order_single.get_parameters())
+        super().change_parameters(parameters)
+
+    def update_fix_message(self, parameters: dict) -> None:
+        temp = dict(
+            OrigClOrdID=basic_custom_actions.client_orderid(9),
+            ClOrdID=parameters["ClOrdID"],
+            Account=parameters["Account"],
+            Side=parameters["Side"],
+            TransactTime=datetime.utcnow().isoformat(),
+        )
+        super().change_parameters(temp)
