1f3ee93586caa80bb712f30ebd3aac8bcd686acd
ostronov <oleksandr.stronov>
2021-10-21 17:44:24 +0300

    update example

--- quod_qa/fx/ui_wrappers/wrapper_test.py
+++ quod_qa/fx/ui_wrappers/wrapper_test.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 from custom import basic_custom_actions as bca
+from quod_qa.win_gui_wrappers.columns_names import OrderBookColumns
 from quod_qa.win_gui_wrappers.forex.fx_order_book import FXOrderBook
 from quod_qa.win_gui_wrappers.forex.fx_quote_book import FXQuoteBook
 from quod_qa.win_gui_wrappers.forex.fx_quote_request_book import FXQuoteRequestBook
@@ -17,14 +18,17 @@ def execute(report_id, session_id):
 
     try:
 
-        # order_book = FXOrderBook(case_id, case_base_request)
-        # order_book.set_filter(["Qty", "1000000"]).check_order_fields_list({"Sts": "1", "ExecSts": "2"})
+        order_book = FXOrderBook(case_id, case_base_request)
+        order_book.set_filter([OrderBookColumns.qty.value, "1000000"]).check_order_fields_list(
+            {"Sts": "1", "ExecSts": "2"})
+        order_book.cancel_order()
 
         quote_book = FXQuoteBook(case_id, case_base_request)
-        quote_book.set_filter(["BidSize", "2000000"]).check_quote_book_fields_list({"Bid Px": "1.181161"})
+        quote_book.set_filter(["BidSize", "2000000"]).check_quote_book_fields_list({"BidPx": "1.181161"})
 
         quote_request_book = FXQuoteRequestBook(case_id, case_base_request)
-        quote_request_book.set_filter(["Qty", "2000000"]).check_quote_book_fields_list({"User":"ostronov", "Status": "Terminated"})
+        quote_request_book.set_filter(["Qty", "2000000"]).check_quote_book_fields_list(
+            {"User": "ostronov", "Status": "Terminated"})
 
     except Exception:
         logging.error("Error execution", exc_info=True)
