ff07ff3b64605f089e2f269930059513a4d712ba
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-10-22 15:24:29 +0300

    QAP-2293 added

--- quod_qa/fx/fx_mm_autohedging/QAP_2293.py
+++ quod_qa/fx/fx_mm_autohedging/QAP_2293.py
@@ -1,6 +1,5 @@
 import random
 import time
-from datetime import timedelta
 from custom.tenor_settlement_date import spo
 from custom.verifier import Verifier, VerificationMethod
 from quod_qa.fx.fx_wrapper.CaseParamsSellRfq import CaseParamsSellRfq
@@ -15,15 +14,16 @@ from win_gui_modules.utils import get_base_request, call
 import logging
 from pathlib import Path
 
+
 api = Stubs.api_service
-ttl_default = 120
-ttl_test = 10
 tif_default = 'DAY'
-tif_test = 'GTD'
+tif_test = 'FOK'
+test_id = 400018
+test_fake_id = 400024
 status_eliminated = "Eliminated"
 
 
-#FIX params
+# FIX params
 client = 'Osmium1'
 client_tier = 'Osmium'
 account_client = "Osmium1_1"
@@ -38,7 +38,7 @@ side = "1"
 qty = str(random.randint(3000000, 4000000))
 
 
-def set_send_hedge_order(case_id, ttl, tif):
+def set_send_hedge_order(case_id, strategy, tif):
     modify_params = {
         "autoHedgerName": "OsmiumAH",
         "hedgeAccountGroupID": "QUOD3",
@@ -58,12 +58,12 @@ def set_send_hedge_order(case_id, ttl, tif):
                 "crossCurrPairHedgingPolicy": "DIR",
                 "useSameLongShortQty": "true",
                 "hedgingStrategy": "POS",
-                "algoPolicyID": 400018,
+                "algoPolicyID": strategy,
                 "shortLowerQty": 0,
                 "shortUpperQty": 0,
                 "timeInForce": tif,
                 "sendHedgeOrders": 'true',
-                "exposureDuration": ttl,
+                "exposureDuration": '120',
                 "hedgeOrderDestination": "EXT"
             }
 
@@ -95,7 +95,7 @@ def compare_position(even_name, case_id, expected_pos, actual_pos):
     verifier.verify()
 
 
-def check_order_book(case_id, base_request, act_ob, o_id, ord_number, expire_time):
+def check_order_book(case_id, base_request, act_ob, o_id, ord_number):
     ob = OrdersDetails()
     extraction_id = bca.client_orderid(4)
     ob.set_default_params(base_request)
@@ -105,10 +105,10 @@ def check_order_book(case_id, base_request, act_ob, o_id, ord_number, expire_tim
     ob.set_filter(["Qty", qty])
     ob_id = ExtractionDetail("orderBook.id", "Order ID")
     ob_sts = ExtractionDetail("orderBook.sts", "Sts")
-    ob_expire = ExtractionDetail("orderBook.expire", "ExpireTime")
+    ob_qty = ExtractionDetail("orderBook.qty", "Qty")
     order_info = OrderInfo.create(
         action=ExtractionAction.create_extraction_action(
-            extraction_details=[ob_sts, ob_expire, ob_id]))
+            extraction_details=[ob_sts, ob_qty, ob_id]))
     order_info.set_number(ord_number)
     ob.add_single_order_info(order_info)
 
@@ -117,44 +117,17 @@ def check_order_book(case_id, base_request, act_ob, o_id, ord_number, expire_tim
     verifier = Verifier(case_id)
     verifier.set_event_name(f'Checking eliminated AH orders (#{ord_number})')
     verifier.compare_values("Sts", status_eliminated, response[ob_sts.name])
-    verifier.compare_values("Expire Time", response[ob_expire.name], expire_time, VerificationMethod.NOT_EQUALS)
-    if expire_time is not None:
-        timedelta_current_order = timedelta(
-            hours=float(response[ob_expire.name].split(' ')[1].split(':')[0]),
-            minutes=float(response[ob_expire.name].split(' ')[1].split(':')[1]),
-            seconds=float(response[ob_expire.name].split(' ')[1].split(':')[2])
-        )
-        timedelta_previous_order = timedelta(
-            hours=float(expire_time.split(' ')[1].split(':')[0]),
-            minutes=float(expire_time.split(' ')[1].split(':')[1]),
-            seconds=float(expire_time.split(' ')[1].split(':')[2])
-        )
-        difference = timedelta_previous_order - timedelta_current_order
-        verifier.compare_values("Difference (sec)", str(difference.seconds), '10')
-    verifier.compare_values("Checking that it is not previous order", response[ob_id.name], o_id, VerificationMethod.NOT_EQUALS)
+    verifier.compare_values("Qty", qty, response[ob_qty.name].repalce(',', ''))
+    verifier.compare_values("Checking that it is not previous order", response[ob_id.name], o_id,
+                            VerificationMethod.NOT_EQUALS)
     verifier.verify()
-    return response[ob_id.name], response[ob_expire.name]
+    return response[ob_id.name]
 
 
-def check_order_book_not_ah(case_id, base_request, act_ob, o_id, ord_number):
-    ob = OrdersDetails()
-    extraction_id = bca.client_orderid(4)
-    ob.set_default_params(base_request)
-    ob.set_extraction_id(extraction_id)
-    ob.set_filter(["Order ID", 'AO'])
-    ob.set_filter(["Orig", 'AutoHedger'])
-    ob_id = ExtractionDetail("orderBook.ID", "Order ID")
-    ob_qty = ExtractionDetail("orderBook.Qty", "Qty")
-    order_info = OrderInfo.create(
-        action=ExtractionAction.create_extraction_action(
-            extraction_details=[ob_id, ob_qty]))
-    order_info.set_number(ord_number)
-    ob.add_single_order_info(order_info)
-    response = call(act_ob.getOrdersDetails, ob.request())
+def check_order_number(case_id, number_of_orders):
     verifier = Verifier(case_id)
-    verifier.set_event_name(f"Checking that next ({ord_number}) order is from another AutoHedger")
-    verifier.compare_values("Checking that it is not previous order", response[ob_id.name], o_id, VerificationMethod.NOT_EQUALS)
-    verifier.compare_values("Qty", response[ob_qty.name].replace(',', ''), qty, VerificationMethod.NOT_EQUALS)
+    verifier.set_event_name(f"Checking that next 5 orders were Eliminated")
+    verifier.compare_values("Number of orders", '5', str(number_of_orders))
     verifier.verify()
 
 
@@ -164,10 +137,9 @@ def execute(report_id, session_id):
     case_base_request = get_base_request(session_id, case_id)
     ob_act = Stubs.win_act_order_book
     pos_service = Stubs.act_fx_dealing_positions
-    expire_time = None
     try:
         # Step 1
-        set_send_hedge_order(case_id, ttl_test, tif_test)
+        set_send_hedge_order(case_id, test_fake_id, tif_test)
 
         params_spot = CaseParamsSellRfq(client, case_id, orderqty=qty, symbol=symbol,
                                         securitytype=security_type_spo, settldate=settle_date_spo,
@@ -187,24 +159,21 @@ def execute(report_id, session_id):
         compare_position('Checking positions Client Osmium1_1', case_id, qty, actual_pos_client)
         actual_pos_quod = get_dealing_positions_details(pos_service, case_base_request, symbol, account_quod)
         compare_position('Checking positions Quod QUOD3_1', case_id, f'-{qty}', actual_pos_quod)
-        time.sleep(60)
+        time.sleep(20)
 
         # Step 3-6
         row = 1
         order_id = ''
-        while row <= 6:
-            if row == 6:
-                check_order_book_not_ah(case_id, case_base_request, ob_act, order_id, row)
-            else:
-                order_id, expire_time = check_order_book(case_id, case_base_request, ob_act, order_id, row, expire_time)
+        while row < 6:
+            order_id = check_order_book(case_id, case_base_request, ob_act, order_id, row)
             row += 1
-
+        check_order_number(case_id, row-1)
     except Exception as e:
         logging.error('Error execution', exc_info=True)
         bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
     finally:
         try:
-            set_send_hedge_order(case_id, ttl_default, tif_default)
+            set_send_hedge_order(case_id, test_id, tif_default)
 
             params_spot = CaseParamsSellRfq(client, case_id, orderqty=qty, symbol=symbol,
                                             securitytype=security_type_spo, settldate=settle_date_spo,
