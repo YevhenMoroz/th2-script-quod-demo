cb6070e8f216e1dcda63ba09c32f1d5377cdf7f4
ostronov <oleksandr.stronov>
2021-11-01 10:06:15 +0200

    Update for new wrappers

--- quod_qa/fx/fx_taker_rfq/QAP_568.py
+++ quod_qa/fx/fx_taker_rfq/QAP_568.py
@@ -1,49 +1,17 @@
 import logging
 from pathlib import Path
 from custom import basic_custom_actions as bca
-from custom.verifier import Verifier
 from quod_qa.fx.fx_wrapper.common_tools import random_qty
 from quod_qa.win_gui_wrappers.base_window import decorator_try_except
+from quod_qa.win_gui_wrappers.data_set import Side
 from quod_qa.win_gui_wrappers.forex.fx_order_book import FXOrderBook
 from quod_qa.win_gui_wrappers.forex.fx_quote_book import FXQuoteBook
 from quod_qa.win_gui_wrappers.forex.fx_quote_request_book import FXQuoteRequestBook
+from quod_qa.win_gui_wrappers.forex.rfq_tile import RFQTile
 from stubs import Stubs
-from win_gui_modules.aggregated_rates_wrappers import RFQTileOrderSide, PlaceRFQRequest, ModifyRFQTileRequest, \
-    ContextAction
-from win_gui_modules.common_wrappers import BaseTileDetails
 from win_gui_modules.layout_panel_wrappers import OptionOrderTicketRequest, DefaultFXValues
 from win_gui_modules.utils import get_base_request, call
 from win_gui_modules.wrappers import set_base
-
-
-def create_or_get_rfq(base_request, service):
-    call(service.createRFQTile, base_request.build())
-
-
-def send_rfq(base_request, service):
-    call(service.sendRFQOrder, base_request.build())
-
-
-def modify_rfq_tile(base_request, service, qty, cur1, cur2, client, tenor, venue):
-    modify_request = ModifyRFQTileRequest(details=base_request)
-    modify_request.set_quantity_as_string(qty)
-    modify_request.set_from_currency(cur1)
-    modify_request.set_to_currency(cur2)
-    modify_request.set_near_tenor(tenor)
-    modify_request.set_client(client)
-    action = ContextAction.create_venue_filter(venue)
-    modify_request.add_context_action(action)
-    call(service.modifyRFQTile, modify_request.build())
-
-
-def place_order_tob(base_request, service):
-    rfq_request = PlaceRFQRequest(details=base_request)
-    rfq_request.set_action(RFQTileOrderSide.SELL)
-    call(service.placeRFQOrder, rfq_request.build())
-
-
-def cancel_rfq(base_request, service):
-    call(service.cancelRFQ, base_request.build())
 
 
 def set_order_ticket_options(option_service, base_request, client):
@@ -56,7 +24,6 @@ def set_order_ticket_options(option_service, base_request, client):
 
 # @decorator_try_except(test_id=Path(__file__).name[:-3])
 def execute(report_id, session_id):
-    ar_service = Stubs.win_act_aggregated_rates_service
     option_service = Stubs.win_act_options
 
     case_name = Path(__file__).name[:-3]
@@ -70,28 +37,29 @@ def execute(report_id, session_id):
     quote_sts_new = "New"
     quote_sts_accepted = "Accepted"
     quote_owner = Stubs.custom_config['qf_trading_fe_user']
+    sell_side = Side.sell.value
 
     # Create sub-report for case
     case_id = bca.create_event(case_name, report_id)
     set_base(session_id, case_id)
     case_base_request = get_base_request(session_id, case_id)
-    base_rfq_details = BaseTileDetails(base=case_base_request)
 
     try:
         # Step 1-3
         set_order_ticket_options(option_service, case_base_request, case_client)
         # Step 4
-        create_or_get_rfq(base_rfq_details, ar_service)
-        modify_rfq_tile(base_rfq_details, ar_service, case_qty, case_from_currency, case_to_currency,
-                        case_client, case_near_tenor, case_venue)
-        send_rfq(base_rfq_details, ar_service)
+        rfq_tile = RFQTile(case_id, case_base_request)
+        rfq_tile.crete_tile().modify_rfq_tile(from_cur=case_from_currency, to_cur=case_to_currency,
+                                              near_qty=case_qty, near_tenor=case_near_tenor,
+                                              client=case_client, single_venue=case_venue)
+        rfq_tile.send_rfq()
         quote_request_book = FXQuoteRequestBook(case_id, case_base_request)
         quote_request_book.set_filter(["Qty", case_qty])
         quote_request_book.check_quote_book_fields_list({"Venue": case_venue,
                                                          "Status": quote_sts_new,
                                                          "QuoteStatus": quote_sts_accepted})
         # Step 5
-        place_order_tob(base_rfq_details, ar_service)
+        rfq_tile.place_order(sell_side)
 
         quote_id = FXOrderBook(case_id, case_base_request).set_filter(["Qty", "2368459"]).extract_field("QuoteID")
 
@@ -108,6 +76,6 @@ def execute(report_id, session_id):
     finally:
         try:
             # Close tile
-            call(ar_service.closeRFQTile, base_rfq_details.build())
+            rfq_tile.close_tile()
         except Exception:
             logging.error("Error execution", exc_info=True)
