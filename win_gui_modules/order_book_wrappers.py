182aa11d4bdf834b0c6ef346480d49c2206878ad
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-02 11:36:08 +0200

    modify order_book_wrappers.py

--- win_gui_modules/order_book_wrappers.py
+++ win_gui_modules/order_book_wrappers.py
@@ -1,10 +1,9 @@
 from th2_grpc_act_gui_quod.care_orders_pb2 import TransferPoolDetails
 from th2_grpc_act_gui_quod.common_pb2 import EmptyRequest
 from th2_grpc_act_gui_quod.order_book_pb2 import ExtractManualCrossValuesRequest
-
-from .order_ticket import OrderTicketDetails, FXOrderDetails
+from th2_grpc_act_gui_quod import basket_book_pb2
 from th2_grpc_act_gui_quod import order_book_pb2, order_book_fx_pb2, ar_operations_pb2, care_orders_pb2
-from .order_ticket import OrderTicketDetails, FXOrderDetails
+from order_ticket import OrderTicketDetails, FXOrderDetails
 from th2_grpc_act_gui_quod import order_book_pb2, order_book_fx_pb2
 from dataclasses import dataclass
 
@@ -997,6 +996,41 @@ class SecondLevelTabDetails:
 
     def build(self):
         return self.request
+
+
+class ExtractOrderDataDetails:
+    def __init__(self):
+        self._request = basket_book_pb2.ExtractOrderDataDetails()
+
+    def set_default_params(self, base_request):
+        self._request.base.CopyFrom(base_request)
+
+    def set_filter(self, filter: dict):
+        self._request.filter.update(filter)
+
+    def set_column_names(self, column_names: list):
+        for name in column_names:
+            self._request.columnNames.append(name)
+
+    def build(self):
+        return self._request
+
+
+class ExtractChildOrderDataDetails:
+    def __init__(self, extract_order_data: ExtractOrderDataDetails = None, rows_number: int = None):
+        self._request = basket_book_pb2.ExtractChildOrderDataDetails()
+        self._request.extractDetails.CopyFrom(extract_order_data)
+        self._request.rowsNumber = rows_number
+
+    def set_extract_order_data(self, extract_order_data: ExtractOrderDataDetails):
+        self._request.extractDetails.CopyFrom(extract_order_data)
+
+    def set_rows_number(self, rows_number: int):
+        self._request.rowsNumber = rows_number
+
+    def build(self):
+        return self._request
+
 
 # class QuoteRequestDetails:
 #     def __init__(self):
