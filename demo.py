31346c2dd85256e3fa63a757cd13fdc13dabdbf5
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-11-02 10:48:13 +0200

    Merge

--- demo.py
+++ demo.py
@@ -1,30 +1,20 @@
 import logging
 from datetime import datetime
-
-import dealer_ineterv
 from custom import basic_custom_actions as bca
-from quod_qa.fx import ui_tests
-from quod_qa.fx.fx_mm_autohedging import QAP_2228, QAP_2290, QAP_2250, QAP_3146, QAP_3147, QAP_4122, QAP_2470
-from quod_qa.fx.fx_mm_esp import QAP_1418, QAP_4094, QAP_2082, QAP_2078, QAP_2797, QAP_1518, QAP_2825, QAP_1558, \
-    QAP_1559, QAP_2966, QAP_1599, QAP_2750, QAP_3661, QAP_1643_wip, QAP_2050, QAP_2051, QAP_3141, QAP_2876
-from quod_qa.fx.fx_mm_positions import QAP_1898, QAP_2500, import_position_layout, QAP_1897
-from quod_qa.fx.fx_mm_rfq import QAP_1552, QAP_1539, QAP_2091, QAP_2101, QAP_2104, QAP_2105, QAP_2295, QAP_2296, \
-    QAP_2297, QAP_2958, QAP_1746, QAP_1540, QAP_1562, QAP_1563, QAP_1970, QAP_2103, QAP_2177, QAP_3565, QAP_2877, \
-    QAP_4228, QAP_4085, QAP_3106, QAP_3107, QAP_3108, QAP_3109, QAP_2382, QAP_3110, QAP_3111, QAP_3112, QAP_3113, \
-    QAP_3234, QAP_3250, QAP_1978, QAP_3409, QAP_3494, QAP_2353, QAP_3704, QAP_3003, QAP_4509, QAP_4510, QAP_2090, \
-    QAP_2345, QAP_4777, QAP_2092, QAP_2294, QAP_2489, QAP_2490, for_test_77679
-from quod_qa.fx.fx_mm_rfq.interpolation import QAP_3766, QAP_3805, QAP_3747
-from quod_qa.fx.fx_mm_rfq.rejection import QAP_3735, QAP_3740
-from quod_qa.fx.fx_taker_esp import QAP_2949, QAP_3157, QAP_3414, QAP_2373, QAP_3415, QAP_3418, QAP_3694, QAP_3140
-from quod_qa.fx.fx_taker_rfq import QAP_2826, QAP_3048, QAP_3002, QAP_568, QAP_848
-from quod_qa.fx.fx_wrapper.common_tools import read_median_file
-from quod_qa.fx.my_methods import send_rfq, send_md, sequence_test
-from quod_qa.fx.ui_wrappers import wrapper_test
-
+from quod_qa.fx.fx_mm_autohedging import QAP_2292, QAP_2291, QAP_2265, QAP_3902, QAP_2470, QAP_2113, QAP_2250, QAP_2252, \
+    QAP_2251, QAP_3819, AH_Precondition, QAP_3147, QAP_2322, QAP_3039, QAP_3082, import_AH_layout, QAP_2293, QAP_4149
+from quod_qa.fx.fx_mm_rfq import QAP_4748, QAP_4223, QAP_2103, QAP_2382, QAP_2296, QAP_2101, QAP_2091, QAP_5345, \
+    QAP_2055, QAP_2958, QAP_4777, QAP_4509
+from quod_qa.fx.fx_mm_esp import QAP_3661, QAP_4016, QAP_2750, QAP_4094, QAP_2844, QAP_3394, QAP_1511, QAP_1589, \
+    QAP_2035, QAP_2556, QAP_4794, QAP_1599, QAP_3563
+from quod_qa.fx.fx_mm_rfq.interpolation import QAP_3734, QAP_3739, QAP_3689, QAP_3747
+from MyFiles import MyTest, SendMD, Test, StringThing, send_rfq
+from quod_qa.fx.fx_taker_esp import QAP_2373, QAP_2761, QAP_2812, QAP_4768, QAP_1591, QAP_105, QAP_3742, QAP_4673, \
+    QAP_4677
+from quod_qa.fx.fx_taker_rfq import QAP_2836
 from rule_management import RuleManager
-
 from stubs import Stubs
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
+from win_gui_modules.utils import set_session_id, get_base_request, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -48,66 +38,50 @@ def rule_destroyer(list_rules):
             rule_manager.remove_rule(rule)
 
 
+def rule_check():
+    rm = RuleManager()
+    rm.print_active_rules()
+    # rm.print_active_rules_sim_test()
+
+
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('ostronov tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('aleksey tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
-
     session_id = set_session_id()
     # rules = rule_creation()
+    start = datetime.now()
+    print(f'start time = {start}')
+    rule_check()
     try:
-        start = datetime.now()
-        print(f'start time = {start}')
-        case_params = {
-            'case_id': bca.create_event_id(),
-            'TraderConnectivity': 'fix-ss-rfq-314-luna-standard',
-            'Account': 'Iridium1',
-            'SenderCompID': 'QUODFX_UAT',
-            'TargetCompID': 'QUOD9',
-        }
-        #
-        if not Stubs.frontend_is_open:
-            prepare_fe_2(report_id, session_id)
-        else:
-            get_opened_fe(report_id, session_id)
+        # pass
+        # if not Stubs.frontend_is_open:
+        #     prepare_fe_2(report_id, session_id)
+        # else:
+        #     get_opened_fe(report_id, session_id)
+        # import_AH_layout.execute(report_id, session_id)
+        # QAP_3082.execute(report_id, session_id)
+        # QAP_2265.execute(report_id, session_id)
+        # QAP_2296.execute(report_id, session_id)
+        # QAP_2293.execute(report_id, session_id)
+        # QAP_4149.execute(report_id, session_id)
 
-        # rm = RuleManager()
-        # rm.remove_rule_by_id(12)
-        # # rm.add_fx_md_to("fix-fh-314-luna")
-        # rm.print_active_rules()
-         # Add scripts
-
+        # region my test files
+        SendMD.execute(report_id)
         # send_rfq.execute(report_id)
-        # QAP_568.execute(report_id, session_id)
-        # dealer_ineterv.execute(report_id, session_id)
-        # QAP_2489.execute(report_id ,session_id)
-        # send_rfq.execute(report_id)
-        # QAP_2490.execute(report_id ,session_id)
-        # QAP_3107.execute(report_id, session_id)
-        # QAP_2294.execute(report_id, session_id)
-        # send_md.execute(report_id)
-        # QAP_2050.execute(report_id, session_id)
-        # QAP_2051.execute(report_id, session_id)
-        # QAP_3141.execute(report_id)
-        # QAP_2876.execute(report_id)
-        QAP_568.execute(report_id, session_id)
-        # for_test_77679.execute(report_id, session_id)
-        # QAP_2345.execute(report_id)
-        # QAP_848.execute(report_id, session_id)
+        # Test.execute(report_id, session_id)
         # ui_tests.execute(report_id, session_id)
-        # wrapper_test.execute(report_id, session_id)
-        # QAP_2750.execute(report_id)
-        # QAP_3140.execute(report_id, session_id)
-        # QAP_2050.execute(report_id, session_id)
-        # QAP_3694.execute(report_id, session_id)
-
-        print('duration time = ' + str(datetime.now() - start))
+        # StringThing.execute()
+        # MyTest.execute(report_id)
+        # AH_Precondition.execute(report_id)
+        # import_AH_layout.execute(report_id, session_id)
+        # endregion
 
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
         Stubs.win_act.unregister(session_id)
+        print('duration time = ' + str(datetime.now() - start))
 
 
 if __name__ == '__main__':
