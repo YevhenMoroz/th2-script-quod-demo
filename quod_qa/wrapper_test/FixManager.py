2574d6582a4cf1ca16b55ce0432e39e18071da0b
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-08 18:35:32 +0200

    Replace get_message_type to enum

--- quod_qa/wrapper_test/FixManager.py
+++ quod_qa/wrapper_test/FixManager.py
@@ -1,16 +1,14 @@
-from re import match
-
 from th2_grpc_act_fix_quod.act_fix_pb2 import PlaceMessageRequest
 
 from custom import basic_custom_actions
+from quod_qa.wrapper_test.DataSet import MessageType
 from quod_qa.wrapper_test.FixMessage import FixMessage
-from quod_qa.wrapper_test.FixMessageNewOrderSingle import FixMessageNewOrderSingle
 from stubs import Stubs
 
 
 class FixManager:
 
-    def __init__(self, session_alias, case_id = None):
+    def __init__(self, session_alias, case_id=None):
         self.simulator = Stubs.simulator
         self.act = Stubs.fix_act
         self.__session_alias = session_alias
@@ -27,31 +25,31 @@ class FixManager:
             ))
 
     def send_message_and_receive_response(self, fix_message: FixMessage) -> PlaceMessageRequest:
-        if fix_message.get_message_type() == "D":
+        if fix_message.get_message_type() == MessageType.NewOrderSingle.value:
             response = self.act.placeOrderFIX(
                 request=basic_custom_actions.convert_to_request(
                     "Send NewOrderSingle",
                     self.__session_alias,
                     self.__case_id,
-                    basic_custom_actions.message_to_grpc('NewOrderSingle', fix_message.get_parameters(),
+                    basic_custom_actions.message_to_grpc(MessageType.NewOrderSingle.value, fix_message.get_parameters(),
                                                          self.__session_alias)
                 ))
-        elif fix_message.get_message_type() == "G":
+        elif fix_message.get_message_type() == MessageType.OrderCancelReplaceRequest.value:
             response = self.act.placeOrderReplaceFIX(
                 request=basic_custom_actions.convert_to_request(
                     "Send OrderCancelReplaceRequest",
                     self.__session_alias,
                     self.__case_id,
-                    basic_custom_actions.message_to_grpc('OrderCancelReplaceRequest', fix_message.get_parameters(),
+                    basic_custom_actions.message_to_grpc(MessageType.OrderCancelReplaceRequest.value, fix_message.get_parameters(),
                                                          self.__session_alias)
                 ))
-        elif fix_message.get_message_type() == "F":
+        elif fix_message.get_message_type() == MessageType.OrderCancelRequest.value:
             response = self.act.placeOrderCancelFIX(
                 request=basic_custom_actions.convert_to_request(
                     "Send OrderCancelRequest",
                     self.__session_alias,
                     self.__case_id,
-                    basic_custom_actions.message_to_grpc('OrderCancelRequest', fix_message.get_parameters(),
+                    basic_custom_actions.message_to_grpc(MessageType.OrderCancelRequest.value, fix_message.get_parameters(),
                                                          self.__session_alias)
                 ))
         else:
