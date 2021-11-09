01fbbc9df8a5643d3f8808abecc1bdab225e6dc5
ostronov <oleksandr.stronov>
2021-11-09 09:29:27 +0200

    Update base tile, add creation base_request inside class

--- quod_qa/win_gui_wrappers/base_tile.py
+++ quod_qa/win_gui_wrappers/base_tile.py
@@ -4,12 +4,13 @@ from custom import basic_custom_actions as bca
 from custom.verifier import Verifier
 from quod_qa.win_gui_wrappers.data_set import Side
 from win_gui_modules.common_wrappers import BaseTileDetails
+from win_gui_modules.utils import get_base_request
 
 
 class BaseTile:
-    def __init__(self, case_id, base_request, index: int = 0):
+    def __init__(self, case_id, session_id, index: int = 0):
         self.case_id = case_id
-        self.base_request = base_request
+        self.base_request = get_base_request(session_id, case_id)
         self.extraction_id = bca.client_orderid(4)
         self.base_details = BaseTileDetails(base=self.base_request, window_index=index)
         self.sell_side = Side.sell.value
