b626c0770a48d07f420a35522a7e58266cbfce8d
Yaroslav <yaroslav.furmuzal17@gmail.com>
2021-11-09 18:24:58 +0200

    Renamed folder fx_market_making into market_making

---
+++ quod_qa/web_admin/web_admin_core/pages/market_making/client_tier/client_tier_instrument_sweepable_quantities_sub_wizard.py
@@ -0,0 +1,52 @@
+from quod_qa.web_admin.web_admin_core.pages.common_page import CommonPage
+from quod_qa.web_admin.web_admin_core.pages.market_making.client_tier.client_tier_constants import \
+    ClientTierConstants
+from quod_qa.web_admin.web_admin_core.utils.web_driver_container import WebDriverContainer
+
+
+class ClientTiersInstrumentSweepableQuantitiesSubWizard(CommonPage):
+    def __init__(self, web_driver_container: WebDriverContainer):
+        super().__init__(web_driver_container)
+
+    def click_on_plus(self):
+        self.find_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_PLUS_BUTTON_XPATH).click()
+
+    def click_on_checkmark(self):
+        self.find_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_CHECKMARK_BUTTON_XPATH).click()
+
+    def click_on_close(self):
+        self.find_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_CLOSE_BUTTON_XPATH).click()
+
+    def click_on_edit(self):
+        self.find_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_EDIT_BUTTON_XPATH).click()
+
+    def click_on_delete(self):
+        self.find_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_DELETE_BUTTON_XPATH).click()
+
+    def set_quantity(self, value):
+        self.set_text_by_xpath(ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_QUANTITY_XPATH,
+                               value)
+
+    def get_quantity(self):
+        return self.get_text_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_QUANTITY_XPATH)
+
+    def set_quantity_filter(self, value):
+        self.set_text_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_QUANTITY_FILTER_XPATH, value)
+
+    def click_on_published_checkbox(self):
+        self.find_by_xpath(ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_PUBLISHED_XPATH).click()
+
+    def get_published(self):
+        return self.is_checkbox_selected(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_PUBLISHED_XPATH)
+
+    def set_published_filter_xpath(self, value):
+        self.set_text_by_xpath(
+            ClientTierConstants.CLIENT_TIER_INSTRUMENTS_SWEEPABLE_QUANTITIES_TAB_PUBLISHED_FILTER_XPATH, value)
