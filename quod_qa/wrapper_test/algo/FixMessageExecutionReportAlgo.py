a09fee59154b8a1a93270644162f294f8f5cb010
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-09 17:33:18 +0200

    fix typo

--- quod_qa/wrapper_test/algo/FixMessageExecutionReportAlgo.py
+++ quod_qa/wrapper_test/algo/FixMessageExecutionReportAlgo.py
@@ -6,7 +6,7 @@ from quod_qa.wrapper_test.DataSet import Instrument
 
 class FixMessageExecutionReportAlgo(FixMessageExecutionReport):
 
-    def __init__(self, parameters: dict = None, new_order_single: FixMessageNewOrderSingle = None, ):
+    def __init__(self, parameters: dict = None, new_order_single: FixMessageNewOrderSingle = None ):
         super().__init__()
         if new_order_single is not None:
             self.update_fix_message(new_order_single.get_parameters())
@@ -38,4 +38,40 @@ class FixMessageExecutionReportAlgo(FixMessageExecutionReport):
         temp["TargetStrategy"] = parameters["TargetStrategy"]
         super().change_parameters(temp)
 
+    def execution_report (self, new_order_single: FixMessageNewOrderSingle = None):
+        temp = dict(
+            Account=new_order_single.get_parameter("Account"),
+            ClOrdID=new_order_single.get_parameter("ClOrdID"),
+            Currency=new_order_single.get_parameter("Currency"),
+            HandlInst=new_order_single.get_parameter("HandlInst"),
+            Instrument=new_order_single.get_parameter("Instrument"),
+            OrderQty=new_order_single.get_parameter("OrderQty"),
+            OrdType=new_order_single.get_parameter("OrdType"),
+            Price=new_order_single.get_parameter("Price"),
+            Side=new_order_single.get_parameter("Side"),
+            TimeInForce=new_order_single.get_parameter("TimeInForce"),
+            TargetStrategy=new_order_single.get_parameter("TargetStrategy"),
+            ExecType="0",
+            OrdStatus="0",
+            TransactTime='*',
+            AvgPx='0',
+            CumQty='0',
+            ExecID='*',
+            LastPx='0',
+            LastQty='0',
+            OrderCapacity='A',
+            QtyType='0',
+            ExecRestatementReason='4',
+            OrderID='*',
+            SettlDate='*',
+            LeavesQty=new_order_single.get_parameter("OrderQty")
+        )
+        super().change_parameters(temp)
+        return self
 
+
+    def change_from_new_to_pendingnew(self) -> FixMessageExecutionReport:
+        super().change_from_new_to_pendingnew()
+        self.remove_parameter("ExecRestatementReason")
+        self.remove_parameter("Account")
+        return self
