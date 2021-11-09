fd5c2dd37b3b80a3ac3995f823e7fe79a9ca5afd
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-09 17:34:28 +0200

    add SecurityType to Instrument

--- quod_qa/wrapper_test/DataSet.py
+++ quod_qa/wrapper_test/DataSet.py
@@ -15,22 +15,25 @@ class MessageType(Enum):
 
 class Instrument(Enum):
     FR0010436584 = dict(
-            Symbol='FR0010436584',
-            SecurityID='FR0010436584',
-            SecurityIDSource='4',
-            SecurityExchange='XPAR'
-        )
+        Symbol='FR0010436584',
+        SecurityID='FR0010436584',
+        SecurityIDSource='4',
+        SecurityExchange='XPAR',
+        SecurityType='CS'
+    )
     test = dict(
-            Symbol='FR0010436000',
-            SecurityID='FR0010436000',
-            SecurityIDSource='4',
-            SecurityExchange='XPAR'
-        )
+        Symbol='FR0010436000',
+        SecurityID='FR0010436000',
+        SecurityIDSource='4',
+        SecurityExchange='XPAR',
+        SecurityType='CS'
+    )
     FR0000062788 = dict(
         Symbol='FR0000062788_EUR',
         SecurityID='FR0000062788',
         SecurityIDSource='4',
-        SecurityExchange='XPAR'
+        SecurityExchange='XPAR',
+        SecurityType='CS'
     )
 
 
