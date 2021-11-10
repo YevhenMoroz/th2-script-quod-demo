2a762713a968e88efb0afe727b8ef9bd526f86a1
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-10 18:41:18 +0200

    add MarketData's messages

---
+++ quod_qa/wrapper_test/FixMessageMarketDataSnapshotFullRefresh.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from quod_qa.wrapper_test.FixMessage import FixMessage
+from quod_qa.wrapper_test.DataSet import Instrument
+
+
+class FixMessageNewOrderSingle(FixMessage):
+
+    def __init__(self, parameters: dict = None):
+        super().__init__(message_type="MarketDataSnapshotFullRefresh")
+        super().change_parameters(parameters)
