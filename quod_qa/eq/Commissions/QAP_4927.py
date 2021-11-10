74c7e528c0707791d083071c1acb8a522ced7277
ipalamar <ivan.palamarchuk.contractor@quodfinancial.com>
2021-11-10 11:46:38 +0200

    Edit QAP_4927.py

--- quod_qa/eq/Commissions/QAP_4927.py
+++ quod_qa/eq/Commissions/QAP_4927.py
@@ -2,13 +2,13 @@ import logging
 import os
 
 from th2_grpc_act_gui_quod.middle_office_pb2 import PanelForExtraction
+
 from custom.basic_custom_actions import create_event
+from quod_qa.win_gui_wrappers import OMSClientInbox
+from quod_qa.win_gui_wrappers import OMSOrderBook
+from quod_qa.win_gui_wrappers import OMSOrderTicket
 from quod_qa.win_gui_wrappers.base_window import BaseWindow, decorator_try_except
-from quod_qa.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
-from quod_qa.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
-from quod_qa.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
 from stubs import Stubs
-from win_gui_modules.utils import get_base_request
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -26,13 +26,12 @@ def execute(report_id, session_id):
     work_dir = Stubs.custom_config['qf_trading_fe_folder']
     username = Stubs.custom_config['qf_trading_fe_user']
     password = Stubs.custom_config['qf_trading_fe_password']
-    base_request = get_base_request(session_id, case_id)
-    base_window = BaseWindow(case_id, base_request)
+    base_window = BaseWindow(case_id, session_id)
     base_window.open_fe(session_id, report_id, work_dir, username, password)
     # create CO order
-    oms_order_book = OMSOrderBook(case_id, base_request)
-    oms_order_inbox = OMSClientInbox(case_id, base_request)
-    oms_order_ticket = OMSOrderTicket(case_id, base_request)
+    oms_order_book = OMSOrderBook(case_id, session_id)
+    oms_order_inbox = OMSClientInbox(case_id, session_id)
+    oms_order_ticket = OMSOrderTicket(case_id, session_id)
     oms_order_ticket.set_order_details(client=client, limit=price, qty=qty, order_type='Limit',
                                        tif='Day', is_sell_side=False, instrument='ISI3', desk='Desk of Order Book')
     oms_order_ticket.oms_create_order(lookup='ISI3')
