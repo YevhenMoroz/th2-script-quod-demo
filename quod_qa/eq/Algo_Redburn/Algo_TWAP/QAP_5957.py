6bfe334b8db6301e807136953ac72e1526a0ab97
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-11-11 10:01:48 +0200

    add parameter nav_exec = 1 and nav_rebalance = 10

--- quod_qa/eq/Algo_Redburn/Algo_TWAP/QAP_5957.py
+++ quod_qa/eq/Algo_Redburn/Algo_TWAP/QAP_5957.py
@@ -35,6 +35,8 @@ price_nav = 30
 tif_day = 0
 order_type = 2
 waves = 10
+nav_exec = 1
+nav_rebalance = 10
 
 # venue param
 ex_destination_1 = "XPAR"
@@ -136,7 +138,7 @@ def execute(report_id):
         fix_message = FixMessageNewOrderSingleAlgo().set_TWAP_Navigator()
         fix_message.add_ClordId('QAP_5766')
         fix_message.change_parameters(dict(Account=client, OrderQty=qty))
-        fix_message.update_fields_in_component('QuodFlatParameters', dict(NavigatorExecution=1, NavigatorLimitPrice=price_nav, Waves=waves, NavigatorRebalanceTime=10))
+        fix_message.update_fields_in_component('QuodFlatParameters', dict(NavigatorExecution = nav_exec, NavigatorLimitPrice = price_nav, Waves = waves, NavigatorRebalanceTime = nav_rebalance))
 
         fix_manager = FixManager(connectivity_sell_side, case_id)
         response_new_order_single = fix_manager.send_message_and_receive_response(fix_message, case_id_1)
