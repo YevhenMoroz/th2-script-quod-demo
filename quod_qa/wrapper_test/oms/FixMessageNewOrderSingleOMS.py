e430b3c92850044c9a55cda922e9305bb9e0a54f
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2021-11-15 13:22:08 +0200

    Coomit FixMessageNewOrderSingleOMS.py

--- quod_qa/wrapper_test/oms/FixMessageNewOrderSingleOMS.py
+++ quod_qa/wrapper_test/oms/FixMessageNewOrderSingleOMS.py
@@ -51,3 +51,27 @@ class FixMessageNewOrderSingleOMS(FixMessageNewOrderSingle):
         if instr:
             self.change_parameters({"Instrument": instr.value})
         return self
+        super().change_parameters(parameters)
+
+    def set_default_DMA(self, instr: Instrument = None):
+        base_parameters = {
+            "Account": "CLIENT1",
+            'ClOrdID': basic_custom_actions.client_orderid(9),
+            "HandlInst": "2",
+            "Side": "1",
+            'OrderQtyData': {'OrderQty': '100'},
+            "TimeInForce": "0",
+            "OrdType": "2",
+            "Instrument": instr.value if instr is not None else Instrument.FR0010436584.value,
+            "TransactTime": datetime.utcnow().isoformat(),
+            "OrderCapacity": "A",
+            "Price": "20",
+            "Currency": "EUR",
+            "ExDestination": "XPAR"
+        }
+        super().change_parameters(base_parameters)
+        return self
+
+    def set_default_CO(self):
+        base_parameters = {
+        }
