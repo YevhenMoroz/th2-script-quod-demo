d8e9fc234bd83389474ea5aa97c71023778bd174
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-17 17:48:50 +0200

    add FixMessageNewOrderListOMS.py

--- quod_qa/eq/Basket/QAP_4648.py
+++ quod_qa/eq/Basket/QAP_4648.py
@@ -2,43 +2,36 @@ import logging
 import os
 
 from custom import basic_custom_actions as bca
-from custom.verifier import Verifier
-from quod_qa.win_gui_wrappers.base_window import decorator_try_except
 from quod_qa.win_gui_wrappers.TestCase import TestCase
-from quod_qa.wrapper_test.DataSet import Connectivity, DirectionEnum
 from quod_qa.wrapper_test.FixManager import FixManager
-from quod_qa.wrapper_test.FixMessageExecutionReport import FixMessageExecutionReport
-from quod_qa.wrapper_test.FixMessageListStatus import FixMessageListStatus
-from quod_qa.wrapper_test.FixMessageNewOrderList import FixMessageNewOrderList
+from quod_qa.wrapper_test.FixMessageNewOrderSingle import FixMessageNewOrderSingle
 from quod_qa.wrapper_test.FixVerifier import FixVerifier
-from quod_qa.wrapper_test.oms.FixMessageExecutionReportOMS import FixMessageExecutionReportOMS
-from rule_management import RuleManager
-from win_gui_modules.application_wrappers import LoginDetailsRequest, OpenApplicationRequest
-from custom.basic_custom_actions import create_event
-from stubs import Stubs
+from quod_qa.wrapper_test.SessionAlias import SessionAliasOMS
+from quod_qa.wrapper_test.oms.FixMessageNewOrderListOMS import FixMessageNewOrderListOMS
+from quod_qa.wrapper_test.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-class Test(TestCase):
+class QAP4648(TestCase):
     def __init__(self, report_id, session_id, file_name):
         super().__init__(report_id, session_id)
         self.case_id = bca.create_event(os.path.basename(__file__), self.test_id)
         self.file_name = file_name
+        self.ss_connectivity = SessionAliasOMS().ss_connectivity
+        self.bs_connectivity = SessionAliasOMS().bs_connectivity
 
     def qap_4648(self):
+        fix_manager = FixManager(self.ss_connectivity, self.report_id)
+        fix_verifier = FixVerifier(self.bs_connectivity, self.report_id)
         # region create OrderList
-
-        fix_manager = FixManager(Connectivity.Ganymede_317_ss.value, self.report_id)
-        fix_verifier = FixVerifier(Connectivity.Ganymede_317_bs.value, self.report_id)
-        new_order_list = FixMessageNewOrderList()
-        #new_order_list.change_parameters()
+        new_order_list = FixMessageNewOrderListOMS().set_default_order_list()
         fix_manager.send_message_and_receive_response_fix_standard(new_order_list)
-        fix_verifier.check_fix_message(new_order_list, direction=DirectionEnum.SECOND)
-        list_status = FixMessageListStatus(new_order_list)
-        fix_verifier.check_fix_message(list_status)
+        # fix_verifier.check_fix_message_fix_standard(new_order_list, direction=DirectionEnum.SECOND)
+        # list_status = FixMessageListStatus(new_order_list)
+        # fix_verifier.check_fix_message_fix_standard(list_status)
 
         # endregion
 
