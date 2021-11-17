a3029abc4416f21a2854c9ca39a40cb651c8dab6
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-17 12:52:14 +0200

    add FixMessageListStatus.py; FixMessageListStatusOMS.py

--- quod_qa/eq/Basket/QAP_4648.py
+++ quod_qa/eq/Basket/QAP_4648.py
@@ -1,26 +1,55 @@
 import logging
+import os
 
+from custom import basic_custom_actions as bca
+from custom.verifier import Verifier
+from quod_qa.win_gui_wrappers.base_window import decorator_try_except
+from quod_qa.win_gui_wrappers.TestCase import TestCase
+from quod_qa.wrapper_test.DataSet import Connectivity, DirectionEnum
+from quod_qa.wrapper_test.FixManager import FixManager
+from quod_qa.wrapper_test.FixMessageExecutionReport import FixMessageExecutionReport
+from quod_qa.wrapper_test.FixMessageNewOrderList import FixMessageNewOrderList
+from quod_qa.wrapper_test.FixVerifier import FixVerifier
+from quod_qa.wrapper_test.oms.FixMessageExecutionReportOMS import FixMessageExecutionReportOMS
+from rule_management import RuleManager
+from win_gui_modules.application_wrappers import LoginDetailsRequest, OpenApplicationRequest
 from custom.basic_custom_actions import create_event
-from quod_qa.wrapper import eq_fix_wrappers
-from win_gui_modules.wrappers import set_base
+from stubs import Stubs
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-def execute(report_id, session_id):
-    case_name = "QAP-4648"
-    # region Declarations
-    client = "CLIENT_YMOROZ"
-    account = "CLIENT_YMOROZ_SA1"
-    account2 = "CLIENT_YMOROZ_SA2"
-    qty = "1800"
-    price = "10"
-    case_id = create_event(case_name, report_id)
-    set_base(session_id, case_id)
+class Test(TestCase):
+    def __init__(self,  report_id, session_id, file_name):
+        super().__init__(report_id, session_id)
+        self.case_id = bca.create_event(os.path.basename(__file__), self.test_id)
+        self.file_name = file_name
 
-    no_order=[eq_fix_wrappers.set_fix_order_detail("3", "2", client, 2, qty, 0, price),
-              eq_fix_wrappers.set_fix_order_detail("3", "2", client, 2, qty, 0, price)]
+    def qap_4648(self):
+        # region create OrderList
+        try:
+            fix_manager = FixManager(Connectivity.Ganymede_317_ss.value, self.report_id)
+            fix_verifier = FixVerifier(Connectivity.Ganymede_317_bs.value, self.report_id)
 
-    print(no_order)
+            new_order_list = FixMessageNewOrderList()
+            new_order_list.change_parameters(dict(OrderQty=100000))
+
+            fix_manager.send_message_and_receive_response(new_order_list)
+            fix_verifier.check_fix_message(new_order_list, direction=DirectionEnum.SECOND)
+
+            execution_report = FixMessageExecutionReportOMS(new_order_list)
+            fix_verifier.check_fix_message(execution_report)
+        except:
+            logging.error("Error execution", exc_info=True)
+        finally:
+            RuleManager.remove_rules(rule_list)
+        # endregion
+
+
+    @decorator_try_except(test_id=os.path.basename(__file__))
+    def execute(self):
+        self.qap_4648()
+
---
+++ quod_qa/wrapper_test/FixMessageListStatus.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from quod_qa.wrapper_test.FixMessageNewOrderList import FixMessage, FixMessageNewOrderList
+
+
+class FixMessageListStatus(FixMessage):
+
+    def __init__(self, new_order_list: FixMessageNewOrderList = None, parameters: dict = None):
+        super().__init__(message_type="ExecutionReport")
+        if new_order_list is not None:
+            self.update_fix_message(new_order_list.get_parameters())
+        super().change_parameters(parameters)
+
+    def update_fix_message(self, parameters: dict) -> FixMessage:
+        temp = dict(
+            NoRpts="*",
+            ListID=parameters["ListID"],
+            RptSeq="*",
+            ListStatusType="*",
+            ListOrderStatus=parameters["ListOrderStatus"],
+            OrdListStatGrp=parameters["OrdListStatGrp"],
+        )
+        super().change_parameters(temp)
+        return self
