3709daff0e77197d6e8678dcf53d9c4f0db51439
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-18 13:57:48 +0200

    add FixMessageNewOrderListOMS.py

--- quod_qa/wrapper_test/FixManager.py
+++ quod_qa/wrapper_test/FixManager.py
@@ -4,6 +4,7 @@ from custom import basic_custom_actions
 from quod_qa.wrapper_test.DataSet import MessageType
 from quod_qa.wrapper_test.FixMessage import FixMessage
 from quod_qa.wrapper_test.FixMessageExecutionReport import FixMessageExecutionReport
+from quod_qa.wrapper_test.FixMessageListStatus import FixMessageListStatus
 from quod_qa.wrapper_test.FixMessageNewOrderSingle import FixMessageNewOrderSingle
 from stubs import Stubs
 
@@ -186,14 +187,12 @@ class FixManager:
             for field in message.fields:
                 #Field
                 if message.fields[field].simple_value != "":
-                    print(message.fields[field].simple_value)
                     fields.update({field: message.fields[field].simple_value})
                 else:
                     component_fields = dict()
                     # Component
                     for component_field in message.fields[field].message_value.fields:
                         if message.fields[field].message_value.fields[component_field].simple_value != "":
-                            print(message.fields[field].message_value.fields[component_field].simple_value)
                             component_fields.update({component_field: message.fields[field].message_value.fields[component_field].simple_value})
                             fields.update({field: component_fields})
                         else:
@@ -204,16 +203,16 @@ class FixManager:
                                 for repeating_group_field in repeating_group.message_value.fields:
                                     repeating_group_list_field.update({repeating_group_field: repeating_group.message_value.fields[repeating_group_field].simple_value})
                                 repeating_group_list.append(repeating_group_list_field)
-                            fields.update({field: repeating_group_list})
+                            fields.update({field:{component_field: repeating_group_list}})
             message_type = message.metadata.message_type
             responce_fix_message = None
             if message_type == MessageType.NewOrderSingle.value:
                 responce_fix_message = FixMessageNewOrderSingle()
             elif message_type == MessageType.ExecutionReport.value:
                 responce_fix_message = FixMessageExecutionReport()
-
+            elif message_type == MessageType.ListStatus.value:
+                responce_fix_message = FixMessageListStatus()
             responce_fix_message.change_parameters(fields)
-
         response_messages.append(responce_fix_message)
         return response_messages
 
