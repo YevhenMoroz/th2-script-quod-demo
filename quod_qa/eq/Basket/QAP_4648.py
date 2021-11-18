ded5c1190df0af04cc4bac7172dc7e4a088aa045
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-18 15:31:42 +0200

    add FixMessageNewOrderListOMS.py

--- quod_qa/eq/Basket/QAP_4648.py
+++ quod_qa/eq/Basket/QAP_4648.py
@@ -3,10 +3,13 @@ import os
 
 from custom import basic_custom_actions as bca
 from quod_qa.win_gui_wrappers.TestCase import TestCase
+from quod_qa.wrapper_test.DataSet import DirectionEnum
 from quod_qa.wrapper_test.FixManager import FixManager
+from quod_qa.wrapper_test.FixMessageListStatus import FixMessageListStatus
 from quod_qa.wrapper_test.FixMessageNewOrderSingle import FixMessageNewOrderSingle
 from quod_qa.wrapper_test.FixVerifier import FixVerifier
 from quod_qa.wrapper_test.SessionAlias import SessionAliasOMS
+from quod_qa.wrapper_test.oms.FixMessageListStatusOMS import FixMessageListStatusOMS
 from quod_qa.wrapper_test.oms.FixMessageNewOrderListOMS import FixMessageNewOrderListOMS
 from quod_qa.wrapper_test.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 
@@ -24,16 +27,27 @@ class QAP4648(TestCase):
         self.bs_connectivity = SessionAliasOMS().bs_connectivity
 
     def qap_4648(self):
+        # region Declaration
         fix_manager = FixManager(self.ss_connectivity, self.report_id)
-        fix_verifier = FixVerifier(self.bs_connectivity, self.report_id)
-        # region create OrderList
-        new_order_list = FixMessageNewOrderListOMS().set_default_order_list()
-        fix_manager.send_message_and_receive_response_fix_standard(new_order_list)
-        # fix_verifier.check_fix_message_fix_standard(new_order_list, direction=DirectionEnum.SECOND)
-        # list_status = FixMessageListStatus(new_order_list)
-        # fix_verifier.check_fix_message_fix_standard(list_status)
-
+        fix_verifier = FixVerifier(self.ss_connectivity, self.report_id)
         # endregion
+        # region Send NewOrderList
+        nol = FixMessageNewOrderListOMS().set_default_order_list()
+        fix_manager.send_message_and_receive_response_fix_standard(nol)
+        # endregion
+        # region Set-up check parameters for ListStatus
+        cl_ord_id1 = nol.get_parameters()['ListOrdGrp']['NoOrders'][0]['ClOrdID']
+        cl_ord_id2 = nol.get_parameters()['ListOrdGrp']['NoOrders'][1]['ClOrdID']
+        list_id = nol.get_parameters()['ListID']
+        list_status = FixMessageListStatusOMS().change_parameters({'ListID':list_id})
+        list_status.set_no_orders(0, cl_ord_id1)
+        list_status.set_no_orders(1, cl_ord_id2)
+        # endregion
+        # region Check ListStatus
+        fix_verifier.check_fix_message_fix_standard(list_status)
+        # endregion
+
+
 
     #@decorator_try_except(test_id=os.path.basename(__file__))
     def execute(self):
