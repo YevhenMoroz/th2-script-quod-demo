f0431bdf6dfa85d262e3ce2ecfd0d069a2bf491b
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-22 15:03:36 +0200

    rename ClassFixMessageOrderCancelRequest

--- quod_qa/wrapper_test/FixMessageOrderCancelReplaceRequest.py
+++ quod_qa/wrapper_test/FixMessageOrderCancelReplaceRequest.py
@@ -5,7 +5,7 @@ from quod_qa.wrapper_test.DataSet import MessageType
 from quod_qa.wrapper_test.FixMessageNewOrderSingle import FixMessage, FixMessageNewOrderSingle
 
 
-class FixMessageOrderCancelRequest(FixMessage):
+class FixMessageOrderCancelReplaceRequest(FixMessage):
 
     def __init__(self, parameters: dict = None):
         super().__init__(message_type=MessageType.OrderCancelReplaceRequest.value)
