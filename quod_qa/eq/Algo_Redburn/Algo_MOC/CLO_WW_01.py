4d4e4da34a3a8e002e0edafd9de2b9cbb4c53e98
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-22 15:04:10 +0200

    rename ClassFixMessageOrderCancelRequest

--- quod_qa/eq/Algo_Redburn/Algo_MOC/CLO_WW_01.py
+++ quod_qa/eq/Algo_Redburn/Algo_MOC/CLO_WW_01.py
@@ -33,7 +33,7 @@ def     execute(report_id):
         'QuodFlatParameters': {
             'MaxParticipation': '10',
             'WouldInAuction': '1',
-            'TriggerPriceRed': '116.5',
+            'TriggerPriceRed': '110.5',
             'ExcludePricePoint2': '1'
         }
     }
