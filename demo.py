8640ca38c7c282969ca45e27b37a81c28d482108
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-23 19:37:41 +0200

    modify demo

--- demo.py
+++ demo.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 from custom import basic_custom_actions as bca
 from stubs import Stubs
-from test_cases.eq.DMA import QAP_2000
-from win_gui_modules.utils import set_session_id
+from test_cases.eq.Basket import QAP_3773
+from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -13,13 +13,15 @@ logger.setLevel(logging.INFO)
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('test ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('Yevhen ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     session_id = set_session_id()
 
     try:
         # example_java_api.TestCase(report_id).execute()
-        QAP_2000.execute(report_id, session_id)
+        #QAP_4649.execute(report_id, session_id)
+        #QAP3304(report_id, session_id, None).execute()
+        QAP_3773.execute(report_id, session_id)
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
