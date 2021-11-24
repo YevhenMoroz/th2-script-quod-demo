60e976b6f822213e6bae657db0598757affd737b
Ziuban Mykhailo <mykhailo.ziuban.contractor@quodfinancial.com>
2021-11-24 14:30:53 +0200

    remove print()

--- test_framework/fix_wrappers/FixManager.py
+++ test_framework/fix_wrappers/FixManager.py
@@ -31,7 +31,6 @@ class FixManager:
             case_id = self.__case_id
 
         if fix_message.get_message_type() == MessageType.NewOrderSingle.value:
-            print(fix_message.get_parameters())
             response = self.act.placeOrderFIX(
                 request=basic_custom_actions.convert_to_request(
                     "Send NewOrderSingle",
