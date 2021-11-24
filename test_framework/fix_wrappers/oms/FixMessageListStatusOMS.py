c57d9df72c3ab8b49a645f72d745e4031e57c7e8
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-11-24 14:17:25 +0200

    modify FixMessageListStatusOMS (not done)

--- test_framework/fix_wrappers/oms/FixMessageListStatusOMS.py
+++ test_framework/fix_wrappers/oms/FixMessageListStatusOMS.py
@@ -1,4 +1,5 @@
 from test_framework.fix_wrappers.FixMessageListStatus import FixMessageListStatus
+from test_framework.fix_wrappers.FixMessageNewOrderList import FixMessageNewOrderList
 
 
 class FixMessageListStatusOMS(FixMessageListStatus):
@@ -27,18 +28,21 @@ class FixMessageListStatusOMS(FixMessageListStatus):
         ]}
     }
 
-    def set_default_list_status(self):
-        self.change_parameters(self.base_parameters)
+    # def set_no_orders(self, num: int, cl_ord_id=None, leaves_qty=None, avg_px=None, cum_qty=None):
+    #     self.change_parameters(self.base_parameters)
+    #     if cl_ord_id is not None:
+    #         self.base_parameters['OrdListStatGrp']['NoOrders'][num]['ClOrdID'] = cl_ord_id
+    #     if leaves_qty is not None:
+    #         self.base_parameters['OrdListStatGrp']['NoOrders'][num]['LeavesQty'] = leaves_qty
+    #     if avg_px is not None:
+    #         self.base_parameters['OrdListStatGrp']['NoOrders'][num]['AvgPx'] = avg_px
+    #     if cum_qty is not None:
+    #         self.base_parameters['OrdListStatGrp']['NoOrders'][num]['CumQty'] = cum_qty
+    #     return self
+
+    def set_default_list_status(self, new_order_list: FixMessageNewOrderList):
+        change_parameters = dict()
+        for ord in new_order_list.get_parameter("ListOrdGrp")["NoOrders"]:
+            self.base_parameters['OrdListStatGrp']['NoOrders'][num]['ClOrdID'] = cl_ord_id
         return self
 
-    def set_no_orders(self, num: int, cl_ord_id=None, leaves_qty=None, avg_px=None, cum_qty=None):
-        self.change_parameters(self.base_parameters)
-        if cl_ord_id is not None:
-            self.base_parameters['OrdListStatGrp']['NoOrders'][num]['ClOrdID'] = cl_ord_id
-        if leaves_qty is not None:
-            self.base_parameters['OrdListStatGrp']['NoOrders'][num]['LeavesQty'] = leaves_qty
-        if avg_px is not None:
-            self.base_parameters['OrdListStatGrp']['NoOrders'][num]['AvgPx'] = avg_px
-        if cum_qty is not None:
-            self.base_parameters['OrdListStatGrp']['NoOrders'][num]['CumQty'] = cum_qty
-        return self
