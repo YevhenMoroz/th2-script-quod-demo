29b8853ceb52d726f8e082592e60bc9d16d79b90
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-12-01 14:18:50 +0200

    small changes

--- test_cases/fx/fx_mm_autohedging/QAP_6008.py
+++ test_cases/fx/fx_mm_autohedging/QAP_6008.py
@@ -182,9 +182,6 @@ def execute(report_id, session_id):
         extracted_pos_quod = get_dealing_positions_details(pos_service, case_base_request, symbol, account)
 
         compare_position('Checking positions', case_id, initial_pos, extracted_pos_quod, account)
-
-
-
     except Exception as e:
         logging.error('Error execution', exc_info=True)
         bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
