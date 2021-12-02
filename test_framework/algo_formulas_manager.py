451344a75bc9e0e8845c9f857267833bfdc16349
Merkyr <43476614+Merkyr@users.noreply.github.com>
2021-12-02 17:57:04 +0200

    update algo_formulas_manager.py

--- test_framework/algo_formulas_manager.py
+++ test_framework/algo_formulas_manager.py
@@ -1,16 +1,45 @@
+import math
+
+
 class AlgoFormulasManager:
+    # region offset calculation
     @staticmethod
-    def calc_ticks_offset_minus(price: float, offset_qty: int, tick: float):
+    def calc_ticks_offset_minus(price: float, offset_qty: int, tick: float) -> float:
         return price - tick * offset_qty
 
     @staticmethod
-    def calc_ticks_offset_plus(price: float, offset: int, tick: float):
+    def calc_ticks_offset_plus(price: float, offset: int, tick: float) -> float:
         return price + tick * offset
 
     @staticmethod
-    def calc_bps_offset_minus(price: float, offset_qty: int):
+    def calc_bps_offset_minus(price: float, offset_qty: int) -> float:
         return price - price / 10000 * offset_qty
 
     @staticmethod
-    def calc_bps_offset_plus(price: float, offset_qty: int):
+    def calc_bps_offset_plus(price: float, offset_qty: int) -> float:
         return price + price / 10000 * offset_qty
+    # endregion
+
+    @staticmethod
+    def get_next_twap_slice(remaining_ord_qty: int, remaining_waves: int) -> int:
+        return math.ceil(remaining_ord_qty / remaining_waves)
+
+    @staticmethod
+    def get_all_twap_slices(remaining_ord_qty: int, remaining_waves: int) -> list:
+        used_qty = 0
+        twap_slices = []
+        for i in range(remaining_waves):
+            temp = AlgoFormulasManager.get_next_twap_slice(remaining_ord_qty - used_qty, remaining_waves - i)
+            used_qty += temp
+            twap_slices.append(used_qty)
+        return twap_slices
+
+    @staticmethod
+    def get_pov_child_qty(per_vol: float, market_vol: int, ord_qty: int) -> int:
+        return max(math.ceil((market_vol * per_vol) / (100 - per_vol)), ord_qty)
+
+    @staticmethod
+    def get_twap_nav_child_qty(remaining_ord_qty: int, remaining_waves: int, ats: int, nav_percentage: float) -> int:
+        first_reserve = max(5 * ats, math.ceil(remaining_ord_qty * (100 - nav_percentage)))
+        reserve = max(first_reserve, AlgoFormulasManager.get_next_twap_slice(remaining_ord_qty, remaining_waves))
+        return remaining_ord_qty - reserve
