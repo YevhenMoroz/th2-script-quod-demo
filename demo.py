884f4a63370e07ca87ccf7cd3545c1255a160476
ostronov <oleksandr.stronov>
2021-12-06 11:31:13 +0200

    Update demo

--- demo.py
+++ demo.py
@@ -2,13 +2,16 @@ import logging
 from datetime import datetime
 
 from custom import basic_custom_actions as bca
-from my_methods import send_md
+from my_methods import send_md, send_rfq
 from rule_management import RuleManager
-from test_cases.fx.fx_mm_esp import QAP_1643
+from test_cases.fx.fx_mm_esp import QAP_1643, QAP_6145
 from test_cases.fx.fx_mm_esp.QAP_5389 import QAP_5389
 
 from stubs import Stubs
-from test_cases.fx.fx_mm_rfq import QAP_4505, for_test_77679
+from test_cases.fx.fx_mm_rfq import QAP_4505, for_test_77679, QAP_5848, QAP_5856
+from test_cases.fx.fx_taker_esp import QAP_5564, QAP_5589, QAP_5591, QAP_5598, QAP_5600
+from test_cases.fx.qs_fx_routine import QAP_5176
+from test_cases.fx.ui_wrappers import wrapper_test
 from win_gui_modules.utils import set_session_id, get_opened_fe, prepare_fe_2
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
@@ -37,14 +40,25 @@ def test_run():
         #     get_opened_fe(report_id, session_id)
 
 
-        # rm= RuleManager()
-        # rm.remove_rule_by_id(3)
-        # # rm.add_fx_md_to("fix-fh-314-luna")
-        # rm.print_active_rules()
-        # # rm.remove_rule_by_id(48)
+        # rm = RuleManager()
+        # rm.add_TRADE_ESP("fix-bs-esp-314-luna-standard")
+        # rm.add_fx_md_to("fix-fh-314-luna")
+        # rm.remove_rule_by_id(48)
         # rm.print_active_rules()
         # send_md.execute(report_id, 1.19, 1.21)
-        QAP_4505.execute(report_id)
+        # QAP_4505.execute(report_id)
+        # QAP_5856.execute(report_id)
+        # QAP_5564.execute(report_id, session_id)
+        # QAP_5589.execute(report_id, session_id)
+        # QAP_5591.execute(report_id, session_id)
+        # QAP_5598.execute(report_id, session_id)
+        # QAP_5176.execute(report_id)
+        # send_rfq.execute(report_id)
+        # send_rfq.execute(report_id)
+        QAP_6145.execute(report_id)
+        # wrapper_test.execute(report_id, session_id)
+        # wrapper_test.execute(report_id, session_id)
+        # QAP_5856.execute(report_id)
         # for_test_77679.execute(report_id, session_id)
         # QAP_1643.execute(report_id, session_id)
         # QAP_5389().execute(report_id)
