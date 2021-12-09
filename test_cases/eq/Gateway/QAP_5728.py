0bdf9feacd94290a08163999c7ac97104ce95f97
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2021-12-09 18:22:12 +0200

    Add QAP_5728.py and modify QAP_5753

--- test_cases/eq/Gateway/QAP_5728.py
+++ test_cases/eq/Gateway/QAP_5728.py
@@ -3,13 +3,13 @@ import os
 
 from custom import basic_custom_actions as bca
 from stubs import Stubs
+from test_framework.fix_wrappers.DataSet import Instrument
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.FixVerifier import FixVerifier
 from test_framework.fix_wrappers.SessionAlias import SessionAliasOMS
-from test_framework.fix_wrappers.oms.FixMessageAllocationInstructionReportOMS import \
-    FixMessageAllocationInstructionReportOMS
 from test_framework.fix_wrappers.oms.FixMessageConfirmationReportOMS import FixMessageConfirmationReportOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.rest_api_wrappers.rest_commissions_sender import RestCommissionsSender
 from test_framework.win_gui_wrappers.TestCase import TestCase
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from test_framework.win_gui_wrappers.base_window import decorator_try_except
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 timeouts = True
 
 
-class Qap5753(TestCase):
+class QAP_5728(TestCase):
     def __init__(self, report_id, session_id, file_name):
         super().__init__(report_id, session_id)
         self.case_id = bca.create_event(os.path.basename(__file__)[:-3], self.test_id)
@@ -30,27 +30,32 @@ class Qap5753(TestCase):
         self.ss_connectivity = SessionAliasOMS().ss_connectivity
         self.bs_connectivity = SessionAliasOMS().bs_connectivity
         self.dc_connectivity = SessionAliasOMS().dc_connectivity
+        self.wa_connectivity = SessionAliasOMS().wa_connectivity
 
-    def qap_5753(self):
+    def qap_5728(self):
         # region Declaration
         order_book = OMSOrderBook(self.case_id, self.session_id)
         base_window = BaseMainWindow(self.case_id, self.session_id)
         client_inbox = OMSClientInbox(self.case_id, self.session_id)
         fix_manager = FixManager(self.ss_connectivity, self.case_id)
-        security_account = 'MOClient_SA1'
-        client = 'MOClient'
+        client = 'CLIENT_COMM_1'
         change_params = {'Account': client,
+                         "Instrument": Instrument.ISI1.value,
+                         "ExDestination": "XEUR",
                          'PreAllocGrp': {
                              'NoAllocs': [{
-                                 'AllocAccount': "MOClient_SA1",
+                                 'AllocAccount': "CLIENT_COMM_1_SA2",
                                  'AllocQty': "100"}]}}
         fix_message = FixMessageNewOrderSingleOMS().set_default_care_limit().change_parameters(change_params)
         fix_verifier = FixVerifier(self.dc_connectivity, self.case_id)
-        CHECKED_VALUE = '42000'
         middle_office = OMSMiddleOfficeBook(self.case_id, self.session_id)
         work_dir = Stubs.custom_config['qf_trading_fe_folder']
         username = Stubs.custom_config['qf_trading_fe_user']
         password = Stubs.custom_config['qf_trading_fe_password']
+        fee_commission = RestCommissionsSender(self.wa_connectivity, self.case_id)
+        fee_commission.send_default_fee()
+        fee_commission.modify_client_commission_request()
+        fee_commission.send_post_request()
         # endregion
 
         # region open FE
@@ -76,20 +81,8 @@ class Qap5753(TestCase):
         # endregion
 
         # region Book order
-        middle_office.set_modify_ticket_details(settl_currency='UAH', exchange_rate='2', exchange_rate_calc='Multiply',
-                                                extract_book=True)
         middle_office.book_order()
         # endregion
-
-        # region verify AllocationInstruction message
-        fix_report_allocinstr = FixMessageAllocationInstructionReportOMS().set_default_ready_to_book(fix_message)
-        fix_report_allocinstr.add_tag(
-            {'RootSettlCurrency': '*', 'RootSettlCurrFxRateCalc': '*', 'RootSettlCurrAmt': '*',
-             'RootSettlCurrAmt': CHECKED_VALUE, 'RootSettlCurrFxRate': '*'})
-        fix_verifier.check_fix_message_fix_standard(fix_report_allocinstr)
-
-        # endregion
-
         # region approve block
         middle_office.approve_block()
         # endregion
@@ -98,22 +91,23 @@ class Qap5753(TestCase):
         middle_office.allocate_block()
         # endregion
 
-        # region verify second allocation instruction
-        fix_report_allocinstr.set_default_preliminary(fix_message).remove_parameter(
-            'NoAllocs').add_fields_into_repeating_group('NoAllocs', [
-            {'AllocSettlCurrAmt': '*', 'AllocSettlCurrency': '*', 'SettlCurrAmt': '*', 'SettlCurrFxRate': '*',
-             'SettlCurrency': '*', 'SettlCurrFxRateCalc': '*', 'AllocQty': '*',
-             'AllocNetPrice': '*', 'AllocAccount': '*', 'AllocPrice': '*'}])
-        fix_verifier.check_fix_message_fix_standard(fix_report_allocinstr)
-        # endregion
-
         # region verify Confirmation message
         fix_response_confirmation = FixMessageConfirmationReportOMS().set_default_confirmation_new(fix_message)
-        fix_response_confirmation.add_tag(
-            {'SettlCurrFxRate': '*', 'SettlCurrFxRateCalc': '*', 'SettlCurrency': '*', 'SettlCurrAmt': CHECKED_VALUE})
+        fix_response_confirmation.change_parameters({'SettlCurrFxRate': '*',
+                                                     'NoMiscFees': [{
+                                                         'MiscFeeAmt': '*',
+                                                         'MiscFeeCurr': '*',
+                                                         'MiscFeeType': '*'
+                                                     }],
+                                                     'CommissionData': {
+                                                         'CommissionType': '*',
+                                                         'Commission': '*',
+                                                         'CommCurrency': '*'
+                                                     }
+                                                     })
         fix_verifier.check_fix_message_fix_standard(fix_response_confirmation)
         # endregion
 
     @decorator_try_except(test_id=os.path.basename(__file__))
     def execute(self):
-        self.qap_5753()
+        self.qap_5728()
