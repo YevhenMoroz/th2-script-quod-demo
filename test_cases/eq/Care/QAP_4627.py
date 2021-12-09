badbbdc086130e7e26dd29a6fcdcac402eaca2b8
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2021-12-09 17:21:46 +0200

    add 4627

--- test_cases/eq/Care/QAP_4627.py
+++ test_cases/eq/Care/QAP_4627.py
@@ -11,16 +11,17 @@ from test_framework.win_gui_wrappers.TestCase import TestCase
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from test_framework.win_gui_wrappers.base_window import decorator_try_except
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
+from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-class QAP4627(TestCase):
+class QAP_4627(TestCase):
     def __init__(self, report_id, session_id, file_name):
         super().__init__(report_id, session_id)
-        self.case_id = bca.create_event(os.path.basename(__file__), self.test_id)
+        self.case_id = bca.create_event(os.path.basename(__file__)[:-3], self.test_id)
         self.file_name = file_name
         self.ss_connectivity = SessionAliasOMS().ss_connectivity
         self.bs_connectivity = SessionAliasOMS().bs_connectivity
@@ -30,21 +31,25 @@ class QAP4627(TestCase):
         fix_manager = FixManager(self.ss_connectivity, self.report_id)
         fix_verifier = FixVerifier(self.ss_connectivity, self.case_id)
         cl_inbox = OMSClientInbox(self.case_id, self.session_id)
+        ord_book = OMSOrderBook(self.case_id, self.session_id)
         main_window = BaseMainWindow(self.case_id, self.session_id)
         work_dir = Stubs.custom_config['qf_trading_fe_folder']
         username = Stubs.custom_config['qf_trading_fe_user']
         password = Stubs.custom_config['qf_trading_fe_password']
         # endregion
         # region Open FE
-        # main_window.open_fe(self.report_id, work_dir, username, password)
+        main_window.open_fe(self.report_id, work_dir, username, password)
         # endregion
         # region Send NewOrderSingle
         nos = FixMessageNewOrderSingleOMS().set_default_care_limit().change_parameters({"Account": "DUMMY_CLIENT"})
-        fix_manager.send_message_and_receive_response_fix_standard(nos)
+        fix_manager.send_message_fix_standard(nos)
         # endregion
         # region Check value
-
-        # region Execute
+        exp_val = {"Sts": "Held", "Client ID": "DUMMY"}
+        ord_book.scroll_order_book()
+        act_val = ord_book.extract_fields_list(exp_val)
+        ord_book.compare_values(exp_val, act_val, "Check Held")
+        # endregion
 
     @decorator_try_except(test_id=os.path.basename(__file__))
     def execute(self):
