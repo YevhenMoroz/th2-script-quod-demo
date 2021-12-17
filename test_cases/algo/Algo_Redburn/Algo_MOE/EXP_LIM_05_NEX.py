7fb8ffa28bf23671bc910a44194954947da15f26
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2021-12-17 12:18:03 +0200

    add new venue without EXP phase

---
+++ test_cases/algo/Algo_Redburn/Algo_MOE/EXP_LIM_05_NEX.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from custom import basic_custom_actions as bca
+from custom.basic_custom_actions import message_to_grpc, convert_to_request
+from stubs import Stubs
+
+timeouts = True
+
+
+def execute(report_id):
+    instrument = {
+        'Symbol': 'DE0005190003-XETR',
+        'SecurityID': 'DE0005190003',
+        'SecurityIDSource': '4',
+        'SecurityExchange': 'XETR'
+    }
+
+    new_order_single_params = {
+        'Account': "REDBURN",
+        'ClOrdID': 'EXP_LIM_05_NEX ' + bca.client_orderid(9),
+        'HandlInst': 2,
+        'Side': 1,
+        'OrderQty': 10000000,
+        'TimeInForce': 0,
+        'Price': 117,
+        'OrdType': 2,
+        'TransactTime': datetime.utcnow().isoformat(),
+        'Instrument': instrument,
+        'OrderCapacity': 'A',
+        'Currency': "EUR",
+        'TargetStrategy': 1014, # Expiry
+        'ExDestination': 'XETR',
+        # 'Text': 'CLO_LIM_01',
+        'QuodFlatParameters': {
+            'MaxParticipation': '10',
+            'LimitPriceReference': 'LTP',
+            'LimitPriceOffset': '2',
+            'ExcludePricePoint2': '1'
+        }
+    }
+
+    Stubs.fix_act.sendMessage(request=convert_to_request(
+        'Send NewOrderSingle',
+        "fix-sell-side-redburn",
+        report_id,
+        message_to_grpc('NewOrderSingle', new_order_single_params, "fix-sell-side-redburn")
+    ))
