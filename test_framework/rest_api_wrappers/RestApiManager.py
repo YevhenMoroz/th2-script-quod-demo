f8fff23a0414495d844a6293d5a3d1e5f36a7cad
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2021-12-28 09:29:54 +0200

    Added parse_response_details method

--- test_framework/rest_api_wrappers/RestApiManager.py
+++ test_framework/rest_api_wrappers/RestApiManager.py
@@ -3,6 +3,8 @@ from test_framework.rest_api_wrappers.RestApiMessages import RestApiMessages
 from custom.basic_custom_actions import convert_to_get_request
 from stubs import Stubs
 from custom import basic_custom_actions as bca
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToDict
 
 
 class RestApiManager:
@@ -28,17 +30,55 @@ class RestApiManager:
                                          request_type=api_message.get_message_type(),
                                          response_type=api_message.get_message_type() + "Reply")
         response = self.act.sendGetRequest(message)
-
         return response.response_message
 
     @staticmethod
-    def get_response_details(response, response_name, expected_entity_name, entity_field_id):
-
+    def get_response_details(response, expected_entity_name, entity_field_id):
+        response_name = list(dict(response.fields).keys())[0]
         for count in range(len(response.fields[response_name].list_value.values)):
             entity = response.fields[response_name].list_value.values[count].message_value.fields[
                 entity_field_id].simple_value
             if entity == expected_entity_name:
                 return response.fields[response_name].list_value.values[count].message_value.fields
+
+    @staticmethod
+    def parse_response_details(response, filter_dict: dict):
+        result = dict()
+        temp = dict()
+        for i in MessageToDict(response)['fields']:
+            temp = MessageToDict(response)['fields'][i]['listValue']['values']
+        for i in range(len(temp)):
+            result.update({i: temp[i]['messageValue']['fields']})
+        temp.clear()
+        for i in result.keys():
+            temp = result[i]
+            for key, value in temp.items():
+                if 'simpleValue' in value.keys():
+                    temp.update({key: value['simpleValue']})
+                elif 'listValue' in value.keys():
+                    temp_list = []
+                    for list_values in value['listValue']['values']:
+                        temp_dict = dict()
+                        for list_key, list_value in list_values['messageValue']['fields'].items():
+                            if 'simpleValue' in list_value.keys():
+                                temp_dict.update({list_key: list_value['simpleValue']})
+                            elif 'listValue' in list_value.keys():
+                                sub_list=[]
+                                for sub_list_values in list_value['listValue']['values']:
+                                    sub_dict = dict()
+                                    for sub_list_key, sub_list_value in sub_list_values['messageValue']['fields'].items():
+                                        sub_dict.update({sub_list_key: sub_list_value['simpleValue']})
+                                    sub_list.append(sub_dict)
+                                temp_dict.update({list_key: sub_list})
+                        temp_list.append(temp_dict)
+                    temp.update({key: temp_list})
+            result.update({i: temp})
+        result_filtered = []
+        for key in result.keys():
+            for filter_key, filter_value in filter_dict:
+                if result[key][filter_key] == filter_value:
+                    result_filtered.append(result[key])
+        return result_filtered
 
     @staticmethod
     def get_response_multiple_details(response, response_name, entity_field_id, field_name):
