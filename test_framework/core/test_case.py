c72f1ba705c96a392bacec7132e1e4c51a29f910
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-01-06 18:53:51 +0200

    rename decorator try_except

--- test_framework/core/test_case.py
+++ test_framework/core/test_case.py
@@ -1,21 +1,23 @@
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from test_framework.data_sets.base_data_set import BaseDataSet
-from test_framework.win_gui_wrappers.base_window import try_except
+from abc import ABC, abstractmethod
 
 
-class TestCase:
+class TestCase(ABC):
     def __init__(self, report_id, session_id=None, data_set: BaseDataSet = None):
         self.session_id = session_id
         self.report_id = report_id
         self.data_set = data_set
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
 
+    @abstractmethod
     def run_pre_conditions_and_steps(self):
-        raise Exception("You need to override run_pre_conditions_and_steps method in child class test")
+        pass
 
+    @abstractmethod
     def run_post_conditions(self):
-        raise Exception("You need to override run_post_conditions method in child class test")
+        pass
 
     def execute(self):
         self.run_pre_conditions_and_steps()
