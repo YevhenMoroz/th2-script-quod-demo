34d5d77068a4abcdcedd4c927ded44d72eb41a76
ysamoilenko <yehor.samoilenko.contractor@gmail.com>
2022-01-10 12:07:09 +0200

    Changed demo.py

--- demo.py
+++ demo.py
@@ -4,10 +4,8 @@ from datetime import datetime
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 from test_cases.eq.Basket.QAP_3882 import QAP3882
-from test_cases.eq.Basket.QAP_4648 import QAP4648
-from test_framework.core.example_of_ideal_test_case_ui import QAP_Example
-from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
+from test_cases.eq.Basket.QAP_4648 import QAP_4648
+from win_gui_modules.utils import set_session_id
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -21,17 +19,8 @@ def test_run():
     session_id = set_session_id()
 
     try:
-        if not Stubs.frontend_is_open:
-            prepare_fe_2(report_id, session_id)
-        else:
-            get_opened_fe(report_id, session_id)
         # example_java_api.TestCase(report_id).execute()
-        # QAP4648(report_id, session_id, None).execute()
-
-        # example of test with usage class TestCase, data_sets and UI wrappers
-        fx_data_set = FxDataSet()
-        QAP_Example(report_id, session_id, fx_data_set).execute()
-
+        QAP_4648(report_id, session_id).execute()
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
