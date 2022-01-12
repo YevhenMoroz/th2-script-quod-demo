2c63e2e64744e1b2159910ef52a99dd5828dd5b7
amedents <artem.medentsev.contractor@quodfinancial.com>
2022-01-12 15:34:59 +0200

    demo is refactored

--- demo.py
+++ demo.py
@@ -2,7 +2,11 @@ import logging
 from datetime import datetime
 from custom import basic_custom_actions as bca
 from stubs import Stubs
+from test_cases.fx.fx_mm_esp import QAP_2069, QAP_1536, QAP_1559
 from test_cases.fx.fx_mm_rfq import QAP_2101
+from test_cases.fx.fx_mm_rfq.QAP_6364 import QAP_6364
+from test_cases.fx.fx_taker_rfq import QAP_569, QAP_574, QAP_2836
+from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
 from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
@@ -28,12 +32,13 @@ def test_run():
     }
 
     session_id = set_session_id()
+    data_set = FxDataSet()
     try:
         if not Stubs.frontend_is_open:
             prepare_fe_2(report_id, session_id)
         else:
             get_opened_fe(report_id, session_id)
-        QAP_2101.execute(report_id, session_id)
+        QAP_6364(report_id, session_id, data_set).execute()
 
 
     except Exception:
