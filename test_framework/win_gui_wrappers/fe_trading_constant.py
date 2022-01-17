ef5bd54d0de39e1c254596285b5b14c80913be06
ipalamar <ivan.palamarchuk.contractor@quodfinancial.com>
2022-01-17 16:14:23 +0200

    Edit constants names

--- test_framework/win_gui_wrappers/fe_trading_constant.py
+++ test_framework/win_gui_wrappers/fe_trading_constant.py
@@ -202,10 +202,10 @@ class MiddleOfficeColumns(Enum):
     price = "AvgPx"
     client_id = "Client ID"
     client_comm = "Client Comm"
-    fees = "Total Fees"
+    total_fees = "Total Fees"
 
 
 class AllocationsColumns(Enum):
     client_comm = "Client Comm"
-    qty = "Alloc Qty"
-    fees = "Total Fees"
+    alloc_qty = "Alloc Qty"
+    total_fees = "Total Fees"
