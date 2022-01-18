3955e6434bf4f32a83377df449a56b844f89c776
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-01-18 16:52:58 +0200

    update dataset

--- test_framework/fix_wrappers/DataSet.py
+++ test_framework/fix_wrappers/DataSet.py
@@ -188,6 +188,7 @@ class FreeNotesReject(Enum):
     MissLimitPriceReference = "missing LimitPriceReference"
     MissNavigatorLimitPriceReference = "missing NavigatorLimitPriceReference"
     MissNavigatorLimitPrice = "missing Limit price for Navigator"
+    InvalidMaxParticipation = "invalid value for MaxParticipation"
 
 class Reference(Enum):
     LastTradePrice = 'LTP'
