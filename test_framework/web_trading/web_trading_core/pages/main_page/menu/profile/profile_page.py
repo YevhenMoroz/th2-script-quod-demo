37bc790273c0f05e1afc7ecba84ef83dd916d8be
Yaroslav <yaroslav.furmuzal17@gmail.com>
2022-01-25 16:45:20 +0200

    QAP-5943: Added declaration of methods for WebTrading framework.

--- test_framework/web_trading/web_trading_core/pages/main_page/menu/profile/profile_page.py
+++ test_framework/web_trading/web_trading_core/pages/main_page/menu/profile/profile_page.py
@@ -3,23 +3,27 @@ from test_cases.web_admin.web_admin_core.pages.common_page import CommonPage
 from test_cases.web_admin.web_admin_core.utils.web_driver_container import WebDriverContainer
 
 
-class MenuPage(CommonPage):
+class ProfilePage(CommonPage):
     def __init__(self, web_driver_container: WebDriverContainer):
         super().__init__(web_driver_container)
 
     # TODO: implement methods, if something changed in FE part, please take a look
 
-    def click_on_profile_button(self):
+    def click_on_personal_details_button(self):
         pass
 
-    def click_on_hide_header_button(self):
+    def click_on_preference_button(self):
         pass
 
-    def click_on_dark_theme_button(self):
+    def click_on_security_button(self):
         pass
 
-    def click_on_contact_us_button(self):
+    def click_on_terms_and_condition_button(self):
         pass
 
-    def click_on_logout_button(self):
+    def click_on_cancel_button(self):
         pass
+
+    def click_on_save_button(self):
+        pass
+
