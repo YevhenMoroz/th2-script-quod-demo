ab640dd979c14e3df237d145adc7f568afc50743
Viktoriia Dikhtiar <viktoriia.dikhtiar.contractor@quodfinancial.com>
2022-01-25 14:59:23 +0200

    added XPATH into menu_constants.py file

---
+++ test_framework/web_trading/web_trading_core/pages/main_page/menu/menu_constants.py
@@ -0,0 +1,5 @@
+PROFILE_BUTTON_XPATH = '//*[@class="toggleprofile-content igx-toggle"]/div[1]'
+HIDE_HEADER_BUTTON_XPATH = '//*[@class="toggleprofile-content igx-toggle"]/div[2]'
+DARK_THEME_BUTTON_XPATH = '//*[@class="toggleprofile-content igx-toggle"]/div[3]'
+CONTACT_US_BUTTON_XPATH = '//*[@class="toggleprofile-content igx-toggle"]/div[4]'
+LOGOUT_BUTTON_XPATH = '//*[@class="toggleprofile-content igx-toggle"]/div[5]'
