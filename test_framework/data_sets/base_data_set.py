1a2f1cba0ec61e7ec323c0045ddbb6e516e8886b
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2022-01-25 18:01:14 +0200

    switch SessionAlias from fix_wrappers.DataSet to data_sets.base_data_set

--- test_framework/data_sets/base_data_set.py
+++ test_framework/data_sets/base_data_set.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from test_framework.data_sets.fx_data_set.fx_const_enum import FxSymbols, FxSecurityTypes, FxSettleTypes, FxSettleDates
 
 
@@ -137,3 +138,63 @@ class BaseDataSet:
             return getattr(self.settle_dates, name).value
         raise ValueError(f"{self.settle_dates} not found!")
     # endregion
+
+# region Common Enums
+class Connectivity(Enum):
+    Ganymede_316_Feed_Handler = 'fix-feed-handler-316-ganymede'
+    Ganymede_316_Sell_Side = 'fix-sell-side-316-ganymede'
+    Ganymede_316_Buy_Side = 'fix-buy-side-316-ganymede'
+    Ganymede_316_Redburn = 'fix-sell-side-316-gnmd-rb'
+    Ganymede_317_ss = 'fix-sell-317-standard-test'
+    Ganymede_317_bs = 'fix-buy-317-standard-test'
+    Ganymede_317_dc = 'fix-sell-317-backoffice'
+    Ganymede_317_wa = "rest_wa317ganymede"
+    Luna_314_ss_rfq = 'fix-ss-rfq-314-luna-standard'
+    Luna_314_bs_rfq = 'fix-bs-rfq-314-luna-standard'
+    Luna_314_ss_esp = 'fix-sell-esp-m-314luna-stand'
+    Luna_314_Feed_Handler = 'fix-fh-314-luna'
+    Luna_314_Feed_Handler_Q = 'fix-fh-q-314-luna'
+    Luna_314_dc = 'fix-sell-m-314luna-drop'
+    Luna_314_wa = "rest_wa314luna"
+    Ganymede_317_ja = '317_java_api'
+    Ganymede_317_als_email_report = 'log317-als-email-report'
+
+
+class DirectionEnum(Enum):
+    FromQuod = "FIRST"
+    ToQuod = "SECOND"
+
+
+class GatewaySide(Enum):
+    Sell = "Sell"
+    Buy = "Buy"
+
+
+class MessageType(Enum):
+    NewOrderSingle = "NewOrderSingle"
+    NewOrderMultiLeg = "NewOrderMultileg"
+    ExecutionReport = "ExecutionReport"
+    OrderCancelReplaceRequest = "OrderCancelReplaceRequest"
+    OrderCancelRequest = "OrderCancelRequest"
+    MarketDataRequest = "MarketDataRequest"
+    MarketDataIncrementalRefresh = "MarketDataIncrementalRefresh"
+    MarketDataSnapshotFullRefresh = "MarketDataSnapshotFullRefresh"
+    NewOrderList = "NewOrderList"
+    ListStatus = "ListStatus"
+    QuoteRequest = "QuoteRequest"
+    Quote = "Quote"
+    Confirmation = "Confirmation"
+    AllocationInstruction = "AllocationInstruction"
+
+
+class Status(Enum):
+    Pending = "Pending"
+    New = "New"
+    Fill = "Fill"
+    PartialFill = "PartialFill"
+    Reject = "Reject"
+    CancelRequest = "CancelReplace"
+    Cancel = "Cancel"
+    Eliminate = "Eliminate"
+# endregion
+
