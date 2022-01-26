0ecf4099491e5dd9a418e0e83b8af06411658c1f
Yevhen Moroz <yevhen.moroz.contractor@quodfinancial.com>
2022-01-26 10:46:38 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.INFO)
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('skomanova ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('test ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     session_id = set_session_id()
     data_set = OmsDataSet()
