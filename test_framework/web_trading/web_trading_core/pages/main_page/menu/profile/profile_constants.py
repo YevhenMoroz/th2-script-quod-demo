a056f9741d4d84b40c9f78fca381912b7bc2769f
Viktoriia Dikhtiar <viktoriia.dikhtiar.contractor@quodfinancial.com>
2022-01-26 12:26:33 +0200

    added #region into profile_constants.py file

--- test_framework/web_trading/web_trading_core/pages/main_page/menu/profile/profile_constants.py
+++ test_framework/web_trading/web_trading_core/pages/main_page/menu/profile/profile_constants.py
@@ -1,3 +1,4 @@
+#region - Personal Details
 PERSONAL_DETAILS_BUTTON_XPATH = '//*[@class="nav"]/span[1]'
 PREFERENCE_BUTTON_XPATH = '//*[@class="nav"]/span[2]'
 SECURITY_BUTTON_XPATH = '//*[@class="nav"]/span[3]'
@@ -10,6 +11,9 @@ COUNTRY_FIELD_XPATH = '//*[@name="country"]'
 ADDRESS_FIELD_XPATH = '//*[@name="address"]'
 DATA_OF_BIRTH_FIELD_XPATH = '//*[@name="dob"]'
 CLOSE_BUTTON_XPATH = '//*[@class="icon-close"]'
+#endregion
+
+#region - Preference
 ORDER_NOTIFICATIONS_SHOW_RADIO_BUTTON_XPATH = '//*[@id="igx-radio-66"]'
 ORDER_NOTIFICATIONS_HIDE_RADIO_BUTTON_XPATH = '//*[@id="igx-radio-67"]'
 EXECUTION_NOTIFICATIONS_SHOW_RADIO_BATTON_XPATH = '//*[@id="igx-radio-68"]'
@@ -19,8 +23,18 @@ OTHER_NOTIFICATIONS_HIDE_RADIO_BUTTON_XPATH = '//*[@id="igx-radio-71"]'
 DEFAULT_CLIENT_SELECT_MENU_XPATH = '//*[@id="default-client"]'
 PREFERENCE_CANCEL_BUTTON_XPATH = '//*[@class="button-style-cancel"]'
 PREFERENCE_SAVE_BUTTON_XPATH = '//*[@class="button-style"]'
+PREFERENCE_CLOSE_BUTTON_XPATH = '//*[@class="icon-close"]'
+#endregion
+
+#region - Security
 OLD_PASSWORD_FIELD_XPATH = '//*[@name="oldPassword"]'
 NEW_PASSWORD_FIELD_XPATH = '//*[@name="newPassword"]'
 CONFIRM_PASSWORD_FIELD_XPATH = '//*[@name="confirmPassword"]'
 SECURITY_CANCEL_BUTTON_XPATH = '//*[@class="button-style-cancel"]'
 SECURITY_SAVE_BUTTON_XPATH = '//*[@class="button-style"]'
+SECURITY_CLOSE_BUTTON_XPATH = '//*[@class="icon-close"]'
+#endregion
+
+#region - Terms and Condition
+TAC_CLOSE_BUTTON_XPATH = '//*[@class="icon-close"]'
+#endregion
