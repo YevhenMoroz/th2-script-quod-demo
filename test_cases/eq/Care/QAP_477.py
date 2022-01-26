55158892b2ee199c2f1a4eb9a7b5a5d912e25ca5
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-01-26 16:46:27 +0200

    modify 477

--- test_cases/eq/Care/QAP_477.py
+++ test_cases/eq/Care/QAP_477.py
@@ -1,13 +1,10 @@
 import logging
-import os
 from pathlib import Path
 
 from custom import basic_custom_actions as bca
 from rule_management import RuleManager
 from stubs import Stubs
 from test_framework.core.test_case import TestCase
-from test_framework.data_sets.oms_data_set.oms_const_enum import OmsMic, OmsVenueClientNames
-from test_framework.data_sets.oms_data_set.oms_data_set import OmsDataSet
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.SessionAlias import SessionAliasOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
@@ -15,25 +12,22 @@ from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from test_framework.win_gui_wrappers.base_window import try_except
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
-from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
+
 # region TestData
 work_dir = Stubs.custom_config['qf_trading_fe_folder']
 username = Stubs.custom_config['qf_trading_fe_user']
 password = Stubs.custom_config['qf_trading_fe_password']
 ss_connectivity = SessionAliasOMS().ss_connectivity
 bs_connectivity = SessionAliasOMS().bs_connectivity
-
 qty = '100'
 limit = '10'
 instr = 'VETO'
-
-
-
 # endregion
+
 
 class QAP_477(TestCase):
     def __init__(self, report_id, session_id=None, data_set=None):
@@ -42,18 +36,11 @@ class QAP_477(TestCase):
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-
-
-
         # region Declaration
         order_book = OMSOrderBook(self.test_id, self.session_id)
-        ord_ticket = OMSOrderTicket(self.test_id, self.session_id)
         base_window = BaseMainWindow(self.test_id, self.session_id)
         fix_manager = FixManager(ss_connectivity)
         fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
-        # endregion
-        # region open FE
-        base_window.open_fe(self.report_id, work_dir, username, password, True)
         # endregion
         # region Create CO order
         fix_manager.send_message_fix_standard(fix_message)
@@ -64,13 +51,10 @@ class QAP_477(TestCase):
         order_inbox.accept_order(instr, qty, limit)
         # endregion
         # region DirectLoc order
-        # account = fix_message.get_parameter('Account')
         price = fix_message.get_parameter('Price')
         try:
-
             rule_manager = RuleManager()
             venue_client_names = self.data_set.get_venue_client_names_by_name("client_1_venue_1")
-                # self.data_set.venue_client_names.client_1_venue_1)
             venue = self.data_set.get_mic_by_name("mic_1")
             nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew_FIXStandard(bs_connectivity,
                                                                                              venue_client_names, venue,
@@ -87,11 +71,6 @@ class QAP_477(TestCase):
                                                 filter_dict={'Order ID': order_id_first})
         base_window.compare_values({'Sts': 'Open', 'Qty': qty}, result[0], 'Equals value')
         # endregion
-
-    @try_except(test_id=Path(__file__).name[:-3])
-    def run_post_conditions(self):
-        pass
-
 
 
 
