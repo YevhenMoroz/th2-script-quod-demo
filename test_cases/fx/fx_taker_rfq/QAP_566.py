d9ea591ed609ada7fc657a3c0c05c003348f712c
RNenakhov <roman.nenakhov.contractor@quodfinancial.com>
2022-01-26 18:10:27 +0200

    small corrections

--- test_cases/fx/fx_taker_rfq/QAP_566.py
+++ test_cases/fx/fx_taker_rfq/QAP_566.py
@@ -10,7 +10,7 @@ from test_framework.win_gui_wrappers.fe_trading_constant import QuoteRequestBook
     Status
 
 
-case_qty = str(random.randint(1000000, 2000000))
+qty = str(random.randint(1000000, 2000000))
 
 
 class QAP_566(TestCase):
@@ -23,21 +23,21 @@ class QAP_566(TestCase):
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        case_eur_currency = self.data_set.get_currency_by_name('currency_eur')
-        case_usd_currency = self.data_set.get_currency_by_name('currency_usd')
-        case_eur_usd_symbol = self.data_set.get_symbol_by_name('symbol_1')
-        case_near_tenor = self.data_set.get_tenor_by_name('tenor_spot')
-        case_venue = self.data_set.get_venue_by_name('venue_1')
-        case_client = self.data_set.get_client_by_name("client_1")
+        eur_currency = self.data_set.get_currency_by_name('currency_eur')
+        usd_currency = self.data_set.get_currency_by_name('currency_usd')
+        eur_usd_symbol = self.data_set.get_symbol_by_name('symbol_1')
+        near_tenor = self.data_set.get_tenor_by_name('tenor_spot')
+        venue = self.data_set.get_venue_by_name('venue_1')
+        client = self.data_set.get_client_by_name("client_1")
         # region Step 1
-        self.rfq_tile.crete_tile().modify_rfq_tile(from_cur=case_eur_currency, to_cur=case_usd_currency,
-                                                   near_qty=case_qty, near_tenor=case_near_tenor,
-                                                   client=case_client, single_venue=case_venue)
+        self.rfq_tile.crete_tile().modify_rfq_tile(from_cur=eur_currency, to_cur=usd_currency,
+                                                   near_qty=qty, near_tenor=near_tenor,
+                                                   client=client, single_venue=venue)
         self.rfq_tile.send_rfq()
 
         self.quote_request_book.set_filter(
-            [qrb.instrument_symbol.value, case_eur_usd_symbol, qrb.qty.value, case_qty]).check_quote_book_fields_list(
-            {qrb.instrument_symbol.value: case_eur_usd_symbol,
+            [qrb.instrument_symbol.value, eur_usd_symbol, qrb.qty.value, qty]).check_quote_book_fields_list(
+            {qrb.instrument_symbol.value: eur_usd_symbol,
              qrb.quote_status.value: qs.accepted.value,
              qrb.status.value: Status.new.value}, 'Checking that regular currency RFQ is placed')
         # endregion
