7346fa2879d08b36a8403e6bc4004108ac0da2d6
RNenakhov <roman.nenakhov.contractor@quodfinancial.com>
2022-01-27 15:28:32 +0200

    QAP_564 rewritten

--- test_cases/fx/fx_taker_rfq/QAP_564.py
+++ test_cases/fx/fx_taker_rfq/QAP_564.py
@@ -1,156 +1,63 @@
-import logging
 from pathlib import Path
-
-import timestring
 from custom import basic_custom_actions as bca
-from custom.tenor_settlement_date import wk1_front_end, spo_front_end
-from custom.verifier import Verifier
-from stubs import Stubs
-from win_gui_modules.aggregated_rates_wrappers import ModifyRFQTileRequest, ExtractRFQTileValues
-from win_gui_modules.common_wrappers import BaseTileDetails
+from custom.tenor_settlement_date import spo_front_end, wk1_front_end
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.win_gui_wrappers.forex.rfq_tile import RFQTile
 
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_base_request, call, get_opened_fe
-from win_gui_modules.wrappers import set_base
-
-
-def create_or_get_rfq(base_request, service):
-    call(service.createRFQTile, base_request.build())
-
-
-def modify_rfq_tile(base_request, service, near_qty, cur1, cur2, near_tenor, client):
-    modify_request = ModifyRFQTileRequest(details=base_request)
-    modify_request.set_near_tenor(near_tenor)
-    modify_request.set_quantity(near_qty)
-    modify_request.set_from_currency(cur1)
-    modify_request.set_to_currency(cur2)
-    modify_request.set_client(client)
-    call(service.modifyRFQTile, modify_request.build())
+qty = '10000000'
+qty1 = '1m'
+qty_1m = '1000000'
+qty2 = '100k'
+qty_100k = '100000'
+usd_label_left = 'Buy USD'
+usd_label_right = 'Sell USD'
 
 
-def check_date(base_request, service, case_id, date):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.extract_near_settlement_date("aggrRfqTile.nearSettlement")
-    extract_value.set_extraction_id(extraction_id)
-    response = call(service.extractRFQTileValues, extract_value.build())
-    extract_date = response["aggrRfqTile.nearSettlement"]
-    extract_date = timestring.Date(extract_date)
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Verify Tenor date on RFQ tile")
-    verifier.compare_values("Date", date, str(extract_date))
-    verifier.verify()
+class QAP_564(TestCase):
+    def __init__(self, report_id, session_id=None, data_set=None):
+        super().__init__(report_id, session_id, data_set)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.rfq_tile = RFQTile(self.test_id, self.session_id)
 
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        eur_currency = self.data_set.get_currency_by_name('currency_eur')
+        usd_currency = self.data_set.get_currency_by_name('currency_usd')
+        eur_usd_symbol = self.data_set.get_symbol_by_name('symbol_1')
+        near_tenor_1 = self.data_set.get_tenor_by_name('tenor_spot')
+        near_tenor_2 = self.data_set.get_tenor_by_name('tenor_1w')
+        client = self.data_set.get_client_by_name("client_1")
+        venue = self.data_set.get_venue_by_name('venue_1')
 
-def check_qty(base_request, service, case_id, near_qty):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.set_extraction_id(extraction_id)
-    extract_value.extract_quantity("aggrRfqTile.nearqty")
-    response = call(service.extractRFQTileValues, extract_value.build())
-    extract_near_qty = response["aggrRfqTile.nearqty"].replace(',', '')
+        # region Step 1
+        self.rfq_tile.crete_tile().modify_rfq_tile(from_cur=eur_currency, to_cur=usd_currency,
+                                                   near_qty=qty, near_tenor=near_tenor_1,
+                                                   client=client, single_venue=venue)
+        # region Step 2
+        self.rfq_tile.check_currency_pair(currency_pair=eur_usd_symbol)
+        # endregion
+        # region Step 3
+        self.rfq_tile.check_date(near_date=spo_front_end())
+        # endregion
+        # region Step 4
+        self.rfq_tile.crete_tile().modify_rfq_tile(change_currency=True)
+        self.rfq_tile.check_labels(left_label=usd_label_left, right_label=usd_label_right)
+        # endregion
+        # region Step 5
+        self.rfq_tile.crete_tile().modify_rfq_tile(near_qty=qty1)
+        self.rfq_tile.check_qty(near_qty=qty_1m)
+        # endregion
+        # region Step 6
+        self.rfq_tile.crete_tile().modify_rfq_tile(near_qty=qty2)
+        self.rfq_tile.check_qty(near_qty=qty_100k)
+        # endregion
+        # region Step 7
+        self.rfq_tile.crete_tile().modify_rfq_tile(near_tenor=near_tenor_2)
+        self.rfq_tile.check_date(near_date=wk1_front_end())
+        # endregion
+        self.rfq_tile.close_tile()
 
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Verify Qty in RFQ tile")
-    verifier.compare_values('Near leg qty', str(near_qty), extract_near_qty[:-3])
-    verifier.verify()
-
-
-def check_currency(base_request, service, case_id, currency):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.set_extraction_id(extraction_id)
-    extract_value.extract_currency("aggrRfqTile.currency")
-    response = call(service.extractRFQTileValues, extract_value.build())
-    extract_currency = response["aggrRfqTile.currency"]
-
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Check currency on RFQ tile")
-    verifier.compare_values("Currency", currency, extract_currency)
-    verifier.verify()
-
-
-def check_currency_pair(base_request, service, case_id, currency_pair):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.set_extraction_id(extraction_id)
-    extract_value.extract_currency_pair("aggrRfqTile.currencypair")
-    response = call(service.extractRFQTileValues, extract_value.build())
-    extract_currency_pair = response["aggrRfqTile.currencypair"]
-
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Check currency pair on RFQ tile")
-    verifier.compare_values("Currency", currency_pair, extract_currency_pair)
-    verifier.verify()
-
-
-def execute(report_id, session_id):
-    case_name = Path(__file__).name[:-3]
-    case_id = bca.create_event(case_name, report_id)
-    
-    set_base(session_id, case_id)
-
-    case_base_request = get_base_request(session_id, case_id)
-    ar_service = Stubs.win_act_aggregated_rates_service
-    base_rfq_details = BaseTileDetails(base=case_base_request)
-    modify_request = ModifyRFQTileRequest(base_rfq_details)
-
-    case_from_currency = "EUR"
-    case_to_currency = "USD"
-    case_client = "ASPECT_CITI"
-    case_pair = case_from_currency + "/" + case_to_currency
-    case_qty = 10000000
-    case_qty2 = "1m"
-    case_qty_1m = "1000000"
-    case_qty3 = "100k"
-    case_qty_100kk = "100000"
-    case_tenor1 = "Spot"
-    case_tenor2 = "1W"
-    spo_front_end()
-    wk1_front_end()
-
-    try:
-
-        # Step 1
-        create_or_get_rfq(base_rfq_details, ar_service)
-
-        # Step 2
-        modify_rfq_tile(base_rfq_details, ar_service, case_qty, case_from_currency,
-                        case_to_currency, case_tenor1, case_client)
-        check_currency_pair(base_rfq_details, ar_service, case_id, case_pair)
-
-        # Step 3
-        check_date(base_rfq_details, ar_service, case_id, spo_front_end())
-
-        # Step 4
-        modify_request.set_change_currency(True)
-        call(ar_service.modifyRFQTile, modify_request.build())
-        check_currency(base_rfq_details, ar_service, case_id, case_to_currency)
-
-        # Step 5
-        modify_request.set_quantity_as_string(case_qty2)
-        modify_request.set_change_currency(False)
-        call(ar_service.modifyRFQTile, modify_request.build())
-        check_qty(base_rfq_details, ar_service, case_id, case_qty_1m)
-
-        # Step 6
-        modify_request.set_quantity_as_string(case_qty3)
-        modify_request.set_change_currency(False)
-        call(ar_service.modifyRFQTile, modify_request.build())
-        check_qty(base_rfq_details, ar_service, case_id, case_qty_100kk)
-
-        # Step 7
-        modify_request.set_near_tenor(case_tenor2)
-        modify_request.set_change_currency(False)
-        call(ar_service.modifyRFQTile, modify_request.build())
-        check_date(base_rfq_details, ar_service, case_id, wk1_front_end())
-        call(ar_service.closeRFQTile, base_rfq_details.build())
-
-    except Exception:
-        logging.error("Error execution", exc_info=True)
-        bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
-    finally:
-        try:
-            # Close tile
-            call(ar_service.closeRFQTile, base_rfq_details.build())
-        except Exception:
-            logging.error("Error execution", exc_info=True)
+    @try_except
+    def run_post_conditions(self):
+        self.rfq_tile.close_tile()
