8b038aeaf0be5f26e83604949df52be3273b08f1
amedents <artem.medentsev.contractor@quodfinancial.com>
2022-01-27 15:21:13 +0200

    demo and fx_const_enum are refactored; QAP_6531 is added.

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 
-from MyFiles import SendMD
+# MyFiles import SendMD
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 from test_cases.fx.fx_mm_autohedging import QAP_2470, QAP_3082
@@ -12,10 +12,13 @@ from test_cases.fx.fx_mm_esp import QAP_2069, QAP_1536, QAP_1559, QAP_1511, QAP_
 from test_cases.fx.fx_mm_esp.QAP_5389 import QAP_5389
 from test_cases.fx.fx_mm_rfq import QAP_2101, QAP_3003, QAP_4777, QAP_3494, QAP_3409, QAP_3110, QAP_3111, QAP_2103, \
     QAP_1537
+from test_cases.fx.fx_mm_rfq.QAP_5992 import QAP_5992
 from test_cases.fx.fx_mm_rfq.QAP_6364 import QAP_6364
+from test_cases.fx.fx_mm_rfq.QAP_6531 import QAP_6531
 from test_cases.fx.fx_mm_rfq.interpolation import QAP_3766, QAP_3762, QAP_3739
 from test_cases.fx.fx_taker_esp import QAP_3141, QAP_2373
 from test_cases.fx.fx_taker_rfq import QAP_569, QAP_574, QAP_2836, QAP_2847, QAP_849, QAP_3048
+from test_cases.fx.fx_taker_rfq.QAP_6 import QAP_6
 from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
 from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
@@ -48,10 +51,11 @@ def test_run():
             prepare_fe_2(report_id, session_id)
         else:
             get_opened_fe(report_id, session_id)
-    # #     QAP_3141(report_id, session_id, data_set).execute()
-    # #     QAP_3766.execute(report_id)
-        QAP_1643.execute(report_id, session_id)
-        SendMD.execute(report_id)
+        QAP_6531(report_id, session_id, data_set).execute()
+        # QAP_5992(report_id, data_set=data_set).execute()
+    #     QAP_3766.execute(report_id)
+        #QAP_653.execute(report_id, session_id)
+        #SendMD.execute(report_id)
 
 
 
