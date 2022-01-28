a0e5f5a5a5b4f4e59f109078963413b82e136681
Yaroslav <yaroslav.furmuzal17@gmail.com>
2022-01-28 16:02:19 +0200

    QAP-5943: Rename file main_page_constants.py. on main_page.py and added main_page_constants.py.

---
+++ test_framework/web_trading/web_trading_core/pages/main_page/main_page.py
@@ -0,0 +1,44 @@
+import time
+
+from test_cases.web_admin.web_admin_core.pages.common_page import CommonPage
+from test_cases.web_admin.web_admin_core.utils.web_driver_container import WebDriverContainer
+
+
+class MainPage(CommonPage):
+    def __init__(self, web_driver_container: WebDriverContainer):
+        super().__init__(web_driver_container)
+
+    #TODO: implement methods, if something changed in FE part, please take a look
+
+    def click_on_new_workspace_button(self):
+        pass
+
+    def click_on_close_new_workspace_button(self):
+        pass
+
+    def click_on_buy_button(self):
+        pass
+
+    def click_on_sell_button(self):
+        pass
+
+    def click_on_watch_list_button(self):
+        pass
+
+    def click_on_order_book_button(self):
+        pass
+
+    def click_on_trades_button(self):
+        pass
+
+    def click_on_position_button(self):
+        pass
+
+    def click_on_position_button(self):
+        pass
+
+    def click_on_symbol_details_button(self):
+        pass
+
+    def click_on_menu_button(self):
+        pass
