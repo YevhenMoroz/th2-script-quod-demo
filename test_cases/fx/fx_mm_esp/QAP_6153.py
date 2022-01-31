811f0676ba792ab338c4e95d42cce9ad71750464
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2022-01-31 10:45:29 +0200

    QAP_6153 rewritten

--- test_cases/fx/fx_mm_esp/QAP_6153.py
+++ test_cases/fx/fx_mm_esp/QAP_6153.py
@@ -23,6 +23,22 @@ class QAP_6153(TestCase):
         super().__init__(report_id, session_id, data_set)
         self.fix_act = Stubs.fix_act
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.ss_connectivity = None
+        self.rest_connectivity = None
+        self.rest_manager = None
+        self.fix_subscribe = None
+        self.fix_md_snapshot = None
+        self.fix_manager_gtw = None
+        self.fix_verifier = None
+        self.rest_massage = None
+        self.nok_sek = None
+        self.eur_usd = None
+        self.no_related_symbols = None
+        self.bands = None
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Test variables
         self.ss_connectivity = SessionAliasFX().ss_esp_connectivity
         self.rest_connectivity = SessionAliasFX().wa_connectivity
         self.rest_manager = RestApiManager(self.rest_connectivity, self.test_id)
@@ -31,23 +47,23 @@ class QAP_6153(TestCase):
         self.fix_manager_gtw = FixManager(self.ss_connectivity, self.test_id)
         self.fix_verifier = FixVerifier(self.ss_connectivity, self.test_id)
         self.rest_massage = RestApiClientTierInstrSymbolMessages()
+        self.nok_sek = self.data_set.get_symbol_by_name('symbol_synth_1')
+        self.eur_usd = self.data_set.get_symbol_by_name('symbol_1')
         self.no_related_symbols = [{
             'Instrument': {
-                'Symbol': 'NOK/SEK',
-                'SecurityType': 'FXSPOT',
+                'Symbol': self.nok_sek,
+                'SecurityType': self.data_set.get_security_type_by_name('fx_spot'),
                 'Product': '4', },
             'SettlType': '0', }]
         self.bands = ["1000000", '3000000']
-
-    @try_except(test_id=Path(__file__).name[:-3])
-    def run_pre_conditions_and_steps(self):
+        # endregion
         # region Step 1
         self.rest_massage.find_all_client_tier_instrument()
         params_eur_usd = self.rest_manager.send_get_request(self.rest_massage)
         params_eur_usd = self.rest_manager. \
             parse_response_details(
             params_eur_usd,
-            {'clientTierID': '2000011', 'instrSymbol': "EUR/USD"})
+            {'clientTierID': '2000011', 'instrSymbol': self.eur_usd})
         # endregion
 
         # region Step 2
@@ -70,19 +86,15 @@ class QAP_6153(TestCase):
         # endregion
 
         # region Postconditions
-        self.rest_massage.modify_client_tier_instrument(). \
+        @try_except(test_id=Path(__file__).name[:-3])
+        def run_post_conditions(self):
+            self.rest_massage.modify_client_tier_instrument(). \
                 update_value_in_component('clientTierInstrSymbolTenor', 'MDQuoteType', 'TRD', {'tenor': 'SPO'})
-        self.rest_manager.send_post_request(self.rest_massage)
-        self.fix_subscribe.set_md_uns_parameters_maker()
-        self.fix_manager_gtw.send_message(self.fix_subscribe, 'Unsubscribe')
+            self.rest_manager.send_post_request(self.rest_massage)
+            self.fix_subscribe.set_md_uns_parameters_maker()
+            self.fix_manager_gtw.send_message(self.fix_subscribe, 'Unsubscribe')
         # endregion
 
-    # @try_except
-    # def run_post_conditions(self):
-    #     self.rest_massage.modify_client_tier_instrument(). \
-    #         update_value_in_component('clientTierInstrSymbolTenor', 'MDQuoteType', 'TRD', {'tenor': 'SPO'})
-    #     self.rest_manager.send_post_request(self.rest_massage)
-    #     self.fix_subscribe.set_md_uns_parameters_maker()
-    #     self.fix_manager_gtw.send_message(self.fix_subscribe, 'Unsubscribe')
+
 
 
