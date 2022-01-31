68a1c7442214bdfd9c9683db0dedf5253c4fa73a
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-01-31 16:06:40 +0200

    modify 1022

--- test_cases/eq/Care/QAP_1022.py
+++ test_cases/eq/Care/QAP_1022.py
@@ -1,20 +1,17 @@
 import logging
-from datetime import datetime
-
 from th2_grpc_hand import rhbatch_pb2
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from test_framework.core.test_case import TestCase
 from custom.basic_custom_actions import create_event, timestamps
 from stubs import Stubs
-from test_framework.old_wrappers import eq_wrappers
-from test_framework.old_wrappers.eq_wrappers import switch_user
+from test_framework.core.try_exept_decorator import try_except
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
-from win_gui_modules.utils import get_base_request, prepare_fe, get_opened_fe, close_fe
-from win_gui_modules.wrappers import set_base
+from win_gui_modules.utils import  close_fe
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -38,29 +35,24 @@ class QAP_1022(TestCase):
         super().__init__(report_id, session_id, data_set)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
 
+    @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        case_name = "QAP-1022"
-        seconds, nanos = timestamps()  # Store case start time
-
         # region Declarations
-        act = Stubs.win_act_order_book
         client = self.data_set.get_client_by_name('client_co_1')
         lookup = self.data_set.get_lookup_by_name('lookup_1')
         # endregion
         # region Open FE
-        stub = Stubs.win_act
-        # case_id = create_event(case_name, report_id)
         session_id2 = Stubs.win_act.register(
             rhbatch_pb2.RhTargetServer(target=Stubs.custom_config['target_server_win'])).sessionID
         base_window = BaseMainWindow(self.test_id, self.session_id)
         base_window2 = BaseMainWindow(self.test_id, session_id2)
         order_ticket = OMSOrderTicket(self.test_id, self.session_id)
+        order_ticket2 = OMSOrderTicket(self.test_id, session_id2)
         order_book = OMSOrderBook(self.test_id, self.session_id)
         client_inbox = OMSClientInbox(self.test_id, self.session_id)
-
+        order_book2 = OMSOrderBook(self.test_id, session_id2)
         # endregion
         # region Switch to user1
-        base_window.open_fe(self.report_id, work_dir, username, password, True)
         base_window2.open_fe(self.report_id, work_dir, username2, password2, False)
         # endregion
         # region Create CO
@@ -74,6 +66,7 @@ class QAP_1022(TestCase):
             {"Sts": "Sent"})
         # endregion
         # region Accept CO
+        base_window.switch_user()
         client_inbox.accept_order(lookup, qty, price)
         # endregion
         # region Check values in OrderBook after Accept
@@ -81,28 +74,24 @@ class QAP_1022(TestCase):
             {"Sts": "Open"})
         # endregion
         # region Switch to user2
-
-
         # endregion
         # region Amend order
         base_window2.switch_user()
-        order_ticket.set_order_details(limit=price2, qty=qty2)
-        order_ticket.amend_order(['Order ID', order_id])
-        client_inbox.accept_order(lookup, qty2, price2)
+        order_ticket2.set_order_details(limit=price2, qty=qty2)
+        order_ticket2.amend_order(['Order ID', order_id])
+        base_window.switch_user()
+        client_inbox.accept_modify_plus_child(lookup, qty2, price2)
         order_book.check_order_fields_list(
             {"Qty": qty2, "Limit": price2})
         # endregion
-
         # region Cancelling order
-        base_window.switch_user()
-        order_book.cancel_order()
-        # endregion
-
-        # region Check values after Cancel
         base_window2.switch_user()
+        order_book2.cancel_order()
+        # endregion
+        # region Check values after Cancel
+        base_window.switch_user()
         client_inbox.accept_and_cancel_children(lookup, qty2, price2)
         order_book.set_filter(['Order ID', order_id]).check_order_fields_list(
             {"Sts": "Cancelled"})
         # endregion
-
-        logger.info(f"Case {case_name} was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
+        close_fe(self.test_id, session_id2)
