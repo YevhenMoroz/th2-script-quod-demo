0fc2179d92ad58f4908e7e452fbb2ae57a895780
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-01-31 16:06:52 +0200

    modify 1025

--- test_cases/eq/Care/QAP_1025.py
+++ test_cases/eq/Care/QAP_1025.py
@@ -6,11 +6,12 @@ from custom import basic_custom_actions as bca
 from test_framework.core.test_case import TestCase
 from custom.basic_custom_actions import create_event
 from stubs import Stubs
+from test_framework.core.try_exept_decorator import try_except
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
-from win_gui_modules.utils import set_session_id, get_base_request
+from win_gui_modules.utils import  close_fe
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -30,6 +31,7 @@ class QAP_1025(TestCase):
         super().__init__(report_id, session_id, data_set)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
 
+    @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
         # region Declarations
         client = self.data_set.get_client_by_name('client_co_1')
@@ -40,10 +42,10 @@ class QAP_1025(TestCase):
         base_window2 = BaseMainWindow(self.test_id, session_id2)
         order_ticket = OMSOrderTicket(self.test_id, self.session_id)
         order_book = OMSOrderBook(self.test_id, self.session_id)
+        order_book2 = OMSOrderBook(self.test_id, session_id2)
         client_inbox = OMSClientInbox(self.test_id, self.session_id)
         # endregion
         # region open FE
-        base_window.open_fe(self.report_id, work_dir, username, password, True)
         base_window2.open_fe(self.report_id, work_dir, username2, password2, False)
         # endregion
         # region switch user 1
@@ -55,15 +57,14 @@ class QAP_1025(TestCase):
         order_ticket.create_order(lookup=lookup)
         order_id = order_book.extract_field('Order ID')
         # endregion
-        base_window2.switch_user()
         order_book.set_filter(['Order ID', order_id]).check_order_fields_list(
             {"Sts": "Sent"})
         # region switch user 2
         # endregion
         # region Reassign order
         base_window2.switch_user()
-        order_book.reassign_order(username)
-        order_book.set_filter(['Order ID', order_id]).check_order_fields_list(
+        order_book2.reassign_order(username)
+        order_book2.set_filter(['Order ID', order_id]).check_order_fields_list(
             {"Sts": "Sent"})
         # endregion
         # region switch user 1
@@ -74,5 +75,6 @@ class QAP_1025(TestCase):
         order_book.set_filter(['Order ID', order_id]).check_order_fields_list(
             {"Sts": "Open"})
         # endregion
+        close_fe(self.test_id, session_id2)
 
 
