425458596b66f6126d96f506ccb981b4d6fde028
Yevhen Moroz <yevhen.moroz.contractor@quodfinancial.com>
2022-01-31 16:23:55 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,9 @@
 import logging
 from datetime import datetime
 from pathlib import Path
-
 from custom import basic_custom_actions as bca
 from stubs import Stubs
-from test_cases.eq.DMA.QAP_2000 import QAP_2000
+from test_cases.eq.Care.QAP_1016 import QAP_1016
 from test_framework.data_sets.oms_data_set.oms_data_set import OmsDataSet
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from win_gui_modules.utils import set_session_id
@@ -16,15 +15,15 @@ logger.setLevel(logging.INFO)
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event('mantonov ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    report_id = bca.create_event('test ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     session_id = set_session_id()
-    data_set = OmsDataSet()
     base_main_window = BaseMainWindow(bca.create_event(Path(__file__).name[:-3], report_id), session_id)
+    data_set = OmsDataSet()
 
     try:
-        base_main_window.open_fe(report_id)
-        QAP_2000(report_id, session_id, data_set).execute()
+        base_main_window.open_fe(report_id=report_id)
+        QAP_1016(report_id, data_set).execute()
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
