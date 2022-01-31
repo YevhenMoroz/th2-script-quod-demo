5e4a06d3e55aa7226ca31f92788978cf628e63e1
amedents <artem.medentsev.contractor@quodfinancial.com>
2022-01-31 15:57:29 +0200

    QAP_1418 is renewed

--- test_cases/fx/fx_mm_esp/QAP_1418.py
+++ test_cases/fx/fx_mm_esp/QAP_1418.py
@@ -1,192 +1,150 @@
 import logging
+import time
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from custom.verifier import Verifier
 from stubs import Stubs
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.data_sets.base_data_set import BaseDataSet
+from test_framework.win_gui_wrappers.fe_trading_constant import RatesColumnNames, ClientPrisingTileAction, PriceNaming
+from test_framework.win_gui_wrappers.forex.client_rates_tile import ClientRatesTile
 from win_gui_modules.client_pricing_wrappers import ModifyRatesTileRequest, ExtractRatesTileValues
 from win_gui_modules.common_wrappers import BaseTileDetails
 from win_gui_modules.utils import call, get_base_request, set_session_id, prepare_fe_2, get_opened_fe
 from win_gui_modules.wrappers import set_base
+from win_gui_modules.order_book_wrappers import ExtractionDetail
+from datetime import datetime, timedelta
 
 
-def create_or_get_rates_tile(base_request, service):
-    call(service.createRatesTile, base_request.build())
+pips_1 = "1"
+pips_2 = "2"
+pips_3 = "3"
+pips_4 = "4"
+pips_5 = "5"
+
+ask_base = RatesColumnNames.ask_base
+bid_base = RatesColumnNames.bid_base
+action = ClientPrisingTileAction.widen_spread
+
+ask_pips = PriceNaming.ask_pips
+bid_pips = PriceNaming.bid_pips
+
+spread = PriceNaming.spread
+widen_spread = ClientPrisingTileAction.widen_spread
+narrow_spread = ClientPrisingTileAction.narrow_spread
+increase_ask = ClientPrisingTileAction.increase_ask
+decrease_bid = ClientPrisingTileAction.decrease_bid
+skew_towards_bid = ClientPrisingTileAction.skew_towards_bid
+skew_towards_ask = ClientPrisingTileAction.skew_towards_ask
 
 
-def modify_rates_tile(base_request, service, instrument, client, pips):
-    modify_request = ModifyRatesTileRequest(details=base_request)
-    modify_request.set_instrument(instrument)
-    modify_request.set_client_tier(client)
-    modify_request.set_pips(pips)
-    call(service.modifyRatesTile, modify_request.build())
 
 
-def use_default(base_request, service):
-    modify_request = ModifyRatesTileRequest(details=base_request)
-    modify_request.press_use_defaults()
-    call(service.modifyRatesTile, modify_request.build())
+class QAP_1418(TestCase):
+    def __init__(self, report_id, session_id=None, data_set: BaseDataSet = None):
+        super().__init__(report_id, session_id, data_set)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.rates_tile = None
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Initialization
+        self.rates_tile = ClientRatesTile(self.test_id, self.session_id)
+        # endregion
+        # region Variables
+        client = self.data_set.get_client_tier_by_name("client_tier_1")
+        symbol = self.data_set.get_symbol_by_name("symbol_1")
+        instrument = symbol + "-Spot"
+        spread_event = "Spread validation"
+        ask_event = "Ask validation"
+        bid_event = "Bid validation"
+        # end region
+
+        # region step 1-2
+        self.rates_tile.crete_tile()
+        self.rates_tile.modify_client_tile(instrument=instrument, client_tier=client)
+        self.rates_tile.press_use_default()
+        bid_n_ask_values = self.rates_tile.extract_prices_from_tile(bid_pips, ask_pips)
+        actual_spread = self.rates_tile.extract_prices_from_tile(spread)[spread.value]
+        expected_spread = str((float(bid_n_ask_values[bid_pips.value]) - float(bid_n_ask_values[ask_pips.value])) * -0.1)
+        self.rates_tile.compare_values(expected_spread, actual_spread,
+                                       event_name=spread_event)
+        # endregion
+
+        # region step 3
+        self.rates_tile.modify_client_tile(pips=pips_2)
+        self.rates_tile.modify_spread(widen_spread)
+        prev_spread = actual_spread
+        actual_spread = self.rates_tile.extract_prices_from_tile(spread)[spread.value]
+        expected_spread = str(float(prev_spread) + int(pips_2) * 2)
+        self.rates_tile.compare_values(expected_spread, actual_spread,
+                                       event_name=spread_event)
+        # endregion
+
+        # region step 4
+        self.rates_tile.modify_client_tile(pips=pips_1)
+        self.rates_tile.modify_spread(narrow_spread)
+        prev_spread = actual_spread
+        actual_spread = self.rates_tile.extract_prices_from_tile(spread)[spread.value]
+        expected_spread = str(round(float(prev_spread) - int(pips_1) * 2, 1))
+        self.rates_tile.compare_values(expected_spread, actual_spread,
+                                       event_name=spread_event)
+        # endregion
+
+        # region step 5
+        self.rates_tile.modify_client_tile(pips=pips_2)
+        self.rates_tile.modify_spread(increase_ask)
+        prev_spread = actual_spread
+        actual_spread = self.rates_tile.extract_prices_from_tile(spread)[spread.value]
+        expected_spread = str(round(float(prev_spread) - int(pips_2), 1))
+        self.rates_tile.compare_values(expected_spread, actual_spread,
+                                       event_name=spread_event)
+        # endregion
+
+        # region step 6
+        self.rates_tile.modify_client_tile(pips=pips_3)
+        self.rates_tile.modify_spread(decrease_bid)
+        prev_spread = actual_spread
+        actual_spread = self.rates_tile.extract_prices_from_tile(spread)[spread.value]
+        expected_spread = str(round(float(prev_spread) + int(pips_3), 1))
+        self.rates_tile.compare_values(expected_spread, actual_spread,
+                                       event_name=spread_event)
+        # endregion
+
+        # region step 7
+        bid_n_ask_values = self.rates_tile.extract_prices_from_tile(bid_pips, ask_pips)
+        expected_bid = str(int(bid_n_ask_values[bid_pips.value]) - 40)
+        expected_ask = str(int(bid_n_ask_values[ask_pips.value]) - 40)
+        self.rates_tile.modify_client_tile(pips=pips_4)
+        self.rates_tile.modify_spread(skew_towards_bid)
+        bid_n_ask_values = self.rates_tile.extract_prices_from_tile(bid_pips, ask_pips)
+        actual_bid = str(int(bid_n_ask_values[bid_pips.value]))
+        actual_ask = bid_n_ask_values[ask_pips.value]
+        self.rates_tile.compare_values(expected_ask, actual_ask,
+                                       event_name=ask_event)
+        self.rates_tile.compare_values(expected_bid, actual_bid,
+                                       event_name=bid_event)
+        # endregion
+
+        # region step 8
+        bid_n_ask_values = self.rates_tile.extract_prices_from_tile(bid_pips, ask_pips)
+        expected_bid = str(int(bid_n_ask_values[bid_pips.value]) + 50)
+        expected_ask = str(int(bid_n_ask_values[ask_pips.value]) + 50)
+        self.rates_tile.modify_client_tile(pips=pips_5)
+        self.rates_tile.modify_spread(skew_towards_ask)
+        bid_n_ask_values = self.rates_tile.extract_prices_from_tile(bid_pips, ask_pips)
+        actual_bid = bid_n_ask_values[bid_pips.value]
+        actual_ask = bid_n_ask_values[ask_pips.value]
+        self.rates_tile.compare_values(expected_ask, actual_ask,
+                                       event_name=ask_event)
+        self.rates_tile.compare_values(expected_bid, actual_bid,
+                                       event_name=bid_event)
+        # endregion
 
 
-def modify_spread(base_request, service, *args):
-    modify_request = ModifyRatesTileRequest(details=base_request)
-    if "increase_ask" in args:
-        modify_request.increase_ask()
-    if "decrease_ask" in args:
-        modify_request.decrease_ask()
-    if "increase_bid" in args:
-        modify_request.increase_bid()
-    if "decrease_bid" in args:
-        modify_request.decrease_bid()
-    if "narrow_spread" in args:
-        modify_request.narrow_spread()
-    if "widen_spread" in args:
-        modify_request.widen_spread()
-    if "skew_towards_ask" in args:
-        modify_request.skew_towards_ask()
-    if "skew_towards_bid" in args:
-        modify_request.skew_towards_bid()
-    call(service.modifyRatesTile, modify_request.build())
 
-
-def check_bid(base_request, service):
-    extract_value_request = ExtractRatesTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value_request.set_extraction_id(extraction_id)
-    extract_value_request.extract_bid_large_value("rates_tile.bid_large")
-    extract_value_request.extract_bid_pips("rates_tile.bid_pips")
-    response = call(service.extractRateTileValues, extract_value_request.build())
-    bid = float(response["rates_tile.bid_large"] + response["rates_tile.bid_pips"])
-    return float(bid)
-
-
-def check_ask(base_request, service):
-    extract_value_request = ExtractRatesTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value_request.set_extraction_id(extraction_id)
-    extract_value_request.extract_ask_large_value("rates_tile.ask_large")
-    extract_value_request.extract_ask_pips("rates_tile.ask_pips")
-    response = call(service.extractRateTileValues, extract_value_request.build())
-    ask = float(response["rates_tile.ask_large"] + response["rates_tile.ask_pips"])
-    return float(ask)
-
-
-def compare_prices(case_id, ask_before, bid_before, ask_after, bid_after, pips, ):
-    ask_difference = abs(round(ask_after - ask_before, 5))
-    bid_difference = abs(round(bid_after - bid_before, 5))
-    expected_dif = float(pips) / 10000
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Compare prices")
-    verifier.compare_values("Price ask", str(expected_dif), str(ask_difference))
-    verifier.compare_values("Price bid", str(expected_dif), str(bid_difference))
-    verifier.verify()
-
-
-def check_spread(base_request, service, case_id):
-    extract_value_request = ExtractRatesTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value_request.set_extraction_id(extraction_id)
-    extract_value_request.extract_spread("rates_tile.spread")
-    extract_value_request.extract_ask_large_value("rates_tile.ask_large")
-    extract_value_request.extract_bid_large_value("rates_tile.bid_large")
-    extract_value_request.extract_ask_pips("rates_tile.ask_pips")
-    extract_value_request.extract_bid_pips("rates_tile.bid_pips")
-    response = call(service.extractRateTileValues, extract_value_request.build())
-
-    bid = float(response["rates_tile.bid_large"]) + float(response["rates_tile.bid_pips"])
-    ask = float(response["rates_tile.ask_large"]) + float(response["rates_tile.ask_pips"])
-    extracted_spread = response["rates_tile.spread"]
-    calculated_spread = round((ask - bid)/10, 1)
-
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Check calculation of spread")
-    verifier.compare_values("Spread", str(calculated_spread), extracted_spread)
-    verifier.verify()
-    return float(extracted_spread)
-
-
-def compare_spreads(case_id, spread_before, spread_after, pips, *args):
-    difference = abs(round(spread_after - spread_before, 1))
-    expected_difference = 0
-    if "increase_ask" in args:
-        expected_difference = float(pips)
-    if "decrease_ask" in args:
-        expected_difference = float(pips)
-    if "increase_bid" in args:
-        expected_difference = float(pips)
-    if "decrease_bid" in args:
-        expected_difference = float(pips)
-    if "narrow_spread" in args:
-        expected_difference = float(pips) * 2
-    if "widen_spread" in args:
-        expected_difference = float(pips) * 2
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Compare spreads")
-    verifier.compare_values("Spread", str(expected_difference), str(difference))
-    verifier.verify()
-
-
-def execute(report_id, session_id):
-    case_name = Path(__file__).name[:-3]
-    case_id = bca.create_event(case_name, report_id)
-
-    set_base(session_id, case_id)
-
-    cp_service = Stubs.win_act_cp_service
-
-    case_base_request = get_base_request(session_id, case_id)
-    base_details = BaseTileDetails(base=case_base_request)
-    instrument = "EUR/USD-Spot"
-    client_tier = "Silver"
-    pips = "0.5"
-
-    try:
-        # Step 1
-        create_or_get_rates_tile(base_details, cp_service)
-        modify_rates_tile(base_details, cp_service, instrument, client_tier, pips)
-        # Step 2
-        spread_before = check_spread(base_details, cp_service, case_id)
-        # Step 3
-        modify_spread(base_details, cp_service, "narrow_spread")
-        spread_after = check_spread(base_details, cp_service, case_id)
-        compare_spreads(case_id, spread_before, spread_after, pips, "narrow_spread")
-        # Step 4
-        spread_before = check_spread(base_details, cp_service, case_id)
-        modify_spread(base_details, cp_service, "widen_spread")
-        spread_after = check_spread(base_details, cp_service, case_id)
-        compare_spreads(case_id, spread_before, spread_after, pips, "widen_spread")
-        # Step 5
-        spread_before = check_spread(base_details, cp_service, case_id)
-        modify_spread(base_details, cp_service, "increase_ask")
-        spread_after = check_spread(base_details, cp_service, case_id)
-        compare_spreads(case_id, spread_before, spread_after, pips, "increase_ask")
-        # Step 6
-        spread_before = check_spread(base_details, cp_service, case_id)
-        modify_spread(base_details, cp_service, "decrease_bid")
-        spread_after = check_spread(base_details, cp_service, case_id)
-        compare_spreads(case_id, spread_before, spread_after, pips, "decrease_bid")
-        # Step 7
-        ask_before = check_ask(base_details, cp_service)
-        bid_before = check_bid(base_details, cp_service)
-        modify_spread(base_details, cp_service, "skew_towards_ask")
-        ask_after = check_ask(base_details, cp_service)
-        bid_after = check_bid(base_details, cp_service)
-        compare_prices(case_id, ask_before, bid_before, ask_after, bid_after, pips)
-        # Step 8
-        ask_before = check_ask(base_details, cp_service)
-        bid_before = check_bid(base_details, cp_service)
-        modify_spread(base_details, cp_service, "skew_towards_bid")
-        ask_after = check_ask(base_details, cp_service)
-        bid_after = check_bid(base_details, cp_service)
-        compare_prices(case_id, ask_before, bid_before, ask_after, bid_after, pips)
-        use_default(base_details, cp_service)
-
-    except Exception:
-        logging.error("Error execution", exc_info=True)
-        bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
-    finally:
-        try:
-            # Close tile
-            call(cp_service.closeRatesTile, base_details.build())
-
-        except Exception:
-            logging.error("Error execution", exc_info=True)
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.rates_tile.press_use_default()
+        self.rates_tile.close_tile()
