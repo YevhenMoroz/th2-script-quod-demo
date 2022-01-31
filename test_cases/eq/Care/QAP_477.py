d56544266b29a848a1c20e9136f9b86ee8574c86
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-01-31 16:06:00 +0200

    modify 477

--- test_cases/eq/Care/QAP_477.py
+++ test_cases/eq/Care/QAP_477.py
@@ -6,13 +6,13 @@ from custom import basic_custom_actions as bca
 from rule_management import RuleManager
 from stubs import Stubs
 from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
 from test_framework.data_sets.oms_data_set.oms_const_enum import OmsMic, OmsVenueClientNames
 from test_framework.data_sets.oms_data_set.oms_data_set import OmsDataSet
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.SessionAlias import SessionAliasOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from test_framework.win_gui_wrappers.base_window import try_except
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
@@ -32,11 +32,6 @@ qty = '100'
 limit = '10'
 instr = 'VETO'
 
-
-
-# endregion
-
-
 class QAP_477(TestCase):
     def __init__(self, report_id, session_id=None, data_set=None):
         super().__init__(report_id, session_id, data_set)
@@ -48,6 +43,9 @@ class QAP_477(TestCase):
         order_book = OMSOrderBook(self.test_id, self.session_id)
         ord_ticket = OMSOrderTicket(self.test_id, self.session_id)
         base_window = BaseMainWindow(self.test_id, self.session_id)
+        venue_client_names = self.data_set.get_venue_client_names_by_name("client_1_venue_1")
+        venue = self.data_set.get_mic_by_name("mic_1")
+        route = self.data_set.get_route("route_1")
         fix_manager = FixManager(ss_connectivity)
         fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
         # endregion
@@ -63,12 +61,9 @@ class QAP_477(TestCase):
         price = fix_message.get_parameter('Price')
         try:
             rule_manager = RuleManager()
-            venue_client_names = self.data_set.get_venue_client_names_by_name("client_1_venue_1")
-            venue = self.data_set.get_mic_by_name("mic_1")
             nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew_FIXStandard(bs_connectivity,
                                                                                              venue_client_names, venue,
                                                                                              float(price))
-            route = self.data_set.get_route("route_1")
             order_book.direct_loc_order_correct(fix_message.get_parameter('OrderQtyData')['OrderQty'], route)
         except Exception:
             logger.error("Error execution", exc_info=True)
