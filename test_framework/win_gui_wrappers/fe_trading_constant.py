2b2f92ca70a39a660314d505333c24157de2ac8a
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2022-02-01 18:03:47 +0200

    Add some function

--- test_framework/win_gui_wrappers/fe_trading_constant.py
+++ test_framework/win_gui_wrappers/fe_trading_constant.py
@@ -11,32 +11,13 @@ class OrderBookColumns(Enum):
     cl_ord_id = "ClOrdID"
     security_id = "Security Id"
     symbol = "Symbol"
-    side = "Side"
-    lookup = "Lookup"
-    stop_price = "Stop Price"
-    inst_type = "InstType"
-    venue = "Venue"
-    sts = "Sts"
-    exec_progress = "ExecProgress"
-    exec_sts = "ExecSts"
-    cd_sts = "CDSts"
-    qty = "Qty"
-    display_qty = "DisplayQty"
-    unmatched_qty = "UnmatchedQty"
-    limit_price = "Limit Price"
-    leaves_qty = "LeavesQty"
-    cum_qty = "CumQty"
-    avg_price = "AvgPrice"
-    ord_type = "OrdType"
-    tif = "TIF"
-    orig = 'Orig'
-    expire_date = "ExpireDate"
-    owner = "Owner"
-    client_id = "Client ID"
+
     client_name = "Client Name"
     free_notes = 'FreeNotes'
     instrument_type = 'InstrType'
     currency = 'Currency'
+    venue_client_account = "Venue Client Account"
+    account_id = 'Account ID'
     # region Executions
     exec_price = 'ExecPrice'
     exec_id = 'ExecID'
