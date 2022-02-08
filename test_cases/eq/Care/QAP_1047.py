f6f7ea226aecc48f181940f70cedb5ce841198ca
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-02-08 19:00:25 +0200

    modify QAP_1047

--- test_cases/eq/Care/QAP_1047.py
+++ test_cases/eq/Care/QAP_1047.py
@@ -1,80 +1,66 @@
 import logging
-import os
-import time
-
-from custom import basic_custom_actions as bca
+from pathlib import Path
 from rule_management import RuleManager
-from stubs import Stubs
-from test_framework.fix_wrappers.DataSet import MessageType
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from custom import basic_custom_actions as bca
+from test_framework.fix_wrappers.DataSet import Connectivity
 from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.FixMessageOrderCancelRequest import FixMessageOrderCancelRequest
-from test_framework.fix_wrappers.SessionAlias import SessionAliasOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.win_gui_wrappers.TestCase import TestCase
-from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from test_framework.win_gui_wrappers.base_window import decorator_try_except
-from test_framework.win_gui_wrappers.data_set import OrderBookColumns, SecondLevelTabs
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts, OrderType
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
-from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
-
+@try_except(test_id=Path(__file__).name[:-3])
 class QAP_1047(TestCase):
-    def __init__(self, report_id, session_id, file_name):
-        super().__init__(report_id, session_id)
-        self.case_id = bca.create_event(os.path.basename(__file__), self.test_id)
-        self.file_name = file_name
-        self.ss_connectivity = SessionAliasOMS().ss_connectivity
-        self.bs_connectivity = SessionAliasOMS().bs_connectivity
 
-    def qap_1047(self):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None):
+        super().__init__(report_id, session_id, data_set)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.ss_connectivity = Connectivity.Ganymede_317_ss.value
+        self.bs_connectivity = Connectivity.Ganymede_317_bs.value
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.route = self.data_set.get_route("route_1")
+        self.venue = self.data_set.get_mic_by_name("mic_1")
+        self.qty_percentage = "100"
+        self.qty_type = self.data_set.get_qty_type("qty_type_1")
+        self.venue_client_names = self.data_set.get_venue_client_names_by_name("client_1_venue_1")
+        self.fix_manager = FixManager(self.ss_connectivity)
+        self.rule_manager = RuleManager()
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
         # region Declaration
-        order_book = OMSOrderBook(self.case_id, self.session_id)
-        base_window = BaseMainWindow(self.case_id, self.session_id)
-        work_dir = Stubs.custom_config['qf_trading_fe_folder']
-        username = Stubs.custom_config['qf_trading_fe_user']
-        password = Stubs.custom_config['qf_trading_fe_password']
-        fix_manager = FixManager(self.ss_connectivity)
-        fix_message = FixMessageNewOrderSingleOMS().set_default_care_limit()
-        qty = fix_message.get_parameter('OrderQtyData')['OrderQty']
-        price = fix_message.get_parameter('Price')
         # endregion
-
-        # region open FE
-        base_window.open_fe(self.report_id, work_dir, username, password, True)
-        # endregion
-
         # region create CO order
-        fix_manager.send_message_fix_standard(fix_message)
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
+        price = self.fix_message.get_parameter('Price')
         # endregion
-
-        # region direct  CO order from client_inbox
+        # region accept CO order
         try:
-            rule_manager = RuleManager()
-            nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew_FIXStandard(self.bs_connectivity,
-                                                                                             'XPAR_' + fix_message.get_parameter(
-                                                                                                 'Account'), 'XPAR',
-                                                                                             float(price))
-            order_inbox = OMSClientInbox(self.case_id, self.session_id)
-            order_inbox.direct_order('VETO', qty, price, qty)
+            nos_rule = self.rule_manager.add_NewOrdSingle_Market_FIXStandard(self.bs_connectivity,
+                                                                                            self.venue_client_names,
+                                                                                                self.venue, True,
+                                                                                                int(qty), float(price))
+            self.client_inbox.client_inbox_direct_moc(self.qty_type, self.qty_percentage, self.route)
+
         except Exception:
-            logger.debug({Exception}, " Exeption")
+            logger.error("Error execution", exc_info=True)
         finally:
-            time.sleep(3)
-            rule_manager.remove_rule(nos_rule)
+            self.rule_manager.remove_rule(nos_rule)
+        # endregion
+        # region check filled child status
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_second_lvl_fields_list(
+                {OrderBookColumns.ord_type.value: OrderType.market.value, OrderBookColumns.exec_sts.value: ExecSts.filled.value})
         # endregion
 
-        # region verify received child order
-        result = order_book.extract_2lvl_fields(SecondLevelTabs.child_tab.value, [OrderBookColumns.sts.value,
-                                                                                  OrderBookColumns.qty.value], [1])
-        order_book.compare_values({OrderBookColumns.sts.value: 'Open', OrderBookColumns.qty.value: '100'}, result[0],
-                                  'Check Value')
-        # endregion
-
-    @decorator_try_except(test_id=os.path.basename(__file__))
-    def execute(self):
-        self.qap_1047()
