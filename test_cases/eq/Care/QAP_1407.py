0604570b9685fb40ac7132f7c11e8d544309e2ce
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-02-08 19:04:54 +0200

    modify QAP_1407

--- test_cases/eq/Care/QAP_1407.py
+++ test_cases/eq/Care/QAP_1407.py
@@ -1,41 +1,45 @@
 import logging
-
-import test_framework.old_wrappers.eq_fix_wrappers
-from custom.basic_custom_actions import create_event, timestamps
-from custom.verifier import Verifier
-from stubs import Stubs
-from test_framework.old_wrappers import eq_wrappers
-from test_framework.old_wrappers.eq_wrappers import open_fe
-from win_gui_modules.utils import get_base_request
+from pathlib import Path
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from custom import basic_custom_actions as bca
+from test_framework.fix_wrappers.DataSet import Connectivity
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns
+from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-def execute(report_id, session_id):
-    case_name = "QAP-1407"
-    seconds, nanos = timestamps()  # Store case start time
+@try_except(test_id=Path(__file__).name[:-3])
+class QAP_1407(TestCase):
 
-    # region Declarations
-    act = Stubs.win_act_order_book
-    qty = "900"
-    price = "20"
-    client = "CLIENT_FIX_CARE"
-    lookup = "VETO"
-    order_type = "Limit"
+    def __init__(self, report_id, session_id=None, data_set=None):
+        super().__init__(report_id, session_id, data_set)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.ss_connectivity = Connectivity.Ganymede_317_ss.value
+        self.fix_manager = FixManager(self.ss_connectivity)
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_market()
+        self.fix_message.change_parameter("Account", "client2341")
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
 
-    case_id = create_event(case_name, report_id)
-    base_request = get_base_request(session_id, case_id)
-    work_dir = Stubs.custom_config['qf_trading_fe_folder']
-    username = Stubs.custom_config['qf_trading_fe_user']
-    password = Stubs.custom_config['qf_trading_fe_password']
-    # endregion
-    # region Open FE
-    open_fe(session_id, report_id, case_id, work_dir, username)
-    # endregion
-    # region Create CO
-    test_framework.old_wrappers.eq_fix_wrappers.create_order_via_fix(case_id, 3, 1, client, 2, qty, 0, price)
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Declaration
+        # region create CO order
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        # endregion
+        # region open group modify window
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).group_modify()
+        # endregion
+        # region group modify window is present
+        self.order_book.is_menu_item_present("Group Modify")
+
+
     # endregion
     # region Verify
     result=eq_wrappers.is_menu_item_present(base_request,"Group Modify")
