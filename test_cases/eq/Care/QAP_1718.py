8d3a91052bd2d9787b1e168bba0929f6b5ee6894
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-02-09 17:41:26 +0200

    modify QAP_1718

--- test_cases/eq/Care/QAP_1718.py
+++ test_cases/eq/Care/QAP_1718.py
@@ -8,7 +8,7 @@ from test_framework.fix_wrappers.DataSet import Connectivity
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.core.test_case import TestCase
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts, OrderType, TimeInForce
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
@@ -35,6 +35,7 @@ class QAP_1718(TestCase):
         self.order_book = OMSOrderBook(self.test_id, self.session_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
         self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
+        self.qty_type = self.data_set.get_qty_type('qty_type_1')
         self.rule_manager = RuleManager()
 
     @try_except(test_id=Path(__file__).name[:-3])
@@ -51,9 +52,10 @@ class QAP_1718(TestCase):
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
             {OrderBookColumns.sts.value: ExecSts.open.value})
         # region Direct CO order
-        self.order_book.direct_moc_order_correct(self.qty, self.route)
+        self.order_book.direct_moc_order(self.qty, self.route, self.qty_type)
         # endregion
         # region check child order has open status
+        time.sleep(5)
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_second_lvl_fields_list(
-            {OrderBookColumns.sts.value: ExecSts.sent.value, OrderBookColumns.qty.value: self.qty})
+            {OrderBookColumns.ord_type.value: OrderType.market.value, OrderBookColumns.qty.value: self.qty, OrderBookColumns.tif.value: TimeInForce.ATC.value})
         # endregion
