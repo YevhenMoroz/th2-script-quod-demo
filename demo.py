d95cf8aac34a985888def854c9acd41641bce0c0
ysamoilenko <yehor.samoilenko.contractor@gmail.com>
2022-02-14 10:59:30 +0200

    Modified demo.py to initial state

--- demo.py
+++ demo.py
@@ -4,18 +4,9 @@ from pathlib import Path
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 from test_cases.eq.Care.QAP_1016 import QAP_1016
-from test_cases.eq.Care.QAP_5000 import QAP_5000
-from test_cases.eq.Care.QAP_5337 import QAP_5337
-from test_cases.eq.Care.QAP_5593 import QAP_5593
-from test_cases.eq.Care.QAP_5835 import QAP_5835
-from test_cases.eq.Care.QAP_5858 import QAP_5858
-from test_cases.eq.PostTrade.QAP_3359 import QAP_3359
-from test_cases.eq.PostTrade.QAP_3362 import QAP_3362
-from test_cases.eq.PostTrade.QAP_5002 import QAP_5002
-from test_framework.configurations.component_configuration import ComponentConfiguration
 from test_framework.data_sets.oms_data_set.oms_data_set import OmsDataSet
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from win_gui_modules.utils import set_session_id, get_opened_fe
+from win_gui_modules.utils import set_session_id
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -27,18 +18,14 @@ def test_run():
     report_id = bca.create_event('test ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     session_id = set_session_id()
+    base_main_window = BaseMainWindow(bca.create_event(Path(__file__).name[:-3], report_id), session_id)
+    data_set = OmsDataSet()
 
     try:
-        configuration = ComponentConfiguration("DMA")
-        base_main_window = BaseMainWindow(bca.create_event(Path(__file__).name[:-3], report_id), session_id)
-        fe_environment = configuration.environment.get_list_fe_environment()[0]
-        print(fe_environment.user, fe_environment.password, fe_environment.path)
-        base_main_window.open_fe(report_id, fe_environment.path, fe_environment.user, fe_environment.password, True)
-        QAP_3359(report_id=report_id, session_id=session_id, data_set=configuration.data_set,
-                 environment=configuration.environment).execute()
+        base_main_window.open_fe(report_id=report_id)
+        QAP_1016(report_id=report_id, session_id=session_id, data_set=data_set).execute()
     except Exception:
         logging.error("Error execution", exc_info=True)
-
     finally:
         Stubs.win_act.unregister(session_id)
 
