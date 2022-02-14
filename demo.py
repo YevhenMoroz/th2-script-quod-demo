2adf8491494e4ec0480cc9ce85fa7107299c595a
AShcherbak <aleksey.shcherbak.contractor@quodfinancial.com>
2022-02-14 10:50:19 +0200

    Small changes

--- demo.py
+++ demo.py
@@ -1,68 +1,71 @@
 import logging
 from datetime import datetime
 
+from MyFiles.MyTest import MyTest
 from custom import basic_custom_actions as bca
-from my_methods import send_rfq, test_ob
 from rule_management import RuleManager
-
 from stubs import Stubs
-from test_cases.fx.fx_mm_esp import QAP_6151, QAP_2957
-from test_cases.fx.fx_mm_esp.QAP_1418 import QAP_1418
-from test_cases.fx.fx_mm_esp.QAP_1589 import QAP_1589
-from test_cases.fx.fx_mm_esp.QAP_6697 import QAP_6697
-from test_cases.fx.fx_mm_rfq import for_test_77679
-from test_cases.fx.fx_mm_rfq.QAP_2472 import QAP_2472
-from test_cases.fx.fx_mm_rfq.QAP_2670 import QAP_2670
-from test_cases.fx.fx_mm_rfq.QAP_3704 import QAP_3704
-from test_cases.fx.fx_taker_esp import QAP_5600
-
+from test_cases.fx.fx_mm_autohedging import QAP_2326
+from test_cases.fx.fx_mm_autohedging.QAP_3142 import QAP_3142
+from test_cases.fx.fx_mm_autohedging.QAP_4149 import QAP_4149
+from test_cases.fx.fx_mm_esp import QAP_1518
+from test_cases.fx.fx_mm_esp.QAP_2098 import QAP_2098
+from test_cases.fx.fx_mm_esp.QAP_2343 import QAP_2343
+from test_cases.fx.fx_mm_esp.QAP_3537 import QAP_3537
+from test_cases.fx.fx_mm_esp.QAP_6149 import QAP_6149
+from test_cases.fx.fx_mm_esp.QAP_6153 import QAP_6153
+from test_cases.fx.fx_mm_rfq.QAP_5992 import QAP_5992
+from test_cases.fx.fx_mm_rfq.QAP_6192 import QAP_6192
+from test_cases.fx.fx_mm_rfq.interpolation.QAP_3761 import QAP_3761
+from test_cases.fx.fx_taker_rfq.QAP_6 import QAP_6
 from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
 from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARN)
 timeouts = False
 
 channels = dict()
 
-
 def test_run():
+
     # Generation id and time for test run
-
-    report_id = bca.create_event("ostronov " + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
-
+    report_id = bca.create_event('Aleksey tests ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
     logging.getLogger().setLevel(logging.WARN)
     Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
+
     session_id = set_session_id()
-    start_time = datetime.now()
-    print(f"Start time :{start_time}")
     data_set = FxDataSet()
 
     try:
-
         # if not Stubs.frontend_is_open:
         #     prepare_fe_2(report_id, session_id)
         # else:
         #     get_opened_fe(report_id, session_id)
-        # rm= RuleManager()
-        # rm.remove_rule_by_id(15)
-        # rm.add_fx_md_to("fix-fh-314-luna")
-        # rm.print_active_rules()
-        # send_md.execute(report_id, 1.18123, 1.18223)
-        QAP_5600.execute(report_id ,session_id)
-        # test_ob.execute(report_id, session_id)
-        # QAP_1418(report_id, session_id, data_set=data_set).execute()
-        # QAP_3805.execute(report_id)
 
-        print(f"Duration is {datetime.now() - start_time}")
+        # QAP_2098(report_id=report_id, session_id=session_id, data_set=data_set).execute()
+        # QAP_2343(report_id=report_id, session_id=session_id, data_set=data_set).execute()
+        # QAP_4149(report_id=report_id, session_id=session_id, data_set=data_set).execute()
+        # QAP_3142(report_id=report_id, session_id=session_id, data_set=data_set).execute()
+        # MyTest(report_id=report_id, session_id=session_id, data_set=data_set).execute()
+        # send_rfq.execute(report_id)
+
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
         Stubs.win_act.unregister(session_id)
 
-
 if __name__ == '__main__':
     logging.basicConfig()
     test_run()
     Stubs.factory.close()
+
+
+
+
+
+
+
+
