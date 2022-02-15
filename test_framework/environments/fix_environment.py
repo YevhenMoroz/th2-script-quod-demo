8c58c93c1b7c3dc6ee92dd789417f6fbcd2a1e16
ostronov <oleksandr.stronov>
2022-02-15 13:13:11 +0200

    QAP-6842 add quod314_luna_standard to env list

--- test_framework/environments/fix_environment.py
+++ test_framework/environments/fix_environment.py
@@ -6,14 +6,21 @@ from test_framework.data_sets.environment_type import EnvironmentType
 class FixEnvironment(BaseEnvironment):
     environment_instances = {}
 
-    def __init__(self, environment_type: str = None, sell_side: str = None, sell_side2: str = None, buy_side: str = None, buy_side2: str = None, feed_handler: str = None, back_office: str = None):
+    def __init__(self, environment_type: str = None, sell_side: str = None, sell_side2: str = None,
+                 buy_side: str = None, buy_side2: str = None, feed_handler: str = None, drop_copy: str = None,
+                 sell_side_rfq: str = None, sell_side_esp: str = None, buy_side_esp: str = None,
+                 feed_handler2: str = None):
         self.environment_type = environment_type
         self.sell_side = sell_side
         self.sell_side2 = sell_side2
         self.buy_side = buy_side
         self.buy_side2 = buy_side2
         self.feed_handler = feed_handler
-        self.back_office = back_office
+        self.drop_copy = drop_copy
+        self.sell_side_rfq = sell_side_rfq
+        self.sell_side_esp = sell_side_esp
+        self.buy_side_esp = buy_side_esp
+        self.feed_handler2 = feed_handler2
 
     @staticmethod
     def get_instance(env: EnvironmentType):
@@ -25,7 +32,8 @@ class FixEnvironment(BaseEnvironment):
                     buy_side=Connectivity.Ganymede_316_Buy_Side.value,
                     feed_handler=Connectivity.Ganymede_316_Feed_Handler.value
                 )
-                FixEnvironment.environment_instances.update({EnvironmentType.quod316_ganymede_standard.value: site_environment})
+                FixEnvironment.environment_instances.update(
+                    {EnvironmentType.quod316_ganymede_standard.value: site_environment})
             return FixEnvironment.environment_instances[EnvironmentType.quod316_ganymede_standard.value]
         elif env.value == EnvironmentType.quod316_ganymede_redburn.value:
             if EnvironmentType.quod316_ganymede_redburn.value not in FixEnvironment.environment_instances.keys():
@@ -35,8 +43,24 @@ class FixEnvironment(BaseEnvironment):
                     buy_side=Connectivity.Ganymede_316_Buy_Side.value,
                     feed_handler=Connectivity.Ganymede_316_Feed_Handler.value
                 )
-                FixEnvironment.environment_instances.update({EnvironmentType.quod316_ganymede_redburn.value: site_environment})
+                FixEnvironment.environment_instances.update(
+                    {EnvironmentType.quod316_ganymede_redburn.value: site_environment})
             return FixEnvironment.environment_instances[EnvironmentType.quod316_ganymede_redburn.value]
+        elif env.value == EnvironmentType.quod314_luna_standard.value:
+            if EnvironmentType.quod314_luna_standard.value not in FixEnvironment.environment_instances.keys():
+                site_environment = FixEnvironment(
+                    environment_type=EnvironmentType.quod314_luna_standard.value,
+                    sell_side_esp=Connectivity.Luna_314_ss_esp.value,
+                    sell_side_rfq=Connectivity.Luna_314_ss_rfq.value,
+                    buy_side_esp=Connectivity.Luna_314_ss_esp_t.value,
+                    feed_handler=Connectivity.Luna_314_Feed_Handler.value,
+                    feed_handler2=Connectivity.Luna_314_Feed_Handler_Q.value,
+                    drop_copy=Connectivity.Luna_314_dc.value,
+
+                )
+                FixEnvironment.environment_instances.update(
+                    {EnvironmentType.quod314_luna_standard.value: site_environment})
+            return FixEnvironment.environment_instances[EnvironmentType.quod314_luna_standard.value]
         else:
             raise Exception('No such environment')
 
