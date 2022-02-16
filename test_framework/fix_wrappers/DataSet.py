712165ad3276c88f0465d75da1f4ab4a01eaa2b6
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-02-16 11:30:25 +0200

    change the multilisting preset

--- test_framework/fix_wrappers/DataSet.py
+++ test_framework/fix_wrappers/DataSet.py
@@ -83,8 +83,8 @@ class Instrument(Enum):
         SecurityType='CS'
     )
     RF = dict(
-        Symbol='FR0000062788_EUR',
-        SecurityID='FR0000062788',
+        Symbol='FR0000121121_EUR',
+        SecurityID='FR0000121121',
         SecurityIDSource='4',
         SecurityExchange='XPAR',
         SecurityType='CS'
