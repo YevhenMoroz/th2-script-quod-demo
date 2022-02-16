909c00700cf9481b767afa9060cfd885913ba1bd
ostronov <oleksandr.stronov>
2022-02-16 10:14:47 +0200

    add test to regression file

--- regression_cycle/fx_regression_cycle/fx_mm_rfq_regression.py
+++ regression_cycle/fx_regression_cycle/fx_mm_rfq_regression.py
@@ -4,10 +4,11 @@ from test_cases.fx.fx_mm_rfq import QAP_1537, QAP_1539, QAP_1540, QAP_1542, QAP_
     QAP_2877, QAP_2878, QAP_2345, QAP_1552, QAP_2062, QAP_2091, QAP_2092, QAP_2101, QAP_2104, QAP_2105, QAP_2143, \
     QAP_2177, QAP_2294, QAP_2295, QAP_2296, QAP_2297, QAP_2353, QAP_2866, QAP_2867, QAP_2868, QAP_2958, \
     QAP_2992, QAP_3003, QAP_3005, QAP_3106, QAP_3107, QAP_3108, \
-    QAP_3109, QAP_3110, QAP_3111, QAP_3112, QAP_3113, QAP_3234, QAP_3250, QAP_3409, QAP_3494, QAP_4085, \
+    QAP_3109, QAP_3110, QAP_3111, QAP_3112, QAP_3113, QAP_3234, QAP_3409, QAP_3494, QAP_4085, \
     QAP_4228, QAP_4509, QAP_4510, QAP_3565, QAP_4223, QAP_4777, QAP_4748, QAP_2382, QAP_5345, QAP_3004, QAP_5848
 from test_cases.fx.fx_mm_rfq.QAP_2472 import QAP_2472
 from test_cases.fx.fx_mm_rfq.QAP_2670 import QAP_2670
+from test_cases.fx.fx_mm_rfq.QAP_3250 import QAP_3250
 from test_cases.fx.fx_mm_rfq.QAP_3704 import QAP_3704
 from test_cases.fx.fx_mm_rfq.QAP_5992 import QAP_5992
 from test_cases.fx.fx_mm_rfq.QAP_6364 import QAP_6364
@@ -163,7 +164,7 @@ def test_run(parent_id=None):
         QAP_3112.execute(report_id, session_id)
         QAP_3113.execute(report_id, session_id)
 
-        QAP_3250.execute(report_id, session_id)
+        QAP_3250(report_id, session_id, configuration.data_set, configuration.environment).execute()
         QAP_3409.execute(report_id, session_id)
         QAP_4223.execute(report_id, session_id)
         QAP_4777.execute(report_id, session_id)
