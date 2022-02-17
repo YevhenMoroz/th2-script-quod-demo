bd776ff8a979665a058dc54dc1b5b56b3cc780b8
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-02-17 11:51:16 +0200

    QAP-3985 add new QA enviroment type (columbia 310 site)

--- test_framework/data_sets/environment_type.py
+++ test_framework/data_sets/environment_type.py
@@ -4,6 +4,7 @@ from enum import Enum
 class EnvironmentType(Enum):
     quod316_ganymede_standard = "quod316_ganymede_standard"
     quod316_ganymede_redburn = "quod316_ganymede_redburn"
+    quod310_columbia_standard = "quod310_columbia_standard"
     quod317_ganymede_standard_test = "quod317_ganymede_standard_test"
     quod317_ganymede_standard = "quod317_ganymede_standard"
     quod317_fe = "quod317_fe"
