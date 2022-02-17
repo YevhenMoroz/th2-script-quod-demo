29f1bcc10a626b051891841183f2d0a3e1b434e2
srublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-02-17 13:23:44 +0200

    QAP-3985 change the OrderCancelReplaceRequest message to algo type

--- test_framework/fix_wrappers/algo/FixMessageOrderCancelReplaceRequestAlgo.py
+++ test_framework/fix_wrappers/algo/FixMessageOrderCancelReplaceRequestAlgo.py
@@ -1,18 +1,34 @@
 from custom import basic_custom_actions
 from test_framework.fix_wrappers.FixMessageNewOrderSingle import FixMessageNewOrderSingle
 from test_framework.fix_wrappers.FixMessageOrderCancelReplaceRequest import FixMessageOrderCancelReplaceRequest
+from datetime import datetime
 
 
 class FixMessageOrderCancelReplaceRequestAlgo(FixMessageOrderCancelReplaceRequest):
 
-    def __init__(self, parameters: dict = None):
+    def __init__(self, new_order_single: FixMessageNewOrderSingle = None, parameters: dict = None):
         super().__init__()
+        if new_order_single is not None:
+            self.update_fix_message(new_order_single.get_parameters())
         super().change_parameters(parameters)
 
-    def set_amend_params(self, new_order_single: FixMessageNewOrderSingle = None):
-        amend_params = dict()
-        for param in new_order_single.get_parameters():
-            amend_params.update(param)
-        amend_params.update(dict(OrigClOrdID=new_order_single.get_parameter("ClOrdID")))
-        amend_params.update(ClOrdID=basic_custom_actions.client_orderid(9))
+    def update_fix_message(self, parameters: dict):
+        temp = dict(
+            Account=parameters['Account'],
+            ClOrdID=parameters['ClOrdID'],
+            HandlInst=parameters['HandlInst'],
+            Side=parameters['Side'],
+            OrderQty=parameters['OrderQty'],
+            TimeInForce=parameters['TimeInForce'],
+            OrdType=parameters['OrdType'],
+            TransactTime=datetime.utcnow().isoformat(),
+            OrderCapacity=parameters['OrderCapacity'],
+            Price=parameters['Price'],
+            Currency=parameters['Currency'],
+            Instrument=parameters['Instrument'],
+            OrigClOrdID=parameters["ClOrdID"],
+            TargetStrategy=parameters['TargetStrategy'],
+            NoStrategyParameters=parameters['NoStrategyParameters']
+        )
+        super().change_parameters(temp)
         return self
