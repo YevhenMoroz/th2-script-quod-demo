3703a2477ba1ceef2dc839513198c0127319415f
ostronov <oleksandr.stronov>
2022-02-18 09:05:33 +0200

    Update web admin env

--- test_framework/data_sets/environment_type.py
+++ test_framework/data_sets/environment_type.py
@@ -5,5 +5,5 @@ class EnvironmentType(Enum):
     quod316_ganymede_standard = "quod316_ganymede_standard"
     quod316_ganymede_redburn = "quod316_ganymede_redburn"
     quod314_luna_standard = "quod314_luna_standard"
-    quod314_luna_rest = "quod314_luna_rest"
+    quod314_luna_web_admin = "quod314_luna_web_admin"
 
--- test_framework/environments/web_admin_enviroment.py
+++ test_framework/environments/web_admin_enviroment.py
@@ -12,14 +12,14 @@ class WebAdminEnvironment(BaseEnvironment):
 
     @staticmethod
     def get_instance(env: EnvironmentType):
-        if env.value == EnvironmentType.quod314_luna_rest.value:
-            if EnvironmentType.quod314_luna_rest.value not in WebAdminEnvironment.environment_instances.keys():
+        if env.value == EnvironmentType.quod314_luna_web_admin.value:
+            if EnvironmentType.quod314_luna_web_admin.value not in WebAdminEnvironment.environment_instances.keys():
                 site_environment = WebAdminEnvironment(
-                    environment_type=EnvironmentType.quod314_luna_rest.value,
+                    environment_type=EnvironmentType.quod314_luna_web_admin.value,
                     wa_alias=Connectivity.Luna_314_wa.value
                 )
                 WebAdminEnvironment.environment_instances.update(
-                    {EnvironmentType.quod314_luna_rest.value: site_environment})
-            return WebAdminEnvironment.environment_instances[EnvironmentType.quod314_luna_rest.value]
+                    {EnvironmentType.quod314_luna_web_admin.value: site_environment})
+            return WebAdminEnvironment.environment_instances[EnvironmentType.quod314_luna_web_admin.value]
         else:
             raise Exception('No such environment')
