f249fcf969138d9d9fef2ff07b72ae4db33f930e
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2022-02-18 17:54:46 +0200

    Add red-log at regression_run_config.xml and environment folder

--- test_framework/data_sets/environment_type.py
+++ test_framework/data_sets/environment_type.py
@@ -8,5 +8,6 @@ class EnvironmentType(Enum):
     quod317_ganymede_standard = "quod317_ganymede_standard"
     quod317_fe = "quod317_fe"
     quod317_java_api = "quod317_java_api"
+    quod317_read_log = 'quod317_read_log'
 
 
