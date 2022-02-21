8768ee7c9cafa5bd01d1af03d480d44275b900b5
Vadim Skulinec <vadim.skulinec.contractor@quodfinancial.com>
2022-02-21 12:05:11 +0200

    Add example of creation bag

--- test_cases/eq/Bag/QAP_1084_not_work.py
+++ test_cases/eq/Bag/QAP_1084_not_work.py
@@ -1,16 +1,13 @@
 import logging
 import os
-import time
 
 from custom import basic_custom_actions as bca
-from rule_management import RuleManager, Simulators
 from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
-from test_framework.java_api_wrappers.ors_messages.UnMatchRequest import UnMatchRequest
-from test_framework.win_gui_wrappers.fe_trading_constant import SecondLevelTabs, OrderBookColumns, TradeBookColumns, \
-    OrderBagColumn, TimeInForce
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBagColumn
 from test_framework.win_gui_wrappers.oms.oms_bag_order_book import OMSBagOrderBook
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
@@ -49,20 +46,17 @@ class QAP_1084(TestCase):
         self.lookup = self.data_set.get_lookup_by_name('lookup_1')
         self.fix_message.change_parameter("HandlInst", '3')
 
-    # @try_except(test_id=Path(__file__).name[:-3])
+    @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
         # region create 3 CO order
-        # self.fix_message.change_parameter("HandlInst", '3')
-        # self.fix_manager.send_message_fix_standard(self.fix_message)
-        # self.client_inbox.accept_order(self.lookup, self.qty, self.price)
-        # self.fix_manager.send_message_fix_standard(self.fix_message)
-        # self.client_inbox.accept_order(self.lookup, self.qty, self.price)
-        # self.fix_manager.send_message_fix_standard(self.fix_message)
-        # self.client_inbox.accept_order(self.lookup, self.qty, self.price)
-        # # endregion
+        self.fix_message.change_parameter("HandlInst", '3')
+        for i in range(3):
+            self.fix_manager.send_message_fix_standard(self.fix_message)
+            self.client_inbox.accept_order(self.lookup, self.qty, self.price)
+            # endregion
 
-        # region create Bag epta
-        self.bag_order_book.create_bag_details([1, 2, 3], name_of_bag='Piece_Of_Shit')
+        # region create Bag
+        self.bag_order_book.create_bag_details([1, 2, 3], name_of_bag='Bag_1')
         self.bag_order_book.create_bag()
         fields = self.bag_order_book.extract_order_bag_book_details('1', [OrderBagColumn.order_bag_qty.value,
                                                                           OrderBagColumn.ord_bag_name.value],
