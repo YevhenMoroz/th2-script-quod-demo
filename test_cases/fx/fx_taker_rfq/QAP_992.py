e81e999911072756161082271fa067849926d019
RNenakhov <roman.nenakhov.contractor@quodfinancial.com>
2022-02-21 14:21:45 +0200

    QAP_992 rewritten

--- test_cases/fx/fx_taker_rfq/QAP_992.py
+++ test_cases/fx/fx_taker_rfq/QAP_992.py
@@ -1,107 +1,46 @@
-import logging
 from pathlib import Path
-import timestring
 from custom import basic_custom_actions as bca
-from custom.tenor_settlement_date import ndf_m1_front_end, ndf_spo_front_end, fixing_ndf_m1_front_end, \
-    ndf_m2_front_end, fixing_ndf_m2_front_end
-from custom.verifier import Verifier
-from stubs import Stubs
-from win_gui_modules.aggregated_rates_wrappers import ModifyRFQTileRequest, ExtractRFQTileValues
-from win_gui_modules.common_wrappers import BaseTileDetails
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_base_request, call, get_opened_fe
-from win_gui_modules.wrappers import set_base
+from custom.tenor_settlement_date import ndf_spo_front_end, ndf_m1_front_end, ndf_m2_front_end, \
+    fixing_ndf_m1_front_end, fixing_ndf_m2_front_end
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.win_gui_wrappers.forex.rfq_tile import RFQTile
 
 
-def create_or_get_rfq(base_request, service):
-    call(service.createRFQTile, base_request.build())
+class QAP_992(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None):
+        super().__init__(report_id, session_id, data_set)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.rfq_tile = RFQTile(self.test_id, self.session_id)
 
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        usd_currency = self.data_set.get_currency_by_name('currency_usd')
+        php_currency = self.data_set.get_currency_by_name('currency_php')
+        tenor_spot = self.data_set.get_tenor_by_name('tenor_spot')
+        tenor_1m = self.data_set.get_tenor_by_name('tenor_1m')
+        tenor_2m = self.data_set.get_tenor_by_name('tenor_2m')
 
-def send_rfq(base_request, service):
-    call(service.sendRFQOrder, base_request.build())
+        # region Step 1
+        self.rfq_tile.crete_tile().modify_rfq_tile(from_cur=usd_currency, to_cur=php_currency, near_tenor=tenor_spot)
+        self.rfq_tile.check_date(near_date=ndf_spo_front_end())
+        # endregion
+        # region Step 2
+        self.rfq_tile.modify_rfq_tile(near_tenor=tenor_1m)
+        self.rfq_tile.check_date(near_date=ndf_m1_front_end())
+        # endregion
+        # region Step 3
+        self.rfq_tile.check_maturity_date(near_date=fixing_ndf_m1_front_end())
+        # endregion
+        # region Step 4
+        self.rfq_tile.modify_rfq_tile(near_tenor=tenor_2m)
+        self.rfq_tile.check_date(near_date=ndf_m2_front_end())
+        # endregion
+        # region Step 5
+        self.rfq_tile.check_maturity_date(near_date=fixing_ndf_m2_front_end())
+        # endregion
 
-
-def modify_rfq_tile(base_request, service, cur1, cur2, near_tenor):
-    modify_request = ModifyRFQTileRequest(details=base_request)
-    modify_request.set_from_currency(cur1)
-    modify_request.set_to_currency(cur2)
-    modify_request.set_near_tenor(near_tenor)
-    call(service.modifyRFQTile, modify_request.build())
-
-
-def change_tenor(base_request, service, tenor):
-    modify_request = ModifyRFQTileRequest(details=base_request)
-    modify_request.set_near_tenor(tenor)
-    call(service.modifyRFQTile, modify_request.build())
-
-
-def check_date(base_request, service, case_id, date):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.extract_near_settlement_date("aggrRfqTile.nearSettlement")
-    extract_value.set_extraction_id(extraction_id)
-    response = call(service.extractRFQTileValues, extract_value.build())
-    extract_date = response["aggrRfqTile.nearSettlement"]
-    extract_date = timestring.Date(extract_date)
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Verify Tenor date on RFQ tile")
-    verifier.compare_values("Date", date, str(extract_date))
-    verifier.verify()
-
-
-def check_fixing_date(base_request, service, case_id, date):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.extract_near_maturity_date("aggrRfqTile.nearFixingDate")
-    extract_value.set_extraction_id(extraction_id)
-    response = call(service.extractRFQTileValues, extract_value.build())
-    extract_date = response["aggrRfqTile.nearFixingDate"]
-    extract_date = timestring.Date(extract_date)
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Verify Fixing date on RFQ tile")
-    verifier.compare_values("Date", date, str(extract_date))
-    verifier.verify()
-
-
-def execute(report_id, session_id):
-    ar_service = Stubs.win_act_aggregated_rates_service
-
-    case_name = Path(__file__).name[:-3]
-    case_from_currency = "USD"
-    case_to_currency = "PHP"
-    case_near_tenor = "Spot"
-    case_tenor_1m = "1M"
-    case_tenor_2m = "2M"
-    ndf_date_spot = ndf_spo_front_end()
-    ndf_date_1m = ndf_m1_front_end()
-    fixing_ndf_date_1m = fixing_ndf_m1_front_end()
-    ndf_date_2m = ndf_m2_front_end()
-    fixing_ndf_date_2m = fixing_ndf_m2_front_end()
-
-    # Create sub-report for case
-    case_id = bca.create_event(case_name, report_id)
-
-    set_base(session_id, case_id)
-    case_base_request = get_base_request(session_id, case_id)
-    base_rfq_details = BaseTileDetails(base=case_base_request)
-
-    try:
-        # Step 1
-        create_or_get_rfq(base_rfq_details, ar_service)
-        modify_rfq_tile(base_rfq_details, ar_service, case_from_currency, case_to_currency, case_near_tenor)
-        check_date(base_rfq_details, ar_service, case_id, ndf_date_spot)
-        # Step 2
-        change_tenor(base_rfq_details, ar_service, case_tenor_1m)
-        check_date(base_rfq_details, ar_service, case_id, ndf_date_1m)
-        # Step 3
-        check_fixing_date(base_rfq_details, ar_service, case_id, fixing_ndf_date_1m)
-        # Step 4
-        change_tenor(base_rfq_details, ar_service, case_tenor_2m)
-        check_date(base_rfq_details, ar_service, case_id, ndf_date_2m)
-        check_fixing_date(base_rfq_details, ar_service, case_id, fixing_ndf_date_2m)
-
-        # Close tile
-        call(ar_service.closeRFQTile, base_rfq_details.build())
-
-    except Exception:
-        logging.error("Error execution", exc_info=True)
-        bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.rfq_tile.close_tile()
