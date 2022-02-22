3f4870325913de0e8bb4bbfab96cad473cce4078
RNenakhov <roman.nenakhov.contractor@quodfinancial.com>
2022-02-22 17:11:42 +0200

    QAP_2774 rewritten

--- test_cases/fx/fx_taker_rfq/QAP_2774.py
+++ test_cases/fx/fx_taker_rfq/QAP_2774.py
@@ -1,59 +1,27 @@
-import logging
 from pathlib import Path
-
 from custom import basic_custom_actions as bca
-from custom.verifier import Verifier
-from stubs import Stubs
-from win_gui_modules.aggregated_rates_wrappers import ExtractRFQTileValues
-from win_gui_modules.common_wrappers import BaseTileDetails
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_base_request, call, get_opened_fe
-from win_gui_modules.wrappers import set_base
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.win_gui_wrappers.forex.rfq_tile import RFQTile
 
 
-def create_or_get_rfq(base_request, service):
-    call(service.createRFQTile, base_request.build())
+class QAP_2774(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None):
+        super().__init__(report_id, session_id, data_set)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.rfq_tile = RFQTile(self.test_id, self.session_id)
+        self.beneficiary = "[Beneficiary]"
 
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        client_1 = self.data_set.get_client_by_name("client_1")
 
-def check_client_and_beneficiary(base_request, service, case_id, client, beneficiary):
-    extract_value = ExtractRFQTileValues(details=base_request)
-    extraction_id = bca.client_orderid(4)
-    extract_value.set_extraction_id(extraction_id)
-    extract_value.extract_beneficiary("aggrRfqTile.beneficiary")
-    extract_value.extract_client("aggrRfqTile.client")
-    response = call(service.extractRFQTileValues, extract_value.build())
+        # region Step 1
+        self.rfq_tile.crete_tile()
+        self.rfq_tile.check_client_beneficiary(client=client_1, beneficiary=self.beneficiary)
+        # endregion
 
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Check client and Beneficiary")
-    verifier.compare_values("Client", client, response["aggrRfqTile.client"])
-    verifier.compare_values("Beneficiary", beneficiary, response["aggrRfqTile.beneficiary"])
-    verifier.verify()
-
-
-def execute(report_id, session_id):
-    ar_service = Stubs.win_act_aggregated_rates_service
-
-    case_name = Path(__file__).name[:-3]
-    # Default client and beneficiary on quod309
-    client = "FIRM_1"
-    beneficiary = "[Beneficiary]"
-
-    # Create sub-report for case
-    case_id = bca.create_event(case_name, report_id)
-
-    set_base(session_id, case_id)
-    case_base_request = get_base_request(session_id, case_id)
-    base_rfq_details = BaseTileDetails(base=case_base_request)
-
-    try:
-        # Step 1
-        create_or_get_rfq(base_rfq_details, ar_service)
-
-        # Step 2
-        check_client_and_beneficiary(base_rfq_details, ar_service, case_id,
-                                     client, beneficiary)
-        # Close tile
-        call(ar_service.closeRFQTile, base_rfq_details.build())
-
-    except Exception:
-        logging.error("Error execution", exc_info=True)
-        bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.rfq_tile.close_tile()
