839bdb3e65140820b63daf6ce75667a5305b6310
Sofia <Sofia@76>
2022-03-14 17:10:37 +0100

    modified 1020

--- test_cases/eq/Care/QAP_1020.py
+++ test_cases/eq/Care/QAP_1020.py
@@ -1,82 +1,61 @@
 import logging
 from datetime import datetime
-
-import test_framework.old_wrappers.eq_fix_wrappers
-from custom.basic_custom_actions import create_event, timestamps
 from stubs import Stubs
-from test_framework.old_wrappers import eq_wrappers
-from test_framework.old_wrappers.eq_wrappers import open_fe
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns
+from pathlib import Path
+from custom import basic_custom_actions as bca
+from rule_management import RuleManager
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.core.test_case import TestCase
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
-from win_gui_modules.utils import get_base_request
-from win_gui_modules.wrappers import set_base
-
+from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
+@try_except(test_id=Path(__file__).name[:-3])
 class QAP_1020(TestCase):
-
-    def __init__(self, report_id, session_id=None, data_set=None):
-        super().__init__(report_id, session_id, data_set)
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
+        self.new_qty = '200'
+        self.new_price = '33'
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-
-        client = "CLIENT_FIX_CARE"
-        time = datetime.utcnow().isoformat()
-        lookup = "VETO"
+        # region Declaration
+        # region Create Market CO order
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
         # endregion
-        # region Open FE
-        qty = "900"
-        qty2 = "400"
-        price = "20"
-        price2 = "50"
-        work_dir = Stubs.custom_config['qf_trading_fe_folder']
-        username = Stubs.custom_config['qf_trading_fe_user']
-        password = Stubs.custom_config['qf_trading_fe_password']
-        ss_connectivity = SessionAliasOMS().ss_connectivity
-        bs_connectivity = SessionAliasOMS().bs_connectivity
-        fix_manager = FixManager(ss_connectivity)
-        order_book = OMSOrderBook(self.test_id, self.session_id)
-        fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
-        fix_manager.send_message_fix_standard(fix_message)
-        order_id_first = order_book.extract_field(OrderBookColumns.order_id.value)
-        order_inbox = OMSClientInbox(self.test_id, self.session_id)
-        order_inbox.reject_order(lookup, qty, price)
-        # region Create CO
-        # test_framework.old_wrappers.eq_fix_wrappers.create_order_via_fix(case_id, 3, 2, client, 2, qty, 0, price)
-        # # endregion
-        # open_fe(session_id, report_id, case_id, work_dir, username)
-        # # endregion
-        # # region Check values in OrderBook
-        # eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Sent")
-        # eq_wrappers.verify_order_value(base_request, case_id, "Qty", qty)
-        # eq_wrappers.verify_order_value(base_request, case_id, "Limit Price", price)
-        # before_order_details_id = "before_order_details"
-        #
-        # # endregion
-        # # region Accept CO
-        # eq_wrappers.accept_order(lookup, qty, price)
-        # # endregion
-        # # region Check values in OrderBook after Accept
-        # eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Open")
-        # # endregion
-        # # region Amend order
-        # eq_wrappers.amend_order(base_request, qty=qty2, price=price2)
-        # # endregion
-        # # region Check values after Amend
-        # eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Open")
-        # eq_wrappers.verify_order_value(base_request, case_id, "Qty", qty2)
-        # eq_wrappers.verify_order_value(base_request, case_id, "Limit Price", price2)
-        # # endregion
-        # # region Cancelling order
-        # eq_wrappers.cancel_order(base_request)
-        # # endregion
-        # # region Check values after Cancel
-        # eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Cancelled")
-        # # endregion
+        # region Accept CO order
+        self.client_inbox.accept_order()
+        # endregion
+        # region check values
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.open.value})
+        # endregion
+        # region amend order
+        self.order_ticket.set_order_details(qty=self.new_qty, limit=self.new_price)
+        self.order_ticket.amend_order(filter_list =[OrderBookColumns.order_id.value, order_id])
+        # endregion
+        # region cancel order
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).cancel_order()
+        # endregion
+        # region check cancel status
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.cancelled.value})
+        # endregion
+
+
