569cb790a2c571b953488b280fd318a51586f0c4
Sofia <Sofia@76>
2022-03-14 11:01:45 +0100

    modify 480

--- test_cases/eq/Care/QAP_480.py
+++ test_cases/eq/Care/QAP_480.py
@@ -1,16 +1,17 @@
 import logging
 import os
 import time
-from rule_management import RuleManager
-from test_framework.win_gui_wrappers.base_window import try_except
-
+from pathlib import Path
 from custom import basic_custom_actions as bca
+from rule_management import RuleManager
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.data_sets.constants import Connectivity
 from stubs import Stubs
 from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.SessionAlias import SessionAliasOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.win_gui_wrappers.TestCase import TestCase
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
@@ -20,45 +21,36 @@ logger.setLevel(logging.INFO)
 timeouts = True
 
 
+@try_except(test_id=Path(__file__).name[:-3])
 class QAP_480(TestCase):
-    def __init__(self, report_id, session_id, file_name):
-        super().__init__(report_id, session_id)
-        self.case_id = bca.create_event(os.path.basename(__file__), self.test_id)
-        self.file_name = file_name
-        self.ss_connectivity = SessionAliasOMS().ss_connectivity
-        self.bs_connectivity = SessionAliasOMS().bs_connectivity
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.ss_connectivity = Connectivity.Ganymede_317_ss.value
+        self.bs_connectivity = Connectivity.Ganymede_317_bs.value
+        self.fix_manager = FixManager(self.ss_connectivity)
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.route = self.data_set.get_route("route_1")
 
-    def qap_480(self):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
         # region Declaration
-        order_book = OMSOrderBook(self.case_id, self.session_id)
-        order_ticket = OMSOrderTicket(self.case_id, self.session_id)
-        base_window = BaseMainWindow(self.case_id, self.session_id)
-        work_dir = Stubs.custom_config['qf_trading_fe_folder']
-        username = Stubs.custom_config['qf_trading_fe_user']
-        password = Stubs.custom_config['qf_trading_fe_password']
-        route = 'Route via FIXBUYTH2 - component'
-        fix_manager = FixManager(self.ss_connectivity)
-        fix_message = FixMessageNewOrderSingleOMS().set_default_care_limit()
-
+        # region Create CO order
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
         # endregion
-
-        # region open FE
-        base_window.open_fe(self.report_id, work_dir, username, password, True)
+        # region Accept CO order
+        self.client_inbox.accept_order()
         # endregion
-        # region create CO order
-        fix_manager.send_message_fix_standard(fix_message)
+        # region direct loc extract error message
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).set_error_message_details()
+        result = self.order_book.direct_loc_extract_error_message('0', self.route)
         # endregion
-
-        # region accept CO order
-        order_inbox = OMSClientInbox(self.case_id, self.session_id)
-        order_inbox.accept_order('O', 'M', 'S')
-        # endregion
-
-        order_book.set_error_message_details()
-        result = order_book.direct_loc_extract_error_message('0', route)
-        order_book.compare_values({'ErrorMessage': 'Error - Qty Percentage should be greater than zero (0)'}, result,
+        # region check error
+        self.order_book.compare_values({'ErrorMessage': 'Error - Qty Percentage should be greater than zero (0)'}, result,
                                   'Verify Error message')
+        # endregion
 
-    @try_except(test_id=os.path.basename(__file__))
-    def execute(self):
-        self.qap_480()
+
