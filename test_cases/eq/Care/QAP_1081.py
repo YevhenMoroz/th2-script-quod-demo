7b09770e7236e1c427caf5edcb8c547d0d06a54b
Sofia <Sofia@76>
2022-03-16 16:38:11 +0100

    modified 1081

--- test_cases/eq/Care/QAP_1081.py
+++ test_cases/eq/Care/QAP_1081.py
@@ -20,16 +20,14 @@ timeouts = True
 class QAP_1081(TestCase):
 
     @try_except(test_id=Path(__file__).name[:-3])
-    def __init__(self, report_id, session_id=None, data_set=None):
-        super().__init__(report_id, session_id, data_set)
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
-        self.ss_connectivity = Connectivity.Ganymede_317_ss.value
-        self.fix_manager = FixManager(self.ss_connectivity)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
         self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
         self.qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
         self.qty2 = "1000"
-        self.price = self.fix_message.get_parameter('Price')
-        self.lookup = "VETO"
         self.order_book = OMSOrderBook(self.test_id, self.session_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
 
@@ -41,7 +39,7 @@ class QAP_1081(TestCase):
         order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
         # endregion
         # region accept CO order
-        self.client_inbox.accept_order(self.lookup, self.qty, self.price)
+        self.client_inbox.accept_order()
         # endregion
         # region check order open status
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
@@ -60,7 +58,7 @@ class QAP_1081(TestCase):
         self.fix_manager.send_message_fix_standard(fix_message_cancel)
         # endregion
         # region accept cancel
-        self.client_inbox.reject_order(self.lookup, self.qty2, self.price)
+        self.client_inbox.reject_order()
         # endregion
         # region check cancelled status
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
