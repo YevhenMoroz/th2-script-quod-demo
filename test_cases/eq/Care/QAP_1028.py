f42b666717eb951f0a9d1063bff43090eeec16b0
Sofia <Sofia@76>
2022-03-16 16:35:20 +0100

    modified 1028

--- test_cases/eq/Care/QAP_1028.py
+++ test_cases/eq/Care/QAP_1028.py
@@ -1,62 +1,54 @@
 import logging
-from th2_grpc_hand import rhbatch_pb2
-from custom.basic_custom_actions import create_event
-from stubs import Stubs
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from test_framework.core.test_case import TestCase
+from stubs import Stubs
 from test_framework.core.try_exept_decorator import try_except
-
-from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, TimeInForce, ExecSts
+from test_framework.win_gui_wrappers.fe_trading_constant import TimeInForce, OrderBookColumns, ExecSts
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
 
-
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
-work_dir = Stubs.custom_config['qf_trading_fe_folder']
-username = Stubs.custom_config['qf_trading_fe_user']
-password = Stubs.custom_config['qf_trading_fe_password']
-username2 = Stubs.custom_config['qf_trading_fe_user_2']
-password2 = Stubs.custom_config['qf_trading_fe_password_2']
-desk = Stubs.custom_config['qf_trading_fe_user_desk']
-order_type = "Limit"
-qty = "900"
-price = "40"
 
+# Precondition: add Recpt column
+@try_except(test_id=Path(__file__).name[:-3])
 class QAP_1028(TestCase):
-    def __init__(self, report_id, session_id=None, data_set=None):
-        super().__init__(report_id, session_id, data_set)
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.client = self.data_set.get_client_by_name('client_co_1')
+        self.lookup = self.data_set.get_lookup_by_name('lookup_1')
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.username2 = environment.get_list_fe_environment()[0].user_2
+        self.desk = environment.get_list_fe_environment()[0].desk_3
+        self.qty = "900"
+        self.price = "40"
+        self.order_type = "Limit"
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # region Declarations
-        client = self.data_set.get_client_by_name('client_co_1')
-        lookup = self.data_set.get_lookup_by_name('lookup_1')
-        order_ticket = OMSOrderTicket(self.test_id, self.session_id)
-        order_book = OMSOrderBook(self.test_id, self.session_id)
-        client_inbox = OMSClientInbox(self.test_id, self.session_id)
-        # endregion
         # region Create CO
-        order_ticket.set_order_details(client=client, limit=price, qty=qty, order_type=order_type,
-                                       tif=TimeInForce.DAY.value, is_sell_side=False, instrument=lookup, recipient=username2)
-        order_ticket.create_order(lookup=lookup)
+        self.order_ticket.set_order_details(client=self.client, limit=self.price, qty=self.qty, order_type=self.order_type,
+                                       tif=TimeInForce.DAY.value, is_sell_side=False, instrument=self.lookup, recipient=self.username2)
+        self.order_ticket.create_order(self.lookup)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
         # endregion
-        order_id = order_book.extract_field(OrderBookColumns.order_id.value)
-        order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
-            {OrderBookColumns.sts.value: "Sent"})
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.sent.value})
+        # region Reassign order
+        self.order_book.reassign_order(self.desk, partial_desk=False)
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.sent.value})
         # endregion
-        # region Reassign orderQA
-        order_book.reassign_order(desk, partial_desk=False)
-        # endregion
-        # region Reject order
-        client_inbox.reject_order(lookup, qty, price)
-        # endregion
-        order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+        # region Accept order
+        self.client_inbox.reject_order()
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
             {OrderBookColumns.sts.value: ExecSts.rejected.value})
-
+        # endregion
