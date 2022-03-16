4874de59377e5e0b2f9756c01e12cdc1824d5dc3
Sofia <Sofia@76>
2022-03-16 16:34:54 +0100

    modified 1023

--- test_cases/eq/Care/QAP_1023.py
+++ test_cases/eq/Care/QAP_1023.py
@@ -18,16 +18,16 @@ timeouts = True
 class QAP_1023(TestCase):
 
     @try_except(test_id=Path(__file__).name[:-3])
-    def __init__(self, report_id, session_id=None, data_set=None):
-        super().__init__(report_id, session_id, data_set)
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
         self.client = self.data_set.get_client_by_name('client_co_1')
-        self.lookup = "VETO"
+        self.lookup = self.data_set.get_lookup_by_name('lookup_1')
         self.order_book = OMSOrderBook(self.test_id, self.session_id)
         self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
-        self.username2 = Stubs.custom_config['qf_trading_fe_user_2']
-        self.desk = Stubs.custom_config['qf_trading_fe_user_desk']
+        self.username2 = environment.get_list_fe_environment()[0].user_2
+        self.desk = environment.get_list_fe_environment()[0].desk_3
         self.qty = "900"
         self.price = "40"
         self.order_type = "Limit"
@@ -37,7 +37,7 @@ class QAP_1023(TestCase):
         # region Create CO
         self.order_ticket.set_order_details(client=self.client, limit=self.price, qty=self.qty, order_type=self.order_type,
                                        tif=TimeInForce.DAY.value, is_sell_side=False, instrument=self.lookup, recipient=self.username2)
-        self.order_ticket.create_order(lookup=self.lookup)
+        self.order_ticket.create_order(self.lookup)
         order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
         # endregion
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
@@ -48,7 +48,7 @@ class QAP_1023(TestCase):
             {OrderBookColumns.sts.value: ExecSts.sent.value})
         # endregion
         # region Accept order
-        self.client_inbox.accept_order(self.lookup, self.qty, self.price)
+        self.client_inbox.accept_order()
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
             {OrderBookColumns.sts.value: ExecSts.open.value})
         # endregion
