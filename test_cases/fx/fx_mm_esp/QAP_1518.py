75ce714db97fd075b8e3329b1ecac01d49d9103a
Medentsev <medentsevartem@gmail.com>
2022-03-18 14:20:37 +0200

    QAP-1518: refactored

--- test_cases/fx/fx_mm_esp/QAP_1518.py
+++ test_cases/fx/fx_mm_esp/QAP_1518.py
@@ -40,7 +40,6 @@ class QAP_1518(TestCase):
 
         self.md_snapshot.set_params_for_md_response(self.md_request, ["*", "*", "*"])
         time.sleep(4)
-        self.md_snapshot.remove_parameters(["OrigMDArrivalTime", "OrigMDTime", "MDTime"])
         self.fix_verifier.check_fix_message(fix_message=self.md_snapshot, direction=DirectionEnum.FromQuod,
                                             key_parameters=["MDReqID"])
         # endregion
