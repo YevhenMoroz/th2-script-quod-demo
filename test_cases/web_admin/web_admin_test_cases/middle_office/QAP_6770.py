3c581e902eff04865dffc51e3aec4f9eba8e4fbb
dmogilny <dmitry.mogilny.contractor@quodfinancial.com>
2022-04-04 17:07:16 +0300

    QAP-6703: Add new TCs

---
+++ test_cases/web_admin/web_admin_test_cases/middle_office/QAP_6770.py
@@ -0,0 +1,54 @@
+import random
+import sys
+import time
+import traceback
+
+from custom import basic_custom_actions
+from test_framework.web_admin_core.pages.login.login_page import LoginPage
+from test_framework.web_admin_core.pages.middle_office.fees.fees_page import FeesPage
+from test_framework.web_admin_core.pages.middle_office.fees.fees_dimensions_sub_wizard import FeesDimensionsSubWizard
+from test_framework.web_admin_core.pages.root.side_menu import SideMenu
+from test_framework.web_admin_core.utils.web_driver_container import WebDriverContainer
+from test_cases.web_admin.web_admin_test_cases.common_test_case import CommonTestCase
+
+
+class QAP_6770(CommonTestCase):
+
+    def __init__(self, web_driver_container: WebDriverContainer, second_lvl_id, data_set=None, environment=None):
+        super().__init__(web_driver_container, self.__class__.__name__, second_lvl_id, data_set=data_set,
+                         environment=environment)
+        self.login = self.data_set.get_user("user_1")
+        self.password = self.data_set.get_password("password_1")
+
+    def precondition(self):
+        login_page = LoginPage(self.web_driver_container)
+        login_page.login_to_web_admin(self.login, self.password)
+        time.sleep(1)
+        side_menu = SideMenu(self.web_driver_container)
+        side_menu.open_fees_page()
+        time.sleep(1)
+        fees_page = FeesPage(self.web_driver_container)
+        fees_page.click_on_new()
+
+    def test_context(self):
+
+        try:
+            self.precondition()
+            time.sleep(2)
+            fees_dimension_tab = FeesDimensionsSubWizard(self.web_driver_container)
+            fees_dimension_tab.set_venue(random.choice(fees_dimension_tab.get_all_venue_from_drop_menu()))
+            time.sleep(1)
+            self.verify("Venue List field become disable", False, fees_dimension_tab.is_venue_list_field_enable())
+            time.sleep(1)
+            fees_dimension_tab.set_venue("Not found")
+            time.sleep(1)
+            fees_dimension_tab.set_venue_list(random.choice(fees_dimension_tab.get_all_venue_list_from_drop_menu()))
+            time.sleep(1)
+            self.verify("Venue field become disable", False, fees_dimension_tab.is_venue_field_enable())
+
+        except Exception:
+            basic_custom_actions.create_event("TEST FAILED before or after verifier", self.test_case_id,
+                                              status='FAILED')
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_tb(exc_traceback, limit=2, file=sys.stdout)
+            print(" Search in ->  " + self.__class__.__name__)
