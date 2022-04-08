5a39d0e4b9fe5c71f6bd235b5bc27d7b00abda2b
Sofia <Sofia@76>
2022-04-08 15:33:21 +0200

    modified QAP-2611

--- test_cases/eq/Care/QAP_2611.py
+++ test_cases/eq/Care/QAP_2611.py
@@ -1,38 +1,56 @@
 import logging
-from datetime import datetime
-from test_framework.old_wrappers import eq_wrappers
-from custom.basic_custom_actions import create_event
-from stubs import Stubs
-from test_framework.old_wrappers.eq_wrappers import open_fe
-from win_gui_modules.utils import get_base_request
+from pathlib import Path
+from custom import basic_custom_actions as bca
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts, TimeInForce
+from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
+from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
+from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-def execute(report_id, session_id):
-    case_name = "QAP-2611"
-    # region Declarations
-    qty = "900"
-    price = "20"
-    client = "CLIENT_FIX_CARE"
-    time = datetime.utcnow().isoformat()
-    lookup = "VETO"
-    order_type = "Limit"
-    washbook = "CareWB"
-    account = "CLIENT_FIX_CARE_SA1"
-    case_id = create_event(case_name, report_id)
-    base_request = get_base_request(session_id, case_id)
-    work_dir = Stubs.custom_config['qf_trading_fe_folder']
-    username = Stubs.custom_config['qf_trading_fe_user']
-    password = Stubs.custom_config['qf_trading_fe_password']
-    # endregion
-    # region Open FE
-    open_fe(session_id, report_id, case_id, work_dir, username)
-    # endregion
-    # region Create CO
-    eq_wrappers.create_order(base_request, qty, client, lookup, order_type, is_care=True, recipient=username,
-                             price=price, washbook=washbook, account=account)
-    # endregion
-    eq_wrappers.verify_order_value(base_request, case_id, "Wash Book", "CareWB")
+
+class QAP_2611(TestCase):
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.client = self.data_set.get_client_by_name('client_co_1')
+        self.lookup = self.data_set.get_lookup_by_name('lookup_1')
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.username2 = environment.get_list_fe_environment()[0].user_2
+        self.desk = environment.get_list_fe_environment()[0].desk_1
+        self.qty = "100"
+        self.price = "1"
+        self.order_type = "Limit"
+        self.washbook = "CareWB"
+        self.account = "TEST2"
+
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region create CO order
+        self.order_ticket.set_order_details(client=self.client, limit=self.price, qty=self.qty,
+                                            order_type=self.order_type,
+                                            tif=TimeInForce.DAY.value, is_sell_side=False, instrument=self.lookup,
+                                            partial_desk=False, recipient=self.desk, washbook=self.washbook, account=self.account)
+        self.order_ticket.create_order(self.lookup)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        # endregion
+        # region accept order
+        self.client_inbox.accept_order()
+        # endregion
+        # region check washbook and account id
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.washbook.value: self.washbook, OrderBookColumns.account_id.value: self.account})
+        # endregion
+
