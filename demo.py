2e945eff588bd29c90919debf781050d308cd839
Alex Stronov <alex.stronov@gmail.com>
2022-05-03 10:58:39 +0200

    Restore after merge

--- demo.py
+++ demo.py
@@ -3,9 +3,32 @@ import time
 from getpass import getuser as get_pc_name
 from datetime import datetime
 from custom import basic_custom_actions as bca
+from rule_management import RuleManager
 from stubs import Stubs
-from test_cases.fx.fx_taker_rfq.QAP_3048 import QAP_3048
-
+from test_cases.fx.fx_mm_esp import QAP_2966, QAP_2012, QAP_2034, QAP_2037, QAP_2035, QAP_2039, QAP_2038, QAP_2050, \
+    QAP_2051, QAP_3661, QAP_4016
+from test_cases.fx.fx_mm_esp.QAP_5389 import QAP_5389
+from test_cases.fx.fx_mm_esp.QAP_6149 import QAP_6149
+from test_cases.fx.fx_mm_esp.QAP_6153 import QAP_6153
+from test_cases.fx.fx_mm_esp.QAP_6691 import QAP_6691
+from test_cases.fx.fx_mm_esp.QAP_7160 import QAP_7160
+from test_cases.fx.fx_mm_esp.QAP_7167 import QAP_7167
+from test_cases.fx.fx_mm_rfq import QAP_4505, QAP_5848, QAP_3494, QAP_2382
+from test_cases.fx.fx_mm_rfq.QAP_6192 import QAP_6192
+from test_cases.fx.fx_mm_rfq.QAP_7125 import QAP_7125
+from test_cases.fx.fx_mm_rfq.QAP_7126 import QAP_7126
+from test_cases.fx.fx_mm_rfq.QAP_7129 import QAP_7129
+from test_cases.fx.fx_mm_rfq.QAP_7130 import QAP_7130
+from test_cases.fx.fx_mm_rfq.QAP_7168 import QAP_7168
+from test_cases.fx.fx_mm_rfq.QAP_7287 import QAP_7287
+from test_cases.fx.fx_mm_rfq.interpolation import QAP_3766
+from test_cases.fx.fx_mm_rfq.interpolation.QAP_5992 import QAP_5992
+from test_cases.fx.fx_taker_esp import QAP_5537, QAP_5600, QAP_5598
+from test_cases.fx.fx_taker_rfq.QAP_6 import QAP_6
+from test_cases.fx.qs_fx_routine import DepositAndLoan
+from test_cases.fx.qs_fx_routine.dep_and_loan import DepAndLoan
+from test_cases.fx.send_md import QAP_MD
+from test_cases.send_rfq import Send_RFQ
 from test_framework.configurations.component_configuration import ComponentConfiguration
 from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
 
@@ -23,7 +46,7 @@ def test_run():
 
     # initializing FE session
     Stubs.custom_config['qf_trading_fe_main_win_name'] = "Quod Financial - Quod site 314"
-    session_id = set_session_id(target_server_win="rnenakho")
+    session_id = set_session_id(target_server_win="quod_11q")
     window_name = "Quod Financial - Quod site 314"
     # region creation FE environment and initialize fe_ values
     configuration = ComponentConfiguration("ESP_MM")  # <--- provide your component from XML (DMA, iceberg, etc)
@@ -33,21 +56,20 @@ def test_run():
     Stubs.frontend_is_open = True
 
     try:
-        if not Stubs.frontend_is_open:
-            prepare_fe_2(report_id, session_id)
-        else:
-            get_opened_fe(report_id, session_id, window_name)
-
+        # if not Stubs.frontend_is_open:
+        #     prepare_fe_2(report_id, session_id)
+        # else:
+        #     get_opened_fe(report_id, session_id, window_name)
+        # #
         # rm = RuleManager()
         # rm.print_active_rules()
 
         # Testing(report_id, session_id, configuration.data_set).execute()
-
+        # DepositAndLoan.execute(report_id)
         # QAP_MD(report_id, data_set=configuration.data_set).execute()
+        # QAP_7160(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         # Send_RFQ(report_id, data_set=configuration.data_set).execute()
-        QAP_3048(report_id, session_id=session_id, data_set=configuration.data_set,
-                 environment=configuration.environment).execute()
-
+        QAP_7168(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         end = time.time()
         print(f"Test duration is {end - start_time} seconds")
 
