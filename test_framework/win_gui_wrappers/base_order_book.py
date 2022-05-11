a34685731a0ce396d0a4b2b8e35d48dcaaae2130
Yevhen Moroz <yevhen.moroz.contractor@quodfinancial.com>
2022-05-11 10:46:39 +0200

    Update base_order_book.py

--- test_framework/win_gui_wrappers/base_order_book.py
+++ test_framework/win_gui_wrappers/base_order_book.py
@@ -371,7 +371,7 @@ class BaseOrderBook(BaseWindow):
         self.clear_details([self.add_to_basket_details])
         return result
 
-    def create_basket(self, orders_rows: [int] = None, basket_name=None, rows_for_delete: int = None, ):
+    def create_basket(self, orders_rows: [int] = None, basket_name=None, rows_for_delete: int = None):
         """
         orders_rows - select rows from order book
         """
