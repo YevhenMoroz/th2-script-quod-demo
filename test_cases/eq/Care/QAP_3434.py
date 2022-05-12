18baaca5c9c0114f89695dc20bc48a0574db7f92
Sofia <Sofia@76>
2022-05-12 09:25:44 +0200

    modify QAP-3434

--- test_cases/eq/Care/QAP_3434.py
+++ test_cases/eq/Care/QAP_3434.py
@@ -1,83 +1,71 @@
 import logging
+from pathlib import Path
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from custom import basic_custom_actions as bca
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
+from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
+from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 
-import test_framework.old_wrappers.eq_fix_wrappers
-from custom.basic_custom_actions import create_event
-from test_framework.old_wrappers import eq_wrappers
-from stubs import Stubs
-from test_framework.old_wrappers.eq_wrappers import open_fe
-from win_gui_modules.order_book_wrappers import ExtractionDetail, ExtractionAction, OrderInfo
-from win_gui_modules.order_book_wrappers import OrdersDetails
-from win_gui_modules.utils import get_base_request, call
-from win_gui_modules.wrappers import verification, verify_ent
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
+@try_except(test_id=Path(__file__).name[:-3])
+class QAP_3434(TestCase):
 
-def execute(report_id, session_id):
-    case_name = "QAP-3434"
-    # region Declarations
-    act = Stubs.win_act_order_book
-    common_act = Stubs.win_act
-    qty = "900"
-    price = "30"
-    client = "CLIENT_FIX_CARE"
-    lookup = "VETO"
-    last_mkt = 'DASI'
-    case_id = create_event(case_name, report_id)
-    base_request = get_base_request(session_id, case_id)
-    work_dir = Stubs.custom_config['qf_trading_fe_folder']
-    username = Stubs.custom_config['qf_trading_fe_user']
-    password = Stubs.custom_config['qf_trading_fe_password']
-    # endregion
-    # region Open FE
-    open_fe(session_id, report_id, case_id, work_dir, username)
-    # endregion
-    # region create order via fix
-    test_framework.old_wrappers.eq_fix_wrappers.create_order_via_fix(case_id, 3, 2, client, 2, qty, 0, price)
-    order_id2 = eq_wrappers.get_order_id(base_request)
-    # endregion
-    # region accept 1 order
-    eq_wrappers.accept_order(lookup, qty, price)
-    # endregion
-    test_framework.old_wrappers.eq_fix_wrappers.create_order_via_fix(case_id, 3, 1, client, 2, qty, 0, price)
-    # region accept 2 order
-    eq_wrappers.accept_order(lookup, qty, price)
-    # endregion
-    # region manual_cross
-    eq_wrappers.manual_cross_orders(base_request, qty, price, (1, 2), last_mkt)
-    # endregion
-    # region check order1
-    before_order_details_id = "before_order_details"
-    order_details = OrdersDetails()
-    order_details.set_default_params(base_request)
-    order_details.set_extraction_id(before_order_details_id)
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.qty = "200"
+        self.price = "10"
+        self.price_mc = "8"
+        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
+        self.fix_message.change_parameter('OrderQtyData', {'OrderQty': self.qty})
+        self.fix_message.change_parameter("Price", self.price)
+        self.last_mkt = "XASE"
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
 
-    order_status = ExtractionDetail("order_status", "Sts")
-    order_exec_sts = ExtractionDetail('order_exec_sts', 'ExecSts')
-    order_extraction_action = ExtractionAction.create_extraction_action(extraction_details=[order_status,
-                                                                                            order_exec_sts
-                                                                                            ])
-    order_details.add_single_order_info(OrderInfo.create(action=order_extraction_action))
-    call(act.getOrdersDetails, order_details.request())
-    call(common_act.verifyEntities, verification(before_order_details_id, "checking order",
-                                                 [verify_ent("Order ExecSts", order_exec_sts.name, "Filled")
-                                                  ]))
-    # endregion
-    # region filter and check 2 order
-    order_info_extraction_cancel = "getOrderInfo_cancelled"
 
-    main_order_details = OrdersDetails()
-    main_order_details.set_default_params(base_request)
-    main_order_details.set_extraction_id(order_info_extraction_cancel)
-    main_order_details.set_filter(["Order ID", order_id2])
-    order_exec_sts = ExtractionDetail('order_exec_sts', 'ExecSts')
-    main_order_details.add_single_order_info(OrderInfo.create(
-        action=ExtractionAction.create_extraction_action(extraction_details=[order_status,
-                                                                             order_exec_sts
-                                                                             ])))
 
-    call(act.getOrdersDetails, main_order_details.request())
-    call(common_act.verifyEntities, verification(order_info_extraction_cancel, "checking order2",
-                                                 [verify_ent("Order ExecSts", order_exec_sts.name, "Filled")]))
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Declaration
+        # region create first CO order
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id1 = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        # endregion
+        # region accept first CO order
+        self.client_inbox.accept_order()
+        # endregion
+        # region create second CO order
+        self.fix_message.change_parameter("Side", "2")
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id2 = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        # endregion
+        # region accept second CO order
+        self.client_inbox.accept_order()
+        # endregion
+        # region manual cross
+        self.order_book.manual_cross_orders([1,2], qty=self.qty, price=self.price_mc, last_mkt=self.last_mkt)
+        # endregion
+        # region check fields
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id1]).check_order_fields_list(
+            {OrderBookColumns.exec_sts.value: ExecSts.filled.value, OrderBookColumns.avg_price.value: self.price_mc})
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id2]).check_order_fields_list(
+            {OrderBookColumns.exec_sts.value: ExecSts.filled.value, OrderBookColumns.avg_price.value: self.price_mc})
+        # endregion
+        # region check child fields
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id1]).check_second_lvl_fields_list(
+            {OrderBookColumns.last_market.value: self.last_mkt, OrderBookColumns.qty.value: self.qty, OrderBookColumns.exec_price.value: self.price_mc})
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id2]).check_second_lvl_fields_list(
+            {OrderBookColumns.last_market.value: self.last_mkt, OrderBookColumns.qty.value: self.qty,
+             OrderBookColumns.exec_price.value: self.price_mc})
+        # endregion
+
