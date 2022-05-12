c3feddb9a133c3f8bb06676d370156073be8f288
Sofia <Sofia@76>
2022-05-12 09:14:34 +0200

    modify QAP-1718

--- test_cases/eq/Care/QAP_1718.py
+++ test_cases/eq/Care/QAP_1718.py
@@ -2,9 +2,7 @@ import logging
 import time
 from pathlib import Path
 from custom import basic_custom_actions as bca
-from rule_management import RuleManager
 from test_framework.core.try_exept_decorator import try_except
-from test_framework.fix_wrappers.DataSet import Connectivity
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.core.test_case import TestCase
@@ -30,7 +28,7 @@ class QAP_1718(TestCase):
         self.qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
         self.price = self.fix_message.get_parameter('Price')
         self.lookup = "DNX"
-        self.route = data_set.get_route("route_1")
+        self.route = self.data_set.get_route("route_1")
         self.order_book = OMSOrderBook(self.test_id, self.session_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
         self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
