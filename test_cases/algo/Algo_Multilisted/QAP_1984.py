ecc895832a5585f8300406acb19f5175dd87af28
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-05-13 17:16:04 +0200

    QAP-1984 change sending MD,add price and StopPx to the execution reports

--- test_cases/algo/Algo_Multilisted/QAP_1984.py
+++ test_cases/algo/Algo_Multilisted/QAP_1984.py
@@ -110,7 +110,7 @@ class QAP_1984(TestCase):
         self.fix_verifier_sell.check_fix_message(pending_multilisting_order_params, key_parameters=self.key_params_cl,message_name='Sell side ExecReport PendingNew')
 
         new_multilisting_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.multilisting_order, self.gateway_side_sell, self.status_new)
-        pending_multilisting_order_params.add_tag(dict(StopPx=self.stop_price))
+        new_multilisting_order_params.add_tag(dict(StopPx=self.stop_price))
         self.fix_verifier_sell.check_fix_message(new_multilisting_order_params, key_parameters=self.key_params_cl, message_name='Sell side ExecReport New')
         # endregion
 
