e130bf7a13d00e09510589e6c980c89782a2635c
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-05-13 17:13:41 +0200

    QAP-1986 change sending MD,add price and StopPx to the execution reports

--- test_cases/algo/Algo_Multilisted/QAP_1986.py
+++ test_cases/algo/Algo_Multilisted/QAP_1986.py
@@ -37,9 +37,9 @@ class QAP_1986(TestCase):
         # endregion
 
         # region order parameters
-        self.qty = 1300
-        self.price = 35
-        self.stop_price = 30
+        self.qty = 2000
+        self.price = 40
+        self.stop_price = 39.95
         self.order_type_stop_lmt = 4
         self.price_ask = 40
         self.price_bid = 30
@@ -86,38 +86,20 @@ class QAP_1986(TestCase):
     def run_pre_conditions_and_steps(self):
         # region Rule creation
         rule_manager = RuleManager()
+        nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side, self.account, self.ex_destination_1, self.price)
         ocr_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account, self.ex_destination_1, True)
-        market_rule = rule_manager.add_NewOrdSingle_Market(self.fix_env1.buy_side, self.account, self.ex_destination_1, False, 0, 0)
-        self.rule_list = [ocr_rule,market_rule]
-        # endregion
-
-        # region Send_MarketData
-        case_id_0 = bca.create_event("Send Market Data", self.test_id)
-
-        self.fix_manager_feed_handler.set_case_id(case_id_0)
-        market_data_snap_shot_par = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
-        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=self.price_bid, MDEntrySize=self.qty_bid)
-        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 1, MDEntryPx=self.price_ask, MDEntrySize=self.qty_ask)
-        self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
-
-        self.fix_manager_feed_handler.set_case_id(case_id_0)
-        market_data_snap_shot_trqx = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID( self.s_trqx, self.fix_env1.feed_handler)
-        market_data_snap_shot_trqx.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=self.price_bid, MDEntrySize=self.qty_bid)
-        market_data_snap_shot_trqx.update_repeating_group_by_index('NoMDEntries', 1, MDEntryPx=self.price_ask, MDEntrySize=self.qty_ask)
-        self.fix_manager_feed_handler.send_message(market_data_snap_shot_trqx)
-
-        time.sleep(3)
+        self.rule_list = [nos_rule, ocr_rule]
         # endregion
 
         # region Send NewOrderSingle (35=D) for Multilisting order
-        case_id_1 = bca.create_event("Create Multilisting Order", self.test_id)
-        self.fix_verifier_sell.set_case_id(case_id_1)
+        case_id_0 = bca.create_event("Create Multilisting Order", self.test_id)
+        self.fix_verifier_sell.set_case_id(case_id_0)
 
         self.multilisting_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_Multilisting_params()
         self.multilisting_order.add_ClordId((os.path.basename(__file__)[:-3]))
-        self.multilisting_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, OrdType=self.order_type_stop_lmt, TimeInForce=self.tif_ato, Instrument=self.instrument, StopPx=self.stop_price))
+        self.multilisting_order.change_parameters(dict(Account=self.client, OrdType=self.order_type_stop_lmt, OrderQty=self.qty, Price=self.price, TimeInForce=self.tif_ato, Instrument=self.instrument, StopPx=self.stop_price))
 
-        self.fix_manager_sell.send_message_and_receive_response(self.multilisting_order, case_id_1)
+        self.fix_manager_sell.send_message_and_receive_response(self.multilisting_order, case_id_0)
 
         time.sleep(3)
         # endregion
@@ -126,16 +108,18 @@ class QAP_1986(TestCase):
         self.fix_verifier_sell.check_fix_message(self.multilisting_order, direction=self.ToQuod,message_name='Sell side NewOrderSingle')
 
         pending_multilisting_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.multilisting_order, self.gateway_side_sell, self.status_pending)
+        pending_multilisting_order_params.add_tag(dict(Price=self.price))
         pending_multilisting_order_params.add_tag(dict(StopPx=self.stop_price))
         self.fix_verifier_sell.check_fix_message(pending_multilisting_order_params, key_parameters=self.key_params_cl,message_name='Sell side ExecReport PendingNew')
 
         new_multilisting_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.multilisting_order, self.gateway_side_sell, self.status_new)
-        pending_multilisting_order_params.add_tag(dict(StopPx=self.stop_price))
+        new_multilisting_order_params.add_tag(dict(Price=self.price))
+        new_multilisting_order_params.add_tag(dict(StopPx=self.stop_price))
         self.fix_verifier_sell.check_fix_message(new_multilisting_order_params, key_parameters=self.key_params_cl, message_name='Sell side ExecReport New')
         # endregion
 
-        # region Send MarketData
-        case_id_2 = bca.create_event("Send Market Data", self.test_id)
+        # region Send MD
+        case_id_1 = bca.create_event("Send Market Data", self.test_id)
 
         MDRefID_1 = Stubs.simulator.getMDRefIDForConnection(request=RequestMDRefID(
             symbol="734",
@@ -148,15 +132,16 @@ class QAP_1986(TestCase):
                 {
                     'MDUpdateAction': '0',
                     'MDEntryType': '2',
-                    'MDEntryPx': '35',
-                    'MDEntrySize': '2000',
+                    'MDEntryPx': '40',
+                    'MDEntrySize': '3000',
                     'MDEntryDate': datetime.utcnow().date().strftime("%Y%m%d"),
                     'MDEntryTime': datetime.utcnow().time().strftime("%H:%M:%S")
                 }
             ]
         }
+
         Stubs.fix_act.sendMessage(request=convert_to_request(
-            'Send MarketDataIncrementalRefresh', "fix-fh-310-columbia", case_id_2,
+            'Send MarketDataIncrementalRefresh', "fix-fh-310-columbia", case_id_1,
             message_to_grpc('MarketDataIncrementalRefresh', mdir_params_trade, "fix-fh-310-columbia")
         ))
         time.sleep(10)
@@ -166,19 +151,20 @@ class QAP_1986(TestCase):
                 {
                     'MDUpdateAction': '0',
                     'MDEntryType': '2',
-                    'MDEntryPx': '35',
-                    'MDEntrySize': '2000',
+                    'MDEntryPx': '40',
+                    'MDEntrySize': '3000',
                     'MDEntryDate': datetime.utcnow().date().strftime("%Y%m%d"),
                     'MDEntryTime': datetime.utcnow().time().strftime("%H:%M:%S")
                 }
             ]
         }
+
         Stubs.fix_act.sendMessage(request=convert_to_request(
-            'Send MarketDataIncrementalRefresh', "fix-fh-310-columbia", case_id_2,
+            'Send MarketDataIncrementalRefresh', "fix-fh-310-columbia", case_id_1,
             message_to_grpc('MarketDataIncrementalRefresh', mdir_params_trade, "fix-fh-310-columbia")
         ))
+
         time.sleep(2)
-        # endregion
 
         # region Check child DMA order
         self.fix_verifier_buy.set_case_id(bca.create_event("Child DMA 1 order", self.test_id))
@@ -187,11 +173,11 @@ class QAP_1986(TestCase):
         self.dma_1_order.change_parameters(dict(OrderQty=self.qty, Price=self.price, TimeInForce=self.tif_ato, Instrument=self.instrument))
         self.fix_verifier_buy.check_fix_message(self.dma_1_order, key_parameters=self.key_params,message_name='Buy side NewOrderSingle Child DMA 1 order')
 
-        pending_dma_1_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_1_order,self.gateway_side_buy, self.status_pending)
-        self.fix_verifier_buy.check_fix_message(pending_dma_1_order_params, key_parameters=self.key_params,direction=self.ToQuod,message_name='Buy side ExecReport PendingNew Child DMA 1 order')
+        pending_dma_1_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_1_order, self.gateway_side_buy, self.status_pending)
+        self.fix_verifier_buy.check_fix_message(pending_dma_1_order_params, key_parameters=self.key_params, direction=self.ToQuod, message_name='Buy side ExecReport PendingNew Child DMA 1 order')
 
-        new_dma_1_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_1_order,self.gateway_side_buy, self.status_pending)
-        self.fix_verifier_buy.check_fix_message(new_dma_1_order_params, key_parameters=self.key_params,direction=self.ToQuod, message_name='Buy side ExecReport New Child DMA 1 order')
+        new_dma_1_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_1_order, self.gateway_side_buy, self.status_pending)
+        self.fix_verifier_buy.check_fix_message(new_dma_1_order_params, key_parameters=self.key_params, direction=self.ToQuod, message_name='Buy side ExecReport New Child DMA 1 order')
         # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
