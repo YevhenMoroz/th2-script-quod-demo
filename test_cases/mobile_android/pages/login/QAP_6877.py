d6c5b6c553baf97866a468c0a18c2d73a194ba13
itserkov <illia.tserkovnyi.contractor@quodfinancial.com>
2022-05-16 15:29:46 +0300

    Actualising the branch

--- test_cases/mobile_android/pages/login/QAP_6877.py
+++ test_cases/mobile_android/pages/login/QAP_6877.py
@@ -1,36 +1,35 @@
 import sys
-import time
 import traceback
 
 from custom import basic_custom_actions
 from test_cases.mobile_android.common_test_case import CommonTestCase
+from test_framework.mobile_android_core.utils.common_page import CommonPage
+from test_framework.mobile_android_core.pages.login.login_constant import LoginConstants
 from test_framework.mobile_android_core.pages.login.login_page import LoginPage
-from test_framework.mobile_android_core.pages.main_page.main_page import MainPage
 
 from test_framework.mobile_android_core.utils.driver import AppiumDriver
 
 
-class QAP_6491(CommonTestCase):
+class QAP_6877(CommonTestCase):
 
     def __init__(self, driver: AppiumDriver, second_lvl_id=None):
         super().__init__(driver, self.__class__.__name__, second_lvl_id)
-        self.email = "User_Ret2"
-        self.password = "User_Ret2"
-
-    def precondition(self):
-        login_page = LoginPage(self.appium_driver)
-        login_page.login_to_mobile_trading(self.email, self.password)
-        time.sleep(2)
-        main_page = MainPage(self.appium_driver)
-        main_page.click_on_create_new_order()
+        self.email = "QA4"
+        self.password = "QA4"
 
     def test_context(self):
         try:
-            self.precondition()
+            login_page = LoginPage(self.appium_driver)
+            common_page = CommonPage(self.appium_driver)
+            login_page.open_login_page(self.email)
+            self.appium_driver.wait_time(2)
+            #time.sleep(2) #add implicit wait -> selenium can help with it
             self.verify("Login successful", True, True)
+            self.verify("Verify: E-mail widget is unclickable", "false",
+                        common_page.get_attribute_of_element_by_xpath(LoginConstants.EMAIL_LOGIN, "clickable"))
 
         except Exception as e:
-            basic_custom_actions.create_event("TEST FAILED before or after verifier", self.test_case_id,
+            basic_custom_actions.create_event("TEST FAILED on Login Page", self.test_case_id,
                                               status='FAILED')
             exc_type, exc_value, exc_traceback = sys.exc_info()
             traceback.print_tb(exc_traceback, limit=2, file=sys.stdout)
