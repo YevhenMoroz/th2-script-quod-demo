8c6d522b2601d76a1a3f7d1c15b2e4261204d5a0
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-05-16 11:20:11 +0300

    add child_order_book_wrappers.py

---
+++ test_framework/win_gui_wrappers/oms/oms_child_order_book.py
@@ -0,0 +1,12 @@
+from stubs import Stubs
+from test_framework.win_gui_wrappers.base_child_order_book import BaseChildOrderBook
+from win_gui_modules.child_order_book_wrappers import ExtractChildOrderBookDataDetails, ExtractSubLvlDataDetails
+
+
+class OMSChildOrderBook(BaseChildOrderBook):
+    def __init__(self, case_id, session_id):
+        super().__init__(case_id, session_id)
+        self.extract_child_order_book_data_details = ExtractChildOrderBookDataDetails()
+        self.extract_child_order_book_sub_lvl_data_details = ExtractSubLvlDataDetails()
+        self.extract_child_order_book_data_call = Stubs.win_act_child_order_book.extractChildOrderBookData
+        self.extract_child_order_book_sub_lvl_data_call = Stubs.win_act_child_order_book.extractChildOrderBookSubLvlData
