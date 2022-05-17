da8b6cb90c81a872c810c6c79cd395c46ea9ad1d
Sofia <Sofia@76>
2022-05-17 08:33:51 +0200

    modified QAP_3701

--- test_cases/eq/Basket/QAP_3701.py
+++ test_cases/eq/Basket/QAP_3701.py
@@ -5,10 +5,9 @@ from pathlib import Path
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
 from custom import basic_custom_actions as bca
-from test_framework.win_gui_wrappers.fe_trading_constant import BasketBookColumns,  TimeInForce
+from test_framework.win_gui_wrappers.fe_trading_constant import BasketBookColumns, TimeInForce
 from test_framework.win_gui_wrappers.oms.oms_basket_order_book import OMSBasketOrderBook
 import getpass
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -26,11 +25,10 @@ class QAP_3701(TestCase):
         self.username = getpass.getuser()
         self.client = self.data_set.get_client_by_name('client_pos_1')
         self.path_xlsx = "C:\\Users\\" + self.username + '\\PycharmProjects\\th2-script-quod-demo\\test_cases\\eq\\Basket' \
-                                               '\\Basket_import_files\\BasketTemplate_withHeader_Mapping1.xlsx'
+                                                         '\\Basket_import_files\\BasketTemplate_withHeader_Mapping1.xlsx'
 
         self.path_csv = "C:\\Users\\" + self.username + '\\PycharmProjects\\th2-script-quod-demo\\test_cases\\eq\\Basket' \
-                                              '\\Basket_import_files\\BasketTemplate_withHeader_Mapping1.csv'
-
+                                                        '\\Basket_import_files\\BasketTemplate_withHeader_Mapping1.csv'
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
@@ -38,14 +36,16 @@ class QAP_3701(TestCase):
         # region create basket via template csv
         self.basket_book.create_basket_via_import(basket_name=self.basket_name,
                                                   basket_template_name="Test Template",
-                                                  path=self.path_csv, client=self.client, tif=TimeInForce.DAY.value, is_csv=True)
+                                                  path=self.path_csv, client=self.client, tif=TimeInForce.DAY.value,
+                                                  is_csv=True)
         # endregion
         # region check basket fields
-        self.basket_book.check_basket_field(BasketBookColumns.exec_policy.value, "Care")
-        self.basket_book.check_basket_field(BasketBookColumns.status.value, "Executing")
-        self.basket_book.check_basket_field("ListExecInstType", "Immediate")
+        self.basket_book.check_basket_field(BasketBookColumns.exec_policy.value, BasketBookColumns.care.value)
+        self.basket_book.check_basket_field(BasketBookColumns.status.value, BasketBookColumns.exec_sts.value)
+        self.basket_book.check_basket_field(BasketBookColumns.list_exec_inst_type.value,
+                                            BasketBookColumns.immediate.value)
         self.basket_book.check_basket_field(BasketBookColumns.basket_name.value, self.basket_name)
-        self.basket_book.check_basket_field("TimeInForce", "DAY")
+        self.basket_book.check_basket_field(BasketBookColumns.time_in_force.value, BasketBookColumns.DAY.value)
         # endregion
         # region create basket via template xlsx
         self.basket_book.create_basket_via_import(basket_name=self.basket_name,
@@ -53,9 +53,10 @@ class QAP_3701(TestCase):
                                                   path=self.path_xlsx, client=self.client, tif=TimeInForce.DAY.value)
         # endregion
         # region check basket fields
-        self.basket_book.check_basket_field(BasketBookColumns.exec_policy.value, "Care")
-        self.basket_book.check_basket_field(BasketBookColumns.status.value, "Executing")
-        self.basket_book.check_basket_field("ListExecInstType", "Immediate")
+        self.basket_book.check_basket_field(BasketBookColumns.exec_policy.value, BasketBookColumns.care.value)
+        self.basket_book.check_basket_field(BasketBookColumns.status.value, BasketBookColumns.exec_sts.value)
+        self.basket_book.check_basket_field(BasketBookColumns.list_exec_inst_type.value,
+                                            BasketBookColumns.immediate.value)
         self.basket_book.check_basket_field(BasketBookColumns.basket_name.value, self.basket_name)
-        self.basket_book.check_basket_field("TimeInForce", "DAY")
+        self.basket_book.check_basket_field(BasketBookColumns.time_in_force.value, BasketBookColumns.DAY.value)
         # endregion
