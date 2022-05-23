8eeb0ed09bfda78d543cb64b8ae6650a305633d3
Sofia <Sofia@76>
2022-05-23 13:10:59 +0200

    modified QAP_3677

--- test_cases/eq/Basket/QAP_3677.py
+++ test_cases/eq/Basket/QAP_3677.py
@@ -1,20 +1,17 @@
 import random
 import string
 import logging
-from datetime import datetime, timedelta
 from pathlib import Path
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
 from custom import basic_custom_actions as bca
-from custom.verifier import Verifier
 from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.FixVerifier import FixVerifier
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts, BasketBookColumns
 from test_framework.win_gui_wrappers.oms.oms_basket_order_book import OMSBasketOrderBook
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
-from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -39,7 +36,6 @@ class QAP_3677(TestCase):
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
         self.basket_book = OMSBasketOrderBook(self.test_id, self.session_id)
         self.basket_name = "Basket_" + "".join(random.choices(string.ascii_letters + string.digits, k=5))
-        self.cl_ord_id = self.fix_message.get_parameter('ClOrdID')
 
 
     @try_except(test_id=Path(__file__).name[:-3])
@@ -47,11 +43,15 @@ class QAP_3677(TestCase):
         # region Declaration
         # region create first CO order
         self.fix_manager.send_message_fix_standard(self.fix_message)
-        order_id1 = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        self.cl_ord_id = self.fix_message.get_parameter('ClOrdID')
+        order_id1 = self.order_book.set_filter([OrderBookColumns.cl_ord_id.value, self.cl_ord_id]).extract_field(
+            OrderBookColumns.order_id.value)
         # endregion
         # region create second CO order
         self.fix_manager.send_message_fix_standard(self.fix_message)
-        order_id2 = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        self.cl_ord_id = self.fix_message.get_parameter('ClOrdID')
+        order_id2 = self.order_book.set_filter([OrderBookColumns.cl_ord_id.value, self.cl_ord_id]).extract_field(
+            OrderBookColumns.order_id.value)
         # endregion
         # region accept CO order
         self.client_inbox.accept_order()
@@ -65,12 +65,14 @@ class QAP_3677(TestCase):
         # endregion
         # check basket fields
         self.basket_book.check_basket_field(BasketBookColumns.exec_policy.value, "Care")
-        self.basket_book.check_basket_field(BasketBookColumns.status.value, "Executing")
+        self.basket_book.check_basket_field(BasketBookColumns.status.value, BasketBookColumns.exec_sts.value)
         self.basket_book.check_basket_field(BasketBookColumns.basket_name.value, self.basket_name)
         # endregion
         # check basket 2nd level fields
-        # self.basket_book.check_basket_sub_lvl_field(1,  "Sts", "Orders", ExecSts.open.value)
-        # self.basket_book.check_basket_sub_lvl_field(2,  "Sts", "Orders", ExecSts.open.value)
+        self.basket_book.check_basket_sub_lvl_field(1, BasketBookColumns.orders_sts.value,
+                                                    BasketBookColumns.orders_tab.value, ExecSts.open.value)
+        self.basket_book.check_basket_sub_lvl_field(2, BasketBookColumns.orders_sts.value,
+                                                    BasketBookColumns.orders_tab.value, ExecSts.open.value)
         # endregion
         # check order in Order Book
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id1]).check_order_fields_list(
@@ -78,5 +80,3 @@ class QAP_3677(TestCase):
         self.order_book.set_filter([OrderBookColumns.order_id.value, order_id2]).check_order_fields_list(
             {OrderBookColumns.basket_name.value: self.basket_name, OrderBookColumns.basket_id.value: basket_id})
         # endregion
-
-
