62106d2737bd9a9d52ca9e6e29a47e5b41321e82
Sofia <Sofia@76>
2022-05-25 13:14:42 +0200

    modified QAP_1364

--- test_cases/eq/Care/QAP_1364.py
+++ test_cases/eq/Care/QAP_1364.py
@@ -1,9 +1,9 @@
 import logging
 from pathlib import Path
+from rule_management import RuleManager, Simulators
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
 from custom import basic_custom_actions as bca
-from test_framework.fix_wrappers.DataSet import Connectivity
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
@@ -28,9 +28,14 @@ class QAP_1364(TestCase):
         self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
         self.qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
         self.price = self.fix_message.get_parameter('Price')
+        self.client_for_rule = self.data_set.get_venue_client_names_by_name('client_co_1_venue_1')
+        self.account = self.data_set.get_client_by_name('client_co_1')
+        self.fix_message.change_parameter("Account", self.account)
         self.order_book = OMSOrderBook(self.test_id, self.session_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
         self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
+        self.rule_manager = RuleManager(Simulators.equity)
+        self.exec_destination = self.data_set.get_mic_by_name('mic_1')
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
@@ -47,9 +52,42 @@ class QAP_1364(TestCase):
             {OrderBookColumns.sts.value: ExecSts.open.value})
         # endregion
         # region split order
-        self.order_ticket.set_order_details(qty=self.qty, limit=self.price)
-        self.order_ticket.split_limit_order()
+        nos_rule= None
+        try:
+            nos_rule = self.rule_manager.add_NewOrdSingleExecutionReportPendingAndNew_FIXStandard(self.fix_env.buy_side,
+                                                                                                  self.client_for_rule,
+                                                                                                  self.exec_destination,
+                                                                                                  float(self.price))
+
+            self.order_ticket.set_order_details(qty=self.qty, limit=self.price)
+            self.order_ticket.split_limit_order()
+        except Exception:
+            logger.error("Error execution", exc_info=True)
+        finally:
+            self.rule_manager.remove_rule(nos_rule)
+            # endregion
+        # region check fields
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.unmatched_qty.value: "0"})
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_second_lvl_fields_list(
+            {OrderBookColumns.qty.value: self.qty, OrderBookColumns.limit_price.value: self.price})
         # endregion
-        # region order is splited
-        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_second_lvl_fields_list({OrderBookColumns.qty.value: self.qty, OrderBookColumns.limit_price.value: self.price})
+        # region cancel child order
+        nos_rule = None
+        try:
+            nos_rule = self.rule_manager.add_OrderCancelRequest_FIXStandard(self.fix_env.buy_side,
+                                                                            self.client_for_rule,
+                                                                            self.exec_destination,
+                                                                            True)
+            self.order_book.cancel_child_orders({OrderBookColumns.order_id.value: order_id})
+        except Exception:
+            logger.error("Error execution", exc_info=True)
+        finally:
+            self.rule_manager.remove_rule(nos_rule)
+        # endregion
+        # region check fields
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.unmatched_qty.value: self.qty})
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_second_lvl_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.cancelled.value})
         # endregion
