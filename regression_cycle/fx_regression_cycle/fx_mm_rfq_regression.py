48970984b5ec1cf328572eaaeb7a14c734d4dfdb
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2022-05-30 11:36:11 +0200

    Add new test to cycle

--- regression_cycle/fx_regression_cycle/fx_mm_rfq_regression.py
+++ regression_cycle/fx_regression_cycle/fx_mm_rfq_regression.py
@@ -5,12 +5,13 @@ from test_cases.fx.fx_mm_rfq import QAP_1537, QAP_1539, QAP_1540, QAP_1542, QAP_
     QAP_2177, QAP_2294, QAP_2295, QAP_2296, QAP_2297, QAP_2353, QAP_2866, QAP_2867, QAP_2868, QAP_2958, \
     QAP_2992, QAP_3003, QAP_3005, QAP_3106, QAP_3108, \
     QAP_3109, QAP_3110, QAP_3111, QAP_3112, QAP_3113, QAP_3234, QAP_3409, QAP_3494, QAP_4085, \
-    QAP_4228, QAP_4509, QAP_4510, QAP_3565, QAP_4223, QAP_4777, QAP_4748, QAP_2382, QAP_5345, QAP_3004, QAP_5848
+    QAP_4228, QAP_4509, QAP_4510, QAP_3565, QAP_4223, QAP_4777, QAP_4748, QAP_2382, QAP_3004, QAP_5848
 # from test_cases.fx.fx_mm_rfq.QAP_1542 import QAP_1542
 from test_cases.fx.fx_mm_rfq.QAP_2472 import QAP_2472
 from test_cases.fx.fx_mm_rfq.QAP_2670 import QAP_2670
 from test_cases.fx.fx_mm_rfq.QAP_3250 import QAP_3250
 from test_cases.fx.fx_mm_rfq.QAP_3704 import QAP_3704
+from test_cases.fx.fx_mm_rfq.QAP_5345 import QAP_5345
 from test_cases.fx.fx_mm_rfq.QAP_7168 import QAP_7168
 from test_cases.fx.fx_mm_rfq.interpolation.QAP_3766 import QAP_3766
 from test_cases.fx.fx_mm_rfq.interpolation.QAP_5992 import QAP_5992
@@ -114,7 +115,7 @@ def test_run(parent_id=None):
         QAP_4228.execute(report_id)
         QAP_4509.execute(report_id)
         QAP_4510.execute(report_id)
-        QAP_5345.execute(report_id)
+        QAP_5345(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
 
         QAP_1537.execute(report_id, case_params)
 
