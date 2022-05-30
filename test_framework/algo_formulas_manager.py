05eb05fe2e8fd72079b453b58997c07bfc1f87e0
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-05-30 16:37:43 +0200

    Add formulas for MP Dark venue weights

--- test_framework/algo_formulas_manager.py
+++ test_framework/algo_formulas_manager.py
@@ -53,3 +53,38 @@ class AlgoFormulasManager:
         first_reserve = max(5 * ats, math.ceil(remaining_ord_qty * (100 - nav_percentage)))
         reserve = max(first_reserve, AlgoFormulasManager.get_next_twap_slice(remaining_ord_qty, remaining_waves))
         return reserve
+
+    @staticmethod
+    def get_child_qty_on_venue_weights(parent_qty, *venue_weights):
+        sum_of_weight = 0
+        for i in venue_weights:
+            sum_of_weight += i
+
+        one_weight = parent_qty / sum_of_weight
+
+        qty_list = []
+        for k in venue_weights:
+            qty_list.append(int(one_weight * k))
+
+        return qty_list
+
+    @staticmethod
+    def get_child_qty_on_venue_weights_with_min_qty(parent_qty, min_qty, *venue_weights):
+        sum_of_weight = 0
+        count_of_venue = 0
+        for i in venue_weights:
+            sum_of_weight += i
+            count_of_venue += 1
+
+        qty_for_distribution = parent_qty - min_qty * count_of_venue
+
+        one_weight = qty_for_distribution / sum_of_weight
+
+        qty_list = []
+        for k in venue_weights:
+            qty_list.append(int(one_weight * k + min_qty))
+
+        return qty_list
+
+
+
