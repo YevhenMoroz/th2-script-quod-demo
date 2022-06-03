3e5215fbe7d4c0f57a985af1561b12e6ec7608d9
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-06-03 15:58:06 +0200

    Edit formulas for MPDark algo: counting weights

--- test_framework/algo_formulas_manager.py
+++ test_framework/algo_formulas_manager.py
@@ -55,80 +55,64 @@ class AlgoFormulasManager:
         reserve = max(first_reserve, AlgoFormulasManager.get_next_twap_slice(remaining_ord_qty, remaining_waves))
         return reserve
 
-    # TODO need review and edit
-    # @staticmethod
-    # def prohibit_float_arguments(func):
-    #     @wraps(func)
-    #     def wrapper(*args):
-    #         for val in args:
-    #             if type(val) == float:
-    #                 raise ValueError('Float arguments are prohibited')
-    #             if type(val) == str:
-    #                 raise ValueError('String arguments are prohibited')
-    #             if val < 0:
-    #                 raise ValueError('Negative arguments are prohibited')
-    #         return func(*args)
-    #     return wrapper
-
-    # @staticmethod
-    # # @prohibit_float_arguments
-    # def get_child_qty_on_venue_weights(parent_qty: int, minqty: int = None, *venue_weights: list) -> list:
-    #     sum_of_weight = 0
-    #     count_of_venue = 0
-    #     qty_list = []
-    #
-    #     for i in venue_weights:
-    #         sum_of_weight += i
-    #         count_of_venue += 1
-    #
-    #     if minqty is None:
-    #         one_weight = parent_qty / sum_of_weight
-    #         j = 0
-    #         while j < len(venue_weights):
-    #             qty_list.append(one_weight * venue_weights[j])
-    #             j += 1
-    #     else:
-    #         qty_for_distribution = parent_qty - minqty * count_of_venue
-    #         one_weight = qty_for_distribution / sum_of_weight
-    #         j = 0
-    #         while j < len(venue_weights):
-    #             qty_list.append(one_weight * venue_weights[j] + minqty)
-    #             j += 1
-    #
-    #     return qty_list
-
-    # Two different formulas for dark venue weights
     @staticmethod
-    def get_child_qty_on_venue_weights(parent_qty: int, *venue_weights: list) -> list:
-        sum_of_weight = 0
-        for i in venue_weights:
-            sum_of_weight += i
-
-        one_weight = parent_qty / sum_of_weight
-
-        qty_list = []
-        for k in venue_weights:
-            qty_list.append(int(one_weight * k))
-
-        return qty_list
-
-    @staticmethod
-    def get_child_qty_on_venue_weights_with_min_qty(parent_qty, min_qty, *venue_weights):
+    def get_child_qty_on_venue_weights(parent_qty: int, minqty: int = None, *venue_weights: int) -> list:
         sum_of_weight = 0
         count_of_venue = 0
+        qty_list = []
+
         for i in venue_weights:
             sum_of_weight += i
             count_of_venue += 1
 
-        qty_for_distribution = parent_qty - min_qty * count_of_venue
-
-        one_weight = qty_for_distribution / sum_of_weight
-
-        qty_list = []
-        for k in venue_weights:
-            qty_list.append(int(one_weight * k + min_qty))
+        if minqty is None:
+            one_weight = parent_qty / sum_of_weight
+            j = 0
+            while j < len(venue_weights):
+                qty_list.append(int(one_weight * venue_weights[j]))
+                j += 1
+        else:
+            qty_for_distribution = parent_qty - minqty * count_of_venue
+            one_weight = qty_for_distribution / sum_of_weight
+            j = 0
+            while j < len(venue_weights):
+                qty_list.append(int(one_weight * venue_weights[j] + minqty))
+                j += 1
 
         return qty_list
+
+    # Two different formulas for dark venue weights
+    # @staticmethod
+    # def get_child_qty_on_venue_weights(parent_qty: int, *venue_weights: list) -> list:
+    #     sum_of_weight = 0
+    #     for i in venue_weights:
+    #         sum_of_weight += i
+    #
+    #     one_weight = parent_qty / sum_of_weight
+    #
+    #     qty_list = []
+    #     for k in venue_weights:
+    #         qty_list.append(int(one_weight * k))
+    #
+    #     return qty_list
+    #
+    # @staticmethod
+    # def get_child_qty_on_venue_weights_with_min_qty(parent_qty, min_qty, *venue_weights):
+    #     sum_of_weight = 0
+    #     count_of_venue = 0
+    #     for i in venue_weights:
+    #         sum_of_weight += i
+    #         count_of_venue += 1
+    #
+    #     qty_for_distribution = parent_qty - min_qty * count_of_venue
+    #
+    #     one_weight = qty_for_distribution / sum_of_weight
+    #
+    #     qty_list = []
+    #     for k in venue_weights:
+    #         qty_list.append(int(one_weight * k + min_qty))
+    #
+    #     return qty_list
 
 
 
