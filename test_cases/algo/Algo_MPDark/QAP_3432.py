111c5e3a6cb301db2e4d1cc295ad0662f13d0949
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2022-06-08 16:44:50 +0200

    update test

--- test_cases/algo/Algo_MPDark/QAP_3432.py
+++ test_cases/algo/Algo_MPDark/QAP_3432.py
@@ -31,7 +31,7 @@ class QAP_3432(TestCase):
 
         # region order parameters
         # weights BATS = 60, CHIX = 30, CBOE = 10
-        self.qty = 10000
+        self.qty = 1000000
         self.qty_1_child = 6000
         self.qty_2_child = 3000
         self.qty_3_child = 1000
@@ -59,9 +59,8 @@ class QAP_3432(TestCase):
         # endregion
 
         # region venue param
-        self.ex_destination_bats = self.data_set.get_mic_by_name("mic_4")
-        self.ex_destination_chix = self.data_set.get_mic_by_name("mic_5")
-        self.ex_destination_cboe = self.data_set.get_mic_by_name("mic_6")
+        self.ex_destination_chixlis = self.data_set.get_mic_by_name("mic_12")
+        self.ex_destination_trql = self.data_set.get_mic_by_name("mic_13")
         self.client = self.data_set.get_client_by_name("client_4")
         self.account_bats = self.data_set.get_account_by_name("account_7")
         self.account_chix = self.data_set.get_account_by_name("account_8")
@@ -71,7 +70,7 @@ class QAP_3432(TestCase):
         # region Key parameters
         self.key_params_without_cl_ord_id_with_cl_ord_id = self.data_set.get_verifier_key_parameters_by_name("verifier_key_parameters_1")
         self.key_params_without_cl_ord_id = self.data_set.get_verifier_key_parameters_by_name("verifier_key_parameters_2")
-        self.key_params_with_ex_destination = self.data_set.get_verifier_key_parameters_by_name("verifier_key_parameters_mp_dark_child")
+        self.key_params_with_ex_destination = self.data_set.get_verifier_key_parameters_by_name("verifier_key_parameters_NOS_child")
         # endregion
 
         self.rule_list = []
@@ -80,25 +79,25 @@ class QAP_3432(TestCase):
     def run_pre_conditions_and_steps(self):
         # region Rule creation
         rule_manager = RuleManager()
-        nos_1_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side,
-                                                                               self.account_bats,
-                                                                               self.ex_destination_bats,
-                                                                               self.child_price)
-        nos_2_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side,
-                                                                               self.account_chix,
-                                                                               self.ex_destination_chix,
-                                                                               self.child_price)
-        nos_3_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side,
-                                                                               self.account_cboe,
-                                                                               self.ex_destination_cboe,
-                                                                               self.child_price)
-        ocr_1_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account_bats,
-                                                         self.ex_destination_bats, True)
-        ocr_2_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account_chix,
-                                                         self.ex_destination_chix, True)
-        ocr_3_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account_cboe,
-                                                         self.ex_destination_cboe, True)
-        self.rule_list = [nos_1_rule, nos_2_rule, nos_3_rule, ocr_1_rule, ocr_2_rule, ocr_3_rule]
+        # nos_1_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side,
+        #                                                                        self.account_bats,
+        #                                                                        self.ex_destination_bats,
+        #                                                                        self.child_price)
+        # nos_2_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side,
+        #                                                                        self.account_chix,
+        #                                                                        self.ex_destination_chix,
+        #                                                                        self.child_price)
+        # nos_3_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side,
+        #                                                                        self.account_cboe,
+        #                                                                        self.ex_destination_cboe,
+        #                                                                        self.child_price)
+        # ocr_1_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account_bats,
+        #                                                  self.ex_destination_bats, True)
+        # ocr_2_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account_chix,
+        #                                                  self.ex_destination_chix, True)
+        # ocr_3_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account_cboe,
+        #                                                  self.ex_destination_cboe, True)
+        # self.rule_list = [nos_1_rule, nos_2_rule, nos_3_rule, ocr_1_rule, ocr_2_rule, ocr_3_rule]
         # endregion
 
         # region Send NewOrderSingle (35=D) for MP Dark order
@@ -107,9 +106,33 @@ class QAP_3432(TestCase):
 
         self.MP_Dark_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_MPDark_params()
         self.MP_Dark_order.add_ClordId((os.path.basename(__file__)[:-3]))
-        self.MP_Dark_order.change_parameters(dict(Account=self.client, OrderQty=self.qty))
-
+        self.MP_Dark_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, ClientAlgoPolicyID="QA_MPDark"))
         self.fix_manager_sell.send_message_and_receive_response(self.MP_Dark_order, case_id_1)
 
         time.sleep(3)
         # endregion
+
+        # TODO
+        # # region Check Sell side
+        # self.fix_verifier_sell.check_fix_message(self.MP_Dark_order, direction=self.ToQuod, message_name='Sell side NewOrderSingle')
+        #
+        # er_pending_new_MP_Dark_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.MP_Dark_order, self.gateway_side_sell, self.status_pending)
+        # self.fix_verifier_sell.check_fix_message(er_pending_new_MP_Dark_order_params, key_parameters=self.key_params_with_cl_ord_id, message_name='Sell side ExecReport PendingNew')
+        #
+        # er_new_MP_Dark_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.MP_Dark_order, self.gateway_side_sell, self.status_new)
+        # self.fix_verifier_sell.check_fix_message(er_new_MP_Dark_order_params, key_parameters=self.key_params_with_cl_ord_id, message_name='Sell side ExecReport New')
+        # # endregion
+
+        # region check that RFQ send to CHIXLIS
+        nos_chixlis_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_RFQ_params().change_parameters(dict(Account=self.client, OrderQty=self.qty, ExDestination=self.ex_destination_chixlis))
+        self.fix_verifier_buy.check_fix_message(nos_chixlis_order, key_parameters=self.key_params_with_ex_destination, message_name='Buy side RFQ on CHIXLIS')
+        # end region
+
+        # region check that RFQ send to TQLIS
+        nos_trql_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_RFQ_params().change_parameters(dict(Account=self.client, OrderQty=self.qty, ExDestination=self.ex_destination_trql))
+        self.fix_verifier_buy.check_fix_message(nos_trql_order, key_parameters=self.key_params_with_ex_destination, message_name='Buy side RFQ on CHIXLIS')
+        # end region
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        RuleManager.remove_rules(self.rule_list)
