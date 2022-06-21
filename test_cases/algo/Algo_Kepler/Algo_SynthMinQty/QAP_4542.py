d2e7b3ea1c36dd2dc6410d997152caec569a4010
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-06-21 16:32:03 +0200

    QAP-4542 fully automated

--- test_cases/algo/Algo_Kepler/Algo_SynthMinQty/QAP_4542.py
+++ test_cases/algo/Algo_Kepler/Algo_SynthMinQty/QAP_4542.py
@@ -160,7 +160,7 @@ class QAP_4542(TestCase):
         self.fix_verifier_buy.check_fix_message(er_fill_dma_qdl1_order, self.key_params_ER_fill_child, self.ToQuod, "Buy Side ExecReport Fill Aggressive DMA 1 order")
         # endregion
         
-        time.sleep(2)
+        time.sleep(3)
 
         # region Check 2 child DMA order
         self.fix_verifier_buy.set_case_id(bca.create_event("Aggressive 2 child DMA order", self.test_id))
@@ -168,20 +168,12 @@ class QAP_4542(TestCase):
         self.dma_qdl2_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_DMA_ChildMinQty_params()
         self.dma_qdl2_order.change_parameters(dict(Account=self.account, ExDestination=self.ex_destination_quodlit2, OrderQty=self.trade_2_qty, Price=self.price))
         self.fix_verifier_buy.check_fix_message(self.dma_qdl2_order, key_parameters=self.key_params_NOS_child, message_name='Buy side NewOrderSingle Aggressive Child DMA 2 order')
-
-        er_pending_new_dma_qdl2_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_qdl2_order, self.gateway_side_buy, self.status_pending)
-        er_pending_new_dma_qdl2_order_params.change_parameters(dict(ExDestination=self.ex_destination_quodlit1))
-        self.fix_verifier_buy.check_fix_message(er_pending_new_dma_qdl2_order_params, key_parameters=self.key_params_ER_child, direction=self.ToQuod, message_name='Buy side ExecReport PendingNew Aggressive Child DMA 2 order')
-
-        er_new_dma_qdl2_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_qdl2_order, self.gateway_side_buy, self.status_pending)
-        er_new_dma_qdl2_order_params.change_parameters(dict(ExDestination=self.ex_destination_quodlit1))
-        self.fix_verifier_buy.check_fix_message(er_new_dma_qdl2_order_params, key_parameters=self.key_params_ER_child, direction=self.ToQuod, message_name='Buy side ExecReport New Aggressive Child DMA 2 order')
         # endregion
 
         # region Check Fill
         self.fix_verifier_buy.set_case_id(bca.create_event("Fill 2 aggressive child Order", self.test_id))
 
-        # region check fill first dma child order
+        # region check fill second dma child order
         er_fill_dma_qdl2_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_qdl2_order, self.gateway_side_buy, self.status_fill)
         er_fill_dma_qdl2_order.change_parameters(dict(CumQty=self.trade_2_qty, LeavesQty=0, LastQty=self.trade_2_qty, LastPx=self.price, Text='Filled')).remove_parameter('ExDestination')
         self.fix_verifier_buy.check_fix_message(er_fill_dma_qdl2_order, self.key_params_ER_fill_child, self.ToQuod, "Buy Side ExecReport Fill Aggressive DMA 2 order")
@@ -189,7 +181,7 @@ class QAP_4542(TestCase):
         
         self.fix_verifier_sell.set_case_id(bca.create_event("Fill Algo Order", self.test_id))
         er_fill_synthMinQty_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.synthMinQty_order, self.gateway_side_sell, self.status_fill)
-        er_fill_synthMinQty_order.change_parameters(dict(LastPx=self.price, CumQty=self.qty, LeavesQty=self.leaves_qty, LastQty=self.trade_2_qty, Text='Filled')).remove_parameters(['SecAltIDGrp', 'SecondaryClOrdID']).add_tag(dict(LastMkt='*', ChildOrderID='*', Instrument='*', ExDestination='*'))
+        er_fill_synthMinQty_order.change_parameters(dict(LastPx=self.price, CumQty=self.qty, LeavesQty=self.leaves_qty, LastQty=self.trade_2_qty, Text='Filled')).remove_parameters(['ReplyReceivedTime', 'TradeReportingIndicator', 'SecAltIDGrp', 'SecondaryClOrdID']).add_tag(dict(LastMkt='*', ChildOrderID='*', Instrument='*', ExDestination='*'))
         self.fix_verifier_sell.check_fix_message(er_fill_synthMinQty_order, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport Fill')
         # endregion
 
