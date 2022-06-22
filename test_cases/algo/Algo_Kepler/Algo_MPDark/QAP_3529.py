f142fbffacc1e81d085e1876b1175157c8b7024b
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-06-22 15:03:16 +0200

    QAP-3529 edit algopolicy

--- test_cases/algo/Algo_Kepler/Algo_MPDark/QAP_3529.py
+++ test_cases/algo/Algo_Kepler/Algo_MPDark/QAP_3529.py
@@ -13,6 +13,7 @@ from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.FixVerifier import FixVerifier
 from test_framework.core.test_case import TestCase
 from test_framework.algo_formulas_manager import AlgoFormulasManager
+from test_framework.data_sets import constants
 
 
 class QAP_3529(TestCase):
@@ -38,6 +39,7 @@ class QAP_3529(TestCase):
         self.weight_itg = 1
         self.qty_chix_child, self.qty_bats_child, self.qty_itg_child = AlgoFormulasManager.get_child_qty_on_venue_weights(self.qty, None, self.weight_chix, self.weight_bats, self.weight_itg)
         self.price = 20
+        self.algopolicy = constants.ClientAlgoPolicy.qa_mpdark_3.value
         # endregion
 
         # region Gateway Side
@@ -98,7 +100,7 @@ class QAP_3529(TestCase):
 
         self.MP_Dark_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_MPDark_params()
         self.MP_Dark_order.add_ClordId((os.path.basename(__file__)[:-3]))
-        self.MP_Dark_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument))
+        self.MP_Dark_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument, ClientAlgoPolicyID=self.algopolicy))
 
         self.fix_manager_sell.send_message_and_receive_response(self.MP_Dark_order, case_id_1)
 
