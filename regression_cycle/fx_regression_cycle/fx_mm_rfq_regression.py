9c8c346990dfa51af29f4cec4194cc8f09607fdb
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2022-06-27 09:24:14 +0200

    Update regression cycle

--- regression_cycle/fx_regression_cycle/fx_mm_rfq_regression.py
+++ regression_cycle/fx_regression_cycle/fx_mm_rfq_regression.py
@@ -26,8 +26,9 @@ from test_cases.fx.fx_mm_rfq.QAP_5345 import QAP_5345
 from test_cases.fx.fx_mm_rfq.QAP_5353 import QAP_5353
 from test_cases.fx.fx_mm_rfq.QAP_7168 import QAP_7168
 from test_cases.fx.fx_mm_rfq.QAP_7862 import QAP_7862
-from test_cases.fx.fx_mm_rfq.interpolation import QAP_3747, QAP_3734, QAP_3739, QAP_3851
+from test_cases.fx.fx_mm_rfq.interpolation import QAP_3747, QAP_3734, QAP_3851
 from test_cases.fx.fx_mm_rfq.interpolation.QAP_3689 import QAP_3689
+from test_cases.fx.fx_mm_rfq.interpolation.QAP_3739 import QAP_3739
 from test_cases.fx.fx_mm_rfq.interpolation.QAP_3766 import QAP_3766
 from test_cases.fx.fx_mm_rfq.interpolation.QAP_3772 import QAP_3772
 from test_cases.fx.fx_mm_rfq.interpolation.QAP_3805 import QAP_3805
@@ -109,7 +110,7 @@ def test_run(parent_id=None):
         QAP_3761(report_id, session_id, configuration.data_set, configuration.environment).execute()
         QAP_3766(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_3772(report_id, session_id, configuration.data_set, configuration.environment).execute()
-        QAP_3739.execute(report_id)
+        QAP_3739(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_3805(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_3806(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_3807(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
