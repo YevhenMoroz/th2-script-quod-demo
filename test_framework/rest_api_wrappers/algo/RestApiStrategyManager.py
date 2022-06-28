4b477db5baf07376688baf6044c8dca8823544a7
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2022-06-28 12:03:34 +0200

    add second version of modify_strategy

--- test_framework/rest_api_wrappers/algo/RestApiStrategyManager.py
+++ test_framework/rest_api_wrappers/algo/RestApiStrategyManager.py
@@ -46,4 +46,42 @@ class RestApiAlgoManager(RestApiManager):
         if not triggered:
             raise ValueError(f"No scenarioParameterName with name {parameter_name}")
         # endregion
-        print()
+
+    def modify_strategy2(self, strategy_name: str, parameters: dict):
+        # region send get request
+        rest_manager = RestApiManager("rest_wa319kuiper", self.case_id)
+        find_all_algo_policy = RestApiAlgoPolicyMessages().find_all_algo_policies()
+        grpc_reply = rest_manager.send_get_request(find_all_algo_policy)
+        strategy = rest_manager.parse_response_details(grpc_reply, {"algoPolicyName": strategy_name})
+        # endregion
+
+        # region modify strategy
+        strategy.pop("alive")
+        if "algoPolicyParameter" in strategy.keys():
+            triggered = 0
+            for param in strategy["algoPolicyParameter"]:
+                if param["scenarioParameterName"] in parameters.keys():
+                    param["algoParameterValue"] = parameters[param["scenarioParameterName"]]
+                    triggered += 1
+            if triggered != len(parameters):
+                raise ValueError(f"Not all scenarioParameterName found")
+        else:
+            raise ValueError("No algoPolicyParameter at current strategy")
+        modify_algo_policy = RestApiAlgoPolicyMessages().modify_algo_policy(strategy)
+        rest_manager.send_post_request(modify_algo_policy)
+        # endregion
+
+        # region check is modify confirmed
+        find_all_algo_policy2 = RestApiAlgoPolicyMessages().find_all_algo_policies()
+        grpc_reply2 = rest_manager.send_get_request(find_all_algo_policy2)
+        strategy_updated = rest_manager.parse_response_details(grpc_reply2, {"algoPolicyName": strategy_name})
+        triggered = 0
+        for param in strategy_updated["algoPolicyParameter"]:
+            if param["scenarioParameterName"] in parameters.keys():
+                triggered += 1
+                if param["algoParameterValue"] != parameters[param["scenarioParameterName"]]:
+                    raise ValueError(f"Strategy didn't modify parameter {param['algoParameterValue']} with value {parameters[param['scenarioParameterName']]}")
+            pass
+        if triggered != len(parameters):
+            raise ValueError(f"Not all scenarioParameterName found at updated strategy")
+        # endregion
