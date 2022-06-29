f8fa48a28f775f51ecdd358e483aa14400b07081
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-06-29 15:16:30 +0200

    QAP-6659 rule and MD for test

---
+++ test_cases/algo/Algo_PercentageVolume/QAP_6659.py
@@ -0,0 +1,166 @@
+import os
+import time
+from pathlib import Path
+
+from test_framework.core.try_exept_decorator import try_except
+from custom import basic_custom_actions as bca
+from rule_management import RuleManager
+from test_framework.data_sets.constants import DirectionEnum, Status, GatewaySide
+from test_framework.fix_wrappers.algo.FixMessageNewOrderSingleAlgo import FixMessageNewOrderSingleAlgo
+from test_framework.fix_wrappers.algo.FixMessageExecutionReportAlgo import FixMessageExecutionReportAlgo
+from test_framework.fix_wrappers.algo.FixMessageMarketDataSnapshotFullRefreshAlgo import FixMessageMarketDataSnapshotFullRefreshAlgo
+from test_framework.fix_wrappers.algo.FixMessageMarketDataIncrementalRefreshAlgo import FixMessageMarketDataIncrementalRefreshAlgo
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.core.test_case import TestCase
+from test_framework.data_sets import constants
+
+
+class QAP_6659(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, data_set=None, environment=None):
+        super().__init__(report_id=report_id, data_set=data_set, environment=environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+
+        self.fix_env1 = self.environment.get_list_fix_environment()[0]
+
+        # region th2 components
+        self.fix_manager_sell = FixManager(self.fix_env1.sell_side, self.test_id)
+        self.fix_manager_feed_handler = FixManager(self.fix_env1.feed_handler, self.test_id)
+        self.fix_verifier_sell = FixVerifier(self.fix_env1.sell_side, self.test_id)
+        self.fix_verifier_buy = FixVerifier(self.fix_env1.buy_side, self.test_id)
+        # endregion
+
+        # region order parameters
+        self.qty = 1000
+        self.price = 1
+        self.childMinValue = 10
+        self.volume = 10
+        self.tif_ioc = constants.TimeInForce.ImmediateOrCancel.value
+        self.md_entry_px = 0
+        self.md_entry_size = 0
+        self.price_ask = 5
+        self.price_bid = 1
+        self.qty_bid = self.qty_ask = 1000000
+        # endregion
+
+        # region Gateway Side
+        self.gateway_side_buy = GatewaySide.Buy
+        self.gateway_side_sell = GatewaySide.Sell
+        # endregion
+
+        # region Status
+        self.status_pending = Status.Pending
+        self.status_new = Status.New
+        self.status_fill = Status.Fill
+        # endregion
+
+        # region instrument
+        self.instrument = self.data_set.get_fix_instrument_by_name("instrument_2")
+        # endregion
+
+        # region Direction
+        self.FromQuod = DirectionEnum.FromQuod
+        self.ToQuod = DirectionEnum.ToQuod
+        # endregion
+
+        # region venue param
+        self.ex_destination_1 = self.data_set.get_mic_by_name("mic_1")
+        self.client = self.data_set.get_client_by_name("client_1")
+        self.account = self.data_set.get_account_by_name("account_1")
+        self.s_par = self.data_set.get_listing_id_by_name("listing_1")
+        # endregion
+
+        # region Key parameters
+        self.key_params_cl = self.data_set.get_verifier_key_parameters_by_name("verifier_key_parameters_1")
+        self.key_params = self.data_set.get_verifier_key_parameters_by_name("verifier_key_parameters_2")
+        # endregion
+
+        self.rule_list = []
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Rule creation
+        rule_manager = RuleManager()
+        nos_ioc_rule = rule_manager.add_NewOrdSingle_IOC(self.fix_env1.buy_side, self.account, self.ex_destination_1, True, self.qty, self.price)
+        self.rule_list = [nos_ioc_rule]
+        # endregion
+
+        case_id_0 = bca.create_event("Send Market Data", self.test_id)
+        # region Send_MarketData
+        self.fix_manager_feed_handler.set_case_id(case_id_0)
+        market_data_snap_shot_par = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
+        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=1, MDEntrySize=80) # 80 or 50
+        self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
+        # endregion
+
+        # region Send_MarketData for LTQ
+        self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data for LTQ", self.test_id))
+        market_data_snap_shot_par = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_ltq().update_MDReqID(self.s_par, self.fix_env1.feed_handler)  # 1015
+        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntriesIR', MDEntryPx=self.md_entry_px, MDEntrySize=self.md_entry_size)
+        self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
+        time.sleep(3)
+        # endregion
+
+        # region Send NewOrderSingle (35=D) for POV order
+        case_id_1 = bca.create_event("Create POV Order", self.test_id)
+        self.fix_verifier_sell.set_case_id(case_id_1)
+
+        self.POV_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_POV_min_value_params()
+        self.POV_order.add_ClordId((os.path.basename(__file__)[:-3]))
+        self.POV_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument))
+
+        self.fix_manager_sell.send_message_and_receive_response(self.POV_order, case_id_1)
+
+        time.sleep(3)
+        # endregion
+
+        # region Check Sell side
+        self.fix_verifier_sell.check_fix_message(self.POV_order, direction=self.ToQuod, message_name='Sell side NewOrderSingle')
+
+        pending_POV_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.POV_order, self.gateway_side_sell, self.status_pending)
+        self.fix_verifier_sell.check_fix_message(pending_POV_order_params, key_parameters=self.key_params_cl, message_name='Sell side ExecReport PendingNew')
+
+        new_POV_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.POV_order, self.gateway_side_sell, self.status_new)
+        self.fix_verifier_sell.check_fix_message(new_POV_order_params, key_parameters=self.key_params_cl, message_name='Sell side ExecReport New')
+        # endregion
+        print('123')
+        # region Send_MarketData
+        self.fix_manager_feed_handler.set_case_id(case_id_0)
+        market_data_snap_shot_par = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
+        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=1, MDEntrySize=112)
+        self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
+        # endregion
+
+        # region Send_MarketData for LTQ
+        self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data for LTQ", self.test_id))
+        market_data_snap_shot_par = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_ltq().update_MDReqID(self.s_par, self.fix_env1.feed_handler)  # 1015
+        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntriesIR', MDEntryPx=self.md_entry_px, MDEntrySize=self.md_entry_size)
+        self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
+        # endregion
+
+        # region Check child DMA order
+        # self.fix_verifier_buy.set_case_id(bca.create_event("Child DMA 1 order", self.test_id))
+        #
+        # self.dma_order = FixMessageNewOrderSingleAlgo().set_DMA_params()
+        # self.dma_order.change_parameters(dict(OrderQty=self.qty, Price=self.price, Instrument=self.instrument, TimeInForce=self.tif_ioc))
+        # self.fix_verifier_buy.check_fix_message(self.dma_order, key_parameters=self.key_params, message_name='Buy side NewOrderSingle Child DMA 1 order')
+        # # endregion
+        #
+        # # region Check Fill
+        # self.fix_verifier_buy.set_case_id(bca.create_event("Fill child Order", self.test_id))
+        # # Fill Order
+        # fill_dma_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_order, self.gateway_side_buy, self.status_fill)
+        # fill_dma_order.change_parameters(dict(CumQty=self.qty, LeavesQty=0, LastQty=self.qty, LastPx=self.last_price))
+        # self.fix_verifier_buy.check_fix_message(fill_dma_order, key_parameters=self.key_params, direction=self.ToQuod, message_name='Buy side ExecReport Fill')
+        #
+        # self.fix_verifier_sell.set_case_id(bca.create_event("Fill Algo Order", self.test_id))
+        # fill_multilisted_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.POV_order, self.gateway_side_sell, self.status_fill)
+        # fill_multilisted_order.change_parameters(dict(CumQty=self.qty, LeavesQty=0, LastQty=self.qty, Instrument="*")).remove_parameter('SecAltIDGrp').remove_parameter('SecondaryClOrdID')
+        # self.fix_verifier_sell.check_fix_message(fill_multilisted_order, key_parameters=self.key_params_cl,  message_name='Sell side ExecReport Fill')
+        # # endregion
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        rule_manager = RuleManager()
+        rule_manager.remove_rules(self.rule_list)
