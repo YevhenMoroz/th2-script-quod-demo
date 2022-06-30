ed0e6f6b3d7c38352f32bfe66560d6c3b6754ac1
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-06-30 13:34:34 +0200

    Edit ER

--- test_framework/fix_wrappers/algo/FixMessageOrderCancelRejectReportAlgo.py
+++ test_framework/fix_wrappers/algo/FixMessageOrderCancelRejectReportAlgo.py
@@ -11,58 +11,58 @@ class FixMessageOrderCancelRejectReportAlgo(FixMessageOrderCancelRejectReport):
         super().__init__()
         super().change_parameters(parameters)
 
-    # def set_params_from_new_order_single(self, new_order_single: FixMessageNewOrderSingle, side: GatewaySide, status: Status):
-    #     if side is GatewaySide.Buy:
-    #         if status is Status.Eliminate:
-    #             self.__set_eliminate_buy(new_order_single)
-    #         else:
-    #             raise Exception(f'Incorrect Status')
-    #     elif side is GatewaySide.Sell:
-    #         if status is Status.New:
-    #             self.__set_new_sell(new_order_single)
-    #         else:
-    #             raise Exception(f'Incorrect Status')
-    #     return self
-    #
-    # def __set_eliminate_buy(self, new_order_single: FixMessageNewOrderSingle = None):
-    #     temp = dict()
-    #     temp.update(
+    def set_params_from_new_order_single(self, new_order_single: FixMessageNewOrderSingle, side: GatewaySide, status: Status):
+        if side is GatewaySide.Buy:
+            if status is Status.Reject:
+                self.__set_reject_buy(new_order_single)
+            else:
+                raise Exception(f'Incorrect Status')
+        elif side is GatewaySide.Sell:
+            if status is Status.New:
+                self.__set_new_sell(new_order_single)
+            else:
+                raise Exception(f'Incorrect Status')
+        return self
+
+    def __set_reject_buy(self, new_order_single: FixMessageNewOrderSingle = None):
+        temp = dict()
+        temp.update(
+            Account=new_order_single.get_parameter('Account'),
+            OrderID='*',
+            ClOrdID='*',
+            OrigClOrdID='*',
+            OrdStatus='8',
+            CxlRejResponseTo='1',
+            Text='cancel reject',
+            TransactTime='*'
+        )
+        super().change_parameters(temp)
+        return self
+
+    def __set_new_sell(self, new_order_single: FixMessageNewOrderSingle = None):
+        temp = dict()
+        temp.update(
+            Account=new_order_single.get_parameter('Account'),
+            OrderID='*',
+            ClOrdID=new_order_single.get_parameter("ClOrdID"),
+            OrigClOrdID='*',
+            OrdStatus="0",
+            CxlRejResponseTo='1',
+            Text='cancel reject',
+            TransactTime='*',
+        )
+        super().change_parameters(temp)
+        return self
+
+    # def update_fix_message(self, parameters: dict) -> None:
+    #     temp = dict(
     #         Account='*',
-    #         OrderID='*',
-    #         ClOrdID='*',
-    #         OrigClOrdID='*',
+    #         OrderID=parameters["ClOrdID"],
+    #         ClOrdID=parameters["ClOrdID"],
+    #         OrigClOrdID=parameters["ClOrdID"],
     #         OrdStatus='0',
     #         CxlRejResponseTo='1',
     #         Text='cancel reject',
     #         TransactTime='*'
     #     )
     #     super().change_parameters(temp)
-    #     return self
-    #
-    # def __set_new_sell(self, new_order_single: FixMessageNewOrderSingle = None):
-    #     temp = dict()
-    #     temp.update(
-    #         Account=new_order_single.get_parameter('Account'),
-    #         OrderID='*',
-    #         ClOrdID=new_order_single.get_parameter("ClOrdID"),
-    #         OrigClOrdID='*',
-    #         OrdStatus="0",
-    #         CxlRejResponseTo='1',
-    #         Text='cancel reject',
-    #         TransactTime='*',
-    #     )
-    #     super().change_parameters(temp)
-    #     return self
-
-    def update_fix_message(self, parameters: dict) -> None:
-        temp = dict(
-            Account='*',
-            OrderID=parameters["ClOrdID"],
-            ClOrdID=parameters["ClOrdID"],
-            OrigClOrdID=parameters["ClOrdID"],
-            OrdStatus='0',
-            CxlRejResponseTo='1',
-            Text='cancel reject',
-            TransactTime='*'
-        )
-        super().change_parameters(temp)
