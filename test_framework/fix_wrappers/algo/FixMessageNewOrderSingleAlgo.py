8c3b776219039af11c1b3e6042005d1a0ac63dfb
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-07-04 15:44:30 +0200

    Add DMA params for child of SORPING

--- test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
+++ test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
@@ -665,3 +665,25 @@ class FixMessageNewOrderSingleAlgo(FixMessageNewOrderSingle):
         }
         super().change_parameters(base_parameters)
         return self
+
+    def set_DMA_Child_of_SORPING_params(self) -> FixMessageNewOrderSingle:
+        base_parameters = {
+            "Account": 'KEPLER',
+            'ClOrdID': '*',
+            'Currency': 'EUR',
+            'HandlInst': '1',
+            'OrderQty': '1000',
+            'OrdType': '2',
+            'Price': '11',
+            'Side': '1',
+            'Instrument': self.get_data_set().get_fix_instrument_by_name('instrument_8'),
+            'TimeInForce': '0',
+            "TransactTime": '*',
+            'ExDestination': 'QDL1',
+            'OrderCapacity': 'A',
+            'ChildOrderID': '*',
+            'IClOrdIdAO': 'OD_5fgfDXg-00',
+            'ShortCode': '17536'
+        }
+        super().change_parameters(base_parameters)
+        return self
