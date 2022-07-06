f9c961ecaf729626e525ef469fba2bcc140735d9
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-07-06 09:54:45 +0200

    QAP-3731 edit setup OrderCancelReplaceRequest message in code according to new FixMessageOrderCancelReplaceRequestAlgo

--- test_cases/algo/Algo_Kepler/Algo_MPDark/QAP_3731.py
+++ test_cases/algo/Algo_Kepler/Algo_MPDark/QAP_3731.py
@@ -153,7 +153,7 @@ class QAP_3731(TestCase):
         self.fix_verifier_sell.set_case_id(case_id_2)
 
         self.MP_Dark_order_replace_params = FixMessageOrderCancelReplaceRequestAlgo(self.MP_Dark_order)
-        self.MP_Dark_order_replace_params.change_parameters(dict(MinQty=self.inc_minQty)).add_tag(dict(IClOrdIdAO='OD_5fgfDXg-00', ShortCode='17536'))
+        self.MP_Dark_order_replace_params.change_parameters(dict(MinQty=self.inc_minQty))
         self.fix_manager_sell.send_message_and_receive_response(self.MP_Dark_order_replace_params, case_id_2)
 
         time.sleep(1)
