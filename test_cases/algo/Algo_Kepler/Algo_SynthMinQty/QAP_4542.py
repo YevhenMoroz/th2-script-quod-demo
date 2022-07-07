4e4ccd9896b270a0fc322a25ffbf422dd6e7fbdf
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-07-07 11:26:35 +0200

    QAP-4542 Edit ERs

--- test_cases/algo/Algo_Kepler/Algo_SynthMinQty/QAP_4542.py
+++ test_cases/algo/Algo_Kepler/Algo_SynthMinQty/QAP_4542.py
@@ -154,7 +154,7 @@ class QAP_4542(TestCase):
 
         # region check fill first dma child order
         er_fill_dma_qdl1_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_qdl1_order, self.gateway_side_buy, self.status_fill)
-        er_fill_dma_qdl1_order.change_parameters(dict(CumQty=self.trade_1_qty, LeavesQty=0, LastQty=self.trade_1_qty, LastPx=self.price)).remove_parameter('ExDestination')
+        er_fill_dma_qdl1_order.change_parameters(dict(CumQty=self.trade_1_qty, LeavesQty=0, LastQty=self.trade_1_qty, LastPx=self.price))
         self.fix_verifier_buy.check_fix_message(er_fill_dma_qdl1_order, self.key_params_ER_fill_child, self.ToQuod, "Buy Side ExecReport Fill Aggressive DMA 1 order")
         # endregion
         
@@ -173,7 +173,7 @@ class QAP_4542(TestCase):
 
         # region check fill second dma child order
         er_fill_dma_qdl2_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_qdl2_order, self.gateway_side_buy, self.status_fill)
-        er_fill_dma_qdl2_order.change_parameters(dict(CumQty=self.trade_2_qty, LeavesQty=0, LastQty=self.trade_2_qty, LastPx=self.price)).remove_parameter('ExDestination')
+        er_fill_dma_qdl2_order.change_parameters(dict(CumQty=self.trade_2_qty, LeavesQty=0, LastQty=self.trade_2_qty, LastPx=self.price))
         self.fix_verifier_buy.check_fix_message(er_fill_dma_qdl2_order, self.key_params_ER_fill_child, self.ToQuod, "Buy Side ExecReport Fill Aggressive DMA 2 order")
         # endregion
         
