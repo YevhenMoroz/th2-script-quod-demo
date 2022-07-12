f67e2cff0f0f7403b00ace1da2362c268104bd86
dmogilny <dmitry.mogilny.contractor@quodfinancial.com>
2022-07-12 15:28:40 +0300

    QAP-6703: Automate and refactor WA test-cases.

--- test_cases/web_admin/web_admin_test_cases/users/QAP_5694.py
+++ test_cases/web_admin/web_admin_test_cases/users/QAP_5694.py
@@ -3,36 +3,40 @@ import time
 import traceback
 
 from custom import basic_custom_actions
-from test_framework.web_admin_core.pages.general.common.common_page import CommonPage
+
 from test_framework.web_admin_core.pages.login.login_page import LoginPage
+from test_framework.web_admin_core.pages.root.side_menu import SideMenu
 from test_framework.web_admin_core.utils.web_driver_container import WebDriverContainer
 from test_cases.web_admin.web_admin_test_cases.common_test_case import CommonTestCase
 
 
-class QAP_3015(CommonTestCase):
+class QAP_5694(CommonTestCase):
 
     def __init__(self, web_driver_container: WebDriverContainer, second_lvl_id, data_set=None, environment=None):
         super().__init__(web_driver_container, self.__class__.__name__, second_lvl_id, data_set=data_set,
                          environment=environment)
-        self.login = 'adm03'
-        self.password = 'adm03'
-        self.link320 = 'http://10.0.22.38:6380/adm/qakharkiv320/#/auth/login'
+
+        self.login = 'adm_loca'
+        self.password = 'adm_loca'
+
+    def precondition(self):
+        login_page = LoginPage(self.web_driver_container)
+        login_page.login_to_web_admin(self.login, self.password)
+        time.sleep(2)
 
     def test_context(self):
-
         try:
-            login_page = LoginPage(self.web_driver_container)
-            login_page.login_to_web_admin(self.login, self.password)
-            time.sleep(2)
-            common_page = CommonPage(self.web_driver_container)
-            site1_cookies = common_page.get_browser_cookies()
+            self.precondition()
 
-            common_page.open_new_browser_tab_and_set_url(self.link320)
+            side_menu = SideMenu(self.web_driver_container)
+            side_menu.open_desks_page()
             time.sleep(2)
-            login_page.login_to_web_admin(self.login, self.password)
-            site2_cookies = common_page.get_browser_cookies()
-
-            self.verify("Is cookies equal?", False, site1_cookies == site2_cookies)
+            self.verify("Only Desks tab displayed",
+                        [side_menu.is_institutions_page_tab_displayed(),
+                         side_menu.is_zones_page_tab_displayed(),
+                         side_menu.is_locations_page_tab_displayed(),
+                         side_menu.is_desks_page_tab_displayed()],
+                        [False, False, False, True])
 
         except Exception:
             basic_custom_actions.create_event("TEST FAILED before or after verifier", self.test_case_id,
