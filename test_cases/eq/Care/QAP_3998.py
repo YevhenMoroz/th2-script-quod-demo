f94e706be7d231ca472d0da42b3d184f1814255c
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-07-19 10:30:36 +0200

    modified QAP_3998

--- test_cases/eq/Care/QAP_3998.py
+++ test_cases/eq/Care/QAP_3998.py
@@ -1,12 +1,11 @@
 import logging
-import time
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from test_framework.core.try_exept_decorator import try_except
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.core.test_case import TestCase
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts, OrderType, TimeInForce
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
@@ -14,6 +13,7 @@ from test_framework.win_gui_wrappers.oms.oms_order_ticket import OMSOrderTicket
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
+
 
 @try_except(test_id=Path(__file__).name[:-3])
 class QAP_3998(TestCase):
@@ -26,10 +26,10 @@ class QAP_3998(TestCase):
         self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
         self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
         self.qty = "1000"
-        self.price ="50"
-        self.fix_message.change_parameters(['OrderQtyData', {'OrderQty': self.qty}, "Price", self.price])
-        self.lookup = "DNX"
-        self.route = self.data_set.get_route("route_1")
+        self.price = "50"
+        self.fix_message.change_parameters({'OrderQtyData': {'OrderQty': self.qty}, "Price": self.price})
+        self.lookup = self.data_set.get_lookup_by_name('lookup_2')
+        self.route = self.data_set.get_route("route_2")
         self.order_book = OMSOrderBook(self.test_id, self.session_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
         self.order_ticket = OMSOrderTicket(self.test_id, self.session_id)
@@ -45,52 +45,10 @@ class QAP_3998(TestCase):
         # region accept CO order
         self.client_inbox.accept_order()
         # endregion
-        # region do direct child care
-        self.order_book.extract_error_from_order_ticket
-
-
-def execute(report_id):
-    case_name = "QAP-3998"
-
-    seconds, nanos = timestamps()  # Store case start time
-
-    # region Declarations
-    act = Stubs.win_act_order_book
-    common_act = Stubs.win_act
-    qty = "800"
-    newQty = "100"
-    price = "40"
-    newPrice = "1"
-    time = datetime.utcnow().isoformat()
-    lookup = "PROL"
-    client = "CLIENTSKYLPTOR"
-    # endregion
-    list_param = {'qty': qty, 'Price': newPrice}
-    # region Open FE
-    case_id = create_event(case_name, report_id)
-    session_id = set_session_id()
-    set_base(session_id, case_id)
-    base_request = get_base_request(session_id, case_id)
-    work_dir = Stubs.custom_config['qf_trading_fe_folder']
-    username = Stubs.custom_config['qf_trading_fe_user']
-    password = Stubs.custom_config['qf_trading_fe_password']
-    eq_wrappers.open_fe(session_id, report_id, case_id, work_dir, username, password)
-    # endregionA
-
-    # region Create CO
-    eq_wrappers.create_order_via_fix(case_id, 3, 1, client, 2, qty, 0, price)
-    # endregion
-    # region DirectChildCare split
-    error_message = ExtractDirectsValuesRequest.DirectsExtractedValue()
-    error_message.name = "ErrorMessage"
-    error_message.type = ExtractDirectsValuesRequest.DirectsExtractedType.ERROR_MESSAGE
-    request = ExtractDirectsValuesRequest()
-    request.extractionId = "DirectErrorMessageExtractionID"
-    request.extractedValues.append(error_message)
-    response = call(Stubs.win_act_order_book.orderBookDirectChildCare,
-                direct_child_care('UnmatchedQty', '0', '', 'ChiX direct access', request))
-    verifier = Verifier(case_id)
-    verifier.set_event_name("Check value")
-    verifier.compare_values('Error_message', 'Error - Qty Percentage should be greater than zero (0)', response['ErrorMessage'])
-    verifier.verify()
-    # endregion
+        # region do direct child care and extract error
+        result = self.order_book.direct_child_care_order(extracted_error=True, qty_percentage="0",
+                                                         qty_type=self.qty_type, route=self.route)
+        print(result)
+        self.order_book.compare_values({"": 'Error - Qty Percentage should be greater than zero (0)'},
+                                       result, "Check error in footer of DirectChildCare Ticket")
+        # endregion
