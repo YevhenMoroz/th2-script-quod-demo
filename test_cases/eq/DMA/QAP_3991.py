5cde3b6456a02e45cd73ecde77511f67d4cf3419
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-07-20 12:25:37 +0200

    modified QAP_3991

--- test_cases/eq/DMA/QAP_3991.py
+++ test_cases/eq/DMA/QAP_3991.py
@@ -1,53 +1,50 @@
 import logging
+from pathlib import Path
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from custom import basic_custom_actions as bca
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
+from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
+from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 
-import test_framework.old_wrappers.eq_fix_wrappers
-from test_framework.old_wrappers import eq_wrappers
-from custom.basic_custom_actions import create_event, timestamps
-from stubs import Stubs
-from test_framework.old_wrappers.eq_wrappers import open_fe
-from win_gui_modules.utils import set_session_id, get_base_request
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-def execute(report_id):
-    case_name = "QAP-3991"
-    case_id = create_event(case_name, report_id)
-    # region Declarations
-    qty = "900"
-    price = "40"
-    dummy_client = "DUMMY_CLIENT"
-    client = "CLIENTYMOROZ"
-    account = "YM_client_SA1"
-    lookup = "VETO"
-    route = "Route via FIXBUYTH2"
+@try_except(test_id=Path(__file__).name[:-3])
+class QAP_3991(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_dma_limit()
+        self.dummy_account = self.data_set.get_client_by_name('client_dummy')
+        self.client = self.data_set.get_client_by_name('client_pt_1')
+        self.sec_acc = self.data_set.get_account_by_name('client_pt_1_acc_1')
+        self.fix_message.change_parameter('Account', self.dummy_account)
+        self.route = self.data_set.get_route('route_2')
+        self.qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
+        self.price = self.fix_message.get_parameter('Price')
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
 
-    act = Stubs.win_act_order_book
-    common_act = Stubs.win_act
-    work_dir = Stubs.custom_config['qf_trading_fe_folder']
-    username = Stubs.custom_config['qf_trading_fe_user']
-    password = Stubs.custom_config['qf_trading_fe_password']
-    session_id = set_session_id()
-    base_request = get_base_request(session_id, case_id)
-    recipient = 'Desk of SalesDealers 1 (CL)'
-    # endregion
-    # region Open FE
-    open_fe(session_id, report_id, case_id, work_dir, username)
-    # endregion
-    # region create DMA
-    responce= test_framework.old_wrappers.eq_fix_wrappers.create_order_via_fix(case_id, 1, 1, dummy_client, 2, qty, 0, price)
-    # endregion
-    # region verify values
-    eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Held")
-    eq_wrappers.verify_order_value(base_request, case_id, "Client ID", "DUMMY")
-    # endregion verify values
-    # region GroupModify
-    eq_wrappers.group_modify(base_request,client,account,route,"Free notes")
-    # endregion
-    # region verify values
-    eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Open")
-    eq_wrappers.verify_order_value(base_request, case_id, "Client ID", client)
-    eq_wrappers.verify_order_value(base_request, case_id, "Account ID", account)
-    eq_wrappers.verify_order_value(base_request, case_id, "Routes", "ESBUYTH2")
-    # endregion verify values
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Declaration
+        # region create Held order
+        response = self.fix_manager.send_message_and_receive_response_fix_standard(self.fix_message)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.held.value})
+        # endregion
+        # region group modify
+        self.order_book.group_modify(client=self.client, security_account=self.sec_acc, routes=self.route,
+                                     free_notes="122321")
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.open.value})
+        # endregion
