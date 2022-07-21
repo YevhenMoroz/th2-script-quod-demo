8fb3e0cd5474b05b6feca81eca119fcca60a171b
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-07-21 17:02:46 +0200

    modified QAP-4627

--- test_cases/eq/Care/QAP_4627.py
+++ test_cases/eq/Care/QAP_4627.py
@@ -1,16 +1,11 @@
 import logging
-import os
-
+from pathlib import Path
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
 from custom import basic_custom_actions as bca
-from stubs import Stubs
 from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.FixVerifier import FixVerifier
-from test_framework.fix_wrappers.SessionAlias import SessionAliasOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.win_gui_wrappers.TestCase import TestCase
-from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from test_framework.win_gui_wrappers.base_window import try_except
-from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
+from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
 
 logger = logging.getLogger(__name__)
@@ -18,37 +13,27 @@ logger.setLevel(logging.INFO)
 timeouts = True
 
 
+@try_except(test_id=Path(__file__).name[:-3])
 class QAP_4627(TestCase):
-    def __init__(self, report_id, session_id, file_name):
-        super().__init__(report_id, session_id)
-        self.case_id = bca.create_event(os.path.basename(__file__)[:-3], self.test_id)
-        self.file_name = file_name
-        self.ss_connectivity = SessionAliasOMS().ss_connectivity
-        self.bs_connectivity = SessionAliasOMS().bs_connectivity
 
-    def qap_4627(self):
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
+        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
+        self.dummy_client = self.data_set.get_client_by_name('client_dummy')
+        self.fix_message.change_parameter('Account', self.dummy_client)
+        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
         # region Declaration
-        fix_manager = FixManager(self.ss_connectivity, self.report_id)
-        ord_book = OMSOrderBook(self.case_id, self.session_id)
-        main_window = BaseMainWindow(self.case_id, self.session_id)
-        work_dir = Stubs.custom_config['qf_trading_fe_folder']
-        username = Stubs.custom_config['qf_trading_fe_user']
-        password = Stubs.custom_config['qf_trading_fe_password']
+        # region create CO order
+        self.fix_manager.send_message_fix_standard(self.fix_message)
+        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
         # endregion
-        # region Open FE
-        main_window.open_fe(self.report_id, work_dir, username, password)
+        # region check fields
+        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
+            {OrderBookColumns.sts.value: ExecSts.held.value, OrderBookColumns.cl_acc_gr_id.value: self.dummy_client})
         # endregion
-        # region Send NewOrderSingle
-        nos = FixMessageNewOrderSingleOMS().set_default_care_limit().change_parameters({"Account": "DUMMY_CLIENT"})
-        fix_manager.send_message_fix_standard(nos)
-        # endregion
-        # region Check value
-        exp_val = {"Sts": "Held", "Client ID": "DUMMY"}
-        ord_book.scroll_order_book()
-        act_val = ord_book.extract_fields_list(exp_val)
-        ord_book.compare_values(exp_val, act_val, "Check Held")
-        # endregion
-
-    @try_except(test_id=os.path.basename(__file__))
-    def execute(self):
-        self.qap_4627()
