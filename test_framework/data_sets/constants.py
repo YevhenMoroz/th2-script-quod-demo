af6bd31ee1ca169848daeb4e8aec5e2b9ea67754
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-07-26 08:15:44 +0200

    Edit algopolicy enums

--- test_framework/data_sets/constants.py
+++ test_framework/data_sets/constants.py
@@ -153,8 +153,8 @@ class ClientAlgoPolicy(Enum):
     qa_sorping = "QA_SORPING"
     qa_sorping_1 = "QA_Auto_SORPING_1"
     qa_sorping_2 = "QA_Auto_SORPING_2"
-    qa_multiply_y = 'QA_Auto_SORPING_ME_Y'
-    qa_multiply_n = 'QA_Auto_SORPING_ME_N'
+    qa_multiple_y = 'QA_Auto_SORPING_ME_Y'
+    qa_multiple_n = 'QA_Auto_SORPING_ME_N'
 
 
 class OrderType(Enum):
