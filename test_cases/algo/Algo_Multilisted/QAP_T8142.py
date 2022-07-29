bcf4a9915c7e51bd756bcb7d9a4d7bf93a3b2334
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-07-29 12:05:55 +0200

    QAP-T8142 fully automated

--- test_cases/algo/Algo_Multilisted/QAP_T8142.py
+++ test_cases/algo/Algo_Multilisted/QAP_T8142.py
@@ -17,7 +17,7 @@ from test_framework.core.test_case import TestCase
 from test_framework.data_sets import constants
 
 
-class QAP_6797(TestCase):
+class QAP_T8142(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, data_set=None, environment=None):
         super().__init__(report_id=report_id, data_set=data_set, environment=environment)
