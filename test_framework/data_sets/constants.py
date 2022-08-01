147982d6c59d325d46c0bf3bc6db74ebe642c174
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-08-01 12:01:45 +0200

    Add algopolicy

--- test_framework/data_sets/constants.py
+++ test_framework/data_sets/constants.py
@@ -154,6 +154,7 @@ class ClientAlgoPolicy(Enum):
     qa_sorping_1 = "QA_Auto_SORPING_1"
     qa_sorping_2 = "QA_Auto_SORPING_2"
     qa_sorping_3 = "QA_Auto_SORPING_3"
+    qa_sorping_4 = "QA_Auto_SORPING_4"
     qa_multiple_y = 'QA_Auto_SORPING_ME_Y'
     qa_multiple_n = 'QA_Auto_SORPING_ME_N'
 
