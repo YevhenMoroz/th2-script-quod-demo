91029ef5f5b9ac4f3a8f160cd7392f2dca62028d
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-08-03 10:41:08 +0300

    modify QAP_T7358.py

--- test_cases/eq/Commissions/QAP_T7534.py
+++ test_cases/eq/Commissions/QAP_T7534.py
@@ -56,7 +56,8 @@ class QAP_T7534(TestCase):
             new_order_single = FixMessageNewOrderSingleOMS(self.data_set).set_default_dma_limit(
                 "instrument_3").add_ClordId((os.path.basename(__file__)[:-3])).change_parameters(
                 {'OrderQtyData': {'OrderQty': self.qty}, "Price": self.price, "Account": self.client,
-                 'PreAllocGrp': no_allocs, "ExDestination": self.data_set.get_mic_by_name("mic_2")})
+                 'PreAllocGrp': no_allocs, "ExDestination": self.data_set.get_mic_by_name("mic_2")
+                 ,"Currency": self.data_set.get_currency_by_name("currency_3")})
 
             self.response = self.fix_manager.send_message_and_receive_response_fix_standard(new_order_single)
         finally:
