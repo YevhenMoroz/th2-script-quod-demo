9681285c9a3460d14c715e78c062d19048a6dc0e
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2022-08-04 16:26:01 +0200

    QAP_T2460: small refactor

--- test_cases/fx/fx_mm_esp/QAP_T2460.py
+++ test_cases/fx/fx_mm_esp/QAP_T2460.py
@@ -16,7 +16,7 @@ from test_framework.fix_wrappers.forex.FixMessageMarketDataSnapshotFullRefreshSe
 from test_framework.fix_wrappers.forex.FixMessageNewOrderSingleFX import FixMessageNewOrderSingleFX
 
 
-class QAP_6148(TestCase):
+class QAP_T2460(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, session_id=None, data_set: BaseDataSet = None, environment: FullEnvironment = None):
         super().__init__(report_id, session_id, data_set, environment)
