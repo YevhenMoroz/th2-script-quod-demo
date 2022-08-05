efeaf53fec29de2908bb0d40bfd1e3307f3587e0
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-08-05 15:33:53 +0200

    Add params

--- test_framework/data_sets/algo_data_set/algo_const_enum.py
+++ test_framework/data_sets/algo_data_set/algo_const_enum.py
@@ -213,6 +213,8 @@ class AlgoVerifierKeyParameters(Enum):
     verifier_key_parameters_1 = ['ClOrdID', 'OrdStatus', 'ExecType', 'OrderQty', 'Price']
     verifier_key_parameters_2 = ['OrdStatus', 'ExecType', 'OrderQty', 'Price', 'TimeInForce']
     verifier_key_parameters_NOS_child = ['ExDestination', 'OrderQty', 'Price', 'TimeInForce']
+    verifier_key_parameters_NOS_child_with_minqty = ['ExDestination', 'OrderQty', 'Price', 'TimeInForce', 'MinQty']
+    verifier_key_parameters_NOS_child_with_stoppx = ['ExDestination', 'OrderQty', 'Price', 'TimeInForce', 'StopPx']
     verifier_key_parameters_ER_child = ['ExDestination', 'OrdStatus', 'ExecType', 'OrderQty', 'Price', 'TimeInForce', "OrdType"]
     verifier_key_parameters_ER_2_child = ['ExDestination', 'OrdStatus', 'ExecType']
     verifier_key_parameters_ER_Reject_Eliminate_child = ['Account', 'OrdStatus', 'ExecType', 'OrderQty', 'Price', 'TimeInForce']
