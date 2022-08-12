6c5fbd7696fdb557613be9ba351485aecb7c4872
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2022-08-12 15:10:45 +0200

    QAP_T2953: Automated

--- test_cases/fx/fx_mm_esp/QAP_T2953.py
+++ test_cases/fx/fx_mm_esp/QAP_T2953.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from test_framework.core.test_case import TestCase
@@ -20,6 +21,7 @@ class QAP_T2953(TestCase):
         self.fix_manager_fh = FixManager(self.fix_env.feed_handler, self.test_id)
         self.rates_tile = ClientRatesTile(self.test_id, self.session_id)
 
+        self.settle_date_spot = self.data_set.get_settle_date_by_name("spot")
         self.client = self.data_set.get_client_tier_by_name("client_tier_1")
         self.symbol = self.data_set.get_symbol_by_name("symbol_1")
         self.instrument = self.symbol + "-Spot"
@@ -31,6 +33,70 @@ class QAP_T2953(TestCase):
         self.ask_pips = PriceNaming.ask_pips
         self.bid_pips = PriceNaming.bid_pips
         self.widen_spread = ClientPrisingTileAction.widen_spread
+        self.md_entry_date = datetime.utcnow().strftime('%Y%m%d')
+        self.md_entry_time = datetime.utcnow().strftime('%H:%M:%S')
+        self.no_md_entries = [
+                {
+                    "MDEntryType": "0",
+                    "MDEntryPx": 1.1815,
+                    "MDEntrySize": 1000000,
+                    "MDQuoteType": 1,
+                    "MDEntryPositionNo": 1,
+                    "SettlDate": self.settle_date_spot,
+                    "MDEntryDate": self.md_entry_date,
+                    "MDEntryTime": self.md_entry_time
+                },
+                {
+                    "MDEntryType": "1",
+                    "MDEntryPx": 1.18151,
+                    "MDEntrySize": 1000000,
+                    "MDQuoteType": 1,
+                    "MDEntryPositionNo": 1,
+                    "SettlDate": self.settle_date_spot,
+                    "MDEntryDate": self.md_entry_date,
+                    "MDEntryTime": self.md_entry_time
+                },
+                {
+                    "MDEntryType": "0",
+                    "MDEntryPx": 1.1813,
+                    "MDEntrySize": 5000000,
+                    "MDQuoteType": 1,
+                    "MDEntryPositionNo": 2,
+                    "SettlDate": self.settle_date_spot,
+                    "MDEntryDate": self.md_entry_date,
+                    "MDEntryTime": self.md_entry_time
+                },
+                {
+                    "MDEntryType": "1",
+                    "MDEntryPx": 1.18165,
+                    "MDEntrySize": 5000000,
+                    "MDQuoteType": 1,
+                    "MDEntryPositionNo": 2,
+                    "SettlDate": self.settle_date_spot,
+                    "MDEntryDate": self.md_entry_date,
+                    "MDEntryTime": self.md_entry_time
+                },
+                {
+                    "MDEntryType": "0",
+                    "MDEntryPx": 1.181,
+                    "MDEntrySize": 10000000,
+                    "MDQuoteType": 1,
+                    "MDEntryPositionNo": 3,
+                    "SettlDate": self.settle_date_spot,
+                    "MDEntryDate": self.md_entry_date,
+                    "MDEntryTime": self.md_entry_time
+                },
+                {
+                    "MDEntryType": "1",
+                    "MDEntryPx": 1.18186,
+                    "MDEntrySize": 10000000,
+                    "MDQuoteType": 1,
+                    "MDEntryPositionNo": 3,
+                    "SettlDate": self.settle_date_spot,
+                    "MDEntryDate": self.md_entry_date,
+                    "MDEntryTime": self.md_entry_time
+                }
+            ]
         self.expected_default_bid = "150"
         self.expected_modified_bid = "140"
 
@@ -39,10 +105,11 @@ class QAP_T2953(TestCase):
         # region Step Precondition
         self.fix_md.set_market_data().\
             update_MDReqID(self.fix_md.get_parameter("MDReqID"), self.fix_env.feed_handler, 'FX')
+        self.fix_md.update_repeating_group("NoMDEntries", self.no_md_entries)
         self.fix_manager_fh.send_message(self.fix_md, "Send MD HSBC EUR/USD")
         self.sleep(10)
         # endregion
-        # region step 1
+        # region step 1-2
         self.rates_tile.crete_tile()
         self.rates_tile.modify_client_tile(instrument=self.instrument, client_tier=self.client)
         self.rates_tile.press_use_default()
@@ -71,3 +138,6 @@ class QAP_T2953(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def run_post_conditions(self):
         self.rates_tile.close_tile()
+        self.fix_md.set_market_data().\
+            update_MDReqID(self.fix_md.get_parameter("MDReqID"), self.fix_env.feed_handler, 'FX')
+        self.fix_manager_fh.send_message(self.fix_md, "Send MD HSBC EUR/USD")
