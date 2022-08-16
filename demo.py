0f49d0aa6a21841e5df5be3ef604b189b9f9c2a0
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2022-08-16 16:16:22 +0200

    Restore demo after merge

--- demo.py
+++ demo.py
@@ -1,75 +1,48 @@
 import logging
-import time
+from getpass import getuser as get_pc_name
 from datetime import datetime
-
+from pathlib import Path
 from custom import basic_custom_actions as bca
-from rule_management import RuleManager
-from send_rqf import Send_RFQ
 from stubs import Stubs
-from test_cases.fx.fx_mm_esp import QAP_T2458
-from test_cases.fx.fx_mm_esp.QAP_2075 import QAP_2075
-from test_cases.fx.fx_mm_esp.QAP_T2497 import QAP_T2497
-
-
-from test_cases.fx.fx_taker_esp import QAP_5600
-from test_cases.fx.fx_taker_esp.QAP_6593 import QAP_6593
-from test_cases.fx.fx_taker_esp.QAP_8090 import QAP_8090
-from test_cases.fx.fx_taker_rfq.QAP_568 import QAP_568
-
-from test_cases.fx.qs_fx_routine import DepositAndLoan
-from test_cases.fx.qs_fx_routine.dep_and_loan import DepAndLoan
-from test_cases.fx.send_md import QAP_MD
+from test_cases.eq.Care.QAP_T7685 import QAP_T7685
 from test_framework.configurations.component_configuration import ComponentConfiguration
-from win_gui_modules.utils import set_session_id, prepare_fe_2, get_opened_fe
+from test_framework.data_sets.oms_data_set.oms_data_set import OmsDataSet
+from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
+from test_framework.data_sets.algo_data_set.algo_data_set import AlgoDataSet
+from test_framework.data_sets.ret_data_set.ret_data_set import RetDataSet
+from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
+from win_gui_modules.utils import set_session_id
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.WARN)
 
 
 def test_run():
     # Generation id and time for test run
-    report_id = bca.create_event(f'[alexs] ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
-    # initializing dataset
+    pc_name = get_pc_name()  # getting PC name
+    report_id = bca.create_event(f'[{pc_name}] ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    logger.info(f"Root event was created (id = {report_id.id})")
     # initializing FE session
-    session_id = set_session_id(target_server_win="ostronov")
-
-    window_name = "Quod Financial - Quod site 309"
+    session_id = set_session_id(pc_name)
+    base_main_window = BaseMainWindow(bca.create_event(Path(__file__).name[:-3], report_id), session_id)
     # region creation FE environment and initialize fe_ values
-    configuration = ComponentConfiguration("ESP_MM")  # <--- provide your component from XML (DMA, iceberg, etc)
-    start_time = time.time()
-    print(f"Test start")
+    configuration = ComponentConfiguration("YOUR_COMPONENT")  # <--- provide your component from XML (DMA, iceberg, etc)
+    fe_env = configuration.environment.get_list_fe_environment()[0]
+    fe_folder = fe_env.folder
+    fe_user = fe_env.user_1
+    fe_pass = fe_env.password_1
     # endregion
-    Stubs.frontend_is_open = True
 
     try:
-        # if not Stubs.frontend_is_open:
-        #     prepare_fe_2(report_id, session_id)
-        # else:
-        #     get_opened_fe(report_id, session_id, window_name)
-
-        # rm = RuleManager()
-        # rm.add_TRADE_ESP_test("fix-bs-esp-314-luna-standard")
-        # rm.print_active_rules()
-        # QAP_568(report_id, session_id, configuration.data_set).execute()
-        # Test_UI(report_id, session_id, configuration.data_set, configuration.environment).execute()
-        # DepositAndLoan.execute(report_id)
-
-        QAP_MD(report_id, data_set=configuration.data_set).execute()
-        # QAP_T2978(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
-        # Send_RFQ(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
-
-        # QAP_T2458.execute(report_id)
-        # QAP_T2497().execute(report_id)
-
-        end = time.time()
-        print(f"Test duration is {end - start_time} seconds")
-
+        base_main_window.open_fe(report_id=report_id, fe_env=fe_env, user_num=1)
+        QAP_T7685(report_id=report_id, session_id=session_id, data_set=configuration.data_set,
+                 environment=configuration.environment) \
+            .execute()
     except Exception:
         logging.error("Error execution", exc_info=True)
     finally:
         Stubs.win_act.unregister(session_id)
-        pass
 
 
 if __name__ == '__main__':
