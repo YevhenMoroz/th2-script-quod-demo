f7060ade73b61928ca5712a5254b74e74d4f6ba6
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-08-17 16:29:26 +0200

    Add new instrument

--- test_framework/data_sets/algo_data_set/algo_const_enum.py
+++ test_framework/data_sets/algo_data_set/algo_const_enum.py
@@ -130,6 +130,14 @@ class AlgoFixInstruments(Enum):
         SecurityType='CS'
     )
 
+    instrument_17 = dict(
+        Symbol='QUODTESTQA05',
+        SecurityID='TESTQA05',
+        SecurityIDSource='8',
+        SecurityExchange='QDL11',
+        SecurityType='CS'
+    )
+
 
 class AlgoVenues(Enum):
     venue_1 = ""
@@ -205,6 +213,8 @@ class AlgoMic(Enum):
     mic_25 = "QDL9"  # QUODLIT9
     mic_26 = "QDL10"  # QUODLIT10
     mic_27 = "BATE"  # BATS UK
+    mic_28 = "QDL11"  # QUODLIT11
+    mic_29 = "QDL12"  # QUODLIT12
 
 
 class AlgoListingId(Enum):
@@ -234,6 +244,8 @@ class AlgoListingId(Enum):
     listing_24 = "1803699"    # Euronext Paris for FR0000133308
     listing_25 = "1872430"    # BATS UK for FR0000133308
     listing_26 = "768319009"    # CHIX UK for FR0000133308
+    listing_27 = "1225020507"    # QUODLIT11 for QUODTESTQA05
+    listing_28 = "1225020508"    # QUODLIT12 for QUODTESTQA05
 
 
 class AlgoCurrency(Enum):
@@ -278,4 +290,9 @@ class AlgoPreFilter(Enum):
                     'MsgType': ('8', "EQUAL")
                 },
                 'ExecType': ('4', "EQUAL")
+                }
+    pre_filer_equal_order_cancel_reject = {
+                'header': {
+                    'MsgType': ('9', "EQUAL")
+                }
                 }
