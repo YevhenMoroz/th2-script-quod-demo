8c10898ca7d779ca18ace6bb5b245ca27bfc0fdf
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-08-17 11:14:14 +0200

    Add MIC and Account for BATS UK (Kepler), add MDReqID for CHIX UK (FR0000133308)

--- test_framework/data_sets/algo_data_set/algo_const_enum.py
+++ test_framework/data_sets/algo_data_set/algo_const_enum.py
@@ -158,6 +158,7 @@ class AlgoAccounts(Enum):
     account_9 = "KEPLER"
     account_10 = "TQDARK_KEPLER"
     account_11 = "TRQX_KEPLER"
+    account_12 = "BATS_KEPLER"
 
 
 class AlgoWashbookAccounts(Enum):
@@ -203,6 +204,7 @@ class AlgoMic(Enum):
     mic_24 = "QDL8"  # QUODLIT8
     mic_25 = "QDL9"  # QUODLIT9
     mic_26 = "QDL10"  # QUODLIT10
+    mic_27 = "BATE"  # BATS UK
 
 
 class AlgoListingId(Enum):
@@ -230,6 +232,8 @@ class AlgoListingId(Enum):
     listing_22 = "925020509"  # QUODLIT10 for QUODTESTQA04
     listing_23 = "897587663"  # TRQX for FR0000133308
     listing_24 = "1803699"    # Euronext Paris for FR0000133308
+    listing_25 = "1872430"    # BATS UK for FR0000133308
+    listing_26 = "768319009"    # CHIX UK for FR0000133308
 
 
 class AlgoCurrency(Enum):
