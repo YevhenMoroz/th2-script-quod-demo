4d89d6dab58eb68bdca396a76b1707bbc3eaaa21
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-08-19 09:56:31 +0200

    changed alloc_details condition in manual_execution method

--- test_framework/win_gui_wrappers/oms/oms_order_ticket.py
+++ test_framework/win_gui_wrappers/oms/oms_order_ticket.py
@@ -66,7 +66,7 @@ class OMSOrderTicket(BaseOrderTicket):
             self.order_details.set_settl_date(settl_date)
         if recipient is not None:
             self.order_details.set_care_order(recipient, partial_desk, disclose_flag)
-        if alloc_details and set_alt_account:
+        if alloc_details is not None and set_alt_account is True:
             allocation_row_details = list()
             for account_name, percent in alloc_details.items():
                 allocation_row_details.append(AllocationsGridRowDetails(alt_account=account_name, percentage=percent).build())
@@ -75,12 +75,13 @@ class OMSOrderTicket(BaseOrderTicket):
             allocation_details = more_tab_allocation_details.build()
             self.order_details.set_allocations_details(allocation_details)
         else:
-            allocation_row_details = list()
-            for account_name, qty in alloc_details.items():
-                allocation_row_details.append(AllocationsGridRowDetails(account=account_name, qty=qty).build())
-            more_tab_allocation_details = MoreTabAllocationsDetails(allocation_row_details)
-            allocation_details = more_tab_allocation_details.build()
-            self.order_details.set_allocations_details(allocation_details)
+            if alloc_details is not None:
+                allocation_row_details = list()
+                for account_name, qty in alloc_details.items():
+                    allocation_row_details.append(AllocationsGridRowDetails(account=account_name, qty=qty).build())
+                more_tab_allocation_details = MoreTabAllocationsDetails(allocation_row_details)
+                allocation_details = more_tab_allocation_details.build()
+                self.order_details.set_allocations_details(allocation_details)
         return self
 
     # endregion
