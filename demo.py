f6bcc94dd170e28a1d9879ec57913b97c172568b
irovchak <illia.rovchak.contractor@quodfinancial.com>
2022-08-22 14:42:26 +0300

    Actual demo and regression_run_config.xml

--- demo.py
+++ demo.py
@@ -1,17 +1,14 @@
 import logging
 from getpass import getuser as get_pc_name
 from datetime import datetime
-from pathlib import Path
 from custom import basic_custom_actions as bca
 from stubs import Stubs
-from test_cases.eq.Care.QAP_T7685 import QAP_T7685
+from test_cases.ret.REST_API.Trading_REST.BuyingPower_API.QAP_T3181 import QAP_T3181
+from test_cases.ret.REST_API.Trading_REST.BuyingPower_API.QAP_T8249 import QAP_T8249
+from test_cases.ret.REST_API.Trading_REST.BuyingPower_API.QAP_T8250 import QAP_T8250
+from test_cases.ret.REST_API.Trading_REST.BuyingPower_API.QAP_T8251 import QAP_T8251
+from test_cases.ret.REST_API.Trading_REST.DMA_API.QAP_T3520 import QAP_T3520
 from test_framework.configurations.component_configuration import ComponentConfiguration
-from test_framework.data_sets.oms_data_set.oms_data_set import OmsDataSet
-from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
-from test_framework.data_sets.algo_data_set.algo_data_set import AlgoDataSet
-from test_framework.data_sets.ret_data_set.ret_data_set import RetDataSet
-from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from win_gui_modules.utils import set_session_id
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -23,26 +20,36 @@ def test_run():
     pc_name = get_pc_name()  # getting PC name
     report_id = bca.create_event(f'[{pc_name}] ' + datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     logger.info(f"Root event was created (id = {report_id.id})")
-    # initializing FE session
-    session_id = set_session_id(pc_name)
-    base_main_window = BaseMainWindow(bca.create_event(Path(__file__).name[:-3], report_id), session_id)
-    # region creation FE environment and initialize fe_ values
-    configuration = ComponentConfiguration("YOUR_COMPONENT")  # <--- provide your component from XML (DMA, iceberg, etc)
-    fe_env = configuration.environment.get_list_fe_environment()[0]
-    fe_folder = fe_env.folder
-    fe_user = fe_env.user_1
-    fe_pass = fe_env.password_1
     # endregion
 
+    # ___WebAdmin REST API configuration block
+    # configuration_site_admin_api = ComponentConfiguration("WA_REST_API_Site")
+    # configuration_client_accounts_api = ComponentConfiguration("WA_REST_API_Client_Accounts")
+    # configuration_risk_limits_admin_api = ComponentConfiguration("WA_REST_API_Risk_Limits")
+    # configuration_positions_admin_api = ComponentConfiguration("WA_REST_API_Positions")
+    # ___Trading REST API configuration block
+    # configuration_trading_api_dma = ComponentConfiguration("Trading_REST_API_Dma")
+    # configuration_trading_api_md = ComponentConfiguration("Trading_REST_API_MarketData")
+    # configuration_trading_api_risk_limit = ComponentConfiguration("Trading_REST_API_RiskLimits")
+    configuration_trading_api_buying_power = ComponentConfiguration("Trading_REST_API_BuyingPower")
+    # configuration_trading_api_positions = ComponentConfiguration("Trading_REST_API_Positions")
+    # configuration_trading_api_others = ComponentConfiguration("Trading_REST_API_Others")
+
     try:
-        base_main_window.open_fe(report_id=report_id, fe_env=fe_env, user_num=1)
-        QAP_T7685(report_id=report_id, session_id=session_id, data_set=configuration.data_set,
-                 environment=configuration.environment) \
-            .execute()
+        start = datetime.now()
+        print(f'start time = {start}')
+        QAP_T3520(report_id, configuration_trading_api_buying_power.data_set,
+                            configuration_trading_api_buying_power.environment).execute()
+        # __BuyingPower_SmokeTests__ block
+        # QAP_T8249(report_id, configuration_trading_api_buying_power.data_set,
+        #           configuration_trading_api_buying_power.environment).execute()
+        # QAP_T8250(report_id, configuration_trading_api_buying_power.data_set,
+        #           configuration_trading_api_buying_power.environment).execute()
+        # QAP_T8251(report_id, configuration_trading_api_buying_power.data_set,
+        #           configuration_trading_api_buying_power.environment).execute()
+        print('duration time = ' + str(datetime.now() - start))
     except Exception:
         logging.error("Error execution", exc_info=True)
-    finally:
-        Stubs.win_act.unregister(session_id)
 
 
 if __name__ == '__main__':
