b9e9cc0ee8380811cf8614ff4cf98cf1611745e8
Vadim <vadimskulinec@gmail.com>
2022-08-25 02:13:26 +0300

    QAP-5096 EQ - Manual Execute: Add Other tab

--- win_gui_modules/order_book_wrappers.py
+++ win_gui_modules/order_book_wrappers.py
@@ -593,6 +593,41 @@ class ExecutionsDetails:
         self.request.settlementDateOffset = offset
 
 
+class OtherTabDetails:
+    def __init__(self, request: order_book_pb2.ManualExecutionDetails.OtherTabDetails):
+        self._request = request
+
+    def set_trade_type(self, trade_type: str):
+        self._request.tradeType = trade_type
+
+    def set_net_gross_ind(self, net_gross_ind: str):
+        self._request.netGrossInd = net_gross_ind
+
+    def set_sec_last_mkt(self, sec_last_mkt: str):
+        self._request.secLastMkt = sec_last_mkt
+
+    def set_settlement_type(self, set_settlement_type: str):
+        self._request.settlementType = set_settlement_type
+
+    def set_settl_currency(self, settl_currency: str):
+        self._request.settlCurrency = settl_currency
+
+    def set_exchange_rate(self, exchange_rate: str):
+        self._request.exchangeRate = exchange_rate
+
+    def set_exchange_rate_cacl(self, exchange_rate_cacl):
+        self._request.exchangeRateCacl = exchange_rate_cacl
+
+    def set_agent_fees(self, agent_fees: str):
+        self._request.agentFees = agent_fees
+
+    def set_market_fees(self, market_fees: str):
+        self._request.marketFees = market_fees
+
+    def set_route_fees(self, route_fees: str):
+        self._request.routeFees = route_fees
+
+
 class MenuItemDetails:
     def __init__(self, base_request: EmptyRequest = None):
         if base_request is not None:
@@ -635,6 +670,10 @@ class ManualExecutingDetails:
     def add_executions_details(self) -> ExecutionsDetails:
         var = self._request.executionDetails.add()
         return ExecutionsDetails(var)
+
+    def add_other_details(self) -> OtherTabDetails:
+        var = self._request.othertabDetails.add()
+        return OtherTabDetails(var)
 
     def set_error_expected(self, error_expected: bool):
         self._request.errorExpected = error_expected
@@ -854,7 +893,6 @@ class FXOrderInfo:
 
     def build(self):
         return self.order_info
-
 
 
 class AddToBasketDetails:
@@ -1139,7 +1177,7 @@ class UnmatchAndTransferDetails:
         self.transfer_details = order_book_pb2.UnmatchAndTransferDetails()
         self.transfer_details.base.CopyFrom(base_request)
 
-    def set_filter_and_sub_filter(self, filter_dict: dict, sub_filter_dict: dict=None):
+    def set_filter_and_sub_filter(self, filter_dict: dict, sub_filter_dict: dict = None):
         self.transfer_details.filter.update(filter_dict)
         if sub_filter_dict:
             self.transfer_details.subFilter.update(sub_filter_dict)
@@ -1189,6 +1227,7 @@ class GetSubLvlDetails:
 
     def build(self):
         return self._request
+
 
 class QuickButtonCreationDetails:
     def __init__(self, base_request: EmptyRequest = None):
