124598b739cb53336e526300eb430ea6951d9b58
Vadim <vadimskulinec@gmail.com>
2022-08-28 20:19:32 +0300

    QAP_T6990 modify

--- test_cases/eq/Commissions/QAP_T6990.py
+++ test_cases/eq/Commissions/QAP_T6990.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import time
 from pathlib import Path
 from custom import basic_custom_actions as bca
 from custom.basic_custom_actions import timestamps
@@ -13,9 +15,11 @@ from test_framework.fix_wrappers.oms.FixMessageConfirmationReportOMS import FixM
 from test_framework.fix_wrappers.oms.FixMessageExecutionReportOMS import FixMessageExecutionReportOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
 from test_framework.rest_api_wrappers.oms.rest_commissions_sender import RestCommissionsSender
+from test_framework.ssh_wrappers.ssh_client import SshClient
 from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, PostTradeStatuses, \
     ExecSts, TradeBookColumns, SecondLevelTabs, Basis, FeeTypeForMiscFeeTab, MiddleOfficeColumns, \
     AllocationsColumns
+import xml.etree.ElementTree as ET
 from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
 from test_framework.win_gui_wrappers.oms.oms_middle_office import OMSMiddleOffice
 from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
@@ -55,6 +59,13 @@ class QAP_T6990(TestCase):
         self.fix_message = FixMessageNewOrderSingleOMS(self.data_set)
         self.fix_manager = FixManager(self.ss_connectivity, self.test_id)
         self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.ssh_client_env = self.environment.get_list_ssh_client_environment()[0]
+        self.ssh_client = SshClient(self.ssh_client_env.host, self.ssh_client_env.port, self.ssh_client_env.user,
+                                    self.ssh_client_env.password, self.ssh_client_env.su_user,
+                                    self.ssh_client_env.su_password)
+        self.local_path = os.path.abspath("test_framework\ssh_wrappers\oms_cfg_files\client_backend.xml")
+        self.remote_path = f"/home/{self.ssh_client_env.su_user}/quod/cfg/client_backend.xml"
+        # endregion
         # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
@@ -72,6 +83,18 @@ class QAP_T6990(TestCase):
         self.rest_commission_sender.change_message_params(
             {'commExecScope': on_calculated_exec_scope, 'instrType': instr_type, "venueID": venue_id})
         self.rest_commission_sender.send_post_request()
+        # endregion
+
+        # region set configuration on backend (precondition)
+        tree = ET.parse(self.local_path)
+        element = ET.fromstring("<automaticCalculatedReportEnabled>true</automaticCalculatedReportEnabled>")
+        quod = tree.getroot()
+        quod.append(element)
+        tree.write("temp.xml")
+        self.ssh_client.send_command("~/quod/script/site_scripts/change_permission_script")
+        self.ssh_client.put_file(self.remote_path, "temp.xml")
+        self.ssh_client.send_command("qrestart all")
+        time.sleep(120)
         # endregion
 
         # region create CO  and partially fill it step 1
@@ -215,3 +238,10 @@ class QAP_T6990(TestCase):
             print(actual_result)
             self.order_book.compare_values(expected_result[list_of_column.index(value)], actual_result,
                                            message)
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.rest_commission_sender.clear_fees()
+        self.ssh_client.put_file(self.remote_path, self.local_path)
+        self.ssh_client.send_command("qrestart all")
+        os.remove("temp.xml")
