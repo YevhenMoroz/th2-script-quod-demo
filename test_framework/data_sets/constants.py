1b6806c5563b632dda4c9d08d91387fa4bd4f7a9
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-09-07 16:55:50 +0200

    Add verifiers

--- test_framework/data_sets/constants.py
+++ test_framework/data_sets/constants.py
@@ -201,6 +201,8 @@ class ReadLogVerifiers(Enum):
     log_319_check_tags_5052_and_207_mapping = "log319-check-tags-5052-and-207-mapping"
     log_319_check_that_venue_was_suspended = "log319-check-that-venue-was-suspended"
     log_319_check_that_lis_phase_is_skipping = "log319-check-that-lis-phase-is-skipping"
+    log_319_check_the_currency_rate = "log319-check-the-currency-rate"
+    log_319_check_the_lis_amount = "log319-check-the-lis=amount"
 
 
 class WebAdminURL(Enum):
