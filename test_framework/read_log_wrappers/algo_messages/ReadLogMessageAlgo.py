473ce71f0e7ab6a8933ada3c910777b32ed7d000
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-09-08 14:37:49 +0200

    Add compare messages

--- test_framework/read_log_wrappers/algo_messages/ReadLogMessageAlgo.py
+++ test_framework/read_log_wrappers/algo_messages/ReadLogMessageAlgo.py
@@ -9,3 +9,20 @@ class ReadLogMessageAlgo(ReadLogMessage):
     def __init__(self, parameters: dict = None, data_set: BaseDataSet = None):
         super().__init__(message_type=ReadLogMessageType.Csv_Message.value, data_set=data_set)
         super().change_parameters(parameters)
+
+    def set_compare_message_for_check_the_currency_rate(self) -> ReadLogMessage:
+        base_parameters = {
+            "Currency": 'SEK',
+            "Rate": '9.960000000'
+        }
+        super().change_parameters(base_parameters)
+        return self
+
+    def set_compare_message_for_check_the_lis_amount(self) -> ReadLogMessage:
+        base_parameters = {
+            "Amount1": '1500',
+            "Amount2": '1992000.000000000',
+            "Venue": 'CHIXLIS'
+        }
+        super().change_parameters(base_parameters)
+        return self
