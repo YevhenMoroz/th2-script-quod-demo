a6074374c49a0ba3a047ead1892f2d1e8d6e9270
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-09-19 11:19:17 +0200

    QAP-T8704 rename

--- test_cases/algo/Algo_Kepler/Algo_SORPING/QAP_T8704.py
+++ test_cases/algo/Algo_Kepler/Algo_SORPING/QAP_T8704.py
@@ -17,7 +17,7 @@ from test_framework.core.test_case import TestCase
 from test_framework.data_sets import constants
 
 
-class QAP_T4965(TestCase):
+class QAP_T8704(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, data_set=None, environment=None):
         super().__init__(report_id=report_id, data_set=data_set, environment=environment)
