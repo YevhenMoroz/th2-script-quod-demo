21460c5d1ba107e567072e34bdee75713c6482c1
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2022-09-23 18:56:04 +0200

    QAP-T2497: refactored

--- test_cases/fx/fx_mm_esp/QAP_T2497.py
+++ test_cases/fx/fx_mm_esp/QAP_T2497.py
@@ -1,147 +1,58 @@
-import logging
-import time
 from pathlib import Path
-from stubs import Stubs
-from th2_grpc_act_java_api_quod.act_java_api_quod_pb2 import ActJavaSubmitMessageRequest
-from custom import basic_custom_actions as bca, tenor_settlement_date as tsd
 
-from datetime import datetime
+from test_cases.fx.fx_wrapper.common_tools import start_mda_q, start_fxfh_q, stop_mda_q, stop_fxfh_q
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.data_sets.base_data_set import BaseDataSet
+from custom import basic_custom_actions as bca
+from test_framework.data_sets.constants import DirectionEnum
+from test_framework.environments.full_environment import FullEnvironment
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.fix_wrappers.forex.FixMessageMarketDataRequestFX import FixMessageMarketDataRequestFX
+from test_framework.fix_wrappers.forex.FixMessageMarketDataSnapshotFullRefreshSellFX import \
+    FixMessageMarketDataSnapshotFullRefreshSellFX
 
-from test_cases.fx.fx_wrapper.CaseParamsBuy import CaseParamsBuy
-from test_cases.fx.fx_wrapper.FixClientBuy import FixClientBuy
-from test_cases.fx.fx_wrapper.common_tools import random_qty
+class QAP_T2497(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set: BaseDataSet = None, environment: FullEnvironment = None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.ss_connectivity = self.fix_env.sell_side_esp
+        self.fix_manager_gtw = FixManager(self.ss_connectivity, self.test_id)
+        self.fix_verifier = FixVerifier(self.ss_connectivity, self.test_id)
+        self.md_request = FixMessageMarketDataRequestFX(data_set=self.data_set)
+        self.md_snapshot = FixMessageMarketDataSnapshotFullRefreshSellFX()
+        self.silver = self.data_set.get_client_by_name("client_mm_1")
+        self.eur_gbp = self.data_set.get_symbol_by_name('symbol_3')
+        self.security_type_spot = self.data_set.get_security_type_by_name("fx_spot")
+        self.settle_type_spot = self.data_set.get_settle_type_by_name("spot")
+        self.instrument_spot = {
+            'Symbol': self.eur_gbp,
+            'SecurityType': self.security_type_spot,
+            'Product': '4', }
+        self.no_related_symbols_spot = [{
+            'Instrument': self.instrument_spot,
+            'SettlType': self.settle_type_spot}]
 
-alias_fh = "fix-fh-314-luna"
-defaultmdsymbol_spo_hsbc = 'GBP/USD:SPO:REG:HSBC'
-no_md_entries_spo_hsbc = [
-    {
-        "MDEntryType": "0",
-        "QuoteEntryID": "1_GBP/USD_20211122292ACF004FF385E1_Bid2",
-        "MDEntryPx": 1.18075,
-        "MDEntrySize": 1000000,
-        "MDEntryPositionNo": 1,
-        "MDQuoteType": 1,
-        'SettlDate': tsd.spo(),
-        "MDEntryTime": datetime.utcnow().strftime('%Y%m%d'),
-    },
-    {
-        "MDEntryType": "1",
-        "QuoteEntryID": "1_GBP/USD_20211122292ACF004FF385E1_Ask2",
-        "MDEntryPx": 1.18141,
-        "MDEntrySize": 1000000,
-        "MDEntryPositionNo": 1,
-        "MDQuoteType": 1,
-        'SettlDate': tsd.spo(),
-        "MDEntryTime": datetime.utcnow().strftime('%Y%m%d'),
-    },
-]
-symbol = 'GBP/USD'
-securitytype = 'FXSPOT'
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region Step 1
+        start_mda_q(0)
+        start_fxfh_q()
+        self.md_request.set_md_req_parameters_maker().change_parameters({"SenderSubID": self.silver})
+        self.md_request.update_repeating_group('NoRelatedSymbols', self.no_related_symbols_spot)
+        self.fix_manager_gtw.send_message_and_receive_response(self.md_request, self.test_id)
+        self.md_snapshot.set_params_for_md_response(self.md_request, ["*", "*"])
+        self.md_snapshot.add_tag({"OrigQuoteEntryID": "*"})
+        self.fix_verifier.check_fix_message(fix_message=self.md_snapshot, direction=DirectionEnum.FromQuod,
+                                            key_parameters=["MDReqID"])
+        # endregion
 
-
-class QAP_T2497:
-    def __init__(self):
-        self.act_java_api = Stubs.act_java_api
-        self.connectivity = '314_java_api'
-
-    def send_mdr_sub(self):
-        md_params = {
-            'SEND_SUBJECT': 'MDA.QUOD.PRICING.2.SUB',
-            'REPLY_SUBJECT': 'MDA.506404433.2000011.D.PRICING.2',
-            'MarketDataRequestBlock': {
-                'MDReqID': random_qty(1, 2, len=10),
-                'MDSymbolList': {
-                    'MDSymbolBlock': [
-                        {
-                            'ListingID': '506404433',
-                            'MDSymbol': '506404433.2000011',
-                            'ClientTierID': '2000011',
-                            'FeedType': 'D',
-                            'SubscriptionRequestType': 'SUB'
-                        }
-                    ]
-                },
-            }
-        }
-
-        self.act_java_api.sendMessage(request=ActJavaSubmitMessageRequest(
-            message=bca.message_to_grpc('Market_MarketDataRequest', md_params, self.connectivity)))
-
-    # Main method
-    def execute(self, report_id):
-        case_name = Path(__file__).name[:-3]
-        case_id = bca.create_event(case_name, report_id)
-        try:
-            checkpoint1 = Stubs.verifier.createCheckpoint(bca.create_checkpoint_request(case_id))
-            # Send market data to the HSBC venue GBP/USD spot
-            FixClientBuy(CaseParamsBuy(case_id, defaultmdsymbol_spo_hsbc, symbol, securitytype,
-                                       connectivity=alias_fh).prepare_custom_md_spot(
-                no_md_entries_spo_hsbc)).send_market_data_spot(even_name_custom='Send Market Data SPOT HSBC')
-            checkpoint_id1 = checkpoint1.checkpoint
-            time.sleep(5)
-            self.send_mdr_sub()
-
-            def_order_exec_report = {
-                'ActiveClientTier': '*',
-                'AutomatedMargin': '*',
-                'MarginPriceType': '*',
-                'MDQuoteType': '*',
-                'OrigQuoteEntryID': '*',
-                'MDQuoteTypeStatus': '*',
-                'MDReportID': '*',
-                'MDTime': '*',
-                'PositionBasedMargins': '*',
-                'QuoteConditionStatus': '*',
-                'OrigVenueID': '*',
-                'OrigMDTime': '*',
-                'MarketDataFullList': {
-                    'MarketDataFullBlock':
-                        [
-                            {
-                                'VenueOrdID': '*',
-                                'MDEntryPx': '*',
-                                'OrdType': '*',
-                                'MDQuoteType': '*',
-                                'MDEntryID': '*',
-                                'MDEntrySize': '*',
-                                'QuoteEntryID': '*',
-                                'MDEntryBaseSize': '*',
-                                'MDEntryPosition': '*',
-                                'MDEntryMargin': '*',
-                                'MDEntryType': '*',
-                                'MDEntryBaseMargin': '*',
-                            },
-                            {
-                                'VenueOrdID': '*',
-                                'MDEntryPx': '*',
-                                'OrdType': '*',
-                                'MDQuoteType': '*',
-                                'MDEntryID': '*',
-                                'MDEntrySize': '*',
-                                'QuoteEntryID': '*',
-                                'MDEntryBaseSize': '*',
-                                'MDEntryPosition': '*',
-                                'MDEntryMargin': '*',
-                                'MDEntryType': '*',
-                                'MDEntryBaseMargin': '*',
-                            }
-                        ]
-                }
-            }
-
-            time.sleep(2)
-
-            Stubs.verifier.submitCheckRule(
-                request=bca.create_check_rule(
-                    'Market_MarketDataSnapshotFullRefresh',
-                    bca.wrap_filter(def_order_exec_report, 'Market_MarketDataSnapshotFullRefresh'),
-                    checkpoint_id1, self.connectivity, case_id
-                ),
-
-            )
-            print(def_order_exec_report)
-        except Exception:
-            logging.error("Error execution", exc_info=True)
-            bca.create_event('Fail test event', status='FAILED', parent_id=case_id)
-
-
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.md_request.set_md_uns_parameters_maker()
+        self.fix_manager_gtw.send_message(self.md_request)
+        stop_mda_q()
+        stop_fxfh_q()
