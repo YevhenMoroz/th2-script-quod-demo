3225a35d50b02f28898f4757de6808470701c010
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-09-26 08:12:04 +0200

    QAP-T4079 Manual test

--- test_cases/algo/Algo_Kepler/Algo_SORPING/QAP_T4079.py
+++ test_cases/algo/Algo_Kepler/Algo_SORPING/QAP_T4079.py
@@ -17,6 +17,7 @@ from test_framework.data_sets import constants
 from test_framework.fix_wrappers.algo.FixMessageOrderCancelReplaceRequestAlgo import FixMessageOrderCancelReplaceRequestAlgo
 
 
+# Warning! This is the manual test case. It needs to do manual and doesn`t include in regression script
 class QAP_T4079(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, data_set=None, environment=None):
