6aa67facd8e91e5fc5b427af36daaef4db63c46f
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-10-07 16:50:22 +0200

    Refactor method for set params

--- test_cases/algo/Algo_Kepler/Algo_Iceberg/QAP_T5015.py
+++ test_cases/algo/Algo_Kepler/Algo_Iceberg/QAP_T5015.py
@@ -151,7 +151,7 @@ class QAP_T5015(TestCase):
         case_id_1 = bca.create_event("Create Iceberg Order", self.test_id)
         self.fix_verifier_sell.set_case_id(case_id_1)
 
-        self.Iceberg_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_Iceberg_Kepler_Custom_Tags()
+        self.Iceberg_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_Iceberg_Kepler()
         self.Iceberg_order.add_ClordId((os.path.basename(__file__)[:-3]))
         self.ClOrdId = self.Iceberg_order.get_parameter('ClOrdID')
         self.Iceberg_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument, DisplayInstruction=dict(DisplayQty=self.display_qty))).add_fields_into_repeating_group('NoParty', self.no_party)
@@ -184,7 +184,7 @@ class QAP_T5015(TestCase):
         # region Check 1st child DMA order
         self.fix_verifier_buy.set_case_id(bca.create_event("Child DMA order", self.test_id))
 
-        self.dma_xpar_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_DMA_child_of_Iceberg_Kepler_Custom_Tags()
+        self.dma_xpar_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_DMA_child_of_Iceberg_Kepler()
         self.dma_xpar_order.change_parameters(dict(Account=self.account, ExDestination=self.ex_destination_xpar, OrderQty=self.display_qty, Price=self.price, Instrument=self.instrument)).add_fields_into_repeating_group('NoParty', self.no_party)
         self.fix_verifier_buy.check_fix_message(self.dma_xpar_order, key_parameters=self.key_params_NOS_child, message_name='Buy side NewOrderSingle Child DMA 1 order')
 
