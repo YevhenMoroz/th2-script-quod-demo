81196ee1530b38903676fad68b3bd22921439a20
irovchak <illia.rovchak.contractor@quodfinancial.com>
2022-10-11 16:19:17 +0300

    Actual demo and regression_run_config

--- demo.py
+++ demo.py
@@ -8,6 +8,8 @@ from test_cases.ret.REST_API.Trading_REST.DMA_API.QAP_T3343 import QAP_T3343
 from test_cases.ret.REST_API.Trading_REST.DMA_API.QAP_T3520 import QAP_T3520
 from test_cases.ret.REST_API.Trading_REST.Risk_Limits_API.QAP_T3505 import QAP_T3505
 from test_cases.ret.REST_API.Trading_REST.Risk_Limits_API.QAP_T3506 import QAP_T3506
+from test_cases.ret.REST_API.Web_Admin_REST.Client_Accounts_API.QAP_T3642 import QAP_T3642
+from test_cases.ret.REST_API.Web_Admin_REST.Client_Accounts_API.QAP_T3646 import QAP_T3646
 from test_cases.ret.REST_API.Web_Admin_REST.Positions_API.QAP_T3139 import QAP_T3139
 from test_cases.ret.REST_API.Web_Admin_REST.Positions_API.QAP_T3140 import QAP_T3140
 from test_cases.ret.REST_API.Web_Admin_REST.Risk_Limits_API.QAP_T3128 import QAP_T3128
@@ -36,10 +38,11 @@ def test_run():
     # region ___WebAdmin REST API configuration block
     configuration_admin_api_site = ComponentConfiguration("WA_REST_API_Site")
     configuration_admin_api_users = ComponentConfiguration("WA_REST_API_Users")
-    # configuration_client_accounts_api = ComponentConfiguration("WA_REST_API_Client_Accounts")
+    configuration_client_accounts_api = ComponentConfiguration("WA_REST_API_Client_Accounts")
     configuration_admin_api_risk_limits = ComponentConfiguration("WA_REST_API_Risk_Limits")
     configuration_admin_api_positions = ComponentConfiguration("WA_REST_API_Positions")
     # endregion
+
     # region ___Trading REST API configuration block
     configuration_trading_api_dma = ComponentConfiguration("Trading_REST_API_Dma")
     # configuration_trading_api_md = ComponentConfiguration("Trading_REST_API_MarketData")
@@ -53,14 +56,14 @@ def test_run():
         start = datetime.now()
         print(f'start time = {start}')
 
-        # region __BuyingPower__ block
+        # region Trading API __BuyingPower__ block
         # QAP_T3315(report_id, configuration_trading_api_buying_power.data_set,
         #           configuration_trading_api_buying_power.environment).execute()
         # QAP_T3316(report_id, configuration_trading_api_buying_power.data_set,
         #           configuration_trading_api_buying_power.environment).execute()
         # endregion
 
-        # region __BuyingPower_SmokeTests__ block
+        # region Trading API __BuyingPower_SmokeTests__ block
         # QAP_T8207(report_id, configuration_trading_api_buying_power.data_set,
         #           configuration_trading_api_buying_power.environment).execute()
         # QAP_T8208(report_id, configuration_trading_api_buying_power.data_set,
@@ -85,7 +88,7 @@ def test_run():
         #           configuration_trading_api_buying_power.environment).execute()
         # endregion
 
-        # region __DMA__ block
+        # region Trading API __DMA__ block
         # QAP_T3343(report_id, configuration_trading_api_dma.data_set,
         #                     configuration_trading_api_dma.environment).execute()
         # QAP_T3520(report_id, configuration_trading_api_dma.data_set,
@@ -103,7 +106,7 @@ def test_run():
         #           configuration_trading_api_risk_limit.environment).execute()
         # endregion
 
-        # region __Site__ block
+        # region Admin API __Site__ block
         # QAP_T3580(report_id, configuration_admin_api_sute.data_set,
         #           configuration_admin_api_sute.environment).execute()
         # QAP_T3583(report_id, configuration_admin_api_sute.data_set,
@@ -112,7 +115,7 @@ def test_run():
         #           configuration_admin_api_sute.environment).execute()
         # endregion
 
-        # region __Users__ block
+        # region Admin API __Users__ block
         # QAP_T3600(report_id, configuration_users_admin_api.data_set,
         #           configuration_users_admin_api.environment).execute()
         # QAP_T3601(report_id, configuration_users_admin_api.data_set,
@@ -130,8 +133,8 @@ def test_run():
         # endregion
 
         # region Admin API __RiskLimit__ block
-        RiskLimitDimensionDeleter(report_id, configuration_admin_api_risk_limits.data_set,
-                                  configuration_admin_api_risk_limits.environment).execute()
+        # RiskLimitDimensionDeleter(report_id, configuration_admin_api_risk_limits.data_set,
+        #                           configuration_admin_api_risk_limits.environment).execute()
         # QAP_T3128(report_id, configuration_admin_api_risk_limits.data_set,
         #           configuration_admin_api_risk_limits.environment).execute()
         # QAP_T3203(report_id, configuration_admin_api_risk_limits.data_set,
@@ -154,6 +157,13 @@ def test_run():
         #           configuration_admin_api_positions.environment).execute()
         # QAP_T3140(report_id, configuration_admin_api_positions.data_set,
         #           configuration_admin_api_positions.environment).execute()
+        # endregion
+
+        # region Admin API __Client/Accounts__ block
+        QAP_T3642(report_id, configuration_client_accounts_api.data_set,
+                  configuration_client_accounts_api.environment).execute()
+        # QAP_T3646(report_id, configuration_client_accounts_api.data_set,
+        #           configuration_client_accounts_api.environment).execute()
         # endregion
 
         print('duration time = ' + str(datetime.now() - start))
