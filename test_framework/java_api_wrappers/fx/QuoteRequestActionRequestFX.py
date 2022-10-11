25297126a7e5e02e4d163fde30b62ddc7e3463c2
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2022-10-11 17:46:52 +0200

    QAP-8671

---
+++ test_framework/java_api_wrappers/fx/QuoteRequestActionRequestFX.py
@@ -0,0 +1,35 @@
+from test_framework.data_sets.base_data_set import BaseDataSet
+from test_framework.data_sets.fx_data_set.fx_data_set import FxDataSet
+from test_framework.data_sets.message_types import ORSMessageType
+from test_framework.java_api_wrappers.JavaApiMessage import JavaApiMessage
+from custom import basic_custom_actions as bca
+
+
+class QuoteRequestActionRequestFX(JavaApiMessage):
+
+    def __init__(self, parameters: dict = None, data_set: BaseDataSet = FxDataSet()):
+        super().__init__(message_type=ORSMessageType.QuoteRequestActionRequest.value, data_set=data_set)
+        super().change_parameters(parameters)
+        self.action_id = bca.client_orderid(10)
+
+    def set_default_params(self, quote_request):
+        quote_request_id = quote_request[0].get_parameter("QuoteRequestNotifBlock")["QuoteRequestID"]
+        params_for_request = {
+            "SEND_SUBJECT": "QUOD.ORS.FE",
+            "REPLY_SUBJECT": "QUOD.FE.ORS",
+            "QuoteRequestActionRequestBlock": {
+                "QuoteRequestID": quote_request_id,
+                "QuoteReqAction": None,
+                "AssigneeUserID": "ostronov",
+                "AssigneeRoleID": "HSD",
+                "ActionID": f"{quote_request_id}{self.action_id}"
+            }
+        }
+        super().change_parameters(params_for_request)
+        return self
+
+    def set_action_assign(self):
+        self.update_fields_in_component("QuoteRequestActionRequestBlock", {"QuoteReqAction": "ASG"})
+
+    def set_action_estimate(self):
+        self.update_fields_in_component("QuoteRequestActionRequestBlock", {"QuoteReqAction": "EST"})
