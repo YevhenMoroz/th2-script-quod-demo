84f00d72247a29a0e170bb65539fed6799638103
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-10-13 16:33:36 +0300

    QAP-8632 update regression tests

--- regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_exa_auction_regression.py
+++ regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_exa_auction_regression.py
@@ -2,7 +2,6 @@ import logging
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 from test_cases.algo.Algo_Redburn.Algo_MOE import QAP_T4307, QAP_T4310
-from test_cases.algo.Algo_Redburn.Algo_POV.QAP_T8718 import QAP_T8718
 from test_framework.configurations.component_configuration import ComponentConfiguration
 
 
@@ -18,7 +17,6 @@ def test_run(parent_id=None, version=None):
     try:
         # region Iceberg: Route/Venue
         configuration = ComponentConfiguration("Expity_Auction")
-        # QAP_T8718(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
 
         QAP_T4307.execute(report_id)
         QAP_T4310.execute(report_id)
