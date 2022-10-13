c01e17ba6cf684bca7c18783f35b13cb7c682f39
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-10-13 16:54:02 +0300

    add QAP_T4316

--- test_framework/java_api_wrappers/JavaApiManager.py
+++ test_framework/java_api_wrappers/JavaApiManager.py
@@ -27,6 +27,8 @@ from test_framework.java_api_wrappers.ors_messages.OrderBagWaveCancelReply impor
 from test_framework.java_api_wrappers.ors_messages.OrderBagWaveModificationReply import OrderBagWaveModificationReply
 from test_framework.java_api_wrappers.ors_messages.OrderBagWaveNotification import OrderBagWaveNotification
 from test_framework.java_api_wrappers.ors_messages.OrderListWaveNotification import OrderListWaveNotification
+from test_framework.java_api_wrappers.ors_messages.OrderModificationReply import OrderModificationReply
+from test_framework.java_api_wrappers.ors_messages.PositionReport import PositionReport
 from test_framework.java_api_wrappers.ors_messages.TradeEntryNotif import Order_TradeEntryNotif
 
 
@@ -249,7 +251,7 @@ class JavaApiManager:
             elif message_type == ORSMessageType.OrderListWaveNotification.value:
                 response_fix_message = OrderListWaveNotification()
             elif message_type == ORSMessageType.PositionReport.value:
-                response_fix_message = OrdListNotification()
+                response_fix_message = PositionReport()
             elif message_type == ESMessageType.NewOrderReply.value:
                 response_fix_message = NewOrderReply()
             elif message_type == ESMessageType.ExecutionReport.value:
@@ -259,7 +261,7 @@ class JavaApiManager:
             elif message_type == ORSMessageType.ManualOrderCrossReply.value:
                 response_fix_message = ManualOrderCrossReply()
             elif message_type == ORSMessageType.OrderModificationReply.value:
-                response_fix_message = ManualOrderCrossReply()
+                response_fix_message = OrderModificationReply()
             elif message_type == ORSMessageType.OrderBagCreationReply.value:
                 response_fix_message = OrderBagCreationReply()
             elif message_type == ORSMessageType.OrderBagNotification.value:
