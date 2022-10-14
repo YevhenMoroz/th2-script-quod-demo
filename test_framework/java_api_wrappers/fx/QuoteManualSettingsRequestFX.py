44e8175c7212b1d4c18c8e22b556963b2f4bf3f3
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2022-10-14 13:03:21 +0200

    Update wrappers for manual changing pricing and executable

--- test_framework/java_api_wrappers/fx/QuoteManualSettingsRequestFX.py
+++ test_framework/java_api_wrappers/fx/QuoteManualSettingsRequestFX.py
@@ -10,48 +10,34 @@ class QuoteManualSettingsRequestFX(JavaApiMessage):
         super().__init__(message_type=QSMessageType.QuoteManualSettingsRequest.value, data_set=data_set)
         super().change_parameters(parameters)
 
-    def set_defaults(self):
+    def set_default_params(self):
         params_for_request = {
             "SEND_SUBJECT": "QUOD.PRICING.2.FE",
             "REPLY_SUBJECT": "QUOD.FE.PRICING.2",
             "QuoteManualSettingsRequestBlock": {
-                "InstrSymbol": "EUR/USD",
-                "ClientTierID": "2200009",
-                "Tenor": "SPO",
+                "InstrSymbol": self.get_data_set().get_symbol_by_name("symbol_1"),
+                "ClientTierID": self.get_data_set().get_client_tier_id_by_name("client_tier_id_1"),
+                "Tenor": self.get_data_set().get_tenor_java_api_by_name("tenor_spot"),
                 "MDQuoteType": "TRD",
                 "AutomatedMargin": "N"}}
         super().change_parameters(params_for_request)
+        return self
 
-    def turn_pricing_off(self):
-        self.change_parameter("QuoteManualSettingsRequestBlock", {
-            "InstrSymbol": "EUR/USD",
-            "ClientTierID": "2200009",
-            "Tenor": "SPO",
-            "MDQuoteType": "TRD",
-            "AutomatedMargin": "N",
-            "QuoteConditionList":
-                {"QuoteConditionBlock": [
-                    {"QuoteConditionEnum": "CLO"}
-                ]}})
+    def set_executable_off(self):
+        self.update_fields_in_component("QuoteManualSettingsRequestBlock", {"MDQuoteType": "IND"})
+        return self
 
-    def turn_executable_off(self):
-        self.change_parameter("QuoteManualSettingsRequestBlock", {
-            "InstrSymbol": "EUR/USD",
-            "ClientTierID": "2200009",
-            "Tenor": "SPO",
-            "MDQuoteType": "IND",
-            "AutomatedMargin": "N"
-                })
+    def set_executable_on(self):
+        self.update_fields_in_component("QuoteManualSettingsRequestBlock", {"MDQuoteType": "TRD"})
+        return self
 
-# "QuoteAdjustmentEntryList": {
-#                     "QuoteAdjustmentEntryBlock":
-#                         [{"BidMargin": "0", "OfferMargin": "0", "MDQuoteType": "TRD",
-#                           "IndiceUpperQty": "1"},
-#
-#                          {"BidMargin": "0", "OfferMargin": "0", "MDQuoteType": "TRD",
-#                           "IndiceUpperQty": "2"},
-#
-#                          {"BidMargin": "0", "OfferMargin": "0", "MDQuoteType": "TRD",
-#                           "IndiceUpperQty": "3"}
-#
-#                          ]}
+    def set_pricing_off(self):
+        self.update_fields_in_component("QuoteManualSettingsRequestBlock",
+                                        {"QuoteConditionList": {"QuoteConditionBlock": [
+                                            {"QuoteConditionEnum": "CLO"}
+                                        ]}})
+        return self
+
+    def set_pricing_on(self):
+        self.remove_fields_from_component("QuoteManualSettingsRequestBlock", ["QuoteConditionList"])
+        return self
