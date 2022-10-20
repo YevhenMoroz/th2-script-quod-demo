760fcd484e5375c921dc99915b795405cb07c90c
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-10-20 12:26:39 +0300

    QAP-8653 add set_POV_Redburn_params parameters preset

--- test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
+++ test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
@@ -128,6 +128,29 @@ class FixMessageNewOrderSingleAlgo(FixMessageNewOrderSingle):
         super().change_parameters(base_parameters)
         return self
 
+    def set_POV_Redburn_params(self) -> FixMessageNewOrderSingle:
+        base_parameters = {
+            'Account': self.get_data_set().get_account_by_name("account_1"),
+            'ClOrdID': basic_custom_actions.client_orderid(9),
+            "HandlInst": "2",
+            "Side": "1",
+            "OrderQty": "1000",
+            "TimeInForce": "0",
+            "OrdType": "2",
+            "TransactTime": datetime.utcnow().isoformat(),
+            "OrderCapacity": "A",
+            "Price": "20",
+            'Currency': self.get_data_set().get_currency_by_name("currency_1"),
+            'ExDestination': self.get_data_set().get_mic_by_name("mic_1"),
+            "Instrument": self.get_data_set().get_fix_instrument_by_name("instrument_1"),
+            "TargetStrategy": "2",
+            'QuodFlatParameters': {
+                'MaxPercentageVolume': '10'
+            }
+        }
+        super().change_parameters(base_parameters)
+        return self
+
     def set_TWAP_Navigator_params(self) -> FixMessageNewOrderSingle:
         base_parameters = {
             'Account': self.get_data_set().get_account_by_name("account_1"),
