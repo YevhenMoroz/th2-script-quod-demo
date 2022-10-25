91a91b7cf49bf27cc64f7f9ffc92eb0bf80bea68
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-10-25 18:05:33 +0300

    refactor QAP_T7310

--- test_cases/eq/Commissions/QAP_T7308.py
+++ test_cases/eq/Commissions/QAP_T7308.py
@@ -1,21 +1,19 @@
 import logging
-import time
 from pathlib import Path
 
 from custom import basic_custom_actions as bca
-from rule_management import RuleManager, Simulators
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
-from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.FixVerifier import FixVerifier
-from test_framework.fix_wrappers.oms.FixMessageConfirmationReportOMS import FixMessageConfirmationReportOMS
-from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.data_sets.message_types import ORSMessageType
+from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.java_api_constants import JavaApiFields, SubmitRequestConst
+from test_framework.java_api_wrappers.oms.ors_messges.ConfirmationOMS import ConfirmationOMS
+from test_framework.java_api_wrappers.oms.ors_messges.DFDManagementBatchOMS import DFDManagementBatchOMS
+from test_framework.java_api_wrappers.oms.ors_messges.ForceAllocInstructionStatusRequestOMS import \
+    ForceAllocInstructionStatusRequestOMS
+from test_framework.java_api_wrappers.oms.ors_messges.OrderSubmitOMS import OrderSubmitOMS
+from test_framework.java_api_wrappers.oms.ors_messges.TradeEntryOMS import TradeEntryOMS
 from test_framework.rest_api_wrappers.oms.rest_commissions_sender import RestCommissionsSender
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns
-from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
-from test_framework.win_gui_wrappers.oms.oms_middle_office import OMSMiddleOffice
-from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
-from test_framework.win_gui_wrappers.oms.oms_trades_book import OMSTradesBook
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -29,63 +27,70 @@ class QAP_T7308(TestCase):
         super().__init__(report_id, session_id, data_set, environment)
         self.fix_env = self.environment.get_list_fix_environment()[0]
         self.wa_connectivity = self.environment.get_list_web_admin_rest_api_environment()[0].session_alias_wa
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.rest_commission_sender = RestCommissionsSender(self.wa_connectivity, self.test_id,
+                                                            self.data_set)
         self.client = self.data_set.get_client_by_name("client_pt_3")
         self.client_acc = self.data_set.get_account_by_name("client_pt_3_acc_1")
-        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
-        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_dma_limit("instrument_3")
-        self.qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
-        self.price = self.fix_message.get_parameter("Price")
-        self.cl_ord_id = self.fix_message.get_parameter('ClOrdID')
-        self.fix_message.change_parameters(
-            {'OrderQtyData': {'OrderQty': self.qty}, "Price": self.price, "Account": self.client,
-             "ExDestination": self.data_set.get_mic_by_name("mic_2"),
-             "Currency": self.data_set.get_currency_by_name("currency_3")})
-        self.mic = self.data_set.get_mic_by_name("mic_2")
-        self.trades = OMSTradesBook(self.test_id, self.session_id)
+        self.currency = self.data_set.get_currency_by_name("currency_3")
+        self.java_api_connectivity = self.java_api = self.environment.get_list_java_api_environment()[0].java_api_conn
+        self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
         self.rest_commission_sender = RestCommissionsSender(self.wa_connectivity, self.test_id, self.data_set)
-        self.fix_verifier_dc = FixVerifier(self.fix_env.drop_copy, self.test_id)
-        self.client_for_rule = self.data_set.get_venue_client_names_by_name("client_pt_3_venue_2")
-        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
-        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
-        self.order_book = OMSOrderBook(self.test_id, self.session_id)
-        self.mid_office = OMSMiddleOffice(self.test_id, self.session_id)
-        self.rule_manager = RuleManager(sim=Simulators.equity)
+        self.force_alloc = ForceAllocInstructionStatusRequestOMS(self.data_set)
+        self.confirm = ConfirmationOMS(self.data_set)
+        self.submit_request = OrderSubmitOMS(self.data_set)
+        self.trade_request = TradeEntryOMS(self.data_set)
+        self.complete_message = DFDManagementBatchOMS(self.data_set)
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # endregion
         # region send commission
         self.rest_commission_sender.clear_commissions()
         self.rest_commission_sender.clear_fees()
-        self.rest_commission_sender.send_default_fee()
+        self.rest_commission_sender.set_modify_client_commission_message(
+            comm_profile=self.data_set.get_comm_profile_by_name("abs_amt"), client=self.client)
+        self.rest_commission_sender.send_post_request()
         # endregion
-        # region create order
-        self.__send_fix_orders()
-        order_id = self.response[0].get_parameter("OrderID")
+        # region step 1
+        self.submit_request.set_default_care_limit(recipient=self.environment.get_list_fe_environment()[0].user_1,
+                                                   desk=self.environment.get_list_fe_environment()[0].desk_ids[0],
+                                                   role=SubmitRequestConst.USER_ROLE_1.value)
+        self.submit_request.update_fields_in_component("NewOrderSingleBlock", {
+            "InstrID": self.data_set.get_instrument_id_by_name("instrument_3"), 'AccountGroupID': self.client,
+            'ListingList': {'ListingBlock': [{'ListingID': self.data_set.get_listing_id_by_name("listing_2")}]},
+            'SettlCurrency': self.currency})
+        self.java_api_manager.send_message_and_receive_response(self.submit_request)
+        order_id = self.java_api_manager.get_last_message(ORSMessageType.OrdNotification.value).get_parameter(
+            JavaApiFields.OrderNotificationBlock.value)["OrdID"]
         # endregion
-
-        # region approve and allocate block
-        self.mid_office.approve_block()
-        allocation_values = [{"Security Account": self.client_acc,
-                              "Alloc Qty": self.qty}]
-        self.mid_office.set_modify_ticket_details(arr_allocation_param=allocation_values)
-        self.mid_office.allocate_block([OrderBookColumns.order_id.value, order_id])
-        conf_report = FixMessageConfirmationReportOMS(self.data_set).set_default_confirmation_new(
-            self.fix_message)
-        conf_report.change_parameters({"Account": self.client, "AvgPx": "0.2", "Currency": "*", "tag5120": "*",
-                                       "CommissionData": {"CommissionType": "3", "Commission": "0.01",
-                                                          "CommCurrency": "*"}})
-        self.fix_verifier_dc.check_fix_message_fix_standard(conf_report)
+        # region step 2
+        self.trade_request.set_default_trade(order_id)
+        self.java_api_manager.send_message_and_receive_response(self.trade_request)
+        exec_reply = self.java_api_manager.get_last_message(ORSMessageType.ExecutionReport.value).get_parameters()[
+            JavaApiFields.ExecutionReportBlock.value]
+        expected_result = {"TransExecStatus": "FIL"}
+        self.java_api_manager.compare_values(expected_result, exec_reply, "Compare TransExecStatus")
+        self.complete_message.set_default_complete(order_id)
+        self.java_api_manager.send_message_and_receive_response(self.complete_message)
+        alloc_report = self.java_api_manager.get_last_message(ORSMessageType.AllocationReport.value).get_parameters()[
+            JavaApiFields.AllocationReportBlock.value]
+        alloc_id = alloc_report["AllocInstructionID"]
         # endregion
-
-    @try_except(test_id=Path(__file__).name[:-3])
-    def __send_fix_orders(self):
-        try:
-            trade_rule = self.rule_manager.add_NewOrdSingleExecutionReportTrade_FIXStandard(self.fix_env.buy_side,
-                                                                                            self.client_for_rule,
-                                                                                            self.mic, int(self.price),
-                                                                                            int(self.qty), 2)
-            self.response = self.fix_manager.send_message_and_receive_response_fix_standard(self.fix_message)
-        finally:
-            time.sleep(1)
-            self.rule_manager.remove_rule(trade_rule)
+        # region step 3
+        self.force_alloc.set_default_approve(alloc_id)
+        self.java_api_manager.send_message(self.force_alloc)
+        self.confirm.set_default_allocation(alloc_id)
+        self.confirm.update_fields_in_component("ConfirmationBlock", {"AllocAccountID": self.client_acc,
+                                                                      "InstrID": self.data_set.get_instrument_id_by_name(
+                                                                          "instrument_3")})
+        self.java_api_manager.send_message_and_receive_response(self.confirm)
+        expected_result = {'CommissionCurrency': self.data_set.get_currency_by_name("currency_2"),
+                           'CommissionBasis': 'ABS', 'CommissionRate': '0.01', 'CommissionAmount': '0.01',
+                           'CommissionAmountType': 'BRK'}
+        confirm_report = \
+            self.java_api_manager.get_last_message(ORSMessageType.ConfirmationReport.value).get_parameters()[
+                JavaApiFields.ConfirmationReportBlock.value]
+        self.java_api_manager.compare_values(expected_result,
+                                             confirm_report["ClientCommissionList"]["ClientCommissionBlock"][0],
+                                             "Compare ClientCommission")
+        # endregion
