a9413702f50482f5fed0196430d72cc164fbaeb4
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-10-26 12:14:21 +0300

    QAP-8653 update test case

--- test_cases/algo/Algo_Redburn/Algo_POV/QAP_T8791.py
+++ test_cases/algo/Algo_Redburn/Algo_POV/QAP_T8791.py
@@ -250,9 +250,9 @@ class QAP_T8791(TestCase):
 
         # region Send MarketData to trigger the POV SOR Aggressive order creation
         self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data Incremental to trigger the POV Aggressive order creation", self.test_id))
-        market_data_incremental_trqx = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_ltq().update_MDReqID(self.s_trqx, self.fix_env1.feed_handler)
-        market_data_incremental_trqx.update_repeating_group_by_index('NoMDEntriesIR', 0, MDEntryPx=self.price_ltq, MDEntrySize=self.qty_ltq_1)
-        self.fix_manager_feed_handler.send_message(market_data_incremental_trqx)
+        market_data_incremental_par = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_ltq().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
+        market_data_incremental_par.update_repeating_group_by_index('NoMDEntriesIR', 0, MDEntryPx=self.price_ltq, MDEntrySize=self.qty_ltq_1)
+        self.fix_manager_feed_handler.send_message(market_data_incremental_par)
 
         time.sleep(20)
         # endregion
