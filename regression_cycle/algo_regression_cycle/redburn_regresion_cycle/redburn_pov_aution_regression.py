6420d1110b37c8cf2d91456cfde83e0f79946dc6
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2022-10-27 18:39:37 +0200

    Update test cycle name - Redburn

--- regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_pov_aution_regression.py
+++ regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_pov_aution_regression.py
@@ -11,7 +11,7 @@ logging.getLogger().setLevel(logging.WARN)
 
 def test_run(parent_id=None, version=None):
     # Generation id and time for test run
-    report_id = bca.create_event(f"POV - Auction | {version}", parent_id)
+    report_id = bca.create_event(f"POV - Auction (verification) | {version}", parent_id)
     logger.info(f"Root event was created (id = {report_id.id})")
     try:
         # region Iceberg: Route/Venue
