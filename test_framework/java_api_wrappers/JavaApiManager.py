cda8b753633d8a84e86cdc000a54264e11ca9980
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2022-11-04 09:49:58 +0100

    QAP-8703 Create messages for Fix_RequestForPostions via JavaAPI in Python framework

--- test_framework/java_api_wrappers/JavaApiManager.py
+++ test_framework/java_api_wrappers/JavaApiManager.py
@@ -4,11 +4,12 @@ from th2_grpc_act_java_api_quod.act_java_api_quod_pb2 import ActJavaSubmitMessag
 from custom import basic_custom_actions as bca
 from custom.verifier import VerificationMethod, Verifier
 from stubs import Stubs
-from test_framework.data_sets.message_types import ORSMessageType, CSMessageType, ESMessageType
+from test_framework.data_sets.message_types import ORSMessageType, CSMessageType, ESMessageType, PKSMessageType
 from test_framework.java_api_wrappers.JavaApiMessage import JavaApiMessage
 from test_framework.java_api_wrappers.cs_message.CDOrdNotif import CDOrdNotif
 from test_framework.java_api_wrappers.es_messages.NewOrderReply import NewOrderReply
 from test_framework.java_api_wrappers.es_messages.OrdReport import OrdReport
+from test_framework.java_api_wrappers.fx.FixPositionReportFX import FixPositionReportFX
 from test_framework.java_api_wrappers.fx.QuoteRequestActionReplyFX import QuoteRequestActionReplyFX
 from test_framework.java_api_wrappers.fx.QuoteRequestNotifFX import QuoteRequestNotifFX
 from test_framework.java_api_wrappers.ors_messages.AllocationReport import AllocationReport
@@ -226,6 +227,12 @@ class JavaApiManager:
                     message=bca.message_to_grpc_fix_standard(message.get_message_type(),
                                                              message.get_parameters(), self.get_session_alias()),
                     parent_event_id=self.get_case_id()))
+        elif message.get_message_type() == PKSMessageType.FixRequestForPositions.value:
+            response = self.act.submitFixRequestForPositions(
+                request=ActJavaSubmitMessageRequest(
+                    message=bca.message_to_grpc_fix_standard(message.get_message_type(),
+                                                             message.get_parameters(), self.get_session_alias()),
+                    parent_event_id=self.get_case_id()))
         else:
             response = None
         return self.parse_response(response)
@@ -324,7 +331,8 @@ class JavaApiManager:
                 response_fix_message = QuoteRequestNotifFX()
             elif message_type == ORSMessageType.QuoteRequestActionReply.value:
                 response_fix_message = QuoteRequestActionReplyFX()
-
+            elif message_type == PKSMessageType.FixRequestForPositions.value:
+                response_fix_message = FixPositionReportFX()
             response_fix_message.change_parameters(fields)
             response_messages.append(response_fix_message)
         self.response = response_messages
