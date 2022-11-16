2b06b798ede6e308d6f74e0098c25136c70b0dc9
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2022-11-16 15:57:35 +0200

    QAP-3985 update name of Sell Gateway (with RB plugin) env for quod316@ganymede

--- test_framework/environments/fix_environment.py
+++ test_framework/environments/fix_environment.py
@@ -43,7 +43,7 @@ class FixEnvironment(BaseEnvironment):
             if EnvironmentType.quod316_ganymede_redburn.value not in FixEnvironment.environment_instances.keys():
                 site_environment = FixEnvironment(
                     environment_type=EnvironmentType.quod316_ganymede_redburn.value,
-                    sell_side=Connectivity.Ganymede_316_Redburn.value,
+                    sell_side=Connectivity.Ganymede_316_Sell_Side_Redburn.value,
                     buy_side=Connectivity.Ganymede_316_Buy_Side_Redburn.value,
                     feed_handler=Connectivity.Ganymede_316_Feed_Handler.value
                 )
