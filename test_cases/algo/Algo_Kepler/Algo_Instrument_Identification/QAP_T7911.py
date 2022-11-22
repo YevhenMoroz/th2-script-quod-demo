da4bdfbfed40fa9bfe87445f7bc8db13ae0f7557
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2022-11-22 14:04:18 +0100

    QAP-T7911 Need to add correct text

--- test_cases/algo/Algo_Kepler/Algo_Instrument_Identification/QAP_T7911.py
+++ test_cases/algo/Algo_Kepler/Algo_Instrument_Identification/QAP_T7911.py
@@ -41,6 +41,7 @@ class QAP_T7911(TestCase):
         self.price_bid = 30
         self.px_for_incr = 0
         self.side = constants.OrderSide.Sell.value
+        self.currency = self.data_set.get_currency_by_name("currency_4")
         # endregion
 
         # region Gateway Side
@@ -49,9 +50,7 @@ class QAP_T7911(TestCase):
         # endregion
 
         # region Status
-        self.status_pending = Status.Pending
-        self.status_new = Status.New
-        self.status_cancel = Status.Cancel
+        self.status_reject = Status.Reject
         # endregion
 
         # region instrument
@@ -84,13 +83,6 @@ class QAP_T7911(TestCase):
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # region Rule creation
-        rule_manager = RuleManager(Simulators.algo)
-        nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side, self.account, self.ex_destination_xfra, self.price)
-        ocr_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account, self.ex_destination_xfra, True)
-        self.rule_list = [nos_rule, ocr_rule]
-        # endregion
-
         # region Send_MarkerData
         self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data", self.test_id))
         market_data_snap_shot_xfra = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.listing_id_xfra, self.fix_env1.feed_handler)
@@ -122,7 +114,7 @@ class QAP_T7911(TestCase):
 
         self.SORPING_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_Iceberg_Kepler()
         self.SORPING_order.add_ClordId((os.path.basename(__file__)[:-3]))
-        self.SORPING_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument, ExDestination=self.ex_destination_xfra, DisplayInstruction=dict(DisplayQty=self.display_qty), Side=self.side))
+        self.SORPING_order.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Currency=self.currency, Instrument=self.instrument, ExDestination=self.ex_destination_xfra, DisplayInstruction=dict(DisplayQty=self.display_qty), Side=self.side))
 
         self.fix_manager_sell.send_message_and_receive_response(self.SORPING_order, case_id_1)
 
@@ -132,47 +124,6 @@ class QAP_T7911(TestCase):
         # region Check Sell side
         self.fix_verifier_sell.check_fix_message(self.SORPING_order, direction=self.ToQuod, message_name='Sell side NewOrderSingle')
 
-        er_pending_new_SORPING_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.SORPING_order, self.gateway_side_sell, self.status_pending)
-        self.fix_verifier_sell.check_fix_message(er_pending_new_SORPING_order_params, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport PendingNew')
-
-        er_new_SORPING_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.SORPING_order, self.gateway_side_sell, self.status_new)
-        self.fix_verifier_sell.check_fix_message(er_new_SORPING_order_params, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport New')
+        er_reject_SORPING_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.SORPING_order, self.gateway_side_sell, self.status_reject)
+        self.fix_verifier_sell.check_fix_message(er_reject_SORPING_order_params, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport Reject')
         # endregion
-
-        # region Check child DMA order
-        self.fix_verifier_buy.set_case_id(bca.create_event("Child DMA order", self.test_id))
-
-        self.dma_xfra_order = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_DMA_child_of_Iceberg_Kepler()
-        self.dma_xfra_order.change_parameters(dict(Account=self.account, ExDestination=self.ex_destination_xfra, OrderQty=self.display_qty, Price=self.price, Instrument=self.instrument, Side=self.side))
-        self.fix_verifier_buy.check_fix_message(self.dma_xfra_order, key_parameters=self.key_params_NOS_child, message_name='Buy side NewOrderSingle Child DMA 1 order')
-
-        er_pending_new_dma_xfra_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_xfra_order, self.gateway_side_buy, self.status_pending)
-        self.fix_verifier_buy.check_fix_message(er_pending_new_dma_xfra_order_params, key_parameters=self.key_params_ER_child, direction=self.ToQuod, message_name='Buy side ExecReport PendingNew Child DMA 1 order')
-
-        er_new_dma_xfra_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_xfra_order, self.gateway_side_buy, self.status_new)
-        self.fix_verifier_buy.check_fix_message(er_new_dma_xfra_order_params, key_parameters=self.key_params_ER_child, direction=self.ToQuod, message_name='Buy side ExecReport New Child DMA 1 order')
-        # endregion
-
-        time.sleep(10)
-
-    @try_except(test_id=Path(__file__).name[:-3])
-    def run_post_conditions(self):
-        # region Cancel Algo Order
-        case_id_2 = bca.create_event("Cancel Algo Order", self.test_id)
-        self.fix_verifier_sell.set_case_id(case_id_2)
-        cancel_request_SORPING_order = FixMessageOrderCancelRequest(self.SORPING_order)
-
-        self.fix_manager_sell.send_message_and_receive_response(cancel_request_SORPING_order, case_id_2)
-        self.fix_verifier_sell.check_fix_message(cancel_request_SORPING_order, direction=self.ToQuod, message_name='Sell side Cancel Request')
-
-        # region check cancel first dma child order
-        er_cancel_dma_xfra_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.dma_xfra_order, self.gateway_side_buy, self.status_cancel)
-        self.fix_verifier_buy.check_fix_message(er_cancel_dma_xfra_order, self.key_params_ER_child, self.ToQuod, "Buy Side ExecReport Cancel child DMA 1 order")
-        # endregion
-
-        er_cancel_SORPING_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.SORPING_order, self.gateway_side_sell, self.status_cancel)
-        self.fix_verifier_sell.check_fix_message(er_cancel_SORPING_order_params, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport Cancel')
-        # endregion
-
-        rule_manager = RuleManager(Simulators.algo)
-        rule_manager.remove_rules(self.rule_list)
