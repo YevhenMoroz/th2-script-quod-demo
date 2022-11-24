749de3b094a40956392cb541d887046738d1bd8a
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-11-24 08:33:35 +0100

    added ignored_fields to exec report for QAP-T7230

--- test_cases/eq/PostTrade/QAP_T7230.py
+++ test_cases/eq/PostTrade/QAP_T7230.py
@@ -84,7 +84,7 @@ class QAP_T7230(TestCase):
             {"ReplyReceivedTime": "*", "LastMkt": "*", "Text": "*", "Account": self.venue_client_names}
         )
         self.exec_report.remove_parameters(["SettlCurrency"])
-        self.fix_verifier.check_fix_message_fix_standard(self.exec_report)
+        self.fix_verifier.check_fix_message_fix_standard(self.exec_report, ignored_fields=['Account', 'SecurityDesc'])
         # endregion
 
         # region Book order and checking values after it in the Order book
