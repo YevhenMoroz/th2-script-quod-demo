435732347b114bf7aad247c835f2f416ab20a986
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2022-11-28 18:31:18 +0100

    QAP_T2614: quote_cancel is added

--- test_cases/fx/fx_mm_rfq/QAP_T2614.py
+++ test_cases/fx/fx_mm_rfq/QAP_T2614.py
@@ -6,6 +6,7 @@ from test_framework.data_sets.base_data_set import BaseDataSet
 from test_framework.environments.full_environment import FullEnvironment
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.fix_wrappers.forex.FixMessageQuoteCancel import FixMessageQuoteCancelFX
 from test_framework.fix_wrappers.forex.FixMessageQuoteFX import FixMessageQuoteFX
 from test_framework.fix_wrappers.forex.FixMessageQuoteRequestFX import FixMessageQuoteRequestFX
 
@@ -20,6 +21,7 @@ class QAP_T2614(TestCase):
         self.fix_env = self.environment.get_list_fix_environment()[0]
         self.fix_manager = FixManager(self.fix_env.sell_side_rfq, self.test_id)
         self.fix_verifier = FixVerifier(self.fix_env.sell_side_rfq, self.test_id)
+        self.quote_cancel = FixMessageQuoteCancelFX()
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
@@ -31,3 +33,8 @@ class QAP_T2614(TestCase):
         self.quote.set_params_for_quote_fwd(self.quote_request)
         self.fix_verifier.check_fix_message(fix_message=self.quote, key_parameters=["QuoteReqID"])
         # endregion
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.quote_cancel.set_params_for_cancel(self.quote_request)
+        self.fix_manager.send_message(self.quote_cancel)
