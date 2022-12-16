9ebb679e9da6fce443638e60b9bd4b050a474e27
Mark Antonov <mark.antonov.contractor@quodfinancial.com>
2022-12-16 18:52:11 +0200

    QAP-T7110 automated

--- test_cases/eq/PostTrade/QAP_T7110.py
+++ test_cases/eq/PostTrade/QAP_T7110.py
@@ -8,17 +8,26 @@ from custom.basic_custom_actions import timestamps
 from rule_management import RuleManager, Simulators
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
+from test_framework.data_sets.message_types import ORSMessageType
 from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.fix_wrappers.oms.FixMessageExecutionReportOMS import FixMessageExecutionReportOMS
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, PostTradeStatuses, \
-    MiddleOfficeColumns
-from test_framework.win_gui_wrappers.oms.oms_middle_office import OMSMiddleOffice
-from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
+from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.java_api_constants import JavaApiFields, OrderReplyConst
+from test_framework.java_api_wrappers.oms.ors_messges.AllocationInstructionOMS import AllocationInstructionOMS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 seconds, nanos = timestamps()  # Test case start time
+
+
+def print_message(message, responses):
+    logger.info(message)
+    for i in responses:
+        logger.info(i)
+        logger.info(i.get_parameters())
 
 
 class QAP_T7110(TestCase):
@@ -30,60 +39,102 @@ class QAP_T7110(TestCase):
         self.fix_env = self.environment.get_list_fix_environment()[0]
         self.ss_connectivity = self.fix_env.sell_side
         self.bs_connectivity = self.fix_env.buy_side
-        self.qty = '500'
-        self.price = '10000'
+        self.qty = "100"
+        self.price = "20"
         self.rule_manager = RuleManager(sim=Simulators.equity)
-        self.currency = self.data_set.get_currency_by_name('currency_3')  # GBp
-        self.venue_client_names = self.data_set.get_venue_client_names_by_name('client_pt_1_venue_2')  # MOClient_EUREX
-        self.venue = self.data_set.get_mic_by_name('mic_2')  # XEUR
-        self.client = self.data_set.get_client('client_pt_1')  # MOClient
-        self.order_book = OMSOrderBook(self.test_id, self.session_id)
-        self.middle_office = OMSMiddleOffice(self.test_id, self.session_id)
+        self.currency = self.data_set.get_currency_by_name("currency_3")  # GBp
+        self.currency_major = self.data_set.get_currency_by_name("currency_2")  # GBP
+        self.venue_client_names = self.data_set.get_venue_client_names_by_name("client_pt_1_venue_2")  # MOClient_EUREX
+        self.venue = self.data_set.get_mic_by_name("mic_2")  # XEUR
+        self.client = self.data_set.get_client("client_pt_1")  # MOClient
         self.fix_manager = FixManager(self.ss_connectivity, self.test_id)
         self.fix_message = FixMessageNewOrderSingleOMS(self.data_set)
+        self.fix_verifier = FixVerifier(self.ss_connectivity, self.test_id)
+        self.execution_report = FixMessageExecutionReportOMS(self.data_set)
+        self.java_api_connectivity = self.environment.get_list_java_api_environment()[0].java_api_conn
+        self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
+        self.allocation_instruction = AllocationInstructionOMS(self.data_set)
         # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # region Create DMA order via FIX
+        # region Step 1,2 - Create DMA order via FIX
         try:
-            trade_rule = self.rule_manager.add_NewOrdSingleExecutionReportTrade_FIXStandard(self.bs_connectivity,
-                                                                                            self.venue_client_names,
-                                                                                            self.venue,
-                                                                                            float(self.price),
-                                                                                            int(self.qty), 0)
-            self.fix_message.set_default_dma_limit(instr='instrument_3')
+            trade_rule = self.rule_manager.add_NewOrdSingleExecutionReportTrade_FIXStandard(
+                self.bs_connectivity, self.venue_client_names, self.venue, float(self.price), int(self.qty), 0
+            )
+            self.fix_message.set_default_dma_limit(instr="instrument_3")
             self.fix_message.change_parameters(
-                {'Side': '2', 'OrderQtyData': {'OrderQty': self.qty}, 'Account': self.client, 'Price': self.price,
-                 'Currency': self.currency, 'ExDestination': 'XEUR'})
+                {
+                    "Side": "1",
+                    "OrderQtyData": {"OrderQty": self.qty},
+                    "Account": self.client,
+                    "Price": self.price,
+                    "Currency": self.currency,
+                    "ExDestination": "XEUR",
+                }
+            )
             response = self.fix_manager.send_message_and_receive_response(self.fix_message)
             # get Client Order ID and Order ID
-            cl_ord_id = response[0].get_parameters()['ClOrdID']
-            order_id = response[0].get_parameters()['OrderID']
+            cl_ord_id = response[0].get_parameters()["ClOrdID"]
+            ord_id = response[0].get_parameters()["OrderID"]
 
         except Exception:
-            logger.error('Error execution', exc_info=True)
+            logger.error("Error execution", exc_info=True)
         finally:
             time.sleep(2)
             self.rule_manager.remove_rule(trade_rule)
         # endregion
 
-        # region Book order and checking PostTradeStatus in the Order book
-        self.middle_office.set_modify_ticket_details(remove_comm=True, remove_fee=True)
-        self.middle_office.book_order(filter=[OrderBookColumns.cl_ord_id.value, cl_ord_id])
-        self.order_book.set_filter([OrderBookColumns.cl_ord_id.value, cl_ord_id])
-        self.order_book.check_order_fields_list(
-            {OrderBookColumns.post_trade_status.value: PostTradeStatuses.booked.value},
-            'Comparing PostTradeStatus after Book')
+        # region Checking Execution report
+        self.execution_report.set_default_filled(self.fix_message)
+        self.execution_report.change_parameters({"Currency": self.currency})
+        list_of_ignored_fields: list = ["ReplyReceivedTime", "LastMkt", "Text", "SettlCurrency", "Account"]
+        self.fix_verifier.check_fix_message_fix_standard(self.execution_report, ignored_fields=list_of_ignored_fields)
         # endregion
 
-        # region Checking Price after the Book in the Middle Office
-        values_after_book = self.middle_office.extract_list_of_block_fields(
-            [MiddleOfficeColumns.price.value, 'Net Price'], [MiddleOfficeColumns.order_id.value, order_id])
-        self.middle_office.compare_values(
-            {MiddleOfficeColumns.price.value: str(round(int(self.price) / 100)),
-             'Net Price': str(round(int(self.price) / 100))}, values_after_book,
-            'Comparing Price in the MiddleOffice after the Book')
+        # region Step 3 - Book order
+        self.allocation_instruction.set_default_book(ord_id)
+        self.allocation_instruction.update_fields_in_component(
+            "AllocationInstructionBlock",
+            {
+                "Qty": self.qty,
+                "AccountGroupID": self.client,
+                "GrossTradeAmt": "20",
+                "AvgPx": str(int(self.price) / 100),
+                "SettlCurrAmt": "20",
+                "Currency": self.currency_major,
+                "InstrID": self.data_set.get_instrument_id_by_name("instrument_3"),
+            },
+        )
+
+        responses = self.java_api_manager.send_message_and_receive_response(self.allocation_instruction)
+        print_message("BOOK", responses)
+
+        # Checking Order_OrdUpdate
+        order_update_reply = self.java_api_manager.get_last_message(ORSMessageType.OrdUpdate.value).get_parameters()[
+            JavaApiFields.OrdUpdateBlock.value
+        ]
+        self.java_api_manager.compare_values(
+            {JavaApiFields.PostTradeStatus.value: OrderReplyConst.PostTradeStatus_BKD.value},
+            order_update_reply,
+            "Comparing PostTradeStatus after Book",
+        )
+
+        # Checking AllocationReportBlock
+        alloc_report_reply = self.java_api_manager.get_last_message(
+            ORSMessageType.AllocationReport.value
+        ).get_parameters()[JavaApiFields.AllocationReportBlock.value]
+        alloc_id: str = alloc_report_reply["ClientAllocID"]
+        self.java_api_manager.compare_values(
+            {
+                JavaApiFields.AllocStatus.value: "APP",
+                JavaApiFields.MatchStatus.value: "UNM",
+                "AvgPrice": str(int(self.price) / 100),
+            },
+            alloc_report_reply,
+            "Comparing Statuses and Price in MO after Book",
+        )
         # endregion
 
         logger.info(f"Case {self.test_id} was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
