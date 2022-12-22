3b9f4fda3b2acdabf202416bad0f281330471c21
skomanov <sofia.komanova.contractor@quodfinancial.com>
2022-12-22 11:10:12 +0100

    modified QAP_T7255

--- test_cases/eq/DMA/QAP_T7255.py
+++ test_cases/eq/DMA/QAP_T7255.py
@@ -1,49 +1,96 @@
 import logging
+import os
 import time
-
-from custom.basic_custom_actions import create_event
-from test_framework.old_wrappers import eq_wrappers, eq_fix_wrappers
-from rule_management import RuleManager
-from stubs import Stubs
-from test_framework.old_wrappers.eq_wrappers import open_fe
-from win_gui_modules.utils import get_base_request
-from win_gui_modules.wrappers import set_base
+from pathlib import Path
+import xml.etree.ElementTree as ET
+from custom import basic_custom_actions as bca
+from custom.basic_custom_actions import timestamps
+from rule_management import RuleManager, Simulators
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.data_sets.message_types import ORSMessageType
+from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.java_api_constants import JavaApiFields
+from test_framework.java_api_wrappers.oms.ors_messges.FixNewOrderSingleOMS import FixNewOrderSingleOMS
+from test_framework.ssh_wrappers.ssh_client import SshClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-timeouts = True
+
+seconds, nanos = timestamps()  # Test case start time
 
 
-def execute(report_id, session_id):
-    case_name = "QAP_T7255"
-    qty = "200"
-    client = "CLIENT_VSKULINEC_PARIS"
-    price = 400
-    side = 1
-    tif = 0
-    case_id = create_event(case_name, report_id)
-    set_base(session_id, case_id)
-    buy_connectivity = eq_fix_wrappers.get_buy_connectivity()
+class QAP_T7255(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id, data_set, environment):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.ss_connectivity = self.fix_env.sell_side
+        self.bs_connectivity = self.fix_env.buy_side
+        self.dc_connectivity = self.fix_env.drop_copy
+        self.java_api_connectivity = self.java_api = self.environment.get_list_java_api_environment()[0].java_api_conn
+        self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
+        self.qty = '500'
+        self.price = '20'
+        self.rule_manager = RuleManager(sim=Simulators.equity)
+        self.venue_client_names = self.data_set.get_venue_client_names_by_name('client_pt_1_venue_1')  # MOClient_PARIS
+        self.venue = self.data_set.get_mic_by_name('mic_1')  # XPAR
+        self.client = self.data_set.get_client('client_pt_1')  # MOClient
+        self.fix_message = FixNewOrderSingleOMS(self.data_set).set_default_dma_limit()
+        self.fix_message.update_fields_in_component('NewOrderSingleBlock', {'ClientAccountGroupID': self.venue_client_names})
+        self.fix_verifier_dc = FixVerifier(self.dc_connectivity, self.test_id)
+        self.ssh_client_env = self.environment.get_list_ssh_client_environment()[0]
+        self.ssh_client = SshClient(self.ssh_client_env.host, self.ssh_client_env.port, self.ssh_client_env.user,
+                                    self.ssh_client_env.password, self.ssh_client_env.su_user,
+                                    self.ssh_client_env.su_password)
+        self.local_path = os.path.abspath("test_framework\ssh_wrappers\oms_cfg_files\client_ors.xml")
+        self.remote_path = f"/home/{self.ssh_client_env.su_user}/quod/cfg/client_ors.xml"
+        # endregion
 
-    try:
-        rule_manager = RuleManager()
-        nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(buy_connectivity, client, "XPAR", price)
-        fix_message = eq_fix_wrappers.create_order_via_fix(case_id, 1, side, client, 2, qty, tif, price=price)
-        fix_message.pop('response')
-    except Exception:
-        logger.error("Error execution", exc_info=True)
-    finally:
-        time.sleep(1)
-        rule_manager.remove_rule(nos_rule)
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region set up configuration on BackEnd(precondition)
+        tree = ET.parse(self.local_path)
+        quod = tree.getroot()
+        quod.find("ors/FrontToBack/acceptMultipleVenueAccountGroups").text = 'true'
+        tree.write("temp.xml")
+        self.ssh_client.send_command("~/quod/script/site_scripts/change_permission_script")
+        self.ssh_client.put_file(self.remote_path, "temp.xml")
+        self.ssh_client.send_command("qrestart ORS")
+        time.sleep(90)
+        # endregion
 
-    base_request = get_base_request(session_id, case_id)
-    open_fe(case_id, report_id, session_id)
-    eq_wrappers.verify_order_value(base_request, case_id, "NIN", client)
-    eq_wrappers.verify_order_value(base_request, case_id, "Sts", "Open")
+        responses = None
+        try:
+            nos_rule = self.rule_manager.add_NewOrdSingleExecutionReportPendingAndNew_FIXStandard(self.bs_connectivity,
+                                                                                                  self.venue_client_names,
+                                                                                                  self.venue,
+                                                                                                  float(self.price))
+            responses = self.java_api_manager.send_message_and_receive_response(self.fix_message)
+        except Exception:
+            logger.error('Error execution', exc_info=True)
+        finally:
+            time.sleep(2)
+            self.rule_manager.remove_rule(nos_rule)
+        # endregion
 
+        # region check
+        self.__return_result(responses, ORSMessageType.OrderReply.value)
+        order_reply_block = self.result.get_parameter('OrdReplyBlock')
+        self.java_api_manager.compare_values({JavaApiFields.AccountGroupID.value: self.client,
+                                              JavaApiFields.VenueClientActGrpName.value: self.venue_client_names},
+                                             order_reply_block, 'Check Order Account and VenueClientActGrpName')
+        # endregion
 
-def open_fe(case_id, report_id, session_id):
-    work_dir = Stubs.custom_config['qf_trading_fe_folder_1']
-    username = Stubs.custom_config['qf_trading_fe_user_1']
-    password = Stubs.custom_config['qf_trading_fe_password_1']
-    open_fe(session_id, report_id, case_id, work_dir, username)
+    def __return_result(self, responses, message_type):
+        for response in responses:
+            if response.get_message_type() == message_type:
+                self.result = response
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.ssh_client.put_file(self.remote_path, self.local_path)
+        self.ssh_client.send_command("qrestart ORS")
+        os.remove("temp.xml")
