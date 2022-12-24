a95275649e3880c331d358741695718d728d447e
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2022-12-24 19:50:02 +0200

    add QAP_T7036

--- test_cases/eq/DMA/QAP_T7036.py
+++ test_cases/eq/DMA/QAP_T7036.py
@@ -1,5 +1,4 @@
 import logging
-import time
 from pathlib import Path
 
 from custom import basic_custom_actions as bca
@@ -8,14 +7,13 @@ from test_framework.core.try_exept_decorator import try_except
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.FixVerifier import FixVerifier
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.ssh_wrappers.ssh_client import SshClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 timeouts = True
 
 
-class QAP_T7074(TestCase):
+class QAP_T7036(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, session_id=None, data_set=None, environment=None):
         super().__init__(report_id, session_id, data_set, environment)
@@ -24,38 +22,17 @@ class QAP_T7074(TestCase):
         self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
         self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_dma_limit()
         self.mic = self.data_set.get_mic_by_name('mic_1')  # XPAR
-        self.mic_blm = self.data_set.get_mic_by_name('mic_1_blm')  # XPAR_BLM
+
         self.fix_verifier = FixVerifier(self.fix_env.buy_side, self.test_id)
-        self.ssh_client_env = self.environment.get_list_ssh_client_environment()[0]
-        self.ssh_client = SshClient(self.ssh_client_env.host, self.ssh_client_env.port, self.ssh_client_env.user,
-                                    self.ssh_client_env.password, self.ssh_client_env.su_user,
-                                    self.ssh_client_env.su_password)
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # region Precondition
-        self.ssh_client.send_command("sql")
-        self.ssh_client.send_command(
-            f"UPDATE exchangecode SET bloombergexchcode='{self.mic_blm}' WHERE mic='{self.mic}';")
-        self.ssh_client.send_command("exit")
-        self.ssh_client.send_command("qrestart AQS ORS ESBUYTH2TEST")
-        time.sleep(70)
-        # endregion
-        self.fix_message.remove_parameter("ExDestination")
-        self.fix_message.update_fields_in_component("Instrument", {"SecurityExchange": self.mic_blm})
+        self.fix_message.update_fields_in_component("Instrument", {"ProductComplex": "SimpleInstrument"})
         venue_client_account = self.data_set.get_venue_client_names_by_name('client_1_venue_1')
         response = self.fix_manager.send_message_and_receive_response_fix_standard(self.fix_message)
         ord_id = response[0].get_parameter("OrderID")
         self.fix_message.change_parameters(
-            {"Account": venue_client_account, "ClOrdID": ord_id, "ExDestination": self.mic})
+            {"Account": venue_client_account, "ClOrdID": ord_id})
         self.fix_message.remove_fields_from_component("Instrument", ["SecurityDesc"])
         ignored_fields = ["TransactTime", "Parties", "SettlDate"]
         self.fix_verifier.check_fix_message_fix_standard(self.fix_message, ["ClOrdID"], ignored_fields=ignored_fields)
-
-    @try_except(test_id=Path(__file__).name[:-3])
-    def run_post_conditions(self):
-        self.ssh_client.send_command("sql")
-        self.ssh_client.send_command(f"UPDATE exchangecode SET bloombergexchcode=null WHERE mic='{self.mic}';")
-        self.ssh_client.send_command("exit")
-        self.ssh_client.send_command("qrestart AQS ORS ESBUYTH2TEST")
-        time.sleep(70)
--- test_cases/eq/DMA/QAP_T7076.py
+++ test_cases/eq/DMA/QAP_T7076.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from pathlib import Path
 
 from custom import basic_custom_actions as bca
@@ -6,6 +7,7 @@ from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.ssh_wrappers.ssh_client import SshClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -23,9 +25,21 @@ class QAP_T7076(TestCase):
         self.instrument_1 = self.data_set.get_fix_instrument_by_name("instrument_1")
         self.mic = self.data_set.get_mic_by_name('mic_1')  # XPAR
         self.mic_blm = self.data_set.get_mic_by_name('mic_1_blm')  # XPAR_BLM
+        self.ssh_client_env = self.environment.get_list_ssh_client_environment()[0]
+        self.ssh_client = SshClient(self.ssh_client_env.host, self.ssh_client_env.port, self.ssh_client_env.user,
+                                    self.ssh_client_env.password, self.ssh_client_env.su_user,
+                                    self.ssh_client_env.su_password)
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
+        # region Precondition
+        self.ssh_client.send_command("sql")
+        self.ssh_client.send_command(
+            f"UPDATE exchangecode SET bloombergexchcode='{self.mic_blm}' WHERE mic='{self.mic}';")
+        self.ssh_client.send_command("exit")
+        self.ssh_client.send_command("qrestart AQS ORS ESBUYTH2TEST")
+        time.sleep(70)
+        # endregion
         self.fix_message.remove_parameter("ExDestination")
         self.fix_message.update_fields_in_component("Instrument", {"SecurityExchange": self.mic_blm})
         self.fix_manager.send_message_and_receive_response_fix_standard(self.fix_message)
@@ -33,3 +47,11 @@ class QAP_T7076(TestCase):
         self.fix_manager.compare_values({"OrdStatus": "A"}, exec_rep, "Check Status")
         self.fix_message.update_fields_in_component("Instrument", {"SecurityExchange": self.mic})
         self.fix_manager.compare_values(self.instrument_1, exec_rep["Instrument"], "Check instrument")
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.ssh_client.send_command("sql")
+        self.ssh_client.send_command(f"UPDATE exchangecode SET bloombergexchcode=null WHERE mic='{self.mic}';")
+        self.ssh_client.send_command("exit")
+        self.ssh_client.send_command("qrestart AQS ORS ESBUYTH2TEST")
+        time.sleep(70)
