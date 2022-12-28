04267f702c3db99f4f06b805c7e9081e16ef8ec9
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2022-12-28 15:51:37 +0200

    QAP-T4353 - add test

--- test_cases/algo/Algo_Redburn/Algo_MOC/QAP_T4353.py
+++ test_cases/algo/Algo_Redburn/Algo_MOC/QAP_T4353.py
@@ -21,7 +21,7 @@ from test_framework.core.test_case import TestCase
 from test_framework.rest_api_wrappers.algo.RestApiStrategyManager import RestApiAlgoManager
 
 
-class QAP_T4368(TestCase):
+class QAP_T4353(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, data_set=None, environment=None):
         super().__init__(report_id=report_id, data_set=data_set, environment=environment)
@@ -44,7 +44,7 @@ class QAP_T4368(TestCase):
         self.indicative_volume = 2000
         self.percentage = 10
         self.child_qty = AFM.get_child_qty_for_auction(self.indicative_volume, self.percentage, self.qty)
-        self.max_would_percentage = 0
+        self.max_would_shares = 0
         self.price = 30
         self.would_price = 29
 
@@ -106,7 +106,7 @@ class QAP_T4368(TestCase):
         # region Update Trading Phase
 
         self.rest_api_manager.set_case_id(case_id=bca.create_event("Modify trading phase profile", self.test_id))
-        trading_phases = AFM.get_timestamps_for_current_phase(TradingPhases.PreOpen)
+        trading_phases = AFM.get_timestamps_for_current_phase(TradingPhases.PreClosed)
         self.rest_api_manager.modify_trading_phase_profile(self.trading_phase_profile, trading_phases)
         time.sleep(3)
         # end region
@@ -122,21 +122,21 @@ class QAP_T4368(TestCase):
         case_id_1 = bca.create_event("Create Auction Order", self.test_id)
         self.fix_verifier_sell.set_case_id(case_id_1)
 
-        self.auction_algo = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_MOO_params()
+        self.auction_algo = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_MOC_params()
         self.auction_algo.add_ClordId((os.path.basename(__file__)[:-3]))
         self.auction_algo.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument, ExDestination=self.mic))
-        self.auction_algo.update_fields_in_component("QuodFlatParameters", dict(MaxParticipation=self.percentage, WouldInAuction=1, TriggerPriceRed=self.would_price, MaxWouldPercentage=self.max_would_percentage))
+        self.auction_algo.update_fields_in_component("QuodFlatParameters", dict(MaxParticipation=self.percentage, WouldInAuction=1, TriggerPriceRed=self.would_price, MaxWouldShares=self.max_would_shares))
         responce = self.fix_manager_sell.send_message_and_receive_response(self.auction_algo, case_id_1)[0]
 
         # region Check Sell side
         self.fix_verifier_sell.check_fix_message(self.auction_algo, key_parameters=self.key_params_NOS_parent, direction=self.ToQuod, message_name='Sell side NewOrderSingle')
 
         er_pending_new = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.auction_algo, self.gateway_side_sell, self.status_pending)
-        er_pending_new.remove_parameters(['Account', 'SettlDate', 'TargetStrategy']).change_parameters(dict(NoStrategyParameters='*', TimeInForce=2, NoParty='*', SecAltIDGrp='*'))
+        er_pending_new.remove_parameters(['Account', 'SettlDate', 'TargetStrategy']).change_parameters(dict(NoStrategyParameters='*', TimeInForce=7, NoParty='*', SecAltIDGrp='*'))
         self.fix_verifier_sell.check_fix_message(er_pending_new, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport PendingNew')
 
         er_new = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.auction_algo, self.gateway_side_sell, self.status_new)
-        er_new.change_parameters(dict(TimeInForce=2, NoParty='*', SecAltIDGrp='*')).remove_parameter('TargetStrategy')
+        er_new.change_parameters(dict(TimeInForce=7, NoParty='*', SecAltIDGrp='*')).remove_parameter('TargetStrategy')
         self.fix_verifier_sell.check_fix_message(er_new, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport New')
         # endregion
 
@@ -160,7 +160,7 @@ class QAP_T4368(TestCase):
 
         # region check Would part
 
-        end_time = AFM.get_timestamp_from_list(phases=trading_phases, phase=TradingPhases.PreOpen, start_time=False) / 1000
+        end_time = AFM.get_timestamp_from_list(phases=trading_phases, phase=TradingPhases.PreClosed, start_time=False) / 1000
 
         self.checkpoint = end_time + 5
         scheduler = sched.scheduler(time.time, time.sleep)
@@ -184,7 +184,7 @@ class QAP_T4368(TestCase):
         time.sleep(3)
 
         er_cancel_auction_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.auction_algo, self.gateway_side_sell, self.status_cancel)
-        er_cancel_auction_order.add_tag(dict(SettlDate='*')).add_tag(dict(NoParty='*', SecAltIDGrp='*')).change_parameters(dict(TimeInForce=2)).remove_parameters(["CxlQty", 'TargetStrategy'])
+        er_cancel_auction_order.add_tag(dict(SettlDate='*')).add_tag(dict(NoParty='*', SecAltIDGrp='*')).change_parameters(dict(TimeInForce=7)).remove_parameters(["CxlQty", 'TargetStrategy'])
         self.fix_verifier_sell.check_fix_message(er_cancel_auction_order, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport Cancel')
 
 
