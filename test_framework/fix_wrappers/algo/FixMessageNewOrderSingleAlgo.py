444c63e437612b63bf3eff4183afe81af70e4ce7
Yehor Yutkin <yehor.yutkin.contractor@quodfinancial.com>
2023-01-16 15:14:57 +0200

    FixMessageNewOrderSingleAlgo.py Added set_Stop_params function

--- test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
+++ test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
@@ -1452,3 +1452,34 @@ class FixMessageNewOrderSingleAlgo(FixMessageNewOrderSingle):
         }
         super().change_parameters(base_parameters)
         return self
+
+    def set_Stop_params(self):
+        base_parameters = {
+            'Account': "CLIENT2",
+            'ClOrdID': '*',
+            'HandlInst': "2",
+            'Side': '1',
+            'OrderQty': '1000',
+            'TimeInForce': "0",
+            'StopPx': "20",
+            'OrdType': 3,
+            'TransactTime': datetime.utcnow().isoformat(),
+            'Instrument': self.get_data_set().get_fix_instrument_by_name('instrument_2'),
+            'OrderCapacity': 'A',
+            'Currency': 'EUR',
+            'TargetStrategy': '1001',
+            'NoStrategyParameters': [
+                {
+                    'StrategyParameterName': 'AvailableVenues',
+                    'StrategyParameterType': '13',
+                    'StrategyParameterValue': 'true'
+                },
+                {
+                    'StrategyParameterName': 'AllowMissingPrimary',
+                    'StrategyParameterType': '13',
+                    'StrategyParameterValue': 'true'
+                },
+            ]
+        }
+        super().change_parameters(base_parameters)
+        return self
