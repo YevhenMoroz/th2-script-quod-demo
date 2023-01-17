ad79f5548096aa80c3be0f7e7cb1aee48be321f0
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2023-01-17 16:35:05 +0200

    refactor QAP_T7342

--- test_cases/eq/Care/QAP_T7342.py
+++ test_cases/eq/Care/QAP_T7342.py
@@ -1,12 +1,12 @@
 import logging
 from pathlib import Path
+
+from custom import basic_custom_actions as bca
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
-from custom import basic_custom_actions as bca
-from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
-from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
+from test_framework.data_sets.message_types import ORSMessageType
+from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.oms.ors_messges.FixNewOrderSingleOMS import FixNewOrderSingleOMS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -21,20 +21,18 @@ class QAP_T7342(TestCase):
         super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
         self.fix_env = self.environment.get_list_fix_environment()[0]
-        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
-        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
+        self.java_api_connectivity = self.java_api = self.environment.get_list_java_api_environment()[0].java_api_conn
+        self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
         self.dummy_client = self.data_set.get_client_by_name('client_dummy')
-        self.fix_message.change_parameter('Account', self.dummy_client)
-        self.order_book = OMSOrderBook(self.test_id, self.session_id)
+        self.nos = FixNewOrderSingleOMS(self.data_set).set_default_care_limit()
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # region Declaration
         # region create CO order
-        self.fix_manager.send_message_and_receive_response_fix_standard(self.fix_message)
-        order_id = self.order_book.extract_field(OrderBookColumns.order_id.value)
-        # endregion
-        # region check fields
-        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
-            {OrderBookColumns.sts.value: ExecSts.held.value, OrderBookColumns.cl_acc_gr_id.value: self.dummy_client})
+        self.nos.update_fields_in_component("NewOrderSingleBlock", {"ClientAccountGroupID": self.dummy_client})
+        self.java_api_manager.send_message_and_receive_response(self.nos)
+        ord_not = self.java_api_manager.get_last_message(ORSMessageType.OrdNotification.value).get_parameters()[
+            "OrdNotificationBlock"]
+        exp_res = {"ClientAccountGroupID": self.dummy_client, "OrdStatus": "HLD"}
+        self.java_api_manager.compare_values(exp_res, ord_not, "Check Client and Sts")
         # endregion
