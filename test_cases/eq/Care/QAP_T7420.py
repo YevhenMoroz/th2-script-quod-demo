2b0feb9073b0142bd673507898bbf58c67987340
skomanov <sofia.komanova.contractor@quodfinancial.com>
2023-01-18 16:38:27 +0100

    modified QAP_T7420

--- test_cases/eq/Care/QAP_T7420.py
+++ test_cases/eq/Care/QAP_T7420.py
@@ -46,7 +46,6 @@ class QAP_T7420(TestCase):
     def run_pre_conditions_and_steps(self):
 
         # region create  DMA order
-        dma_order_id = None
         trade_rule = None
         nos_rule = None
         exec_id = None
