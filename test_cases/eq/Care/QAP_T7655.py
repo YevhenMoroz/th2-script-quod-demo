6ab5ee5184ce6dd77ab43bbc4fd719c8d2d8f4b0
Mark Antonov <mark.antonov.contractor@quodfinancial.com>
2023-01-25 11:33:13 +0200

    QAP-T7655 automated

--- test_cases/eq/Care/QAP_T7655.py
+++ test_cases/eq/Care/QAP_T7655.py
@@ -1,71 +1,135 @@
 import logging
+from datetime import datetime
 from pathlib import Path
+
+from custom import basic_custom_actions as bca
+from custom.basic_custom_actions import timestamps
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
-from custom import basic_custom_actions as bca
-from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
-from test_framework.fix_wrappers.oms.FixMessageOrderCancelRequestOMS import FixMessageOrderCancelRequestOMS
-from test_framework.win_gui_wrappers.fe_trading_constant import OrderBookColumns, ExecSts
-from test_framework.win_gui_wrappers.oms.oms_client_inbox import OMSClientInbox
-from test_framework.win_gui_wrappers.oms.oms_order_book import OMSOrderBook
+from test_framework.data_sets.message_types import ORSMessageType, CSMessageType
+from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.cs_message.CDOrdAckBatchRequest import CDOrdAckBatchRequest
+from test_framework.java_api_wrappers.java_api_constants import OrderReplyConst, JavaApiFields, ExecutionReportConst
+from test_framework.java_api_wrappers.oms.ors_messges.FixNewOrderSingleOMS import FixNewOrderSingleOMS
+from test_framework.java_api_wrappers.oms.ors_messges.FixOrderCancelRequestOMS import FixOrderCancelRequestOMS
+from test_framework.java_api_wrappers.oms.ors_messges.TradeEntryOMS import TradeEntryOMS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-timeouts = True
+
+seconds, nanos = timestamps()  # Test case start time
 
 
-@try_except(test_id=Path(__file__).name[:-3])
+def print_message(message, responses):
+    logger.info(message)
+    for i in responses:
+        logger.info(i)
+        logger.info(i.get_parameters())
+
+
 class QAP_T7655(TestCase):
-
     @try_except(test_id=Path(__file__).name[:-3])
-    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+    def __init__(self, report_id, session_id, data_set, environment):
         super().__init__(report_id, session_id, data_set, environment)
+        # region Declarations
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
-        self.fix_env = self.environment.get_list_fix_environment()[0]
-        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
-        self.fix_message = FixMessageNewOrderSingleOMS(self.data_set).set_default_care_limit()
-        self.qty = self.fix_message.get_parameter('OrderQtyData')['OrderQty']
-        self.order_book = OMSOrderBook(self.test_id, self.session_id)
-        self.client_inbox = OMSClientInbox(self.test_id, self.session_id)
+        self.qty = "100"
+        self.price = "20"
+        self.client = self.data_set.get_client("client_2")  # CLIENT2
+        self.java_api_connectivity = self.environment.get_list_java_api_environment()[0].java_api_conn
+        self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
+        self.accept_request = CDOrdAckBatchRequest()
+        self.nos = FixNewOrderSingleOMS(self.data_set)
+        self.cancel_request = FixOrderCancelRequestOMS(self.data_set)
+        self.java_api_connectivity2 = self.environment.get_list_java_api_environment()[0].java_api_conn_user2
+        self.java_api_manager2 = JavaApiManager(self.java_api_connectivity2, self.test_id)
+        self.trd_request = TradeEntryOMS(self.data_set)
+        # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        # region Declaration
-        # region create CO order via Fix
-        response = self.fix_manager.send_message_and_receive_response_fix_standard(self.fix_message)
-        order_id = response[0].get_parameters()['OrderID']
+        # region Precondition - Create CO order
+        self.nos.set_default_care_limit()
+        self.nos.update_fields_in_component("NewOrderSingleBlock", {"ClientAccountGroupID": self.client})
+        responses = self.java_api_manager2.send_message_and_receive_response(self.nos)
+        print_message("Create CO", responses)
+        cd_order_notif_message = self.java_api_manager2.get_last_message(CSMessageType.CDOrdNotif.value)
+        cd_order_notif_id = cd_order_notif_message.get_parameter("CDOrdNotifBlock")["CDOrdNotifID"]
+        order_notif_message = self.java_api_manager2.get_last_message(
+            ORSMessageType.OrdNotification.value
+        ).get_parameters()[JavaApiFields.OrderNotificationBlock.value]
+        ord_id = order_notif_message["OrdID"]
+        cl_ord_id = order_notif_message["ClOrdID"]
+        desk_id = order_notif_message["RecipientDeskID"]
+        self.java_api_manager2.compare_values(
+            {JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_SEN.value},
+            order_notif_message,
+            "Step 6 - Comparing Status of Care order",
+        )
         # endregion
-        # region accept CO order
-        self.client_inbox.accept_order()
+
+        # region Accept CO order in Client Inbox
+        self.accept_request.set_default(ord_id, cd_order_notif_id, desk_id)
+        responses = self.java_api_manager.send_message_and_receive_response(self.accept_request)
+        print_message("Accept order", responses)
+        order_reply = self.java_api_manager.get_last_message(ORSMessageType.OrdReply.value).get_parameters()[
+            JavaApiFields.OrdReplyBlock.value
+        ]
+        self.java_api_manager.compare_values(
+            {JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_OPN.value},
+            order_reply,
+            "Comparing Status of Care order after Accept",
+        )
         # endregion
-        # region check order open status
-        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
-            {OrderBookColumns.sts.value: ExecSts.open.value})
+
+        # region Trade Care order
+        self.trd_request.set_default_trade(ord_id, self.price, self.qty)
+        responses = self.java_api_manager.send_message_and_receive_response(self.trd_request)
+        print_message("Trade order", responses)
+        exec_rep = self.java_api_manager.get_last_message(ORSMessageType.ExecutionReport.value).get_parameters()[
+            JavaApiFields.ExecutionReportBlock.value
+        ]
+        self.java_api_manager.compare_values(
+            {
+                JavaApiFields.TransExecStatus.value: ExecutionReportConst.TransExecStatus_FIL.value,
+                JavaApiFields.TransStatus.value: "OPN",
+            },
+            exec_rep,
+            "Comparing Statuses after Trade Care order",
+        )
         # endregion
-        # region manual execution
-        self.order_book.manual_execution(qty=self.qty)
+
+        # region Step 1-2 - Send cancel request
+        self.cancel_request.set_default_cancel(cl_ord_id)
+        self.java_api_manager2.send_message_and_receive_response(self.cancel_request)
+        cd_order_notif_message = self.java_api_manager2.get_last_message(CSMessageType.CDOrdNotif.value)
+        cd_order_notif_id = cd_order_notif_message.get_parameter("CDOrdNotifBlock")["CDOrdNotifID"]
         # endregion
-        # region check order partially filled status
-        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
-            {OrderBookColumns.exec_sts.value: ExecSts.filled.value})
+
+        # region Step 3 - Trying to cancel a filled order
+        self.accept_request.set_default(ord_id, cd_order_notif_id, desk_id, "C")
+        responses = self.java_api_manager.send_message_and_receive_response(self.accept_request)
+        print_message("Trying to Cancel order", responses)
+        cd_ord_ack_batch_reply = self.java_api_manager.get_last_message(
+            CSMessageType.CDOrdAckBatchReply.value
+        ).get_parameters()["CDOrdAckBatchReplyBlock"]
+        # self.java_api_manager.compare_values(
+        #     {"ErrorMsg": f"Invalid quantity :  The LeavesQty=0.0 for TransID={ord_id}"}, cd_ord_ack_batch_reply,
+        #     "Check Trying to cancel")
         # endregion
-        # region cancel order by Fix
-        fix_message_cancel = FixMessageOrderCancelRequestOMS()
-        fix_message_cancel.set_default(self.fix_message)
-        self.fix_manager.send_message_fix_standard(fix_message_cancel)
+
+        # region Step 4 - Reject cancel request
+        self.accept_request.set_default(ord_id, cd_order_notif_id, desk_id, "C", True)
+        responses = self.java_api_manager.send_message_and_receive_response(self.accept_request)
+        print_message("Cancel order", responses)
+        order_reply = self.java_api_manager.get_last_message(ORSMessageType.OrdReply.value, ord_id).get_parameters()[
+            JavaApiFields.OrdReplyBlock.value
+        ]
+        self.java_api_manager.compare_values(
+            {JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_OPN.value},
+            order_reply,
+            "Step 4 - Checking that Cancel Request is rejected",
+        )
         # endregion
-        # region accept cancel
-        self.client_inbox.accept_and_cancel_children()
-        # endregion
-        # region check open status
-        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
-            {OrderBookColumns.sts.value: ExecSts.open.value})
-        # endregion
-        # region accept cancel
-        self.client_inbox.reject_order()
-        # endregion
-        # region check open status
-        self.order_book.set_filter([OrderBookColumns.order_id.value, order_id]).check_order_fields_list(
-            {OrderBookColumns.sts.value: ExecSts.open.value})
-        # endregion
+
+        logger.info(f"Case {self.test_id} was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
