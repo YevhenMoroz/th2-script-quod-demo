6a22141ab58bb3f224d4fd49b0b3869e245d0eb2
Mark Antonov <mark.antonov.contractor@quodfinancial.com>
2023-01-25 11:30:27 +0200

    added CDOrdAckBatchReply to class CSMessageType

--- test_framework/data_sets/message_types.py
+++ test_framework/data_sets/message_types.py
@@ -189,6 +189,7 @@ class CSMessageType(Enum):
     CDOrdNotif = "Order_CDOrdNotif"
     ManualMatchExecToParentOrdersRequest = "Order_ManualMatchExecToParentOrdersRequest"
     ManualMatchExecToParentOrdersReply = "Order_ManualMatchExecToParentOrdersReply"
+    CDOrdAckBatchReply = "Order_CDOrdAckBatchReply"
 
 
 class MDAMessageType(Enum):
