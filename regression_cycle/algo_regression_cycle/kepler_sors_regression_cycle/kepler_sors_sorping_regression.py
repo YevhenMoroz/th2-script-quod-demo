b9491c59bf9ffc4f5c25febfea31f41d7f868b88
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2023-01-27 14:32:38 +0100

    QAP-T4957 Automated

--- regression_cycle/algo_regression_cycle/kepler_sors_regression_cycle/kepler_sors_sorping_regression.py
+++ regression_cycle/algo_regression_cycle/kepler_sors_regression_cycle/kepler_sors_sorping_regression.py
@@ -2,6 +2,7 @@ import logging
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 from test_cases.algo.Algo_Kepler.Algo_SORPING.QAP_T10393 import QAP_T10393
+from test_cases.algo.Algo_Kepler.Algo_SORPING.QAP_T4957 import QAP_T4957
 from test_cases.algo.Algo_Kepler.Algo_SORPING.QAP_T4959 import QAP_T4959
 from test_cases.algo.Algo_Kepler.Algo_SORPING.QAP_T9011 import QAP_T9011
 from test_cases.algo.Algo_Kepler.Algo_SORPING.QAP_T9274 import QAP_T9274
@@ -122,6 +123,7 @@ def test_run(parent_id=None, version=None):
         QAP_T4940(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4941(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4944(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T4957(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4959(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4963(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4966(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
