8843cf726373dadf43ff2edbb0166f30ebd14563
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2023-02-08 16:26:08 +0200

    QAP-8975 - add new test QAP_T4494

--- regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_auction_scaling_regression.py
+++ regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_auction_scaling_regression.py
@@ -16,6 +16,7 @@ from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4508 import QAP_T450
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4509 import QAP_T4509
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T10488 import QAP_T10488
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T10489 import QAP_T10489
+from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4494 import QAP_T4494
 from test_framework.configurations.component_configuration import ComponentConfigurationAlgo
 
 
@@ -37,6 +38,7 @@ def test_run(parent_id=None, version=None):
         QAP_T4495(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4508(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4509(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T4494(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         # endregion
 
         QAP_T10278(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
