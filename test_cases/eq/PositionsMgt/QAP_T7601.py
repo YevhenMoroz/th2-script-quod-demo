46c42787ab7aad11e3ac7cabc5436b6cb1b827c7
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2023-02-15 14:46:52 +0200

    add QAP_T7601

--- test_cases/eq/PositionsMgt/QAP_T7601.py
+++ test_cases/eq/PositionsMgt/QAP_T7601.py
@@ -9,6 +9,7 @@ from test_framework.core.try_exept_decorator import try_except
 from test_framework.data_sets.message_types import ORSMessageType, PKSMessageType
 from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
 from test_framework.java_api_wrappers.java_api_constants import SubmitRequestConst, JavaApiFields
+from test_framework.java_api_wrappers.oms.es_messages.ExecutionReportOMS import ExecutionReportOMS
 from test_framework.java_api_wrappers.oms.ors_messges.AllocationInstructionOMS import AllocationInstructionOMS
 from test_framework.java_api_wrappers.oms.ors_messges.DFDManagementBatchOMS import DFDManagementBatchOMS
 from test_framework.java_api_wrappers.oms.ors_messges.OrderSubmitOMS import OrderSubmitOMS
@@ -22,19 +23,17 @@ timeouts = True
 
 
 @try_except(test_id=Path(__file__).name[:-3])
-class QAP_T7600(TestCase):
+class QAP_T7601(TestCase):
     def __init__(self, report_id, session_id=None, data_set=None, environment=None):
         super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
         self.ja_manager = JavaApiManager(environment.get_list_java_api_environment()[0].java_api_conn, self.test_id)
         self.client = self.data_set.get_client_by_name("client_pos_1")
-        self.washbook_acc = self.data_set.get_washbook_account_by_name('washbook_account_2')
+        self.washbook_acc = self.data_set.get_washbook_account_by_name('washbook_account_1')
         self.recipient = self.data_set.get_recipient_by_name("recipient_user_3")
-        self.order_submit = OrderSubmitOMS(data_set).set_default_care_limit(
-            self.environment.get_list_fe_environment()[0].user_1, "1", SubmitRequestConst.USER_ROLE_1.value)
-        self.order_submit2 = OrderSubmitOMS(data_set).set_default_care_limit(
-            self.environment.get_list_fe_environment()[0].user_1, "1", SubmitRequestConst.USER_ROLE_1.value)
-        self.trd_req = TradeEntryOMS(data_set)
+        self.order_submit = OrderSubmitOMS(data_set).set_default_dma_limit()
+        self.order_submit2 = OrderSubmitOMS(data_set).set_default_dma_limit()
+        self.trd_req = ExecutionReportOMS(data_set)
         self.dfd_manage_batch = DFDManagementBatchOMS(self.data_set)
         self.allocation_instruction = AllocationInstructionOMS(self.data_set)
 
@@ -43,8 +42,8 @@ class QAP_T7600(TestCase):
         # region Step 1
         self.order_submit.update_fields_in_component("NewOrderSingleBlock", {"AccountGroupID": self.client})
         self.ja_manager.send_message_and_receive_response(self.order_submit)
-        ord_rep = self.ja_manager.get_last_message(ORSMessageType.OrdNotification.value).get_parameters()[
-            JavaApiFields.OrderNotificationBlock.value]
+        ord_rep = self.ja_manager.get_last_message(ORSMessageType.OrdReply.value).get_parameters()[
+            JavaApiFields.OrdReplyBlock.value]
         ord_id = ord_rep["OrdID"]
         self.ja_manager.compare_values({"WashBookAccountID": self.washbook_acc}, ord_rep, "Check WashBookAccountID")
         # endregion
