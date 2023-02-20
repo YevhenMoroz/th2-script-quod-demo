acb39baab92d325b28e4c71a0dc1ef32efc3c596
Yehor Yutkin <yehor.yutkin.contractor@quodfinancial.com>
2023-02-20 16:07:10 +0200

    QAP-T4140, QAP-T4183, QAP-T4191, QAP-T4280, QAP-T4917, QAP-T4918, QAP-T4919, QAP-T4925 - Removed remove_parameter..['SecondaryAlgoPolicyID', 'NoStrategyParameters'])

--- test_cases/algo/Algo_Iceberg/QAP_T4183.py
+++ test_cases/algo/Algo_Iceberg/QAP_T4183.py
@@ -159,7 +159,6 @@ class QAP_T4183(TestCase):
         self.fix_verifier_sell.check_fix_message(cancel_request_iceberg_order, direction=self.ToQuod, message_name='Sell side Cancel Request')
 
         cancel_iceberg_order_params = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.iceberg_order, self.gateway_side_sell, self.status_cancel)
-        cancel_iceberg_order_params.remove_parameters(['NoStrategyParameters', 'SecondaryAlgoPolicyID'])
         self.fix_verifier_sell.check_fix_message(cancel_iceberg_order_params, key_parameters=self.key_params, message_name='Sell side ExecReport Cancel')
         # endregion
 
