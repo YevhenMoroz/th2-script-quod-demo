13b01f50f76769d628cfec49203959826fc9b8aa
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-02-23 16:57:41 +0100

    Add cancel for synergy

--- test_framework/fix_wrappers/forex/FixMessageQuoteCancel.py
+++ test_framework/fix_wrappers/forex/FixMessageQuoteCancel.py
@@ -2,6 +2,7 @@ from test_framework.data_sets.message_types import FIXMessageType, ORSMessageTyp
 from test_framework.fix_wrappers.FixMessage import FixMessage
 from test_framework.fix_wrappers.forex.FixMessageQuoteFX import FixMessageQuoteFX
 from test_framework.fix_wrappers.forex.FixMessageQuoteRequestFX import FixMessageQuoteRequestFX
+from test_framework.fix_wrappers.forex.FixMessageQuoteSynergyFX import FixMessageQuoteSynergyFX
 from test_framework.java_api_wrappers.fx.FixQuoteRequestFX import FixQuoteRequestFX
 
 
@@ -23,7 +24,8 @@ class FixMessageQuoteCancelFX(FixMessage):
         elif quote_request.get_message_type() is java_api_request:
             temp = dict(
                 QuoteReqID=quote_request.get_parameter("QuoteRequestBlock")["ClientQuoteReqID"],
-                QuoteID=quote_request.get_parameter("QuoteRequestBlock")["QuoteReqList"]["QuoteReqBlock"][0]["LiveQuoteID"],
+                QuoteID=quote_request.get_parameter("QuoteRequestBlock")["QuoteReqList"]["QuoteReqBlock"][0][
+                    "LiveQuoteID"],
                 QuoteCancelType="5"
             )
         else:
@@ -61,6 +63,16 @@ class FixMessageQuoteCancelFX(FixMessage):
         temp = dict(
             QuoteReqID=quote_request.get_parameter("QuoteReqID"),
             QuoteID="*",
+            QuoteCancelType="5",
+            VenueType=quote_request.get_parameter("VenueType"),
+        )
+        super().change_parameters(temp)
+        return self
+
+    def set_params_for_cancel_synergy(self, quote_request: FixMessageQuoteRequestFX, quote: FixMessageQuoteSynergyFX):
+        temp = dict(
+            QuoteReqID=quote_request.get_parameter("QuoteReqID"),
+            QuoteID=quote.get_parameter("QuoteID"),
             QuoteCancelType="5",
             VenueType=quote_request.get_parameter("VenueType"),
         )
