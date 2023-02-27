9bd4b310bd04a6e784c5d61a481f54bf05b2da08
Yehor Yutkin <yehor.yutkin.contractor@quodfinancial.com>
2023-02-27 11:21:23 +0200

    ssh_client_environment.py Added variables for ssh on quod310@columbia

--- test_framework/environments/ssh_client_environment.py
+++ test_framework/environments/ssh_client_environment.py
@@ -31,6 +31,19 @@ class SshClientEnvironment(BaseEnvironment):
                 )
                 SshClientEnvironment.environment_instances.update({EnvironmentType.quod317_ssh_client.value: site_environment})
             return SshClientEnvironment.environment_instances[EnvironmentType.quod317_ssh_client.value]
+        elif env.value == EnvironmentType.quod310_ssh_client.value:
+            if EnvironmentType.quod310_ssh_client.value not in SshClientEnvironment.environment_instances.keys():
+                site_environment = SshClientEnvironment(
+                    environment_type=EnvironmentType.quod310_fe.value,
+                    host=SshClientEnv.HOST_310.value,
+                    port=SshClientEnv.PORT_310.value,
+                    user=SshClientEnv.USER_310.value,
+                    password=SshClientEnv.PASSWORD_310.value,
+                    su_user=SshClientEnv.SU_USER_310.value,
+                    su_password=SshClientEnv.SU_PASSWORD_310.value,
+                )
+                SshClientEnvironment.environment_instances.update({EnvironmentType.quod310_ssh_client.value: site_environment})
+            return SshClientEnvironment.environment_instances[EnvironmentType.quod310_ssh_client.value]
         else:
             raise Exception('No such environment')
 
