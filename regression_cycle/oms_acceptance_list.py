5f1f12b8087429fdc27a6e8fa10b935b6bf480d2
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2023-02-28 20:50:35 +0200

    update oms_acceptance_list.py

--- regression_cycle/oms_acceptance_list.py
+++ regression_cycle/oms_acceptance_list.py
@@ -3,31 +3,28 @@ import os
 from datetime import datetime
 from pathlib import Path
 
+
 from custom import basic_custom_actions as bca
 from custom.basic_custom_actions import timestamps
 from stubs import Stubs
 from test_cases.eq.ArchiveWindows.QAP_T8159 import QAP_T8159
-from test_cases.eq.ArchiveWindows.QAP_T7541 import QAP_T7541
 from test_cases.eq.Bag.QAP_T7634 import QAP_T7634
-from test_cases.eq.Basket.QAP_T7453 import QAP_T7453
-from test_cases.eq.Basket.QAP_T7447 import QAP_T7447
 from test_cases.eq.Basket.QAP_T7433 import QAP_T7433
-from test_cases.eq.Care.QAP_T7689 import QAP_T7689
+from test_cases.eq.Basket.QAP_T7453 import QAP_T7453
 from test_cases.eq.Care.QAP_T7626 import QAP_T7626
-from test_cases.eq.Commissions.QAP_T7534 import QAP_T7534
+from test_cases.eq.Care.QAP_T7689 import QAP_T7689
 from test_cases.eq.Commissions.QAP_T7497 import QAP_T7497
+from test_cases.eq.Commissions.QAP_T7534 import QAP_T7534
 from test_cases.eq.DMA.QAP_T7549 import QAP_T7549
-from test_cases.eq.DMA.QAP_T7615 import QAP_T7615
 from test_cases.eq.DMA.QAP_T7610 import QAP_T7610
+from test_cases.eq.DMA.QAP_T7615 import QAP_T7615
 from test_cases.eq.Gateway.QAP_T7507 import QAP_T7507
 from test_cases.eq.GatingRules.QAP_T4928 import QAP_T4928
 from test_cases.eq.GatingRules.QAP_T4929 import QAP_T4929
 from test_cases.eq.GatingRules.QAP_T4930 import QAP_T4930
 from test_cases.eq.GatingRules.QAP_T4931 import QAP_T4931
-from test_cases.eq.Position.QAP_T7598 import QAP_T7598
-from test_cases.eq.PositionsMgt.QAP_T7606 import QAP_T7606
-from test_cases.eq.PostTrade.QAP_T7552 import QAP_T7552
 from test_cases.eq.PostTrade.QAP_T7517 import QAP_T7517
+from test_cases.eq.PostTrade.QAP_T7552 import QAP_T7552
 from test_framework.configurations.component_configuration import ComponentConfiguration
 from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
 from win_gui_modules.utils import set_session_id
@@ -52,8 +49,6 @@ def test_run(parent_id=None, version=None):
     layout_name = "all_columns_layout.xml"
 
     try:
-        base_main_window.open_fe(report_id=report_id, fe_env=fe_env, user_num=1)
-        # base_main_window.import_layout(layout_path, layout_name)
         QAP_T4930(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
             .execute()
         QAP_T4931(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
@@ -102,10 +97,10 @@ def test_run(parent_id=None, version=None):
             .execute()
         QAP_T7433(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
             .execute()
-        QAP_T7598(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
-            .execute()
-        QAP_T7606(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
-            .execute()
+        # QAP_T7598(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
+        #     .execute()
+        # QAP_T7606(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
+        #     .execute()
         # QAP_T7447(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
         #     .execute()
         QAP_T7634(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
