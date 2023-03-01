78446ebb0c0fc6ad08ff97002f329499d9c58d40
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2023-03-01 18:09:24 +0100

    QAP-8945 - add os.getcwd()

--- regression_cycle/algo_regression_cycle/kepler_sors_regression_cycle/kepler_sors_regression.py
+++ regression_cycle/algo_regression_cycle/kepler_sors_regression_cycle/kepler_sors_regression.py
@@ -1,4 +1,6 @@
 from xml.etree import ElementTree
+import os
+print(os.getcwd())
 from regression_cycle.algo_regression_cycle.kepler_sors_regression_cycle import kepler_sors_iceberg_regression, kepler_sors_sorping_regression, kepler_sors_synthminqty_regression, kepler_sors_mpdark_dark_phase_regression, kepler_sors_mpdark_LIS_dark_phase_regression, \
     kepler_sors_mpdark_other_regression, kepler_sors_multiple_emulation_regression, kepler_iceberg_check_party_info, kepler_iceberg_multiday_phase, kepler_custom_tags, kepler_synthetic_tif, kepler_multiple_emulation_additional, kepler_multilisting, kepler_iceberg_modify, \
     kepler_instrument_identification, kepler_mic_identification, kepler_sors_mpdark_round_robin
