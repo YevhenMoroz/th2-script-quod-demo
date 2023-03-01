c47db466588d23aa2b7e11dee4ac21f49951b2e2
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2023-03-01 09:14:38 +0200

    modify regressions files

--- regression_cycle/oms_acceptance_list.py
+++ regression_cycle/oms_acceptance_list.py
@@ -1,8 +1,5 @@
 import logging
-import os
 from datetime import datetime
-from pathlib import Path
-
 
 from custom import basic_custom_actions as bca
 from custom.basic_custom_actions import timestamps
@@ -26,8 +23,6 @@ from test_cases.eq.GatingRules.QAP_T4931 import QAP_T4931
 from test_cases.eq.PostTrade.QAP_T7517 import QAP_T7517
 from test_cases.eq.PostTrade.QAP_T7552 import QAP_T7552
 from test_framework.configurations.component_configuration import ComponentConfiguration
-from test_framework.win_gui_wrappers.base_main_window import BaseMainWindow
-from win_gui_modules.utils import set_session_id
 
 logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -37,16 +32,14 @@ channels = dict()
 
 
 def test_run(parent_id=None, version=None):
-    report_id = bca.create_event(f"PEQ Acceptance v.180" if version is None else f"PEQ Acceptance v.180 | {version}", parent_id)
+    report_id = bca.create_event(f"PEQ Acceptance v.188" if version is None else f"PEQ Acceptance v.188 | {version}", parent_id)
     seconds, nanos = timestamps()  # Store case start time
     configuration = ComponentConfiguration("AcceptanceList")
     data_set = configuration.data_set
     fe_env = configuration.environment.get_list_fe_environment()[0]
-    session_id = set_session_id(fe_env.target_server_win)
-    test_id = bca.create_event(Path(__file__).name[:-3], report_id)
-    base_main_window = BaseMainWindow(test_id, session_id)
-    layout_path = os.path.abspath("layouts")
-    layout_name = "all_columns_layout.xml"
+    session_id = None #set_session_id(fe_env.target_server_win)
+    # test_id = bca.create_event(Path(__file__).name[:-3], report_id)
+
 
     try:
         QAP_T4930(report_id=report_id, session_id=session_id, data_set=data_set, environment=configuration.environment) \
@@ -111,8 +104,7 @@ def test_run(parent_id=None, version=None):
         logging.error("Error execution", exc_info=True)
     finally:
         logger.info(f"Acceptance list was executed in {str(round(datetime.now().timestamp() - seconds))} sec.")
-        Stubs.win_act.unregister(session_id)
-        base_main_window.close_fe()
+        # Stubs.win_act.unregister(session_id)
 
 
 if __name__ == '__main__':
