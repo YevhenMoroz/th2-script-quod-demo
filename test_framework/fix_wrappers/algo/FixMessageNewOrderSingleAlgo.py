d9bca84420b23bf523f74378219eacd02ab9c092
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2023-03-09 19:19:41 +0200

    QAP-8728 - add set_TWAP_Redburn_params

--- test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
+++ test_framework/fix_wrappers/algo/FixMessageNewOrderSingleAlgo.py
@@ -77,6 +77,28 @@ class FixMessageNewOrderSingleAlgo(FixMessageNewOrderSingle):
         super().change_parameters(base_parameters)
         return self
 
+    def set_TWAP_Redburn_params(self) -> FixMessageNewOrderSingle:
+        base_parameters = {
+            'Account': self.get_data_set().get_account_by_name("account_1"),
+            'ClOrdID': basic_custom_actions.client_orderid(9),
+            "HandlInst": "2",
+            "Side": "1",
+            "OrderQty": "1000",
+            "TimeInForce": "0",
+            "OrdType": "2",
+            "TransactTime": datetime.utcnow().isoformat(),
+            "OrderCapacity": "A",
+            "Price": "20",
+            'Currency': self.get_data_set().get_currency_by_name("currency_1"),
+            'ExDestination': self.get_data_set().get_mic_by_name("mic_1"),
+            "Instrument": self.get_data_set().get_fix_instrument_by_name("instrument_1"),
+            "TargetStrategy": "1005",
+            'QuodFlatParameters': {
+            }
+        }
+        super().change_parameters(base_parameters)
+        return self
+
     def set_TWAP_auction_params(self) -> FixMessageNewOrderSingle:
         base_parameters = {
             'Account': self.get_data_set().get_account_by_name('account_1'),
