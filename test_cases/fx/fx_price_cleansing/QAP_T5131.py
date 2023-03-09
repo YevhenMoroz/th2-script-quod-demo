0d79bd2df604d18291a9170e3ce963cf7b67f869
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2023-03-09 13:04:22 +0100

    Timer is added to prc cln tests

--- test_cases/fx/fx_price_cleansing/QAP_T5131.py
+++ test_cases/fx/fx_price_cleansing/QAP_T5131.py
@@ -168,3 +168,4 @@ class QAP_T5131(TestCase):
         self.md_request.set_md_uns_parameters_maker(). \
             change_parameters({'MDReqID': self.md_req_id})
         self.fix_manager_marketdata_th2.send_message(self.md_request)
+        self.sleep(2)
