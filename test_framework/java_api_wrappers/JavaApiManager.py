c2e43f88073776f1374976d66d198e75b4a1805d
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-03-13 09:22:31 +0100

    Split TradeEntryRequest for EQ and FX

--- test_framework/java_api_wrappers/JavaApiManager.py
+++ test_framework/java_api_wrappers/JavaApiManager.py
@@ -51,12 +51,6 @@ class JavaApiManager:
                                                              message.get_parameters(), self.get_session_alias()),
                     parent_event_id=self.get_case_id(), response_time=response_time))
         elif message.get_message_type() == ORSMessageType.TradeEntryRequest.value:
-            response = self.act.submitTradeEntry(
-                request=ActJavaSubmitMessageRequest(
-                    message=bca.message_to_grpc_fix_standard(message.get_message_type(),
-                                                             message.get_parameters(), self.get_session_alias()),
-                    parent_event_id=self.get_case_id(), filterFields=filter_dict, response_time=response_time))
-        elif message.get_message_type() == ORSMessageType.TradeEntryRequest.value:
             if "OrdID" in message.get_parameter("TradeEntryRequestBlock"):
                 response = self.act.submitTradeEntry(
                     request=ActJavaSubmitMessageRequest(
