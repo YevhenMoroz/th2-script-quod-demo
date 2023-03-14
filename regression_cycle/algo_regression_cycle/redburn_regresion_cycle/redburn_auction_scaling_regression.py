e2f29d09974123a2f0cf87675f6d218d12a0faee
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2023-03-14 17:48:26 +0200

    QAP-8975 - add new test

--- regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_auction_scaling_regression.py
+++ regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_auction_scaling_regression.py
@@ -5,6 +5,7 @@ from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T10278 import QAP_T10
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T10273 import QAP_T10273
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4348 import QAP_T4348
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4390 import QAP_T4390
+from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4447 import QAP_T4447
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4450 import QAP_T4450
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4451 import QAP_T4451
 from test_cases.algo.Algo_Redburn.Algo_Auction_Scaling.QAP_T4460 import QAP_T4460
@@ -79,6 +80,7 @@ def test_run(parent_id=None, version=None):
         QAP_T4450(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4451(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4390(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T4447(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
 
 
     except Exception:
