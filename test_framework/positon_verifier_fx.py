5d392482ad84f145fd3d1f5e5640caeb1f4d7ef6
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-03-17 17:37:26 +0100

    Some work with positions

---
+++ test_framework/positon_verifier_fx.py
@@ -0,0 +1,42 @@
+from custom.verifier import Verifier
+
+
+class PositionVerifier:
+
+    def __init__(self, test_id):
+        self.test_id = test_id
+        self.verifier = Verifier(test_id)
+
+    def check_base_position(self, report, expected_value):
+        pos_amount_date = report[0].get_parameters()["PositionAmountData"][0]
+        amount = pos_amount_date["PosAmt"]
+        self.verifier.set_event_name("Check Base Position")
+        self.verifier.compare_values("Compare Base", expected_value, amount)
+        self.verifier.verify()
+        self.verifier = Verifier(self.test_id)
+
+    def check_avg_price(self, report, expected_value):
+        pos_amount_date = report[0].get_parameters()["PositionAmountData"][6]
+        amount = pos_amount_date["PosAmt"]
+        self.verifier.set_event_name("Check Avg Price")
+        self.verifier.compare_values("Compare Avg PX", expected_value, amount)
+        self.verifier.verify()
+        self.verifier = Verifier(self.test_id)
+
+    def check_quote_position(self, report, expected_value):
+        pos_amount_date = report[0].get_parameters()["PositionAmountData"][7]
+        amount = pos_amount_date["PosAmt"]
+        self.verifier.set_event_name("Check Quote Position")
+        self.verifier.compare_values("Compare Quote Position", expected_value, amount)
+        self.verifier.verify()
+        self.verifier = Verifier(self.test_id)
+
+    def check_system_cur_position(self, report, expected_value):
+        pos_amount_date = report[0].get_parameters()["PositionAmountData"][8]
+        amount = pos_amount_date["PosAmt"]
+        self.verifier.set_event_name("Check System Currency Position")
+        self.verifier.compare_values("Compare Position", expected_value, amount)
+        self.verifier.verify()
+        self.verifier = Verifier(self.test_id)
+
+    # TODO Add new fields to check
