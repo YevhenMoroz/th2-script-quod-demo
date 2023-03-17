230672ab89c74171b9f33affbf772b9afd5ebd98
Vadim <vadim.skulinec.contractor@quodfinancial.com>
2023-03-17 15:12:10 +0200

    QAP_T10719 (update 0.3)

--- test_framework/data_sets/oms_data_set/oms_const_enum.py
+++ test_framework/data_sets/oms_data_set/oms_const_enum.py
@@ -563,7 +563,7 @@ class OMSCounterPartyIDs_FIX(Enum):
                                                          'PartyRoleQualifier': '12',
                                                          'PartyID': 'InvestorID - ClCounterpart_SA1',
                                                          'PartyIDSource': "C"}
-    counterpart_id_custodian_user_2 = {'PartyRole': '28', 'PartyRoleQualifier': '24','PartyID': 'CustodianUser2',
+    counterpart_id_custodian_user_2 = {'PartyRole': '28', 'PartyRoleQualifier': '24', 'PartyID': 'CustodianUser2',
                                        'PartyIDSource': 'C'}
     counterpart_id_custodian_user = {'PartyRole': '28', 'PartyRoleQualifier': '12', 'PartyID': 'CustodianUser',
                                      'PartyIDSource': 'C'}
@@ -609,6 +609,7 @@ class OMSCounterPartyIDs_JavaAPI(Enum):
     counterpart_regulatory_body_venue = {'CounterpartID': '200008', 'PartyRole': 'REB'}
     counterpart_custodian_user_2 = {'PartyRole': "CUS", 'CounterpartID': '800006'}
     counterpart_agent = {'CounterpartID': '1400025', 'PartyRole': 'AGE'}
+    counterpart_entering_firm = {'CounterpartID': '1200012' 'ENF'}
 
 
 class OMSVenueClientAccountName(Enum):
