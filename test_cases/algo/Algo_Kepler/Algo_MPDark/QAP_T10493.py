074aa7d138abb95e84bd6b60eb470b3de401465b
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2023-03-21 15:22:06 +0100

    Fix after the test run

--- test_cases/algo/Algo_Kepler/Algo_MPDark/QAP_T10493.py
+++ test_cases/algo/Algo_Kepler/Algo_MPDark/QAP_T10493.py
@@ -139,7 +139,7 @@ class QAP_T10493(TestCase):
 
         # region Check that the 1st child order didn't eliminate after the partial fill
         self.fix_verifier_buy.set_case_id(bca.create_event("Check that the 1st child order didn't eliminate after the partial fill", self.test_id))
-        self.fix_verifier_buy.check_fix_message_sequence([er_pending_new_dma_chix_order_params, er_new_dma_chix_order_params, er_partial_fill_dma_chix_order_params], [self.key_params_ER_child, self.key_params_ER_child, self.key_params_ER_child], self.ToQuod, pre_filter=None)
+        self.fix_verifier_buy.check_fix_message_sequence([er_pending_new_dma_chix_order_params, er_new_dma_chix_order_params, er_partial_fill_dma_chix_order_params], [self.key_params_ER_child, self.key_params_ER_child, self.key_params_ER_child], self.ToQuod, pre_filter=None, check_order=False)
         # endregion
 
         time.sleep(7)
