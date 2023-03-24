7c4cddf8df88705fd877decf45fcb905666e33e9
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2023-03-24 16:16:53 +0100

    QAP-9073 - QAP-T10708 - add test

--- regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_vwap_aution_regression.py
+++ regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_vwap_aution_regression.py
@@ -1,6 +1,7 @@
 import logging
 from custom import basic_custom_actions as bca
 from stubs import Stubs
+from test_cases.algo.Algo_Redburn.Algo_VWAP_Auction.QAP_T10708 import QAP_T10708
 from test_cases.algo.Algo_Redburn.Algo_VWAP_Auction.QAP_T9062 import QAP_T9062
 from test_framework.configurations.component_configuration import ComponentConfigurationAlgo
 
@@ -19,6 +20,7 @@ def test_run(parent_id=None, version=None):
         # configuration = ComponentConfiguration("Vwap")
         configuration = ComponentConfigurationAlgo("Vwap")
         QAP_T9062(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10708(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         pass
 
     except Exception:
