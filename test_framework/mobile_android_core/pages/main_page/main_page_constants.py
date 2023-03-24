dbd28d8ffa5c7f13580658c40e7fc3d8c2aa10e0
itserkov <illia.tserkovnyi.contractor@quodfinancial.com>
2023-03-24 04:47:45 +0200

    PEQ-10717 - Create User Profile Page for basic login/logout flow

--- test_framework/mobile_android_core/pages/main_page/main_page_constants.py
+++ test_framework/mobile_android_core/pages/main_page/main_page_constants.py
@@ -1,3 +1,7 @@
 
 class MainPageConstants:
+    """
+    class describes Value Key for each element displayed on Main Page
+    Value Key should be provided by DEV team what they implemented in application
+    """
     buttonUserProfile = "buttonUserProfile"
