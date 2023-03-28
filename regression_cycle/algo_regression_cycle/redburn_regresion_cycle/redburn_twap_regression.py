642d054a0545f2d26fb1d60309fa423a42837067
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2023-03-28 15:06:10 +0300

    PALGO-1201 - add new TWAP test QAP_T10471 to regression file

--- regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_twap_regression.py
+++ regression_cycle/algo_regression_cycle/redburn_regresion_cycle/redburn_twap_regression.py
@@ -1,6 +1,7 @@
 import logging
 from custom import basic_custom_actions as bca
 from stubs import Stubs
+from test_cases.algo.Algo_Redburn.Algo_TWAP.QAP_T10471 import QAP_T10471
 from test_cases.algo.Algo_Redburn.Algo_TWAP.QAP_T4673 import QAP_T4673
 from test_framework.configurations.component_configuration import ComponentConfigurationAlgo
 
@@ -17,6 +18,7 @@ def test_run(parent_id=None, version=None):
         # region TWAP tests
         configuration = ComponentConfigurationAlgo("Twap")
         QAP_T4673(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10471(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
 
         # endregion
 
