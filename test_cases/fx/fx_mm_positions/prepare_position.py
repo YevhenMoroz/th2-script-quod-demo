4048e26047e7a2c7f6fd1f33657341250b703d1e
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-03-30 11:38:14 +0200

    Add sleep

--- test_cases/fx/fx_mm_positions/prepare_position.py
+++ test_cases/fx/fx_mm_positions/prepare_position.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 from test_cases.fx.fx_wrapper.common_tools import clear_position, restart_pks
 
 
 def prepare_position():
     clear_position()
     restart_pks()
+    sleep(120)
