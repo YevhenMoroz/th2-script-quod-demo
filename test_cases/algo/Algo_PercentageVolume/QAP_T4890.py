b926bdd6d5a545745dcc3309fc79688781ba7774
Roman Murza <murzarom2@gmail.com>
2023-04-03 13:17:55 +0300

    QAP-9067 - update venue

--- test_cases/algo/Algo_PercentageVolume/QAP_T4890.py
+++ test_cases/algo/Algo_PercentageVolume/QAP_T4890.py
@@ -102,19 +102,19 @@ class QAP_T4890(TestCase):
         # endregion
 
         # region Set TradingPhase and LTQ for POV
-        # self.fix_manager_feed_handler.set_case_id(bca.create_event("Set TradingPhase for POV", self.test_id))
-        # market_data_incr_par = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_ltq().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
-        # market_data_incr_par.update_repeating_group_by_index('NoMDEntriesIR', MDEntryPx=self.price_bid, MDEntrySize=0)
-        # self.fix_manager_feed_handler.send_message(market_data_incr_par)
-        # # endregion
-        #
-        # # region Send_MarkerData
-        # self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data", self.test_id))
-        # market_data_snap_shot_par = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
-        # market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=self.price_bid, MDEntrySize=0)
-        # market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 1, MDEntryPx=self.price_ask, MDEntrySize=self.open_ord_qty)
-        # self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
-        # # endregion
+        self.fix_manager_feed_handler.set_case_id(bca.create_event("Set TradingPhase for POV", self.test_id))
+        market_data_incr_par = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_ltq().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
+        market_data_incr_par.update_repeating_group_by_index('NoMDEntriesIR', MDEntryPx=self.price_bid, MDEntrySize=0)
+        self.fix_manager_feed_handler.send_message(market_data_incr_par)
+        # endregion
+
+        # region Send_MarkerData
+        self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data", self.test_id))
+        market_data_snap_shot_par = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.s_par, self.fix_env1.feed_handler)
+        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=self.price_bid, MDEntrySize=0)
+        market_data_snap_shot_par.update_repeating_group_by_index('NoMDEntries', 1, MDEntryPx=self.price_ask, MDEntrySize=self.open_ord_qty)
+        self.fix_manager_feed_handler.send_message(market_data_snap_shot_par)
+        # endregion
 
         # region Send NewOrderSingle (35=D) for POV order
         case_id_1 = bca.create_event("Create POV Order", self.test_id)
