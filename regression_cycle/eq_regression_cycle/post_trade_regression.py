9c25ea1be1acec778bbed01b6a8900233a22522c
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2023-04-03 13:25:50 +0300

    refactor regression files

--- regression_cycle/eq_regression_cycle/post_trade_regression.py
+++ regression_cycle/eq_regression_cycle/post_trade_regression.py
@@ -21,7 +21,7 @@ def check_ssh(file):
 
 
 def test_run(parent_id=None, version=None, skip_ssh=False, only_ssh=False):
-    report_id = bca.create_event(f"PostTrade Analysis" if version is None else f"PostTrade Analysis | {version}",
+    report_id = bca.create_event(f"PostTrade Analysis" if version is None else f"PostTrade | {version}",
                                  parent_id)
     seconds, nanos = timestamps()  # Store case start time
     configuration = ComponentConfiguration("PostTrade")
