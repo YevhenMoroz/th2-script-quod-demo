e141734e775d845fb731d3d2107c2e7263d82c6b
mziuban <mykhailo.ziuban.contractor@quodfinancial.com>
2023-04-07 19:38:43 +0200

    PALGO-1237 - add test QAP-T11010, fix QAP-T9100

--- test_cases/algo/Algo_Redburn/Algo_MOO/QAP_T11010.py
+++ test_cases/algo/Algo_Redburn/Algo_MOO/QAP_T11010.py
@@ -25,7 +25,7 @@ from test_framework.read_log_wrappers.algo_messages.ReadLogMessageAlgo import Re
 from test_framework.rest_api_wrappers.algo.RestApiStrategyManager import RestApiAlgoManager
 
 
-class QAP_T9100(TestCase):
+class QAP_T11010(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, data_set=None, environment=None):
         super().__init__(report_id=report_id, data_set=data_set, environment=environment)
@@ -51,7 +51,7 @@ class QAP_T9100(TestCase):
         self.percentage = 10
         self.child_qty = AFM.get_child_qty_for_auction(self.indicative_volume, self.percentage, self.qty)
         self.price = 30
-        self.tif_atc = TimeInForce.AtTheClose.value
+        self.tif_ato = TimeInForce.AtTheOpening.value
         # endregion
 
         # region Gateway Side
@@ -97,9 +97,11 @@ class QAP_T9100(TestCase):
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
+
+
         # region Update Trading Phase
         self.rest_api_manager.set_case_id(case_id=bca.create_event("Modify trading phase profile", self.test_id))
-        trading_phases = AFM.get_timestamps_for_current_phase(TradingPhases.PreClosed)
+        trading_phases = AFM.get_timestamps_for_current_phase(TradingPhases.PreOpen)
         self.rest_api_manager.modify_trading_phase_profile(self.trading_phase_profile, trading_phases)
         # end region
 
@@ -110,8 +112,8 @@ class QAP_T9100(TestCase):
 
 
         # region Send MarketDate
-        self.fix_manager_feed_handler.set_case_id(case_id=bca.create_event("Send trading phase - PreClose", self.test_id))
-        self.incremental_refresh = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_indicative().update_MDReqID(self.listing_id, self.fix_env1.feed_handler).update_value_in_repeating_group('NoMDEntriesIR', 'MDEntrySize', self.indicative_volume).set_phase(TradingPhases.PreClosed)
+        self.fix_manager_feed_handler.set_case_id(case_id=bca.create_event("Send trading phase - PreOpen", self.test_id))
+        self.incremental_refresh = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_indicative().update_MDReqID(self.listing_id, self.fix_env1.feed_handler).update_value_in_repeating_group('NoMDEntriesIR', 'MDEntrySize', self.indicative_volume).set_phase(TradingPhases.PreOpen)
         self.fix_manager_feed_handler.send_message(fix_message=self.incremental_refresh)
         # endregion
 
@@ -119,7 +121,7 @@ class QAP_T9100(TestCase):
         case_id_1 = bca.create_event("Create Auction Order", self.test_id)
         self.fix_verifier_sell.set_case_id(case_id_1)
 
-        self.auction_algo = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_MOC_params()
+        self.auction_algo = FixMessageNewOrderSingleAlgo(data_set=self.data_set).set_MOO_params()
         self.auction_algo.add_ClordId((os.path.basename(__file__)[:-3]))
         self.auction_algo.change_parameters(dict(Account=self.client, OrderQty=self.qty, Price=self.price, Instrument=self.instrument, ExDestination=self.mic))
         self.auction_algo.update_fields_in_component("QuodFlatParameters", dict(MaxParticipation=self.percentage))
@@ -129,11 +131,11 @@ class QAP_T9100(TestCase):
         self.fix_verifier_sell.check_fix_message(self.auction_algo, key_parameters=self.key_params_NOS_parent, direction=self.ToQuod, message_name='Sell side NewOrderSingle')
 
         er_pending_new = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.auction_algo, self.gateway_side_sell, self.status_pending)
-        er_pending_new.change_parameters(dict(TimeInForce=self.tif_atc))
+        er_pending_new.change_parameters(dict(TimeInForce=self.tif_ato))
         self.fix_verifier_sell.check_fix_message(er_pending_new, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport PendingNew')
 
         er_new = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.auction_algo, self.gateway_side_sell, self.status_new)
-        er_new.change_parameters(dict(TimeInForce=self.tif_atc))
+        er_new.change_parameters(dict(TimeInForce=self.tif_ato))
         self.fix_verifier_sell.check_fix_message(er_new, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport New')
         # endregion
 
@@ -142,14 +144,14 @@ class QAP_T9100(TestCase):
         self.fix_verifier_buy.set_case_id(case_id_2)
 
         # region
-        case_id_3 = bca.create_event("Send trading phase - PreClose", self.test_id)
+        case_id_3 = bca.create_event("Send trading phase - Open", self.test_id)
         self.fix_manager_feed_handler.set_case_id(case_id_3)
 
         scheduler = sched.scheduler(time.time, time.sleep)
-        end_time = AFM.get_timestamp_from_list(phases=trading_phases, phase=TradingPhases.AtLast, start_time=True) + 1
-        seconds_to_check = AFM.calculate_how_many_sec_to_this_time(phases=trading_phases, phase=TradingPhases.AtLast, start_time=True)
+        end_time = AFM.get_timestamp_from_list(phases=trading_phases, phase=TradingPhases.Open, start_time=True) + 1
+        seconds_to_check = AFM.calculate_how_many_sec_to_this_time(phases=trading_phases, phase=TradingPhases.Open, start_time=True)
 
-        self.incremental_refresh_at_last = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_indicative().update_value_in_repeating_group('NoMDEntriesIR', 'MDEntrySize', self.indicative_volume).update_MDReqID(self.listing_id, self.fix_env1.feed_handler).set_phase(TradingPhases.AtLast)
+        self.incremental_refresh_at_last = FixMessageMarketDataIncrementalRefreshAlgo().set_market_data_incr_refresh_indicative().update_value_in_repeating_group('NoMDEntriesIR', 'MDEntrySize', self.indicative_volume).update_MDReqID(self.listing_id, self.fix_env1.feed_handler).set_phase(TradingPhases.Open)
         scheduler.enterabs(end_time + 3, 2, self.fix_manager_feed_handler.send_message, kwargs=dict(fix_message=self.incremental_refresh_at_last))
 
         scheduler.enterabs(end_time + 4, 2, self.fix_verifier_buy.check_no_message_found, kwargs=dict(message_timeout=seconds_to_check * 1000))
@@ -163,7 +165,6 @@ class QAP_T9100(TestCase):
         self.fix_verifier_sell.set_case_id(case_id_2)
         # endregion
 
-
         # region Update Trading Phase
         self.rest_api_manager.set_case_id(case_id=bca.create_event("Revert trading phase profile", self.test_id))
         trading_phases = AFM.get_default_timestamp_for_trading_phase()
@@ -173,5 +174,5 @@ class QAP_T9100(TestCase):
         time.sleep(3)
 
         er_cancel_auction_order = FixMessageExecutionReportAlgo().set_params_from_new_order_single(self.auction_algo, self.gateway_side_sell, self.status_eliminate)
-        er_cancel_auction_order.change_parameters(dict(TimeInForce=self.tif_atc, Text='reached uncross')).remove_parameter("LastMkt")
+        er_cancel_auction_order.change_parameters(dict(TimeInForce=self.tif_ato, Text='reached uncross')).remove_parameter("LastMkt")
         self.fix_verifier_sell.check_fix_message(er_cancel_auction_order, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport Cancel')
