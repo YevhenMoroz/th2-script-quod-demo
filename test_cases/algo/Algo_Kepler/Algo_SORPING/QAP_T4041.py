956db5ba774e460a3e85bb66d85e7cf537209874
Sofiia Kolesnyk <sofiia.kolesnyk.contractor@quodfinancial.com>
2023-04-12 12:11:31 +0200

    QAP-T4041 Add the temporary rule

--- test_cases/algo/Algo_Kepler/Algo_SORPING/QAP_T4041.py
+++ test_cases/algo/Algo_Kepler/Algo_SORPING/QAP_T4041.py
@@ -68,6 +68,7 @@ class QAP_T4041(TestCase):
         self.account = self.data_set.get_account_by_name("account_9")
         self.listing_id_qdl2 = self.data_set.get_listing_id_by_name("listing_39")
         self.listing_id_qdl1 = self.data_set.get_listing_id_by_name("listing_40")
+        self.feed_handler = 'fix-feed-handler-319-kuiper'
         # endregion
 
         # region Key parameters
@@ -81,20 +82,26 @@ class QAP_T4041(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
         # region Rule creation
+        # The default rule will be changed (for manual executing restart the FH after the deleting the default rule and adding a new market data rule)
         rule_manager = RuleManager(Simulators.algo)
+        special_market_rule = rule_manager.add_MarketDataRequestWithTimeout(self.feed_handler, [self.listing_id_qdl1])
         nos_ioc_rule = rule_manager.add_NewOrdSingle_IOC(self.fix_env1.buy_side, self.account, self.ex_destination_quodlit1, False, 0, self.price)
         nos_rule = rule_manager.add_NewOrdSingleExecutionReportPendingAndNew(self.fix_env1.buy_side, self.account, self.ex_destination_quodlit1, self.price)
         ocr_rule = rule_manager.add_OrderCancelRequest(self.fix_env1.buy_side, self.account, self.ex_destination_quodlit1, True)
-        self.rule_list = [nos_ioc_rule, nos_rule, ocr_rule]
+        self.rule_list = [special_market_rule, nos_ioc_rule, nos_rule, ocr_rule]
+
+        rule_manager.remove_rule_by_id(2)
         # endregion
+
+        time.sleep(5)
 
         # region Send_MarkerData
         self.fix_manager_feed_handler.set_case_id(bca.create_event("Send Market Data", self.test_id))
 
-        market_data_snap_shot_qdl1 = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.listing_id_qdl1, self.fix_env1.feed_handler)
-        market_data_snap_shot_qdl1.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=10, MDEntrySize=100)
-        market_data_snap_shot_qdl1.update_repeating_group_by_index('NoMDEntries', 1, MDEntryPx=self.price_ask, MDEntrySize=0)
-        self.fix_manager_feed_handler.send_message(market_data_snap_shot_qdl1)
+        # market_data_snap_shot_qdl1 = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.listing_id_qdl1, self.fix_env1.feed_handler)
+        # market_data_snap_shot_qdl1.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=10, MDEntrySize=100)
+        # market_data_snap_shot_qdl1.update_repeating_group_by_index('NoMDEntries', 1, MDEntryPx=self.price_ask, MDEntrySize=0)
+        # self.fix_manager_feed_handler.send_message(market_data_snap_shot_qdl1)
 
         market_data_snap_shot_qdl2 = FixMessageMarketDataSnapshotFullRefreshAlgo().set_market_data().update_MDReqID(self.listing_id_qdl2, self.fix_env1.feed_handler)
         market_data_snap_shot_qdl2.update_repeating_group_by_index('NoMDEntries', 0, MDEntryPx=self.price_bid, MDEntrySize=self.qty_for_md)
@@ -102,7 +109,7 @@ class QAP_T4041(TestCase):
         self.fix_manager_feed_handler.send_message(market_data_snap_shot_qdl2)
 
         time.sleep(3)
-        
+
         # endregion
 
         # region Send NewOrderSingle (35=D) for SynthMinQty order
@@ -163,5 +170,8 @@ class QAP_T4041(TestCase):
         self.fix_verifier_sell.check_fix_message(er_cancel_Multilisting_order_params, key_parameters=self.key_params_ER_parent, message_name='Sell side ExecReport Cancel')
         # endregion
 
+        time.sleep(5)
+
         rule_manager = RuleManager(Simulators.algo)
         rule_manager.remove_rules(self.rule_list)
+        rule_manager.add_MDRule(self.feed_handler)
