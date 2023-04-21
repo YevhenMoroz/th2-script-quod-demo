b9baf54e142de2df4b26e3612e05c16aae1cba57
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2023-04-21 13:36:58 +0200

    QAP-T9243: automated QAP_T9425: deprecated

--- test_cases/fx/fx_price_cleansing/QAP_T9243.py
+++ test_cases/fx/fx_price_cleansing/QAP_T9243.py
@@ -1,6 +1,8 @@
 import time
 from datetime import datetime
 from pathlib import Path
+from random import randint
+
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
 from test_framework.data_sets.base_data_set import BaseDataSet
@@ -23,7 +25,7 @@ from test_framework.rest_api_wrappers.forex.RestApiPriceCleansingCrossedReferenc
 from test_framework.win_gui_wrappers.forex.rates_tile import RatesTile
 
 
-class QAP_T9425(TestCase):
+class QAP_T9243(TestCase):
     def __init__(self, report_id, session_id=None, data_set: BaseDataSet = None, environment: FullEnvironment = None):
         super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
@@ -38,12 +40,15 @@ class QAP_T9425(TestCase):
         self.rates_tile = RatesTile(self.test_id, self.session_id)
         self.fix_md = FixMessageMarketDataSnapshotFullRefreshBuyFX()
         self.fix_md_snapshot = FixMessageMarketDataSnapshotFullRefreshSellFX()
-        self.md_reject = FixMessageMarketDataRequestRejectFX()
+        self.fix_md_snapshot_doubler = FixMessageMarketDataSnapshotFullRefreshSellFX()
+        self.fix_md_reject = FixMessageMarketDataRequestRejectFX()
         self.gbp_aud = self.data_set.get_symbol_by_name('symbol_9')
+        self.eur_usd = self.data_set.get_symbol_by_name('symbol_2')
         self.tenor_spot = self.data_set.get_tenor_by_name('tenor_spot')
-        self.settle_type = self.data_set.get_settle_type_by_name("spot")
-        self.security_type = self.data_set.get_security_type_by_name("fx_spot")
-        self.instr_type_wa = self.data_set.get_fx_instr_type_wa('fx_spot')
+        self.settle_type = self.data_set.get_settle_type_by_name("wk1")
+        self.security_type = self.data_set.get_security_type_by_name("fx_fwd")
+        self.instr_type_wa_spot = self.data_set.get_fx_instr_type_wa('fx_spot')
+        self.instr_type_wa_fwd = self.data_set.get_fx_instr_type_wa('fx_fwd')
         self.barx = self.data_set.get_venue_by_name('venue_6')
         self.bnp = self.data_set.get_venue_by_name('venue_10')
         self.rest_message = RestApiPriceCleansingCrossedReferenceRatesMessages(data_set=self.data_set)
@@ -51,6 +56,10 @@ class QAP_T9425(TestCase):
         self.rest_message_params = None
         self.instrument = {
             "Symbol": self.gbp_aud,
+            "SecurityType": self.security_type,
+        }
+        self.instrument_eur_usd = {
+            "Symbol": self.eur_usd,
             "SecurityType": self.security_type,
         }
         self.no_related_symbols = [{
@@ -62,10 +71,24 @@ class QAP_T9425(TestCase):
             "SettlType": self.settle_type,
             'MarketID': f"{self.bnp}-SW"
         }]
-        self.md_id_bnp = f"{self.gbp_aud}:{self.instr_type_wa}:REG:{self.bnp}"
-        self.md_id_bnp_123 = f"{self.gbp_aud}:{self.instr_type_wa}:REG:{self.bnp}" + "_123"
-        self.md_id_barx = f"{self.gbp_aud}:{self.instr_type_wa}:REG:{self.barx}"
-        self.md_id_barx_123 = f"{self.gbp_aud}:{self.instr_type_wa}:REG:{self.barx}" + "_123"
+        self.no_related_symbols_eur_usd = [{
+            "Instrument": {
+                "Symbol": self.eur_usd,
+                "CFICode": f"{self.bnp}-SW",
+                "SecurityType": self.security_type,
+                "Product": "4", },
+            "SettlType": self.settle_type,
+            'MarketID': f"{self.bnp}-SW"
+        }]
+
+        self.md_id_bnp_fwd = f"{self.gbp_aud}:{self.instr_type_wa_fwd}:WK1:{self.bnp}"
+        self.md_id_bnp_123_fwd = f"{self.gbp_aud}:{self.instr_type_wa_fwd}:WK1:{self.bnp}" + "_123"
+        self.md_id_bnp_fwd_b = f"{self.gbp_aud}:{self.instr_type_wa_fwd}:WK1:{self.bnp}" + "_" + str(randint(100, 9999))
+        self.md_id_bnp_fwd_c = f"{self.gbp_aud}:{self.instr_type_wa_fwd}:WK1:{self.bnp}" + "_" + str(randint(100, 9999))
+        self.md_id_barx_fwd = f"{self.gbp_aud}:{self.instr_type_wa_fwd}:WK1:{self.barx}"
+        self.md_id_barx_123_fwd = f"{self.gbp_aud}:{self.instr_type_wa_fwd}:WK1:{self.barx}" + "_123"
+        self.md_id_bnp_eur_usd_fwd = f"{self.eur_usd}:{self.instr_type_wa_fwd}:WK1:{self.bnp}"
+        self.md_id_bnp_eur_usd_123_fwd = f"{self.eur_usd}:{self.instr_type_wa_fwd}:WK1:{self.bnp}" + "_123"
         self.md_req_id = ''
         self.md_entries_bnp = [
             {
@@ -74,7 +97,7 @@ class QAP_T9425(TestCase):
                 "MDEntrySize": 1000000,
                 "MDQuoteType": 1,
                 "MDEntryPositionNo": 1,
-                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "SettlDate": self.data_set.get_settle_date_by_name('wk1'),
                 "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
                 "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
             },
@@ -84,7 +107,7 @@ class QAP_T9425(TestCase):
                 "MDEntrySize": 1000000,
                 "MDQuoteType": 1,
                 "MDEntryPositionNo": 1,
-                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "SettlDate": self.data_set.get_settle_date_by_name('wk1'),
                 "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
                 "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
             }
@@ -95,8 +118,9 @@ class QAP_T9425(TestCase):
                 "MDEntryPx": 1.18129,
                 "MDEntrySize": 1000000,
                 "MDQuoteType": 1,
+                "MDEntryForwardPoints": "0.0000001",
                 "MDEntryPositionNo": 1,
-                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "SettlDate": self.data_set.get_settle_date_by_name('wk1'),
                 "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
                 "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
             },
@@ -105,8 +129,9 @@ class QAP_T9425(TestCase):
                 "MDEntryPx": 1.18099,
                 "MDEntrySize": 1000000,
                 "MDQuoteType": 1,
+                "MDEntryForwardPoints": "0.0000001",
                 "MDEntryPositionNo": 1,
-                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "SettlDate": self.data_set.get_settle_date_by_name('wk1'),
                 "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
                 "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
             }
@@ -117,101 +142,155 @@ class QAP_T9425(TestCase):
         # Region Rule creation
         self.rest_message.set_default_params().create_cross_ref_rates_cleansing_rule().set_target_venue(
             self.bnp).set_symbol(
-            self.gbp_aud).clear_ref_venues().add_ref_venue(self.barx)
+            self.gbp_aud).clear_ref_venues().add_ref_venue(self.barx).set_fwd()
         response = self.rest_message_params = self.rest_manager.parse_create_response(
             self.rest_manager.send_multiple_request(self.rest_message))
         time.sleep(3)
         # endregion
+
         self.md_request.set_md_req_parameters_taker(). \
-            change_parameters({'MDReqID': self.md_id_bnp_123}). \
+            change_parameters({'MDReqID': self.md_id_bnp_123_fwd}). \
             update_repeating_group("NoRelatedSymbols", self.no_related_symbols)
         self.fix_manager_marketdata_th2.send_message_and_receive_response(self.md_request, self.test_id)
 
         self.md_request.set_md_uns_parameters_maker(). \
-            change_parameters({'MDReqID': self.md_id_bnp_123})
+            change_parameters({'MDReqID': self.md_id_bnp_123_fwd})
         self.fix_manager_marketdata_th2.send_message(self.md_request)
 
-        self.fix_md.change_parameter("MDReqID", self.md_id_bnp)
+        self.fix_md.change_parameter("MDReqID", self.md_id_bnp_fwd)
         self.fix_md.change_parameter("NoMDEntries", self.md_entries_bnp)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp_fwd}")
         time.sleep(3)
 
         self.md_request.set_md_req_parameters_taker(). \
-            change_parameters({'MDReqID': self.md_id_barx_123}). \
+            change_parameters({'MDReqID': self.md_id_barx_123_fwd}). \
             update_repeating_group("NoRelatedSymbols", self.no_related_symbols)
         self.fix_manager_marketdata_th2.send_message_and_receive_response(self.md_request, self.test_id)
 
         self.md_request.set_md_uns_parameters_maker(). \
-            change_parameters({'MDReqID': self.md_id_barx_123})
+            change_parameters({'MDReqID': self.md_id_barx_123_fwd})
         self.fix_manager_marketdata_th2.send_message(self.md_request)
 
-        self.fix_md.change_parameter("MDReqID", self.md_id_barx)
+        self.fix_md.change_parameter("MDReqID", self.md_id_barx_fwd)
         self.fix_md.change_parameter("NoMDEntries", self.md_entries_barx)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_barx}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_barx_fwd}")
         time.sleep(3)
 
-        self.fix_md.change_parameter("MDReqID", self.md_id_bnp)
-        self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
-                                   self.fx_fh_connectivity,
-                                   'FX')
-        self.md_req_id = self.fix_md.get_parameter("MDReqID")
+        # self.md_request.set_md_req_parameters_taker(). \
+        #     change_parameters({'MDReqID': self.md_id_bnp_eur_usd_123}). \
+        #     update_repeating_group("NoRelatedSymbols", self.no_related_symbols_eur_usd)
+        # self.fix_manager_marketdata_th2.send_message_and_receive_response(self.md_request, self.test_id)
+        #
+        # self.md_request.set_md_uns_parameters_maker(). \
+        #     change_parameters({'MDReqID': self.md_id_bnp_eur_usd_123})
+        # self.fix_manager_marketdata_th2.send_message(self.md_request)
+        #
+        # self.fix_md.change_parameter("MDReqID", self.md_id_bnp_eur_usd_123)
+        # self.fix_md.change_parameter("NoMDEntries", self.md_entries_bnp)
+        # self.fix_md.change_parameter("Instrument", self.instrument_eur_usd)
+        # self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
+        #                            self.fx_fh_connectivity,
+        #                            'FX')
+        # self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp_eur_usd}")
+        # time.sleep(3)
 
         self.md_request.set_md_req_parameters_taker(). \
-            change_parameters({'MDReqID': self.md_req_id}). \
+            change_parameters({'MDReqID': self.md_id_bnp_fwd_b}). \
             update_repeating_group("NoRelatedSymbols", self.no_related_symbols)
         self.fix_manager_marketdata_th2.send_message_and_receive_response(self.md_request, self.test_id)
         # endregion
         # region Step 3
-        self.md_reject.set_md_reject_params(self.md_request, text="suspect data").remove_parameter("MDReqRejReason")
-        self.fix_verifier.check_fix_message(self.md_reject)
+        self.fix_md_snapshot.set_params_for_md_response_taker_wk1(self.md_request, [])
+        self.fix_md_snapshot.add_tag({'PriceCleansingReason': '6'})
+        self.fix_verifier.check_fix_message(self.fix_md_snapshot, ignored_fields=["header", "trailer", "CachedUpdate"])
+        # self.fix_md_reject.set_md_reject_params(self.md_request, text="suspect data")
+        # self.fix_md_reject.remove_parameter("MDReqRejReason")
+        # self.fix_verifier.check_fix_message(self.fix_md_reject,
+        #                                     ignored_fields=["header", "trailer", "CachedUpdate"])
 
         # Region Rule creation
+        # self.rest_message.clear_message_params().set_params(
+        #     self.rest_message_params).delete_cross_ref_rates_cleansing_rule()
+        # self.rest_manager.send_post_request(self.rest_message)
+        # self.rest_message.set_default_params().create_cross_ref_rates_cleansing_rule().set_target_venue(
+        #     self.bnp).set_symbol(
+        #     self.gbp_aud).clear_ref_venues().add_ref_venue(self.barx)
+        # self.rest_message.change_params({"removeDetectedUpdate": "false"})
+        # self.rest_message_params = self.rest_manager.parse_create_response(
+        #     self.rest_manager.send_multiple_request(self.rest_message))
         self.rest_message.clear_message_params().modify_cross_ref_rates_cleansing_rule().set_params(response)
         self.rest_message.change_params({"removeDetectedUpdate": "false"})
         self.rest_manager.send_post_request(self.rest_message)
         time.sleep(3)
+        self.fix_md.change_parameter("MDReqID", self.md_id_bnp_fwd)
+        self.fix_md.change_parameter("NoMDEntries", self.md_entries_bnp)
+        self.fix_md.change_parameter("Instrument", self.instrument)
+        self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
+                                   self.fx_fh_connectivity,
+                                   'FX')
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp_fwd}")
+        time.sleep(3)
+        self.fix_md.change_parameter("MDReqID", self.md_id_barx_fwd)
+        self.fix_md.change_parameter("NoMDEntries", self.md_entries_barx)
+        self.fix_md.change_parameter("Instrument", self.instrument)
+        self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
+                                   self.fx_fh_connectivity,
+                                   'FX')
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_barx_fwd}")
+        time.sleep(3)
         # endregion
 
         self.md_request.set_md_req_parameters_taker(). \
-            change_parameters({'MDReqID': self.md_req_id}). \
+            change_parameters({'MDReqID': self.md_id_bnp_fwd_c}). \
             update_repeating_group("NoRelatedSymbols", self.no_related_symbols)
         self.fix_manager_marketdata_th2.send_message_and_receive_response(self.md_request, self.test_id)
         # endregion
         # region Step 3
-        self.fix_md_snapshot.set_params_for_md_response_taker_spo(self.md_request, ["*", "*", "*", "*", "*"])
-        self.fix_md_snapshot.add_tag({"PriceCleansingReason": "6"})
-        self.fix_verifier.check_fix_message(self.fix_md_snapshot, ignored_fields=["header", "trailer", "CachedUpdate"])
+        self.fix_md_snapshot_doubler.set_params_for_md_response_taker_wk1(self.md_request, ["*"])
+        self.fix_md_snapshot_doubler.remove_fields_in_repeating_group("NoMDEntries",
+                                                                      ["MDEntryForwardPoints", "MDEntrySpotRate"])
+        self.fix_md_snapshot_doubler.add_tag({'PriceCleansingReason': '6'})
+        self.fix_verifier.check_fix_message(self.fix_md_snapshot_doubler,
+                                            ignored_fields=["header", "trailer", "CachedUpdate"])
+
     @try_except(test_id=Path(__file__).name[:-3])
     def run_post_conditions(self):
         # Deleting rule
         self.rest_message.clear_message_params().set_params(
             self.rest_message_params).delete_cross_ref_rates_cleansing_rule()
         self.rest_manager.send_post_request(self.rest_message)
-        self.fix_md.set_market_data()
-        self.fix_md.change_parameter("MDReqID", self.md_id_bnp)
+        self.fix_md.set_market_data_fwd()
+        self.fix_md.change_parameter("MDReqID", self.md_id_bnp_fwd)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp_fwd}")
         self.md_request.set_md_uns_parameters_maker(). \
             change_parameters({'MDReqID': self.md_req_id})
         self.fix_manager_marketdata_th2.send_message(self.md_request)
-        self.fix_md.set_market_data()
-        self.fix_md.change_parameter("MDReqID", self.md_id_barx)
+        self.fix_md.set_market_data_fwd()
+        self.fix_md.change_parameter("MDReqID", self.md_id_barx_fwd)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_barx}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_barx_fwd}")
+        self.fix_md.set_market_data_fwd()
+        self.fix_md.change_parameter("MDReqID", self.md_id_barx_fwd)
+        self.fix_md.change_parameter("Instrument", self.instrument_eur_usd)
+        self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
+                                   self.fx_fh_connectivity,
+                                   'FX')
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_bnp_eur_usd_fwd}")
         self.md_request.set_md_uns_parameters_maker(). \
             change_parameters({'MDReqID': self.md_req_id})
         self.fix_manager_marketdata_th2.send_message(self.md_request)
