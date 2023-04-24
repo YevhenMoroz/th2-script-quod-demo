09ae800946ae37e5c117424143ec7763782e7265
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-04-24 09:23:44 +0200

    PFX-6281 - QAP_T10411 rewrite to complete check

--- test_cases/fx/fx_mm_positions/QAP_T10411.py
+++ test_cases/fx/fx_mm_positions/QAP_T10411.py
@@ -12,7 +12,7 @@ from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
 from test_framework.java_api_wrappers.fx.FixPositionMaintenanceRequestFX import FixPositionMaintenanceRequestFX
 
 
-class QAP_T10401(TestCase):
+class QAP_T10411(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, session_id=None, data_set: BaseDataSet = None, environment: FullEnvironment = None):
         super().__init__(report_id, session_id, data_set, environment)
@@ -21,12 +21,15 @@ class QAP_T10401(TestCase):
         self.pks_connectivity = self.environment.get_list_fix_environment()[0].sell_side_pks
         self.java_api_manager = JavaApiManager(self.java_api_env, self.test_id)
         self.maintenance_request = FixPositionMaintenanceRequestFX()
+        self.maintenance_request_2 = FixPositionMaintenanceRequestFX()
         self.fix_manager = FixManager(self.pks_connectivity, self.test_id)
         self.fix_verifier = FixVerifier(self.pks_connectivity, self.test_id)
         self.request_for_position = FixMessageRequestForPositionsFX()
         self.position_report = FixMessagePositionReportFX()
-        self.client = self.data_set.get_client_by_name("client_mm_7")
-        self.account = self.data_set.get_account_by_name("account_mm_7")
+        self.position_report_2 = FixMessagePositionReportFX()
+        self.client = self.data_set.get_client_by_name("client_pos_1")
+        self.account_1 = self.data_set.get_account_by_name("account_pos_1")
+        self.account_2 = self.data_set.get_account_by_name("account_pos_2")
         self.currency = self.data_set.get_currency_by_name("currency_gbp")
         self.gbp_cad = self.data_set.get_symbol_by_name("symbol_synth_5")
         self.sec_type = self.data_set.get_security_type_by_name("fx_spot")
@@ -41,22 +44,45 @@ class QAP_T10401(TestCase):
     def run_pre_conditions_and_steps(self):
         # region Step 1
         self.maintenance_request.set_default_params()
-        self.maintenance_request.change_account(self.account)
+        self.maintenance_request.change_account(self.account_1)
         self.maintenance_request.change_client(self.client)
         self.maintenance_request.change_instrument(self.gbp_cad, self.sec_type_java)
         self.java_api_manager.send_message(self.maintenance_request)
-        self.sleep(1)
+        self.sleep(5)
+        self.maintenance_request_2.set_default_params()
+        self.maintenance_request_2.change_account(self.account_2)
+        self.maintenance_request_2.change_client(self.client)
+        self.maintenance_request_2.change_instrument(self.gbp_cad, self.sec_type_java)
+        self.java_api_manager.send_message(self.maintenance_request_2)
+        self.sleep(5)
         # endregion
         # region Step 2
         self.request_for_position.set_default()
         self.request_for_position.change_parameters({"Instrument": self.instrument, "Currency": self.currency,
                                                      "Account": self.client})
         self.fix_manager.send_message_and_receive_response(self.request_for_position, self.test_id)
-        self.position_report.set_params_from_reqeust(self.request_for_position)
-        self.position_report.change_parameter("SettlDate", self.settle_date)
-        self.position_report.change_parameter("LastPositEventType", "11")
 
-        self.fix_verifier.check_fix_message(self.position_report, message_name="Check that report contains Account")
+        self.position_report.set_params_from_reqeust(self.request_for_position)
+        self.position_report.change_parameter("LastPositEventType", "11")
+        self.position_report.change_parties(self.account_2)
+        self.position_report_2.set_params_from_reqeust(self.request_for_position)
+        self.position_report_2.change_parameter("LastPositEventType", "11")
+        self.position_report_2.change_parties(self.account_1)
+        self.sleep(3)
+
+        prefilter = {
+            "header": {
+                "MsgType": ("AP", "EQUAL"),
+                "TargetCompID": "PKSTH2",
+                "SenderCompID": "QUODFX_UAT"
+            }
+        }
+        key_params = ["PosReqID"]
+
+        self.fix_verifier.check_fix_message_sequence_standard([self.position_report_2, self.position_report],
+                                                              key_parameters_list=[key_params, key_params],
+                                                              pre_filter=prefilter,
+                                                              message_name="Check that we receive 2 reports")
         # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
