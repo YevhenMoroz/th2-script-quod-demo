3aebda4ece604a8706d11eaaab8b24125304a582
dmogilny <dmitry.mogilny.contractor@quodfinancial.com>
2023-04-24 11:41:37 +0300

    PEQ-10932: WebAdminUI: Automate tests for WaPositions component. Create new TCs and a little for fixes old.

--- test_cases/web_admin/web_admin_test_cases/positions/QAP_T3340.py
+++ test_cases/web_admin/web_admin_test_cases/positions/QAP_T3340.py
@@ -5,11 +5,8 @@ import time
 import traceback
 
 from custom import basic_custom_actions
-from test_framework.db_wrapper.db_manager import DBManager
-
 from test_framework.web_admin_core.pages.positions.cash_positions.main_page import *
 from test_framework.web_admin_core.pages.positions.cash_positions.wizards import *
-from test_framework.web_admin_core.pages.general.common.common_page import CommonPage
 
 from test_framework.web_admin_core.pages.login.login_page import LoginPage
 from test_framework.web_admin_core.pages.root.side_menu import SideMenu
@@ -17,7 +14,7 @@ from test_framework.web_admin_core.utils.web_driver_container import WebDriverCo
 from test_cases.web_admin.web_admin_test_cases.common_test_case import CommonTestCase
 
 
-class QAP_T3496(CommonTestCase):
+class QAP_T3340(CommonTestCase):
 
     def __init__(self, web_driver_container: WebDriverContainer, second_lvl_id, data_set=None, environment=None):
         super().__init__(web_driver_container, self.__class__.__name__, second_lvl_id, data_set=data_set,
@@ -30,8 +27,8 @@ class QAP_T3496(CommonTestCase):
         self.venue_cash_account_id = ''.join(random.sample((string.ascii_uppercase + string.digits) * 6, 6))
         self.currency = 'EUR'
         self.client = ''
-        self.enable_message = f"Cash Position {self.name} Disabled"
-        self.disable_message = f"Cash Position {self.name} Enabled"
+        self.transaction_type = 'ReserveLimit'
+        self.amount = random.randint(50, 150)
 
     def precondition(self):
         login_page = LoginPage(self.web_driver_container)
@@ -39,7 +36,6 @@ class QAP_T3496(CommonTestCase):
         cash_positions_page = MainPage(self.web_driver_container)
         values_tab = ValuesTab(self.web_driver_container)
         wizard = MainWizard(self.web_driver_container)
-        common_act = CommonPage(self.web_driver_container)
 
         login_page.login_to_web_admin(self.login, self.password)
         side_menu.open_cash_positions_page()
@@ -57,32 +53,25 @@ class QAP_T3496(CommonTestCase):
             cash_positions_page.set_name(self.name)
             time.sleep(1)
 
-        self.db_manager.execute_query(f"UPDATE cashaccount SET initialbalance= '0', amountbought = '0', "
-                                      f"amountsoldin = '0', cashdeposited = '0', cashwithdrawn = '0', "
-                                      f"bookedamt = '0' WHERE cashaccountname = '{self.name}'")
-
-        common_act.refresh_page(True)
-        cash_positions_page.set_name(self.name)
-        time.sleep(1)
-
     def test_context(self):
         pass
         cash_positions_page = MainPage(self.web_driver_container)
-        common_act = CommonPage(self.web_driver_container)
+        positions_tab = PositionsTab(self.web_driver_container)
 
         try:
             self.precondition()
 
-            cash_positions_page.click_on_enable_disable_button()
+            cash_positions_page.click_on_transaction()
+            cash_positions_page.set_transaction_type(self.transaction_type)
+            cash_positions_page.set_amount(self.amount)
             cash_positions_page.click_on_ok_button()
             time.sleep(1)
-            self.verify("Entity disabled", self.disable_message, common_act.get_error_pop_up_text())
+            cash_positions_page.click_on_more_actions()
+            cash_positions_page.click_on_edit()
+            time.sleep(1)
 
-            common_act.click_on_info_error_message_pop_up()
-            cash_positions_page.click_on_enable_disable_button()
-            cash_positions_page.click_on_ok_button()
-            time.sleep(1)
-            self.verify("Entity enabled", self.enable_message, common_act.get_error_pop_up_text())
+            self.verify(f"Reserve Limit, is an absolute change to the reserved amount. So it equals to {self.amount}.",
+                        "{:.2f}".format(self.amount), str(positions_tab.get_reserved_limit()))
 
         except Exception:
             basic_custom_actions.create_event("TEST FAILED before or after verifier", self.test_case_id,
