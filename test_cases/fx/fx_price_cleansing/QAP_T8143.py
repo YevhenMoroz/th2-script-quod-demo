b7194aba44829715d3542ff862051009e467cff6
Medentsev <artem.medentsev.contractor@quodfinancial.com>
2023-04-24 18:08:18 +0200

    QAP_T8143: fix

--- test_cases/fx/fx_price_cleansing/QAP_T8143.py
+++ test_cases/fx/fx_price_cleansing/QAP_T8143.py
@@ -43,8 +43,9 @@ class QAP_T8143(TestCase):
         self.settle_type = self.data_set.get_settle_type_by_name("spot")
         self.security_type = self.data_set.get_security_type_by_name("fx_spot")
         self.instr_type_wa = self.data_set.get_fx_instr_type_wa('fx_spot')
-        self.venue_target = self.data_set.get_venue_by_name('venue_2')
-        self.venue_reference = self.data_set.get_venue_by_name('venue_1')
+        self.hsbc = self.data_set.get_venue_by_name('venue_2')
+        self.citi = self.data_set.get_venue_by_name('venue_1')
+        self.jpm = self.data_set.get_venue_by_name('venue_4')
         self.rest_message = RestApiPriceCleansingDeviationMessages(data_set=self.data_set)
         self.rest_manager = RestApiManager(self.rest_env, self.test_id)
         self.rest_message_params = None
@@ -55,20 +56,20 @@ class QAP_T8143(TestCase):
         self.no_related_symbols = [{
             "Instrument": {
                 "Symbol": self.symbol,
-                "CFICode": f"{self.venue_target}-SW",
+                "CFICode": f"{self.hsbc}-SW",
                 "SecurityType": self.security_type,
                 "Product": "4", },
             "SettlType": self.settle_type,
-            'MarketID': f"{self.venue_target}-SW"
+            'MarketID': f"{self.hsbc}-SW"
         }]
-        self.md_id_reference = f"{self.symbol}:{self.instr_type_wa}:REG:{self.venue_reference}"
-        self.md_id_target = f"{self.symbol}:{self.instr_type_wa}:REG:{self.venue_target}"
-        self.md_req_id = f"{self.symbol}:{self.instr_type_wa}:REG:{self.venue_target}" + "_" + str(randint(100, 9999))
-        self.md_entries_reference = []
-        self.md_entries_target = [
+        self.hsbc_for_set = f"{self.symbol}:{self.instr_type_wa}:REG:{self.hsbc}"
+        self.citi_for_set = f"{self.symbol}:{self.instr_type_wa}:REG:{self.citi}"
+        self.jpm_for_set = f"{self.symbol}:{self.instr_type_wa}:REG:{self.jpm}"
+        self.md_req_id = f"{self.symbol}:{self.instr_type_wa}:REG:{self.hsbc}" + "_" + str(randint(100, 9999))
+        self.md_entries_citi = [
             {
                 "MDEntryType": "0",
-                "MDEntryPx": 1.19148,
+                "MDEntryPx": 1.18138,
                 "MDEntrySize": 1000000,
                 "MDQuoteType": 1,
                 "MDEntryPositionNo": 1,
@@ -78,7 +79,7 @@ class QAP_T8143(TestCase):
             },
             {
                 "MDEntryType": "1",
-                "MDEntryPx": 1.19168,
+                "MDEntryPx": 1.18138,
                 "MDEntrySize": 1000000,
                 "MDQuoteType": 1,
                 "MDEntryPositionNo": 1,
@@ -87,37 +88,88 @@ class QAP_T8143(TestCase):
                 "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
             }
         ]
-        self.reference_venues = [
-            {"venueID": self.venue_reference}
+        self.md_entries_jpm = [
+            {
+                "MDEntryType": "0",
+                "MDEntryPx": 1.18158,
+                "MDEntrySize": 1000000,
+                "MDQuoteType": 1,
+                "MDEntryPositionNo": 1,
+                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
+                "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
+            },
+            {
+                "MDEntryType": "1",
+                "MDEntryPx": 1.18158,
+                "MDEntrySize": 1000000,
+                "MDQuoteType": 1,
+                "MDEntryPositionNo": 1,
+                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
+                "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
+            }
         ]
+        self.md_entries_hsbc = [
+            {
+                "MDEntryType": "0",
+                "MDEntryPx": 1.18141,
+                "MDEntrySize": 1000000,
+                "MDQuoteType": 1,
+                "MDEntryPositionNo": 1,
+                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
+                "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
+            },
+            {
+                "MDEntryType": "1",
+                "MDEntryPx": 1.18155,
+                "MDEntrySize": 1000000,
+                "MDQuoteType": 1,
+                "MDEntryPositionNo": 1,
+                "SettlDate": self.data_set.get_settle_date_by_name('spot'),
+                "MDEntryDate": datetime.utcnow().strftime('%Y%m%d'),
+                "MDEntryTime": datetime.utcnow().strftime('%H:%M:%S')
+            }
+        ]
+        self.reference_venues = (self.citi, self.jpm)
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
         # Region Rule creation
-        self.rest_message.set_default_params().create_deviation_cleansing_rule().set_target_venue(self.venue_target). \
-            set_ref_venues(self.reference_venues).set_symbol(self.symbol)
+        self.rest_message.set_default_params().create_deviation_cleansing_rule().set_target_venue(self.hsbc). \
+            clear_ref_venues().add_ref_venues(self.reference_venues).set_symbol(self.symbol).set_deviation("5")
         self.rest_message_params = self.rest_manager.parse_create_response(
             self.rest_manager.send_multiple_request(self.rest_message))
         time.sleep(3)
         # endregion
-        self.fix_md.set_market_data()
-        self.fix_md.change_parameter("MDReqID", self.md_id_reference)
-        self.fix_md.change_parameter("NoMDEntries", self.md_entries_reference)
+        self.fix_md.change_parameter("MDReqID", self.hsbc_for_set)
+        self.fix_md.change_parameter("NoMDEntries", self.md_entries_hsbc)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_reference}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.hsbc_for_set}")
         time.sleep(3)
 
-        self.fix_md.change_parameter("MDReqID", self.md_id_target)
-        self.fix_md.change_parameter("NoMDEntries", self.md_entries_target)
+        self.fix_md.set_market_data()
+        self.fix_md.change_parameter("MDReqID", self.citi_for_set)
+        self.fix_md.change_parameter("NoMDEntries", self.md_entries_citi)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_target}")
-        time.sleep(6)
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.citi_for_set}")
+        time.sleep(3)
+
+        self.fix_md.change_parameter("MDReqID", self.jpm_for_set)
+        self.fix_md.change_parameter("NoMDEntries", self.md_entries_jpm)
+        self.fix_md.change_parameter("Instrument", self.instrument)
+        self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
+                                   self.fx_fh_connectivity,
+                                   'FX')
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.jpm_for_set}")
+        time.sleep(10)
 
         self.md_request.set_md_req_parameters_taker(). \
             change_parameters({'MDReqID': self.md_req_id}). \
@@ -138,19 +190,26 @@ class QAP_T8143(TestCase):
         self.rest_manager.send_post_request(self.rest_message)
 
         self.fix_md.set_market_data()
-        self.fix_md.change_parameter("MDReqID", self.md_id_target)
+        self.fix_md.change_parameter("MDReqID", self.hsbc_for_set)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_target}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.hsbc_for_set}")
 
-        self.fix_md.change_parameter("MDReqID", self.md_id_reference)
+        self.fix_md.change_parameter("MDReqID", self.citi_for_set)
         self.fix_md.change_parameter("Instrument", self.instrument)
         self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
                                    self.fx_fh_connectivity,
                                    'FX')
-        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.md_id_reference}")
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.jpm_for_set}")
+
+        self.fix_md.change_parameter("MDReqID", self.citi_for_set)
+        self.fix_md.change_parameter("Instrument", self.instrument)
+        self.fix_md.update_MDReqID(self.fix_md.get_parameter("MDReqID"),
+                                   self.fx_fh_connectivity,
+                                   'FX')
+        self.fix_manager_gtw.send_message(self.fix_md, f"Send MD {self.jpm_for_set}")
 
         self.md_request.set_md_uns_parameters_maker(). \
             change_parameters({'MDReqID': self.md_req_id})
