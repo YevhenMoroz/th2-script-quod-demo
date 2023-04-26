28772c63596a928424f3fa52bb3f92335ad757d5
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-04-26 09:03:30 +0200

    Add new cases

--- regression_cycle/fx_regression_cycle/fx_mm_AH_regression.py
+++ regression_cycle/fx_regression_cycle/fx_mm_AH_regression.py
@@ -1,6 +1,8 @@
+from test_cases.fx.fx_mm_autohedging.QAP_T10301 import QAP_T10301
 from test_cases.fx.fx_mm_autohedging.QAP_T10439 import QAP_T10439
 from test_cases.fx.fx_mm_autohedging.QAP_T10444 import QAP_T10444
 from test_cases.fx.fx_mm_autohedging.QAP_T10775 import QAP_T10775
+from test_cases.fx.fx_mm_autohedging.QAP_T10780 import QAP_T10780
 from test_cases.fx.fx_mm_autohedging.QAP_T2681 import QAP_T2681
 from test_cases.fx.fx_mm_autohedging.QAP_T2682 import QAP_T2682
 from test_cases.fx.fx_mm_autohedging.QAP_T2862 import QAP_T2862
@@ -24,7 +26,9 @@ def test_run(parent_id=None, version=None):
 
     configuration = ComponentConfiguration("AutoHedger")
     try:
-        prepare_position()
+        # prepare_position()
+        QAP_T10301(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10444(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T2681(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T2681(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T2862(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
@@ -34,6 +38,7 @@ def test_run(parent_id=None, version=None):
         QAP_T10439(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10444(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10775(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10780(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
 
     except Exception:
         logging.error("Error execution", exc_info=True)
