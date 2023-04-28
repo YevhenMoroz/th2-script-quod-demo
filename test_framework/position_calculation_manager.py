1aabe520e02011a3cb3359f358f8a5c078d5dbb9
Vadim <vadim.skulinec.contractor@quodfinancial.com>
2023-04-28 13:41:08 +0300

    PEQ-11139 add QAP_T6939

--- test_framework/position_calculation_manager.py
+++ test_framework/position_calculation_manager.py
@@ -80,23 +80,26 @@ class PositionCalculationManager:
                 return '0.0'
 
     @staticmethod
-    def calculate_buy_avg_px(posit_qty, exec_qty, exec_price, buy_avg_px, cross_rate='1', fees='0',
-                             client_commission='0'):
-        if float(posit_qty) >= 0 or (float(posit_qty) < 0 and float(exec_qty) < abs(float(posit_qty))):
-            buy_avg_px = (float(buy_avg_px) * float(posit_qty) + float(exec_qty) * float(cross_rate) *
-                          float(exec_price) + float(fees) *
-                          float(cross_rate) + float(client_commission) * float(cross_rate)) / \
-                         (float(posit_qty) + float(exec_qty))
-        elif float(posit_qty) < 0:
-            if float(exec_qty) > abs(float(posit_qty)):
-                buy_avg_px = (float(exec_qty) * float(cross_rate) *
-                              float(exec_price) + float(fees) *
-                              float(cross_rate) + float(client_commission) * float(cross_rate)) / \
-                             (float(exec_qty))
-            elif float(exec_qty) == abs(float(posit_qty)):
-                buy_avg_px = 0
+    def calculate_buy_avg_px_execution_buy_side(posit_qty, exec_qty, exec_price, buy_avg_px, cross_rate='1', fees='0',
+                                                client_commission='0'):
+        if float(posit_qty) >= 0:
+            buy_avg_px = (float(buy_avg_px) * float(posit_qty) + float(exec_qty) * float(exec_price) * float(
+                cross_rate) + (float(client_commission) + float(fees)) * float(cross_rate)) / (
+                                 float(posit_qty) + float(exec_qty))
+            return str(buy_avg_px)
+        if float(posit_qty) < 0:
+            if float(exec_qty) < -float(posit_qty):
+                buy_avg_px = (float(buy_avg_px) * float(posit_qty) + float(exec_qty) * float(exec_price) * float(
+                    cross_rate) + (float(client_commission) + float(fees)) * float(cross_rate)) / (
+                                     float(posit_qty) + float(exec_qty))
+                return str(buy_avg_px)
+            if float(exec_qty) > -float(posit_qty):
+                buy_avg_px = (float(exec_qty) * float(exec_price) * float(cross_rate) + (
+                            float(fees) + float(client_commission)) * float(cross_rate)) / float(exec_qty)
+                return str(buy_avg_px)
 
-        return buy_avg_px
+            if float(exec_qty)==-float(posit_qty):
+                return '0.0'
 
     @staticmethod
     def calculate_net_weighted_avg_px_for_position_transfer_source_acc(posit_qty, qty_to_transfer, net_weighted_avg_px,
