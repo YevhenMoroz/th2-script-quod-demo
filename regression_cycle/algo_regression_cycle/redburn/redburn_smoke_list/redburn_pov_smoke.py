e52e76f88c981b333ea6fe78539a4d1e7e18e930
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2023-04-28 14:27:53 +0300

    PALGO-1248 - add new smoke test

--- regression_cycle/algo_regression_cycle/redburn/redburn_smoke_list/redburn_pov_smoke.py
+++ regression_cycle/algo_regression_cycle/redburn/redburn_smoke_list/redburn_pov_smoke.py
@@ -9,6 +9,7 @@ from test_cases.algo.Algo_Redburn.Algo_POV.QAP_T8845 import QAP_T8845
 from test_cases.algo.Algo_Redburn.Algo_POV_Scaling.QAP_T4464 import QAP_T4464
 from test_cases.algo.Algo_Redburn.Algo_POV_Scaling.QAP_T4470 import QAP_T4470
 from test_cases.algo.Algo_Redburn.Algo_POV_Scaling.QAP_T4478 import QAP_T4478
+from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4386 import QAP_T4386
 from test_framework.configurations.component_configuration import ComponentConfigurationAlgo
 
 
@@ -53,6 +54,7 @@ def test_run(parent_id=None, version=None):
         # endregion
 
         # region Auction
+        QAP_T4386(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         # endregion
 
         # region POV Scaling
