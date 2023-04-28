dd92a7bd75ba52b7fd3be3f2a62e7554e1004bd2
Vadim <vadim.skulinec.contractor@quodfinancial.com>
2023-04-28 15:07:06 +0300

    PEQ-11139 modify QAP_T6939 (1.2) add QAP_T6942

--- test_cases/eq/Positions/QAP_T6942.py
+++ test_cases/eq/Positions/QAP_T6942.py
@@ -8,9 +8,9 @@ from test_framework.core.try_exept_decorator import try_except
 from test_framework.data_sets.message_types import PKSMessageType, ORSMessageType
 from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
 from test_framework.java_api_wrappers.java_api_constants import JavaApiFields, SubscriptionRequestTypes, PosReqTypes, \
-    OrderReplyConst, ExecutionReportConst
-from test_framework.java_api_wrappers.oms.es_messages.ExecutionReportOMS import ExecutionReportOMS
+    OrderReplyConst, ExecutionReportConst, SubmitRequestConst
 from test_framework.java_api_wrappers.oms.ors_messges.OrderSubmitOMS import OrderSubmitOMS
+from test_framework.java_api_wrappers.oms.ors_messges.TradeEntryOMS import TradeEntryOMS
 from test_framework.java_api_wrappers.ors_messages.CancelOrderRequest import CancelOrderRequest
 from test_framework.java_api_wrappers.pks_messages.RequestForPositions import RequestForPositions
 from test_framework.position_calculation_manager import PositionCalculationManager
@@ -22,22 +22,22 @@ timeouts = True
 
 
 @try_except(test_id=Path(__file__).name[:-3])
-class QAP_T6939(TestCase):
+class QAP_T6942(TestCase):
     def __init__(self, report_id, session_id=None, data_set=None, environment=None):
         super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
         self.ja_manager = JavaApiManager(environment.get_list_java_api_environment()[0].java_api_conn, self.test_id)
-        self.venue_client_name = self.data_set.get_venue_client_names_by_name('client_pos_3_venue_1')
         self.client = self.data_set.get_client_by_name("client_pos_3")
         self.instrument_id = self.data_set.get_instrument_id_by_name("instrument_1")
         self.fix_env = self.environment.get_list_fix_environment()[0]
-        self.mic = self.data_set.get_mic_by_name('mic_1')
-        self.bs_connectivity = self.fix_env.buy_side
-        self.order_submit = OrderSubmitOMS(data_set).set_default_dma_limit()
-        self.exec_rep = ExecutionReportOMS(data_set)
+        self.order_submit = OrderSubmitOMS(data_set).set_default_care_limit(
+            recipient=self.environment.get_list_fe_environment()[0].user_1,
+            desk=self.environment.get_list_fe_environment()[0].desk_ids[0],
+            role=SubmitRequestConst.USER_ROLE_1.value)
+        self.trade_entry_oms = TradeEntryOMS(self.data_set)
         self.rule_manager = RuleManager(sim=Simulators.equity)
         self.price = '2'
-        self.qty = '120'
+        self.qty = '100'
         self.request_for_position = RequestForPositions()
         self.sec_account = self.data_set.get_account_by_name("client_pos_3_acc_4")  # "PROP_TEST"
         self.wa_connectivity = self.environment.get_list_web_admin_rest_api_environment()[0].session_alias_wa
@@ -60,7 +60,7 @@ class QAP_T6939(TestCase):
         self.rest_commission_sender.send_post_request()
         # endregion
 
-        # region step 1: Create DMA order
+        # region step 1: Create CO order
         self.order_submit.update_fields_in_component(JavaApiFields.NewOrderSingleBlock.value,
                                                      {JavaApiFields.PreTradeAllocationBlock.value: {
                                                          JavaApiFields.PreTradeAllocationList.value: {
@@ -73,32 +73,21 @@ class QAP_T6939(TestCase):
         self.ja_manager.send_message_and_receive_response(self.order_submit, response_time=15_000)
         order_reply = self.ja_manager.get_last_message(ORSMessageType.OrderReply.value).get_parameters()[
             JavaApiFields.OrdReplyBlock.value]
-        self.ja_manager.compare_values({JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_SEN.value},
+        self.ja_manager.compare_values({JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_OPN.value},
                                        order_reply, 'Verify that order created (step 1)')
         order_id = order_reply[JavaApiFields.OrdID.value]
         # endregion
 
-        # region step 2: Partially Fill DMA order
+        # region step 2: Partially Fill CO order
         position = self._extract_cum_values_for_acc(self.sec_account)
-        self.exec_rep.set_default_trade(order_id)
         trade_qty = '50'
-        remaining_qty = str(float(self.qty) - float(trade_qty))
-        self.exec_rep.update_fields_in_component(JavaApiFields.ExecutionReportBlock.value,
-                                                 {
-                                                     JavaApiFields.OrdQty.value: self.qty,
-                                                     JavaApiFields.LastTradedQty.value: trade_qty,
-                                                     JavaApiFields.LastPx.value: self.price,
-                                                     JavaApiFields.Price.value: self.price,
-                                                     JavaApiFields.LeavesQty.value: remaining_qty,
-                                                     JavaApiFields.CumQty.value: trade_qty,
-                                                     JavaApiFields.AvgPrice.value: self.price
-                                                 })
-        self.ja_manager.send_message_and_receive_response(self.exec_rep)
+        self.trade_entry_oms.set_default_trade(order_id, self.price, trade_qty)
+        self.ja_manager.send_message_and_receive_response(self.trade_entry_oms)
         execution_report = self.ja_manager.get_last_message(ORSMessageType.ExecutionReport.value).get_parameters()[
             JavaApiFields.ExecutionReportBlock.value]
         self.ja_manager.compare_values(
             {JavaApiFields.TransExecStatus.value: ExecutionReportConst.TransExecStatus_PFL.value},
-            execution_report, 'Verify that DMA order partially filled (step 2)')
+            execution_report, 'Verify that CO order partially filled (step 2)')
         # endregion
 
         # region step 3: Check that BuyAvgPx has properly calculated value
@@ -120,26 +109,15 @@ class QAP_T6939(TestCase):
                                        f'Verify that {JavaApiFields.BuyAvgPx.value} of {self.sec_account} has properly calculated value (step 3)')
         # endregion
 
-        # region step 4: Trade DMA order again
-        remaining_qty = float(remaining_qty) - float(trade_qty)
+        # region step 4: Trade Care order again
         second_price = '1.2'
-        self.exec_rep.update_fields_in_component(JavaApiFields.ExecutionReportBlock.value,
-                                                 {
-                                                     JavaApiFields.OrdQty.value: self.qty,
-                                                     JavaApiFields.LastTradedQty.value: trade_qty,
-                                                     JavaApiFields.LastPx.value: second_price,
-                                                     JavaApiFields.Price.value: second_price,
-                                                     JavaApiFields.LeavesQty.value: remaining_qty,
-                                                     JavaApiFields.CumQty.value: trade_qty,
-                                                     JavaApiFields.VenueExecID.value: bca.client_orderid(9),
-                                                     JavaApiFields.AvgPrice.value: '1.6'
-                                                 })
-        self.ja_manager.send_message_and_receive_response(self.exec_rep)
+        self.trade_entry_oms.set_default_trade(order_id, second_price, trade_qty)
+        self.ja_manager.send_message_and_receive_response(self.trade_entry_oms)
         execution_report = self.ja_manager.get_last_message(ORSMessageType.ExecutionReport.value).get_parameters()[
             JavaApiFields.ExecutionReportBlock.value]
         self.ja_manager.compare_values(
-            {JavaApiFields.TransExecStatus.value: ExecutionReportConst.TransExecStatus_PFL.value},
-            execution_report, 'Verify that DMA order partially filled (step 4)')
+            {JavaApiFields.TransExecStatus.value: ExecutionReportConst.TransExecStatus_FIL.value},
+            execution_report, 'Verify that Care order partially filled (step 4)')
         # endregion
 
         # region step 5 : Verify that BuyAvgPx value calculated properly
@@ -161,31 +139,26 @@ class QAP_T6939(TestCase):
                                        f'Verify that {JavaApiFields.BuyAvgPx.value} of {self.sec_account} has properly calculated value (step 5)')
         # endregion
 
-        # region step 6: Cancel DMA order
-        order_cancel_rule = None
-        try:
-            order_cancel_rule = self.rule_manager.add_OrderCancelRequest_FIXStandard(
-                self.fix_env.buy_side, self.venue_client_name, self.mic, True)
-            self.cancel_request.set_default(order_id)
-            self.ja_manager.send_message_and_receive_response(self.cancel_request)
-            ord_reply = self.ja_manager.get_last_message(ORSMessageType.OrdReply.value,
-                                                        OrderReplyConst.TransStatus_CXL.value).get_parameter(
-                JavaApiFields.OrdReplyBlock.value)
-            self.ja_manager.compare_values({JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_CXL.value},
-                                                 ord_reply, 'Verify that order canceled (step 6)')
-            position_after_cancel = \
-                self.ja_manager.get_last_message_by_multiple_filter(PKSMessageType.PositionReport.value,
-                                                                    [JavaApiFields.PositQty.value,
-                                                                     self.sec_account]).get_parameters()[
-                    JavaApiFields.PositionReportBlock.value][JavaApiFields.PositionList.value][
-                    JavaApiFields.PositionBlock.value][0]
-            self.ja_manager.compare_values({JavaApiFields.BuyAvgPx.value: buy_avg_px_expected_after_second_trade},
-                                           position_after_cancel,
-                                           f'Verify that {JavaApiFields.BuyAvgPx.value} does not change (step 6)')
-        except Exception as ex:
-            logger.exception(f'{ex} - your exception', exc_info=True)
-        finally:
-            self.rule_manager.remove_rule(order_cancel_rule)
+        # region step 6: Cancel Care order
+        self.cancel_request.set_default(order_id)
+        self.ja_manager.send_message_and_receive_response(self.cancel_request)
+        ord_reply = self.ja_manager.get_last_message(ORSMessageType.OrdReply.value,
+                                                     OrderReplyConst.TransStatus_CXL.value).get_parameter(
+            JavaApiFields.OrdReplyBlock.value)
+        self.ja_manager.compare_values({JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_CXL.value},
+                                       ord_reply, 'Verify that order canceled (step 6)')
+        # endregion
+
+        # region step 7: Check BuyAvgPx
+        position_after_cancel = \
+            self.ja_manager.get_last_message_by_multiple_filter(PKSMessageType.PositionReport.value,
+                                                                [JavaApiFields.PositQty.value,
+                                                                 self.sec_account]).get_parameters()[
+                JavaApiFields.PositionReportBlock.value][JavaApiFields.PositionList.value][
+                JavaApiFields.PositionBlock.value][0]
+        self.ja_manager.compare_values({JavaApiFields.BuyAvgPx.value: buy_avg_px_expected_after_second_trade},
+                                       position_after_cancel,
+                                       f'Verify that {JavaApiFields.BuyAvgPx.value} does not change (step 7)')
         # endregion
 
     def _extract_cum_values_for_acc(self, acc):
