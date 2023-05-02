17e3ad3e60ad05039591b7bb854e5c9b1c5c0a8a
Sergey Rublyov <sergey.rublyov.contractor@quodfinancial.com>
2023-05-02 15:19:20 +0300

    PALGO-1322 - add new POV auction test to rb regression script

--- regression_cycle/algo_regression_cycle/redburn/redburn_regresion_cycle/redburn_pov_aution_regression.py
+++ regression_cycle/algo_regression_cycle/redburn/redburn_regresion_cycle/redburn_pov_aution_regression.py
@@ -2,6 +2,7 @@ import logging
 from custom import basic_custom_actions as bca
 from stubs import Stubs
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T11233 import QAP_T11233
+from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T11234 import QAP_T11234
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4386 import QAP_T4386
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T8719 import QAP_T8719
 from test_framework.configurations.component_configuration import ComponentConfigurationAlgo
@@ -23,6 +24,7 @@ def test_run(parent_id=None, version=None):
         QAP_T4386(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T8719(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T11233(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T11234(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         pass
 
     except Exception:
