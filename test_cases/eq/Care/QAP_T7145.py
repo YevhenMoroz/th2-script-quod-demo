c1c78165e8f4670e6023a3668fa3fce675fb1345
Vadim <vadim.skulinec.contractor@quodfinancial.com>
2023-05-08 22:43:30 +0300

    PEQ-11329 add QAP_T7145

---
+++ test_cases/eq/Care/QAP_T7145.py
@@ -0,0 +1,148 @@
+import logging
+import os
+import time
+from pathlib import Path
+
+from pkg_resources import resource_filename
+import xml.etree.ElementTree as ET
+from custom import basic_custom_actions as bca
+from rule_management import RuleManager, Simulators
+from test_framework.core.test_case import TestCase
+from test_framework.core.try_exept_decorator import try_except
+from test_framework.data_sets.message_types import CSMessageType, ORSMessageType
+from test_framework.db_wrapper.db_manager import DBManager
+from test_framework.fix_wrappers.DataSet import DirectionEnum
+from test_framework.fix_wrappers.FixManager import FixManager
+from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.fix_wrappers.oms.FixMessageCancelRejectReportOMS import FixMessageOrderCancelRejectReportOMS
+from test_framework.fix_wrappers.oms.FixMessageNewOrderSingleOMS import FixMessageNewOrderSingleOMS
+from test_framework.fix_wrappers.oms.FixMessageOrderCancelReplaceRequestOMS import \
+    FixMessageOrderCancelReplaceRequestOMS
+from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.cs_message.CDOrdAckBatchRequest import CDOrdAckBatchRequest
+from test_framework.java_api_wrappers.java_api_constants import JavaApiFields, OrderReplyConst
+from test_framework.java_api_wrappers.oms.ors_messges.FixNewOrderSingleOMS import FixNewOrderSingleOMS
+from test_framework.java_api_wrappers.oms.ors_messges.FixOrderModificationRequestOMS import \
+    FixOrderModificationRequestOMS
+from test_framework.java_api_wrappers.oms.ors_messges.OrderSubmitOMS import OrderSubmitOMS
+from test_framework.ssh_wrappers.ssh_client import SshClient
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+timeouts = True
+
+
+@try_except(test_id=Path(__file__).name[:-3])
+class QAP_T7145(TestCase):
+    @try_except(test_id=Path(__file__).name[:-3])
+    def __init__(self, report_id, session_id=None, data_set=None, environment=None):
+        super().__init__(report_id, session_id, data_set, environment)
+        self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
+        self.fix_env = self.environment.get_list_fix_environment()[0]
+        self.fix_verifier = FixVerifier(self.fix_env.sell_side, self.test_id)
+        self.java_api_connectivity = self.environment.get_list_java_api_environment()[0].java_api_conn
+        self.java_api_connectivity2 = self.environment.get_list_java_api_environment()[0].java_api_conn_user2
+        self.fix_manager = FixManager(self.fix_env.sell_side, self.test_id)
+        self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
+        self.java_api_manager2 = JavaApiManager(self.java_api_connectivity2, self.test_id)
+        self.new_order = FixMessageNewOrderSingleOMS(self.data_set)
+        self.order_modification_request = FixMessageOrderCancelReplaceRequestOMS(self.data_set)
+        self.client = self.data_set.get_client_by_name("client_2")
+        self.venue = self.data_set.get_mic_by_name('mic_1')
+        self.venue_client_names = self.data_set.get_venue_client_names_by_name('client_2_venue_1')
+        self.accept_request = CDOrdAckBatchRequest()
+        self.ssh_client_env = self.environment.get_list_ssh_client_environment()[0]
+        self.ssh_client = SshClient(self.ssh_client_env.host, self.ssh_client_env.port, self.ssh_client_env.user,
+                                    self.ssh_client_env.password, self.ssh_client_env.su_user,
+                                    self.ssh_client_env.su_password)
+        self.local_path = resource_filename("test_resources.be_configs.oms_be_configs", "client_cs.xml")
+        self.remote_path = f"/home/{self.ssh_client_env.su_user}/quod/cfg/client_cs.xml"
+        self.rule_manager = RuleManager(Simulators.equity)
+        self.db_manager = DBManager(environment.get_list_data_base_environment()[0])
+        self.order_cancel_reject = FixMessageOrderCancelRejectReportOMS()
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_pre_conditions_and_steps(self):
+        # region prepare pre-precondition:
+        tree = ET.parse(self.local_path)
+        cs = tree.getroot().find("cs/fixAutoAcknowledge")
+        cs.text = 'false'
+        tree.write("temp.xml")
+        self.ssh_client.send_command('~/quod/script/site_scripts/change_permission_script')
+        self.ssh_client.put_file(self.remote_path, "temp.xml")
+        self.ssh_client.send_command("qrestart CS")
+        time.sleep(80)
+        # endergion
+
+        # region  step 1: create CO order via FIX
+        desk = self.environment.get_list_fe_environment()[0].desk_ids[0]
+        self.new_order.set_default_care_limit(account='client_2')
+        price = self.new_order.get_parameters()[JavaApiFields.Price.value]
+        self.new_order.add_tag({'header': {
+            JavaApiFields.SenderSubID.value: 'SENDER_SUB_ID',
+            JavaApiFields.TargetSubID.value: 'TARGET_SUB_ID',
+            JavaApiFields.DeliverToCompID.value: 'DELIVER_TO_COMP_ID',
+            JavaApiFields.DeliverToSubID.value: 'DELIVER_TO_SUB_ID',
+            JavaApiFields.OnBehalfOfCompID.value: 'ON_BEHALF_OF_COMP_ID',
+            JavaApiFields.OnBehalfOfSubID.value: 'ON_BEHALF_OF_SUB_ID'
+        }})
+        self.fix_manager.send_message_and_receive_response_fix_standard(self.new_order)
+        execution_report_last = self.fix_manager.get_last_message('ExecutionReport').get_parameters()
+        parent_order_id = execution_report_last['OrderID']
+        cd_ord_notif_id = str(int(
+            self.db_manager.execute_query(f"SELECT cdordnotifid FROM cdordnotif WHERE transid = '{parent_order_id}'")[
+                0][0]))
+        self.accept_request.set_default(parent_order_id, cd_ord_notif_id, desk)
+        self.java_api_manager.send_message_and_receive_response(self.accept_request)
+        order_reply = self.java_api_manager.get_last_message(ORSMessageType.OrdReply.value).get_parameters()[
+            JavaApiFields.OrdReplyBlock.value]
+        self.java_api_manager.compare_values(
+            {JavaApiFields.TransStatus.value: OrderReplyConst.TransStatus_OPN.value}, order_reply,
+            f'Verifying that order created (step 1)')
+
+        # endregion
+
+        # region step 2:
+        self.order_modification_request.set_default(self.new_order)
+        self.fix_manager.send_message(self.order_modification_request)
+        # endreigon
+
+        # region reject modify request:step 2-3
+        cd_ord_notif_id = str(int(
+            self.db_manager.execute_query(
+                f"SELECT cdordnotifid FROM cdordnotif WHERE transid = '{parent_order_id}' AND cdrequesttype='MOD'")[
+                0][0]))
+        self.accept_request.set_default(parent_order_id, cd_ord_notif_id, desk, 'M', set_reject=True)
+        self.java_api_manager.send_message_and_receive_response(self.accept_request)
+        order_reply = self.java_api_manager.get_last_message(ORSMessageType.OrdReply.value).get_parameters()[
+            JavaApiFields.OrdReplyBlock.value]
+        self.java_api_manager.compare_values(
+            {JavaApiFields.Price.value: str(float(price))}, order_reply,
+            f'Verifying that order is not changed (step 3)')
+        # endregion
+
+        # region step 4:
+        list_ignore_field = ['CxlRejResponseTo', 'Account',
+                             'Text', 'MsgType', 'MsgSeqNum',
+                             'TargetCompID', 'SenderCompID', 'BeginString',
+                             'BodyLength', 'ApplVerID', 'SendingTime']
+        self.order_cancel_reject.set_default(self.new_order)
+        self.order_cancel_reject.change_parameters({'header': {
+            JavaApiFields.SenderSubID.value: 'TARGET_SUB_ID',
+            JavaApiFields.TargetSubID.value: 'SENDER_SUB_ID',
+            JavaApiFields.DeliverToCompID.value: 'DELIVER_TO_SUB_ID',
+            JavaApiFields.DeliverToSubID.value: 'DELIVER_TO_COMP_ID',
+            JavaApiFields.OnBehalfOfCompID.value: 'ON_BEHALF_OF_SUB_ID',
+            JavaApiFields.OnBehalfOfSubID.value: 'ON_BEHALF_OF_COMP_ID',
+        }})
+        self.fix_verifier.check_fix_message_fix_standard(self.order_cancel_reject, ignored_fields=list_ignore_field,
+                                                         ignore_header=False)
+        # endregion
+
+    @try_except(test_id=Path(__file__).name[:-3])
+    def run_post_conditions(self):
+        self.ssh_client.put_file(self.remote_path, self.local_path)
+        self.ssh_client.send_command("qrestart CS")
+        os.remove('temp.xml')
+        time.sleep(80)
+        self.ssh_client.close()
--- test_framework/fix_wrappers/FixVerifier.py
+++ test_framework/fix_wrappers/FixVerifier.py
@@ -29,8 +29,10 @@ class FixVerifier:
         self.__case_id = case_id
 
     def check_fix_message(self, fix_message: FixMessage, key_parameters: list = None,
-                          direction: DirectionEnum = DirectionEnum.FromQuod, message_name: str = None, ignored_fields: list = None):
-        if fix_message.is_parameter_exist('TransactTime') and fix_message.get_parameter('TransactTime')[0] not in ('!', '%', '<', '>', '%'):
+                          direction: DirectionEnum = DirectionEnum.FromQuod, message_name: str = None,
+                          ignored_fields: list = None):
+        if fix_message.is_parameter_exist('TransactTime') and fix_message.get_parameter('TransactTime')[0] not in (
+        '!', '%', '<', '>', '%'):
             fix_message = deepcopy(fix_message)
             fix_message.change_parameter('TransactTime', "*")
         if fix_message.get_message_type() == FIXMessageType.NewOrderSingle.value:
@@ -40,7 +42,8 @@ class FixVerifier:
             if message_name is None:
                 message_name = "Check NewOrderSingle"
 
-            if fix_message.is_parameter_exist('TransactTime') and fix_message.get_parameter('TransactTime')[0] not in ('!', '%', '<', '>', '%'):
+            if fix_message.is_parameter_exist('TransactTime') and fix_message.get_parameter('TransactTime')[0] not in (
+            '!', '%', '<', '>', '%'):
                 fix_message.change_parameter('TransactTime', fix_message.get_parameter('TransactTime').split('.')[0])
                 if re.compile(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:00').search(fix_message.get_parameter('TransactTime')):
                     fix_message.change_parameter('TransactTime', fix_message.get_parameter('TransactTime')[:-3])
@@ -89,7 +92,7 @@ class FixVerifier:
                 basic_custom_actions.create_check_rule(
                     message_name,
                     basic_custom_actions.filter_to_grpc("OrderCancelReplaceRequest", fix_message.get_parameters(),
-                                                        key_parameters,ignored_fields),
+                                                        key_parameters, ignored_fields),
                     self.__checkpoint,
                     self.__session_alias,
                     self.__case_id,
@@ -160,7 +163,7 @@ class FixVerifier:
                 basic_custom_actions.create_check_rule(
                     "Check OrderCancelReject",
                     basic_custom_actions.filter_to_grpc(FIXMessageType.OrderCancelReject.value,
-                                                                     fix_message.get_parameters(), key_parameters, ignored_fields),
+                                                        fix_message.get_parameters(), key_parameters, ignored_fields),
                     self.__checkpoint,
                     self.__session_alias,
                     self.__case_id,
@@ -246,7 +249,8 @@ class FixVerifier:
             self.__verifier.submitCheckRule(
                 basic_custom_actions.create_check_rule(
                     message_name,
-                    basic_custom_actions.filter_to_grpc("BusinessMessageReject", fix_message.get_parameters(), key_parameters, ignored_fields),
+                    basic_custom_actions.filter_to_grpc("BusinessMessageReject", fix_message.get_parameters(),
+                                                        key_parameters, ignored_fields),
                     self.__checkpoint,
                     self.__session_alias,
                     self.__case_id,
@@ -291,7 +295,8 @@ class FixVerifier:
             pass
         # TODO add exeption into else
 
-    def check_fix_message_sequence(self, fix_messages_list: list, key_parameters_list: list = None, direction: DirectionEnum = DirectionEnum.FromQuod,
+    def check_fix_message_sequence(self, fix_messages_list: list, key_parameters_list: list = None,
+                                   direction: DirectionEnum = DirectionEnum.FromQuod,
                                    message_name: str = None, pre_filter: dict = None, check_order=True):
         if pre_filter is None:
             pre_filter = {
@@ -300,16 +305,18 @@ class FixVerifier:
                 }
             }
 
-
         pre_filter_req = basic_custom_actions.prefilter_to_grpc(pre_filter)
         message_filters_req = list()
         if len(fix_messages_list) != len(key_parameters_list):
-            raise ValueError("Not correct qty of object at list expect len(fix_messages_list) equal len(key_parameters_list)")
+            raise ValueError(
+                "Not correct qty of object at list expect len(fix_messages_list) equal len(key_parameters_list)")
 
         for index, message in enumerate(fix_messages_list):
             if not isinstance(message, FixMessage):
                 raise ValueError("Not correct object type at fix_messages_list, expect only FixMessages")
-            message_filters_req.append(basic_custom_actions.filter_to_grpc(message.get_message_type(), message.get_parameters(), key_parameters_list[index]))
+            message_filters_req.append(
+                basic_custom_actions.filter_to_grpc(message.get_message_type(), message.get_parameters(),
+                                                    key_parameters_list[index]))
 
         if message_name is None:
             message_name = "Check banch of messages"
@@ -327,7 +334,8 @@ class FixVerifier:
             )
         )
 
-    def check_no_message_found(self, message_timeout: 10000, direction: DirectionEnum = DirectionEnum.FromQuod, message_name: str = None, pre_filter: dict = None):
+    def check_no_message_found(self, message_timeout: 10000, direction: DirectionEnum = DirectionEnum.FromQuod,
+                               message_name: str = None, pre_filter: dict = None):
         if pre_filter is None:
             pre_filter = {
                 'header': {
@@ -339,7 +347,7 @@ class FixVerifier:
         if message_name is None:
             message_name = "Check no message found"
 
-        self.__verifier.submitNoMessageCheck (
+        self.__verifier.submitNoMessageCheck(
             basic_custom_actions.create_check_no_message_found(
                 description=message_name,
                 prefilter=pre_filter_req,
@@ -352,9 +360,9 @@ class FixVerifier:
             )
         )
 
-
     def check_fix_message_fix_standard(self, fix_message: FixMessage, key_parameters: list = None,
-                                       direction: DirectionEnum = DirectionEnum.FromQuod, ignored_fields: list = None):
+                                       direction: DirectionEnum = DirectionEnum.FromQuod, ignored_fields: list = None,
+                                       ignore_header=True):
         if fix_message.get_message_type() == FIXMessageType.NewOrderSingle.value:
             if key_parameters is None:
                 key_parameters = ['ClOrdID', 'OrdStatus']
@@ -364,7 +372,7 @@ class FixVerifier:
                     "Check NewOrderSingle",
                     basic_custom_actions.filter_to_grpc_fix_standard(FIXMessageType.NewOrderSingle.value,
                                                                      fix_message.get_parameters(), key_parameters,
-                                                                     ignored_fields),
+                                                                     ignored_fields, ignore_header),
                     self.__checkpoint,
                     self.__session_alias,
                     self.__case_id,
@@ -443,7 +451,7 @@ class FixVerifier:
                     "Check OrderCancelReject",
                     basic_custom_actions.filter_to_grpc_fix_standard(FIXMessageType.OrderCancelReject.value,
                                                                      fix_message.get_parameters(), key_parameters,
-                                                                     ignored_fields),
+                                                                     ignored_fields, ignore_header),
                     self.__checkpoint,
                     self.__session_alias,
                     self.__case_id,
@@ -500,5 +508,7 @@ class FixVerifier:
             pass
         # TODO add exeption into else
 
-    def check_fix_message_kepler(self, fix_message: FixMessage, key_parameters: list = None, direction: DirectionEnum = DirectionEnum.FromQuod, message_name: str = None, ignored_fields: Union[list, tuple] = ('trailer', 'header', 'NoTradingSessions')):
+    def check_fix_message_kepler(self, fix_message: FixMessage, key_parameters: list = None,
+                                 direction: DirectionEnum = DirectionEnum.FromQuod, message_name: str = None,
+                                 ignored_fields: Union[list, tuple] = ('trailer', 'header', 'NoTradingSessions')):
         self.check_fix_message(fix_message, key_parameters, direction, message_name, ignored_fields)
