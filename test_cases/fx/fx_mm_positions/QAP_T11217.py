205b25b80da5a9c0012ef1d46ff2729e1bf6fd39
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-05-19 09:06:26 +0200

    PFX-6280 - QAP_T11217 refactored

--- test_cases/fx/fx_mm_positions/QAP_T11217.py
+++ test_cases/fx/fx_mm_positions/QAP_T11217.py
@@ -6,12 +6,16 @@ from test_framework.data_sets.base_data_set import BaseDataSet
 from test_framework.environments.full_environment import FullEnvironment
 from test_framework.fix_wrappers.FixManager import FixManager
 from test_framework.fix_wrappers.FixVerifier import FixVerifier
+from test_framework.fix_wrappers.forex.FixMessageExecutionReportPrevQuotedFX import \
+    FixMessageExecutionReportPrevQuotedFX
+from test_framework.fix_wrappers.forex.FixMessageNewOrderSinglePrevQuotedFX import FixMessageNewOrderSinglePrevQuotedFX
+from test_framework.fix_wrappers.forex.FixMessageQuoteFX import FixMessageQuoteFX
+from test_framework.fix_wrappers.forex.FixMessageQuoteRequestFX import FixMessageQuoteRequestFX
 from test_framework.fix_wrappers.forex.FixMessageRequestForPositionsAckFX import FixMessageRequestForPositionsAckFX
 from test_framework.fix_wrappers.forex.FixMessageRequestForPositionsFX import FixMessageRequestForPositionsFX
 from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
 from test_framework.java_api_wrappers.fx.FixPositionMaintenanceRequestFX import FixPositionMaintenanceRequestFX
 from test_framework.java_api_wrappers.fx.FixPositionMassCancelRequestFX import FixPositionMassCancelRequestFX
-from test_framework.java_api_wrappers.fx.TradeEntryRequestFX import TradeEntryRequestFX
 
 
 class QAP_T11217(TestCase):
@@ -21,16 +25,22 @@ class QAP_T11217(TestCase):
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
         self.java_api_env = self.environment.get_list_java_api_environment()[0].java_api_conn
         self.pks_connectivity = self.environment.get_list_fix_environment()[0].sell_side_pks
+        self.ss_rfq_connectivity = self.environment.get_list_fix_environment()[0].sell_side_rfq
         self.java_api_manager = JavaApiManager(self.java_api_env, self.test_id)
         self.fix_manager = FixManager(self.pks_connectivity, self.test_id)
+        self.fix_manager_gtw = FixManager(self.ss_rfq_connectivity, self.test_id)
+        self.fix_verifier_gtw = FixVerifier(self.ss_rfq_connectivity, self.test_id)
         self.fix_verifier = FixVerifier(self.pks_connectivity, self.test_id)
         self.request_for_position = FixMessageRequestForPositionsFX()
         self.position_report = FixMessageRequestForPositionsAckFX()
-        self.trade_request_wk1 = TradeEntryRequestFX()
+        self.new_order_single = FixMessageNewOrderSinglePrevQuotedFX()
+        self.execution_report = FixMessageExecutionReportPrevQuotedFX()
+        self.quote = FixMessageQuoteFX()
+        self.quote_request = FixMessageQuoteRequestFX(data_set=data_set)
+        self.cancel_request = FixPositionMassCancelRequestFX()
         self.cancel_request = FixPositionMassCancelRequestFX()
         self.client = self.data_set.get_client_by_name("client_mm_7")
         self.account = self.data_set.get_account_by_name("account_mm_7")
-        self.listing_gbp_cad_wk1 = self.data_set.get_listing_id_by_name("gbp_cad_wk1")
         self.currency = self.data_set.get_currency_by_name("currency_gbp")
         self.gbp_cad = self.data_set.get_symbol_by_name("symbol_synth_5")
         self.sec_type_fwd = self.data_set.get_security_type_by_name("fx_fwd")
@@ -45,12 +55,17 @@ class QAP_T11217(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
         # region Step 1
-        self.trade_request_wk1.set_params_for_fwd()
-        self.trade_request_wk1.update_fields_in_component("TradeEntryRequestBlock",
-                                                          {"AccountGroupID": self.client,
-                                                           "SettlDate": self.settle_date_wk1,
-                                                           "ListingID": self.listing_gbp_cad_wk1})
-        self.java_api_manager.send_message_and_receive_response(self.trade_request_wk1)
+        self.quote_request.set_rfq_params_fwd()
+        self.quote_request.update_repeating_group_by_index("NoRelatedSymbols", 0,
+                                                           Account=self.client,
+                                                           Currency=self.currency,
+                                                           Instrument=self.instrument,
+                                                           Side="2")
+        response: list = self.fix_manager_gtw.send_message_and_receive_response(self.quote_request)
+
+        self.quote.set_params_for_quote(self.quote_request)
+        self.new_order_single.set_default_prev_quoted(self.quote_request, response[0])
+        self.fix_manager_gtw.send_message_and_receive_response(self.new_order_single)
 
         # endregion
 
@@ -69,8 +84,7 @@ class QAP_T11217(TestCase):
         # endregion
 
         # region Step 4
-        self.position_report.set_params_for_none(self.request_for_position)
-
+        self.position_report.set_params_from_reqeust(self.request_for_position)
         self.fix_verifier.check_fix_message(self.position_report)
         # endregion
 
