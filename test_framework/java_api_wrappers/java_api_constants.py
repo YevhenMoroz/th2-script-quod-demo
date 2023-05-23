4f7ff3e4f894b2c3b408d21806546e91b2003305
skomanov <sofia.komanova.contractor@quodfinancial.com>
2023-05-23 09:26:21 +0200

    add SingleAllocClientAccountID field

--- test_framework/java_api_wrappers/java_api_constants.py
+++ test_framework/java_api_wrappers/java_api_constants.py
@@ -375,7 +375,7 @@ class JavaApiFields(Enum):
     AlgoType = 'AlgoType'
     LastPx = 'LastPx'
     LastTradedQty = 'LastTradedQty'
-
+    SingleAllocClientAccountID = 'SingleAllocClientAccountID'
     ComputeBookingFeesCommissionsReplyBlock = 'ComputeBookingFeesCommissionsReplyBlock'
     RootMiscFeeBasis = 'RootMiscFeeBasis'
     RootMiscFeeRate = 'RootMiscFeeRate'
