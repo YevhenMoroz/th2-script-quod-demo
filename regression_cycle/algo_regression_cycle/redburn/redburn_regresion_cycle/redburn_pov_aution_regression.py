d5d20c6d57cf34c22e08aa4a6f441783599d6d3f
Sergey Rublyov <sergey.rublyov@quodfinancial.com>
2023-05-24 11:44:46 +0300

    PALGO-1397 - add test to rb regression script

--- regression_cycle/algo_regression_cycle/redburn/redburn_regresion_cycle/redburn_pov_aution_regression.py
+++ regression_cycle/algo_regression_cycle/redburn/redburn_regresion_cycle/redburn_pov_aution_regression.py
@@ -8,6 +8,7 @@ from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4524 import QAP_T4524
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4526 import QAP_T4526
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4527 import QAP_T4527
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4534 import QAP_T4534
+from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4535 import QAP_T4535
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T4536 import QAP_T4536
 from test_cases.algo.Algo_Redburn.POV_Auction.QAP_T8719 import QAP_T8719
 from test_framework.configurations.component_configuration import ComponentConfigurationAlgo
@@ -35,6 +36,7 @@ def test_run(parent_id=None, version=None):
         QAP_T4524(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4527(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T4536(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T4535(report_id=report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
 
     except Exception:
         # bca.create_event('Fail test event', status='FAILED', parent_id=parent_id)
