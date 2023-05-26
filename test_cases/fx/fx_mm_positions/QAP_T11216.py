9f4602508262d35d09f70ba0d211092e91d164d4
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-05-26 09:17:22 +0200

    PFX-6434 - QAP_T11216 automated

--- test_cases/fx/fx_mm_positions/QAP_T11216.py
+++ test_cases/fx/fx_mm_positions/QAP_T11216.py
@@ -4,15 +4,13 @@ from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
 from test_framework.data_sets.base_data_set import BaseDataSet
 from test_framework.environments.full_environment import FullEnvironment
-from test_framework.fix_wrappers.FixManager import FixManager
-from test_framework.fix_wrappers.FixVerifier import FixVerifier
-from test_framework.fix_wrappers.forex.FixMessageRequestForPositionsAckFX import FixMessageRequestForPositionsAckFX
-from test_framework.fix_wrappers.forex.FixMessageRequestForPositionsFX import FixMessageRequestForPositionsFX
 from test_framework.java_api_wrappers.JavaApiManager import JavaApiManager
+from test_framework.java_api_wrappers.JavaApiVerifier import JavaApiVerifier
 from test_framework.java_api_wrappers.fx.FixPositionMaintenanceRequestFX import FixPositionMaintenanceRequestFX
 from test_framework.java_api_wrappers.fx.FixPositionMassCancelRequestFX import FixPositionMassCancelRequestFX
 from test_framework.java_api_wrappers.fx.FixRequestForPositionsFX import FixRequestForPositionsFX
 from test_framework.java_api_wrappers.fx.TradeEntryRequestFX import TradeEntryRequestFX
+from test_framework.java_api_wrappers.pks_messages.FixRequestForPositionsAck import FixRequestForPositionsAck
 
 
 class QAP_T11216(TestCase):
@@ -21,65 +19,47 @@ class QAP_T11216(TestCase):
         super().__init__(report_id, session_id, data_set, environment)
         self.test_id = bca.create_event(Path(__file__).name[:-3], self.report_id)
         self.java_api_env = self.environment.get_list_java_api_environment()[0].java_api_conn
-        self.pks_connectivity = self.environment.get_list_fix_environment()[0].sell_side_pks
         self.java_api_manager = JavaApiManager(self.java_api_env, self.test_id)
-        self.fix_manager = FixManager(self.pks_connectivity, self.test_id)
-        self.fix_verifier = FixVerifier(self.pks_connectivity, self.test_id)
+        self.java_api_verifier = JavaApiVerifier(self.java_api_env, self.test_id)
         self.request_for_position = FixRequestForPositionsFX()
-        self.position_report = FixMessageRequestForPositionsAckFX()
-        self.trade_request_wk1 = TradeEntryRequestFX()
+        self.report = FixRequestForPositionsAck()
+        self.trade_request = TradeEntryRequestFX()
         self.cancel_request = FixPositionMassCancelRequestFX()
         self.client = self.data_set.get_client_by_name("client_mm_7")
         self.account = self.data_set.get_account_by_name("account_mm_7")
-        self.listing_gbp_cad_wk1 = self.data_set.get_listing_id_by_name("gbp_cad_wk1")
         self.currency = self.data_set.get_currency_by_name("currency_gbp")
         self.gbp_cad = self.data_set.get_symbol_by_name("symbol_synth_5")
-        self.sec_type_fwd = self.data_set.get_security_type_by_name("fx_fwd")
-        self.sec_type_java = self.data_set.get_fx_instr_type_ja("fx_fwd")
+        self.sec_type_java = self.data_set.get_fx_instr_type_ja("fx_spot")
         self.instrument = {
-            "SecurityType": self.sec_type_fwd,
-            "Symbol": self.gbp_cad
+            "InstrType": self.sec_type_java,
+            "InstrSymbol": self.gbp_cad
         }
-        self.settle_date_wk1 = self.data_set.get_settle_date_by_name("wk1_java_api")
-        self.maintenance_request = FixPositionMaintenanceRequestFX()
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
         # region Step 1
-        # self.trade_request_wk1.set_params_for_fwd()
-        # self.trade_request_wk1.update_fields_in_component("TradeEntryRequestBlock",
-        #                                                   {"AccountGroupID": self.client,
-        #                                                    "SettlDate": self.settle_date_wk1,
-        #                                                    "ListingID": self.listing_gbp_cad_wk1})
-        # self.java_api_manager.send_message_and_receive_response(self.trade_request_wk1)
-        #
-        # # endregion
-        #
-        # # region Step 2
-        # self.cancel_request.set_params(self.account)
-        # self.java_api_manager.send_message(self.cancel_request)
-        # self.sleep(5)
-        #
-        # # endregion
+        self.trade_request.set_default_params()
+        self.trade_request.update_fields_in_component("TradeEntryRequestBlock", {"ClientAccountGroupID": self.client})
+        self.trade_request.change_instrument(self.gbp_cad, self.sec_type_java)
+        self.java_api_manager.send_message_and_receive_response(self.trade_request)
+        # endregion
+
+        # region Step 2
+        self.cancel_request.set_params(self.account)
+        self.java_api_manager.send_message(self.cancel_request)
+        self.sleep(5)
+        # endregion
 
         # region Step 3
         self.request_for_position.set_default_params()
-        response = self.java_api_manager.send_message_and_receive_response(self.request_for_position)
-        for i in response:
-            print(i.get_parameters())
-        # print(response.get_parameters())
-        self.sleep(1)
-        # self.request_for_position.set_default_params()
-        # self.request_for_position.change_parameters({"Instrument": self.instrument, "Currency": self.currency,
-        #                                              "Account": self.client})
-        # self.java_api_manager.send_message_and_receive_response(self.request_for_position, self.test_id)
-        # # endregion
-
-        # # region Step 4
-        # self.position_report.set_params_for_none(self.request_for_position)
-        #
-        # self.fix_verifier.check_fix_message(self.position_report)
-        # # endregion
+        self.request_for_position.change_client(self.client)
+        self.request_for_position.change_instrument(self.instrument, self.currency)
+        self.java_api_manager.send_message_and_receive_response(self.request_for_position)
+        # endregion
+        # region Step 4
+        self.report.set_params_from_request_sub(self.request_for_position)
+        self.java_api_verifier.check_java_message(self.report)
+        # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_post_conditions(self):
