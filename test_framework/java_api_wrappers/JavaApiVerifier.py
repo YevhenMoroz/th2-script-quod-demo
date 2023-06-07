f386b8f9d50beabd4c3ed2e60fabd58c7a0a791c
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-06-07 08:53:19 +0200

    Add new msg to verifier

--- test_framework/java_api_wrappers/JavaApiVerifier.py
+++ test_framework/java_api_wrappers/JavaApiVerifier.py
@@ -5,7 +5,7 @@ import re
 from copy import deepcopy
 from typing import Union
 
-from test_framework.data_sets.message_types import PKSMessageType
+from test_framework.data_sets.message_types import PKSMessageType, MPASMessageType
 from test_framework.fix_wrappers.DataSet import DirectionEnum
 from custom import basic_custom_actions
 from test_framework.java_api_wrappers.JavaApiMessage import JavaApiMessage
@@ -49,7 +49,24 @@ class JavaApiVerifier:
                     Direction.Value(direction.value)
                 )
             )
+        elif java_message.get_message_type() == MPASMessageType.AutoHedgerInstrSymbolBatchUpdate.value:
+            if key_parameters is None:
+                key_parameters = ["AutoHedgerID"]
 
+            if message_name is None:
+                message_name = "Check AutoHedgerInstrSymbolBatchUpdateBlock  message"
+            self.__verifier.submitCheckRule(
+                basic_custom_actions.create_check_rule(
+                    message_name,
+                    basic_custom_actions.filter_to_grpc(f"{MPASMessageType.AutoHedgerInstrSymbolBatchUpdate.value}",
+                                                        java_message.get_parameters(),
+                                                        key_parameters, ignored_fields),
+                    self.__checkpoint,
+                    self.__session_alias,
+                    self.__case_id,
+                    Direction.Value(direction.value)
+                )
+            )
         else:
             pass
 # TODO add exeption into els
