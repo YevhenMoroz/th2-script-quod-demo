fd671270f1a2926066400c89e77d712604c92c11
Vadim <vadim.skulinec.contractor@quodfinancial.com>
2023-06-08 17:27:49 +0300

    PEQ-11480 QAP_T7577 QAP_T7583 update 1.2

--- test_cases/eq/Positions/QAP_T7577.py
+++ test_cases/eq/Positions/QAP_T7577.py
@@ -105,5 +105,5 @@ class QAP_T7577(TestCase):
             get_parameters()[JavaApiFields.RequestForPositionsAckBlock.value][JavaApiFields.PositionReportBlock.value] \
             [JavaApiFields.PositionList.value][JavaApiFields.PositionBlock.value]
         for position_record in request_for_position_ack:
-            if self.instrument_id == position_record[JavaApiFields.InstrID.value]:
+            if self.instrument_id == position_record[JavaApiFields.InstrID.value] and position_record[JavaApiFields.PositionType.PositionList]=='N':
                 return position_record
