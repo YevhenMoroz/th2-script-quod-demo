74941f616ae32709f86a109d599e338b9f611d72
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-06-16 12:06:36 +0200

    PFX-6547 - add logging traceback to TH2 report

--- test_framework/core/try_exept_decorator.py
+++ test_framework/core/try_exept_decorator.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 from functools import wraps
 from custom import basic_custom_actions as bca
 
@@ -13,13 +14,19 @@ def try_except(test_id):
             try:
                 return decorated_function(*args, **kwargs)
             except Exception as e:
-                logger.exception(
-                    f"Exception raised in {test_id}, func {decorated_function.__name__} raised exception: {str(e)}")
-                bca.create_event(f'Fail test event on the step - {decorated_function.__name__.upper()}',
-                                 status='FAILED',
-                                 parent_id=args[0].__dict__['test_id'],
-                                 body=str(e))
+                error_message = f"Exception raised in {test_id}, func {decorated_function.__name__.upper()} raised exception - {str(e)}"
+                error_traceback = traceback.format_exc()
+                full_error_message = f"{error_message}\n\n{error_traceback}"
+                logger.exception(error_message)
+                bca.create_event(f"Fail test event on the step - {decorated_function.__name__.upper()}",
+                                 status="FAILED",
+                                 parent_id=args[0].__dict__["test_id"],
+                                 body=full_error_message)
+
+
             finally:
                 pass
+
         return improved_function
+
     return get_function
