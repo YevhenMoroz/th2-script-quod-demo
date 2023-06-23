b160db6d332c4bcc078fbed35574ba1ad699695b
ostronov <oleksandr.stronov.contractor@quodfinancial.com>
2023-06-23 17:21:15 +0200

    Added new cases to cycle

--- regression_cycle/fx_regression_cycle/mm_positions_regression.py
+++ regression_cycle/fx_regression_cycle/mm_positions_regression.py
@@ -1,6 +1,9 @@
+from test_cases.fx.fx_mm_positions.QAP_T10342 import QAP_T10342
 from test_cases.fx.fx_mm_positions.QAP_T10400 import QAP_T10400
 from test_cases.fx.fx_mm_positions.QAP_T10411 import QAP_T10411
 from test_cases.fx.fx_mm_positions.QAP_T10457 import QAP_T10457
+from test_cases.fx.fx_mm_positions.QAP_T10460 import QAP_T10460
+from test_cases.fx.fx_mm_positions.QAP_T10461 import QAP_T10461
 from test_cases.fx.fx_mm_positions.QAP_T10612 import QAP_T10612
 from test_cases.fx.fx_mm_positions.QAP_T10613 import QAP_T10613
 from test_cases.fx.fx_mm_positions.QAP_T10614 import QAP_T10614
@@ -35,32 +38,37 @@ timeouts = False
 channels = dict()
 
 
-def test_run(parent_id=None, version="5.1.178.193"):
+def test_run(parent_id=None, version="5.1.178.194"):
     # report_id = bca.create_event(f"FX_MM_Positions" if version is None else f"FX_MM_Positions | {version}", parent_id)
     report_id = bca.create_event(f"FX_MM_Positions | {version}", parent_id)
-    configuration = ComponentConfiguration("ESP_MM")
+    configuration = ComponentConfiguration("Position")
     try:
         # prepare_position()
         # QAP_T11053(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
-        QAP_T11080(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
-        QAP_T10649(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+
+
         QAP_T2932(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T2933(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T2934(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T2935(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         #
         QAP_T9408(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10342(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10400(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10411(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10457(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10460(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10461(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10612(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10613(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10614(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10635(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10636(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T10649(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10760(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10840(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T10845(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
+        QAP_T11080(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T11216(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T11217(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
         QAP_T11218(report_id, data_set=configuration.data_set, environment=configuration.environment).execute()
