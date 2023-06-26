e8fc430c72a1c08667ebb02c15a65655db8cf1a2
Vadim <vadim.skulinec.contractor@quodfinancial.com>
2023-06-26 17:23:15 +0300

    PEQ-12095 QAP-T11154 update 1.1

--- test_cases/eq/DMA/QAP_T11154.py
+++ test_cases/eq/DMA/QAP_T11154.py
@@ -3,7 +3,6 @@ import time
 from pathlib import Path
 
 from custom import basic_custom_actions as bca
-from custom.basic_custom_actions import timestamps
 from rule_management import RuleManager, Simulators
 from test_framework.core.test_case import TestCase
 from test_framework.core.try_exept_decorator import try_except
