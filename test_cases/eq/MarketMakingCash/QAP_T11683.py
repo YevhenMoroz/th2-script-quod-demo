14dcc6ecda9945e5298058e87f7fedc51d8d6d51
ymoroz <yevhen.moroz.contractor@quodfinancial.com>
2023-06-27 16:57:42 +0300

    PEQ-11957 add QAP_T11683

--- test_cases/eq/MarketMakingCash/QAP_T11683.py
+++ test_cases/eq/MarketMakingCash/QAP_T11683.py
@@ -23,7 +23,7 @@ def print_message(message, responses):
         logger.info(i.get_parameters())
 
 
-class QAP_T11108(TestCase):
+class QAP_T11683(TestCase):
     @try_except(test_id=Path(__file__).name[:-3])
     def __init__(self, report_id, session_id, data_set, environment):
         super().__init__(report_id, session_id, data_set, environment)
@@ -32,13 +32,12 @@ class QAP_T11108(TestCase):
         self.java_api_connectivity = self.java_api = self.environment.get_list_java_api_environment()[0].java_api_conn
         self.java_api_manager = JavaApiManager(self.java_api_connectivity, self.test_id)
         self.quote_request = QuoteRequest(data_set=self.data_set)
-        self.route_id =  self.data_set.get_route_id_by_name("route_1")
         # endregion
 
     @try_except(test_id=Path(__file__).name[:-3])
     def run_pre_conditions_and_steps(self):
-        self.quote_request.set_default(route_id=self.route_id)
+        self.quote_request.set_default()
         self.java_api_manager.send_message_and_receive_response(self.quote_request)
         quote_notify = self.java_api_manager.get_last_message(ORSMessageType.QuoteRequestNotif.value).get_parameters()[
             "QuoteRequestNotifBlock"]
-        self.java_api_manager.compare_values({"QuoteRequestStatus":"NEW"}, quote_notify, "Verify QuoteRequestStatus")
+        self.java_api_manager.compare_values({"QuoteRequestStatus":"REJ"}, quote_notify, "Verify QuoteRequestStatus")
--- test_framework/java_api_wrappers/ors_messages/QuoteRequest.py
+++ test_framework/java_api_wrappers/ors_messages/QuoteRequest.py
@@ -13,18 +13,20 @@ class QuoteRequest(JavaApiMessage):
         super().change_parameters(parameters)
         self.data_set = data_set
 
-    def set_default(self, side="B", qty="100") -> None:
+    def set_default(self, qty="100", side=None, route_id=None) -> None:
         base_parameters = {
             'SEND_SUBJECT': 'QUOD.ORS.FE',
             'REPLY_SUBJECT': 'QUOD.FE.ORS',
             "QuoteRequestBlock": {
                 'QuoteReqList': {'QuoteReqBlock': [{
-                    "Side": side,
                     "OrdQty": qty,
                     "InstrID": self.data_set.get_instrument_id_by_name("instrument_1"),
                     "ListingID": self.data_set.get_listing_id_by_name("listing_1"),
                 }]},
                 "QuoteReqBatchID": f"QuoteReqBatchID-{basic_custom_actions.client_orderid(9)}",
-                "RouteID": self.data_set.get_route_id_by_name("route_1")
             }}
+        if side is not None:
+            base_parameters["QuoteRequestBlock"]["QuoteReqList"]["QuoteReqBlock"][0]["Side"] = side
+        if route_id is not None:
+            base_parameters["QuoteRequestBlock"]["RouteID"] = route_id
         super().change_parameters(base_parameters)
